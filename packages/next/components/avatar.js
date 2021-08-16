388df36abcbc36eea6b52b5293285bbf2b0b9eb3
qiyisi <qiyisi@qq.com>
2021-08-16 14:28:23 +0800

    Ref #51, show kusama avatar

--- packages/next/components/avatar.js
+++ packages/next/components/avatar.js
@@ -2,6 +2,6 @@ import * as React from "react";
 import Identicon from "@polkadot/react-identicon";
 
 export default function Avatar({ address, size = 24 }) {
-  const theme = "polkadot";
+  const theme = "kusama";
   return <Identicon value={address} size={size} theme={theme} />;
 }
