e2c63b1840e915c988c4af6b89408cace173cc74
qiyisi <qiyisi@qq.com>
2021-08-19 16:32:24 +0800

    Update footer

--- packages/next/pages/[chain]/discussions.js
+++ packages/next/pages/[chain]/discussions.js
@@ -8,45 +8,37 @@ import { withLoginUser, withLoginUserRedux } from "../../lib";
 import nextApi from "../../services/nextApi";
 import { EmptyList } from "../../utils/constants";
 
-export default withLoginUserRedux(
-  ({ loginUser, posts, chain }) => {
+export default withLoginUserRedux(({ loginUser, posts, chain }) => {
+  const items = (posts.items || []).map((post) => ({
+    time: post.lastActivityAt,
+    comments: post.commentsCount,
+    title: post.title,
+    author: post.author.username,
+    authorEmailMd5: post.author.emailMd5,
+    postUid: post.postUid,
+    ...(post.author.addresses
+      ? { address: post.author.addresses?.[0]?.address ?? null }
+      : {}),
+  }));
 
-    const items = (posts.items || []).map((post) =>({
-      time: post.lastActivityAt,
-      comments: post.commentsCount,
-      title: post.title,
-      author: post.author.username,
-      authorEmailMd5: post.author.emailMd5,
-      postUid: post.postUid,
-      ...(post.author.addresses
-        ? { address: post.author.addresses?.[0]?.address ?? null }
-        : {}),
-     }));
-
-    return (
-      <Layout
-        user={loginUser}
-        left={<Menu menu={mainMenu} />}
-        right={
-          <>
-            <Trends user={loginUser} chain={chain} />
-            <Footer />
-          </>
-        }
-      >
-        <List
-          category={"Discussion"}
-          items={items}
-          pagination={{
-            page: posts.page,
-            pageSize: posts.pageSize,
-            total: posts.total,
-          }}
-        />
-      </Layout>
-    );
-  }
-);
+  return (
+    <Layout
+      user={loginUser}
+      left={<Menu menu={mainMenu} />}
+      right={<Trends user={loginUser} chain={chain} />}
+    >
+      <List
+        category={"Discussion"}
+        items={items}
+        pagination={{
+          page: posts.page,
+          pageSize: posts.pageSize,
+          total: posts.total,
+        }}
+      />
+    </Layout>
+  );
+});
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const { page, chain } = context.query;
