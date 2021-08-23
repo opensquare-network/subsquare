ece6f8c2664f323bbf584ae36cd523d524314d1b
yoshiyuki <brother-zhou@qq.com>
2021-08-23 16:51:47 +0800

    feature: add new createLink

--- packages/next/pages/[chain]/discussions.js
+++ packages/next/pages/[chain]/discussions.js
@@ -1,12 +1,25 @@
 import List from "components/list";
 import Layout from "components/layout";
 import Menu from "components/menu";
-import { mainMenu } from "utils/constants";
-import { withLoginUser, withLoginUserRedux } from "../../lib";
+import {mainMenu} from "utils/constants";
+import {withLoginUser, withLoginUserRedux} from "../../lib";
 import nextApi from "../../services/nextApi";
-import { EmptyList } from "../../utils/constants";
+import {EmptyList} from "../../utils/constants";
+import styled from "styled-components";
+import PlusIcon from "../../public/imgs/icons/plusInCircle.svg";
 
-export default withLoginUserRedux(({ loginUser, posts, chain }) => {
+const Create = styled.a`
+  display: flex;
+  align-items: center;
+  color: #6848FF;
+
+  svg {
+    margin-right: 8px;
+  }
+  cursor: pointer;
+`
+
+export default withLoginUserRedux(({loginUser, posts, chain}) => {
   const items = (posts.items || []).map((post) => ({
     chain: post.chain,
     time: post.lastActivityAt,
@@ -16,17 +29,20 @@ export default withLoginUserRedux(({ loginUser, posts, chain }) => {
     authorEmailMd5: post.author.emailMd5,
     postUid: post.postUid,
     ...(post.author.addresses
-      ? { address: post.author.addresses?.[0]?.address ?? null }
+      ? {address: post.author.addresses?.[0]?.address ?? null}
       : {}),
   }));
+
+  const create = <Create href="post/create"><PlusIcon/>New Post</Create>;
 
   return (
     <Layout
       user={loginUser}
-      left={<Menu menu={mainMenu} />}
+      left={<Menu menu={mainMenu}/>}
     >
       <List
         category={"Discussion"}
+        create={create}
         items={items}
         pagination={{
           page: posts.page,
@@ -39,10 +55,10 @@ export default withLoginUserRedux(({ loginUser, posts, chain }) => {
 });
 
 export const getServerSideProps = withLoginUser(async (context) => {
-  const { page, chain } = context.query;
+  const {page, chain} = context.query;
 
-  const [{ result: posts }] = await Promise.all([
-    nextApi.fetch(`posts`, { chain, page, pageSize: 2 }),
+  const [{result: posts}] = await Promise.all([
+    nextApi.fetch(`posts`, {chain, page, pageSize: 2}),
   ]);
 
   return {
