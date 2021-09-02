5a9322659c35ef85ce8a98b54282ede3c656639e
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-02 15:37:15 +0800

    Remove old way

--- packages/scan/src/business/common/treasuryProposal/meta.js
+++ packages/scan/src/business/common/treasuryProposal/meta.js
@@ -2,22 +2,6 @@ const { expandMetadata } = require("@polkadot/types");
 const { findMetadata } = require("../../../mongo/service/specs");
 const { getApi } = require("../../../api");
 
-async function getMeta(blockHash, proposalIndex) {
-  const api = await getApi();
-  // Some very early proposal meta of Kusama can not be got in this way
-  const rawMeta = await api.query.treasury.proposals.at(
-    blockHash,
-    proposalIndex
-  );
-  let meta = rawMeta.toJSON();
-
-  if (!meta) {
-    throw new Error("Can not find treasury proposal meta");
-  }
-
-  return meta;
-}
-
 async function getTreasuryProposalMeta(
   proposalIndex,
   { blockHeight, blockHash }
