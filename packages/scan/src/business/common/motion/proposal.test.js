7cce661ed47d8b755524249c686e662e52e3e2ae
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-02 21:21:19 +0800

    Add test case for kusama treasury#approveProposal motion

--- packages/scan/src/business/common/motion/proposal.test.js
+++ packages/scan/src/business/common/motion/proposal.test.js
@@ -7,6 +7,19 @@ const { setChain, CHAINS } = require("../../../env");
 
 jest.setTimeout(3000000);
 
+const ksmTargetCall = {
+  callIndex: "0x1202",
+  section: "treasury",
+  method: "approveProposal",
+  args: [
+    {
+      name: "proposal_id",
+      type: "Compact<ProposalIndex>",
+      value: 0,
+    },
+  ],
+};
+
 const targetCall = {
   callIndex: "0x3c01",
   section: "authority",
@@ -108,5 +121,34 @@ describe("test get karura motion proposal", () => {
 
     const proposalRaw = await getMotionProposalCall(motionHash, indexer);
     expect(proposalRaw).toEqual(targetCall);
+  });
+});
+
+describe("test get kusama motion proposal", () => {
+  let api;
+  let provider;
+
+  beforeAll(async () => {
+    provider = new WsProvider("wss://kusama.api.onfinality.io/public-ws", 1000);
+    api = await ApiPromise.create({ provider });
+    setApi(api);
+    setChain(CHAINS.KUSAMA);
+  });
+
+  afterAll(async () => {
+    await provider.disconnect();
+  });
+
+  test("works", async () => {
+    const blockHeight = 126209;
+    setSpecHeights([blockHeight]);
+    const blockHash = await api.rpc.chain.getBlockHash(blockHeight);
+    const indexer = { blockHash, blockHeight };
+
+    const motionHash =
+      "0x59fe7bd64951667f91f36db33077b1ada93b093b363a32cf869d2a833d72ce08";
+
+    const normalizedProposal = await getMotionProposalCall(motionHash, indexer);
+    expect(normalizedProposal).toEqual(ksmTargetCall);
   });
 });
