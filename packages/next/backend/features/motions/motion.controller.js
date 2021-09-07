29471e52d1ceaf15cf2d3def056ca9bf8bd5272e
chaojun <chaojun@opensquare.network>
2021-09-07 17:10:13 +0800

    Refs #315, Update motions API

--- packages/next/backend/features/motions/motion.controller.js
+++ packages/next/backend/features/motions/motion.controller.js
@@ -1,48 +1,7 @@
-const { HttpError } = require("../../exc");
-const postService = require("../../services/post.service")("motion");
-const motionPostService = require("../../services/motion.service");
-const { ContentType } = require("../../constants");
+const motionService = require("../../services/motion.service");
 const { extractPage } = require("../../utils");
 
-async function updatePost(ctx) {
-  const { chain, postId } = ctx.params;
-  const {
-    title,
-    content,
-    contentType: paramContentType,
-  } = ctx.request.body;
-
-  if (!title) {
-    throw new HttpError(400, { title: ["Post title is missing"] });
-  }
-
-  if (!content) {
-    throw new HttpError(400, { content: ["Post content is missing"] });
-  }
-
-  if (
-    paramContentType !== undefined &&
-    paramContentType !== ContentType.Markdown &&
-    paramContentType !== ContentType.Html
-  ) {
-    throw new HttpError(400, { contentType: ["Unknown content type"] });
-  }
-
-  const contentType = paramContentType
-    ? paramContentType
-    : ContentType.Markdown;
-
-  ctx.body = await motionPostService.updatePost(
-    chain,
-    postId,
-    title,
-    content,
-    contentType,
-    ctx.user
-  );
-}
-
-async function getPosts(ctx) {
+async function getMotions(ctx) {
   const { chain } = ctx.params;
 
   const { page, pageSize } = extractPage(ctx);
@@ -51,85 +10,16 @@ async function getPosts(ctx) {
     return;
   }
 
-  ctx.body = await motionPostService.getPostsByChain(chain, page, pageSize);
+  ctx.body = await motionService.getMotionsByChain(chain, page, pageSize);
 }
 
-async function getPostById(ctx) {
-  const { chain, postId } = ctx.params;
+async function getMotionById(ctx) {
+  const { chain, motionId } = ctx.params;
 
-  ctx.body = await motionPostService.getPostById(chain, postId);
+  ctx.body = await motionService.getMotionById(chain, motionId);
 }
-
-async function postComment(ctx) {
-  const { chain, postId } = ctx.params;
-  const { content, contentType: paramContentType } = ctx.request.body;
-
-  if (!content) {
-    throw new HttpError(400, { content: ["Comment content is missing"] });
-  }
-
-  if (
-    paramContentType !== undefined &&
-    paramContentType !== ContentType.Markdown &&
-    paramContentType !== ContentType.Html
-  ) {
-    throw new HttpError(400, { contentType: ["Unknown content type"] });
-  }
-
-  const contentType = paramContentType
-    ? paramContentType
-    : ContentType.Markdown;
-
-  ctx.body = await postService.postComment(
-    chain,
-    postId,
-    content,
-    contentType,
-    ctx.user
-  );
-}
-
-async function getComments(ctx) {
-  const { page, pageSize } = extractPage(ctx);
-  if (pageSize === 0 || page < 1) {
-    ctx.status = 400;
-    return;
-  }
-
-  const { chain, postId } = ctx.params;
-  ctx.body = await postService.getComments(chain, postId, page, pageSize);
-}
-
-async function setPostReaction(ctx) {
-  const { chain, postId } = ctx.params;
-  const { reaction } = ctx.request.body;
-
-  if (reaction === undefined) {
-    throw new HttpError(400, "Reaction is missing");
-  }
-
-  const user = ctx.user;
-  ctx.body = await postService.setPostReaction(
-    chain,
-    postId,
-    reaction,
-    user
-  );
-}
-
-async function unsetPostReaction(ctx) {
-  const { chain, postId } = ctx.params;
-  const user = ctx.user;
-  ctx.body = await postService.unsetPostReaction(chain, postId, user);
-}
-
 
 module.exports = {
-  updatePost,
-  getPosts,
-  getPostById,
-  postComment,
-  getComments,
-  setPostReaction,
-  unsetPostReaction,
+  getMotions,
+  getMotionById,
 };
