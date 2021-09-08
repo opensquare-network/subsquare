23f886a264e0858c48088c527664d98d4adea149
Yongfeng LI <wliyongfeng@gmail.com>
2021-09-08 17:22:00 +0800

    Fix TechComm proposal test

--- packages/scan/src/business/common/techComm/proposal.test.js
+++ packages/scan/src/business/common/techComm/proposal.test.js
@@ -1,3 +1,4 @@
+const { testTechCommProposal } = require("./testData");
 const { getTechCommMotionProposalCall } = require("./proposalStorage");
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 const { typesBundleForPolkadot } = require("@acala-network/type-definitions");
@@ -26,7 +27,7 @@ describe("test get karura Tech Comm", () => {
     await provider.disconnect();
   });
 
-  test("", async () => {
+  test("motion proposal works", async () => {
     const blockHeight = 212454;
     setSpecHeights([blockHeight]);
     const blockHash = await api.rpc.chain.getBlockHash(blockHeight);
@@ -36,6 +37,6 @@ describe("test get karura Tech Comm", () => {
       "0xca04d33103a7764c1f68dbbdbb0f42209de4b96673e1210b68b6f46feeecf700";
 
     const proposal = await getTechCommMotionProposalCall(motionHash, indexer);
-    expect(proposal).toEqual({});
+    expect(proposal).toEqual(testTechCommProposal);
   });
 });
