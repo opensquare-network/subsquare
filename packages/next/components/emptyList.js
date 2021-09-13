34f26444b8c28a7c816b59f7916129bfbf7e70dc
qiyisi <qiyisi@qq.com>
2021-09-13 15:17:17 +0800

    Ref #410, update height

--- packages/next/components/emptyList.js
+++ packages/next/components/emptyList.js
@@ -4,12 +4,11 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 143px;
   background: #ffffff;
   border: 1px solid #ebeef4;
   box-shadow: 0 6px 7px rgba(30, 33, 52, 0.02),
-  0 1.34018px 1.56354px rgba(30, 33, 52, 0.0119221),
-  0 0.399006px 0.465507px rgba(30, 33, 52, 0.00807786);
+    0 1.34018px 1.56354px rgba(30, 33, 52, 0.0119221),
+    0 0.399006px 0.465507px rgba(30, 33, 52, 0.00807786);
   border-radius: 6px;
   padding: 24px;
 
@@ -17,13 +16,15 @@ const Wrapper = styled.div`
   font-weight: normal;
   font-size: 14px;
   text-align: center;
-  color: #9DA9BB;
+  color: #9da9bb;
 `;
 
-export default function EmptyList({type = "discussions"}) {
+export default function EmptyList({ type = "discussions" }) {
   return (
     <Wrapper>
-      <span>No current <span style={{textTransform: "lowercase"}}>{type}</span></span>
+      <span>
+        No current <span style={{ textTransform: "lowercase" }}>{type}</span>
+      </span>
     </Wrapper>
   );
 }
