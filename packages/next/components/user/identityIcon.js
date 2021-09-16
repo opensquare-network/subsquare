25f242a1f07737624c27321587958f89dfd94ccb
yoshiyuki <brother-zhou@qq.com>
2021-09-16 15:29:45 +0800

    refs#558 use a new identity API

--- packages/next/components/user/identityIcon.js
+++ packages/next/components/user/identityIcon.js
@@ -3,14 +3,16 @@ import SubIcon from "./icons/sub.svg";
 import ErrorIcon from "./icons/error.svg";
 import UnauthorizedIcon from "./icons/error-grey.svg";
 import SubGreyIcon from "./icons/sub-grey.svg";
+import SubRedIcon from "./icons/sub-red.svg";
 
 export default function IdentityIcon({ identity }) {
   const statusIconMap = new Map([
-    ["authorized", AuthIcon],
-    ["LINKED", SubIcon],
-    ["error", ErrorIcon],
-    ["unauthorized", UnauthorizedIcon],
-    ["unauthorized-sub", SubGreyIcon],
+    ["NOT_VERIFIED", UnauthorizedIcon],
+    ["VERIFIED", AuthIcon],
+    ["ERRONEOUS", ErrorIcon],
+    ["VERIFIED_LINKED", SubIcon],
+    ["NOT_VERIFIED_LINKED", SubGreyIcon],
+    ["ERRONEOUS_LINKED", SubRedIcon],
   ]);
 
   const StatusIcon = statusIconMap.get(identity?.info?.status) ?? ErrorIcon;
