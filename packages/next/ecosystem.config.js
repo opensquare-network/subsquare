3d258697649654a13426acfd2167e6aa54f415fc
chaojun <chaojun@opensquare.network>
2021-09-22 16:41:08 +0800

    Refs #604, update cron config

--- packages/next/ecosystem.config.js
+++ packages/next/ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
       name: "subsquare-time-updater-karura",
       script: "backend/scripts/update-post-time.js",
       log_date_format: "YYYY-MM-DD HH:mm Z",
+      cron_restart: "*/30 * * * * *",
+      autorestart: false,
       env: {
         NODE_ENV: "development",
         CHAIN: "karura"
