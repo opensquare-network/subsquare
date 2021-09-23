42aef6d757d567d27c1f5ead15f038b41828ea4e
yoshiyuki <brother-zhou@qq.com>
2021-09-23 20:12:22 +0800

    lint: disable React import rule (we dont need it for a next.js project)

--- packages/next/.eslintrc.js
+++ packages/next/.eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "accessor-pairs": "error",
     "array-bracket-newline": "off",
     "array-bracket-spacing": ["error", "never"],
