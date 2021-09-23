43e8ae4fb43f7a66397c7f51bb40add92cd7f110
yoshiyuki <brother-zhou@qq.com>
2021-09-23 11:13:59 +0800

    init: create styled-components css lib

---
+++ packages/next/styles/componentCss.js
@@ -0,0 +1,8 @@
+import { css } from "styled-components";
+
+export const shadow_100 = css`
+  background: #c4c4c4;
+  box-shadow: 0px 6px 7px rgba(30, 33, 52, 0.02),
+    0px 1.34018px 1.56354px rgba(30, 33, 52, 0.0119221),
+    0px 0.399006px 0.465507px rgba(30, 33, 52, 0.00807786);
+`;
