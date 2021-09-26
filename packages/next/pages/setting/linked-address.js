6c1b6a0a7c14b694f09facaf2bdf44e66d9bf20f
yoshiyuki <brother-zhou@qq.com>
2021-09-26 10:15:26 +0800

    fix: lint error

--- packages/next/pages/setting/linked-address.js
+++ packages/next/pages/setting/linked-address.js
@@ -3,7 +3,6 @@ import Menu from "components/menu";
 import { settingMenu } from "utils/constants";
 import dynamic from "next/dynamic";
 import {withLoginUser, withLoginUserRedux} from "../../lib";
-import Layout from "../../components/layout";
 
 const LinkedAddressComp = dynamic(() => import("components/linkedAddress"), {
   ssr: false,
