6e006761bf3fc6c109e7188a9f87487607f3cd6b
Yongfeng LI <wliyongfeng@gmail.com>
2021-10-19 12:13:45 +0800

    Upgrade karura types

--- packages/scan/src/play.js
+++ packages/scan/src/play.js
@@ -1,21 +1,19 @@
 const { scanNormalizedBlock } = require("./scan/block");
 const { getApi } = require("./api");
-const { getRegistryByHeight } = require("./utils/registry");
 const { setSpecHeights } = require("./specs");
 
 async function test() {
-  const blockHeights = [231979];
+  const blockHeights = [231979, 233248, 233286, 237665];
 
   for (const height of blockHeights) {
     setSpecHeights([height - 1]);
 
     const api = await getApi();
-    const registry = await getRegistryByHeight(height);
     const blockHash = await api.rpc.chain.getBlockHash(height);
     const block = await api.rpc.chain.getBlock(blockHash);
     const allEvents = await api.query.system.events.at(blockHash);
 
-    await scanNormalizedBlock(registry, block.block, allEvents);
+    await scanNormalizedBlock(block.block, allEvents);
   }
 }
 
