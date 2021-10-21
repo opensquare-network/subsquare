0016b7a7f918ec4472c8c76524eecf42aea7eb38
yoshiyuki <634554815@qq.com>
2021-10-21 17:41:19 +0800

    add khala configuaration to @polkadot/api (#684)

--- packages/next/services/polkadotApi.js
+++ packages/next/services/polkadotApi.js
@@ -1,14 +1,18 @@
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { typesBundleForPolkadot } from "@acala-network/type-definitions";
+const { khala } = require("@phala/typedefs");
 
 const apiInstanceMap = new Map();
 
 export const getApi = async (chain, queryUrl) => {
   if (!apiInstanceMap.has(queryUrl)) {
     const provider = new WsProvider(queryUrl, 1000);
-    const options = { provider };
+    const options = {provider};
     if (chain === "karura") {
-      options.typesBundle = { ...typesBundleForPolkadot };
+      options.typesBundle = {...typesBundleForPolkadot};
+    }
+    if (chain === "khala") {
+      options.types = khala;
     }
 
     apiInstanceMap.set(queryUrl, ApiPromise.create(options));
