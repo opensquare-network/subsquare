a7f05fb189140a6b0ddbf90aa5fdb2e755e9ca89
yoshiyuki <brother-zhou@qq.com>
2021-10-25 12:15:48 +0800

    refs #695 display hex data correctly

--- packages/next/components/motion/motionProposal.js
+++ packages/next/components/motion/motionProposal.js
@@ -5,6 +5,8 @@ import dynamic from "next/dynamic";
 import InnerDataTable from "../table/innerDataTable";
 import BigNumber from "bignumber.js";
 import { hexToString } from "@polkadot/util";
+import { addressEllipsis } from "../../utils";
+const DownloadText = dynamic(() => import("../downloadText"),{ ssr: false })
 
 const JsonView = dynamic(
   () => import("components/jsonView").catch((e) => console.error(e)),
@@ -69,6 +71,11 @@ const TagItem = styled.div`
     `}
 `;
 
+const Between = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
 function convertProposalForTableView(proposal, chain) {
   if (!proposal) {
     return {};
@@ -90,7 +97,13 @@ function convertProposalForTableView(proposal, chain) {
             ];
           }
           case "Bytes": {
-            return [arg.name, hexToString(arg.value)];
+            if(arg.value?.length <= 200){
+              return [arg.name, addressEllipsis(arg.value)];
+            }
+            return [arg.name, <Between key="0">
+              {addressEllipsis(arg.value)}
+              <DownloadText text={arg.value}/>
+            </Between>];
           }
           case "Balance": {
             const value = new BigNumber(arg.value).toString();
@@ -124,7 +137,10 @@ function convertProposalForJsonView(proposal, chain) {
             return arg.value.map((v) => convertProposalForJsonView(v, chain));
           }
           case "Bytes": {
-            return hexToString(arg.value);
+            if(arg.value?.length <= 200){
+              return (arg.value);
+            }
+            return addressEllipsis(arg.value);
           }
           default: {
             return arg.value;
