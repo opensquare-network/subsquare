16b85593d037080075ecdc26474cd8ed7ba31e73
Yongfeng LI <wliyongfeng@gmail.com>
2021-10-29 19:49:15 +0800

    Add khala time update script

--- packages/next/ecosystem.config.js
+++ packages/next/ecosystem.config.js
@@ -1,20 +1,35 @@
+const common = {
+  script: "backend/scripts/update-post-time.js",
+  log_date_format: "YYYY-MM-DD HH:mm Z",
+  cron_restart: "*/30 * * * * *",
+  autorestart: false,
+};
+
 module.exports = {
   apps: [
     {
       name: "subsquare-time-updater-karura",
-      script: "backend/scripts/update-post-time.js",
-      log_date_format: "YYYY-MM-DD HH:mm Z",
-      cron_restart: "*/30 * * * * *",
-      autorestart: false,
+      ...common,
       env: {
         NODE_ENV: "development",
-        CHAIN: "karura"
+        CHAIN: "karura",
       },
       env_production: {
         NODE_ENV: "production",
-        CHAIN: "karura"
+        CHAIN: "karura",
       },
     },
-
+    {
+      name: "subsquare-time-updater-khala",
+      ...common,
+      env: {
+        NODE_ENV: "development",
+        CHAIN: "khala",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        CHAIN: "khala",
+      },
+    },
   ],
 };
