48bfabdf16145c02137415850e0adb93cc5324a4
Yongfeng LI <wliyongfeng@gmail.com>
2021-11-05 22:29:53 +0800

    Fix registry

--- packages/scan/src/chain/specs.js
+++ packages/scan/src/chain/specs.js
@@ -1,12 +1,12 @@
 const { getAllVersionChangeHeights } = require("../mongo/meta");
 const findLast = require("lodash.findlast");
+const { getRegistryByHeight } = require("./registry");
 const { getApi } = require("../api");
 const { isUseMetaDb } = require("../env");
 const { logger } = require("../logger");
 
 let versionChangedHeights = [];
 let registryMap = {};
-let apiMap = {};
 let blockApiMap = {};
 
 async function updateSpecs(height) {
@@ -41,27 +41,11 @@ async function findRegistry(height) {
 
   let registry = registryMap[mostRecentChangeHeight];
   if (!registry) {
-    const api = await getApi();
-    const blockHash = await api.rpc.chain.getBlockHash(height);
-    registry = (await api.getBlockRegistry(blockHash)).registry;
+    registry = await getRegistryByHeight(mostRecentChangeHeight);
     registryMap[mostRecentChangeHeight] = registry;
   }
 
   return registry;
-}
-
-async function findBlockApiByHeight(blockHeight) {
-  const mostRecentChangeHeight = findRecentHeight(blockHeight);
-
-  let targetApi = apiMap[mostRecentChangeHeight];
-  if (!targetApi) {
-    const api = await getApi();
-    const blockHash = await api.rpc.chain.getBlockHash(mostRecentChangeHeight);
-    targetApi = await api.at(blockHash);
-    apiMap[mostRecentChangeHeight] = targetApi;
-  }
-
-  return targetApi;
 }
 
 function findRecentHeight(blockHeight) {
