a534a2e149b77af997b28f4cfaa3d4054274aa96
yoshiyuki <634554815@qq.com>
2021-12-09 13:19:05 +0800

    HOTFIX: techcomm motions & council motions (#818)

--- packages/next/pages/council/motion/[id].js
+++ packages/next/pages/council/motion/[id].js
@@ -4,7 +4,7 @@ import Back from "components/back";
 import { withLoginUser, withLoginUserRedux } from "lib";
 import { ssrNextApi as nextApi } from "services/nextApi";
 import Layout from "components/layout";
-import MotionDetail from "components/motion/motionDetail";
+import MotionDetail from "components/motion/councilMotionDetail";
 import { to404 } from "utils/serverSideUtil";
 import { TYPE_MOTION } from "utils/viewConstants";
 import { isMotionCompleted } from "../../../utils/viewfuncs";
