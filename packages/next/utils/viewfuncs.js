49b144acef14f2632c5d9b61f8107eed20fad3d8
yoshiyuki <brother-zhou@qq.com>
2021-12-09 14:18:16 +0800

    seo : fix author

--- packages/next/utils/viewfuncs.js
+++ packages/next/utils/viewfuncs.js
@@ -238,5 +238,7 @@ export const getMetaDesc = (post, type = "Discussion") => {
       contentDesc = post.content;
     }
   }
-  return `${type} - @${post.author?.username} - ${contentDesc}`;
+  return `${type} ${
+    post.author?.username ? ` - @${post.author?.username}` : ``
+  } - ${contentDesc}`;
 };
