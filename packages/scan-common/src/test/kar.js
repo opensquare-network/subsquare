83e46713b4cdb4c71a2023606ad4590745283029
Yongfeng LI <wliyongfeng@gmail.com>
2021-12-23 23:29:09 +0800

    Upgrade dependency

--- packages/scan-common/src/test/kar.js
+++ packages/scan-common/src/test/kar.js
@@ -1,11 +1,11 @@
 const { setChain, CHAINS } = require("../env");
 const { setProvider, setApi } = require("../chain/api");
-const { onFinalityKarura } = require("./constants");
+const { karuraEndpoint } = require("./constants");
 const { typesBundleForPolkadot } = require("@acala-network/type-definitions");
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 
 async function setKarura() {
-  const provider = new WsProvider(onFinalityKarura, 1000);
+  const provider = new WsProvider(karuraEndpoint, 1000);
   const api = await ApiPromise.create({
     provider,
     typesBundle: { ...typesBundleForPolkadot },
