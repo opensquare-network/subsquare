3e18499a027cd0fc695c7d0f8891eea7e48f1514
yoshiyuki <brother-zhou@qq.com>
2021-12-27 10:26:06 +0800

    refactor: migrate next components to next-common

---
+++ packages/next-common/avatar.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import Identicon from "@polkadot/react-identicon";
+
+export default function Avatar({ address, size = 24 }) {
+  const theme = "polkadot";
+  return <Identicon value={address} size={size} theme={theme} />;
+}
