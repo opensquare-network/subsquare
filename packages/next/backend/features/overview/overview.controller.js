933a3acad2f728a09429f1dddbf6f3b9705635b2
chaojun <chaojun@opensquare.network>
2021-12-30 16:18:20 +0800

    fix: return financial motions overview data

--- packages/next/backend/features/overview/overview.controller.js
+++ packages/next/backend/features/overview/overview.controller.js
@@ -8,6 +8,7 @@ const externalProposalPostService = require("../../services/external-proposal.se
 const publicProposalPostService = require("../../services/public-proposal.service");
 const referendumPostService = require("../../services/referendum.service");
 const techCommMotionService = require("../../services/tech-comm-motion.service");
+const financialMotionService = require("../../services/financial-motion.service");
 
 const myCache = new NodeCache( { stdTTL: 30, checkperiod: 36 } );
 
@@ -38,6 +39,7 @@ async function getOverview(ctx) {
     publicProposalPostService.getActivePostsOverview(),
     referendumPostService.getActivePostsOverview(),
     techCommMotionService.getActiveMotionsOverview(),
+    financialMotionService.getActiveMotionsOverview(),
   ]);
 
   const overview = {
