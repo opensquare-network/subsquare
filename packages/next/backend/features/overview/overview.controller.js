6a01508d880b7a0bbddd9ac386741088f5022958
chaojun <chaojun@opensquare.network>
2021-12-30 16:28:14 +0800

    fix: return financial motions for karura only

--- packages/next/backend/features/overview/overview.controller.js
+++ packages/next/backend/features/overview/overview.controller.js
@@ -13,6 +13,8 @@ const financialMotionService = require("../../services/financial-motion.service"
 const myCache = new NodeCache( { stdTTL: 30, checkperiod: 36 } );
 
 async function getOverview(ctx) {
+  const chain = process.env.CHAIN;
+
   const cachedOverview = myCache.get(`overview`);
   if (cachedOverview) {
     ctx.body = cachedOverview;
@@ -40,7 +42,9 @@ async function getOverview(ctx) {
     publicProposalPostService.getActivePostsOverview(),
     referendumPostService.getActivePostsOverview(),
     techCommMotionService.getActiveMotionsOverview(),
-    financialMotionService.getActiveMotionsOverview(),
+    chain === "karura"
+      ? financialMotionService.getActiveMotionsOverview()
+      : undefined,
   ]);
 
   const overview = {
@@ -61,10 +65,13 @@ async function getOverview(ctx) {
     techComm: {
       motions: techCommMotions,
     },
-    financialCouncil: {
-      motions: financialMotions,
-    }
   };
+
+  if (chain === "karura") {
+    overview.financialCouncil = {
+      motions: financialMotions,
+    };
+  }
 
   myCache.set(`overview`, overview, 30);
 
