b77cf0644935415d0a6d30fe5cda116ff79d6b9b
yoshiyuki <brother-zhou@qq.com>
2022-01-07 14:39:06 +0800

    fix: run error

--- packages/kintsugi-next/backend/features/overview/overview.controller.js
+++ packages/kintsugi-next/backend/features/overview/overview.controller.js
@@ -1,14 +1,9 @@
 const NodeCache = require("node-cache");
 const discussionPostService = require("../../services/post.service")("post");
-const tipPostService = require("../../services/tip.service");
 const treasuryProposalPostService = require("../../services/treasury-proposal.service");
-const bountyPostService = require("../../services/bounty.service");
-const motionService = require("../../services/motion.service");
-const externalProposalPostService = require("../../services/external-proposal.service");
 const publicProposalPostService = require("../../services/public-proposal.service");
 const referendumPostService = require("../../services/referendum.service");
 const techCommMotionService = require("../../services/tech-comm-motion.service");
-const financialMotionService = require("../../services/financial-motion.service");
 
 const myCache = new NodeCache({ stdTTL: 30, checkperiod: 36 });
 
--- packages/kintsugi-next/backend/routes.js
+++ packages/kintsugi-next/backend/routes.js
@@ -4,15 +4,10 @@ const router = new Router();
 
 const chainFeatureRouters = [
   require("./features/posts/routes"),
-  require("./features/tips/routes"),
   require("./features/proposals/routes"),
-  require("./features/bounties/routes"),
-  require("./features/motions/routes"),
   require("./features/public-proposals/routes"),
-  require("./features/external-proposals/routes"),
   require("./features/referendums/routes"),
   require("./features/tech-comm/motions/routes"),
-  require("./features/financial-motions/routes"),
   require("./features/comments/routes"),
   require("./features/overview/routes"),
 ];
