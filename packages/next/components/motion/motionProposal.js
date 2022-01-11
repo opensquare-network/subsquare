e0fe34afbab2f9ce397a3a0b3de77bf68f356880
yoshiyuki <634554815@qq.com>
2022-01-11 14:21:51 +0800

    hotfix: turn OrmlTraitsChangeU128 karura type into Number #983 (#985)
    * hotfix: turn OrmlTraitsChangeU128 karura type into Number #983
    
    * remove un-used importation

--- packages/next/components/motion/motionProposal.js
+++ packages/next/components/motion/motionProposal.js
@@ -81,6 +81,22 @@ function convertProposalForTableView(proposal, chain) {
     args: Object.fromEntries(
       proposal.args.map((arg) => {
         switch (arg.type) {
+          case "OrmlTraitsChangeU128": {
+            if (typeof arg.value === "string") {
+              return [arg.name, new BigNumber(arg.value).toString()];
+            }
+            if (typeof arg.value === "object") {
+              const argHexToString = {};
+              Object.keys(arg.value).map((key) => {
+                if (typeof arg.value[key] === "string") {
+                  argHexToString[key] = new BigNumber(
+                    arg.value[key]
+                  ).toString();
+                }
+              });
+              return [arg.name, argHexToString];
+            }
+          }
           case "Call":
           case "CallOf": {
             return [arg.name, convertProposalForTableView(arg.value, chain)];
