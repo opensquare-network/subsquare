6811d022c996042ca6bf5950afc878d04849e67b
yoshiyuki <brother-zhou@qq.com>
2022-01-13 15:42:23 +0800

    fix: typo

--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -38,7 +38,7 @@ export default withLoginUserRedux(({ overview, loginUser, chain, siteUrl }) => {
         toTechCommMotionListItem(chain, item)
       ),
     },
-    （chain === "karura" || chain === "acala")
+    (chain === "karura" || chain === "acala")
       ? {
           category: "Financial Council Motions",
           items: (overview?.financialCouncil?.motions ?? []).map((item) =>
