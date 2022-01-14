1b3481252bfac8e353fdcc35da5b05bfbcea6619
chaojun <chaojun@opensquare.network>
2022-01-14 15:38:02 +0800

    Update

--- packages/kintsugi-next/ecosystem.config.js
+++ packages/kintsugi-next/ecosystem.config.js
@@ -1,15 +1,11 @@
-const common = {
-  script: "backend/scripts/update-post-time.js",
-  log_date_format: "YYYY-MM-DD HH:mm Z",
-  cron_restart: "*/30 * * * * *",
-  autorestart: false,
-};
-
 module.exports = {
   apps: [
     {
       name: "subsquare-time-updater-kintsugi",
-      ...common,
+      script: "backend/scripts/update-post-time.js",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      cron_restart: "*/30 * * * * *",
+      autorestart: false,
       env: {
         NODE_ENV: "development",
         CHAIN: "kintsugi",
