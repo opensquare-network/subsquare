cbcca3030bf0d9b86a7cb78233fa81aeb4b3e0dc
yoshiyuki <brother-zhou@qq.com>
2022-01-14 19:24:31 +0800

    bugfix: subscan icon doesnt show up

--- packages/kintsugi-next/components/timeline/links.js
+++ packages/kintsugi-next/components/timeline/links.js
@@ -38,23 +38,6 @@ export default function Links({
   if (!indexer && !address) {
     return null;
   }
-  if (chain === "karura" || chain === "khala" || chain === "basilisk") {
-    return (
-      <Wrapper style={style}>
-        <SubscanLink
-          href={
-            address
-              ? `https://${chain}.subscan.io/account/${address}`
-              : `https://${chain}.subscan.io/extrinsic/${indexer.blockHeight}-${
-                  indexer.extrinsicIndex ?? indexer.index ?? 0
-                }`
-          }
-          target="_blank"
-          rel="noreferrer"
-        />
-      </Wrapper>
-    );
-  }
   return (
     <Wrapper style={style}>
       <SubscanLink
