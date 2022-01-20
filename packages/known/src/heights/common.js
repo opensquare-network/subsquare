977abe3a315b37b55c76e16f545d6e749e314243
Yongfeng LI <wliyongfeng@gmail.com>
2022-01-20 21:19:05 +0800

    Enhace known heights script

--- packages/known/src/heights/common.js
+++ packages/known/src/heights/common.js
@@ -1,6 +1,10 @@
 function extractHeights(records = []) {
   const heights = [];
   for (const record of records) {
+    if (record.indexer) {
+      heights.push(record.indexer.blockHeight);
+    }
+
     (record.timeline || []).map((item) => {
       const indexer =
         item.indexer || item.extrinsicIndexer || item.eventIndexer;
