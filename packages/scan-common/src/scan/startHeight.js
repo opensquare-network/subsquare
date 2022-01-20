fd8bce69a45de504617ab6b24ef110a833f377e5
Yongfeng LI <wliyongfeng@gmail.com>
2022-01-20 15:51:11 +0800

    Fix khala scan start height

--- packages/scan-common/src/scan/startHeight.js
+++ packages/scan-common/src/scan/startHeight.js
@@ -1,6 +1,6 @@
 const { CHAINS } = require("../env");
 const scanStartHeight = {
-  [CHAINS.KHALA]: 320020,
+  [CHAINS.KHALA]: 310000,
   [CHAINS.KARURA]: 43249,
   [CHAINS.KINTSUGI]: 173680,
   [CHAINS.BIFROST]: 347015,
