478d60e00569d92f2022c3ae62037fcc29dfd726
chaojun <chaojun@opensquare.network>
2022-01-27 14:39:59 +0800

    fix: ss58 format

--- packages/backend-common/constants.js
+++ packages/backend-common/constants.js
@@ -1,6 +1,15 @@
 const chainStatusRoom = "CHAIN_STATUS_ROOM";
 
 const SS58Format = Object.freeze({
+  Polkadot: 0,
+  Kusama: 2,
+  Karura: 8,
+  Khala: 30,
+  Basilisk: 10041,
+  Substrate: 42,
+  Kabocha: 2,
+  Bifrost: 6,
+  Acala: 10,
   Kintsugi: 2092,
 });
 
