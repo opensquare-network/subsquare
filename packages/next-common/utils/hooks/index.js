410e717a53a89268b1bfbb9c33a8e8e98cc6a6b4
Yongfeng LI <wliyongfeng@gmail.com>
2022-02-10 22:05:22 +0800

    Refactor useBlockTime code

--- packages/next-common/utils/hooks/index.js
+++ packages/next-common/utils/hooks/index.js
@@ -4,25 +4,27 @@ import { BN_THOUSAND, BN_TWO } from "@polkadot/util";
 export function useBlockTime(api) {
   const [blockTime, setBlockTime] = useState();
   useEffect(() => {
-    if (api) {
-      const THRESHOLD = BN_THOUSAND.div(BN_TWO);
-      setBlockTime(
-        api.consts.babe?.expectedBlockTime ||
-          // POW, eg. Kulupu
-          api.consts.difficulty?.targetBlockTime ||
-          // Subspace
-          api.consts.subspace?.expectedBlockTime ||
-          // Check against threshold to determine value validity
-          (api.consts.timestamp?.minimumPeriod.gte(THRESHOLD)
-            ? // Default minimum period config
-              api.consts.timestamp.minimumPeriod.mul(BN_TWO)
-            : api.query.parachainSystem
-            ? // default guess for a parachain
-              DEFAULT_TIME.mul(BN_TWO)
-            : // default guess for others
-              DEFAULT_TIME)
-      );
+    if (!api) {
+      return;
     }
+
+    const THRESHOLD = BN_THOUSAND.div(BN_TWO);
+    setBlockTime(
+      api.consts.babe?.expectedBlockTime ||
+        // POW, eg. Kulupu
+        api.consts.difficulty?.targetBlockTime ||
+        // Subspace
+        api.consts.subspace?.expectedBlockTime ||
+        // Check against threshold to determine value validity
+        (api.consts.timestamp?.minimumPeriod.gte(THRESHOLD)
+          ? // Default minimum period config
+            api.consts.timestamp.minimumPeriod.mul(BN_TWO)
+          : api.query.parachainSystem
+          ? // default guess for a parachain
+            DEFAULT_TIME.mul(BN_TWO)
+          : // default guess for others
+            DEFAULT_TIME)
+    );
   }, [api]);
   return blockTime;
 }
