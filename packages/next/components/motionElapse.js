26a276d54372f1f63edf6543a8114c8803ff06e6
yoshiyuki <brother-zhou@qq.com>
2022-02-11 13:24:39 +0800

    style: remove redundent gap8px for <Info>, apply margin to elapseIcon only instead #1252

--- packages/next/components/motionElapse.js
+++ packages/next/components/motionElapse.js
@@ -9,16 +9,28 @@ export default function MotionElapse({ motion, chain }) {
   const currentFinalHeight = useSelector(nodesHeightSelector);
   const motionEndHeight = motion?.voting?.end;
   const motionStartHeight = motion?.indexer?.blockHeight;
-  const blockTime = useBlockTime(currentFinalHeight - motionEndHeight, chain)
+  const blockTime = useBlockTime(currentFinalHeight - motionEndHeight, chain);
   const motionEnd = isMotionEnded(motion);
 
-  if (motionEnd || !motionEndHeight || !currentFinalHeight || currentFinalHeight >= motionEndHeight || !blockTime) {
-    return null;
+  if (
+    motionEnd ||
+    !motionEndHeight ||
+    !currentFinalHeight ||
+    currentFinalHeight >= motionEndHeight ||
+    !blockTime
+  ) {
+    // return null;
   }
 
-  const elapsePercent = (currentFinalHeight - motionStartHeight) / (motionEndHeight - motionStartHeight);
+  const elapsePercent =
+    (currentFinalHeight - motionStartHeight) /
+    (motionEndHeight - motionStartHeight);
   return (
-    <Tooltip content={`End in ${blockTime}, #${bigNumber2Locale(motionEndHeight.toString())}`}>
+    <Tooltip
+      content={`End in ${blockTime}, #${bigNumber2Locale(
+        motionEndHeight.toString()
+      )}`}
+    >
       <div>
         <CountDown percent={parseInt(elapsePercent * 100)} />
       </div>
