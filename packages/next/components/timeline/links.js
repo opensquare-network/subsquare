69d79d78398bcbe6eb2758a5dc20ac660021f8c9
Yongfeng LI <wliyongfeng@gmail.com>
2022-02-11 16:25:05 +0800

    Remove polkassembly link

--- packages/next/components/timeline/links.js
+++ packages/next/components/timeline/links.js
@@ -9,16 +9,6 @@ const Wrapper = styled(Flex)`
   }
 `;
 
-const PolkascanLink = styled.a`
-  width: 20px;
-  height: 20px;
-  background: url("/imgs/icons/link-polkascan.svg");
-
-  :hover {
-    background: url("/imgs/icons/link-polkascan-active.svg");
-  }
-`;
-
 const SubscanLink = styled.a`
   width: 20px;
   height: 20px;
