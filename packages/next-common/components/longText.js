970eda7646550f4c4fa7afc81dae17669356b86a
yoshiyuki <brother-zhou@qq.com>
2022-02-14 11:56:27 +0800

    refactor: extract <LongText> to next-common #1275

--- packages/next-common/components/longText.js
+++ packages/next-common/components/longText.js
@@ -1,5 +1,6 @@
+import React from "react";
 import styled from "styled-components";
-import { hexEllipsis } from "../utils";
+import { hexEllipsis } from "next-common/utils";
 
 const Between = styled.div`
   display: flex;
