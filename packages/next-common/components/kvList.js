0ded51d6c46492cb540066d2f6e07f5f189272ae
Yongfeng LI <wliyongfeng@gmail.com>
2022-02-14 13:06:04 +0800

    Improve import path

--- packages/next-common/components/kvList.js
+++ packages/next-common/components/kvList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { shadow_100 } from "styles/componentCss";
+import { shadow_100 } from "../styles/componentCss";
 
 const Wrapper = styled.div`
   background: #ffffff;
