58dc9a7f392214d0256d11dfc606221f9d432cd8
yoshiyuki <brother-zhou@qq.com>
2022-02-14 21:17:02 +0800

    bugfix: css importation for markdown editor in next

--- packages/next-common/components/post/postEdit.js
+++ packages/next-common/components/post/postEdit.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import { useState } from "react";
 import React from "react";
-import Input from "components/input";
-import EditInput from "components/editInput";
+import Input from "next-common/components/input";
+import EditInput from "next-common/components/editInput";
 import nextApi from "services/nextApi";
 import { toApiType } from "utils/viewfuncs";
 
