644a64a24ff07689c4135dbb454764fd6a84ab93
Yongfeng LI <wliyongfeng@gmail.com>
2022-02-14 11:41:08 +0800

    Extract ReferendumTimeline component

---
+++ packages/next/pages/democracy/referendum/timeline.js
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+import Timeline from "next-common/components/timeline";
+
+const getTimelineData = (args, method) => {
+  switch (method) {
+    case "Executed":
+      const rawResult = args.result;
+      let result;
+      if (typeof rawResult === "boolean") {
+        result = rawResult;
+      } else if (typeof args.result === "object") {
+        result = Object.keys(rawResult)[0];
+      } else {
+        result = JSON.stringify(rawResult);
+      }
+
+      return { result };
+  }
+
+  return args;
+};
+
+export default function ReferendumTimeline({ timeline = [], chain }) {
+  const timelineData = timeline.map((item) => {
+    return {
+      time: dayjs(item.indexer.blockTime).format("YYYY-MM-DD HH:mm:ss"),
+      indexer: item.indexer,
+      status: { value: item.method ?? item.name },
+      data: getTimelineData(item.args, item.method ?? item.name),
+    };
+  });
+
+  return <Timeline data={timelineData} chain={chain} />;
+}
