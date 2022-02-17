8319d09b2418ebcf04eed4b41b84d3f7113eb236
yoshiyuki <brother-zhou@qq.com>
2022-02-17 12:39:35 +0800

    bugfix: not showing AET sign when abbreviated result is extractly equal to the number #1307

--- packages/next/utils/index.js
+++ packages/next/utils/index.js
@@ -136,6 +136,18 @@ export const sleep = (time) => {
   });
 };
 
+export function getEffectiveNumbers(n) {
+  const result = [];
+  let flag = false;
+  n.split('').reverse().forEach(dig => {
+    if (!isNaN(parseInt(dig))) {
+      flag = flag || parseInt(dig) > 0;
+      flag && result.push(dig);
+    }
+  });
+  return result.reverse().join();
+}
+
 export function abbreviateBigNumber(x, fixed = 2) {
   const n = new BigNumber(x);
   const fmt = {
