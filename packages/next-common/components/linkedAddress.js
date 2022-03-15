150f411cd68095a1b47b1f20b29eba5293c14784
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-15 16:06:23 +0800

    Remove useless return

--- packages/next-common/components/linkedAddress.js
+++ packages/next-common/components/linkedAddress.js
@@ -175,7 +175,6 @@ export default function LinkedAddress({ chain }) {
         if (isMounted.current) {
           setHasExtension(false);
         }
-        return;
       }
     })();
   }, [isMounted]);
