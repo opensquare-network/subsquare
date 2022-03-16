b92a51da52540ab4e7c4fcf9dbfe91b29a52cc6f
chaojun <chaojun@opensquare.network>
2022-03-16 09:55:02 +0800

    Remove debug log

--- packages/backend-common/index.js
+++ packages/backend-common/index.js
@@ -17,7 +17,6 @@ app.use(async (ctx, next) => {
   try {
     await next();
   } catch (err) {
-    console.error(err.message);
     ctx.status = err.statusCode || err.status || 500;
     ctx.body = {
       message: err.message,
