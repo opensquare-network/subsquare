4916240ec7bfd4d45fbef8a7da84acb35c3c1f59
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-16 14:41:19 +0800

    Extract Auth component

--- packages/kintsugi-next/components/layout.js
+++ packages/kintsugi-next/components/layout.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import Header from "./header";
 import Content from "next-common/components/layout/content";
 import Toast from "./toast";
-import Auth from "./auth";
 import SEO from "next-common/components/SEO";
 import { DEFAULT_SEO_INFO } from "next-common/utils/constants";
 import capitalize from "next-common/utils/capitalize";
@@ -16,6 +15,7 @@ import {
   setFinalizedHeight,
 } from "next-common/store/reducers/chainSlice";
 import { useEffect } from "react";
+import Auth from "next-common/components/auth";
 
 const Wrapper = styled.div`
   display: flex;
