1bc7bb327a849c1af79f0bc519749e8672ab1208
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-22 12:09:30 +0800

    Implement timeline accordion, #1321

--- packages/next-common/components/timeline/index.js
+++ packages/next-common/components/timeline/index.js
@@ -1,38 +1,8 @@
-import styled from "styled-components";
 import React, { Fragment } from "react";
 
 import Item from "./item";
 import FoldableItem from "./foldableItem";
-import { timeDurationFromNow } from "../../utils";
-import { shadow_100 } from "../../styles/componentCss";
-
-const Wrapper = styled.div`
-  background: #ffffff;
-  border: 1px solid #ebeef4;
-  ${shadow_100};
-  border-radius: 4px;
-  padding: 48px;
-  @media screen and (max-width: 768px) {
-    max-width: 100%;
-    padding: 24px;
-    border-radius: 0;
-  }
-`;
-
-const TitleWrapper = styled.div`
-  display: flex;
-  align-items: flex-end;
-  justify-content: space-between;
-  margin-bottom: 24px;
-  > :first-child {
-    font-weight: bold;
-    font-size: 16px;
-  }
-  > :last-child {
-    font-size: 12px;
-    color: #9da9bb;
-  }
-`;
+import TimelineAccordion from "./timelineAccordion";
 
 export default function Timeline({
   data,
@@ -49,17 +19,11 @@ export default function Timeline({
   if (Array.isArray(lastTimelineItem)) {
     lastTimelineItem = lastTimelineItem[lastTimelineItem.length - 1];
   }
-  const lastTimelineItemTime = lastTimelineItem?.indexer?.blockTime;
-
   return (
-    <Wrapper>
-      <TitleWrapper>
-        <div>Timeline</div>
-        <div>
-          {motionEndInfo ||
-            `Latest activity ${timeDurationFromNow(lastTimelineItemTime)}`}
-        </div>
-      </TitleWrapper>
+    <TimelineAccordion
+      motionEndInfo={motionEndInfo}
+      lastActivityTime={lastTimelineItem?.indexer?.blockTime}
+    >
       {data.map((item, index) => (
         <Fragment key={index}>
           {Array.isArray(item) ? (
@@ -74,6 +38,6 @@ export default function Timeline({
           )}
         </Fragment>
       ))}
-    </Wrapper>
+    </TimelineAccordion>
   );
 }
