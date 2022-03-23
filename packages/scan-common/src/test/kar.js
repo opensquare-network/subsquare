19a6ffef3534cd01f202fd6df953e335dd7a60ec
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-23 23:22:42 +0800

    Upgrade dependency (#1358)
    * Remove unused dependency to @polkadot/api
    
    * Upgrade dependency

--- packages/scan-common/src/test/kar.js
+++ packages/scan-common/src/test/kar.js
@@ -1,14 +1,14 @@
+const { karuraOptions } = require("../chain/karura/options");
 const { setChain, CHAINS } = require("../env");
 const { setProvider, setApi } = require("../chain/api");
 const { karuraEndpoint } = require("./constants");
-const { typesBundleForPolkadot } = require("@acala-network/type-definitions");
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 
 async function setKarura() {
   const provider = new WsProvider(karuraEndpoint, 1000);
   const api = await ApiPromise.create({
     provider,
-    typesBundle: { ...typesBundleForPolkadot },
+    ...karuraOptions,
   });
 
   setProvider(provider);
