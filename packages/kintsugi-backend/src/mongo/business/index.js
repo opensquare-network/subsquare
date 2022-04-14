14e4796bcebc920740ed1eb710441acf71087cfa
chaojun <chaojun@opensquare.network>
2022-04-14 17:24:02 +0800

    Refs #1420,  separate backend repo and fix next dependency

--- packages/kintsugi-backend/src/mongo/business/index.js
+++
@@ -1,8 +0,0 @@
-const businessDb = require("@subsquare/backend-common/mongo/business");
-
-module.exports = {
-  ...businessDb,
-  getTreasuryProposalCollection: () => businessDb.getCollection("treasuryProposal"),
-  getDemocracyCollection: () => businessDb.getCollection("democracy"),
-  getTechCommMotionCollection: () => businessDb.getCollection("techCommMotion"),
-};
