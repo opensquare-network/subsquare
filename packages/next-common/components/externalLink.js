e5e63e3f194b1295f9dc40582fd832a8791b0b80
yoshiyuki <brother-zhou@qq.com>
2022-04-14 09:34:43 +0800

    refactor footer layout, #1362

--- packages/next-common/components/externalLink.js
+++ packages/next-common/components/externalLink.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function ExternalLink({ href, children }) {
+export default function ExternalLink({ href, children, title ="" }) {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
+      title={title}
       onClick={(e) => {
         e.stopPropagation();
       }}
