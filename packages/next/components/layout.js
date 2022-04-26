eb9efd4f50c687d6ab364dc2c8c86db7a44f78d9
Yongfeng LI <wliyongfeng@gmail.com>
2022-04-26 11:20:48 +0800

    Fix og preview

--- packages/next/components/layout.js
+++ packages/next/components/layout.js
@@ -55,21 +55,24 @@ export default function Layout({
     }
   }, [bestNumber, dispatch, isMounted]);
 
+  const seo = (
+    <SEO
+      title={
+        seoInfo?.title || `SubSquare | ${capitalize(chain)} governance platform`
+      }
+      desc={seoInfo?.desc || DEFAULT_SEO_INFO.desc}
+      chain={chain}
+    />
+  );
+
   const { width } = useWindowSize();
   if (isNil(width)) {
-    return null;
+    return <Wrapper>{seo}</Wrapper>;
   }
 
   return (
     <Wrapper>
-      <SEO
-        title={
-          seoInfo?.title ||
-          `SubSquare | ${capitalize(chain)} governance platform`
-        }
-        desc={seoInfo?.desc || DEFAULT_SEO_INFO.desc}
-        chain={chain}
-      />
+      {seo}
       <Auth chain={chain} />
       <Header user={user} left={left} chain={chain} isWeb3Login={isWeb3Login} />
       <Content left={left}>{children}</Content>
