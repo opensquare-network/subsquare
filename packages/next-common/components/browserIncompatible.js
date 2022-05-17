a3f2270a1ac3dcd3dbbc437a192a51a6e412a2ec
yoshiyuki <brother-zhou@qq.com>
2022-05-17 18:10:16 +0800

    refactor: extract common css

--- packages/next-common/components/browserIncompatible.js
+++ packages/next-common/components/browserIncompatible.js
@@ -1,10 +1,33 @@
 import React from "react";
 import Contacts from "next-common/components/layout/contacts";
 import Hint from "../assets/imgs/oops.svg";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  p{
+    margin-top: 24px;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 100%;
+  }
+  span{
+    max-width: 343px;
+    font-size: 14px;
+    line-height: 140%;
+    text-align: center;
+    color: #506176;
+   }
+`;
 
 export default function BrowserIncompatible() {
   return (
-    <>
+    <Wrapper>
       <Hint/>
       <p>Browser not Supported</p>
       <span>
@@ -12,6 +35,6 @@ export default function BrowserIncompatible() {
       </span>
       <p style={{fontSize:14}}>Contact Us</p>
       <Contacts/>
-    </>
+    </Wrapper>
   );
 }
--- packages/next/pages/incompatible.js
+++ packages/next/pages/incompatible.js
@@ -1,34 +1,11 @@
-import styled from "styled-components";
 import NextHead from "next-common/components/nextHead";
 import BrowserIncompatible from "next-common/components/browserIncompatible";
 
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  p{
-    margin-top: 24px;
-    font-weight: 700;
-    font-size: 20px;
-    line-height: 100%;
-  }
-  span{
-    max-width: 343px;
-    font-size: 14px;
-    line-height: 140%;
-    text-align: center;
-    color: #506176;
-   }
-`;
-
 export default function CustomErrPage() {
   return (
-    <Wrapper>
+    <>
       <NextHead title={`Browser not Supported`} desc={`Please use Google Chrome, Microsoft Edge or Safari 14.1+ to access for good web experience.`} />
       <BrowserIncompatible/>
-    </Wrapper>
+    </>
   );
 }
