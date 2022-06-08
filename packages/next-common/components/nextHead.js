61f6a48e110d722a62600ee5aaffba3d6227fcd1
2nthony <ant.hu@outlook.com>
2022-06-08 15:24:00 +0800

    feat: next head support `ogImage` prop

--- packages/next-common/components/nextHead.js
+++ packages/next-common/components/nextHead.js
@@ -6,6 +6,7 @@ export default function NextHead({
   desc = "MetaDescription",
   type = null,
   url = "",
+  ogImage = "",
 }) {
   return (
     <Head>
@@ -21,7 +22,7 @@ export default function NextHead({
       {type === "post" && <meta property="twitter:card" content="summary" />}
       <meta
         property="og:image"
-        content={`${process.env.NEXT_PUBLIC_SITE_URL}/imgs/logo.png`}
+        content={ogImage ?? `${process.env.NEXT_PUBLIC_SITE_URL}/imgs/logo.png`}
       />
       <meta
         property="twitter:image"
