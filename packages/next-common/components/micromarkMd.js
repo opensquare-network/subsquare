4689ebc2f46f12fa02febe0496f118de0023611e
2nthony <ant.hu@outlook.com>
2022-06-08 16:34:59 +0800

    hotfix: markdown inline code css

--- packages/next-common/components/micromarkMd.js
+++ packages/next-common/components/micromarkMd.js
@@ -115,13 +115,13 @@ const Wrapper = styled.div`
         Liberation Mono, monospace !important;
       ${no_scroll_bar};
       max-width: 100%;
-      margin: 16px 0;
       padding: 0 0.25rem;
       background: #f5f8fa !important;
       border-radius: 0.25rem;
       white-space: nowrap !important;
       word-break: keep-all;
       overflow-x: scroll;
+      display: inline-flex;
     }
 
     a {
