56a84737c56e95614ea7dde398b0f23927cc04f9
Yongfeng LI <wliyongfeng@gmail.com>
2022-06-11 17:20:10 +0800

    Improve the code to get chain settings

--- packages/next-common/utils/consts/settings/index.js
+++ packages/next-common/utils/consts/settings/index.js
@@ -1,4 +1,3 @@
-import Chains from "../chains";
 import polkadot from "./polkadot";
 import kusama from "./kusama";
 import acala from "./acala";
@@ -12,35 +11,30 @@ import khala from "./khala";
 import kintsugi from "./kintsugi";
 import polkadex from "./polkadex";
 import turing from "./turing";
+import crust from "./crust";
+
+const settingsMap = {
+  polkadot,
+  kusama,
+  acala,
+  basilisk,
+  bifrost,
+  calamari,
+  crust,
+  crab,
+  interlay,
+  karura,
+  khala,
+  kintsugi,
+  polkadex,
+  turing,
+};
 
 export default function getChainSettings(chain) {
-  if (Chains.polkadot === chain) {
-    return polkadot;
-  } else if (Chains.kusama === chain) {
-    return kusama;
-  } else if (Chains.acala === chain) {
-    return acala;
-  } else if (Chains.basilisk === chain) {
-    return basilisk;
-  } else if (Chains.bifrost === chain) {
-    return bifrost;
-  } else if (Chains.calamari === chain) {
-    return calamari;
-  } else if (Chains.crab === chain) {
-    return crab;
-  } else if (Chains.interlay === chain) {
-    return interlay;
-  } else if (Chains.karura === chain) {
-    return karura;
-  } else if (Chains.khala === chain) {
-    return khala;
-  } else if (Chains.kintsugi === chain) {
-    return kintsugi;
-  } else if (Chains.polkadex === chain) {
-    return polkadex;
-  } else if (Chains.turing === chain) {
-    return turing;
+  const settings = settingsMap[chain];
+  if (!settings) {
+    throw `can not get chain settings of ${chain}`;
   }
 
-  throw `can not get chain settings of ${chain}`;
+  return settings;
 }
