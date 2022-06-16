5e039d46fe262c2fb483e52222881b9a1a462da0
2nthony <ant.hu@outlook.com>
2022-06-16 14:53:36 +0800

    fix: banner toggle button

--- packages/next-common/components/uploadBanner/toggleText.js
+++ packages/next-common/components/uploadBanner/toggleText.js
@@ -3,10 +3,12 @@ import styled, { css } from "styled-components";
 import { primary_purple_500 } from "next-common/styles/colors";
 import { p_12_medium } from "next-common/styles/componentCss";
 
-const Text = styled.div`
+const Text = styled.button`
   user-select: none;
   ${p_12_medium};
   color: ${primary_purple_500};
+  background-color: transparent;
+  border: none;
 
   &:hover {
     cursor: pointer;
