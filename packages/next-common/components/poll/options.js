87fb79ea42a23b7038eff6629a1be14d2ecf7eb3
chaojun <chaojun@opensquare.network>
2022-06-17 18:15:46 +0800

    Update

--- packages/next-common/components/poll/options.js
+++ packages/next-common/components/poll/options.js
@@ -15,24 +15,41 @@ const Title = styled.div`
   font-weight: 500;
   font-size: 14px;
   line-height: 100%;
-  color: #1E2134;
+  color: #1e2134;
 `;
 
-function Option({ text, votes, anonymous }) {
+const VotesCount = styled.div``;
+
+const VotesPrecent = styled.div``;
+
+function Option({ text, votes, anonymous, totalVotesCount }) {
+  const count = anonymous ? votes : votes.length;
+  const precent = count / totalVotesCount;
   return (
     <OptionWrapper>
       <Title>{text}</Title>
-
+      <VotesCount>{count}</VotesCount>
+      <VotesPrecent>{precent}</VotesPrecent>
     </OptionWrapper>
   );
 }
 
 export default function PollOptions({ options, votes, anonymous }) {
+  const totalVotesCount = Object.values(votes)
+    .map((item) => (anonymous ? item.length : item))
+    .reduce((prev, curr) => prev + curr, 0);
+
   return (
     <Wrapper>
       {options.map((option, index) => (
-        <Option key={index} text={option} votes={votes[options]} anonymous={anonymous} />
+        <Option
+          key={index}
+          text={option}
+          votes={votes[options]}
+          anonymous={anonymous}
+          totalVotesCount={totalVotesCount}
+        />
       ))}
     </Wrapper>
-  )
+  );
 }
