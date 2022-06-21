43de325fde9d50850112e6ebf66cdfe74907022e
2nthony <ant.hu@outlook.com>
2022-06-21 16:42:12 +0800

    feat: light text primary color

--- packages/next-common/styles/colors.js
+++ packages/next-common/styles/colors.js
@@ -1,5 +1,6 @@
 export const primary_purple_500 = "#6848ff";
 
+export const light_text_primary = "#1e2134";
 export const light_text_tertiary = "#9da9bb";
 export const light_text_secondary = "#506176";
 
