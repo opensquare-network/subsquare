8ce00232530fad8d13c2b580263971482ae3807c
2nthony <ant.hu@outlook.com>
2022-06-21 18:54:55 +0800

    feat: if has redirect query, then redirect it

--- packages/next-common/components/emailPage/email.js
+++ packages/next-common/components/emailPage/email.js
@@ -100,7 +100,7 @@ const EmailPage = withLoginUserRedux(({ loginUser, chain }) => {
           <Button
             isFill
             onClick={() => {
-              router.replace("/");
+              router.replace(router.query?.redirect || "/");
             }}
           >
             Remind me later
