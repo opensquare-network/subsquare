0d28d53af2c747fffc39e52acfe43d7f11b96b4e
yoshiyuki <brother-zhou@qq.com>
2022-07-08 18:21:44 +0800

    fix reply button

--- packages/next-common/utils/post.js
+++ packages/next-common/utils/post.js
@@ -84,6 +84,9 @@ export function getOnReply(
       const memberId = getMemberId(user, chain);
       if (contentType === "markdown") {
         reply = `[@${name}](/member/${memberId}) `;
+        if(user.username.startsWith("polkadot-key-0x")) {
+          reply = `[@${name}](${memberId}-${chain}) `;
+        }
         const at = content ? `${reply}` : reply;
         if (content === reply) {
           setContent(``);
@@ -92,6 +95,8 @@ export function getOnReply(
         }
       } else if (contentType === "html") {
         const contents = quillRef.getContents();
+        const isKeyRegistered= user.username.startsWith("polkadot-key-0x");
+        const address =  user.addresses?.[0]?.address ?? "";
         reply = {
           ops: [
             {
@@ -99,15 +104,25 @@ export function getOnReply(
                 mention: {
                   index: "0",
                   denotationChar: "@",
-                  id: memberId,
+                  id: isKeyRegistered ? address :  memberId,
                   value: name + " &nbsp; ",
+                  isKeyRegistered,
+                  chain,
+                  address,
+                  ...(isKeyRegistered ? {
+                    osnPolkaAddress:address,
+                    osnPolkaNetwork: chain,
+                  } : {}),
                 },
               },
             },
-            { insert: "\n" },
           ],
         };
-        quillRef.setContents(contents.ops.concat(reply.ops));
+        if( JSON.stringify(contents.ops) === `[{"insert":"\\n"}]`){
+          quillRef.setContents(reply.ops);
+        }else{
+          quillRef.setContents(contents.ops.concat(reply.ops));
+        }
       }
       focusEditor();
     });
