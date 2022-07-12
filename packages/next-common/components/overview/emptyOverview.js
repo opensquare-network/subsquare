1d3876dd4683b9e4c67765bc2753c8af9e428bf8
yoshiyuki <brother-zhou@qq.com>
2022-07-12 09:51:43 +0800

    Hotfix alignup (#1746)
    * style: fix list page align up
    
    * style: fix list page align up

--- packages/next-common/components/overview/emptyOverview.js
+++ packages/next-common/components/overview/emptyOverview.js
@@ -6,7 +6,7 @@ import Flex from "next-common/components/styled/flex";
 import Icon from "../../assets/imgs/icons/new-discussion.svg";
 
 const Wrapper = styled.div`
-  max-width: 852px;
+  max-width: 932px;
   @media screen and (max-width: 1024px) {
     max-width: 960px;
   }
