00ac8867c481ac382c969724a1c1c137e452c7f9
chaojun <chaojun@opensquare.network>
2022-07-13 14:59:16 +0800

    Hotfix: vote button is not shown for council member in motion page

--- packages/next/components/motion/motionDetail.js
+++ packages/next/components/motion/motionDetail.js
@@ -26,12 +26,13 @@ export default function MotionDetail({ user, motion, onReply, chain, type }) {
   const votingMethod = api?.query?.[toApiCouncil(chain, type)]?.voting;
   const membersMethod = api?.query?.[toApiCouncil(chain, type)]?.members;
 
-  const voters = useCall(membersMethod, []) || [];
+  const voters = useCall(membersMethod, [])?.toJSON() || [];
   const userCanVote = voters?.some((address) =>
     user?.addresses?.some(
       (item) => item.address === address && item.chain === chain
     )
   );
+  console.log({ voters, user, userCanVote })
   const motionEnd = isMotionEnded(post.onchainData);
 
   const blockHash = motionEnd ? post.onchainData?.state?.indexer?.blockHash : null;
