ba450933feb73f760dc3c272654aa4159470e710
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-14 15:42:01 +0800

    Fix typo (#1757)

--- packages/next-common/components/treasury/tip/popup/signer.js
+++ packages/next-common/components/treasury/tip/popup/signer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PopupLabelWithBalance from "../../../popup/balanceLabel";
 import SignerSelect from "../../../signerSelect";
-import { toPrecision } from "utils";
+import { toPrecision } from "../../../../utils";
 
 export default function Signer({
   api,
