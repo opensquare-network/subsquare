02c064b796f2f22bd4654c7033f75274281cb7c9
chaojun <chaojun@opensquare.network>
2022-07-22 11:14:45 +0800

    Add polkassembly comment loading effect

---
+++ packages/next/components/polkassembly/comment/commentButton.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "next-common/components/button";
+
+const Wrapper = styled.div`
+  margin-top: 48px;
+  display: flex;
+  justify-content: flex-end;
+  > :not(:first-child) {
+    margin-left: 12px;
+  }
+`;
+
+export default function PolkassemblyCommentButton({ chain, paId, type }) {
+  let link = `https://${chain}.polkassembly.io/post/${paId}`;
+  return (
+    <Wrapper>
+      <a href={link} target="_blank">
+        <Button secondary>
+          Comment on Polkassembly
+        </Button>
+      </a>
+    </Wrapper>
+  );
+}
