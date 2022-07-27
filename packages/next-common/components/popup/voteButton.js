04e38089ba89811516d91530e4b49747021075b5
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-27 11:34:31 +0800

    Add Positive/Negative Button (#1810)
    * Add Positive/Negative Button
    
    * Improve positive naming

--- packages/next-common/components/popup/voteButton.js
+++ packages/next-common/components/popup/voteButton.js
@@ -1,30 +1,25 @@
 import React from "react";
 import { VoteLoadingEnum } from "../../utils/voteEnum";
-
-import Button from "../button";
 import { ButtonWrapper } from "./styled";
+import { NegativeButton, PositiveButton } from "../buttons/colorButton";
 
 export default function VoteButton({ loadingState, doVote }) {
   return (
     <ButtonWrapper>
-      <Button
-        primary
-        background="#4CAF50"
+      <PositiveButton
         onClick={() => doVote(true)}
         isLoading={loadingState === VoteLoadingEnum.Aye}
         disabled={loadingState === VoteLoadingEnum.Nay}
       >
         Aye
-      </Button>
-      <Button
-        primary
-        background="#F44336"
+      </PositiveButton>
+      <NegativeButton
         onClick={() => doVote(false)}
         isLoading={loadingState === VoteLoadingEnum.Nay}
         disabled={loadingState === VoteLoadingEnum.Aye}
       >
         Nay
-      </Button>
+      </NegativeButton>
     </ButtonWrapper>
   );
 }
