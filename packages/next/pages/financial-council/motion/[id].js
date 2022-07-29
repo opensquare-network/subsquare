261bc1d8ff7d9debaca690de83b1ddeffa16bb68
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-29 17:03:50 +0800

    Remove redundant gloval constants (#1853)

--- packages/next/pages/financial-council/motion/[id].js
+++ packages/next/pages/financial-council/motion/[id].js
@@ -4,49 +4,47 @@ import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import Layout from "next-common/components/layout";
 import MotionDetail from "components/motion/motionDetail";
 import { to404 } from "next-common/utils/serverSideUtil";
-import { TYPE_FINANCIAL_MOTION } from "utils/viewConstants";
 import { getMetaDesc } from "../../../utils/viewfuncs";
 import { EmptyList } from "next-common/utils/constants";
 import OutWrapper from "next-common/components/styled/outWrapper";
 import MainCard from "next-common/components/styled/mainCard";
 import useUniversalComments from "components/universalComments";
+import { detailPageCategory } from "next-common/utils/consts/business/category";
 
-export default withLoginUserRedux(
-  ({ loginUser, motion, comments, chain }) => {
-    const { CommentComponent, focusEditor } = useUniversalComments({
-      detail: motion,
-      comments,
-      loginUser,
-      chain,
-      type: TYPE_FINANCIAL_MOTION,
-    });
+export default withLoginUserRedux(({ loginUser, motion, comments, chain }) => {
+  const { CommentComponent, focusEditor } = useUniversalComments({
+    detail: motion,
+    comments,
+    loginUser,
+    chain,
+    type: detailPageCategory.FINANCIAL_MOTION,
+  });
 
-    motion.status = motion.state?.state;
+  motion.status = motion.state?.state;
 
-    const desc = getMetaDesc(motion, "Financial Motion");
-    return (
-      <Layout
-        user={loginUser}
-        chain={chain}
-        seoInfo={{ title: motion?.title, desc, ogImage: motion?.bannerUrl }}
-      >
-        <OutWrapper>
-          <MainCard className="post-content">
-            <Back href={`/financial-council/motions`} text="Back to Motions" />
-            <MotionDetail
-              motion={motion}
-              user={loginUser}
-              chain={chain}
-              type={TYPE_FINANCIAL_MOTION}
-              onReply={focusEditor}
-            />
-            {CommentComponent}
-          </MainCard>
-        </OutWrapper>
-      </Layout>
-    );
-  }
-);
+  const desc = getMetaDesc(motion, "Financial Motion");
+  return (
+    <Layout
+      user={loginUser}
+      chain={chain}
+      seoInfo={{ title: motion?.title, desc, ogImage: motion?.bannerUrl }}
+    >
+      <OutWrapper>
+        <MainCard className="post-content">
+          <Back href={`/financial-council/motions`} text="Back to Motions" />
+          <MotionDetail
+            motion={motion}
+            user={loginUser}
+            chain={chain}
+            type={detailPageCategory.FINANCIAL_MOTION}
+            onReply={focusEditor}
+          />
+          {CommentComponent}
+        </MainCard>
+      </OutWrapper>
+    </Layout>
+  );
+});
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const chain = process.env.CHAIN;
