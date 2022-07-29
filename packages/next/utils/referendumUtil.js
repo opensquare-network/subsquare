e45f4d16f79ba39f8c240621bdc5e16e74e871ff
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-29 18:35:52 +0800

    Extract compareRationals to next-common package

--- packages/next/utils/referendumUtil.js
+++ packages/next/utils/referendumUtil.js
@@ -1,4 +1,5 @@
 import BigNumber from "bignumber.js";
+import compareRationals from "next-common/utils/democracy/rational";
 
 const ONE = new BigNumber(1);
 
@@ -32,33 +33,6 @@ export function getThresholdOfSuperMajorityApprove(turnout, totalIssuance) {
   const threshold = bnAyes.div(bnAyes.plus(ONE)).times(100).toFixed(0);
 
   return `${threshold}%`;
-}
-
-function compareRationals(n1, d1, n2, d2) {
-  while (true) {
-    const q1 = n1.div(d1);
-    const q2 = n2.div(d2);
-
-    if (q1.lt(q2)) {
-      return true;
-    } else if (q2.lt(q1)) {
-      return false;
-    }
-
-    const r1 = n1.mod(d1);
-    const r2 = n2.mod(d2);
-
-    if (r2.isZero()) {
-      return false;
-    } else if (r1.isZero()) {
-      return true;
-    }
-
-    n1 = d2;
-    n2 = d1;
-    d1 = r2;
-    d2 = r1;
-  }
 }
 
 export function calcPassing(referendumInfo, totalIssuance) {
