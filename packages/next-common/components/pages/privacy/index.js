2d9efe88b00bf4c077c7cd7d8819465c9963fdcb
yoshiyuki <brother-zhou@qq.com>
2022-08-01 11:50:14 +0800

    style: darkmode batch fixup (#1870)

--- packages/next-common/components/pages/privacy/index.js
+++ packages/next-common/components/pages/privacy/index.js
@@ -1,29 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import Link from "next/link";
 import { withLoginUserRedux } from "next-common/lib";
 import NextHead from "next-common/components/nextHead";
 import privacyMd from "./privacy-policy.md";
 import Markdown from "next-common/components/micromarkMd";
-import ArrowLeft from "../../icons/arrowLeft";
 import BaseLayout from "../../layout/baseLayout";
+import Back from "../../back";
 
 const Wrapper = styled.div`
   margin: 0 auto;
   max-width: 852px;
-`;
-
-const BackButton = styled.div`
-  margin-top: 8px;
-  display: flex;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 100%;
-  align-items: center;
-  cursor: pointer;
-  > svg {
-    margin-right: 12px;
-  }
 `;
 
 const Content = styled.div`
@@ -54,14 +40,7 @@ const Privacy = withLoginUserRedux(({ loginUser, chain }) => {
     <BaseLayout user={loginUser} chain={chain}>
       <NextHead title="Privacy Policy" desc="Privacy Policy" />
       <Wrapper>
-        <Link passHref={true} href="/signup">
-          <a>
-            <BackButton>
-              <ArrowLeft />
-              <div>Back</div>
-            </BackButton>
-          </a>
-        </Link>
+        <Back text="Back" href="signup" />
         <Content>
           <Title>Privacy Policy</Title>
           <Markdown md={privacyMd} />
--- packages/next-common/components/pages/terms/index.js
+++ packages/next-common/components/pages/terms/index.js
@@ -1,29 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import Link from "next/link";
 import { withLoginUserRedux } from "next-common/lib";
 import NextHead from "next-common/components/nextHead";
 import termsMd from "./terms-of-service.md";
 import Markdown from "next-common/components/micromarkMd";
-import ArrowLeft from "../../icons/arrowLeft";
 import BaseLayout from "../../layout/baseLayout";
+import Back from "../../back";
 
 const Wrapper = styled.div`
   margin: 0 auto;
   max-width: 852px;
-`;
-
-const BackButton = styled.div`
-  display: flex;
-  margin-top: 8px;
-  font-weight: bold;
-  font-size: 16px;
-  line-height: 100%;
-  align-items: center;
-  cursor: pointer;
-  > svg {
-    margin-right: 12px;
-  }
 `;
 
 const Content = styled.div`
@@ -55,12 +41,7 @@ const Terms = withLoginUserRedux(({ loginUser, chain }) => {
     <BaseLayout user={loginUser} chain={chain}>
       <NextHead title="Terms of Service" desc="Terms of Service" />
       <Wrapper>
-        <Link href="/signup" passHref>
-          <BackButton>
-            <ArrowLeft />
-            <div>Back</div>
-          </BackButton>
-        </Link>
+        <Back text="Back" href="signup" />
         <Content>
           <Title>Terms of Service</Title>
           <Markdown md={termsMd} />
