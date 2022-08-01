5312860c4e1413d697f48cc6e6ff6f2a78481340
yoshiyuki <brother-zhou@qq.com>
2022-08-01 14:38:51 +0800

    style: darkmode caret fixup

--- packages/next-common/components/icons/caret.js
+++ packages/next-common/components/icons/caret.js
@@ -1,22 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const SVG = styled.svg`
+  path {
+    stroke: ${(props) =>
+      props.isGrey ? props.theme.textTertiary : props.theme.textPrimary};
+  }
+`;
 
 export default function Caret({ isGrey = false, size = 14, down = true }) {
   return (
-    <svg
+    <SVG
       width={size}
       height={size}
       viewBox="0 0 14 14"
       fill="none"
       transform={`rotate(${down ? 0 : 180})`}
       xmlns="http://www.w3.org/2000/svg"
+      isGrey={isGrey}
     >
       <path
         d="M10 6L7 9L4 6"
-        stroke={isGrey ? "#9DA9BB" : "#1E2134"}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-    </svg>
+    </SVG>
   );
 }
