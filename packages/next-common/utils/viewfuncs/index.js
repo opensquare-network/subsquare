433e718e4c105d702519f766fa377824f7101f0a
yoshiyuki <brother-zhou@qq.com>
2022-08-08 16:36:46 +0800

    feat: user can one-key copy contents, #1731 (#1912)

--- packages/next-common/utils/viewfuncs/index.js
+++ packages/next-common/utils/viewfuncs/index.js
@@ -99,3 +99,14 @@ export const renderDisableNonAddressLink = (el) => {
     }
   });
 };
+
+export function richTextCopy(str) {
+  function listener(e) {
+    e.clipboardData.setData("text/html", str);
+    e.clipboardData.setData("text/plain", str);
+    e.preventDefault();
+  }
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+}
