f67e188abac29b44af32b26cf33d9a6cce357724
yoshiyuki <brother-zhou@qq.com>
2022-08-19 10:41:17 +0800

    stash： shareSNS

--- packages/next-common/components/shareSNS/index.js
+++ packages/next-common/components/shareSNS/index.js
@@ -1,8 +1,10 @@
 import { useCallback } from "react";
 import styled from "styled-components";
 import Twitter from "../../assets/imgs/icons/share.svg";
+import { shadow_200 } from "../../styles/componentCss";
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
   gap: 8px;
   margin-bottom: 32px;
@@ -22,6 +24,34 @@ const ShareItem = styled.span`
   }
 `;
 
+const OptionWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 100%;
+  background: ${(props) => props.theme.neutral};
+  width: 96px;
+  padding: 8px 0;
+  border-radius: 4px;
+  border-width: ${(props) => (props.theme.isDark ? 1 : 0)}px;
+  border-style: ${(props) => (props.theme.isDark ? "solid" : "none")};
+  border-color: ${(props) => props.theme.grey200Border};
+  color: ${(props) => props.theme.textPrimary};
+  ${shadow_200};
+`;
+
+const OptionItem = styled.div`
+  height: 36px;
+  line-height: 36px;
+  cursor: pointer;
+  padding: 0 12px;
+  font-weight: 500;
+  color: ${(props) => props.theme.textPrimary};
+
+  :hover {
+    background: ${(props) => props.theme.grey100Bg};
+  }
+`;
+
 export default function Share({}) {
   const tweet = useCallback(() => {
     const url =
@@ -38,9 +68,14 @@ export default function Share({}) {
 
   return (
     <Wrapper>
-      <ShareItem onClick={tweet}>
-        <Twitter className="twitter" />
-      </ShareItem>
+      <Twitter className="twitter" />
+      Share
+      <OptionWrapper>
+        <OptionItem>
+          <ShareItem onClick={tweet}>Twitter</ShareItem>
+        </OptionItem>
+        <OptionItem>Copy Link</OptionItem>
+      </OptionWrapper>
     </Wrapper>
   );
 }
