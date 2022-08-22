ab71a99801145667976643a72a369ed4386b3a2a
Yongfeng LI <wliyongfeng@gmail.com>
2022-08-23 00:04:02 +0800

    Fix twitter text, #1530

--- packages/next-common/utils/post/share.js
+++ packages/next-common/utils/post/share.js
@@ -5,26 +5,26 @@ export const getShare2SNStext = (post, type) => {
 
   let category = "";
   if (detailPageCategory.DEMOCRACY_PROPOSAL === type) {
-    category = `Public proposal #${post.proposalIndex}:`
+    category = `Public proposal #${ post.proposalIndex }:`
   } else if (detailPageCategory.DEMOCRACY_REFERENDUM === type) {
-    category = `Referendum #${post.referendumIndex}:`
-  } else if (detailPageCategory.DEMOCRACY_EXTERNAL) {
+    category = `Referendum #${ post.referendumIndex }:`
+  } else if (detailPageCategory.DEMOCRACY_EXTERNAL === type) {
     category = `External proposal:`
-  } else if (detailPageCategory.TREASURY_PROPOSAL) {
-    category = `Treasury proposal #${post.proposalIndex}:`
-  } else if (detailPageCategory.TREASURY_BOUNTY) {
-    category = `Bounty #${post.bountyIndex}:`
-  } else if (detailPageCategory.TREASURY_CHILD_BOUNTY) {
-    category = `Child bounty #${post.index}:`
-  } else if (detailPageCategory.TREASURY_TIP) {
-    category = `Tip #${post.index}:`
-  } else if (detailPageCategory.COUNCIL_MOTION) {
-    category = `Council motion #${post.motionIndex}:`
-  } else if (detailPageCategory.TECH_COMM_MOTION) {
-    category = `Tech. Comm. motion #${post.motionIndex}:`
-  } else if (detailPageCategory.FINANCIAL_MOTION) {
-    category = `Financial council motion #${post.motionIndex}:`
+  } else if (detailPageCategory.TREASURY_PROPOSAL === type) {
+    category = `Treasury proposal #${ post.proposalIndex }:`
+  } else if (detailPageCategory.TREASURY_BOUNTY === type) {
+    category = `Bounty #${ post.bountyIndex }:`
+  } else if (detailPageCategory.TREASURY_CHILD_BOUNTY === type) {
+    category = `Child bounty #${ post.index }:`
+  } else if (detailPageCategory.TREASURY_TIP === type) {
+    category = `Tip #${ post.index }:`
+  } else if (detailPageCategory.COUNCIL_MOTION === type) {
+    category = `Council motion #${ post.motionIndex }:`
+  } else if (detailPageCategory.TECH_COMM_MOTION === type) {
+    category = `Tech. Comm. motion #${ post.motionIndex }:`
+  } else if (detailPageCategory.FINANCIAL_MOTION === type) {
+    category = `Financial council motion #${ post.motionIndex }:`
   }
 
-  return `[${chain}] ${category}${post.title}`;
+  return `[${ chain }] ${ category }${ post.title }`;
 };
