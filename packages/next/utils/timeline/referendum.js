4c9d089c8518872a6b19370a83f8cbfd19c8984a
Yongfeng LI <wliyongfeng@gmail.com>
2022-08-24 23:48:35 +0800

    Refactor (#1970)
    * Fix referendum tag color in treasury timeline
    
    * Add comment for referendum time creation function
    
    * Refactor motion voters

--- packages/next/utils/timeline/referendum.js
+++ packages/next/utils/timeline/referendum.js
@@ -2,22 +2,14 @@ import dayjs from "dayjs";
 import businessCategory from "next-common/utils/consts/business/category";
 import TimelineReferendumVote from "components/timelineReferendumVote";
 
+// used in treasury proposal timeline
 export function createReferendumTimelineData(
   referendum = {},
   chain,
   linkable = false,
   linkPrefix = ""
 ) {
-  const {
-    indexer,
-    referendumIndex,
-    meta,
-    method,
-    state,
-    timeline = [],
-  } = referendum;
-
-  const type = businessCategory.collective;
+  const { referendumIndex, timeline = [] } = referendum;
 
   return timeline.map((item) => {
     switch (item.method) {
@@ -35,7 +27,7 @@ export function createReferendumTimelineData(
           status: {
             value: linkable ? `Referendum #${referendumIndex}` : "Started",
             link,
-            type,
+            type: businessCategory.collective,
           },
           method: item.method,
           data: (
@@ -48,7 +40,7 @@ export function createReferendumTimelineData(
           indexer: item.indexer,
           referendumIndex: referendum.referendumIndex,
           time: dayjs(item.indexer.blockTime).format("YYYY-MM-DD HH:mm:ss"),
-          status: { value: item.method, type },
+          status: { value: item.method, type: businessCategory.democracyReferenda },
           method: item.method,
         };
       }
