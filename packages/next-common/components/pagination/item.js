5c6f531e31fdea1209dff12bfbf7328e860dee8f
yoshiyuki <brother-zhou@qq.com>
2022-08-31 16:04:54 +0800

    refactor: implement SSR/FSR switchable <Pagination>

--- packages/next-common/components/pagination/item.js
+++ packages/next-common/components/pagination/item.js
@@ -15,33 +15,42 @@ const Item = styled.a`
   justify-content: center;
   font-size: 14px;
   font-weight: 500;
-  color: ${ (props) => props.theme.textSecondary };
+  color: ${(props) => props.theme.textSecondary};
 
   :hover {
-    background: ${ (props) => props.theme.grey200Border };
+    background: ${(props) => props.theme.grey200Border};
   }
 
-  ${ (p) =>
-  p.active &&
-  css`
-            background: ${ (props) => props.theme.grey200Border };
-            color: ${ (props) => props.theme.textPrimary };
-            cursor: auto;
-            pointer-events: none;
-          ` }
+  ${(p) =>
+    p.active &&
+    css`
+      background: ${(props) => props.theme.grey200Border};
+      color: ${(props) => props.theme.textPrimary};
+      cursor: auto;
+      pointer-events: none;
+    `}
 `;
 
-export default function PageItem({ page, now }) {
+export default function PageItem({ page, now, onPageChange = null }) {
   const router = useRouter();
 
-  return <Link
-    key={ page }
-    href={ `${ router.pathname }?${ encodeUriQuery({
-      ...router.query,
-      page: page + 1,
-    }) }` }
-    passHref
-  >
-    <Item active={ now === page + 1 }>{ page + 1 }</Item>
-  </Link>
+  return (
+    <Link
+      key={page}
+      href={`${router.pathname}?${encodeUriQuery({
+        ...router.query,
+        page: page + 1,
+      })}`}
+      passHref
+    >
+      <Item
+        active={now === page + 1}
+        onClick={(e) => {
+          onPageChange && onPageChange(e, page + 1);
+        }}
+      >
+        {page + 1}
+      </Item>
+    </Link>
+  );
 }
