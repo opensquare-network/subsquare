a4dd82b20d529a4ed2fc3689e101c5f26193610d
yoshiyuki <brother-zhou@qq.com>
2022-09-08 14:51:49 +0800

    fix: force frontend set cookie to / path (#2029)
    * fix: force frontend set cookie to / path
    
    * fix: wrong commit

--- packages/next-common/utils/viewfuncs/cookies.js
+++ packages/next-common/utils/viewfuncs/cookies.js
@@ -6,9 +6,11 @@ const converter = {
     return value.replace(/(%[\dA-F]{2})+/gi, decodeURIComponent);
   },
   write: function (value) {
-    return encodeURIComponent(value).replace(
-      /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
-      decodeURIComponent
+    return (
+      encodeURIComponent(value).replace(
+        /%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,
+        decodeURIComponent
+      ) + `;path=/;`
     );
   },
 };
