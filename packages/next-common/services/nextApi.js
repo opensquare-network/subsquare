f8153527b121194c060121dd87ecbb00b307c1f9
yoshiyuki <brother-zhou@qq.com>
2022-09-08 14:44:53 +0800

    fix: force frontend set cookie to / path

--- packages/next-common/services/nextApi.js
+++ packages/next-common/services/nextApi.js
@@ -5,5 +5,5 @@ export default new Api(
 );
 
 export const ssrNextApi = new Api(
-  new URL(process.env.NEXT_PUBLIC_BACKEND_API_END_POINT).href
+  new URL(`/api/`, process.env.NEXT_PUBLIC_BACKEND_API_END_POINT).href
 );
