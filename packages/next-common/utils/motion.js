931cf175907001cf625e436af6f4469eada0bbde
Yongfeng LI <wliyongfeng@gmail.com>
2022-09-16 10:29:49 +0800

    Fix motion without index

--- packages/next-common/utils/motion.js
+++ packages/next-common/utils/motion.js
@@ -1,11 +1,17 @@
 import Chains from "./consts/chains";
+import isNil from "lodash.isnil";
 
 export function getMotionId(motion, chain) {
+  let id;
   if ([Chains.kusama, Chains.polkadot].includes(chain)) {
-    return motion.motionIndex === undefined ? motion.index : motion.motionIndex;
+    id = motion.motionIndex === undefined ? motion.index : motion.motionIndex;
   }
 
-  return `${motion?.indexer?.blockHeight}_${motion?.hash}`;
+  if (isNil(id)) {
+    id = `${motion?.indexer?.blockHeight}_${motion?.hash}`;
+  }
+
+  return id;
 }
 
 export function getUniqueMotionId(motion) {
