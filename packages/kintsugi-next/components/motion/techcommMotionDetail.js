f6a194977f1a00170ae5b29420d916856983e767
Yongfeng LI <wliyongfeng@gmail.com>
2022-10-04 22:27:27 +0800

    Remove useless code

--- packages/kintsugi-next/components/motion/techcommMotionDetail.js
+++ packages/kintsugi-next/components/motion/techcommMotionDetail.js
@@ -35,37 +35,6 @@ const DividerWrapper = styled(Flex)`
   }
 `;
 
-const TitleWrapper = styled.div`
-  margin-bottom: 8px;
-  overflow: hidden;
-
-  > :not(:first-child) {
-    ::before {
-      content: "·";
-      font-size: 20px;
-      line-height: 28px;
-      color: ${(props) => props.theme.textTertiary};
-      margin: 0 8px;
-    }
-  }
-`;
-
-const Title = styled.div`
-  max-width: 750px;
-  word-break: break-all;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 140%;
-  margin-bottom: 12px;
-`;
-
-const Index = styled.div`
-  float: left;
-  font-weight: 500;
-  font-size: 20px;
-  line-height: 140%;
-`;
-
 const FlexWrapper = styled.div`
   display: flex;
   justify-content: space-between;
@@ -163,7 +132,6 @@ export default function TechcommMotionDetail({
   loginUser,
   type,
 }) {
-  console.log('motion', motion);
   const node = getNode(chain);
   const [post, setPost] = useState(motion);
   const [isEdit, setIsEdit] = useState(false);
