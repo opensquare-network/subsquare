12804b6268017c3eb4ebc43ab0cb93fef03841b4
chaojun <chaojun@opensquare.network>
2022-10-26 11:54:06 +0800

    Fix: enable save button only when user settings is different from UI settings

--- packages/next-common/components/setting/notification/useCouncilOptions.js
+++ packages/next-common/components/setting/notification/useCouncilOptions.js
@@ -4,15 +4,20 @@ import { useCallback, useState } from "react";
 import { Label, Sections, SubLabel, ToggleItem } from "./styled";
 
 export default function useCouncilOptions({ saving, disabled, ...data }) {
-  const [isChanged, setIsChanged] = useState(false);
   const [councilMotionProposed, setTreasuryProposalProposed] = useState(data.councilMotionProposed?.isOn);
   const [councilMotionVoted, setCouncilMotionVoted] = useState(data.councilMotionVoted?.isOn);
   const [councilMotionApproved, setTreasuryProposalApproved] = useState(data.councilMotionApproved?.isOn);
   const [councilMotionDisApproved, setCouncilMotionDisApproved] = useState(data.councilMotionDisApproved?.isOn);
 
+  const isChanged = (
+    councilMotionProposed !== data.councilMotionProposed?.isOn ||
+    councilMotionVoted !== data.councilMotionVoted?.isOn ||
+    councilMotionApproved !== data.councilMotionApproved?.isOn ||
+    councilMotionDisApproved !== data.councilMotionDisApproved?.isOn
+  );
+
   const changeGuard = (setter) => (data) => {
     if (!saving && !disabled) {
-      setIsChanged(true);
       setter(data);
     }
   };
