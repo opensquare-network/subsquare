46a9a550a1644c79c983fbcf8a2e4d71a6875e4e
chaojun <chaojun@opensquare.network>
2022-11-01 10:23:43 +0800

    Fix: fetch post

--- packages/next-common/context/post/update.js
+++ packages/next-common/context/post/update.js
@@ -2,7 +2,7 @@ import { toApiType } from "../../utils/viewfuncs";
 import nextApi from "../../services/nextApi";
 import { POST_UPDATE_ACTION } from "./index";
 
-export default async function fetchAndUpdatePost(dispatch, postId, type) {
+export default async function fetchAndUpdatePost(dispatch, type, postId) {
   const { result: newPost } = await nextApi.fetch(`${ toApiType(type) }/${ postId }`);
 
   if (newPost) {
