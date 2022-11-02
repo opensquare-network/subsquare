ea01bb773b945d5ce3dc32725a77f63b5d9821b8
2nthony <ant.hu@outlook.com>
2022-11-02 16:46:48 +0800

    home layout menus

--- packages/next-common/components/layout/HomeLayout.js
+++ packages/next-common/components/layout/HomeLayout.js
@@ -1,15 +1,17 @@
 import React from "react";
 import BaseLayout from "./baseLayout";
-import homeMenus from "../../utils/consts/menu";
 import Menu from "../menu";
+import { useHomeMenus } from "../../utils/hooks/useHomeMenu";
 
 export default function HomeLayout({ user, children, seoInfo }) {
+  const menus = useHomeMenus();
+
   return (
     <BaseLayout
       user={user}
       children={children}
       seoInfo={seoInfo}
-      left={<Menu menu={homeMenus} chain={process.env.NEXT_PUBLIC_CHAIN} />}
+      left={<Menu menu={menus} chain={process.env.NEXT_PUBLIC_CHAIN} />}
     />
   );
 }
