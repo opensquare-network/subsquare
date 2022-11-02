569d2b44ed036af0a38577576988994bd1773aad
2nthony <ant.hu@outlook.com>
2022-11-02 19:04:20 +0800

    page gov2 init

---
+++ packages/next/pages/gov2/index.js
@@ -0,0 +1,28 @@
+import { withLoginUser, withLoginUserRedux } from "next-common/lib";
+import { EmptyList } from "next-common/utils/constants";
+import Gov2Layout from "next-common/components/layout/Gov2Layout";
+import PostList from "next-common/components/postList";
+
+export default withLoginUserRedux(({ loginUser, chain }) => {
+  // FIXME: seo
+  const seoInfo = { title: "", desc: "" };
+
+  return (
+    <Gov2Layout user={loginUser} seoInfo={seoInfo}>
+      <PostList chain={chain} create={null} items={[]} />
+    </Gov2Layout>
+  );
+});
+
+export const getServerSideProps = withLoginUser(async (context) => {
+  const chain = process.env.CHAIN;
+
+  const { page, page_size: pageSize } = context.query;
+
+  return {
+    props: {
+      chain,
+      posts: EmptyList,
+    },
+  };
+});
