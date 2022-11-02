a71930d4365dacbcbedc504fd875e7d47ce6f4b2
2nthony <ant.hu@outlook.com>
2022-11-02 16:45:59 +0800

    governance v2 menu

---
+++ packages/next-common/utils/consts/menu/governanceV2.js
@@ -0,0 +1,13 @@
+import Gov2Icon from "../../../assets/imgs/icons/type-gov2.svg";
+import GovernanceV2Button from "../../../components/menu/governanceV2Button";
+
+// FIXME: the pathname
+const governanceV2Menu = {
+  value: "governance-v2",
+  name: "Governance V2",
+  pathname: "/governance-v2",
+  icon: <Gov2Icon />,
+  itemRender: (icon, name) => <GovernanceV2Button icon={icon} name={name} />,
+};
+
+export default governanceV2Menu;
