84b73a029357464660ce5d901f4abc965a7fd618
2nthony <ant.hu@outlook.com>
2022-11-02 18:53:42 +0800

    fix menus

--- packages/next-common/utils/consts/menu/gov2.js
+++ packages/next-common/utils/consts/menu/gov2.js
@@ -1,7 +1,10 @@
 import Gov2Icon from "../../../assets/imgs/icons/type-gov2.svg";
+import TrackAllIcon from "../../../assets/imgs/icons/track-all.svg";
+import BackIcon from "../../../assets/imgs/icons/back.svg";
 import Gov2Button from "../../../components/menu/gov2Button";
 
-const gov2Menu = {
+// for v1 entry
+const gov2Entry = {
   value: "gov2",
   name: "Governance V2",
   pathname: "/gov2",
@@ -9,4 +12,29 @@ const gov2Menu = {
   itemRender: (icon, name) => <Gov2Button icon={icon} name={name} />,
 };
 
-export default gov2Menu;
+const back = {
+  items: [
+    {
+      value: "gov1",
+      name: "Back to Gov1",
+      pathname: "/",
+      icon: <BackIcon />,
+    },
+  ],
+};
+
+const gov2Menus = [
+  back,
+  {
+    name: "REFERENDA",
+    items: [
+      {
+        value: "all",
+        name: "All",
+        pathname: "/gov2",
+        icon: <TrackAllIcon />,
+      },
+    ],
+  },
+];
+export { gov2Entry, gov2Menus };
