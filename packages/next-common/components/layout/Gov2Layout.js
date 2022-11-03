7e637d9731eb11efbda4461c431e7ca29e4a06dd
2nthony <ant.hu@outlook.com>
2022-11-03 16:01:07 +0800

    use gov2 menu

--- packages/next-common/components/layout/Gov2Layout.js
+++ packages/next-common/components/layout/Gov2Layout.js
@@ -1,14 +1,16 @@
-import { gov2Menus } from "../../utils/consts/menu/gov2";
+import { useGov2Menu } from "../../utils/hooks/useGov2Menu";
 import Menu from "../menu";
 import BaseLayout from "./baseLayout";
 
 export default function Gov2Layout({ user, children, seoInfo }) {
+  const gov2Menu = useGov2Menu();
+
   return (
     <BaseLayout
       user={user}
       children={children}
       seoInfo={seoInfo}
-      left={<Menu menu={gov2Menus} chain={process.env.NEXT_PUBLIC_CHAIN} />}
+      left={<Menu menu={gov2Menu} chain={process.env.NEXT_PUBLIC_CHAIN} />}
     />
   );
 }
