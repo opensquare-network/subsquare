48b4e6cc90bc592adbd04aec0ad3f24ce605bdda
2nthony <ant.hu@outlook.com>
2022-11-03 10:47:32 +0800

    gov2 summary

--- packages/next/pages/gov2/index.js
+++ packages/next/pages/gov2/index.js
@@ -2,15 +2,20 @@ import { withLoginUser, withLoginUserRedux } from "next-common/lib";
 import { EmptyList } from "next-common/utils/constants";
 import Gov2Layout from "next-common/components/layout/Gov2Layout";
 import PostList from "next-common/components/postList";
+// TODO: use this directly?
+import DemocracySummary from "next-common/components/summary/democracySummary";
+import businessCategory from "next-common/utils/consts/business/category";
 
 export default withLoginUserRedux(({ loginUser, chain, posts }) => {
   // FIXME: seo
+  const category = businessCategory.democracyReferenda;
   const seoInfo = { title: "", desc: "" };
 
   return (
     <Gov2Layout user={loginUser} seoInfo={seoInfo}>
       <PostList
         chain={chain}
+        category={category}
         create={null}
         items={[]}
         pagination={{
@@ -18,6 +23,7 @@ export default withLoginUserRedux(({ loginUser, chain, posts }) => {
           pageSize: posts.pageSize,
           total: posts.total,
         }}
+        summary={<DemocracySummary chain={chain} />}
       />
     </Gov2Layout>
   );
