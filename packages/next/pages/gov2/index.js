3e47bd8e230dc67e115d70a29d86618f62a04236
2nthony <ant.hu@outlook.com>
2022-11-03 10:00:58 +0800

    pagination

--- packages/next/pages/gov2/index.js
+++ packages/next/pages/gov2/index.js
@@ -3,13 +3,22 @@ import { EmptyList } from "next-common/utils/constants";
 import Gov2Layout from "next-common/components/layout/Gov2Layout";
 import PostList from "next-common/components/postList";
 
-export default withLoginUserRedux(({ loginUser, chain }) => {
+export default withLoginUserRedux(({ loginUser, chain, posts }) => {
   // FIXME: seo
   const seoInfo = { title: "", desc: "" };
 
   return (
     <Gov2Layout user={loginUser} seoInfo={seoInfo}>
-      <PostList chain={chain} create={null} items={[]} />
+      <PostList
+        chain={chain}
+        create={null}
+        items={[]}
+        pagination={{
+          page: posts.page,
+          pageSize: posts.pageSize,
+          total: posts.total,
+        }}
+      />
     </Gov2Layout>
   );
 });
