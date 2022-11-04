4d2c602dc8d1ad7b1fa55d49579f8190c51d315b
2nthony <ant.hu@outlook.com>
2022-11-04 21:03:02 +0800

    gov2 track menu

--- packages/next/pages/gov2/index.js
+++ packages/next/pages/gov2/index.js
@@ -6,38 +6,47 @@ import PostList from "next-common/components/postList";
 import DemocracySummary from "next-common/components/summary/democracySummary";
 import { toGov2ReferendaListItem } from "utils/viewfuncs";
 import mockGov2Posts from "next-common/utils/mocks/gov2-posts.json";
+import nextApi from "next-common/services/nextApi";
+import { gov2TracksApi } from "next-common/services/url";
+import { composeGov2TracksMenu } from "next-common/utils/gov2";
 
-export default withLoginUserRedux(({ loginUser, chain, posts, title }) => {
-  // FIXME: seo
-  const seoInfo = { title: "", desc: "" };
-  const items = (posts.items || []).map((item) =>
-    toGov2ReferendaListItem(chain, item)
-  );
+export default withLoginUserRedux(
+  ({ loginUser, chain, posts, title, tracks }) => {
+    // FIXME: seo
+    const seoInfo = { title: "", desc: "" };
+    const items = (posts.items || []).map((item) =>
+      toGov2ReferendaListItem(chain, item)
+    );
 
-  return (
-    <Gov2Layout user={loginUser} seoInfo={seoInfo}>
-      <PostList
-        chain={chain}
-        title={title}
-        category="gov2"
-        create={null}
-        items={items}
-        pagination={{
-          page: posts.page,
-          pageSize: posts.pageSize,
-          total: posts.total,
-        }}
-        summary={<DemocracySummary chain={chain} />}
-      />
-    </Gov2Layout>
-  );
-});
+    return (
+      <Gov2Layout user={loginUser} seoInfo={seoInfo} tracks={tracks}>
+        <PostList
+          chain={chain}
+          title={title}
+          category="gov2"
+          create={null}
+          items={items}
+          pagination={{
+            page: posts.page,
+            pageSize: posts.pageSize,
+            total: posts.total,
+          }}
+          summary={<DemocracySummary chain={chain} />}
+        />
+      </Gov2Layout>
+    );
+  }
+);
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const chain = process.env.CHAIN;
 
   const { page, page_size: pageSize } = context.query;
   // TODO: fetch posts
+
+  const [{ result: tracks }] = await Promise.all([
+    nextApi.fetch(gov2TracksApi),
+  ]);
 
   const posts = {
     items: mockGov2Posts,
@@ -50,6 +59,7 @@ export const getServerSideProps = withLoginUser(async (context) => {
       chain,
       posts: posts ?? EmptyList,
       title: "All Referenda",
+      tracks,
     },
   };
 });
