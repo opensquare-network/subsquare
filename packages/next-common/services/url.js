7762348bac603b9fd5cc4bbf19fbd2dd2a02eddb
2nthony <ant.hu@outlook.com>
2022-11-07 15:16:22 +0800

    api changes

--- packages/next-common/services/url.js
+++ packages/next-common/services/url.js
@@ -1,7 +1,8 @@
 // gov2
 export const gov2TracksApi = "/api/gov2/tracks";
-export const gov2ReferendumsApi = "/api/gov2/referendums";
-export const gov2ReferendumsDetailApi = (index) =>
-  gov2ReferendumsApi + `/${index}`;
-export const gov2ReferendumsCommentApi = (id) =>
-  gov2ReferendumsApi + `/${id}/comments`;
+export const gov2ReferendumsTrackApi = (trackId) =>
+  "/api/gov2/referendums/track" + `/${trackId}`;
+export const gov2ReferendumsTrackDetailApi = (index) =>
+  gov2ReferendumsTrackApi + `/${index}`;
+export const gov2ReferendumsTrackCommentApi = (id) =>
+  gov2ReferendumsTrackApi + `/${id}/comments`;
