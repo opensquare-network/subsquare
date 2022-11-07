4d9a5004833bf5988206f1432c68cda3a2cd3257
2nthony <ant.hu@outlook.com>
2022-11-07 13:13:48 +0800

    referendum api urls

--- packages/next-common/services/url.js
+++ packages/next-common/services/url.js
@@ -1,2 +1,7 @@
 // gov2
 export const gov2TracksApi = "/api/gov2/tracks";
+export const gov2ReferendumsApi = "/api/gov2/referendums";
+export const gov2ReferendumsDetailApi = (index) =>
+  gov2ReferendumsApi + `/${index}`;
+export const gov2ReferendumsCommentApi = (id) =>
+  gov2ReferendumsApi + `/${id}/comments`;
