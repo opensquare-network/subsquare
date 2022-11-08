e13b74f629386fc87c1cc7f229bf942ecea4f2fe
2nthony <ant.hu@outlook.com>
2022-11-08 19:18:05 +0800

    gov2 metadata api

--- packages/next-common/components/gov2/referendum/metadata/index.js
+++ packages/next-common/components/gov2/referendum/metadata/index.js
@@ -2,37 +2,105 @@ import KVList from "../../../listInfo/kvList";
 import React from "react";
 import Proposal from "../../../proposal";
 import User from "../../../user";
+import { estimateBlocksTime, getNode, toPrecision } from "../../../../utils";
+import { useSelector } from "react-redux";
+import { blockTimeSelector } from "../../../../store/reducers/chainSlice";
+import styled from "styled-components";
+import { SubScanAccountLink } from "../../../links/subscanLink";
+import Flex from "../../../styled/flex";
+import Tooltip from "../../../tooltip";
 
-export default function Gov2ReferendumMetadata({
-  api,
-  proposer,
-  chain,
-  onchainData = {},
-  detail,
-}) {
-  const call = detail?.onchainData?.preImage?.call || detail?.onchainData?.call;
-  const shorten = detail?.onchainData?.preImage?.shorten;
+// submissionDeposit
+// decisionDeposit
+const DepositWrapper = styled(Flex)`
+  gap: 8px;
+`;
+const BondValueWrapper = styled(Flex)`
+  gap: 8px;
+  &::before {
+    content: "·";
+    color: ${(p) => p.theme.textTertiary};
+  }
+`;
+
+function BondValue({ deposit, decimals, symbol }) {
+  const value = `${toPrecision(deposit, decimals)} ${symbol}`;
+
+  return (
+    <BondValueWrapper>
+      <span>{value}</span>
+      <Tooltip content={`Bond: ${value}`} />
+    </BondValueWrapper>
+  );
+}
+
+export default function Gov2ReferendumMetadata({ chain, detail }) {
+  const blockTime = useSelector(blockTimeSelector);
+
+  const node = getNode(chain);
+  if (!node) {
+    return null;
+  }
+
+  const decimals = node.decimals;
+  const symbol = node.voteSymbol || node.symbol;
+
+  const info = detail?.onchainData?.info ?? {};
+  const proposal = detail?.onchainData?.proposal ?? {};
+  const trackInfo = detail?.onchainData?.trackInfo ?? {};
+
+  const decisionPeriod = estimateBlocksTime(
+    trackInfo.decisionPeriod,
+    blockTime
+  );
+  const confirmPeriod = estimateBlocksTime(trackInfo.confirmPeriod, blockTime);
+
+  console.log(detail);
 
   const metadata = [
-    ["Submission", <User add={proposer} fontSize={14} />],
-    ["Decision", <User add={proposer} fontSize={14} />],
-    ["Decision Period", "todo"],
-    ["Confirming Period", "todo"],
-    ["Enact", "todo"],
-    ["Proposal", detail?.title],
+    [
+      "Submission",
+      <DepositWrapper>
+        <User add={info?.submissionDeposit?.who} fontSize={14} />
+        <SubScanAccountLink address={info?.submissionDeposit?.who} />
+        <BondValue
+          deposit={info?.submissionDeposit?.amount}
+          decimals={decimals}
+          symbol={symbol}
+        />
+      </DepositWrapper>,
+    ],
+    [
+      "Decision",
+      <DepositWrapper>
+        <User add={info?.decisionDeposit?.who} fontSize={14} />
+        <SubScanAccountLink address={info?.decisionDeposit?.who} />
+        <BondValue
+          deposit={info?.decisionDeposit?.amount}
+          decimals={decimals}
+          symbol={symbol}
+        />
+      </DepositWrapper>,
+    ],
+    ["Decision Period", `${decisionPeriod[0]} ${decisionPeriod[1]}`],
+    ["Confirming Period", `${confirmPeriod[0]} ${confirmPeriod[1]}`],
+    ["Enact", info?.enactment?.at],
+    ["Proposal", detail?.title ?? "Untitled"],
   ];
 
-  if (call) {
+  if (proposal?.args) {
     metadata.push([
       <Proposal
         key="preimage"
-        call={call}
+        call={proposal}
         chain={chain}
-        shorten={shorten}
-        referendumIndex={onchainData.referendumIndex}
+        referendumIndex={detail?.onchainData.referendumIndex}
+        headerWidth={160}
       />,
     ]);
   }
 
-  return <KVList title={"Metadata"} data={metadata} showFold={true} />;
+  return (
+    <KVList KWidth={160} title={"Metadata"} data={metadata} showFold={true} />
+  );
 }
