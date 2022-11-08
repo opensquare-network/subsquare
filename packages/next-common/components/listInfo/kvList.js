fde8c19e156deb76d800d16d75ce8b299201a8c0
2nthony <ant.hu@outlook.com>
2022-11-08 19:17:06 +0800

    gov2 kvlist header width

--- packages/next-common/components/listInfo/kvList.js
+++ packages/next-common/components/listInfo/kvList.js
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 import Accordion from "./accordion";
 import Row from "./row";
 
-function KVList({ data = [], title, showFold = true }) {
+function KVList({ data = [], title, showFold = true, KWidth }) {
   if (!data || data?.length === 0) {
     return null;
   }
@@ -10,7 +10,7 @@ function KVList({ data = [], title, showFold = true }) {
   return (
     <Accordion title={title} showFold={showFold}>
       {data.map((row, index) => (
-        <Row row={row} key={index} />
+        <Row headerWidth={KWidth} row={row} key={index} />
       ))}
     </Accordion>
   );
