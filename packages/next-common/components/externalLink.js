ca99534af507868fbfd2cdf5e06cfc6ab56cc4db
2nthony <ant.hu@outlook.com>
2022-11-09 10:36:14 +0800

    gov2 sidebar gov2 link

--- packages/next-common/components/externalLink.js
+++ packages/next-common/components/externalLink.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-export default function ExternalLink({ href, children, title ="" }) {
+export default function ExternalLink({
+  href,
+  children,
+  title = "",
+  className = "",
+}) {
   return (
     <a
+      className={className}
       href={href}
       target="_blank"
       rel="noopener noreferrer"
--- packages/next/components/gov2/sidebar/index.js
+++ packages/next/components/gov2/sidebar/index.js
@@ -1,3 +1,4 @@
+import ExternalLink from "next-common/components/externalLink";
 import ExternalLinkIcon from "next-common/components/icons/externalLink";
 import { p_12_normal } from "next-common/styles/componentCss";
 import styled, { useTheme } from "styled-components";
@@ -34,7 +35,7 @@ const VoteButton = styled.button`
   border-radius: 4px;
 `;
 
-const Tip = styled.a`
+const Link = styled(ExternalLink)`
   display: inline-flex;
   align-items: center;
   gap: 8px;
@@ -42,7 +43,7 @@ const Tip = styled.a`
   ${p_12_normal};
 `;
 
-// FIXME: gov2 sidebar href
+// FIXME: status
 // FIXME: vote button
 export default function Gov2Sidebar({ detail, chain }) {
   const { primaryPurple500 } = useTheme();
@@ -54,10 +55,11 @@ export default function Gov2Sidebar({ detail, chain }) {
 
       <VoteButton>Vote</VoteButton>
 
-      <Tip href="/">
+      {/* NOTE: link to polkadot gov2 blog */}
+      <Link href="https://polkadot.network/blog/gov2-polkadots-next-generation-of-decentralised-governance/">
         How Goverenance V2 Works
         <ExternalLinkIcon color={primaryPurple500} />
-      </Tip>
+      </Link>
     </Wrapper>
   );
 }
