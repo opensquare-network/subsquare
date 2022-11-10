f74fe9484f7f7d578b1defa27b177d551b109247
2nthony <ant.hu@outlook.com>
2022-11-10 20:37:02 +0800

    feat: estimate remain block time

--- packages/next-common/utils/index.js
+++ packages/next-common/utils/index.js
@@ -3,6 +3,11 @@ import BigNumber from "bignumber.js";
 import { nodes } from "./constants";
 import { extractTime } from "@polkadot/util";
 import { encodeAddress } from "@polkadot/util-crypto";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
 
 BigNumber.config({ EXPONENTIAL_AT: 36 });
 
@@ -263,4 +268,33 @@ export function isSameAddress(addr1, addr2) {
 
 export function isAddressInGroup(addr, addresses = []) {
   return addresses.some((item) => isSameAddress(addr, item));
+}
+
+export function estimateRemainBlockTime(blocks, blockTime, startTimeMs) {
+  const value = new BigNumber(blockTime).multipliedBy(blocks).toNumber();
+  const time = extractTime(Math.abs(value));
+
+  const now = dayjs();
+  const endDate = dayjs(startTimeMs).add(dayjs.duration(time));
+
+  const diffMs = endDate.diff(now);
+  const remainTime = extractTime(diffMs);
+  const { days, hours, minutes } = remainTime;
+
+  let format = ``;
+  if (days) {
+    format += `${days}d `;
+  }
+  if (hours) {
+    format += `${hours}hrs `;
+  }
+  if (minutes) {
+    format += `${minutes}mins `;
+  }
+  format += "remaining";
+
+  return {
+    ...remainTime,
+    format,
+  };
 }
