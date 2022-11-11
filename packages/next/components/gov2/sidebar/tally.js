0dde34f29bbd56cc0c469f2491d97023ab165ed4
chaojun <chaojun@opensquare.network>
2022-11-11 16:56:43 +0800

    Fix: use info.tally to get the newest data

--- packages/next/components/gov2/sidebar/tally.js
+++ packages/next/components/gov2/sidebar/tally.js
@@ -59,8 +59,7 @@ export default function Gov2Tally({ detail }) {
 
   // When the referendum is Cancelled or TimedOut,
   // Then info.tally is not saving the latest status
-  const tally =
-    detail?.onchainData?.state?.args?.tally || detail?.onchainData?.info?.tally;
+  const tally = detail?.onchainData?.info?.tally;
 
   const nAyes = toPrecision(tally?.ayes ?? 0, decimals);
   const nNays = toPrecision(tally?.nays ?? 0, decimals);
