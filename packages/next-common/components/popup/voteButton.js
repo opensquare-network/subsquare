de8f727bac5701e530ca25afb5ebdb75c5753ef6
2nthony <ant.hu@outlook.com>
2022-11-14 17:28:11 +0800

    swap aye nay button

--- packages/next-common/components/popup/voteButton.js
+++ packages/next-common/components/popup/voteButton.js
@@ -6,13 +6,6 @@ import { NegativeButton, PositiveButton } from "../buttons/colorButton";
 export default function VoteButton({ loadingState, doVote }) {
   return (
     <ButtonWrapper>
-      <PositiveButton
-        onClick={() => doVote(true)}
-        isLoading={loadingState === VoteLoadingEnum.Aye}
-        disabled={loadingState === VoteLoadingEnum.Nay}
-      >
-        Aye
-      </PositiveButton>
       <NegativeButton
         onClick={() => doVote(false)}
         isLoading={loadingState === VoteLoadingEnum.Nay}
@@ -20,6 +13,14 @@ export default function VoteButton({ loadingState, doVote }) {
       >
         Nay
       </NegativeButton>
+
+      <PositiveButton
+        onClick={() => doVote(true)}
+        isLoading={loadingState === VoteLoadingEnum.Aye}
+        disabled={loadingState === VoteLoadingEnum.Nay}
+      >
+        Aye
+      </PositiveButton>
     </ButtonWrapper>
   );
 }
