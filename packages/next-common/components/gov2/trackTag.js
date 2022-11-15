90f2040686fa97fc93eb7392d8f0468341f3f346
2nthony <ant.hu@outlook.com>
2022-11-15 15:24:46 +0800

    gov2 track tag

--- packages/next-common/components/gov2/trackTag.js
+++ packages/next-common/components/gov2/trackTag.js
@@ -1,17 +1,25 @@
-import styled, { useTheme } from "styled-components";
+import styled from "styled-components";
 import { p_12_medium } from "../../styles/componentCss";
 import { parseGov2TrackName } from "../../utils/gov2";
 
 const Tag = styled.span`
   padding: 2px 8px;
   border-radius: 10px;
-  color: ${(p) => p.theme.textSeondary};
-  background-color: ${(p) => p.theme.grey100Bg};
+  color: ${(p) => p.fg || p.theme.textSeondary};
+  background-color: ${(p) => p.bg || p.theme.grey100Bg};
   ${p_12_medium};
 `;
 
 export default function Gov2TrackTag({ name = "" }) {
-  const {} = useTheme();
+  const makeColorPair = (fg, bg) => ({ fg, bg });
 
-  return <Tag>{parseGov2TrackName(name)}</Tag>;
+  const trackColor = {
+    root: makeColorPair("#4caf91", "rgba(76, 175, 145, 0.1)"),
+  };
+
+  return (
+    <Tag fg={trackColor[name]?.fg} bg={trackColor[name]?.bg}>
+      {parseGov2TrackName(name)}
+    </Tag>
+  );
 }
