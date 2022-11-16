e1698181c30064577a6c99227883a2e910304fb9
2nthony <ant.hu@outlook.com>
2022-11-16 16:52:54 +0800

    fix use is gov2 page hook

--- packages/next-common/utils/hooks/useIsGov2Page.js
+++ packages/next-common/utils/hooks/useIsGov2Page.js
@@ -1,8 +1,9 @@
-import router from "next/router";
+import { useRouter } from "next/router";
 
 const gov2pathname = "/referenda";
 
 export function useIsGov2Page() {
+  const router = useRouter();
   const { pathname } = router;
   return pathname.startsWith(gov2pathname);
 }
