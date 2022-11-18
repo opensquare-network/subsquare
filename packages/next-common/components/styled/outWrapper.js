bf59a3aa38827b9c2f1c64bc5ba6a11a06abeecf
2nthony <ant.hu@outlook.com>
2022-11-18 15:24:46 +0800

    fix/detail page md reponsive

--- packages/next-common/components/styled/outWrapper.js
+++ packages/next-common/components/styled/outWrapper.js
@@ -1,11 +1,15 @@
-import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { mdcss } from "../../utils/responsive";
 
 const OutWrapper = styled.div`
   display: flex;
   max-width: 1184px;
   margin: 0 auto;
   position: relative;
+
+  ${mdcss(css`
+    max-width: 100%;
+  `)}
 `;
 
 export default OutWrapper;
