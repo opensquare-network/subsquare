f62dd6e8c7fb6aed781e6a86b0b471c4c5741eb5
Yongfeng LI <wliyongfeng@gmail.com>
2022-11-19 22:44:45 +0800

    Fix layout of council/tc members and settings page, #2296

--- packages/next-common/components/setting/styled.js
+++ packages/next-common/components/setting/styled.js
@@ -7,7 +7,6 @@ export const Wrapper = styled.div`
   @media screen and (max-width: 1024px) {
     max-width: 960px;
   }
-  margin: auto;
   @media screen and (min-width: 1080px) {
     padding-bottom: 16px;
   }
