2733853964e477dc7fe39c23892f9376045e7cb2
2nthony <ant.hu@outlook.com>
2022-11-21 15:51:24 +0800

    refactor: rename to all votes

--- packages/next-common/components/democracy/allVotesPopup/index.js
+++ packages/next-common/components/democracy/allVotesPopup/index.js
@@ -1,24 +1,30 @@
 import React, { useState } from "react";
 import Popup from "../../popup/wrapper/Popup";
 import VotesTab, { tabs } from "./tab";
-import { useSelector } from "react-redux";
-import {
-  isLoadingVotesSelector,
-  votesSelector,
-} from "../../../store/reducers/referendumSlice";
 import VotersList from "./votesList";
 import Pagination from "next-common/components/pagination";
 
-export default function VotesPopup({ setShowVoteList }) {
+export default function VotesPopup({
+  setShowVoteList,
+  allAye,
+  allNay,
+  isLoadingVotes,
+}) {
   const [tabIndex, setTabIndex] = useState(tabs[0].tabId);
-  const { allAye = [], allNay = [] } = useSelector(votesSelector);
-  const loading = useSelector(isLoadingVotesSelector);
   const [ayePage, setAyePage] = useState(1);
   const [nayPage, setNayPage] = useState(1);
-
   const pageSize = 50;
 
   const votes = tabIndex === tabs[0].tabId ? allAye : allNay;
+
+  function onPageChange(e, target) {
+    e.preventDefault();
+    if (tabIndex === "Aye") {
+      setAyePage(target);
+    } else {
+      setNayPage(target);
+    }
+  }
 
   const pagination = {
     page: tabIndex === "Aye" ? ayePage : nayPage,
@@ -30,25 +36,18 @@ export default function VotesPopup({ setShowVoteList }) {
   const sliceFrom = (pagination.page - 1) * pageSize;
   const sliceTo = sliceFrom + pageSize;
 
-  function onPageChange(e, target) {
-    e.preventDefault();
-    if (tabIndex === "Aye") {
-      setAyePage(target);
-    } else {
-      setNayPage(target);
-    }
-  }
-
   return (
-    <Popup title="Votes" onClose={() => setShowVoteList(false)}>
+    <Popup title="All Votes" onClose={() => setShowVoteList(false)}>
       <VotesTab
         tabIndex={tabIndex}
         setTabIndex={setTabIndex}
         ayesCount={allAye?.length || 0}
         naysCount={allNay?.length || 0}
       />
-      <VotersList items={votes.slice(sliceFrom, sliceTo)} loading={loading} />
-
+      <VotersList
+        items={votes.slice(sliceFrom, sliceTo)}
+        loading={isLoadingVotes}
+      />
       <Pagination {...pagination} />
     </Popup>
   );
