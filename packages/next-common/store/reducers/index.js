258d765873904e70c68dc2d10d72dc3999f6bd0e
chaojun <chaojun@opensquare.network>
2022-11-23 14:45:52 +0800

    Update

--- packages/next-common/store/reducers/index.js
+++ packages/next-common/store/reducers/index.js
@@ -4,7 +4,6 @@ import toastReducer from "./toastSlice";
 import nodeReducer from "./nodeSlice";
 import chainReducer from "./chainSlice";
 import tipReducer from "./tipSlice";
-// import settingReducer from "./settingSlice";
 import referendumReducer from "./referendumSlice";
 import gov2ReferendumReducer from "./gov2ReferendumSlice";
 
@@ -13,7 +12,6 @@ export default combineReducers({
   node: nodeReducer,
   chain: chainReducer,
   tip: tipReducer,
-  // setting: settingReducer,
   referendum: referendumReducer,
   gov2Referendum: gov2ReferendumReducer,
 });
