abe22df11777cf4d716b3b57ee913d179167be90
2nthony <ant.hu@outlook.com>
2022-11-28 19:05:38 +0800

    democracy summary delegation entry

---
+++ packages/next-common/components/summary/democracySummaryDelegationButton.js
@@ -0,0 +1,140 @@
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import styled, { css } from "styled-components";
+import { useUser } from "next-common/context/user";
+import useApi from "next-common/utils/hooks/useApi";
+import useIsMounted from "next-common/utils/hooks/useIsMounted";
+import { getSigner, sendTx } from "next-common/utils/sendTx";
+import DelegatePopup from "components/gov2/delegatePopup";
+import AddSVG from "next-common/assets/imgs/icons/add.svg";
+import RemoveSVG from "next-common/assets/imgs/icons/remove.svg";
+import { newErrorToast } from "next-common/store/reducers/toastSlice";
+
+const Button = styled.div`
+  cursor: pointer;
+
+  ${(p) =>
+    p.disabled &&
+    css`
+      pointer-events: none;
+    `}
+
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${(p) => (p.disabled ? p.theme.textSecondary : p.theme.textPrimary)};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4px 12px;
+  gap: 4px;
+  background: ${(p) => p.theme.neutral};
+  border: 1px solid ${(p) => p.theme.grey300Border};
+  border-radius: 4px;
+  :hover {
+    border-color: ${(p) => p.theme.grey400Border};
+  }
+
+  svg {
+    path {
+      stroke: ${(p) =>
+        p.disabled ? p.theme.textSecondary : p.theme.textPrimary};
+    }
+  }
+`;
+
+export default function DemocracySummaryDelegationButton({
+  delegating,
+  trackId,
+  onUndelegateInBlock,
+  onDelegateInBlock,
+}) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [showDelegatePopup, setShowDelegatePopup] = useState(false);
+
+  const loginUser = useUser();
+  const signerAddress = loginUser?.address;
+
+  const api = useApi();
+  const isMounted = useIsMounted();
+  const dispatch = useDispatch();
+
+  const showErrorToast = useCallback(
+    (message) => dispatch(newErrorToast(message)),
+    [dispatch]
+  );
+
+  const removeDelegating = useCallback(async () => {
+    if (!api) {
+      return showErrorToast("Chain network is not connected yet");
+    }
+
+    if (!signerAddress) {
+      return showErrorToast("Please login first");
+    }
+
+    try {
+      const signer = await getSigner(signerAddress);
+      api.setSigner(signer);
+    } catch (e) {
+      return showErrorToast(`Unable to find injected ${signerAddress}`);
+    }
+
+    const tx = api.tx.convictionVoting.undelegate(trackId);
+
+    setIsLoading(true);
+    try {
+      await sendTx({
+        tx,
+        dispatch,
+        setLoading: setIsLoading,
+        onInBlock: onUndelegateInBlock,
+        signerAddress,
+        isMounted,
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }, [
+    api,
+    dispatch,
+    signerAddress,
+    onUndelegateInBlock,
+    isMounted,
+    trackId,
+    showErrorToast,
+  ]);
+
+  const openDelegatePopup = useCallback(() => {
+    setShowDelegatePopup(true);
+  }, []);
+
+  const addDelegationButton = (
+    <Button onClick={openDelegatePopup}>
+      <AddSVG />
+      Delegate
+    </Button>
+  );
+
+  const removeDelegationButton = (
+    <Button disabled={isLoading} onClick={removeDelegating}>
+      <RemoveSVG />
+      Remove
+    </Button>
+  );
+
+  return (
+    <>
+      {delegating ? removeDelegationButton : addDelegationButton}
+      {showDelegatePopup && (
+        <DelegatePopup
+          trackId={trackId}
+          onInBlock={onDelegateInBlock}
+          onClose={() => setShowDelegatePopup(false)}
+        />
+      )}
+    </>
+  );
+}
---
+++ packages/next-common/components/summary/democracySummaryDelegationInfo.js
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Conviction } from "utils/referendumUtil";
+import { useChainSettings } from "next-common/context/chain";
+import { toPrecision } from "next-common/utils";
+import VoteLabel from "next-common/components/democracy/allVotesPopup/voteLabel";
+import ValueDisplay from "next-common/components/displayValue";
+import User from "next-common/components/user";
+
+const Wrapper = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+  color: ${(p) => p.theme.textTertiary};
+
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 4px 12px;
+  gap: 8px;
+  background: ${(p) => p.theme.grey100Bg};
+  border-radius: 4px;
+
+  > :nth-child(3),
+  > :nth-child(4) {
+    ::before {
+      content: "·";
+      margin-right: 8px;
+      color: ${(p) => p.theme.textTertiary};
+    }
+    color: ${(p) => p.theme.textSecondary};
+  }
+`;
+
+export default function DemocracySummaryDelegationInfo({ delegating }) {
+  const node = useChainSettings();
+  if (!delegating) {
+    return <div />;
+  }
+
+  const conviction = Conviction[delegating.conviction];
+
+  return (
+    <Wrapper>
+      <span>Delegating to</span>
+      <User add={delegating.target} />
+      <span>
+        <VoteLabel conviction={conviction} />
+      </span>
+      <ValueDisplay
+        value={toPrecision(delegating.balance, node.decimals)}
+        symbol={node.symbol}
+      />
+    </Wrapper>
+  );
+}
