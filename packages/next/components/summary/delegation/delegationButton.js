5ab4cef1a8a1784dd5a8702d4de7446b651e1f29
2nthony <ant.hu@outlook.com>
2022-11-28 12:44:57 +0800

    refactor: move add remove icons

--- packages/next/components/summary/delegation/delegationButton.js
+++ packages/next/components/summary/delegation/delegationButton.js
@@ -6,8 +6,8 @@ import useApi from "next-common/utils/hooks/useApi";
 import useIsMounted from "next-common/utils/hooks/useIsMounted";
 import { getSigner, sendTx } from "next-common/utils/sendTx";
 import DelegatePopup from "components/gov2/delegatePopup";
-import AddSVG from "./add.svg";
-import RemoveSVG from "./remove.svg";
+import AddSVG from "next-common/assets/imgs/icons/add.svg";
+import RemoveSVG from "next-common/assets/imgs/icons/remove.svg";
 import { newErrorToast } from "next-common/store/reducers/toastSlice";
 
 const Button = styled.div`
