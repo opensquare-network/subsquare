2640340003d5baabdc5b58c4fb2880af8756fffd
2nthony <ant.hu@outlook.com>
2022-11-30 21:18:11 +0800

    feat: multiple progress

---
+++ packages/next-common/components/progress/multiProgress.js
@@ -0,0 +1,83 @@
+import { useMemo } from "react";
+import styled, { css } from "styled-components";
+import Tooltip from "../tooltip";
+
+const bar_css = css`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  border-radius: 4px;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  border-radius: 4px;
+  height: 8px;
+`;
+
+const Bar = styled.div`
+  ${bar_css};
+`;
+
+const Background = styled(Bar)`
+  background-color: ${(p) => p.theme.grey100Bg};
+`;
+
+const Percentage = styled(Bar)``;
+const PercentageWrapper = styled(Bar)``;
+
+const TooltipWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(p) => p.start || 0}%;
+  width: ${(p) => (p.end >= 100 ? p.end - p.start : p.end)}%;
+  background-color: ${(p) => p.bg || p.theme.secondaryBlue100};
+  border-radius: 4px;
+
+  /* tooltip children wrapper */
+  > div {
+    width: ${(p) => p.percentage}%;
+    background-color: ${(p) => p.fg || p.theme.secondaryBlue500};
+    ${bar_css};
+  }
+`;
+
+const ensureMax100 = (n) => (Number(n) > 100 ? 100 : Number(n));
+
+export default function MultiProgress({ progressItems = [] }) {
+  const items = useMemo(() => {
+    return progressItems.map((item) => {
+      return {
+        ...item,
+        percentage: ensureMax100(item.percentage),
+        start: ensureMax100(item.start),
+        end: ensureMax100(item.end),
+      };
+    });
+  }, [progressItems]);
+
+  return (
+    <Wrapper>
+      <Background />
+      {items.map((item, idx) => (
+        <TooltipWrapper
+          key={idx}
+          start={Number(item.start) || 0}
+          end={Math.abs(Number(item.end) || 100)}
+          percentage={item.percentage > 100 ? 100 : item.percentage}
+          fg={item.fg}
+          bg={item.bg}
+        >
+          <Tooltip content={item.tooltipContent}>
+            <PercentageWrapper>
+              <Percentage />
+            </PercentageWrapper>
+          </Tooltip>
+        </TooltipWrapper>
+      ))}
+    </Wrapper>
+  );
+}
--- packages/next/components/gov2/sidebar/status/ConfirmProgress.js
+++ packages/next/components/gov2/sidebar/status/ConfirmProgress.js
@@ -15,7 +15,7 @@ import {
   useConfirmRemaining,
 } from "./useConfirmPercentage";
 import Remaining from "./remaining";
-import Progress from "next-common/components/progress";
+import MultiProgress from "next-common/components/progress/multiProgress";
 import { useMemo } from "react";
 import {
   useConfirmingStarted,
@@ -141,7 +141,7 @@ function ConfirmationStarted() {
 
   return (
     <ProgressGroup>
-      <Progress progressItems={progressItems} />
+      <MultiProgress progressItems={progressItems} />
       <ConfirmationInfo />
     </ProgressGroup>
   );
