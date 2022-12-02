7e5390d3063fd57d4b190bb48f84973f23d1a3e2
Yongfeng LI <wliyongfeng@gmail.com>
2022-12-02 10:00:07 +0800

    Fix lint error

--- packages/next-common/context/page/index.js
+++ packages/next-common/context/page/index.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import React, { createContext, useContext } from "react";
 import { detailPageCategory } from "../../utils/consts/business/category";
 
 const PageContext = createContext({});
