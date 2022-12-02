41fa038551ed8b8732e9a30e5149326e71a36224
2nthony <ant.hu@outlook.com>
2022-12-02 14:43:37 +0800

    fix: summary usage

--- packages/next-common/components/summary/overview/index.js
+++ packages/next-common/components/summary/overview/index.js
@@ -1,26 +1,34 @@
 import React from "react";
+import Content from "../cardContent";
 import Summary from "../new";
+import { SummaryItem, SummaryItemTitle, SummaryItemWrapper } from "../styled";
 
 export default function OverviewSummary() {
   // TODO: fetch data
 
   return (
-    <Summary
-      description="Due client synergize developing tentative strategic vec pushback."
-      items={[
-        {
-          title: "Treasury",
-        },
-        {
-          title: "Council / T.C.",
-        },
-        {
-          title: "Democracy",
-        },
-        {
-          title: "Open Gov",
-        },
-      ]}
-    />
+    <Summary description="Due client synergize developing tentative strategic vec pushback.">
+      <SummaryItemWrapper>
+        <SummaryItem>
+          <SummaryItemTitle>Treasury</SummaryItemTitle>
+          <Content></Content>
+        </SummaryItem>
+
+        <SummaryItem>
+          <SummaryItemTitle>Council / T.C.</SummaryItemTitle>
+          <Content></Content>
+        </SummaryItem>
+
+        <SummaryItem>
+          <SummaryItemTitle>Democracy</SummaryItemTitle>
+          <Content></Content>
+        </SummaryItem>
+
+        <SummaryItem>
+          <SummaryItemTitle>Open Gov</SummaryItemTitle>
+          <Content></Content>
+        </SummaryItem>
+      </SummaryItemWrapper>
+    </Summary>
   );
 }
