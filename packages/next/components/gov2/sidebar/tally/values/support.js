e2c8ad9f08668c10d5c66f2a40430260a526afe9
Yongfeng LI <wliyongfeng@gmail.com>
2022-12-08 11:17:08 +0800

    Add fellowship referendum sidebar, #2421 (#2428)
    * Remove TimelineTallyInfo component, #2421
    
    * Init fellowship referendum right bar, #2421
    
    * Add deciding status and tally to fellowship referendum, #2421

--- packages/next/components/gov2/sidebar/tally/values/support.js
+++ packages/next/components/gov2/sidebar/tally/values/support.js
@@ -1,7 +1,10 @@
 import ElectorateIcon from "../../../../../public/imgs/icons/electorate.svg";
-import { BorderedRow, Header } from "./styled";
 import { useTally } from "next-common/context/post/gov2/referendum";
 import SymbolValue from "./symbolValue";
+import {
+  BorderedRow,
+  Header,
+} from "next-common/components/referenda/tally/styled";
 
 export default function Support() {
   const tally = useTally();
