b66a7e5413d057ac9ba902c46821f693a611263a
chaojun <chaojun@opensquare.network>
2022-12-13 10:43:46 +0800

    Update UI to show correct nav data

--- packages/next-common/components/detail/navigation/democracyProposal.js
+++ packages/next-common/components/detail/navigation/democracyProposal.js
@@ -3,24 +3,39 @@ import {
   DemocracyProposalNavigator,
   NavigationWrapper,
   ReferendumNavigationItem,
-  TechCommMotionNavigator
+  TechCommMotionNavigator,
 } from "./navigators";
 import isNil from "lodash.isnil";
 
-export default function DemocracyProposalNavigation({ proposalIndex, referendumIndex }) {
-  return <NavigationWrapper>
-    <DemocracyProposalNavigator proposalIndex={ proposalIndex } isLink={ false } />
-    <ReferendumNavigationItem referendumIndex={ referendumIndex } />
-  </NavigationWrapper>
+export default function DemocracyProposalNavigation({
+  proposalIndex,
+  referendumIndex,
+}) {
+  return (
+    <NavigationWrapper>
+      <DemocracyProposalNavigator
+        proposalIndex={proposalIndex}
+        isLink={false}
+      />
+      <ReferendumNavigationItem referendumIndex={referendumIndex} />
+    </NavigationWrapper>
+  );
 }
 
 export function KintsugiDemocracyProposalNavigation({ post }) {
-  return <NavigationWrapper>
-    <div>{ `Proposal #${ post.proposalIndex }` }</div>
-    { post?.onchainData?.techCommMotions?.map(
-      (techCommMotion, key) => <TechCommMotionNavigator motion={ techCommMotion } key={ key } />
-    ) }
-    { !isNil(post?.referendumIndex) && <ReferendumNavigationItem referendumIndex={ post.referendumIndex } /> }
-  </NavigationWrapper>
+  const onchainData = post?.onchainData;
 
+  return (
+    <NavigationWrapper>
+      <div>{`Proposal #${onchainData?.proposalIndex}`}</div>
+      {onchainData?.techCommMotions?.map((techCommMotion, key) => (
+        <TechCommMotionNavigator motion={techCommMotion} key={key} />
+      ))}
+      {!isNil(onchainData?.referendumIndex) && (
+        <ReferendumNavigationItem
+          referendumIndex={onchainData.referendumIndex}
+        />
+      )}
+    </NavigationWrapper>
+  );
 }
