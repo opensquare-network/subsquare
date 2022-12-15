86aff343546e3f9a1ae4c5a92d497edc8d3c1867
Yongfeng LI <wliyongfeng@gmail.com>
2022-12-15 12:18:12 +0800

    Fix gov2 referendum tally items border

--- packages/next/components/gov2/sidebar/tally/values/support.js
+++ packages/next/components/gov2/sidebar/tally/values/support.js
@@ -1,18 +1,21 @@
 import ElectorateIcon from "../../../../../public/imgs/icons/electorate.svg";
 import { useTally } from "next-common/context/post/gov2/referendum";
 import SymbolValue from "./symbolValue";
-import { Row, Header } from "next-common/components/referenda/tally/styled";
+import {
+  BorderedRow,
+  Header,
+} from "next-common/components/referenda/tally/styled";
 
 export default function Support() {
   const tally = useTally();
 
   return (
-    <Row>
+    <BorderedRow>
       <Header>
         <ElectorateIcon />
         Support
       </Header>
       <SymbolValue value={tally?.support} />
-    </Row>
+    </BorderedRow>
   );
 }
