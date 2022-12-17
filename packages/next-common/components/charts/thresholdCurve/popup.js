0f91a808157bf665be342a41afeadf6b052d33be
2nthony <ant.hu@outlook.com>
2022-12-17 17:42:56 +0800

    feat: curve chart for referenda track (#2475)
    * chore(deps): chartjs
    
    * feat: referenda curve stage 1 (#2434)
    
    * chore(deps): chartjs
    
    * feat: threshold curve legend
    
    * fix: legend color
    
    * chart global config
    
    * temp stash curve chart
    
    * hs
    
    * refactor: array range
    
    * fix: ;
    
    * themed arrow out simple icon
    
    * no legend by default, is standalone component
    
    * feat: popup support styled extend
    
    * scales display props
    
    * base popup curves
    
    * props data
    
    * fix: track summary
    
    * fix: import react
    
    * fix: threshold curves chart no clip
    
    * fix: hs item length
    
    * chore: add fixme
    
    * refactor: use lodash.range
    
    * feat: add decision deposit
    
    * fix: mobile css
    
    * fix: decision deposit title no wrap
    
    * fix: typo
    
    * fix: popup mobile css
    
    * fix: threshold curves popup css
    
    * fix: track summary curve
    
    * fix: curve line width
    
    * fix import
    
    * Fix: curve data (#2472)
    
    * fix: threshold curves line border color
    
    * feat: threshold support layout padding
    
    * fix: track summary chart gap
    
    * Add fixme for referenda curve chart
    
    Co-authored-by: chaojun <chaojun@opensquare.network>
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>
    
    * Adjust track properties order
    
    * Make sure curve value minimum is 0
    
    Co-authored-by: chaojun <chaojun@opensquare.network>
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

---
+++ packages/next-common/components/charts/thresholdCurve/popup.js
@@ -0,0 +1,139 @@
+import React from "react";
+import styled, { css, useTheme } from "styled-components";
+import ThresholdCurvesChart from ".";
+import { emptyFunction } from "../../../utils";
+import PopupOrigin from "../../popup/wrapper/Popup";
+import Flex from "../../styled/flex";
+import ThresholdCurvesLegend from "./legend";
+import {
+  EmptyTd,
+  RowSpliter,
+  StyledTable,
+  StyledTd,
+  StyledTh,
+  StyledTr,
+} from "../../styled/table";
+import { pretty_scroll_bar } from "../../../styles/componentCss";
+import Loading from "../../loading";
+import _range from "lodash.range";
+import { smcss } from "../../../utils/responsive";
+
+const Center = styled(Flex)`
+  justify-content: center;
+`;
+
+const Popup = styled(PopupOrigin)`
+  width: 480px;
+
+  ${smcss(css`
+    width: 100%;
+  `)}
+`;
+
+const Table = styled(StyledTable)`
+  border: none;
+  padding: 0;
+  tbody {
+    display: block;
+    max-height: 180px;
+    overflow-y: auto;
+    overflow-x: hidden;
+
+    ${pretty_scroll_bar};
+  }
+  thead,
+  tbody tr {
+    display: table;
+    width: 100%;
+    table-layout: fixed;
+  }
+
+  tbody {
+    ${StyledTr} {
+      height: 44px;
+      &:not(:last-child) {
+        border-bottom: 1px solid ${(p) => p.theme.grey100Bg};
+      }
+    }
+  }
+
+  box-shadow: none;
+`;
+
+export default function ThresholdCurvesPopup({
+  setShow = emptyFunction,
+  loading = true,
+  labels = [],
+  supportData = [],
+  approvalData = [],
+}) {
+  const theme = useTheme();
+
+  const list = _range(labels.length).map((n) => {
+    const support = Number(supportData[n]).toFixed(2);
+    const approval = Number(approvalData[n]).toFixed(2);
+
+    return {
+      hs: n,
+      support,
+      approval,
+    };
+  });
+
+  return (
+    <Popup
+      title="Thoreshold Curves"
+      onClose={() => {
+        setShow(false);
+      }}
+    >
+      <ThresholdCurvesChart
+        height={144}
+        labels={labels}
+        supportData={supportData}
+        approvalData={approvalData}
+      />
+
+      <Center>
+        <ThresholdCurvesLegend />
+      </Center>
+
+      <Table>
+        <thead>
+          <StyledTr>
+            <StyledTh style={{ textAlign: "left" }}>TIME (HS)</StyledTh>
+            <StyledTh style={{ textAlign: "right" }}>SUPPORT</StyledTh>
+            <StyledTh style={{ textAlign: "right" }}>APPROVAL</StyledTh>
+          </StyledTr>
+          <RowSpliter
+            backgroundColor={
+              theme.isDark ? theme.grey200Border : theme.grey100Bg
+            }
+            padding={"16px 0 0 0"}
+          />
+        </thead>
+        <tbody>
+          {list.length ? (
+            list.map((item, idx) => (
+              <StyledTr key={idx}>
+                <StyledTd style={{ textAlign: "left" }}>{item.hs}</StyledTd>
+                <StyledTd style={{ textAlign: "right" }}>
+                  {item.support}%
+                </StyledTd>
+                <StyledTd style={{ textAlign: "right" }}>
+                  {item.approval}%
+                </StyledTd>
+              </StyledTr>
+            ))
+          ) : (
+            <StyledTr>
+              <EmptyTd colSpan="3">
+                {loading ? <Loading size={16} /> : "No threshold curves"}
+              </EmptyTd>
+            </StyledTr>
+          )}
+        </tbody>
+      </Table>
+    </Popup>
+  );
+}
