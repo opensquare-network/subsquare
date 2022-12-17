6c2f4dbfec3e934ccba97566676c6c80fb7cd98d
Yongfeng LI <wliyongfeng@gmail.com>
2022-12-17 19:07:09 +0800

    Add vote threshold bar for fellowship referendum, #2479

--- packages/next/components/fellowship/referendum/sidebar/tally/index.js
+++ packages/next/components/fellowship/referendum/sidebar/tally/index.js
@@ -8,6 +8,8 @@ import MaxVoters from "./maxVoters";
 import SupportBar from "../../../../gov2/sidebar/tally/supportBar";
 import { useTally } from "next-common/context/post/gov2/referendum";
 import useMaxVoters from "next-common/context/post/fellowship/useMaxVoters";
+import useApprovalThreshold from "../../../../gov2/sidebar/tally/threshold/useApprovalThreshold";
+import VoteBar from "../../../../referenda/voteBar";
 
 const Title = styled(TitleContainer)`
   margin-bottom: 16px;
@@ -16,11 +18,17 @@ const Title = styled(TitleContainer)`
 export default function FellowshipTally() {
   const tally = useTally();
   const maxVoters = useMaxVoters();
+  const approvalThreshold = useApprovalThreshold();
 
   return (
     <SecondaryCardDetail>
       <Title>Tally</Title>
 
+      <VoteBar
+        tally={tally}
+        threshold="percentage"
+        percentage={approvalThreshold}
+      />
       <Aye />
       <Nay />
 
