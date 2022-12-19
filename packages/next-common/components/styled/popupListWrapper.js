c5de38aff25f624a8c7827ba6a40c6be36aabf13
chaojun <chaojun@opensquare.network>
2022-12-19 18:28:19 +0800

    Fix lint

--- packages/next-common/components/styled/popupListWrapper.js
+++ packages/next-common/components/styled/popupListWrapper.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import { pretty_scroll_bar } from "../../styles/componentCss";
 import { pageHomeLayoutMainContentWidth } from "../../utils/constants";
