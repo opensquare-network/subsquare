5f407148fcdf204ac713f3c442064c9227cbfa72
2nthony <ant.hu@outlook.com>
2022-12-27 14:22:00 +0800

    refactor: learn gov2 link component

--- packages/next/components/gov2/sidebar/index.js
+++ packages/next/components/gov2/sidebar/index.js
@@ -1,15 +1,13 @@
-import ExternalLinkIcon from "next-common/components/icons/externalLink";
 import { emptyFunction } from "next-common/utils";
 import { gov2VotingState } from "next-common/utils/consts/state";
 import dynamic from "next/dynamic";
 import { useState } from "react";
-import { useTheme } from "styled-components";
 import Gov2Status from "./status";
 import Gov2Tally from "./tally";
 import { RightBarWrapper } from "next-common/components/layout/sidebar/rightBarWrapper";
 import { usePost } from "next-common/context/post";
-import { Link } from "next-common/components/detail/sidebar/styled";
 import SecondaryButton from "next-common/components/buttons/secondaryButton";
+import LearnGov2Link from "next-common/components/links/learnGov2Link";
 
 const Popup = dynamic(() => import("../votePopup"), {
   ssr: false,
@@ -17,7 +15,6 @@ const Popup = dynamic(() => import("../votePopup"), {
 
 export default function Gov2Sidebar({ onVoteFinalized = emptyFunction }) {
   const detail = usePost();
-  const { primaryPurple500 } = useTheme();
   const [showVote, setShowVote] = useState(false);
   const referendumIndex = detail?.referendumIndex;
   const trackId = detail?.track;
@@ -47,10 +44,7 @@ export default function Gov2Sidebar({ onVoteFinalized = emptyFunction }) {
         />
       )}
 
-      <Link href="https://wiki.polkadot.network/docs/learn-gov2/">
-        How Governance V2 Works
-        <ExternalLinkIcon color={primaryPurple500} />
-      </Link>
+      <LearnGov2Link />
     </RightBarWrapper>
   );
 }
