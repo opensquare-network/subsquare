ea01678e250ffb5df7f20f9ae5e4fa25b874eda8
hyifeng <chaojun@opensquare.network>
2023-01-03 16:01:05 +0800

    Gov2 created treasury proposal timeline (#2523)
    * Add treasury timeline link to gov2 referenda
    
    * Update treasury proposal timeline
    
    * Update

--- packages/next/components/treasuryProposal/timeline.js
+++ packages/next/components/treasuryProposal/timeline.js
@@ -1,6 +1,7 @@
-import User from "next-common/components/user";
 import { getTimelineStatus } from "utils";
 import dayjs from "dayjs";
+import flatten from "lodash.flatten";
+import User from "next-common/components/user";
 import Timeline from "next-common/components/timeline";
 import { createMotionTimelineData } from "utils/timeline/motion";
 import sortTimeline from "next-common/utils/timeline/sort";
@@ -8,34 +9,64 @@ import { detailPageCategory } from "next-common/utils/consts/business/category";
 import { createReferendumTimelineData } from "utils/timeline/referendum";
 import SymbolBalance from "next-common/components/values/symbolBalance";
 
-export default function TreasuryProposalTimeline({ treasuryProposal }) {
-  const getTimelineData = (args, method) => {
-    switch (method) {
-      case "Proposed":
-        return {
-          Index: `#${args.index}`,
-        };
-      case "Awarded":
-        return {
-          Beneficiary: <User add={args.beneficiary} />,
-          Award: <SymbolBalance value={args.award} />,
-        };
-    }
-    return args;
-  };
+function getTimelineData(args, method) {
+  switch (method) {
+    case "Proposed":
+      return {
+        Index: `#${args.index}`,
+      };
+    case "Awarded":
+      return {
+        Beneficiary: <User add={args.beneficiary} />,
+        Award: <SymbolBalance value={args.award} />,
+      };
+  }
+  return args;
+}
 
-  const timelineData = (treasuryProposal?.timeline || []).map((item) => {
-    const indexer = item.extrinsicIndexer ?? item.indexer;
-    return {
+function getGov2ReferendumTimeline(timelineItem, treasuryProposal) {
+  const indexer = timelineItem.extrinsicIndexer ?? timelineItem.indexer;
+
+  return [
+    {
+      indexer,
+      time: dayjs(indexer.blockTime).format("YYYY-MM-DD HH:mm:ss"),
+      status: {
+        value: `Referenda #${treasuryProposal.gov2Referendum}`,
+        link: `/referenda/referendum/${treasuryProposal.gov2Referendum}`,
+      },
+    },
+    {
       indexer,
       time: dayjs(indexer?.blockTime).format("YYYY-MM-DD HH:mm:ss"),
       status: getTimelineStatus(
         detailPageCategory.TREASURY_PROPOSAL,
-        item.method ?? item.name
+        "Approved"
       ),
-      data: getTimelineData(item.args, item.method ?? item.name),
-    };
-  });
+    },
+  ];
+}
+
+export default function TreasuryProposalTimeline({ treasuryProposal }) {
+  const timelineData = flatten(
+    (treasuryProposal?.timeline || []).map((item) => {
+      const indexer = item.extrinsicIndexer ?? item.indexer;
+
+      const method = item.method ?? item.name;
+
+      // Handle Gov2 created treasury proposal
+      if (method === "SpendApproved" && treasuryProposal.isByGov2) {
+        return getGov2ReferendumTimeline(item, treasuryProposal);
+      }
+
+      return {
+        indexer,
+        time: dayjs(indexer?.blockTime).format("YYYY-MM-DD HH:mm:ss"),
+        status: getTimelineStatus(detailPageCategory.TREASURY_PROPOSAL, method),
+        data: getTimelineData(item.args, item.method ?? item.name),
+      };
+    })
+  );
 
   const motions = treasuryProposal?.motions?.map((motion) => {
     return createMotionTimelineData(motion, true, "/council/motion");
