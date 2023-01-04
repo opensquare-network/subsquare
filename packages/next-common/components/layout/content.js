c7c448ff699c46c54ee8a0712427c04bd5d82561
2nthony <ant.hu@outlook.com>
2023-01-04 15:25:01 +0800

    fix: layout content

--- packages/next-common/components/layout/content.js
+++ packages/next-common/components/layout/content.js
@@ -24,6 +24,7 @@ const Wrapper = styled.div`
 
 const ContentWrapper = styled.div`
   display: flex;
+  flex-grow: 1;
   position: relative;
   @media screen and (max-width: 768px) {
     flex-direction: column-reverse;
