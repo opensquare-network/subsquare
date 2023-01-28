9597e700139d6ff5b524e67abfbd8b99b91e134f
chaojun <chaojun@opensquare.network>
2023-01-28 16:29:58 +0800

    Show loading page for un-finalized treasury proposal

---
+++ packages/next-common/components/checkUnFinalizedBase.js
@@ -0,0 +1,93 @@
+import React from "react";
+import noop from "lodash.noop";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useState } from "react";
+import Loading from "../assets/imgs/icons/block-loading.svg";
+import useApi from "../utils/hooks/useApi";
+import { Wrapper, H2, P } from "./styled/notFound";
+import NotFound from "./notFound";
+
+export default function CheckUnFinalizedBase({
+  onChainDataFetcher = noop,
+  serverPostFetcher = noop,
+}) {
+  const api = useApi();
+  const router = useRouter();
+  const [unFinalized, setIsUnFinalized] = useState(false);
+  const [notFound, setNotFound] = useState(false);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    if (onChainDataFetcher === noop) {
+      return;
+    }
+
+    // Check if the proposal is present on-chain
+    onChainDataFetcher(api).then((onchainData) => {
+      const data = onchainData.toJSON();
+      if (!data) {
+        // Proposal is not exist, show 404
+        setNotFound(true);
+        return;
+      }
+
+      // Proposal exists on-chain, show un-finalized
+      setIsUnFinalized(true);
+    });
+  }, [api, onChainDataFetcher, router]);
+
+  const checkServerPostAvailable = useCallback(async () => {
+    if (serverPostFetcher === noop) {
+      return false;
+    }
+
+    const { result } = await serverPostFetcher();
+    if (result) {
+      // Server post is ready, refresh current page
+      router.replace(router.asPath);
+      return true;
+    }
+
+    return false;
+  }, [serverPostFetcher, router]);
+
+  useEffect(() => {
+    if (!unFinalized) {
+      return;
+    }
+
+    // Check if server post available
+    const intervalId = setInterval(async () => {
+      const available = await checkServerPostAvailable();
+      if (available) {
+        clearInterval(intervalId);
+      }
+    }, 6000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [unFinalized, checkServerPostAvailable]);
+
+  if (notFound) {
+    return <NotFound />;
+  }
+
+  return (
+    <Wrapper>
+      <Loading />
+      {unFinalized && (
+        <>
+          <H2>Waiting for block confirmation</H2>
+          <P>
+            We found the proposal on chain but not finalized. This page will be
+            refreshed when blocks get finalized.
+          </P>
+        </>
+      )}
+    </Wrapper>
+  );
+}
--- packages/next/pages/treasury/proposal/[id].js
+++ packages/next/pages/treasury/proposal/[id].js
@@ -4,7 +4,6 @@ import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import { EmptyList } from "next-common/utils/constants";
 import Timeline from "components/treasuryProposal/timeline";
 import Metadata from "next-common/components/treasury/proposal/metadata";
-import { to404 } from "next-common/utils/serverSideUtil";
 import getMetaDesc from "next-common/utils/post/getMetaDesc";
 import useUniversalComments from "components/universalComments";
 import DetailLayout from "next-common/components/layout/DetailLayout";
@@ -12,14 +11,37 @@ import { getBannerUrl } from "next-common/utils/banner";
 import { PostProvider } from "next-common/context/post";
 import BreadcrumbWrapper from "next-common/components/detail/common/BreadcrumbWrapper";
 import Breadcrumb from "next-common/components/_Breadcrumb";
+import CheckUnFinalized from "components/treasuryProposal/checkUnFinalized";
 
-export default withLoginUserRedux(({ detail, comments }) => {
+function TreasuryProposalContent({ detail, comments }) {
   const { CommentComponent, focusEditor } = useUniversalComments({
     detail,
     comments,
   });
 
-  const desc = getMetaDesc(detail);
+  return (
+    <>
+      <DetailItem onReply={focusEditor} />
+      <Metadata treasuryProposal={detail?.onchainData} />
+      <Timeline treasuryProposal={detail?.onchainData} />
+      {CommentComponent}
+    </>
+  );
+}
+
+export default withLoginUserRedux(({ id, detail, comments }) => {
+  let breadcrumbItemName = "";
+  let postContent = null;
+
+  if (detail) {
+    breadcrumbItemName = `#${detail?.proposalIndex}`;
+    postContent = (
+      <TreasuryProposalContent detail={detail} comments={comments} />
+    );
+  } else {
+    breadcrumbItemName = `Proposal #${id}`;
+    postContent = <CheckUnFinalized id={id} />;
+  }
 
   const breadcrumbItems = [
     {
@@ -30,9 +52,11 @@ export default withLoginUserRedux(({ detail, comments }) => {
       path: "/treasury/proposals",
     },
     {
-      content: `#${detail?.proposalIndex}`,
+      content: breadcrumbItemName,
     },
   ];
+
+  const desc = getMetaDesc(detail);
 
   return (
     <PostProvider post={detail}>
@@ -47,10 +71,7 @@ export default withLoginUserRedux(({ detail, comments }) => {
           <Breadcrumb items={breadcrumbItems} />
         </BreadcrumbWrapper>
 
-        <DetailItem onReply={focusEditor} />
-        <Metadata treasuryProposal={detail?.onchainData} />
-        <Timeline treasuryProposal={detail?.onchainData} />
-        {CommentComponent}
+        {postContent}
       </DetailLayout>
     </PostProvider>
   );
@@ -60,12 +81,16 @@ export const getServerSideProps = withLoginUser(async (context) => {
   const { id, page, page_size } = context.query;
   const pageSize = Math.min(page_size ?? 50, 100);
 
-  const [{ result: detail }] = await Promise.all([
-    nextApi.fetch(`treasury/proposals/${id}`),
-  ]);
+  const { result: detail } = await nextApi.fetch(`treasury/proposals/${id}`);
 
   if (!detail) {
-    return to404(context);
+    return {
+      props: {
+        id,
+        detail: null,
+        comments: EmptyList,
+      },
+    };
   }
 
   const { result: comments } = await nextApi.fetch(
@@ -78,6 +103,7 @@ export const getServerSideProps = withLoginUser(async (context) => {
 
   return {
     props: {
+      id,
       detail,
       comments: comments ?? EmptyList,
     },
