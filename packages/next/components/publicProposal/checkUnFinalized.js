da248f07a0bc0dc6b6e57c94f174fbf01fd3486a
chaojun <chaojun@opensquare.network>
2023-01-29 13:58:12 +0800

    Add support for un-finalized public proposal

---
+++ packages/next/components/publicProposal/checkUnFinalized.js
@@ -0,0 +1,22 @@
+import React, { useCallback } from "react";
+import nextApi from "next-common/services/nextApi";
+import CheckUnFinalizedBase from "next-common/components/checkUnFinalizedBase";
+
+export default function CheckUnFinalized({ id }) {
+  const getPublicProposal = useCallback(
+    async (api) => {
+      const proposals = await api.query.democracy.publicProps();
+      return (proposals || []).find(
+        ([index]) => index.toNumber() === parseInt(id)
+      );
+    },
+    [id]
+  );
+
+  return (
+    <CheckUnFinalizedBase
+      onChainDataFetcher={getPublicProposal}
+      serverPostFetcher={() => nextApi.fetch(`democracy/proposals/${id}`)}
+    />
+  );
+}
