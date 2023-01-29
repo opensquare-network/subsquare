4f045d5b58bec94ab543017bb67cbf1b80f15b11
chaojun <chaojun@opensquare.network>
2023-01-29 18:10:46 +0800

    Add support for un-finalized tech-comm motion

--- packages/next/components/motion/checkUnFinalized.js
+++ packages/next/components/motion/checkUnFinalized.js
@@ -1,28 +1,36 @@
 import React, { useCallback } from "react";
 import nextApi from "next-common/services/nextApi";
 import CheckUnFinalizedBase from "next-common/components/checkUnFinalizedBase";
+import { useDetailType } from "next-common/context/page";
+import { useChain } from "next-common/context/chain";
+import toApiCouncil from "next-common/utils/toApiCouncil";
 
 export default function CheckUnFinalized({ id }) {
+  const type = useDetailType();
+  const chain = useChain();
+
   const findMotion = useCallback(
     async (api) => {
+      const councilApi = api.query[toApiCouncil(chain, type)];
+
       if (id?.match(/^[0-9]+$/)) {
         // If id is a motion index
-        const allMotions = await api.query.council.proposals();
-        const allVoting = await api.query.council.voting.multi(allMotions);
+        const allMotions = await councilApi.proposals();
+        const allVoting = await councilApi.voting.multi(allMotions);
 
         for (let i = 0; i < allVoting.length; i++) {
           const data = allVoting[i].toJSON();
           const { index: motionIndex } = data || {};
           if (motionIndex === parseInt(id)) {
-            return await api.query.council.proposalOf(allMotions[i]);
+            return await councilApi.proposalOf(allMotions[i]);
           }
         }
       } else {
         // If id is a motion hash
-        return await api.query.council.proposalOf(id);
+        return await councilApi.proposalOf(id);
       }
     },
-    [id]
+    [id, chain, type]
   );
 
   return (
