48fcc73e3a9176d0f36a4b0ff164b8b259af99f1
2nthony <ant.hu@outlook.com>
2023-01-30 14:33:58 +0800

    refactor: use latest block time hook

--- packages/next-common/utils/hooks/useBlockTime.js
+++ packages/next-common/utils/hooks/useBlockTime.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { latestHeightSelector } from "../../store/reducers/chainSlice";
 import { getBlockTimeByHeight } from "../blockTime";
 import useApi from "./useApi";
+import useIsMounted from "./useIsMounted";
 
-export default function useLatestBlockTime(blockHeight, isMounted) {
+export default function useLatestBlockTime() {
   const api = useApi();
+  const isMounted = useIsMounted();
+  const blockHeight = useSelector(latestHeightSelector);
   const [time, setTime] = useState(0);
 
   useEffect(() => {
