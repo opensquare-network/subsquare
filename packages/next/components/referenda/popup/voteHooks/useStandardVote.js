9206cc6ea5f6c0f7f680a90d844c85eac37b53a1
chaojun <chaojun@opensquare.network>
2023-02-01 09:32:07 +0800

    Update

--- packages/next/components/referenda/popup/voteHooks/useStandardVote.js
+++ packages/next/components/referenda/popup/voteHooks/useStandardVote.js
@@ -32,7 +32,6 @@ export default function useStandardVote({
       setInputVoteBalance={setInputVoteBalance}
       voteLock={voteLock}
       setVoteLock={setVoteLock}
-      node={node}
     />
   );
 
