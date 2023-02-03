d099e15a22633001ffc73336c10ae366eafb73ac
2nthony <ant.hu@outlook.com>
2023-02-03 14:01:33 +0800

    been delegated summary

--- packages/next-common/components/summary/democracyAllBeenDelegatedPopup/trackList.js
+++ packages/next-common/components/summary/democracyAllBeenDelegatedPopup/trackList.js
@@ -1,13 +1,20 @@
+import { useTrackDelegations } from "../../../utils/hooks/referenda/useDelegation";
 import DelegationList from "../democracyBeenDelegated/beenDelegatedListPopup/delegationList";
+import DelegationSummary from "../democracyBeenDelegated/beenDelegatedListPopup/delegationSummary";
 
 export default function AllBeenDelegatedPopupTrackList({
   loading = false,
-  items,
+  trackBeenDelegatedList,
+  track,
 }) {
+  const delegations = useTrackDelegations(track);
   return (
     <>
-      {/* TODO: add summary */}
-      <DelegationList loading={loading} items={items} />
+      <DelegationSummary
+        delegations={delegations}
+        beenDelegatedList={trackBeenDelegatedList || []}
+      />
+      <DelegationList loading={loading} items={trackBeenDelegatedList} />
     </>
   );
 }
