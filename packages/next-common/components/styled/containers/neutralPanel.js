15c2ea21a8d8594fc6c9ec46320562c383ac3f3e
2nthony <ant.hu@outlook.com>
2023-02-03 11:50:42 +0800

    fix: neutral panel rounded to 8px

--- packages/next-common/components/styled/containers/neutralPanel.js
+++ packages/next-common/components/styled/containers/neutralPanel.js
@@ -4,5 +4,5 @@ export const NeutralPanel = styled.div`
   background: ${(p) => p.theme.neutral};
   border: 1px solid ${(p) => p.theme.grey200Border};
   box-shadow: ${(p) => p.theme.shadow200};
-  border-radius: 6px;
+  border-radius: 8px;
 `;
