be47379660acd5fc3f33ee4d4085799db923a904
chaojun <chaojun@opensquare.network>
2023-02-06 16:54:37 +0800

    Fix: list tracks in dropdown only when track has delegation data

--- packages/next-common/components/summary/democracyAllBeenDelegatedPopup/tab.js
+++ packages/next-common/components/summary/democracyAllBeenDelegatedPopup/tab.js
@@ -1,4 +1,6 @@
+import uniq from "lodash.uniq";
 import React, { useMemo, useState } from "react";
+import { usePageProps } from "../../../context/page";
 import Track from "../../popup/fields/trackField";
 import VStack from "../../styled/vStack";
 import AllBeenDelegatedPopupAllList from "./allList";
@@ -6,6 +8,7 @@ import AllBeenDelegatedPopupTrackList from "./trackList";
 
 export default function AllBeenDelegatedPopupTabList({ beenDelegatedList }) {
   const [track, setTrack] = useState("all");
+  const { tracks } = usePageProps();
 
   const trackBeenDelegatedList = useMemo(() => {
     const beenDelegated = beenDelegatedList?.find((item) => {
@@ -14,9 +17,22 @@ export default function AllBeenDelegatedPopupTabList({ beenDelegatedList }) {
     return beenDelegated?.beenDelegated;
   }, [track, beenDelegatedList]);
 
+  const trackList = useMemo(() => {
+    const uniqTrackIds = uniq(
+      (beenDelegatedList || []).map((item) => item.track?.id)
+    );
+    return (tracks || []).filter((track) => uniqTrackIds.includes(track.id));
+  }, [tracks, beenDelegatedList]);
+
   return (
     <VStack space={16}>
-      <Track title="" hasAll track={track} setTrack={setTrack} />
+      <Track
+        title=""
+        hasAll
+        track={track}
+        setTrack={setTrack}
+        trackList={trackList}
+      />
 
       {track === "all" ? (
         <AllBeenDelegatedPopupAllList beenDelegatedList={beenDelegatedList} />
