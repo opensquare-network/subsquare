ae4a6fa13bcd3ccba9e8c9d083ee3c93c3d4bb64
2nthony <ant.hu@outlook.com>
2023-02-08 10:59:37 +0800

    build: fix babel

--- packages/kintsugi-next/babel.config.js
+++ packages/kintsugi-next/babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: true } }],
-    "@babel/preset-react",
-  ],
+  presets: ["next/babel"],
+  plugins: [["styled-components", { ssr: true }]],
 };
