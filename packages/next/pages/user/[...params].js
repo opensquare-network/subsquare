c30ff8c94c5f4a530193dd9d5e0a0b09b6129fc1
chaojun <chaojun@opensquare.network>
2023-02-17 17:38:14 +0800

    Add gov2 user activities to profile page

--- packages/next/pages/user/[...params].js
+++ packages/next/pages/user/[...params].js
@@ -1,6 +1,6 @@
+import React from "react";
 import { withLoginUser, withLoginUserRedux } from "next-common/lib";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
-import React from "react";
 import Profile from "next-common/components/profile";
 
 export default withLoginUserRedux(Profile);
@@ -16,10 +16,6 @@ export const getServerSideProps = withLoginUser(async (context) => {
     nextApi.fetch(`users/${id}`),
   ]);
 
-  // let username = id;
-  // if (user?.username && !isKeyRegisteredUser(user)) {
-  //   username = user?.username;
-  // }
   return {
     props: {
       id,
