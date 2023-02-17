ee8ea919b48ed23f775502ea74dc6a9ddbb46edd
Yongfeng LI <wliyongfeng@gmail.com>
2023-02-17 23:43:32 +0800

    Fix lint errors, #2540

--- packages/next-common/utils/consts/menu/alliance.js
+++ packages/next-common/utils/consts/menu/alliance.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { getExcludeChains } from "../../viewfuncs";
 import Chains from "../chains";
 import MenuIconWrapper from "../../../components/icons/menuIconWrapper";
