08230d0c6738db226918d138e7fa0945951546e9
chaojun <chaojun@opensquare.network>
2023-02-28 09:47:16 +0800

    Clean code

--- packages/next/pages/setting/subscription.js
+++ packages/next/pages/setting/subscription.js
@@ -64,13 +64,6 @@ const ButtonWrapper = styled.div`
   }
 `;
 
-function checkSubMenu(menus, menuName) {
-  const chain = process.env.NEXT_PUBLIC_CHAIN;
-  const menu = menus?.find((item) => item.name === menuName);
-  const hasMenu = menu && !menu.excludeToChains?.includes(chain);
-  return { hasMenu, menu };
-}
-
 export default withLoginUserRedux(
   ({ loginUser, subscription: _subscription, unsubscribe }) => {
     const dispatch = useDispatch();
