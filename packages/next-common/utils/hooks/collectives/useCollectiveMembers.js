65e983338a327e23598ccbcde84aa9fbdaf885c0
Yongfeng LI <wliyongfeng@gmail.com>
2023-03-01 09:48:16 +0800

    Extract more hooks for collective pallet (#2680)

---
+++ packages/next-common/utils/hooks/collectives/useCollectiveMembers.js
@@ -0,0 +1,42 @@
+import useApi from "../useApi";
+import { useEffect, useRef, useState } from "react";
+import useIsMounted from "../useIsMounted";
+
+export default function useCollectiveMembers(moduleName = "council") {
+  const api = useApi();
+  const cache = useRef({});
+  const isMounted = useIsMounted();
+  const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!api || !api.query?.[moduleName]?.members) {
+      return
+    }
+
+    if (cache.current[moduleName]) {
+      if (isMounted.current) {
+        setMembers(cache.current[moduleName]);
+      }
+      return;
+    }
+
+    setLoading(true);
+    api.query[moduleName].members().then((members) => {
+      if (isMounted.current) {
+        const normalized = members.toJSON();
+        setMembers(normalized);
+        cache.current[moduleName] = normalized;
+      }
+    }).finally(() => {
+      if (isMounted.current) {
+        setLoading(false);
+      }
+    })
+  }, [api]);
+
+  return {
+    members,
+    loading,
+  };
+}
