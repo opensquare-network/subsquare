8e3e0da79b867808a57c111cf82ea55e0552e7f6
2nthony <ant.hu@outlook.com>
2023-03-02 17:20:16 +0800

    init events

--- packages/next-common/components/calendar/dayEvents/index.js
+++ packages/next-common/components/calendar/dayEvents/index.js
@@ -1,8 +1,59 @@
 import dayjs from "dayjs";
 import React from "react";
 import advancedFormat from "dayjs/plugin/advancedFormat";
+import styled from "styled-components";
+import { NeutralPanel } from "../../styled/containers/neutralPanel";
+import {
+  flex,
+  flex_col,
+  gap_y,
+  m,
+  m_l,
+  p_x,
+  p_y,
+  text_primary,
+  text_tertiary,
+} from "../../../styles/tailwindcss";
+import {
+  p_12_normal,
+  p_16_bold,
+  shadow_100,
+} from "../../../styles/componentCss";
+import Divider from "../../styled/layout/divider";
+import DayEventsTimeline from "./timelines";
 dayjs.extend(advancedFormat);
 
+const Wrapper = styled(NeutralPanel)`
+  ${p_x(24)}
+  ${p_y(20)}
+  ${shadow_100}
+  ${flex}
+  ${flex_col}
+  ${gap_y(16)}
+`;
+
+const Title = styled.h2`
+  ${m(0)}
+  ${p_16_bold}
+  ${text_primary}
+`;
+const TitleDate = styled.small`
+  ${m_l(12)}
+  ${p_12_normal}
+  ${text_tertiary}
+`;
+
 export default function DayEvents({ selectedDate }) {
-  return <div>Day events {dayjs(selectedDate).format("MMMM Do, YYYY")}</div>;
+  return (
+    <Wrapper>
+      <Title>
+        Event
+        <TitleDate>{dayjs(selectedDate).format("MMMM Do, YYYY")}</TitleDate>
+      </Title>
+
+      <Divider />
+
+      <DayEventsTimeline />
+    </Wrapper>
+  );
 }
---
+++ packages/next-common/components/calendar/dayEvents/timelines.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DayEventsTimeline() {
+  return <div>timelines</div>;
+}
