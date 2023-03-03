ab38b37033b98c79d74f40d37488d547b515ae14
2nthony <ant.hu@outlook.com>
2023-03-03 15:11:20 +0800

    eslint update

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,7 +2,16 @@ module.exports = {
   rules: {
     semi: ["warn", "always"],
     quotes: ["warn", "double"],
-    "comma-dangle": ["warn", "always-multiline"],
+    "comma-dangle": [
+      "warn",
+      {
+        arrays: "only-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "never",
+      },
+    ],
     "object-curly-newline": ["warn", { multiline: true, consistent: true }],
     "object-curly-spacing": ["warn", "always"],
     "object-property-newline": [
