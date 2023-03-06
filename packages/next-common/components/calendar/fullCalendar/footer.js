0fe73b3c1fed074e486adca32569aa1983c456f5
2nthony <ant.hu@outlook.com>
2023-03-06 16:37:41 +0800

    fix: calendar footer assert gov2

--- packages/next-common/components/calendar/fullCalendar/footer.js
+++ packages/next-common/components/calendar/fullCalendar/footer.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
+import { useMenuHasGov2 } from "../../../context/chain";
 import { flex, gap_x, justify_center, p_b } from "../../../styles/tailwindcss";
 import { useIsScreenSize } from "../../../utils/hooks/useIsScreenSize";
 import FullCalendarCategory from "./category";
+import {
+  FULLCALENDAR_CATEGORY_COLLECTIVES,
+  FULLCALENDAR_CATEGORY_DEMOCRACY,
+  FULLCALENDAR_CATEGORY_OPEN_GOV,
+  FULLCALENDAR_CATEGORY_TREASURY,
+} from "./consts";
 
 const Wrapper = styled.div`
   ${flex}
@@ -11,10 +18,21 @@ const Wrapper = styled.div`
   ${p_b(8)}
 `;
 
-const categories = ["OpenGov", "Democracy", "Treasury", "Collectives"];
-
 export default function FullCalendarFooter() {
   const { isSmSize } = useIsScreenSize();
+  const hasGov2 = useMenuHasGov2();
+
+  const categories = useMemo(
+    () =>
+      [
+        hasGov2 && FULLCALENDAR_CATEGORY_OPEN_GOV,
+        FULLCALENDAR_CATEGORY_DEMOCRACY,
+        FULLCALENDAR_CATEGORY_TREASURY,
+        FULLCALENDAR_CATEGORY_COLLECTIVES,
+      ].filter(Boolean),
+    [hasGov2]
+  );
+
   if (isSmSize) {
     return null;
   }
