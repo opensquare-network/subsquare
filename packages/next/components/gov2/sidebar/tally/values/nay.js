242b6b027734ca976899a82f4bfc1a4c6b189cf6
2nthony <ant.hu@outlook.com>
2023-03-24 14:24:03 +0800

    reuse aye nay icons

--- packages/next/components/gov2/sidebar/tally/values/nay.js
+++ packages/next/components/gov2/sidebar/tally/values/nay.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { votesSelector } from "next-common/store/reducers/gov2ReferendumSlice";
-import NayIcon from "../../../../../public/imgs/icons/nay.svg";
+import NayIcon from "next-common/assets/imgs/icons/nay.svg";
 import { useTally } from "next-common/context/post/gov2/referendum";
 import LoadingCount from "./loadingCount";
 import SymbolValue from "./symbolValue";
