2f61ddb24d69f4c0341285e17952ebd2e8869565
2nthony <ant.hu@outlook.com>
2023-03-24 18:56:21 +0800

    props

--- packages/next-common/components/layout/HomeLayout.js
+++ packages/next-common/components/layout/HomeLayout.js
@@ -1,9 +1,16 @@
+/* eslint-disable */
+
 import React from "react";
 import BaseLayout from "./baseLayout";
 import Menu from "../menu";
 import homeMenus from "../../utils/consts/menu";
 
-export default function HomeLayout({ children, seoInfo }) {
+export default function HomeLayout({
+  children,
+  seoInfo,
+  tracks,
+  fellowshipTracks,
+}) {
   return (
     <BaseLayout seoInfo={seoInfo} left={<Menu menu={homeMenus} />}>
       {children}
