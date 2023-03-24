de8cf72d16206990aa2db4032a0aa5b0ff6dab30
2nthony <ant.hu@outlook.com>
2023-03-24 18:56:50 +0800

    menu files

---
+++ packages/next-common/utils/consts/menu/referenda.js
@@ -0,0 +1,40 @@
+import { getExcludeChains } from "../../viewfuncs";
+import Chains from "../chains";
+import { TrackIconMap } from "../../../components/icons/track";
+import { parseGov2TrackName } from "../../gov2";
+import sumBy from "lodash.sumby";
+
+export function resolveReferendaMenu(tracks = []) {
+  const totalActiveCount = sumBy(tracks, (t) => t.activeCount || 0);
+
+  const menu = {
+    name: "REFERENDA",
+    excludeToChains: getExcludeChains([Chains.development, Chains.kusama]),
+    items: [
+      {
+        value: "all",
+        name: "All",
+        pathname: "/referenda",
+        icon: TrackIconMap.All,
+        activeCount: totalActiveCount,
+      },
+    ],
+  };
+
+  const resolveTrackItem = (track) => {
+    return {
+      value: track.id,
+      name: parseGov2TrackName(track.name),
+      pathname: `/referenda/track/${track.id}`,
+      activeCount: track.activeCount,
+      icon: TrackIconMap[track.id] ?? TrackIconMap.Default,
+    };
+  };
+
+  for (let idx = 0; idx < tracks.length; idx++) {
+    const track = tracks[idx];
+    menu.items.push(resolveTrackItem(track));
+  }
+
+  return [];
+}
