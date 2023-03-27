4e3be5131d46e2d5a13f496ab76ed56cd4292a29
2nthony <ant.hu@outlook.com>
2023-03-27 16:00:42 +0800

    democracy pass tracks props

--- packages/next/pages/democracy/referenda.js
+++ packages/next/pages/democracy/referenda.js
@@ -7,30 +7,40 @@ import businessCategory from "next-common/utils/consts/business/category";
 import HomeLayout from "next-common/components/layout/HomeLayout";
 import DemocracySummaryFooter from "next-common/components/summary/democracySummaryFooter";
 import normalizeReferendaListItem from "next-common/utils/viewfuncs/democracy/normalizeReferendaListItem";
+import { fellowshipTracksApi, gov2TracksApi } from "next-common/services/url";
 
-export default withLoginUserRedux(({ posts, chain }) => {
-  const items = (posts.items || []).map((item) =>
-    normalizeReferendaListItem(chain, item)
-  );
-  const category = businessCategory.democracyReferenda;
-  const seoInfo = { title: "Democracy Referenda", desc: "Democracy Referenda" };
+export default withLoginUserRedux(
+  ({ posts, chain, tracks, fellowshipTracks }) => {
+    const items = (posts.items || []).map((item) =>
+      normalizeReferendaListItem(chain, item)
+    );
+    const category = businessCategory.democracyReferenda;
+    const seoInfo = {
+      title: "Democracy Referenda",
+      desc: "Democracy Referenda",
+    };
 
-  return (
-    <HomeLayout seoInfo={seoInfo}>
-      <PostList
-        category={category}
-        create={null}
-        items={items}
-        pagination={{
-          page: posts.page,
-          pageSize: posts.pageSize,
-          total: posts.total,
-        }}
-        summary={<DemocracySummary footer={<DemocracySummaryFooter />} />}
-      />
-    </HomeLayout>
-  );
-});
+    return (
+      <HomeLayout
+        seoInfo={seoInfo}
+        tracks={tracks}
+        fellowshipTracks={fellowshipTracks}
+      >
+        <PostList
+          category={category}
+          create={null}
+          items={items}
+          pagination={{
+            page: posts.page,
+            pageSize: posts.pageSize,
+            total: posts.total,
+          }}
+          summary={<DemocracySummary footer={<DemocracySummaryFooter />} />}
+        />
+      </HomeLayout>
+    );
+  }
+);
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const chain = process.env.CHAIN;
@@ -44,10 +54,17 @@ export const getServerSideProps = withLoginUser(async (context) => {
     }),
   ]);
 
+  const [{ result: tracks }, { result: fellowshipTracks }] = await Promise.all([
+    nextApi.fetch(gov2TracksApi),
+    nextApi.fetch(fellowshipTracksApi),
+  ]);
+
   return {
     props: {
       chain,
       posts: posts ?? EmptyList,
+      tracks: tracks ?? [],
+      fellowshipTracks: fellowshipTracks ?? [],
     },
   };
 });
