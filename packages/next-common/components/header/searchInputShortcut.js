a196c2abcbd6b066f3f57cfbbf0a84467bc41ce8
2nthony <ant.hu@outlook.com>
2023-03-29 14:22:09 +0800

    hotfix: search input shortcut trigger check

--- packages/next-common/components/header/searchInputShortcut.js
+++ packages/next-common/components/header/searchInputShortcut.js
@@ -2,10 +2,16 @@ import React from "react";
 import { useKey } from "../../utils/hooks/useKey";
 import SlashShortIcon from "../icons/slashShortcut";
 
+const ignoreTagNames = ["INPUT","TEXTAREA"];
+
 export default function SearchBarShortcut({ focus, inputRef }) {
   useKey(
     "/",
     (event) => {
+      if (ignoreTagNames.includes(event.target?.tagName)) {
+        return;
+      }
+
       if (!focus) {
         event.preventDefault();
         inputRef.current.focus();
