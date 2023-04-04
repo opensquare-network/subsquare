530823d97b5d0ed33e5bd079f9692308a757b60b
2nthony <ant.hu@outlook.com>
2023-04-04 16:45:43 +0800

    clean react quill

--- packages/next/pages/_app.js
+++ packages/next/pages/_app.js
@@ -6,7 +6,6 @@ import "nprogress/nprogress.css";
 import "next-common/styles/globals.css";
 import { store } from "next-common/store";
 import "next-common/styles/richTextStyles.scss";
-// import "react-quill/dist/quill.snow.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { connect } from "next-common/services/websocket";
 import React, { useEffect } from "react";
