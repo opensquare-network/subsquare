1d9edb9096a5cc12afde8ef797f36acf5cad6352
2nthony <ant.hu@outlook.com>
2023-04-11 12:06:05 +0800

    hotfix: tally, mobile, support percentage (#2816)
    * nowrap
    
    * support percentage
    
    * fix: tally icon width

--- packages/next/components/gov2/sidebar/tally/values/support.js
+++ packages/next/components/gov2/sidebar/tally/values/support.js
@@ -5,8 +5,18 @@ import {
   BorderedRow,
   Header,
 } from "next-common/components/referenda/tally/styled";
+import Percentage from "next-common/components/referenda/tally/support/percentage";
+import styled from "styled-components";
+import { p_14_medium } from "next-common/styles/componentCss";
+import { m_l, text_tertiary } from "next-common/styles/tailwindcss";
 
-export default function Support() {
+const PercentageWrapper = styled.span`
+  ${p_14_medium};
+  ${text_tertiary};
+  ${m_l(4)};
+`;
+
+export default function Support({ supportPerbill = 0 }) {
   const tally = useTally();
 
   return (
@@ -14,6 +24,9 @@ export default function Support() {
       <Header>
         <ElectorateIcon />
         Support
+        <PercentageWrapper>
+          (<Percentage perbill={supportPerbill} />)
+        </PercentageWrapper>
       </Header>
       <SymbolValue value={tally?.support} />
     </BorderedRow>
