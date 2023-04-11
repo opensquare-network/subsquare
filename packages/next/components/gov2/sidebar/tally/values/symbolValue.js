6a87d4c510f931a1bab9df5d71265d0dd9c2e320
2nthony <ant.hu@outlook.com>
2023-04-11 10:55:43 +0800

    nowrap

--- packages/next/components/gov2/sidebar/tally/values/symbolValue.js
+++ packages/next/components/gov2/sidebar/tally/values/symbolValue.js
@@ -1,17 +1,15 @@
 import ValueDisplay from "next-common/components/valueDisplay";
 import { useChainSettings } from "next-common/context/chain";
 import { toPrecision } from "next-common/utils";
-import useWindowSize from "next-common/utils/hooks/useWindowSize";
 import { Value } from "next-common/components/referenda/tally/styled";
 
 export default function SymbolValue({ value = 0 }) {
   const { decimals, symbol } = useChainSettings();
   const nValue = toPrecision(value || 0, decimals);
-  const { width } = useWindowSize();
 
   return (
     <Value>
-      <ValueDisplay value={nValue} symbol={symbol} noWrap={width <= 1024} />
+      <ValueDisplay value={nValue} symbol={symbol} />
     </Value>
   );
 }
