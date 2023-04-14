f7324533c62f088adc9b973407b6f90c1221e4ab
2nthony <ant.hu@outlook.com>
2023-04-14 21:47:20 +0800

    treasury proposal timeline

--- packages/next/components/treasuryProposal/timeline.js
+++ packages/next/components/treasuryProposal/timeline.js
@@ -8,6 +8,7 @@ import { detailPageCategory } from "next-common/utils/consts/business/category";
 import { createReferendumTimelineData } from "utils/timeline/referendum";
 import SymbolBalance from "next-common/components/values/symbolBalance";
 import formatTime from "next-common/utils/viewfuncs/formatDate";
+import { useEffect, useState } from "react";
 
 function getTimelineData(args, method) {
   switch (method) {
@@ -41,48 +42,54 @@ function getGov2ReferendumTimeline(timelineItem, treasuryProposal) {
       time: formatTime(indexer?.blockTime),
       status: getTimelineStatus(
         detailPageCategory.TREASURY_PROPOSAL,
-        "Approved"
+        "Approved",
       ),
     },
   ];
 }
 
 export default function TreasuryProposalTimeline({ treasuryProposal }) {
-  const timelineData = flatten(
-    (treasuryProposal?.timeline || []).map((item) => {
-      const indexer = item.extrinsicIndexer ?? item.indexer;
+  const [timelineData, setTimelineData] = useState([]);
+  useEffect(() => {
+    const data = flatten(
+      (treasuryProposal?.timeline || []).map((item) => {
+        const indexer = item.extrinsicIndexer ?? item.indexer;
 
-      const method = item.method ?? item.name;
+        const method = item.method ?? item.name;
 
-      // Handle Gov2 created treasury proposal
-      if (method === "SpendApproved" && treasuryProposal.isByGov2) {
-        return getGov2ReferendumTimeline(item, treasuryProposal);
-      }
+        // Handle Gov2 created treasury proposal
+        if (method === "SpendApproved" && treasuryProposal.isByGov2) {
+          return getGov2ReferendumTimeline(item, treasuryProposal);
+        }
 
-      return {
-        indexer,
-        time: formatTime(indexer?.blockTime),
-        status: getTimelineStatus(detailPageCategory.TREASURY_PROPOSAL, method),
-        data: getTimelineData(item.args, item.method ?? item.name),
-      };
-    })
-  );
-
-  const motions = treasuryProposal?.motions?.map((motion) => {
-    return createMotionTimelineData(motion, true, "/council/motion");
-  });
-  timelineData.push(...motions);
-
-  const referendums = treasuryProposal?.referendums?.map((referendum) => {
-    return createReferendumTimelineData(
-      referendum,
-      true,
-      "/democracy/referendum"
+        return {
+          indexer,
+          time: formatTime(indexer?.blockTime),
+          status: getTimelineStatus(
+            detailPageCategory.TREASURY_PROPOSAL,
+            method,
+          ),
+          data: getTimelineData(item.args, item.method ?? item.name),
+        };
+      }),
     );
-  });
-  timelineData.push(...referendums);
 
-  sortTimeline(timelineData);
+    const motions =
+      treasuryProposal?.motions?.map((motion) => {
+        return createMotionTimelineData(motion, true, "/council/motion");
+      }) ?? [];
+
+    const referendums =
+      treasuryProposal?.referendums?.map((referendum) => {
+        return createReferendumTimelineData(
+          referendum,
+          true,
+          "/democracy/referendum",
+        );
+      }) ?? [];
+
+    setTimelineData(sortTimeline([...data, ...motions, ...referendums]));
+  }, [treasuryProposal]);
 
   return <Timeline data={timelineData} indent={false} />;
 }
