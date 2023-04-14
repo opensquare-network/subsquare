8d85df56425d3e01337f2450816cf6f509a2997b
2nthony <ant.hu@outlook.com>
2023-04-14 21:13:17 +0800

    Revert "usememo should work"
    This reverts commit 4a143421fab6402d0be35db648f11f03785a6405.

--- packages/next/components/bounty/timeline.js
+++ packages/next/components/bounty/timeline.js
@@ -6,7 +6,7 @@ import sortTimeline from "next-common/utils/timeline/sort";
 import { detailPageCategory } from "next-common/utils/consts/business/category";
 import SymbolBalance from "next-common/components/values/symbolBalance";
 import formatTime from "next-common/utils/viewfuncs/formatDate";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 
 export default function BountyTimeline({ bounty }) {
   const getTimelineData = (args, method) => {
@@ -49,7 +49,26 @@ export default function BountyTimeline({ bounty }) {
     return args;
   };
 
-  const timelineData = useMemo(() => {
+  // const timelineData = (bounty?.timeline || []).map((item) => {
+  //   const indexer = item.extrinsicIndexer ?? item.indexer;
+  //   return {
+  //     indexer,
+  //     time: formatTime(indexer?.blockTime),
+  //     status: getTimelineStatus(
+  //       detailPageCategory.TREASURY_BOUNTY,
+  //       item.method ?? item.name,
+  //     ),
+  //     data: getTimelineData(item.args, item.method ?? item.name),
+  //   };
+  // });
+  //
+  // const motions = bounty?.motions?.map((motion) => {
+  //   return createMotionTimelineData(motion, true, "/council/motion");
+  // });
+  // timelineData.push(...motions);
+  // sortTimeline(timelineData);
+  const [timelineData, setTimelineData] = useState([]);
+  useEffect(() => {
     const data = (bounty?.timeline || []).map((item) => {
       const indexer = item.extrinsicIndexer ?? item.indexer;
       return {
@@ -68,7 +87,7 @@ export default function BountyTimeline({ bounty }) {
         return createMotionTimelineData(motion, true, "/council/motion");
       }) ?? [];
 
-    return sortTimeline([...data, ...motions].filter(Boolean));
+    setTimelineData(sortTimeline([...data, ...motions].filter(Boolean)));
   }, [bounty]);
 
   return <Timeline data={timelineData} />;
