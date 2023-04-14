adf88679825523e17dbba85a3458a3b4649464a3
2nthony <ant.hu@outlook.com>
2023-04-14 21:40:35 +0800

    announcement timeline

--- packages/next-common/components/alliance/announcement/timeline.js
+++ packages/next-common/components/alliance/announcement/timeline.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDetailType } from "../../../context/page";
 import sortTimeline from "../../../utils/timeline/sort";
 import Timeline from "../../timeline";
@@ -10,7 +10,7 @@ function getData(item) {
   const { method, args = {} } = item;
   if ("Announced" === method) {
     return {
-      cid: <IpfsCidWithLink cid={ args.cid } />,
+      cid: <IpfsCidWithLink cid={args.cid} />,
     };
   }
 
@@ -21,17 +21,26 @@ export default function AnnouncementTimeline({ data }) {
   const type = useDetailType();
 
   const { timeline = [], motion } = data;
-  const timelineData = timeline.map(item => {
-    const { indexer, method } = item;
-    return {
-      indexer: item.indexer,
-      time: formatTime(indexer?.blockTime),
-      status: { value: method, type },
-      data: getData(item),
-    };
-  });
-  const motionTimeline = createMotionTimelineData(motion, true, "/alliance/motion");
-  timelineData.push(motionTimeline);
-  sortTimeline(timelineData);
-  return <Timeline data={ timelineData } indent={ false } />;
+
+  const [timelineData, setTimelineData] = useState([]);
+  useEffect(() => {
+    const data = timeline.map((item) => {
+      const { indexer, method } = item;
+      return {
+        indexer: item.indexer,
+        time: formatTime(indexer?.blockTime),
+        status: { value: method, type },
+        data: getData(item),
+      };
+    });
+    const motionTimeline = createMotionTimelineData(
+      motion,
+      true,
+      "/alliance/motion",
+    );
+
+    setTimelineData(sortTimeline([...data, ...motionTimeline].filter(Boolean)));
+  }, [timeline, motion]);
+
+  return <Timeline data={timelineData} indent={false} />;
 }
