661b6265c2799794243d766b14ce0d6a4ec029c7
2nthony <ant.hu@outlook.com>
2023-04-14 20:12:42 +0800

    Revert "format time"
    This reverts commit 2fee18cfb95ddc0757b142f9f63686aaa760450d.

--- packages/next/components/bounty/timeline.js
+++ packages/next/components/bounty/timeline.js
@@ -52,8 +52,7 @@ export default function BountyTimeline({ bounty }) {
     const indexer = item.extrinsicIndexer ?? item.indexer;
     return {
       indexer,
-      // time: formatTime(indexer?.blockTime),
-      time: Date.now(),
+      time: formatTime(indexer?.blockTime),
       status: getTimelineStatus(
         detailPageCategory.TREASURY_BOUNTY,
         item.method ?? item.name,
