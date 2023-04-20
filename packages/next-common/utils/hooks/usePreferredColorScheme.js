4422b374f8ba5faba633083e82d44b2611111eaa
2nthony <ant.hu@outlook.com>
2023-04-20 15:32:15 +0800

    clean

--- packages/next-common/utils/hooks/usePreferredColorScheme.js
+++ packages/next-common/utils/hooks/usePreferredColorScheme.js
@@ -1,10 +1,8 @@
 import { useMediaQuery } from "usehooks-ts";
 
 export function usePreferredColorScheme() {
-  const isLight = useMediaQuery("(prefers-color-scheme: light)");
   const isDark = useMediaQuery("(prefers-color-scheme: dark)");
 
   if (isDark) return "dark";
-  else if (isLight) return "light";
-  else return "no-preference";
+  else return "light";
 }
