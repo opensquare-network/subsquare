be9042985c7e4eaf2771aec6bea2ff6de5266f51
chaojun <chaojun@opensquare.network>
2023-04-25 18:35:47 +0800

    Remove

--- packages/next-common/utils/consts/connect/index.js
+++ packages/next-common/utils/consts/connect/index.js
@@ -24,11 +24,4 @@ export const WALLETS = [
       "https://chrome.google.com/webstore/detail/talisman-wallet/fijngjgcjhjmmpcmkeiomlglpeiijkld",
     logo: TalismanLogo,
   },
-  {
-    extensionName: "polkagate",
-    title: "PolkaGate",
-    installUrl:
-      "https://chrome.google.com/webstore/detail/polkagate-the-gateway-to/ginchbkmljhldofnbjabmeophlhdldgp",
-    logo: TalismanLogo,
-  },
 ];
