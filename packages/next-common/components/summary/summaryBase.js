f3deda8750ca5182df3fb240c47a300c84bbced9
chaojun <chaojun@opensquare.network>
2023-04-28 15:53:01 +0800

    fix: refactor

--- packages/next-common/components/summary/summaryBase.js
+++ packages/next-common/components/summary/summaryBase.js
@@ -1,20 +1,8 @@
 import React from "react";
-import styled, { css } from "styled-components";
-import { smcss } from "../../utils/responsive";
+import styled from "styled-components";
 import { SummaryCard, SummaryDescription } from "./styled";
 import Divider from "../styled/layout/divider";
-
-export const SummaryItems = styled.div`
-  display: flex;
-  ${smcss(css`
-    flex-direction: column;
-    gap: 16px;
-  `)}
-`;
-
-export const SummaryItem = styled.div`
-  flex: 1;
-`;
+import SummaryItems from "./summaryItems";
 
 const Wrapper = styled(SummaryCard)`
   margin: 16px 0px;
