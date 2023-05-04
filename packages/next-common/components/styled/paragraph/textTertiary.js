77f9c21fe3908255767ade6d5a0fd8faa19b0bef
Yongfeng LI <wliyongfeng@gmail.com>
2023-05-04 16:24:01 +0800

    Show min enact period on OpenGov track panel, #2849 (#2850)

---
+++ packages/next-common/components/styled/paragraph/textTertiary.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const TextTertiary = styled.span`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: ${(props) => props.theme.textTertiary};
+`;
+
+export default TextTertiary;
