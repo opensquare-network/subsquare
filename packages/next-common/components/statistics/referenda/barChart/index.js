d1067f27a9b2369f3391b84728b18d98e7bfa764
2nthony <ant.hu@outlook.com>
2023-05-06 17:40:21 +0800

    fix: bar chart y ticks font color

--- packages/next-common/components/statistics/referenda/barChart/index.js
+++ packages/next-common/components/statistics/referenda/barChart/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Bar } from "react-chartjs-2";
 import "../../../charts/globalConfig";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import LegendItem from "../../../charts/legend/item";
 import {
   flex,
@@ -19,48 +19,57 @@ const Legend = styled.div`
   ${m_t(16)};
 `;
 
-/** @type {import("react-chartjs-2").ChartProps} */
-export const options = {
-  indexAxis: "y",
-  responsive: true,
-  plugins: {
-    legend: {
-      display: false,
+/**
+ * @returns {import("react-chartjs-2").ChartProps}
+ */
+function useOptions() {
+  const theme = useTheme();
+
+  return {
+    indexAxis: "y",
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+      // title: {
+      //   display: true,
+      //   text: "Chart.js Horizontal Bar Chart",
+      // },
     },
-    // title: {
-    //   display: true,
-    //   text: "Chart.js Horizontal Bar Chart",
-    // },
-  },
-  scales: {
-    x: {
-      border: {
-        display: false,
-      },
-      ticks: {
-        display: false,
-      },
-      grid: {
-        display: false,
-      },
-    },
-    y: {
-      border: {
-        display: false,
-      },
-      ticks: {
-        font: {
-          size: 14,
+    scales: {
+      x: {
+        border: {
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+        grid: {
+          display: false,
         },
       },
-      grid: {
-        display: false,
+      y: {
+        border: {
+          display: false,
+        },
+        ticks: {
+          font: {
+            size: 14,
+          },
+          color: theme.textPrimary,
+        },
+        grid: {
+          display: false,
+        },
       },
     },
-  },
-};
+  };
+}
 
 export default function BarChart({ data }) {
+  const options = useOptions();
+
   return (
     <Wrapper>
       <Bar data={data} options={options} />
