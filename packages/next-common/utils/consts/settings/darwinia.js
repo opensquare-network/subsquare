a45d5c7b3380ef8c11012d6d111fc533ff35db89
chaojun <chaojun@opensquare.network>
2023-05-09 14:01:42 +0800

    Update darwinia icons

--- packages/next-common/utils/consts/settings/darwinia.js
+++ packages/next-common/utils/consts/settings/darwinia.js
@@ -1,6 +1,6 @@
-import Logo from "../../../assets/header-logos/polkadot.svg";
-import DarkModeLogo from "../../../assets/header-logos/polkadot-dark.svg";
-import Avatar from "../../../assets/icons/chain/polkadot.png";
+import Logo from "../../../assets/header-logos/darwinia.svg";
+import DarkModeLogo from "../../../assets/header-logos/darwinia-dark.svg";
+import Avatar from "../../../assets/icons/chain/darwinia.png";
 
 import Chains from "../chains";
 import capitalize from "../../capitalize";
@@ -26,7 +26,7 @@ const darwinia = {
   decimals: 9,
   hasElections: false,
   ss58Format: 18,
-  snsCoverCid: "bafybeidmqvyfi467agi4cum26idgh5h56wmegrjh7jnl5wvtkzbvgucmpm",
+  snsCoverCid: "QmePHNth5sm1P55WeKWwMeSXnNNw42LyagzTs7NeU5YN9A",
   endpoints: DEFAULT_DARWINIA_NODES,
   headerLogo: Logo,
   darkHeaderLogo: DarkModeLogo,
