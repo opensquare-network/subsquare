5474814c2bc545588b9460390f60cb7a6dff5b97
2nthony <ant.hu@outlook.com>
2023-05-12 14:12:48 +0800

    fix: statistics been delegated dialog width

--- packages/next-common/components/statistics/referenda/beenDelegatedPopup/index.js
+++ packages/next-common/components/statistics/referenda/beenDelegatedPopup/index.js
@@ -2,6 +2,17 @@ import React from "react";
 import Popup from "next-common/components/popup/wrapper/Popup";
 import DelegationSummary from "./delegationSummary";
 import DelegationTabList from "./delegationTabList";
+import styled from "styled-components";
+import { smcss } from "next-common/utils/responsive";
+import { w_full, overflow_y_scroll } from "next-common/styles/tailwindcss";
+
+const StyledPopup = styled(Popup)`
+  width: 720px;
+  max-height: 80vh;
+  ${overflow_y_scroll};
+
+  ${smcss(w_full)};
+`;
 
 export default function BeenDelegatedListPopup({
   delegatee,
@@ -12,7 +23,7 @@ export default function BeenDelegatedListPopup({
   setShow,
 }) {
   return (
-    <Popup wide title="Been Delegated" onClose={() => setShow(false)}>
+    <StyledPopup wide title="Been Delegated" onClose={() => setShow(false)}>
       <DelegationSummary
         delegatee={delegatee}
         delegatorsCount={delegatorsCount}
@@ -21,6 +32,6 @@ export default function BeenDelegatedListPopup({
         tracksCount={tracksCount}
       />
       <DelegationTabList delegatee={delegatee} />
-    </Popup>
+    </StyledPopup>
   );
 }
