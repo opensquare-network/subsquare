65a9f489311cad01e3c0bb66dba1b4c3c9426594
2nthony <ant.hu@outlook.com>
2023-05-19 10:43:36 +0800

    styledlist support no current votes [skip ci]

--- packages/next-common/components/styledList/index.js
+++ packages/next-common/components/styledList/index.js
@@ -4,6 +4,7 @@ import Loading from "next-common/components/loading";
 import { EmptyTd, StyledTable } from "./styled";
 import { Headers } from "./headers";
 import DataRows from "./dataRows";
+import { StyledTr } from "../styled/table";
 
 function EmptyOrLoading({ loading }) {
   return (
@@ -18,10 +19,16 @@ function EmptyOrLoading({ loading }) {
 function StyledList({ columns = [], rows = [], loading = false }) {
   let tableBody = null;
 
-  if (!loading) {
-    tableBody = <DataRows rows={rows} columns={columns} />;
-  } else {
+  if (loading) {
     tableBody = <EmptyOrLoading loading={loading} />;
+  } else if (!rows?.length) {
+    tableBody = (
+      <StyledTr>
+        <EmptyTd>No current votes</EmptyTd>
+      </StyledTr>
+    );
+  } else {
+    tableBody = <DataRows rows={rows} columns={columns} />;
   }
 
   return (
