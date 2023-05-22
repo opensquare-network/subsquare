d13783060cc8c912204b106636040657382a993c
chaojun <chaojun@opensquare.network>
2023-05-22 19:35:22 +0800

    hotfix: metamask popup

--- packages/next-common/components/popupWithAddress/index.js
+++ packages/next-common/components/popupWithAddress/index.js
@@ -3,6 +3,8 @@ import useExtensionAccounts from "../../utils/hooks/useExtensionAccounts";
 import Popup from "../popup/wrapper/Popup";
 import MaybeLogin from "../maybeLogin";
 import { useMetaMaskAccounts } from "next-common/utils/metamask";
+import { useChainSettings } from "next-common/context/chain";
+import ChainTypes from "next-common/utils/consts/chainTypes";
 
 export default function PopupWithAddress({
   Component,
@@ -11,6 +13,7 @@ export default function PopupWithAddress({
   autoCloseAfterLogin,
   ...props
 }) {
+  const { chainType } = useChainSettings();
   const { accounts: polkadotExtensionAccounts, detecting: extensionDetecting } =
     useExtensionAccounts("subsquare");
   const polkadotAccounts = polkadotExtensionAccounts.map((item) => ({
@@ -18,7 +21,7 @@ export default function PopupWithAddress({
     name: item.meta.name,
   }));
 
-  const metamaskAccounts = useMetaMaskAccounts();
+  const metamaskAccounts = useMetaMaskAccounts(chainType === ChainTypes.ETHEREUM);
   const extensionAccounts = [...polkadotAccounts, ...metamaskAccounts];
 
   if (extensionDetecting) {
