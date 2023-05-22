e5e94b2575e57d573ad97c79e71f1a7bc8b3fc09
2nthony <ant.hu@outlook.com>
2023-05-22 12:13:34 +0800

    extract capital table item

---
+++ packages/next-common/components/popup/capitalTableItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import VoteLabel from "next-common/components/democracy/allVotesPopup/voteLabel";
+import ValueDisplay from "next-common/components/valueDisplay";
+import { p_14_normal } from "next-common/styles/componentCss";
+import { inline_flex, text_tertiary } from "next-common/styles/tailwindcss";
+import styled from "styled-components";
+import { useChain, useChainSettings } from "next-common/context/chain";
+import Chains from "next-common/utils/consts/chains";
+
+const Capital = styled.div`
+  ${inline_flex};
+  ${p_14_normal};
+`;
+const CapitalConvictionLabel = styled.span`
+  width: 60px;
+  ${text_tertiary};
+`;
+
+export default function CapitalTableItem({ capital, item, tab, conviction }) {
+  const chainSettings = useChainSettings();
+  const chain = useChain();
+  const symbol = chainSettings.voteSymbol || chainSettings.symbol;
+  const hasLabel = ![Chains.kintsugi, Chains.interlay].includes(chain);
+
+  return (
+    <Capital>
+      <ValueDisplay value={capital} symbol={symbol} showTooltip={false} />
+      {hasLabel && (
+        <CapitalConvictionLabel>
+          <VoteLabel conviction={conviction} {...item} tab={tab} />
+        </CapitalConvictionLabel>
+      )}
+    </Capital>
+  );
+}
