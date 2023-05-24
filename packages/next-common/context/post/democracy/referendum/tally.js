89143b215c9595df1ca9f5fb1642e17c9e31d2a1
Yongfeng LI <wliyongfeng@gmail.com>
2023-05-24 21:06:31 +0800

    Hotfix legacy referendum tally

--- packages/next-common/context/post/democracy/referendum/tally.js
+++ packages/next-common/context/post/democracy/referendum/tally.js
@@ -2,5 +2,15 @@ import { useOnchainData } from "../../index";
 
 export default function useDemocracyTally() {
   const onchain = useOnchainData();
-  return onchain?.tally;
+  if (onchain.tally) {
+    return onchain?.tally;
+  }
+
+  if (onchain.info?.ongoing) {
+    return onchain.info?.ongoing?.tally;
+  }
+
+  if (onchain.status) {
+    return onchain.status?.tally;
+  }
 }
