f60a461656fed0520c505a562d3ba7e8f2fb24f0
Yongfeng LI <wliyongfeng@gmail.com>
2023-05-25 22:28:24 +0800

    Extract dedicated component for tip detail page (#2916)
    * Extract dedicated component for tip detail page
    
    * Remove useless lines
    
    * Remove tipSlice
    
    * Refactor Tipper params

--- packages/next-common/store/reducers/index.js
+++ packages/next-common/store/reducers/index.js
@@ -3,7 +3,6 @@ import { combineReducers } from "@reduxjs/toolkit";
 import toastReducer from "./toastSlice";
 import nodeReducer from "./nodeSlice";
 import chainReducer from "./chainSlice";
-import tipReducer from "./tipSlice";
 import referendumReducer from "./referendumSlice";
 import gov2ReferendumReducer from "./gov2ReferendumSlice";
 import cmdkReducer from "./cmdkSlice";
@@ -13,7 +12,6 @@ export default combineReducers({
   toast: toastReducer,
   node: nodeReducer,
   chain: chainReducer,
-  tip: tipReducer,
   referendum: referendumReducer,
   gov2Referendum: gov2ReferendumReducer,
   cmdk: cmdkReducer,
--- packages/next/pages/treasury/tip/[id].js
+++ packages/next/pages/treasury/tip/[id].js
@@ -1,88 +1,35 @@
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 
 import { withLoginUser, withLoginUserRedux } from "next-common/lib";
 import { EmptyList } from "next-common/utils/constants";
 import getMetaDesc from "next-common/utils/post/getMetaDesc";
-
-import DetailItem from "components/detailItem";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import Timeline from "components/tip/timeline";
 import Metadata from "components/tip/metadata";
 import Tipper from "components/tipper";
 import useUniversalComments from "components/universalComments";
 import DetailWithRightLayout from "next-common/components/layout/detailWithRightLayout";
-import { NoticeWrapper } from "next-common/components/styled/containers/titleContainer";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  setTipCountDownBlockNum,
-  tipCountDownBlockNumSelector,
-} from "next-common/store/reducers/tipSlice";
-import isNil from "lodash.isnil";
-import { latestHeightSelector } from "next-common/store/reducers/chainSlice";
-import Loading from "next-common/components/loading";
-import TreasuryCountDown from "next-common/components/treasury/common/countdown";
 import { getBannerUrl } from "next-common/utils/banner";
 import { hashEllipsis } from "next-common/utils";
-import {
-  PostProvider,
-  usePost,
-  usePostDispatch,
-} from "next-common/context/post";
+import { PostProvider, usePost, usePostDispatch } from "next-common/context/post";
 import useWaitSyncBlock from "next-common/utils/hooks/useWaitSyncBlock";
 import BreadcrumbWrapper from "next-common/components/detail/common/BreadcrumbWrapper";
 import Breadcrumb from "next-common/components/_Breadcrumb";
-import useApi from "next-common/utils/hooks/useApi";
 import fetchAndUpdatePost from "next-common/context/post/update";
 import { useDetailType } from "next-common/context/page";
 import CheckUnFinalized from "components/tip/checkUnFinalized";
 import NonNullPost from "next-common/components/nonNullPost";
-import { TipStateMap } from "next-common/utils/viewfuncs/treasury/normalizeTipListItem";
-
-const TipCountDown = ({ meta = {}, state }) => {
-  const nowHeight = useSelector(latestHeightSelector);
-  const tipCountdownBlockNum = useSelector(tipCountDownBlockNumSelector);
-  const closes = meta.closes;
-  if (isNil(closes) || !state || state !== TipStateMap.Tipping) {
-    return null;
-  }
-
-  if (isNil(nowHeight) || isNil(tipCountdownBlockNum)) {
-    return <Loading />;
-  }
-
-  const startHeight = closes - tipCountdownBlockNum;
-
-  return (
-    <NoticeWrapper>
-      <TreasuryCountDown
-        startHeight={startHeight}
-        targetHeight={closes}
-        prefix="Closable"
-      />
-    </NoticeWrapper>
-  );
-};
+import TipDetail from "next-common/components/detail/treasury/tip";
 
 function TreasuryTipContent({ comments }) {
   const post = usePost();
   const postDispatch = usePostDispatch();
   const type = useDetailType();
-  const api = useApi();
 
-  const chainData = post?.onchainData ?? {};
   const { CommentComponent, focusEditor } = useUniversalComments({
     detail: post,
     comments,
   });
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (api) {
-      dispatch(
-        setTipCountDownBlockNum(api.consts.tips?.tipCountdown.toNumber())
-      );
-    }
-  }, [api, dispatch]);
 
   const refreshPageData = useCallback(async () => {
     fetchAndUpdatePost(postDispatch, type, post?._id);
@@ -94,14 +41,8 @@ function TreasuryTipContent({ comments }) {
 
   return (
     <>
-      <DetailItem
-        onReply={focusEditor}
-        countDown={
-          <TipCountDown meta={chainData.meta} state={chainData.state?.state} />
-        }
-      />
+      <TipDetail onReply={focusEditor} />
       <Tipper
-        chainData={chainData}
         onEndorseFinalized={onEndorseFinalized}
         onCloseTipFinalized={onCloseTipFinalized}
         onRetractFinalized={onRetractFinalized}
@@ -114,8 +55,8 @@ function TreasuryTipContent({ comments }) {
 }
 
 export default withLoginUserRedux(({ id, detail, comments }) => {
-  let breadcrumbItemName = "";
-  let postContent = null;
+  let breadcrumbItemName;
+  let postContent;
 
   if (detail) {
     breadcrumbItemName = `#${hashEllipsis(detail?.hash)}`;
