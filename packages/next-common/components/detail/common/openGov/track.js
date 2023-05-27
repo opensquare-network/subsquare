eb176ea8355c45a24b274cbf5211c1a17d7299c9
Yongfeng LI <wliyongfeng@gmail.com>
2023-05-27 18:15:44 +0800

    Extract detail component for fellowship

---
+++ packages/next-common/components/detail/common/openGov/track.js
@@ -0,0 +1,20 @@
+import { useOnchainData } from "../../../../context/post";
+import LinkInfo from "../../../styled/linkInfo";
+import Gov2TrackTag from "../../../gov2/trackTag";
+import React from "react";
+import Link from "next/link";
+
+export default function ReferendaTrack({ isFellowship = false }) {
+  const onchain = useOnchainData();
+  const { name: trackName, id: trackId } = onchain?.trackInfo || {};
+  const section = isFellowship ? "fellowship" : "referenda";
+  let href = `/${ section }/track/${ trackId }`;
+
+  return <div>
+    <Link href={href} passHref legacyBehavior>
+      <LinkInfo>
+        <Gov2TrackTag name={trackName} />
+      </LinkInfo>
+    </Link>
+  </div>;
+}
