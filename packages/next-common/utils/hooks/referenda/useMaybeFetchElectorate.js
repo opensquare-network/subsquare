b6b2cb0f8ec43077bc5659ca26a01c4db785d8c0
Yongfeng LI <wliyongfeng@gmail.com>
2023-05-29 19:42:06 +0800

    Use latest electorate for not finished referenda

--- packages/next-common/utils/hooks/referenda/useMaybeFetchElectorate.js
+++ packages/next-common/utils/hooks/referenda/useMaybeFetchElectorate.js
@@ -8,20 +8,31 @@ import {
   setElectorate,
 } from "../../../store/reducers/referendumSlice";
 import { nowHeightSelector } from "../../../store/reducers/chainSlice";
+import useDemocracyTally from "../../../context/post/democracy/referendum/tally";
+import useDemocracyVoteFinishedHeight from "../../../context/post/democracy/referendum/voteFinishedHeight";
 
 export default function useMaybeFetchElectorate(referendum, api) {
   const dispatch = useDispatch();
   const electorate = useSelector(electorateSelector);
   const referendumStatus = useSelector(referendumStatusSelector);
   const nowHeight = useSelector(nowHeightSelector);
+  const tally = useDemocracyTally();
+  useDemocracyVoteFinishedHeight();
 
-  const { voteFinishedHeight } = extractVoteInfo(referendum?.timeline);
+  const { voteFinished, voteFinishedHeight } = extractVoteInfo(referendum?.timeline);
   const possibleElectorate = referendumStatus?.tally?.electorate;
 
   useEffect(() => {
-    if (possibleElectorate) {
-      dispatch(setElectorate(possibleElectorate));
-    } else if (api) {
+    if (voteFinished && tally?.electorate) {
+      dispatch(setElectorate(tally.electorate));
+      return;
+    }
+
+    if (!electorate && tally?.electorate) {
+      dispatch(setElectorate(tally.electorate));
+    }
+
+    if (api) {
       dispatch(
         fetchElectorate(
           api,
