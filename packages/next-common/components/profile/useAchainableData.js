9bb4efe178873e43843454f8670c4b5bb90d1b1d
hyifeng <chaojun@opensquare.network>
2023-05-29 17:53:30 +0800

    Add achainable labels to profile page (#2936)
    * Add achainable labels to profile page
    
    * Update
    
    * Show achainable for kusama only
    
    * Update
    
    * Update

---
+++ packages/next-common/components/profile/useAchainableData.js
@@ -0,0 +1,13 @@
+import nextApi from "next-common/services/nextApi";
+import { useEffect, useState } from "react";
+
+export default function useAchainableData(id) {
+  const [data, setData] = useState();
+  useEffect(() => {
+    nextApi.fetch(`users/${id}/achainable-labels`).then(({ result }) => {
+      setData(result);
+    });
+  }, [id]);
+
+  return data;
+}
