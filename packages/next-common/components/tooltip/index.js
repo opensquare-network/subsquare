6423fb91c85492eecf888ac5035b133640cf1868
2nthony <ant.hu@outlook.com>
2023-05-29 11:29:59 +0800

    refactor tooltip

--- packages/next-common/components/tooltip/index.js
+++ packages/next-common/components/tooltip/index.js
@@ -4,6 +4,7 @@ import * as RadixTooltip from "@radix-ui/react-tooltip";
 
 import Icon from "next-common/assets/imgs/icons/circle-question.svg";
 import { p_12_normal } from "next-common/styles/componentCss";
+import { cursor_pointer } from "next-common/styles/tailwindcss";
 
 const QuestionIcon = styled(Icon)`
   path {
@@ -11,71 +12,8 @@ const QuestionIcon = styled(Icon)`
   }
 `;
 
-const Wrapper = styled.span`
-  display: inline-block;
-  position: relative;
-  font-size: 12px;
-  font-weight: 400;
-  line-height: 16px;
-  height: 12px;
-  color: rgba(17, 17, 17, 0.65);
-  > svg {
-    cursor: pointer;
-  }
-  :hover {
-    color: ${(props) => props.theme.textPrimary};
-    > svg {
-      stroke-opacity: 1;
-    }
-    > * {
-      display: block;
-    }
-  }
-`;
-
-const PopupWrapper = styled.div`
-  display: none;
-  position: absolute;
-  padding-bottom: 10px;
-  left: 50%;
-  bottom: 100%;
-  transform: translateX(-50%);
-  z-index: 1;
-`;
-
-const Popup = styled.div`
-  position: relative;
-  background: rgba(0, 0, 0, 0.65);
-  border-radius: 4px;
-  width: max-content;
-  padding: 6px 12px;
-  font-size: 12px;
-  line-height: 16px;
-  color: ${(props) => props.theme.textContrast} !important;
-  word-wrap: break-word;
-  text-align: center;
-`;
-
-const Triangle = styled.div`
-  position: absolute;
-  width: 0;
-  height: 0;
-  border-left: 6px solid transparent;
-  border-right: 6px solid transparent;
-  border-top: 6px solid rgba(0, 0, 0, 0.65);
-  left: 50%;
-  top: 100%;
-  transform: translateX(-50%);
-`;
-
-const ChildrenWrapper = styled.div`
-  position: relative;
-  display: inline-block;
-  :hover {
-    > * {
-      display: block;
-    }
-  }
+const LabelWrapper = styled.div`
+  ${cursor_pointer};
 `;
 
 const TooltipContent = styled(RadixTooltip.Content)`
@@ -93,60 +31,28 @@ const TooltipArrow = styled(RadixTooltip.Arrow)`
 `;
 
 export default function Tooltip({ content, children, label, className }) {
+  const tooltipTrigger = children ? (
+    <div className={className}>{children}</div>
+  ) : (
+    <LabelWrapper>{label ? label : <QuestionIcon />}</LabelWrapper>
+  );
+
+  const tooltipContent = content && (
+    <RadixTooltip.Portal>
+      <TooltipContent sideOffset={5}>
+        {content}
+        <TooltipArrow />
+      </TooltipContent>
+    </RadixTooltip.Portal>
+  );
+
   return (
     <RadixTooltip.Provider>
       <RadixTooltip.Root delayDuration={0}>
-        {children ? (
-          <>
-            <RadixTooltip.Trigger asChild>
-              <div>{children}</div>
-            </RadixTooltip.Trigger>
+        <RadixTooltip.Trigger asChild>{tooltipTrigger}</RadixTooltip.Trigger>
 
-            {content && (
-              <RadixTooltip.Portal>
-                <TooltipContent sideOffset={5}>
-                  {content}
-                  <TooltipArrow />
-                </TooltipContent>
-              </RadixTooltip.Portal>
-            )}
-          </>
-        ) : (
-          ""
-        )}
+        {tooltipContent}
       </RadixTooltip.Root>
     </RadixTooltip.Provider>
-  );
-
-  // eslint-disable-next-line no-unreachable
-  return (
-    <>
-      {children ? (
-        <ChildrenWrapper className={className}>
-          {children}
-          {content && (
-            <PopupWrapper>
-              <Popup>
-                {content}
-                <Triangle />
-              </Popup>
-            </PopupWrapper>
-          )}
-        </ChildrenWrapper>
-      ) : (
-        <Wrapper>
-          {label && label}
-          {!label && <QuestionIcon />}
-          {content && (
-            <PopupWrapper>
-              <Popup>
-                {content}
-                <Triangle />
-              </Popup>
-            </PopupWrapper>
-          )}
-        </Wrapper>
-      )}
-    </>
   );
 }
