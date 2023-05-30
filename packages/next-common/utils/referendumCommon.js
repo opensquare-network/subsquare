3b3cc12bc1d8d453f2232f5d1bef1512078c9e48
2nthony <ant.hu@outlook.com>
2023-05-30 11:21:07 +0800

    move `getTallyVotesBarPercent` to common

--- packages/next-common/utils/referendumCommon.js
+++ packages/next-common/utils/referendumCommon.js
@@ -1,3 +1,5 @@
+import BigNumber from "bignumber.js";
+
 export const Conviction = {
   None: 0,
   Locked1x: 1,
@@ -44,3 +46,21 @@ export const convictionToLockX = (conviction) => {
       return "0.1x";
   }
 };
+
+export function getTallyVotesBarPercent(tally) {
+  const ayes = tally?.ayes ?? 0;
+  const nays = tally?.nays ?? 0;
+
+  let ayesPercent = 50;
+  let naysPercent = 50;
+  const nTotal = new BigNumber(ayes).plus(nays);
+  if (nTotal.gt(0)) {
+    ayesPercent = Math.round(new BigNumber(ayes).div(nTotal).toNumber() * 100);
+    naysPercent = 100 - ayesPercent;
+  }
+
+  return {
+    ayesPercent,
+    naysPercent,
+  };
+}
