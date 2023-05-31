18afd820928662ab4e8fb7401f1f83ea9acfff6c
2nthony <ant.hu@outlook.com>
2023-05-31 13:44:01 +0800

    been delegated list

--- packages/next-common/components/summary/democracyBeenDelegated/beenDelegatedListPopup/delegationList.js
+++ packages/next-common/components/summary/democracyBeenDelegated/beenDelegatedListPopup/delegationList.js
@@ -1,125 +1,78 @@
-import React, { Fragment, useMemo } from "react";
-import { withTheme } from "styled-components";
+import React from "react";
 import { toPrecision } from "next-common/utils";
 import User from "next-common/components/user";
 import ValueDisplay from "next-common/components/valueDisplay";
-import Loading from "next-common/components/loading";
 
-import {
-  EmptyTd,
-  RowSplitter,
-  StyledTable,
-  StyledTd,
-  StyledTh,
-  StyledTr,
-} from "next-common/components/styled/table";
-import VoteLabel from "next-common/components/democracy/flattenedVotesPopup/voteLabel";
-import Chains from "next-common/utils/consts/chains";
-import { useChain, useChainSettings } from "next-common/context/chain";
-import { Conviction } from "utils/referendumUtil";
+import { useChainSettings } from "next-common/context/chain";
 import PopupListWrapper from "../../../styled/popupListWrapper";
-import { useScreenSize } from "../../../../utils/hooks/useScreenSize";
+import StyledList from "next-common/components/styledList";
+import CapitalTableItem from "next-common/components/popup/capitalTableItem";
 
-function DelegationList({ items, theme, loading = true }) {
-  const chain = useChain();
+function DelegationList({ items, loading = true }) {
   const node = useChainSettings();
-
-  const hasLabel = ![Chains.kintsugi, Chains.interlay].includes(chain);
   const symbol = node.voteSymbol || node.symbol;
 
-  const { sm } = useScreenSize();
-  const colWidths = useMemo(() => {
-    let widths = {
-      address: 208,
-      label: 80,
-      support: 128,
-    };
+  const colWidths = {
+    address: 266,
+    capital: 168,
+    votes: 128,
+  };
 
-    if (sm && hasLabel) {
-      widths.address = 124;
-    }
+  const columns = [
+    {
+      name: "ADDRESS",
+      style: {
+        textAlign: "left",
+        width: colWidths.address,
+        minWidth: colWidths.address,
+      },
+    },
+    {
+      name: "CAPITAL",
+      style: {
+        textAlign: "right",
+        width: colWidths.capital,
+        minWidth: colWidths.capital,
+      },
+    },
+    {
+      name: "VOTES",
+      style: {
+        textAlign: "right",
+        width: colWidths.votes,
+        minWidth: colWidths.votes,
+      },
+    },
+  ];
 
-    return widths;
-  }, [sm]);
+  const rows = items?.map?.((item) => {
+    const row = [
+      <User
+        key="user"
+        add={item.delegator}
+        fontSize={14}
+        maxWidth={colWidths.address - 50}
+      />,
+      <CapitalTableItem
+        key="capital"
+        item={item}
+        capital={toPrecision(item.balance, node.decimals)}
+        conviction={item.conviction}
+      />,
+      <ValueDisplay
+        key="votes"
+        value={toPrecision(item.votes, node.decimals)}
+        symbol={symbol}
+      />,
+    ];
+    return row;
+  });
 
   return (
     <PopupListWrapper>
-      <StyledTable>
-        <thead>
-          <StyledTr>
-            <StyledTh style={{ textAlign: "left", width: colWidths.address }}>
-              ADDRESS
-            </StyledTh>
-            {hasLabel && (
-              <StyledTh style={{ textAlign: "right", width: colWidths.label }}>
-                LABEL
-              </StyledTh>
-            )}
-            <StyledTh style={{ textAlign: "right", width: colWidths.support }}>
-              SUPPORT
-            </StyledTh>
-          </StyledTr>
-          <RowSplitter
-            backgroundColor={
-              theme.isDark ? theme.grey200Border : theme.grey100Bg
-            }
-            padding={"16px 0 4px 0"}
-          />
-        </thead>
-        <tbody>
-          {items?.length > 0 ? (
-            items.map((item, index) => (
-              <Fragment key={index}>
-                <StyledTr>
-                  <StyledTd
-                    style={{ textAlign: "left", width: colWidths.address }}
-                  >
-                    <User
-                      add={item.delegator}
-                      fontSize={14}
-                      maxWidth={colWidths.address - 30}
-                      noTooltip={true}
-                    />
-                  </StyledTd>
-                  {hasLabel && (
-                    <StyledTd
-                      style={{ textAlign: "right", width: colWidths.label }}
-                    >
-                      <VoteLabel
-                        conviction={Conviction[item.conviction]}
-                        isDelegating={item.isDelegating}
-                      />
-                    </StyledTd>
-                  )}
-                  <StyledTd
-                    style={{ textAlign: "right", width: colWidths.support }}
-                  >
-                    <ValueDisplay
-                      value={toPrecision(item.balance, node.decimals)}
-                      symbol={symbol}
-                    />
-                  </StyledTd>
-                </StyledTr>
-                {index !== items.length - 1 && (
-                  <RowSplitter
-                    backgroundColor={
-                      theme.isDark ? theme.grey200Border : theme.grey100Bg
-                    }
-                  />
-                )}
-              </Fragment>
-            ))
-          ) : (
-            <StyledTr>
-              <EmptyTd colSpan="3">
-                {loading ? <Loading size={16} /> : "No current delegations"}
-              </EmptyTd>
-            </StyledTr>
-          )}
-        </tbody>
-      </StyledTable>
+      <StyledList columns={columns} rows={rows} loading={loading} />
     </PopupListWrapper>
   );
 }
 
-export default withTheme(DelegationList);
+export default DelegationList;
