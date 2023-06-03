54fd9f31bf09c1dfdd924e9434a502bc91ccee57
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-03 23:42:56 +0800

    Create referenda referendum tally slice

---
+++ packages/next-common/store/reducers/referenda/tally.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const name = "referendaReferendumTally";
+
+const referendaReferendumSlice = createSlice({
+  name,
+  initialState: {
+    tally: null,
+  },
+  reducers: {
+    setReferendaReferendumTally(state, { payload }) {
+      state.tally = payload;
+    },
+    clearReferendaReferendumTally(state) {
+      state.tally = null;
+    },
+  },
+});
+
+export const {
+  setReferendaReferendumTally,
+  clearReferendaReferendumTally,
+} = referendaReferendumSlice.actions;
+
+export const referendaReferendumSelector = (state) => state[name].tally;
+
+export default referendaReferendumSlice.reducer;
