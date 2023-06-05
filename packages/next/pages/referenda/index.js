64ec9a1cb0c9cf711b6ef00a313a9d17b72bad96
2nthony <ant.hu@outlook.com>
2023-06-05 17:16:09 +0800

    referenda page add status filter

--- packages/next/pages/referenda/index.js
+++ packages/next/pages/referenda/index.js
@@ -10,10 +10,27 @@ import {
 import Gov2Summary from "components/summary/gov2Summary";
 import Gov2Page from "components/gov2/gov2Page";
 import StatisticLinkButton from "next-common/components/statisticsLinkButton";
+import ReferendaStatusSelectField from "next-common/components/popup/fields/referendaStatusSelectField";
+import { useRouter } from "next/router";
 
 export default withLoginUserRedux(
-  ({ posts, title, tracks, fellowshipTracks, summary }) => {
+  ({ posts, title, tracks, fellowshipTracks, summary, status }) => {
+    const router = useRouter();
+
     const summaryComponent = <Gov2Summary summary={summary} />;
+
+    function onStatusChange(item) {
+      const q = router.query;
+
+      delete q.page;
+      if (item.value) {
+        q.status = item.value;
+      } else {
+        delete q.status;
+      }
+
+      router.replace({ query: q });
+    }
 
     return (
       <Gov2Page
@@ -23,13 +40,24 @@ export default withLoginUserRedux(
         fellowshipTracks={fellowshipTracks}
         summary={summaryComponent}
         topRightCorner={<StatisticLinkButton href="/referenda/statistics" />}
+        listTitle="List"
+        listTitleExtra={
+          <ReferendaStatusSelectField
+            value={status}
+            onChange={onStatusChange}
+          />
+        }
       />
     );
-  }
+  },
 );
 
 export const getServerSideProps = withLoginUser(async (context) => {
-  const { page = 1, page_size: pageSize = defaultPageSize } = context.query;
+  const {
+    page = 1,
+    page_size: pageSize = defaultPageSize,
+    status = "",
+  } = context.query;
 
   const [
     { result: tracks },
@@ -42,6 +70,7 @@ export const getServerSideProps = withLoginUser(async (context) => {
     ssrNextApi.fetch(gov2ReferendumsApi, {
       page,
       pageSize,
+      status,
     }),
     ssrNextApi.fetch(gov2ReferendumsSummaryApi),
   ]);
@@ -53,6 +82,7 @@ export const getServerSideProps = withLoginUser(async (context) => {
       tracks: tracks ?? [],
       fellowshipTracks: fellowshipTracks ?? [],
       summary: summary ?? {},
+      status,
     },
   };
 });
