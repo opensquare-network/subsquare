3f2c7ca0c36c07e9cb4c531ac282dd2912da2d01
2nthony <ant.hu@outlook.com>
2023-06-06 17:04:01 +0800

    fulfill colors

--- packages/next-common/components/styled/theme/dark.js
+++ packages/next-common/components/styled/theme/dark.js
@@ -1,10 +1,60 @@
 import light from "./light";
 
 const dark = {
+  ...light,
   isDark: true,
-  textPrimary: "#FFFFFF",
-  textSecondary: "rgba(255, 255, 255, 0.5)",
-  textTertiary: "rgba(255, 255, 255, 0.2)",
+
+  // text
+  textPrimary: "rgba(255,255,255,1)",
+  textSecondary: "rgba(255,255,255,0.500)",
+  textTertiary: "rgba(255,255,255,0.200)",
+  textDisabled: "rgba(255,255,255,0.150)",
+  textPrimaryContrast: "rgba(255,255,255,1)",
+  textSecondaryContrast: "rgba(255,255,255,0.600)",
+  textTertiaryContrast: "rgba(255,255,255,0.300)",
+  textDisabledContrast: "rgba(255,255,255,0.150)",
+  // color
+  purple100: "rgba(104,72,255,0.100)",
+  purple300: "rgba(104,72,255,0.400)",
+  purple500: "rgba(104,72,255,1)",
+  green100: "rgba(76,175,80,0.100)",
+  green300: "rgba(76,175,80,0.400)",
+  green500: "rgba(76,175,80,1)",
+  orange100: "rgba(255,152,0,0.100)",
+  orange300: "rgba(255,152,0,0.400)",
+  orange500: "rgba(255,152,0,1)",
+  red100: "rgba(244,67,54,0.100)",
+  red300: "rgba(244,67,54,0.400)",
+  red500: "rgba(244,67,54,1)",
+  azure100: "rgba(33,150,243,0.100)",
+  azure300: "rgba(33,150,243,0.400)",
+  azure500: "rgba(33,150,243,1)",
+  blue100: "rgba(15,111,255,0.100)",
+  blue300: "rgba(15,111,255,0.400)",
+  blue500: "rgba(15,111,255,1)",
+  sapphire100: "rgba(31,112,199,0.100)",
+  sapphire300: "rgba(31,112,199,0.400)",
+  sapphire500: "rgba(31,112,199,1)",
+  // neutral
+  neutral100: "rgba(33,36,51,1)",
+  neutral200: "rgba(29,30,44,1)",
+  neutral300: "rgba(39,42,58,1)",
+  neutral400: "rgba(54,58,77,1)",
+  neutral500: "rgba(87,92,114,1)",
+  // other
+  tooltipBg: "rgba(0,0,0,0.800)",
+  // customized
+  // kusama
+  theme100: "rgba(230,0,122,0.100)",
+  theme300: "rgba(230,0,122,0.400)",
+  theme500: "rgba(230,0,122,1)",
+  navigationBg: "rgba(33,36,51,1)",
+  navigationActive: "rgba(39,42,58,1)",
+  navigationBorder: "rgba(39,42,58,1)",
+  // shadow
+
+  // OLD
+
   textPlaceholder: "rgba(255, 255, 255, 0.15)",
   textContrast: light.textContrast,
   primaryPurple100: light.primaryPurple100,
--- packages/next-common/components/styled/theme/light.js
+++ packages/next-common/components/styled/theme/light.js
@@ -1,8 +1,57 @@
 const light = {
   isDark: false,
-  textPrimary: "#1E2134",
-  textSecondary: "#506176",
-  textTertiary: "#9DA9BB",
+
+  // text
+  textPrimary: "rgba(30,33,52,1)",
+  textSecondary: "rgba(80,97,118,1)",
+  textTertiary: "rgba(157,169,187,1)",
+  textDisabled: "rgba(194,200,212,1)",
+  textPrimaryContrast: "rgba(255,255,255,1)",
+  textSecondaryContrast: "rgba(255,255,255,0.600)",
+  textTertiaryContrast: "rgba(255,255,255,0.300)",
+  textDisabledContrast: "rgba(255,255,255,0.150)",
+  // color
+  purple100: "rgba(104,72,255,0.100)",
+  purple300: "rgba(104,72,255,0.400)",
+  purple500: "rgba(104,72,255,1)",
+  green100: "rgba(76,175,80,0.100)",
+  green300: "rgba(76,175,80,0.400)",
+  green500: "rgba(76,175,80,1)",
+  orange100: "rgba(255,152,0,0.100)",
+  orange300: "rgba(255,152,0,0.400)",
+  orange500: "rgba(255,152,0,1)",
+  red100: "rgba(244,67,54,0.100)",
+  red300: "rgba(244,67,54,0.400)",
+  red500: "rgba(244,67,54,1)",
+  azure100: "rgba(33,150,243,0.100)",
+  azure300: "rgba(33,150,243,0.400)",
+  azure500: "rgba(33,150,243,1)",
+  blue100: "rgba(15,111,255,0.100)",
+  blue300: "rgba(15,111,255,0.400)",
+  blue500: "rgba(15,111,255,1)",
+  sapphire100: "rgba(31,112,199,0.100)",
+  sapphire300: "rgba(31,112,199,0.400)",
+  sapphire500: "rgba(31,112,199,1)",
+  //neutral
+  neutral100: "rgba(255,255,255,1)",
+  neutral200: "rgba(246,247,250,1)",
+  neutral300: "rgba(235,238,244,1)",
+  neutral400: "rgba(224,228,235,1)",
+  neutral500: "rgba(194,200,212,1)",
+  // other
+  tooltipBg: "rgba(0,0,0,0.800)",
+  // customized
+  // kusama
+  theme100: "rgba(230,0,122,0.100)",
+  theme300: "rgba(230,0,122,0.400)",
+  theme500: "rgba(230,0,122,1)",
+  navigationBg: "rgba(0,0,0,1)",
+  navigationActive: "rgba(20,20,20,1)",
+  navigationBorder: "rgba(38,38,38,1)",
+  // shadow
+
+  // OLD
+
   textPlaceholder: "#D7DEE8",
   textContrast: "#FFFFFF",
   primaryPurple100: "rgba(104, 72, 255, 0.1)",
