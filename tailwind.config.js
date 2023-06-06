953a5d6509eb1634e6b6273d420dcaeb35d55273
2nthony <ant.hu@outlook.com>
2023-06-06 12:13:01 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 const path = require("node:path");
+const colors = require("tailwindcss/colors");
 
 const resolve = (dir) => path.resolve(__dirname, dir);
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     resolve("./packages/next/**/*.{html,js,jsx}"),
     resolve("./packages/kintsugi-next/**/*.{html,js,jsx}"),
@@ -11,6 +13,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      // TODO: v2, fulfill colors
+      neutral: {
+        100: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
