e1ca4d85802803ebfbe2e450bee5cd1d72be7a18
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-11 21:44:03 +0800

    Set bifrost elections to false, #2993

--- packages/next-common/utils/consts/settings/bifrostPolkadot.js
+++ packages/next-common/utils/consts/settings/bifrostPolkadot.js
@@ -22,7 +22,7 @@ const bifrostPolkadot = {
   identity: "bifrost-polkadot",
   symbol: "bnc",
   decimals: 12,
-  hasElections: true,
+  hasElections: false,
   ss58Format: 6,
   snsCoverCid: "bafybeicykjzlwi3rjs637txy4fn2m6qbjofjwlypgmthbbb7i3f522xjzy",
   endpoints: DEFAULT_NODES,
