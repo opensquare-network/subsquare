d298a0ca6b29169f09a101d8a2967707b43300a4
chaojun <chaojun@opensquare.network>
2023-06-14 15:18:17 +0800

    fix: menu

--- packages/next/pages/open-techcomm/proposals.js
+++ packages/next/pages/open-techcomm/proposals.js
@@ -6,8 +6,9 @@ import HomeLayout from "next-common/components/layout/HomeLayout";
 import { useChain } from "next-common/context/chain";
 import businessCategory from "next-common/utils/consts/business/category";
 import normalizeOpenTechCommProposalListItem from "next-common/utils/viewfuncs/collective/normalizeOpenTechCommProposalListItem";
+import { fellowshipTracksApi, gov2TracksApi } from "next-common/services/url";
 
-export default withLoginUserRedux(({ motions }) => {
+export default withLoginUserRedux(({ tracks, fellowshipTracks, motions }) => {
   const chain = useChain();
   const items = (motions.items || []).map((item) =>
     normalizeOpenTechCommProposalListItem(chain, item)
@@ -16,7 +17,11 @@ export default withLoginUserRedux(({ motions }) => {
   const seoInfo = { title: category, desc: category };
 
   return (
-    <HomeLayout seoInfo={seoInfo}>
+    <HomeLayout
+      seoInfo={seoInfo}
+      tracks={tracks}
+      fellowshipTracks={fellowshipTracks}
+    >
       <PostList
         category={category}
         items={items}
@@ -33,6 +38,11 @@ export default withLoginUserRedux(({ motions }) => {
 export const getServerSideProps = withLoginUser(async (context) => {
   const { page, page_size: pageSize } = context.query;
 
+  const [{ result: tracks }, { result: fellowshipTracks }] = await Promise.all([
+    nextApi.fetch(gov2TracksApi),
+    nextApi.fetch(fellowshipTracksApi),
+  ]);
+
   const [{ result: motions }] = await Promise.all([
     nextApi.fetch("open-techcomm/motions", {
       page: page ?? 1,
@@ -42,6 +52,8 @@ export const getServerSideProps = withLoginUser(async (context) => {
 
   return {
     props: {
+      tracks: tracks ?? [],
+      fellowshipTracks: fellowshipTracks ?? [],
       motions: motions ?? EmptyList,
     },
   };
