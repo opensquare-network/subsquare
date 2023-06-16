18db14d5a4479342f718fe8e707f625eebdcb116
chaojun <chaojun@opensquare.network>
2023-06-16 11:35:35 +0800

    Add fellowship to collectives chain

--- packages/next-common/components/summary/allianceOverviewSummary/index.js
+++ packages/next-common/components/summary/allianceOverviewSummary/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import Summary from "next-common/components/summary/summaryBase";
 
 export default function AllianceOverviewSummary({ summaryData }) {
-  const { activeAllianceMotionsCount, activeAllianceAnnouncementsCount } =
+  const { activeAllianceMotionsCount, activeAllianceAnnouncementsCount, activeFellowshipReferendaCount } =
     summaryData;
   return (
     <Summary
       description="Active proposal numbers of various governance processes."
       items={[
+        { title: "Fellowship", content: activeFellowshipReferendaCount },
         { title: "Motions", content: activeAllianceMotionsCount || 0 },
         {
           title: "Announcements",
