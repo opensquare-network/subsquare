5eb30271189dd20b59885ff5248023edc05ab7a7
2nthony <ant.hu@outlook.com>
2023-06-16 10:51:48 +0800

    feat: kintsugi new home page

--- packages/kintsugi-next/pages/index.js
+++ packages/kintsugi-next/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Overview from "next-common/components/overview";
+import OverviewV2 from "next-common/components/overview/v2";
 import { withLoginUser, withLoginUserRedux } from "next-common/lib";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import { toDiscussionListItem } from "utils/viewfuncs";
@@ -8,6 +9,7 @@ import normalizeTechCommMotionListItem from "next-common/utils/viewfuncs/collect
 import normalizeReferendaListItem from "next-common/utils/viewfuncs/democracy/normalizeReferendaListItem";
 import normalizeProposalListItem from "next-common/utils/viewfuncs/democracy/normalizeProposalListItem";
 import normalizeTreasuryProposalListItem from "next-common/utils/viewfuncs/treasury/normalizeProposalListItem";
+import ListLayout from "next-common/components/layout/ListLayout";
 
 export default withLoginUserRedux(({ overview, chain }) => {
   let overviewData = [
@@ -15,48 +17,57 @@ export default withLoginUserRedux(({ overview, chain }) => {
       category: "Discussions",
       link: "/discussions",
       items: (overview?.discussions ?? []).map((item) =>
-        toDiscussionListItem(chain, item)
+        toDiscussionListItem(chain, item),
       ),
     },
     {
       category: "Referenda",
       link: "/democracy/referenda",
       items: (overview?.democracy?.referenda ?? []).map((item) =>
-        normalizeReferendaListItem(chain, item)
+        normalizeReferendaListItem(chain, item),
       ),
     },
     {
       category: "Democracy Public Proposals",
       link: "/democracy/proposals",
       items: (overview?.democracy?.proposals ?? []).map((item) =>
-        normalizeProposalListItem(chain, item)
+        normalizeProposalListItem(chain, item),
       ),
     },
     {
       category: "Treasury Proposals",
       link: "/treasury/proposals",
       items: (overview?.treasury?.proposals ?? []).map((item) =>
-        normalizeTreasuryProposalListItem(chain, item)
+        normalizeTreasuryProposalListItem(chain, item),
       ),
     },
     {
       category: "Tech. Comm. Proposals",
       link: "/techcomm/proposals",
       items: (overview?.techComm?.motions ?? []).map((item) =>
-        normalizeTechCommMotionListItem(chain, item)
+        normalizeTechCommMotionListItem(chain, item),
       ),
     },
   ];
 
   const filteredOverviewData = overviewData.filter(
-    (data) => data?.items?.length > 0 || data?.category === "Discussions"
+    (data) => data?.items?.length > 0 || data?.category === "Discussions",
   );
 
   // Sort the items with length = 0 to the end of the list
   filteredOverviewData.sort((a, b) =>
     a?.items?.length > 0 && b?.items?.length > 0
       ? 0
-      : b?.items?.length - a?.items?.length
+      : b?.items?.length - a?.items?.length,
+  );
+
+  return (
+    <ListLayout>
+      <OverviewV2
+        overviewData={filteredOverviewData}
+        summaryData={overview?.summary}
+      />
+    </ListLayout>
   );
 
   return (
