0e3e1ff607d4354789ec18875a2df69918edfe19
2nthony <ant.hu@outlook.com>
2023-06-19 14:45:00 +0800

    fix: header chainicon dark mode

--- packages/next-common/components/header/chainIcon.js
+++ packages/next-common/components/header/chainIcon.js
@@ -1,16 +1,18 @@
 import { useChain, useChainSettings } from "next-common/context/chain";
 import { useThemeSetting } from "next-common/context/theme";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ChainIcon() {
   const theme = useThemeSetting();
   const chain = useChain();
   const chainSetting = useChainSettings();
+  const [image, setImage] = useState(chainSetting.avatar);
 
-  let image = chainSetting.avatar;
-  if (theme.isDark && chainSetting.darkAvatar) {
-    image = chainSetting.darkAvatar;
-  }
+  useEffect(() => {
+    if (theme.isDark && chainSetting.darkAvatar) {
+      setImage(chainSetting.darkAvatar);
+    }
+  }, [theme.isDark, chainSetting.darkAvatar]);
 
   if (!image) {
     throw new Error(`Can not get icon of ${chain}`);
