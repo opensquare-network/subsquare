6e68f87848f8e8af6960e82f2be1189dc32c1c12
Yongfeng LI <wliyongfeng@gmail.com>
2023-06-19 18:10:54 +0800

    Improve referendum votes query (#3016)
    * Not set loading when OpenGov tally changed
    
    * Reduce useless democracy referendum tally subscription

--- packages/next-common/utils/hooks/referenda/useFetchVotes.js
+++ packages/next-common/utils/hooks/referenda/useFetchVotes.js
@@ -2,27 +2,33 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import extractVoteInfo from "../../democracy/referendum";
 import {
+  clearVotes,
+  democracyVotesTriggerSelector,
   fetchVotes,
+  setIsLoadingVotes,
   votesSelector,
 } from "../../../store/reducers/referendumSlice";
 import useApi from "../useApi";
-import { clearVotes } from "../../../store/reducers/referendumSlice";
 
 export default function useFetchVotes(referendum) {
   const { allAye = [], allNay = [] } = useSelector(votesSelector);
   const { voteFinishedHeight } = extractVoteInfo(referendum?.timeline);
   const referendumIndex = referendum?.referendumIndex;
   const api = useApi();
+  const voterTriggerCount = useSelector(democracyVotesTriggerSelector);
 
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (api) {
+      if (voterTriggerCount <= 1) {
+        dispatch(setIsLoadingVotes(true));
+      }
       dispatch(fetchVotes(api, referendumIndex, voteFinishedHeight));
     }
 
     return () => dispatch(clearVotes());
-  }, [api, dispatch, referendumIndex, voteFinishedHeight]);
+  }, [api, dispatch, referendumIndex, voteFinishedHeight, voterTriggerCount]);
 
   return { allAye, allNay };
 }
