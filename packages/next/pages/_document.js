f5430da97d467bfcdeb57813a2e8c5acb6a1085d
2nthony <ant.hu@outlook.com>
2023-06-19 22:34:05 +0800

    fix: avoid flicker when following system appearance

--- packages/next/pages/_document.js
+++ packages/next/pages/_document.js
@@ -1,5 +1,17 @@
-import Document from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import light from "next-common/styles/light";
+import dark from "next-common/styles/dark";
+
+const chain = process.env.NEXT_PUBLIC_CHAIN;
+
+function buildThemeVariables(theme) {
+  if (!theme) return "";
+
+  return Object.keys(theme)
+    .map((k) => `--${k}: ${theme[k]}`)
+    .join(";");
+}
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -26,5 +38,62 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+                :root {${buildThemeVariables(light.base)}}
+                :root {${buildThemeVariables(light.chain[chain])}}
+                .dark:root {${buildThemeVariables(dark.base)}}
+                .dark:root {${buildThemeVariables(dark.chain[chain])}}
+
+                body {
+                  background-color: var(--neutral200);
+                }
+              `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  var cookie = document.cookie;
+                  var theme_mode = cookie.match(/theme-mode=([^;]+)/);
+                  if (theme_mode) {
+                    theme_mode = theme_mode[1];
+                    if (!theme_mode || theme_mode === "system") {
+                      if ("matchMedia" in window) {
+                        var preferredDark = window.matchMedia(
+                          "(prefers-color-scheme: dark)",
+                        ).matches;
+                        if (preferredDark) {
+                          add_dark();
+                        }
+                      }
+                    } else if (theme_mode === "dark") {
+                      add_dark();
+                    }
+
+                    function add_dark() {
+                      var root = document.documentElement;
+                      root.classList.add("dark");
+                    }
+                  }
+                })()
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
