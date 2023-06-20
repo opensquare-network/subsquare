0d452b7b16dc55e91a79878013182907bc4dd41e
2nthony <ant.hu@outlook.com>
2023-06-20 14:26:02 +0800

    replace sapphire

--- packages/next-common/components/calendar/dayEvents/eventInfoCard/infoItem/proposerItem.js
+++ packages/next-common/components/calendar/dayEvents/eventInfoCard/infoItem/proposerItem.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { useThemeSetting } from "../../../../../context/theme";
 import User from "../../../../user";
 import { ItemWrapper } from "./styled";
 
 export default function ProposerItem({ proposer }) {
-  const theme = useThemeSetting();
   return (
     <ItemWrapper>
       <span>Proposer:</span>
@@ -12,7 +10,7 @@ export default function ProposerItem({ proposer }) {
         add={proposer}
         fontSize={12}
         showAvatar={false}
-        color={theme.secondarySapphire500}
+        color="var(--sapphire500)"
       />
     </ItemWrapper>
   );
