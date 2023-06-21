296d4ce7175b8e61e3462326999b69bb0add977c
2nthony <ant.hu@outlook.com>
2023-06-21 15:58:21 +0800

    feat: postList

--- packages/next-common/components/postList.js
+++ packages/next-common/components/postList.js
@@ -1,17 +1,9 @@
-import React from "react";
 import styled from "styled-components";
 import Post from "next-common/components/post";
 import Pagination from "next-common/components/pagination/index.js";
 import { TitleContainer } from "./styled/containers/titleContainer";
 import { EmptyList } from "./emptyList";
 import Link from "next/link";
-import {
-  flex,
-  gap_x,
-  items_center,
-  justify_between,
-} from "next-common/styles/tailwindcss";
-import { p_14_medium, p_16_bold } from "next-common/styles/componentCss";
 
 const Wrapper = styled.div`
   > :not(:first-child) {
@@ -29,60 +21,34 @@ const TitleLink = styled.a`
   }
 `;
 
-const ListTitleGroup = styled.div`
-  margin: 16px 0;
-  padding-top: 8px;
-  ${flex};
-  ${justify_between};
-  ${items_center};
-`;
-
-const ListTitle = styled.h3`
-  margin: 0;
-  ${gap_x(8)};
-  color: var(--textPrimary);
-  ${p_16_bold};
-`;
-
-const ListTitleCount = styled.small`
-  color: var(--textTertiary);
-  ${p_14_medium};
-  margin-left: 8px;
-`;
-
 export default function PostList({
   title,
+  titleCount = null,
+  titleExtra = null,
   category,
   link,
   items,
   pagination,
   topRightCorner = null,
   summary,
-  listTitle = "",
-  listTitleExtra,
-  listTitleCount = null,
 }) {
   return (
     <Wrapper>
       <TitleContainer>
-        <Link href={link || ""} passHref legacyBehavior>
-          <TitleLink>{title ?? category}</TitleLink>
-        </Link>
-        {topRightCorner}
+        <div>
+          <Link href={link || ""} passHref legacyBehavior>
+            <TitleLink>{title ?? category}</TitleLink>
+          </Link>
+
+          {!!titleCount && (
+            <small className="text-textTertiary ml-2 text14Medium">
+              {titleCount}
+            </small>
+          )}
+        </div>
+        {titleExtra || topRightCorner}
       </TitleContainer>
       {summary}
-
-      {listTitle && (
-        <ListTitleGroup>
-          <ListTitle>
-            {listTitle}
-            {!!listTitleCount && (
-              <ListTitleCount>{listTitleCount}</ListTitleCount>
-            )}
-          </ListTitle>
-          {listTitleExtra}
-        </ListTitleGroup>
-      )}
 
       {items?.length > 0 ? (
         items.map((item, index) => (
