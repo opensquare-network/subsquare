ba7892cb6ba3a653b4102ff1e58615e54321b866
2nthony <ant.hu@outlook.com>
2023-06-21 11:30:58 +0800

    feat: page discussions layout

--- packages/next/pages/polkassembly/discussions.js
+++ packages/next/pages/polkassembly/discussions.js
@@ -2,39 +2,33 @@ import PostList from "next-common/components/postList";
 import { defaultPageSize, EmptyList } from "next-common/utils/constants";
 import { withLoginUser, withLoginUserRedux } from "next-common/lib";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
-import HomeLayout from "next-common/components/layout/HomeLayout";
 import businessCategory from "next-common/utils/consts/business/category";
 import normalizePolkassemblyDiscussionListItem from "next-common/utils/viewfuncs/discussion/normalizePaListItem";
 import { fellowshipTracksApi, gov2TracksApi } from "next-common/services/url";
+import ListLayout from "next-common/components/layout/ListLayout";
 
-export default withLoginUserRedux(
-  ({ posts, chain, tracks, fellowshipTracks }) => {
-    const items = (posts.items || []).map((item) =>
-      normalizePolkassemblyDiscussionListItem(chain, item)
-    );
+export default withLoginUserRedux(({ posts, chain }) => {
+  const items = (posts.items || []).map((item) =>
+    normalizePolkassemblyDiscussionListItem(chain, item),
+  );
 
-    const category = businessCategory.polkassemblyDiscussions;
-    const seoInfo = { title: category, desc: category };
+  const category = businessCategory.polkassemblyDiscussions;
+  const seoInfo = { title: category, desc: category };
 
-    return (
-      <HomeLayout
-        seoInfo={seoInfo}
-        tracks={tracks}
-        fellowshipTracks={fellowshipTracks}
-      >
-        <PostList
-          category={category}
-          items={items}
-          pagination={{
-            page: posts.page,
-            pageSize: posts.pageSize,
-            total: posts.total,
-          }}
-        />
-      </HomeLayout>
-    );
-  }
-);
+  return (
+    <ListLayout seoInfo={seoInfo}>
+      <PostList
+        category={category}
+        items={items}
+        pagination={{
+          page: posts.page,
+          pageSize: posts.pageSize,
+          total: posts.total,
+        }}
+      />
+    </ListLayout>
+  );
+});
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const chain = process.env.CHAIN;
