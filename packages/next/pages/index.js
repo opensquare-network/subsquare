6daad310494ba843d5c763c140fffce61fb0940b
2nthony <ant.hu@outlook.com>
2023-06-21 18:38:29 +0800

    add fixme comment

--- packages/next/pages/index.js
+++ packages/next/pages/index.js
@@ -198,6 +198,7 @@ export default withLoginUserRedux(({ overview, tracks, fellowshipTracks }) => {
   return (
     <ListLayout
       title={chainSettings.name}
+      // FIXME: v2, chain description
       description={"{chainSettings.description}"}
       headContent={<ChainSocialLinks />}
       summary={<SummaryComponent summaryData={overview?.summary} />}
