bc42a37ee39d24528a0267842441487d864ef47c
2nthony <ant.hu@outlook.com>
2023-06-25 18:00:21 +0800

    feat: techcomm pages layout

--- packages/next/pages/techcomm/members.js
+++ packages/next/pages/techcomm/members.js
@@ -4,13 +4,16 @@ import useApi from "next-common/utils/hooks/useApi";
 import useCall from "next-common/utils/hooks/useCall";
 import usePrime from "next-common/utils/hooks/usePrime";
 import { detailPageCategory } from "next-common/utils/consts/business/category";
-import HomeLayout from "next-common/components/layout/HomeLayout";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import { fellowshipTracksApi, gov2TracksApi } from "next-common/services/url";
+import ListLayout from "next-common/components/layout/ListLayout";
 
-export default withLoginUserRedux(({ tracks, fellowshipTracks }) => {
+export default withLoginUserRedux(({}) => {
   const api = useApi();
-  const [members, loadingMembers] = useCall(api?.derive?.technicalCommittee?.members, []);
+  const [members, loadingMembers] = useCall(
+    api?.derive?.technicalCommittee?.members,
+    [],
+  );
   const prime = usePrime({ type: detailPageCategory.TECH_COMM_MOTION });
 
   const data = members?.toJSON() || [];
@@ -19,18 +22,9 @@ export default withLoginUserRedux(({ tracks, fellowshipTracks }) => {
   const seoInfo = { title: category, desc: category };
 
   return (
-    <HomeLayout
-      seoInfo={seoInfo}
-      tracks={tracks}
-      fellowshipTracks={fellowshipTracks}
-    >
-      <MembersList
-        prime={prime}
-        category={category}
-        items={data}
-        loading={loadingMembers}
-      />
-    </HomeLayout>
+    <ListLayout seoInfo={seoInfo} title={category} description="Description">
+      <MembersList prime={prime} items={data} loading={loadingMembers} />
+    </ListLayout>
   );
 });
 
