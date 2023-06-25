2d89a0d80659073ff18749cb9a2f8aaded860f9b
2nthony <ant.hu@outlook.com>
2023-06-25 17:49:16 +0800

    feat: council motion layout

--- packages/next/pages/council/motions.js
+++ packages/next/pages/council/motions.js
@@ -3,37 +3,37 @@ import { defaultPageSize, EmptyList } from "next-common/utils/constants";
 import { withLoginUser, withLoginUserRedux } from "next-common/lib";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import businessCategory from "next-common/utils/consts/business/category";
-import HomeLayout from "next-common/components/layout/HomeLayout";
 import normalizeCouncilMotionListItem from "next-common/utils/viewfuncs/collective/normalizeCouncilMotionListItem";
 import { fellowshipTracksApi, gov2TracksApi } from "next-common/services/url";
+import ListLayout from "next-common/components/layout/ListLayout";
 
-export default withLoginUserRedux(
-  ({ motions, chain, tracks, fellowshipTracks }) => {
-    const items = (motions.items || []).map((item) =>
-      normalizeCouncilMotionListItem(chain, item),
-    );
-    const category = businessCategory.councilMotions;
-    const seoInfo = { title: category, desc: category };
+export default withLoginUserRedux(({ motions, chain }) => {
+  const items = (motions.items || []).map((item) =>
+    normalizeCouncilMotionListItem(chain, item),
+  );
+  const category = businessCategory.councilMotions;
+  const seoInfo = { title: category, desc: category };
 
-    return (
-      <HomeLayout
-        seoInfo={seoInfo}
-        tracks={tracks}
-        fellowshipTracks={fellowshipTracks}
-      >
-        <PostList
-          category={category}
-          items={items}
-          pagination={{
-            page: motions.page,
-            pageSize: motions.pageSize,
-            total: motions.total,
-          }}
-        />
-      </HomeLayout>
-    );
-  },
-);
+  return (
+    <ListLayout
+      seoInfo={seoInfo}
+      title={category}
+      description="Council motions"
+    >
+      <PostList
+        category={category}
+        title="List"
+        titleCount={motions.total}
+        items={items}
+        pagination={{
+          page: motions.page,
+          pageSize: motions.pageSize,
+          total: motions.total,
+        }}
+      />
+    </ListLayout>
+  );
+});
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const chain = process.env.CHAIN;
