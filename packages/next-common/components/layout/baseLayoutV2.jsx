3e4d5aea3987b33a2f6d3bfcdbe3f806fafa1de8
2nthony <ant.hu@outlook.com>
2023-06-26 15:08:41 +0800

    fix: base layout init block hooks

--- packages/next-common/components/layout/baseLayoutV2.jsx
+++ packages/next-common/components/layout/baseLayoutV2.jsx
@@ -6,12 +6,53 @@ import Nav from "../nav";
 import SEO from "../SEO";
 import Toast from "../toast";
 import Footer from "./footer/v2";
+import useApi from "next-common/utils/hooks/useApi";
+import {
+  useBlockTime,
+  useChainHeight,
+  useSubscribeChainHead,
+} from "next-common/utils/hooks";
+import { useDispatch } from "react-redux";
+import { useIsMountedBool } from "next-common/utils/hooks/useIsMounted";
+import useUpdateNodesDelay from "next-common/utils/hooks/useUpdateNodesDelay";
+import { useEffect } from "react";
+import {
+  setBlockTime,
+  setLatestHeight,
+  setNowHeight,
+} from "next-common/store/reducers/chainSlice";
 
 /**
  * @description a base layout includes nav, header and footer
  */
 export default function BaseLayout({ children, seoInfo = {} }) {
   const { sm } = useScreenSize();
+
+  const api = useApi();
+  const blockTime = useBlockTime(api);
+  const latestHeight = useSubscribeChainHead(api);
+  const nowHeight = useChainHeight(api);
+
+  const dispatch = useDispatch();
+  const isMounted = useIsMountedBool();
+
+  useUpdateNodesDelay();
+
+  useEffect(() => {
+    if (blockTime && isMounted()) {
+      dispatch(setBlockTime(blockTime.toNumber()));
+    }
+  }, [blockTime, dispatch, isMounted]);
+
+  useEffect(() => {
+    if (latestHeight && isMounted()) {
+      dispatch(setLatestHeight(latestHeight));
+    }
+  }, [latestHeight, dispatch, isMounted]);
+
+  useEffect(() => {
+    dispatch(setNowHeight(nowHeight));
+  }, [nowHeight, dispatch]);
 
   return (
     <>
