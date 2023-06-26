92ba4555253d09d31a1a4ed63bbd4481ccfb7c6f
2nthony <ant.hu@outlook.com>
2023-06-26 15:25:12 +0800

    feat: kintsugi treasury proposals layout

--- packages/kintsugi-next/pages/treasury/proposals.js
+++ packages/kintsugi-next/pages/treasury/proposals.js
@@ -1,79 +1,9 @@
-import PostList from "next-common/components/postList";
 import { EmptyList } from "next-common/utils/constants";
-import { withLoginUser, withLoginUserRedux } from "next-common/lib";
+import { withLoginUser } from "next-common/lib";
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
-import Summary from "next-common/components/summary";
-import PlusIcon from "public/imgs/icons/plusInCircle.svg";
-import { useCallback, useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-import { Create } from "next-common/components/treasury/common/styled";
-import businessCategory from "next-common/utils/consts/business/category";
-import HomeLayout from "next-common/components/layout/HomeLayout";
-import useWaitSyncBlock from "next-common/utils/hooks/useWaitSyncBlock";
-import useIsMounted from "next-common/utils/hooks/useIsMounted";
-import normalizeTreasuryProposalListItem from "next-common/utils/viewfuncs/treasury/normalizeProposalListItem";
+import TreasuryProposalsPage from "@subsquare/next/pages/treasury/proposals";
 
-const Popup = dynamic(
-  () => import("next-common/components/treasury/proposal/popup"),
-  {
-    ssr: false,
-  },
-);
-
-export default withLoginUserRedux(({ proposals: ssrProposals, chain }) => {
-  const [showPopup, setShowPopup] = useState(false);
-  const [proposals, setProposals] = useState(ssrProposals);
-  useEffect(() => setProposals(ssrProposals), [ssrProposals]);
-  const isMounted = useIsMounted();
-
-  const items = (proposals.items || []).map((item) =>
-    normalizeTreasuryProposalListItem(chain, item),
-  );
-
-  const create = (
-    <Create onClick={() => setShowPopup(true)}>
-      <PlusIcon />
-      New Proposal
-    </Create>
-  );
-
-  const refreshPageData = useCallback(async () => {
-    const { result } = await nextApi.fetch("treasury/proposals");
-    if (result && isMounted.current) {
-      setProposals(result);
-    }
-  }, [isMounted]);
-
-  const onProposeFinalized = useWaitSyncBlock(
-    "Proposal proposed",
-    refreshPageData,
-  );
-
-  const category = businessCategory.treasuryProposals;
-  const seoInfo = { title: category, desc: category };
-
-  return (
-    <HomeLayout seoInfo={seoInfo}>
-      <PostList
-        category={category}
-        topRightCorner={create}
-        items={items}
-        summary={<Summary />}
-        pagination={{
-          page: proposals.page,
-          pageSize: proposals.pageSize,
-          total: proposals.total,
-        }}
-      />
-      {showPopup && (
-        <Popup
-          onClose={() => setShowPopup(false)}
-          onFinalized={onProposeFinalized}
-        />
-      )}
-    </HomeLayout>
-  );
-});
+export default TreasuryProposalsPage;
 
 export const getServerSideProps = withLoginUser(async (context) => {
   const chain = process.env.CHAIN;
