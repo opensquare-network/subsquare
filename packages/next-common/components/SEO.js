f795910fadf1d634bd34cdacd974a87a93d64fde
2nthony <ant.hu@outlook.com>
2023-06-28 15:08:24 +0800

    fix: seo title

--- packages/next-common/components/SEO.js
+++ packages/next-common/components/SEO.js
@@ -1,18 +1,17 @@
-import React from "react";
 import { useRouter } from "next/router";
 import NextSeo from "./nextSeo";
-import { useChainSettings, useChain } from "../context/chain";
+import { useChainSettings } from "../context/chain";
 import getIpfsLink from "../utils/env/ipfsEndpoint";
 import { DEFAULT_SEO_INFO } from "next-common/utils/constants";
-import { capitalize } from "lodash";
+import usePageTitle from "next-common/hooks/usePageTitle";
 
 export default function SEO({ title: titleProp, desc, ogImage }) {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
   const route = useRouter();
-  const chain = useChain();
 
-  const title =
-    titleProp || `SubSquare | ${capitalize(chain)} governance platform`;
+  const title = `SubSquare | ${usePageTitle(
+    titleProp || "governance platform",
+  )}`;
   const description = desc || DEFAULT_SEO_INFO.desc;
   const settings = useChainSettings();
 
