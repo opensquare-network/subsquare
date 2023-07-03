09e599d1c0bd01968ef638462855c8377317ee43
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-03 17:07:04 +0800

    Add treasury award count down component, #3055

---
+++ packages/next-common/components/detail/treasury/proposal/awardCountDown.js
@@ -0,0 +1,61 @@
+import useApi from "next-common/utils/hooks/useApi";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { latestHeightSelector } from "next-common/store/reducers/chainSlice";
+import BigNumber from "bignumber.js";
+import { useEstimateBlocksTime } from "next-common/utils/hooks";
+import { bigNumber2Locale } from "next-common/utils";
+import CountDown from "next-common/components/_CountDown";
+import styled from "styled-components";
+import { NoticeWrapper } from "next-common/components/styled/containers/titleContainer";
+import useCall from "next-common/utils/hooks/useCall";
+import { useOnchainData } from "next-common/context/post";
+
+const Wrapper = styled(NoticeWrapper)`
+  position: static;
+  height: 38px;
+  
+  gap: 8px;
+`;
+
+export default function TreasuryAwardCountDown() {
+  const api = useApi();
+  const blockHeight = useSelector(latestHeightSelector);
+  const [period, setPeriod] = useState(null);
+  const [gone, setGone] = useState(null);
+  const estimatedBlocksTime = useEstimateBlocksTime(period - gone);
+  const [rawApprovals] = useCall(api?.query?.treasury?.approvals, []);
+  const { proposalIndex } = useOnchainData();
+
+  useEffect(() => {
+    if (!api || !api.consts.treasury) {
+      return;
+    }
+
+    setPeriod(api.consts.treasury.spendPeriod.toNumber());
+  }, [api]);
+
+  useEffect(() => {
+    if (period && blockHeight) {
+      setGone(new BigNumber(blockHeight).mod(period).toNumber());
+    }
+  }, [blockHeight, period]);
+
+  if (!gone) {
+    return null;
+  }
+
+  if (!rawApprovals || !(rawApprovals.toJSON() || []).includes(proposalIndex)) {
+    return;
+  }
+
+  const tooltipContent = `Next award in ${ estimatedBlocksTime }, #${ bigNumber2Locale(new BigNumber(blockHeight + period - gone).toString()) }`;
+  return <Wrapper>
+    <CountDown
+      numerator={gone}
+      denominator={period}
+      tooltipContent={tooltipContent}
+    />
+    <span>{tooltipContent}</span>
+  </Wrapper>;
+}
--- packages/next-common/components/detail/treasury/proposal/index.js
+++ packages/next-common/components/detail/treasury/proposal/index.js
@@ -6,12 +6,14 @@ import { usePost } from "../../../../context/post";
 import TreasuryProposalNavigation from "../../navigation/treasuryProposalNavigation";
 import useSetEdit from "../../common/hooks/useSetEdit";
 import TreasuryProposalPostMeta from "./meta";
+import TreasuryAwardCountDown from "next-common/components/detail/treasury/proposal/awardCountDown";
 
 export default function TreasuryProposalDetail({ onReply }) {
   const post = usePost();
   const setIsEdit = useSetEdit();
 
   return <DetailContentBase>
+    <TreasuryAwardCountDown />
     <TreasuryProposalNavigation />
     <PostTitle />
     <TreasuryProposalPostMeta />
