47b955cc8fb9c330db49e8a3b116ef37e5802a4b
2nthony <ant.hu@outlook.com>
2023-07-07 23:26:33 +0800

    fix: detail layout!

--- packages/next-common/components/layout/DetailLayoutV2.jsx
+++ packages/next-common/components/layout/DetailLayoutV2.jsx
@@ -21,11 +21,11 @@ import Breadcrumb from "../_Breadcrumb";
  * breadcrumbs: Breadcrumb[]
  * seoInfo?: SeoInfo
  * children: JSX.Element
- * side: JSX.Element
  * title: JSX.Element
  * titleHead: JSX.Element
- * head: JSX.Element
- * meta: JSX.Element
+ * header: JSX.Element
+ * footer: JSX.Element
+ * sider: JSX.Element
  * }} DetailLayoutProps
  */
 
@@ -35,33 +35,45 @@ import Breadcrumb from "../_Breadcrumb";
 export default function DetailLayout({
   seoInfo,
   breadcrumbs,
-  side,
-  head,
+  header,
+  footer,
+  sider,
   children,
 }) {
   return (
     <BaseLayout seoInfo={seoInfo}>
-      <div className={clsx("bg-neutral100 px-6", "max-sm:px-0")}>
-        <div className={clsx("mx-auto max-w-[1200px] w-full py-6")}>
+      <div
+        className={clsx(
+          "bg-neutral100 px-6 flex-1 flex flex-col",
+          "max-sm:px-0",
+        )}
+      >
+        <div className={clsx("mx-auto py-6 max-w-[1200px] w-full")}>
           {!!breadcrumbs?.length && (
             <div className="mb-6 px-6">
               <Breadcrumb items={breadcrumbs} />
             </div>
           )}
 
-          {head && <div className="px-6">{head}</div>}
+          {header && <div className="px-6">{header}</div>}
 
           <div className="flex gap-x-6 mt-6 max-w-full">
             <div
               className={clsx(
-                side && "max-w-[calc(100%-320px-24px)]",
+                sider ? "max-w-[calc(100%-320px-24px)]" : "max-w-full",
                 "max-sm:max-w-full",
               )}
             >
               {children}
+
+              {sider && <div className={clsx("mt-6 sm:hidden")}>{sider}</div>}
+
+              {footer}
             </div>
 
-            {side && <div className={clsx("w-80 px-6")}>{side}</div>}
+            {sider && (
+              <div className={clsx("w-80 px-6 max-sm:hidden")}>{sider}</div>
+            )}
           </div>
         </div>
       </div>
