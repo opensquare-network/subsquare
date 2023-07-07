da598beb86c3c65d5ca58632d3a4822efd13d184
2nthony <ant.hu@outlook.com>
2023-07-07 16:07:52 +0800

    style: Update formItem.js component formatting

--- packages/next-common/components/post/advanced/formItem.js
+++ packages/next-common/components/post/advanced/formItem.js
@@ -1,23 +1,33 @@
-import React from "react";
-import {
-  FormItemWrapper,
-  FormLabel,
-  FormLabelWrapper,
-  FormExternalLabel,
-} from "./elements";
+import clsx from "clsx";
 
-function FormItem({ label, labelExternal, children }) {
+/**
+ * Render a form item with a label, optional extra label, and children.
+ *
+ * @param {Object} props - The props object.
+ * @param {string} props.label - The label for the form item.
+ * @param {string} props.className - The className for the form item.
+ * @param {string | JSX.Element} [props.labelExtra] - An optional extra label for the form item.
+ * @param {string | JSX.Element} [props.labelExternal] - An optional extra label for the form item.
+ * @param {JSX.Element} props.children - The children of the form item.
+ * @return {JSX.Element} - The rendered form item.
+ */
+function FormItem({
+  label,
+  labelExternal,
+  labelExtra,
+  children,
+  className = "",
+}) {
   return (
-    <FormItemWrapper>
-      <FormLabelWrapper>
-        <FormLabel>{label}</FormLabel>
-        {labelExternal && (
-          <FormExternalLabel>{labelExternal}</FormExternalLabel>
+    <div className={clsx("mb-4 last:mb-0", className)}>
+      <div className="flex items-end justify-between mb-4 px-6">
+        <div className="text16Bold text-textPrimary">{label}</div>
+        {(labelExtra || labelExternal) && (
+          <div className="text12Bold">{labelExtra || labelExternal}</div>
         )}
-      </FormLabelWrapper>
-
+      </div>
       {children}
-    </FormItemWrapper>
+    </div>
   );
 }
 
