c8b876d079c129ba5bad484f5fe7813d353b3e89
2nthony <ant.hu@outlook.com>
2023-07-10 13:59:11 +0800

    feat: referenda detail layout

--- packages/next-common/components/detail/referenda/index.js
+++ packages/next-common/components/detail/referenda/index.js
@@ -1,26 +1,15 @@
-import React from "react";
 import { usePost } from "../../../context/post";
 import DetailContentBase from "../common/detailBase";
-import PostTitle from "../common/Title";
 import ArticleContent from "../../articleContent";
-import ReferendaReferendumNavigation from "../navigation/referendaReferendumNavigation";
 import useSetEdit from "../common/hooks/useSetEdit";
-import ReferendaPostMeta from "../common/openGov/meta";
-import ReferendaWhiteListNavigation from "./whitelistNavigation";
 
 export default function ReferendaDetail({ onReply }) {
   const post = usePost();
   const setIsEdit = useSetEdit();
 
-  return <DetailContentBase>
-    <ReferendaWhiteListNavigation />
-    <ReferendaReferendumNavigation />
-    <PostTitle />
-    <ReferendaPostMeta />
-    <ArticleContent
-      post={post}
-      onReply={onReply}
-      setIsEdit={setIsEdit}
-    />
-  </DetailContentBase>;
+  return (
+    <DetailContentBase>
+      <ArticleContent post={post} onReply={onReply} setIsEdit={setIsEdit} />
+    </DetailContentBase>
+  );
 }
