e90587af755fe57f20aa8cbf9ee787e97aab144f
2nthony <ant.hu@outlook.com>
2023-07-10 11:20:43 +0800

    add `prose` to article content

--- packages/next-common/components/articleContent.js
+++ packages/next-common/components/articleContent.js
@@ -89,7 +89,7 @@ export default function ArticleContent({ votes, myVote, onReply, setIsEdit }) {
   const bannerUrl = getBannerUrl(post.bannerCid);
 
   return (
-    <Wrapper>
+    <Wrapper className="prose">
       {!post.content && (
         <NonEdited
           setIsEdit={setIsEdit}
