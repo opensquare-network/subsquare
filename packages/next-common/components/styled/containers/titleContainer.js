b92f18a173ad86ed0a88a08a6f7868334c7e6989
2nthony <ant.hu@outlook.com>
2023-07-10 16:13:17 +0800

    fix: notice wrapper font size

--- packages/next-common/components/styled/containers/titleContainer.js
+++ packages/next-common/components/styled/containers/titleContainer.js
@@ -46,8 +46,12 @@ export const NoticeWrapper = styled(GreyPanel)`
   flex-wrap: wrap;
   padding: 12px;
   margin-bottom: 16px;
-  font-weight: 500;
   color: var(--textSecondary);
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px;
 `;
 
 export const SettingSection = styled.div`
