8be25d678b33c42c734b79450acc48cb2512a323
2nthony <ant.hu@outlook.com>
2023-07-11 16:47:04 +0800

    fix: popup nested close

--- packages/next-common/components/popup/baseVotesPopup.js
+++ packages/next-common/components/popup/baseVotesPopup.js
@@ -1,16 +1,9 @@
-import React from "react";
-import { max_w_full } from "next-common/styles/tailwindcss";
-import styled from "styled-components";
 import Popup from "./wrapper/Popup";
-
-const StyledPopup = styled(Popup)`
-  width: 640px;
-  ${max_w_full};
-`;
+import clsx from "clsx";
 
 /**
  * @param {Parameters<Popup>[0]} props
  */
 export default function BaseVotesPopup(props) {
-  return <StyledPopup {...props} />;
+  return <Popup {...props} className={clsx("!w-[640px]", props.className)} />;
 }
--- packages/next-common/components/popup/wrapper/Popup.js
+++ packages/next-common/components/popup/wrapper/Popup.js
@@ -1,96 +1,63 @@
-import React, { useEffect, useRef } from "react";
-import styled, { css } from "styled-components";
-import Background from "../../styled/backgroundShade";
-import ClosePanelIcon from "../../../assets/imgs/icons/close-panel.svg";
-import { emptyFunction } from "../../../utils";
-import { useScrollLock } from "../../../utils/hooks/useScrollLock";
+import { useEffect, useState } from "react";
 import { NeutralPanel } from "../../styled/containers/neutralPanel";
-import { useEscapeKeyOnce } from "next-common/utils/hooks/useEscapeKeyOnce";
-import { breakpoint } from "../../../utils/responsive";
-import { w_full } from "../../../styles/tailwindcss";
-import useOnClickOutside from "next-common/utils/hooks/useOnClickOutside";
+import * as Dialog from "@radix-ui/react-dialog";
+import noop from "lodash.noop";
+import clsx from "clsx";
+import { SystemClose } from "@osn/icons/subsquare";
 
-const Wrapper = styled(NeutralPanel)`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  margin-top: 0 !important;
-  width: 400px;
-  ${(p) =>
-    p.wide &&
-    css`
-      @media screen and (min-width: 768px) {
-        width: 480px;
-      }
-    `}
-  padding: 24px;
-  transform: translate(-50%, -50%);
-  color: var(--textPrimary);
-
-  ${breakpoint(400, w_full)};
-
-  > :not(:first-child) {
-    margin-top: 16px;
-  }
-`;
-
-const TopWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-weight: bold;
-  font-size: 14px;
-  line-height: 100%;
-  h3 {
-    margin: 0;
-  }
-  > svg {
-    cursor: pointer;
-  }
-`;
-
-let z;
+let z_overlay_init = 999;
+let z_content_init = 1000;
 export default function Popup({
   children,
+  onClose = noop,
   title,
-  onClose = emptyFunction,
-  className,
+  className = "",
   wide,
-  zIndex = 999,
 }) {
-  const ref = useRef();
-  useEscapeKeyOnce(onClose);
-  useOnClickOutside(ref, onClose);
-
-  const [, setIsLocked] = useScrollLock();
-
+  const [zOverlay] = useState(z_overlay_init);
+  const [zContent] = useState(z_content_init);
   useEffect(() => {
-    z = zIndex;
-    z++;
+    z_overlay_init++;
+    z_content_init++;
   }, []);
 
-  useEffect(() => {
-    setIsLocked(true);
-
-    return () => setIsLocked(false);
-  });
-
   return (
-    <Background zIndex={z}>
-      <Wrapper
-        wide={wide}
-        ref={ref}
-        className={className}
-        onClick={(event) => event.stopPropagation()}
-      >
-        {title && (
-          <TopWrapper>
-            <h3>{title}</h3>
-            <ClosePanelIcon onClick={onClose} />
-          </TopWrapper>
-        )}
-        {children}
-      </Wrapper>
-    </Background>
+    <Dialog.Root open onOpenChange={onClose}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          className={clsx("fixed inset-0 bg-black/25")}
+          style={{ zIndex: zOverlay }}
+        />
+        <Dialog.Content asChild onEscapeKeyDown={onClose}>
+          <NeutralPanel
+            className={clsx(
+              "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2",
+              "w-[400px] max-w-full",
+              wide && "sm:w-[480px]",
+              "p-6 space-y-4",
+              className,
+            )}
+            style={{
+              zIndex: zContent,
+            }}
+          >
+            {title && (
+              <div className="flex items-center justify-between">
+                <Dialog.Title asChild>
+                  <h3 className="text14Bold">{title}</h3>
+                </Dialog.Title>
+                <SystemClose
+                  className="w-4 h-4 [&_path]:stroke-textTertiary"
+                  role="button"
+                  onClick={onClose}
+                />
+              </div>
+            )}
+
+            {children}
+          </NeutralPanel>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
