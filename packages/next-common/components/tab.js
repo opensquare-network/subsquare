eafff3f21a5f4fd8d0f05054fde669e4624da6b7
2nthony <ant.hu@outlook.com>
2023-07-11 17:22:17 +0800

    fix: tab button remove outline

--- packages/next-common/components/tab.js
+++ packages/next-common/components/tab.js
@@ -14,7 +14,7 @@ export default function Tab({ small, tabs, selectedTabId, setSelectedTabId }) {
         <button
           key={tabId}
           className={clsx(
-            "text14Medium",
+            "text14Medium outline-none",
             "flex justify-center items-center flex-1",
             "py-2.5 w-[172px]",
             small && "py-0.5 w-[156px]",
