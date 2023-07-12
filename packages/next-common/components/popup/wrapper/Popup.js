9f5d393b593c138851392c758cc6cfe999b85682
2nthony <ant.hu@outlook.com>
2023-07-12 12:20:21 +0800

    fix: popup margin top

--- packages/next-common/components/popup/wrapper/Popup.js
+++ packages/next-common/components/popup/wrapper/Popup.js
@@ -22,38 +22,45 @@ export default function Popup({
   return (
     <Dialog.Root open onOpenChange={onClose}>
       <Dialog.Portal>
-        <Dialog.Overlay
-          className={clsx("fixed inset-0 bg-black/25")}
-          style={{ zIndex: zOverlay }}
-        />
+        <Dialog.Overlay />
         <Dialog.Content asChild onEscapeKeyDown={onClose}>
-          <NeutralPanel
-            className={clsx(
-              "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2",
-              "w-[400px] max-w-full",
-              wide && "sm:w-[480px]",
-              "p-6 space-y-4",
-              className,
-            )}
-            style={{
-              zIndex: zContent,
+          <div
+            className="fixed inset-0 bg-black/25 flex justify-center items-start overflow-auto overscroll-y-none"
+            style={{ zIndex: zOverlay }}
+            onClick={(event) => {
+              if (event.target === event.currentTarget) {
+                onClose();
+              }
             }}
           >
-            {title && (
-              <div className="flex items-center justify-between">
-                <Dialog.Title asChild>
-                  <h3 className="text14Bold text-textPrimary">{title}</h3>
-                </Dialog.Title>
-                <SystemClose
-                  className="w-4 h-4 [&_path]:stroke-textTertiary"
-                  role="button"
-                  onClick={onClose}
-                />
-              </div>
-            )}
+            <NeutralPanel
+              className={clsx(
+                "relative mt-[12vh] mb-4",
+                "w-[400px] max-w-full",
+                wide && "sm:w-[480px]",
+                "p-6 space-y-4",
+                className,
+              )}
+              style={{
+                zIndex: zContent,
+              }}
+            >
+              {title && (
+                <div className="flex items-center justify-between">
+                  <Dialog.Title asChild>
+                    <h3 className="text14Bold text-textPrimary">{title}</h3>
+                  </Dialog.Title>
+                  <SystemClose
+                    className="w-4 h-4 [&_path]:stroke-textTertiary"
+                    role="button"
+                    onClick={onClose}
+                  />
+                </div>
+              )}
 
-            {children}
-          </NeutralPanel>
+              {children}
+            </NeutralPanel>
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
