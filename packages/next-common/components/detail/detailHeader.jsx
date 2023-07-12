50202e541c6895febb1679b8ea89cf7bdae127cd
2nthony <ant.hu@outlook.com>
2023-07-12 17:19:54 +0800

    refactor: detail header

---
+++ packages/next-common/components/detail/detailHeader.jsx
@@ -0,0 +1,67 @@
+// origin from `next/components/detailItem.js`
+// https://github.com/opensquare-network/subsquare/pull/3107/files#diff-e35b3d25f03a97441a2846161a6bdb1715487b4949717a7f5a0abc70b5333942
+
+// - navigation
+// - countdown
+// - title
+// - post meta
+
+import { detailPageCategory } from "next-common/utils/consts/business/category";
+import PostMeta from "next-common/components/detail/container/Meta";
+import PostTitle from "next-common/components/detail/common/Title";
+import ReferendumNavigation from "next-common/components/detail/navigation/ReferendumNavigation";
+import DemocracyProposalNavigation from "next-common/components/detail/navigation/democracyProposal";
+import ExternalNavigation from "next-common/components/detail/navigation/external";
+import TreasuryProposalNavigation from "next-common/components/detail/navigation/treasuryProposalNavigation";
+import ReferendaReferendumNavigation from "next-common/components/detail/navigation/referendaReferendumNavigation";
+import AnnouncementNavigation from "next-common/components/detail/navigation/announcementNavigation";
+import MaliciousHead from "next-common/components/detail/maliciousHead";
+import ReferendumVoteEndCountDown from "next-common/components/democracy/referendum/voteEndCountDown";
+import { usePost } from "next-common/context/post";
+import { useDetailType } from "next-common/context/page";
+import { useSelector } from "react-redux";
+import { isEditingPostSelector } from "next-common/store/reducers/userSlice";
+import Divider from "../styled/layout/divider";
+
+export default function DetailHeader({ countDown = null }) {
+  const post = usePost();
+  const type = useDetailType();
+  const isEditing = useSelector(isEditingPostSelector);
+
+  return (
+    !isEditing && (
+      <>
+        {post?.isMalicious && <MaliciousHead />}
+        {type === detailPageCategory.DEMOCRACY_REFERENDUM && (
+          <ReferendumVoteEndCountDown />
+        )}
+
+        {type === detailPageCategory.GOV2_REFERENDUM && (
+          <ReferendaReferendumNavigation />
+        )}
+
+        {type === detailPageCategory.TREASURY_PROPOSAL && (
+          <TreasuryProposalNavigation />
+        )}
+
+        {type === detailPageCategory.DEMOCRACY_EXTERNAL && (
+          <ExternalNavigation post={post} />
+        )}
+        {type === detailPageCategory.DEMOCRACY_PROPOSAL && (
+          <DemocracyProposalNavigation
+            proposalIndex={post.proposalIndex}
+            referendumIndex={post?.referendumIndex}
+          />
+        )}
+        {type === detailPageCategory.DEMOCRACY_REFERENDUM && (
+          <ReferendumNavigation post={post} />
+        )}
+        <AnnouncementNavigation />
+        {countDown}
+        <PostTitle />
+        <Divider className="my-4" />
+        <PostMeta />
+      </>
+    )
+  );
+}
