58c922aba0c5f3b668688672ee51455979210949
hyifeng <chaojun@opensquare.network>
2023-07-14 16:09:04 +0800

    fix: statistics resize layout (#3130)

--- packages/next-common/components/statistics/referenda/summary/index.js
+++ packages/next-common/components/statistics/referenda/summary/index.js
@@ -7,6 +7,7 @@ import TrackList from "./trackList";
 
 const Content = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 24px;
   @media screen and (max-width: 768px) {
     flex-direction: column;
