faaba8f404bbeda686c57293cd193bdf509a3eaf
2nthony <ant.hu@outlook.com>
2023-07-17 15:15:37 +0800

    fellowship

--- packages/next-common/components/detail/fellowship/index.js
+++ packages/next-common/components/detail/fellowship/index.js
@@ -2,14 +2,34 @@ import DetailContentBase from "../common/detailBase";
 import ArticleContent from "../../articleContent";
 import { usePost } from "../../../context/post";
 import useSetEdit from "../common/hooks/useSetEdit";
+import PostTitle from "next-common/components/detail/common/Title";
+import Divider from "next-common/components/styled/layout/divider";
+import ReferendaPostMeta from "next-common/components/detail/common/openGov/meta";
+import { useSelector } from "react-redux";
+import { isEditingPostSelector } from "next-common/store/reducers/userSlice";
+import FellowshipWhitelistNavigation from "next-common/components/detail/fellowship/whitelistNavigation";
 
 export default function FellowshipReferendaDetail({ onReply }) {
   const post = usePost();
   const setIsEdit = useSetEdit();
+  const isEditing = useSelector(isEditingPostSelector);
 
   return (
     <DetailContentBase>
-      <ArticleContent post={post} onReply={onReply} setIsEdit={setIsEdit} />
+      {!isEditing && (
+        <>
+          <FellowshipWhitelistNavigation />
+        </>
+      )}
+      <PostTitle />
+      <Divider className="my-4" />
+      <ReferendaPostMeta isFellowship />
+      <ArticleContent
+        className="mt-6"
+        post={post}
+        onReply={onReply}
+        setIsEdit={setIsEdit}
+      />
     </DetailContentBase>
   );
 }
