9e3756e79ba5cf81d55ca5b30beb113ea1011a45
2nthony <ant.hu@outlook.com>
2023-07-17 15:22:02 +0800

    treasury child bounty

--- packages/next-common/components/detail/treasury/childBounty/index.js
+++ packages/next-common/components/detail/treasury/childBounty/index.js
@@ -3,13 +3,28 @@ import useSetEdit from "../../common/hooks/useSetEdit";
 import DetailContentBase from "../../common/detailBase";
 import ArticleContent from "../../../articleContent";
 import React from "react";
+import PostTitle from "next-common/components/detail/common/Title";
+import { useSelector } from "react-redux";
+import { isEditingPostSelector } from "next-common/store/reducers/userSlice";
+import Divider from "next-common/components/styled/layout/divider";
+import BountyPostMeta from "next-common/components/detail/treasury/common/bountyMeta";
+import ChildBountyCountDown from "next-common/components/detail/treasury/childBounty/countDown";
 
 export default function ChildBountyDetail({ onReply }) {
   const post = usePost();
   const setIsEdit = useSetEdit();
+  const isEditing = useSelector(isEditingPostSelector);
 
   return (
     <DetailContentBase>
+      {!isEditing && (
+        <>
+          <ChildBountyCountDown data={post.onchainData} />
+        </>
+      )}
+      <PostTitle />
+      <Divider className="my-4" />
+      <BountyPostMeta />
       <ArticleContent post={post} onReply={onReply} setIsEdit={setIsEdit} />
     </DetailContentBase>
   );
