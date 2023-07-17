8351b2ffab410346c34ecca48917469bde93501f
2nthony <ant.hu@outlook.com>
2023-07-17 14:55:13 +0800

    delete

--- packages/next-common/components/layout/democracyLayout/referendaDetail.jsx
+++
@@ -1,8 +0,0 @@
-import DetailLayout from "../DetailLayoutV2";
-
-/**
- * @param {import("../DetailLayoutV2").DetailLayoutProps} props
- */
-export default function DemocracyReferendaDetailLayout(props) {
-  return <DetailLayout {...props}>{props.children}</DetailLayout>;
-}
