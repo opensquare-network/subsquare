b9b4fc98f3784472fae88a9d1e84ae6c597807e8
2nthony <ant.hu@outlook.com>
2023-07-20 18:23:47 +0800

    democracy external proposal call

--- packages/next/components/external/metadata.js
+++ packages/next/components/external/metadata.js
@@ -1,5 +1,4 @@
 import KVList from "next-common/components/listInfo/kvList";
-import Proposal from "next-common/components/proposal";
 
 export default function Metadata({ external }) {
   if (!external) {
@@ -10,18 +9,6 @@ export default function Metadata({ external }) {
     ["Hash", external.proposalHash],
     ["Threshold", external.voteThreshold],
   ];
-
-  if (external.preImage) {
-    metadata.push([
-      <Proposal
-        key="proposal"
-        call={external.preImage.call}
-        shorten={external.preImage.shorten}
-        motionIndex={external.motionIndex}
-        referendumIndex={external.referendumIndex}
-      />,
-    ]);
-  }
 
   return <KVList title="Metadata" data={metadata} showFold />;
 }
