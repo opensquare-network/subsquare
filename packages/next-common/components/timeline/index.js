0267df7ed04ca40ce543312926bf1f88934d0230
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-21 23:57:03 +0800

    Extract call fields for kintsugi public proposal

--- packages/next-common/components/timeline/index.js
+++ packages/next-common/components/timeline/index.js
@@ -4,10 +4,6 @@ import Item from "./item";
 import FoldableItem from "./foldableItem";
 
 export default function Timeline({ data = [], indent = true, compact }) {
-  let lastTimelineItem = data[data.length - 1];
-  if (Array.isArray(lastTimelineItem)) {
-    lastTimelineItem = lastTimelineItem[lastTimelineItem.length - 1];
-  }
   if (!data || data?.length === 0) {
     return null;
   }
