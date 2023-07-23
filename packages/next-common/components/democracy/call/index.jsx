882d003dca05a274fdc9698fa517b43151748869
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-23 22:00:43 +0800

    Add treasury link for kintsugi referendum call

--- packages/next-common/components/democracy/call/index.jsx
+++ packages/next-common/components/democracy/call/index.jsx
@@ -1,9 +1,10 @@
 import { useDemocracyReferendumHash } from "next-common/hooks/democracy/useDemocracyReferendumHash";
-import KvList from "../listInfo/kvList";
-import Proposal from "../proposal";
+import KvList from "../../listInfo/kvList";
+import Proposal from "../../proposal";
 import { useChain } from "next-common/context/chain";
 import React from "react";
 import extractKintsugiFields from "next-common/components/democracy/common/kintsugiCallFields";
+import extractTreasuryFields from "next-common/components/democracy/call/treasury";
 
 export default function ReferendumCall({ call, shorten, onchainData = {} }) {
   const hash = useDemocracyReferendumHash();
@@ -20,6 +21,7 @@ export default function ReferendumCall({ call, shorten, onchainData = {} }) {
       />,
     ],
     ...extractKintsugiFields(chain, call),
+    ...extractTreasuryFields(call),
   ];
 
   return <KvList data={data} />;
---
+++ packages/next-common/components/democracy/call/treasury.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+export default function extractTreasuryFields(call = {}) {
+  const { section, method, args = [] } = call;
+  if (
+    "treasury" !== section ||
+    !["approveProposal", "rejectProposal"].includes(method)
+  ) {
+    return [];
+  }
+
+  const proposalId = args[0].value;
+  return [
+    [
+      "Link to",
+      <Link
+        key="proposal-link"
+        href={`/treasury/proposal/${proposalId}`}
+        legacyBehavior
+      >{`Treasury Proposal #${proposalId}`}</Link>,
+    ],
+  ];
+}
