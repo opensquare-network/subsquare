466896c8494b8b118e442eb2ca59908dc30edd99
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-23 18:17:46 +0800

    Improve kintsugi call with remark

--- packages/next-common/components/democracy/common/kintsugiCallFields.js
+++ packages/next-common/components/democracy/common/kintsugiCallFields.js
@@ -3,15 +3,13 @@ import getChainSettings from "next-common/utils/consts/settings";
 import { toPrecision } from "next-common/utils";
 import User from "next-common/components/user";
 import React from "react";
+import { hexIsValidUTF8 } from "next-common/utils/utf8validate";
+import { hexToString } from "@polkadot/util";
 
-export default function extractKintsugiFields(chain, call = {}) {
-  if (![Chains.kintsugi, Chains.interlay].includes(chain)) {
-    return [];
-  }
-
+function extractTreasuryFields(chain, call = {}) {
   const { section, method, args = [] } = call;
   if ("democracy" !== section || "spendFromTreasury" !== method) {
-    return [];
+    return null;
   }
   const value = args[0].value;
   const beneficiary = args[1].value;
@@ -30,4 +28,42 @@ export default function extractKintsugiFields(chain, call = {}) {
       />,
     ],
   ];
+}
+
+function extreactRemarkFields(call = {}) {
+  const { section, method, args = [] } = call;
+  if ("system" !== section || "remark" !== method) {
+    return null;
+  }
+  let remark = args[0].value;
+  if (hexIsValidUTF8(remark)) {
+    remark = hexToString(remark);
+  }
+
+  return [
+    [
+      "Remark",
+      <p className="whitespace-pre-wrap" key="remark">
+        {remark}
+      </p>,
+    ],
+  ];
+}
+
+export default function extractKintsugiFields(chain, call = {}) {
+  if (![Chains.kintsugi, Chains.interlay].includes(chain)) {
+    return [];
+  }
+
+  const treasuryFields = extractTreasuryFields(chain, call);
+  if (treasuryFields) {
+    return treasuryFields;
+  }
+
+  const remarkFields = extreactRemarkFields(call);
+  if (remarkFields) {
+    return remarkFields;
+  }
+
+  return [];
 }
