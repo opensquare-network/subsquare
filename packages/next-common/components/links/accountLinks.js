7e34608026e17d9e2ca30050e12be2aeff21bb30
chaojun <chaojun@opensquare.network>
2023-07-24 17:56:08 +0800

    Add user profile links

--- packages/next-common/components/links/accountLinks.js
+++ packages/next-common/components/links/accountLinks.js
@@ -4,6 +4,8 @@ import Flex from "../styled/flex";
 import DotreasuryAccountLink from "./dotreasuryLink";
 import { SubScanAccountLink } from "./subscanLink";
 import { StatescanAccountLink } from "./statescan";
+import useIdentity from "next-common/utils/hooks/useIdentity";
+import { useChain } from "next-common/context/chain";
 
 const Wrapper = styled(Flex)`
   height: 20px;
@@ -14,6 +16,11 @@ const Wrapper = styled(Flex)`
 `;
 
 export default function AccountLinks({ address }) {
+  const chain = useChain();
+  const identity = useIdentity(address, chain);
+  const { email, riot, twitter, web } = identity;
+  console.log({ email, riot, twitter, web });
+
   if (!address) {
     throw new Error("No address provided");
   }
