b7538659a5cde9345b4829555d18c99ed095ea22
2nthony <ant.hu@outlook.com>
2023-07-28 18:46:07 +0800

    kintsugi post create

--- packages/kintsugi-next/pages/post/create.js
+++ packages/kintsugi-next/pages/post/create.js
@@ -1,60 +1,9 @@
-import styled from "styled-components";
-import { withLoginUser, withLoginUserRedux } from "next-common/lib";
-import NextHead from "next-common/components/nextHead";
-import PostCreate from "next-common/components/post/postCreate";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import BaseLayout from "next-common/components/layout/baseLayout";
-import BreadcrumbWrapper from "next-common/components/detail/common/BreadcrumbWrapper";
-import Breadcrumb from "next-common/components/_Breadcrumb";
+import { withLoginUser } from "next-common/lib";
+import CreatePostPage from "@subsquare/next/pages/post/create";
 
-const Wrapper = styled.div`
-  > :not(:first-child) {
-    margin-top: 16px;
-  }
-  max-width: 852px;
-  margin: auto;
-`;
+export default CreatePostPage;
 
-export default withLoginUserRedux(({ loginUser }) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (loginUser === null) {
-      router.push({
-        pathname: "/login",
-        query: {
-          redirect: router.route,
-        },
-      });
-    }
-  }, [loginUser, router]);
-
-  const breadcrumbItems = [
-    {
-      content: "Discussions",
-      path: "/discussions",
-    },
-    {
-      content: "New Post",
-    },
-  ];
-
-  return (
-    <BaseLayout>
-      <NextHead title={"Create post"} desc={""} />
-      <Wrapper>
-        <BreadcrumbWrapper>
-          <Breadcrumb items={breadcrumbItems} />
-        </BreadcrumbWrapper>
-
-        <PostCreate />
-      </Wrapper>
-    </BaseLayout>
-  );
-});
-
-export const getServerSideProps = withLoginUser(async (context) => {
+export const getServerSideProps = withLoginUser(async () => {
   return {
     props: {},
   };
