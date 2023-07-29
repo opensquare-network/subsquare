ca933dd7ad5d69b995952d7b4dea0a4d55a52c1f
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-29 17:01:44 +0800

    Add OpenGov preimage missing warning, #3018 (#3225)
    * Refactor Malicious component, #3018
    
    * Add warning component when OpenGov preimage not found, #3018

---
+++ packages/next-common/components/detail/malicious/styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const MaliciousWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--orange500);
+  background-color: var(--orange100);
+  border-radius: 4px;
+  padding: 10px;
+  margin-bottom: 16px;
+  font-size: 14px;
+`;
+
+export const WarningText = styled.div`
+  line-height: 18px;
+`;
+
+export const IconWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  top: 3px;
+  margin-right: 8px;
+`;
