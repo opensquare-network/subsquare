74fd5fa702bf8cb4ef8c2b048ae6c54474fca890
2nthony <ant.hu@outlook.com>
2023-07-31 16:18:07 +0800

    fix: close login popup when router pathname changes

--- packages/next-common/components/login/popup.jsx
+++ packages/next-common/components/login/popup.jsx
@@ -1,12 +1,19 @@
 import Popup from "../popup/wrapper/Popup";
 import { useLoginPopup } from "next-common/hooks/useLoginPopup";
 import LoginContent from "./content";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 /**
  * @param {Parameters<Popup>[0]} props
  */
 export default function LoginPopup(props = {}) {
   const { loginPopupOpen, closeLoginPopup } = useLoginPopup();
+  const router = useRouter();
+
+  useEffect(() => {
+    closeLoginPopup();
+  }, [router.pathname]);
 
   return (
     loginPopupOpen && (
