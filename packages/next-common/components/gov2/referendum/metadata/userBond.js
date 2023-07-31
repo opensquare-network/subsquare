c4746fc9cb2350ac1063014ade5fd6789d909946
Yongfeng LI <wliyongfeng@gmail.com>
2023-07-31 17:07:23 +0800

    Add OpenGov decision deposit refund, #3218

--- packages/next-common/components/gov2/referendum/metadata/userBond.js
+++ packages/next-common/components/gov2/referendum/metadata/userBond.js
@@ -6,8 +6,10 @@ import Tooltip from "../../../tooltip";
 import styled from "styled-components";
 import Flex from "../../../styled/flex";
 import { useDecimals, useVoteSymbol } from "../../../../context/chain";
+import RefundDecisionDeposit from "next-common/components/gov2/referendum/metadata/refund";
 
 const BondValueWrapper = styled(Flex)`
+  color: var(--textTertiary);
   gap: 8px;
 
   &::before {
@@ -27,14 +29,23 @@ function BondValue({ deposit, decimals, symbol }) {
   );
 }
 
-export default function UserBond({ address, bond }) {
+export default function UserBond({ address, bond, children }) {
   const decimals = useDecimals();
   const symbol = useVoteSymbol();
 
   return (
-    <ValueWrapper>
+    <ValueWrapper className="flex-wrap">
       <User add={address} fontSize={14} />
       <BondValue deposit={bond} decimals={decimals} symbol={symbol} />
+      {children}
     </ValueWrapper>
+  );
+}
+
+export function DecisionUserBond({ address, bond, pallet = "referenda" }) {
+  return (
+    <UserBond address={address} bond={bond}>
+      <RefundDecisionDeposit pallet={pallet} />
+    </UserBond>
   );
 }
