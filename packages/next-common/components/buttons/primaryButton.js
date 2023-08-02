82df1b1ed529e2e70fe3b48db4ae2b3836122220
2nthony <ant.hu@outlook.com>
2023-08-02 16:23:25 +0800

    fix: primary button border

--- packages/next-common/components/buttons/primaryButton.js
+++ packages/next-common/components/buttons/primaryButton.js
@@ -4,13 +4,15 @@ import { BackgroundButton, DisabledButton } from "./styled";
 import { LightLoading } from "./loading";
 
 const RawButton = styled(BackgroundButton)`
+  height: 40px;
   background-color: var(--theme500);
-  border-color: var(--theme500);
+  border: none;
 `;
 
 const Disabled = styled(DisabledButton)`
+  height: 40px;
   background-color: var(--theme300);
-  border-color: var(--theme300);
+  border: none;
   color: var(--textPrimaryContrast);
 `;
 
