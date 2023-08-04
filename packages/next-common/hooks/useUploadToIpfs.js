af63a45d5dc67860059bd741bb03c719332fdf23
2nthony <ant.hu@outlook.com>
2023-08-04 11:08:14 +0800

    use upload to ipfs

---
+++ packages/next-common/hooks/useUploadToIpfs.js
@@ -0,0 +1,41 @@
+import nextApi from "next-common/services/nextApi";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { newErrorToast } from "next-common/store/reducers/toastSlice";
+
+export function useUploadToIpfs() {
+  const [uploading, setUploading] = useState(false);
+  const dispatch = useDispatch();
+
+  function upload(files) {
+    if (files && files.length) {
+      const image = files[0];
+      if (!/image\/\w+/.exec(image.type)) {
+        return;
+      }
+
+      setUploading(true);
+
+      const formData = new FormData();
+      formData.append("file", image, image.name);
+
+      return nextApi
+        .postFormData("ipfs/files", formData)
+        .then((response) => {
+          if (response?.error) {
+            dispatch(newErrorToast(response?.error?.message));
+          }
+
+          return response;
+        })
+        .finally(() => {
+          setUploading(false);
+        });
+    }
+  }
+
+  return {
+    uploading,
+    upload,
+  };
+}
