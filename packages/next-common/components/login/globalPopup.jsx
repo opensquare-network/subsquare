1b19de5b859292cdc6621c01560f56f2ecd1e719
2nthony <ant.hu@outlook.com>
2023-08-07 13:33:33 +0800

    extract, add global login popup

---
+++ packages/next-common/components/login/globalPopup.jsx
@@ -0,0 +1,15 @@
+import { useLoginPopup } from "next-common/hooks/useLoginPopup";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import LoginPopup from "./popup";
+
+export default function GlobalLoginPopup() {
+  const { loginPopupOpen, closeLoginPopup } = useLoginPopup();
+  const router = useRouter();
+
+  useEffect(() => {
+    closeLoginPopup();
+  }, [router.pathname]);
+
+  return loginPopupOpen && <LoginPopup onClose={closeLoginPopup} />;
+}
