c906d003d83f67a302519b5a769befa173bed041
2nthony <ant.hu@outlook.com>
2023-08-07 14:09:33 +0800

    refactor: reuse login popup (#3270)
    * fix: border
    
    * extract, add global login popup
    
    * delete connect wallet
    
    * rename

--- packages/next-common/components/layout/baseLayout.jsx
+++ packages/next-common/components/layout/baseLayout.jsx
@@ -23,7 +23,7 @@ import {
 } from "next-common/store/reducers/chainSlice";
 import clsx from "clsx";
 import { useNavCollapsed } from "next-common/context/nav";
-import LoginPopup from "../login/popup";
+import LoginGlobalPopup from "../login/globalPopup";
 
 /**
  * @description a base layout includes nav, header and footer
@@ -93,7 +93,7 @@ export default function BaseLayout({ children, seoInfo = {} }) {
       <CMDKPalette />
       <Toast />
       <CookiesConsent />
-      <LoginPopup />
+      <LoginGlobalPopup />
     </>
   );
 }
