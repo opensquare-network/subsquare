748956c55ec4451a2cd7a9519f471cd389f4efb5
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-07 17:28:14 +0800

    Add hook to get address track no votes lock, #2529

--- packages/next/components/myvotes/summary/useMyClassLocksFor.js
+++ packages/next/components/myvotes/summary/useMyClassLocksFor.js
@@ -1,16 +1,56 @@
 import useApi from "next-common/utils/hooks/useApi";
-import useCall from "next-common/utils/hooks/useCall";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import { useEffect, useState } from "react";
+
+async function queryNoVoteTracks(api, address) {
+  const [entries, classLocks] = await Promise.all([
+    api.query.convictionVoting.votingFor.entries(address),
+    api?.query?.convictionVoting?.classLocksFor(address),
+  ]);
+
+  const trackLock = [];
+  for (const [trackId, locked] of classLocks.toJSON()) {
+    const entry = entries.find((entry) => {
+      const [storageKey, votingOf] = entry;
+      if (storageKey.args[1].toNumber() !== trackId) {
+        return false;
+      }
+
+      if (votingOf.isDelegating) {
+        return false;
+      }
+      return votingOf.asCasting.votes.length <= 0;
+    });
+
+    if (entry) {
+      const prior = entry[1].asCasting.prior;
+      trackLock.push({
+        trackId,
+        locked,
+        prior: {
+          unlockAt: prior[0].toNumber(),
+          balance: prior[1].toString(),
+        },
+      });
+    }
+  }
+
+  return trackLock;
+}
 
 export default function useMyClassLocksFor() {
   const api = useApi();
   const realAddress = useRealAddress();
-  const [classLocks] = useCall(api?.query?.convictionVoting?.classLocksFor, [
-    realAddress,
-  ]);
-  if (classLocks) {
-    return classLocks.toJSON().map(([trackId, lock]) => ({ trackId, lock }));
-  }
+  const [trackLocks, setTrackLocks] = useState();
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
 
-  return classLocks;
+    queryNoVoteTracks(api, realAddress).then((trackLocks) => {
+      setTrackLocks(trackLocks);
+    });
+  }, [api, realAddress]);
+
+  return trackLocks;
 }
