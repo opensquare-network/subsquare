63c6d92f58640d8c13bb7462cae6fba9a33ce2d0
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-13 17:42:15 +0800

    User my votes from redux, #2529

--- packages/next/components/myvotes/myDemocracyVotes.js
+++ packages/next/components/myvotes/myDemocracyVotes.js
@@ -1,8 +1,6 @@
-import useAccountDemocracyVotes from "next-common/hooks/democracy/votes/useAccountDemocracyVotes";
-import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useEffect } from "react";
 import nextApi from "next-common/services/nextApi";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   setMyVotedPosts,
   setMyVotedPostsLoading,
@@ -11,26 +9,26 @@ import DemocracySummary from "./summary/democracySummary";
 import MyVotesList from "./myVotesList";
 import useFetchMyDemocracyVoting from "./democracy/useFetchMyDemocracyVoting";
 import useFetchDemocracyLockingPeriod from "./democracy/useFetchDemocracyLockingPeriod";
+import myDemocracyVotesSelector from "next-common/store/reducers/myOnChainData/democracy/selectors/votes";
 
 export default function MyDemocracyVotes() {
   const dispatch = useDispatch();
-  const realAddress = useRealAddress();
-  const { votes } = useAccountDemocracyVotes(realAddress);
+  const myDemocracyVotes = useSelector(myDemocracyVotesSelector);
   useFetchMyDemocracyVoting();
   useFetchDemocracyLockingPeriod();
 
   useEffect(() => {
-    if (!votes || !votes.length) {
+    if (!myDemocracyVotes || !myDemocracyVotes.length) {
       return;
     }
 
-    const q = votes
+    const q = myDemocracyVotes
       .map((vote) => `referendum_index=${vote.referendumIndex}`)
       .join("&");
 
     dispatch(setMyVotedPostsLoading(true));
     nextApi
-      .fetch(`democracy/referendums?${q}&page_size=${votes.length}`)
+      .fetch(`democracy/referendums?${q}&page_size=${myDemocracyVotes.length}`)
       .then(({ result }) => {
         if (result) {
           dispatch(setMyVotedPosts(result.items));
@@ -39,12 +37,12 @@ export default function MyDemocracyVotes() {
       .finally(() => {
         dispatch(setMyVotedPostsLoading(false));
       });
-  }, [dispatch, votes]);
+  }, [dispatch, myDemocracyVotes]);
 
   return (
     <div className="flex flex-col gap-[16px]">
       <DemocracySummary />
-      <MyVotesList votes={votes} />
+      <MyVotesList votes={myDemocracyVotes} />
     </div>
   );
 }
