1ee475e26c742dc860f77ebdcc23d1695bb522d7
chaojun <chaojun@opensquare.network>
2023-08-14 16:02:43 +0800

    fix

--- packages/next-common/components/summary/externalInfo/item.js
+++ packages/next-common/components/summary/externalInfo/item.js
@@ -1,9 +1,9 @@
-import { SystemLink } from "@osn/icons/subsquare";
 import styled from "styled-components";
 
 const Wrapper = styled.a`
   display: flex;
   align-items: center;
+  padding: 6px 12px;
   gap: 4px;
   border-radius: 4px;
   background: linear-gradient(
@@ -11,15 +11,17 @@ const Wrapper = styled.a`
     rgba(4, 210, 197, 0.2) 0%,
     rgba(104, 72, 255, 0.2) 100%
   );
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px;
 `;
 
 export default function Item({ title, href }) {
   return (
     <Wrapper href={href} target="_blank">
       <span>{title}</span>
-      <div>
-        <SystemLink />
-      </div>
+      <div className="inline-flex text-textTertiary">↗</div>
     </Wrapper>
   );
 }
--- packages/next-common/components/summary/externalInfo/offChainVoting.js
+++ packages/next-common/components/summary/externalInfo/offChainVoting.js
@@ -3,6 +3,9 @@ import Item from "./item";
 export default function OffChainVoting() {
   return (
     <div className="flex flex-col gap-[8px]">
+      <span className="text-[12px] font-medium leading-[16px]">
+        Active off-chain voting
+      </span>
       <Item
         title="Sportsgeist P2P-Sportbetting Proposal"
         href="https://voting.opensquare.io"
