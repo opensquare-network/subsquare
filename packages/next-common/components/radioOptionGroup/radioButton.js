3690586f4a1486c8671ed9a4dc0e2cc3cb81f654
chaojun <chaojun@opensquare.network>
2023-08-14 14:48:24 +0800

    Allow report reason text clicking, #3263

--- packages/next-common/components/radioOptionGroup/radioButton.js
+++ packages/next-common/components/radioOptionGroup/radioButton.js
@@ -2,14 +2,10 @@ import {
   SystemRadioButtonOn,
   SystemRadioButtonOff,
 } from "@osn/icons/subsquare";
-import noop from "lodash.noop";
 
-export default function RadioButton({ checked, onClick = noop }) {
+export default function RadioButton({ checked }) {
   return (
-    <div
-      className="cursor-pointer inline-flex [&_svg]:w-[20px] [&_svg]:h-[20px] [&_path]:fill-theme500"
-      onClick={onClick}
-    >
+    <div className="inline-flex [&_svg]:w-[20px] [&_svg]:h-[20px] [&_path]:fill-theme500">
       {checked ? <SystemRadioButtonOn /> : <SystemRadioButtonOff />}
     </div>
   );
