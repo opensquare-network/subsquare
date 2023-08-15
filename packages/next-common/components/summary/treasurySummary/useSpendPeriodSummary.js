978320fcd8cca8cc951a14c565c16e788d120fda
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-15 11:29:26 +0800

    Take back treasury spend period countdown component (#3303)
    * Show treasury spend countdown for non-interlay chains
    
    * Extract SpendPeriod component

---
+++ packages/next-common/components/summary/treasurySummary/useSpendPeriodSummary.js
@@ -0,0 +1,39 @@
+import useApi from "next-common/utils/hooks/useApi";
+import { useSelector } from "react-redux";
+import {
+  blockTimeSelector,
+  latestHeightSelector,
+} from "next-common/store/reducers/chainSlice";
+import { useEffect, useState } from "react";
+import useIsMounted from "next-common/utils/hooks/useIsMounted";
+import BigNumber from "bignumber.js";
+import { estimateBlocksTime } from "next-common/utils";
+
+export default function useSpendPeriodSummary() {
+  const api = useApi();
+  const blockHeight = useSelector(latestHeightSelector);
+  const [summary, setSummary] = useState({});
+  const isMounted = useIsMounted();
+  const blockTime = useSelector(blockTimeSelector);
+
+  useEffect(() => {
+    if (api?.consts?.treasury?.spendPeriod && blockHeight) {
+      const spendPeriod = api.consts.treasury.spendPeriod.toNumber();
+      const goneBlocks = new BigNumber(blockHeight).mod(spendPeriod).toNumber();
+      const progress = new BigNumber(goneBlocks)
+        .div(spendPeriod)
+        .multipliedBy(100)
+        .toNumber();
+      const timeArray = estimateBlocksTime(spendPeriod - goneBlocks, blockTime);
+      if (isMounted.current) {
+        setSummary({
+          progress,
+          spendPeriod: timeArray,
+          totalPeriod: ["/"].concat(estimateBlocksTime(spendPeriod, blockTime)),
+        });
+      }
+    }
+  }, [api, blockHeight, blockTime]);
+
+  return summary;
+}
