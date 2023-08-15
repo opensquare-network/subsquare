86cb9be7e0f082c113f2ed53111b5aa8f03f9c14
chaojun <chaojun@opensquare.network>
2023-08-15 12:26:45 +0800

    fix: voting & bounties list style

--- packages/next-common/components/summary/externalInfo/item.js
+++ packages/next-common/components/summary/externalInfo/item.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 const Wrapper = styled.li`
+  :not(:last-child) {
+    margin-bottom: 8px;
+  }
+`;
+
+const Link = styled.a`
   display: flex;
   align-items: center;
   padding: 6px 12px;
@@ -15,19 +21,15 @@ const Wrapper = styled.li`
   font-style: normal;
   font-weight: 500;
   line-height: 16px;
-
-  :not(:last-child) {
-    margin-bottom: 8px;
-  }
 `;
 
 export default function Item({ title, href }) {
   return (
     <Wrapper>
-      <a href={href} target="_blank" rel="noreferrer">
+      <Link href={href} target="_blank" rel="noreferrer">
         <span className="text-textPrimary">{title}</span>
         <div className="inline-flex text-textTertiary">↗</div>
-      </a>
+      </Link>
     </Wrapper>
   );
 }
