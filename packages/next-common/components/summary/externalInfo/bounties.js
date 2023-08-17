2b30ea4fb777d18f7fdff711793e7eb0e4e286ad
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-17 15:58:03 +0800

    Refactor external bounty proposals component, #2387

--- packages/next-common/components/summary/externalInfo/bounties.js
+++ packages/next-common/components/summary/externalInfo/bounties.js
@@ -1,15 +1,35 @@
-import { usePageProps } from "next-common/context/page";
 import List from "./list";
+import { hasDefinedBounties } from "next-common/utils/summaryExternalInfo";
+import { useEffect, useState } from "react";
+import Api from "next-common/services/api";
+import { useChain } from "next-common/context/chain";
 
-export default function Bounties() {
-  const { activeBountyPosts } = usePageProps();
+function BountyProposals() {
+  const chain = useChain();
+  const [posts, setPosts] = useState();
+  useEffect(() => {
+    new Api(process.env.NEXT_PUBLIC_BOUNTIES_API_URL)
+      .fetch("child-bounties")
+      .then(({ result: { items } }) => {
+        setPosts(items.filter((item) => item.network === chain));
+      });
+  }, []);
+
   return (
     <List
       title="Bounties calling for hunters"
-      items={(activeBountyPosts || []).map((item) => ({
+      items={(posts || []).map((item) => ({
         title: item.title,
         href: `${process.env.NEXT_PUBLIC_BOUNTIES_SITE_URL}/#/network/${process.env.NEXT_PUBLIC_CHAIN}/bounty/${item.parentBountyIndex}/child-bounty/${item.index}`,
       }))}
     />
   );
+}
+
+export default function Bounties() {
+  if (!hasDefinedBounties()) {
+    return null;
+  }
+
+  return <BountyProposals />;
 }
