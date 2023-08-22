42f6db1e1cc24ecd83be9ea8455caa6ceb415741
2nthony <ant.hu@outlook.com>
2023-08-22 18:53:43 +0800

    update bubble chart

--- packages/next-common/components/charts/circlePacking/index.jsx
+++ packages/next-common/components/charts/circlePacking/index.jsx
@@ -1,78 +1,57 @@
+"use client";
+
 // d3
 // https://github.com/opensquare-network/subsquare/issues/3326
 // https://react-graph-gallery.com/circular-packing
 
 import clsx from "clsx";
 import * as d3 from "d3";
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
+import Tooltip from "next-common/components/tooltip";
 
-export default function CirclePacking({ data, height, width }) {
-  const [interactionData, setInteractionData] = useState();
-
+export default function CirclePacking({ data, width, height }) {
   const hierarchy = d3
     .hierarchy(data)
     .sum((d) => d.votes)
     .sort((a, b) => b.votes - a.votes);
-  const packGenerator = d3.pack().size([width, height]).padding(4);
-  const root = packGenerator(hierarchy);
+  const pack = d3
+    .pack()
+    .size([width || 1, height || 1])
+    .padding(4);
+  const root = pack(hierarchy);
+  const nodes = root.descendants().slice(1);
 
   return (
-    <div className="relative">
-      <svg width={width} height={height}>
-        {root
-          .descendants()
-          .slice(1)
-          .map((node, idx) => {
-            const { x, y, r } = node;
-            const d = r * 2;
+    <svg width={width} height={height}>
+      {nodes.map((node, idx) => {
+        const { x, y, r } = node;
+        const d = r * 2;
 
-            return (
-              <Fragment key={idx}>
-                <circle
-                  cx={x}
-                  cy={y}
-                  r={r}
-                  className={clsx(
-                    node.data.aye && "fill-green300",
-                    node.data.nay && "fill-red300",
-                    node.data.abstain && "fill-neutral400",
-                  )}
-                />
-                <foreignObject
-                  x={node.x - r}
-                  y={node.y - r}
-                  width={d}
-                  height={d}
-                >
-                  <div
-                    className="rounded-full w-full h-full"
-                    onMouseEnter={() => {
-                      setInteractionData(node);
-                    }}
-                    onMouseLeave={() => {
-                      setInteractionData(null);
-                    }}
-                  >
-                    <div
-                      className={clsx(
-                        "flex items-center justify-center",
-                        "text14Medium truncate",
-                        node.data.aye && "text-green500",
-                        node.data.nay && "text-red500",
-                        node.data.abstain && "text-textSecondary",
-                      )}
-                    >
-                      {node.data.name}
-                    </div>
-                  </div>
-                </foreignObject>
-              </Fragment>
-            );
-          })}
-      </svg>
-
-      {/* tooltip */}
-      {interactionData && <div className="absolute"></div>}
-    </div>
+        return (
+          <Fragment key={idx}>
+            <circle
+              cx={x}
+              cy={y}
+              r={r}
+              className={clsx(
+                node.data.aye && "fill-green300",
+                node.data.aye === false && "fill-red300",
+                node.data.isAbstain && "fill-neutral400",
+              )}
+            />
+            <foreignObject x={node.x - r} y={node.y - r} width={d} height={d}>
+              <Tooltip
+                className="!block h-full"
+                content={<div>{node.data.account}</div>}
+              >
+                <div className="rounded-full w-full h-full">
+                  {/* TODO: identity */}
+                </div>
+              </Tooltip>
+            </foreignObject>
+          </Fragment>
+        );
+      })}
+    </svg>
   );
 }
