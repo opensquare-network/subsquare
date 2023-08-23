1703a7b0ee6430f700fe1ab4fb78e35d1840d87e
chaojun <chaojun@opensquare.network>
2023-08-23 17:19:22 +0800

    fix: api url

--- packages/next-common/utils/viewfuncs/index.js
+++ packages/next-common/utils/viewfuncs/index.js
@@ -5,45 +5,73 @@ import { detailPageCategory } from "../consts/business/category";
 import isMoonChain from "../isMoonChain";
 
 export function toApiType(type) {
-  if (type === detailPageCategory.TREASURY_BOUNTY) {
-    return "treasury/bounties";
-  }
-  if (type === detailPageCategory.TREASURY_CHILD_BOUNTY) {
-    return "treasury/child-bounties";
-  }
-  if (type === detailPageCategory.FINANCIAL_MOTION) {
-    return "financial-motions";
-  }
-  if (type === detailPageCategory.TECH_COMM_MOTION) {
-    return "tech-comm/motions";
-  }
-  if (type === detailPageCategory.OPEN_TECH_COMM_PROPOSAL) {
-    return "open-techcomm/motions";
-  }
-  if (type === detailPageCategory.PA_POST) {
-    return "polkassembly-discussions";
-  }
-  if (type === detailPageCategory.COUNCIL_MOTION) {
-    if (isMoonChain()) {
-      return "moon-council/motions";
-    }
-    return "motions";
-  }
-  if (type === detailPageCategory.TREASURY_COUNCIL_MOTION) {
-    return "motions";
-  }
+  // Open Gov
   if (type === detailPageCategory.GOV2_REFERENDUM) {
     return "gov2/referendums";
   }
   if (type === detailPageCategory.FELLOWSHIP_REFERENDUM) {
     return "fellowship/referenda";
   }
-  if (type === detailPageCategory.ADVISORY_MOTION) {
-    return "advisory-motions";
-  }
+
+  // Democracy
   if (type === detailPageCategory.DEMOCRACY_REFERENDUM) {
     return "democracy/referendums";
   }
+  if (type === detailPageCategory.DEMOCRACY_PROPOSAL) {
+    return "democracy/proposals";
+  }
+  if (type === detailPageCategory.DEMOCRACY_EXTERNAL) {
+    return "democracy/externals";
+  }
+
+  // Treasury
+  if (type === detailPageCategory.TREASURY_BOUNTY) {
+    return "treasury/bounties";
+  }
+  if (type === detailPageCategory.TREASURY_CHILD_BOUNTY) {
+    return "treasury/child-bounties";
+  }
+  if (type === detailPageCategory.TREASURY_PROPOSAL) {
+    return "treasury/proposals";
+  }
+  if (type === detailPageCategory.TREASURY_TIP) {
+    return "treasury/tips";
+  }
+
+  // Motions
+  if (type === detailPageCategory.COUNCIL_MOTION) {
+    if (isMoonChain()) {
+      return "moon-council/motions";
+    }
+    return "motions";
+  }
+  if (type === detailPageCategory.TECH_COMM_MOTION) {
+    return "tech-comm/motions";
+  }
+
+  // Polkassembly
+  if (type === detailPageCategory.PA_POST) {
+    return "polkassembly-discussions";
+  }
+
+  // Karura
+  if (type === detailPageCategory.FINANCIAL_MOTION) {
+    return "financial-motions";
+  }
+
+  // Moonbeam / Moonriver
+  if (type === detailPageCategory.OPEN_TECH_COMM_PROPOSAL) {
+    return "open-techcomm/motions";
+  }
+  if (type === detailPageCategory.TREASURY_COUNCIL_MOTION) {
+    return "motions";
+  }
+
+  // Zeitgeist
+  if (type === detailPageCategory.ADVISORY_MOTION) {
+    return "advisory-motions";
+  }
+
   return `${type}s`;
 }
 
