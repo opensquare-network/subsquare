d1c0638bbf53a64c77c75ef8574768c97cc90b5e
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-24 22:02:36 +0800

    Extract whitelist call hash from referenda call

--- packages/next-common/components/gov2/referendum/call/index.jsx
+++ packages/next-common/components/gov2/referendum/call/index.jsx
@@ -5,6 +5,7 @@ import useReferendaBusinessData from "@subsquare/next/hooks/useReferendaBusiness
 import React from "react";
 import Copyable from "next-common/components/copyable";
 import extractRemarkMetaFields from "next-common/components/common/call/remarks";
+import extractWhitelistCallHash from "next-common/components/common/call/whitelist";
 
 export default function Gov2ReferendumCall() {
   const onchainData = useOnchainData();
@@ -28,7 +29,12 @@ export default function Gov2ReferendumCall() {
   if (businessData) {
     data.push(...businessData);
   }
-  data.push(...extractRemarkMetaFields(proposal?.call));
+  data.push(
+    ...[
+      ...extractRemarkMetaFields(proposal?.call),
+      ...extractWhitelistCallHash(proposal?.call),
+    ],
+  );
 
   return <KvList data={data} />;
 }
