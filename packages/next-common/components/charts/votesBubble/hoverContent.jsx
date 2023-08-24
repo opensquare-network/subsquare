7a2465418601d541d498818be0f1068f753b4cf7
2nthony <ant.hu@outlook.com>
2023-08-24 14:31:20 +0800

    hover content

---
+++ packages/next-common/components/charts/votesBubble/hoverContent.jsx
@@ -0,0 +1,47 @@
+import VoteLabel from "next-common/components/democracy/flattenedVotesPopup/voteLabel";
+import User from "next-common/components/user";
+import ValueDisplay from "next-common/components/valueDisplay";
+import { useChain, useChainSettings } from "next-common/context/chain";
+import { detailMultiTabsVotesBubbleView } from "next-common/store/reducers/detailSlice";
+import { toPrecision } from "next-common/utils";
+import Chains from "next-common/utils/consts/chains";
+import { useSelector } from "react-redux";
+
+export default function VotesBubbleHoverContent({ node, sizeField }) {
+  const chainSettings = useChainSettings();
+  const chain = useChain();
+  const hasLabel = ![Chains.kintsugi, Chains.interlay].includes(chain);
+  const view = useSelector(detailMultiTabsVotesBubbleView);
+
+  return (
+    <ul>
+      <li>
+        <User add={node.data.account} ellipsis={false} />
+      </li>
+      {view === "flattened" && (
+        <li>
+          Capital:{" "}
+          <ValueDisplay
+            value={toPrecision(node.data.balance, chainSettings.decimals)}
+            symbol={chainSettings.symbol}
+          />
+          {hasLabel && (
+            <>
+              (<VoteLabel {...node.data} />)
+            </>
+          )}
+        </li>
+      )}
+      <li>
+        Votes:{" "}
+        <ValueDisplay
+          value={toPrecision(node.data[sizeField], chainSettings.decimals)}
+          symbol={chainSettings.symbol}
+        />
+      </li>
+      {view === "nested" && (
+        <li>Delegators: {node.data.directVoterDelegations?.length}</li>
+      )}
+    </ul>
+  );
+}
