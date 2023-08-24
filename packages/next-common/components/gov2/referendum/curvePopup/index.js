967808df05bceb5afbc089dfcf8246e0fd4b3d41
chaojun <chaojun@opensquare.network>
2023-08-24 18:20:49 +0800

    Update

--- packages/next-common/components/gov2/referendum/curvePopup/index.js
+++ packages/next-common/components/gov2/referendum/curvePopup/index.js
@@ -23,47 +23,49 @@ function calcDataFromTallyHistory(tallyHistory, labels) {
   let currentSupportData = null;
   let currentApprovalData = null;
 
+  if (!tallyHistory) {
+    return { currentSupportData, currentApprovalData };
+  }
+
   // We need to calculate the current support and approval data from tally history if it is provided
-  if (tallyHistory) {
-    currentSupportData = [];
-    currentApprovalData = [];
+  currentSupportData = [];
+  currentApprovalData = [];
 
-    if (tallyHistory.length > 0) {
-      const firstDataPointTime = tallyHistory[0].indexer.blockTime;
-      const { ayes, nays, support, issuance } = tallyHistory[0].tally;
+  if (tallyHistory.length > 0) {
+    const firstDataPointTime = tallyHistory[0].indexer.blockTime;
+    const { ayes, nays, support, issuance } = tallyHistory[0].tally;
 
-      let currentSupport = new BigNumber(support).div(issuance).toNumber();
-      let currentApprove = new BigNumber(ayes)
-        .div(new BigNumber(ayes).plus(nays))
-        .toNumber();
-      currentSupportData.push(currentSupport * 100);
-      currentApprovalData.push(currentApprove * 100);
+    let currentSupport = new BigNumber(support).div(issuance).toNumber();
+    let currentApprove = new BigNumber(ayes)
+      .div(new BigNumber(ayes).plus(nays))
+      .toNumber();
+    currentSupportData.push(currentSupport * 100);
+    currentApprovalData.push(currentApprove * 100);
 
-      let currentPointNum = 0;
+    let currentPointNum = 0;
 
-      // Loop through tally history to find nearest data point for each hour
-      for (let i = 1; i < tallyHistory.length; i++) {
-        const nextDataPointTime =
-          firstDataPointTime + (currentPointNum + 1) * 3600 * 1000;
-        if (tallyHistory[i].indexer.blockTime > nextDataPointTime) {
-          const { ayes, nays, support, issuance } = tallyHistory[i - 1].tally;
+    // Loop through tally history to find nearest data point for each hour
+    for (let i = 1; i < tallyHistory.length; i++) {
+      const nextDataPointTime =
+        firstDataPointTime + (currentPointNum + 1) * 3600 * 1000;
+      if (tallyHistory[i].indexer.blockTime > nextDataPointTime) {
+        const { ayes, nays, support, issuance } = tallyHistory[i - 1].tally;
 
-          currentSupport = new BigNumber(support).div(issuance).toNumber();
-          currentApprove = new BigNumber(ayes)
-            .div(new BigNumber(ayes).plus(nays))
-            .toNumber();
-          currentSupportData.push(currentSupport * 100);
-          currentApprovalData.push(currentApprove * 100);
-
-          currentPointNum++;
-        }
-      }
-
-      // Fill the rest of the data points with the last data point
-      for (let i = currentPointNum + 1; i < labels.length; i++) {
+        currentSupport = new BigNumber(support).div(issuance).toNumber();
+        currentApprove = new BigNumber(ayes)
+          .div(new BigNumber(ayes).plus(nays))
+          .toNumber();
         currentSupportData.push(currentSupport * 100);
         currentApprovalData.push(currentApprove * 100);
+
+        currentPointNum++;
       }
+    }
+
+    // Fill the rest of the data points with the last data point
+    for (let i = currentPointNum + 1; i < labels.length; i++) {
+      currentSupportData.push(currentSupport * 100);
+      currentApprovalData.push(currentApprove * 100);
     }
   }
 
