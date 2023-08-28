42bfb360a65e091f109af591f8b15a0cdd43b827
Yongfeng LI <wliyongfeng@gmail.com>
2023-08-28 16:07:33 +0800

    Fix kintsugi votes bubble data field, #3326 (#3363)
    * Fix kintsugi votes bubble data field, #3326
    
    * Remove useless log, #3326

--- packages/next-common/components/charts/circlePacking/index.jsx
+++ packages/next-common/components/charts/circlePacking/index.jsx
@@ -6,7 +6,7 @@
 
 import * as d3 from "d3";
 import noop from "lodash.noop";
-import { Fragment } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 
 export default function CirclePacking({
   data,
@@ -17,41 +17,43 @@ export default function CirclePacking({
   bubbleClassName = "",
   renderBubbleContent = noop,
 }) {
-  if (!width || !height) {
-    return null;
-  }
+  const [nodes, setNodes] = useState([]);
+  useEffect(() => {
+    if (!width || !height) {
+      return;
+    }
 
-  const hierarchy = d3.hierarchy(data).sum((d) => d[sizeField]);
+    const hierarchy = d3.hierarchy(data).sum((d) => parseInt(d[sizeField]));
+    const pack = d3.pack().size([width, height]).padding(4);
+    const root = pack(hierarchy);
+    setNodes(root.descendants().slice(1));
+  }, [width, height, data]);
 
-  const pack = d3.pack().size([width, height]).padding(4);
-  const root = pack(hierarchy);
-  const nodes = root.descendants().slice(1);
+  const allBubbles = useMemo(() => {
+    return nodes.map((node, idx) => {
+      const { x, y, r } = node;
+      const d = r * 2;
+      const circleClassName = bubbleClassName?.(node) || bubbleClassName;
+      const bubbleContent = renderBubbleContent?.(node) || null;
 
-  const allBubbles = nodes.map((node, idx) => {
-    const { x, y, r } = node;
-    const d = r * 2;
-
-    const circleClassName = bubbleClassName?.(node) || bubbleClassName;
-
-    const bubbleContent = renderBubbleContent?.(node) || null;
-
-    return (
-      <Fragment key={node.data[keyField] || idx}>
-        <circle cx={x} cy={y} r={r} className={circleClassName} />
-        {bubbleContent && (
-          <foreignObject
-            x={x - r}
-            y={y - r}
-            width={d}
-            height={d}
-            className="rounded-full"
-          >
-            {bubbleContent}
-          </foreignObject>
-        )}
-      </Fragment>
-    );
-  });
+      return (
+        <Fragment key={node.data[keyField] || idx}>
+          <circle cx={x} cy={y} r={r} className={circleClassName} />
+          {bubbleContent && (
+            <foreignObject
+              x={x - r}
+              y={y - r}
+              width={d}
+              height={d}
+              className="rounded-full"
+            >
+              {bubbleContent}
+            </foreignObject>
+          )}
+        </Fragment>
+      );
+    });
+  }, [nodes]);
 
   return (
     <svg width={width} height={height}>
