1840305f64b9a18d7727aee87a19ca8f66c2322c
2nthony <ant.hu@outlook.com>
2023-08-29 18:00:50 +0800

    fix: post title alignment

--- packages/next-common/components/postList/postTitle.js
+++ packages/next-common/components/postList/postTitle.js
@@ -1,39 +1,20 @@
-import styled from "styled-components";
 import isNil from "lodash.isnil";
-import React from "react";
 import ReasonLink from "../reasonLink";
 import Link from "next/link";
-import { Index } from "./styled";
-
-const TitleWrapper = styled.div`
-  flex: 1;
-  overflow: hidden;
-  color: var(--textPrimary);
-`;
-
-const Title = styled.a`
-  word-break: break-word;
-  font-weight: 500;
-  font-size: 16px;
-  cursor: pointer;
-  line-height: 140%;
-
-  :hover {
-    text-decoration: underline;
-  }
-`;
 
 export default function ListPostTitle({ data = {}, href }) {
   let title = data.title?.trim() || "--";
 
   return (
-    <TitleWrapper>
-      {!isNil(data?.index) && <Index>{`#${data.index}`}</Index>}
+    <div className="flex-1 overflow-hidden text-textPrimary text16Medium">
+      {!isNil(data?.index) && (
+        <span className="after:content-['·'] after:mx-2 after:text-textTertiary">{`#${data.index}`}</span>
+      )}
 
-      <Link href={href} passHref legacyBehavior>
-        <Title>{title}</Title>
+      <Link href={href} className="break-words cursor-pointer hover:underline">
+        {title}
       </Link>
       <ReasonLink text={data.title} hideText={true} />
-    </TitleWrapper>
+    </div>
   );
 }
