2dd16d52b4a6eade22d6f13c8782570a452eab17
2nthony <ant.hu@outlook.com>
2023-08-30 18:25:00 +0800

    fix: optimize proposal external link

--- packages/next-common/components/postDataSource.js
+++ packages/next-common/components/postDataSource.js
@@ -1,33 +1,10 @@
-import React from "react";
-import styled from "styled-components";
-import ExternalLinkIcon from "next-common/components/icons/externalLink";
 import Chains from "../utils/consts/chains";
 import { usePost } from "../context/post";
 import { useChain } from "../context/chain";
 import { getPolkassemblyLink } from "next-common/utils/polkassembly";
 import { useDetailType } from "../context/page";
 import { GreyPanel } from "./styled/containers/greyPanel";
-
-const ExternalReference = styled(GreyPanel)`
-  padding: 12px;
-  margin-top: 16px;
-  gap: 8px;
-
-  height: 41px;
-
-  span {
-    font-style: normal;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 140%;
-
-    color: var(--textTertiary);
-  }
-
-  a {
-    display: flex;
-  }
-`;
+import ExternalLink from "./externalLink";
 
 export default function PostDataSource() {
   const post = usePost();
@@ -51,11 +28,10 @@ export default function PostDataSource() {
   }
 
   return (
-    <ExternalReference>
-      <span>Polkassembly</span>
-      <a href={paLink} target="_blank" rel="noreferrer">
-        <ExternalLinkIcon color="var(--theme500)" />
-      </a>
-    </ExternalReference>
+    <GreyPanel className="!rounded-lg px-4 py-2.5 mt-4 gap-2">
+      <ExternalLink className="text14Medium text-sapphire500" href={paLink}>
+        Polkassembly <span className="text-textSecondary">↗</span>
+      </ExternalLink>
+    </GreyPanel>
   );
 }
