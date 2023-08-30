9aa16d48891e494e934ebfa7fd79859565236dc6
2nthony <ant.hu@outlook.com>
2023-08-30 12:01:28 +0800

    fix: dark reason link icons

--- packages/next-common/utils/viewfuncs/tip.jsx
+++ packages/next-common/utils/viewfuncs/tip.jsx
@@ -1,20 +1,26 @@
 import YoutubeSvg from "../../assets/imgs/icons/youtube-logo.svg";
-import GithubSvg from "../../assets/imgs/icons/github-logo.svg";
 import LinkSvg from "../../assets/imgs/icons/link-icon.svg";
 import MediumSvg from "../../assets/imgs/icons/medium-logo.svg";
 import PolkassemblySvg from "../../assets/imgs/icons/polkassembly-logo.svg";
 import TwitterSvg from "../../assets/imgs/icons/twitter-logo.svg";
 import TelegramSvg from "../../assets/imgs/icons/telegram-logo.svg";
-import googleDocSvg from "../../assets/imgs/icons/googledoc-logo.svg";
-import googleDriveSvg from "../../assets/imgs/icons/googledrive-logo.svg";
+import GoogleDocSvg from "../../assets/imgs/icons/googledoc-logo.svg";
+import GoogleDriveSvg from "../../assets/imgs/icons/googledrive-logo.svg";
 import OpenSquareSvg from "../../assets/imgs/icons/opensquare-logo.svg";
-import dotreasurySvg from "../../assets/imgs/icons/dotreasury-logo.svg";
-import subSquareSvg from "../../assets/imgs/icons/subsquare-logo.svg";
+import DotreasurySvg from "../../assets/imgs/icons/dotreasury-logo.svg";
+import {
+  LinkGithub,
+  ProjectIconSubsquareDark,
+  ProjectIconSubsquareLight,
+  SystemLink,
+} from "@osn/icons/subsquare";
 
 export const extractLinks = (text) =>
   [...text.matchAll(/(https?:\/\/[^ ]+)/g)].map((item) => item[0]);
 
-export const defaultLinkSvg = LinkSvg;
+export const defaultLinkSvg = (
+  <SystemLink className="w-5 h-5 [&_path]:fill-textPrimary" />
+);
 
 export function getLinkIcon(link) {
   const url = new URL(link);
@@ -24,25 +30,30 @@ export function getLinkIcon(link) {
     url.host.endsWith("github.com") ||
     url.host.endsWith("github.io")
   ) {
-    return GithubSvg;
+    return <LinkGithub className="w-5 h-5 [&_path]:fill-textPrimary" />;
   } else if (url.host.endsWith("medium.com")) {
-    return MediumSvg;
+    return <MediumSvg />;
   } else if (url.host.endsWith("polkassembly.io")) {
-    return PolkassemblySvg;
+    return <PolkassemblySvg />;
   } else if (url.host.endsWith("twitter.com")) {
-    return TwitterSvg;
+    return <TwitterSvg />;
   } else if (url.host === "t.me") {
-    return TelegramSvg;
+    return <TelegramSvg />;
   } else if (url.host.endsWith("docs.google.com")) {
-    return googleDocSvg;
+    return <GoogleDocSvg />;
   } else if (url.host.endsWith("drive.google.com")) {
-    return googleDriveSvg;
+    return <GoogleDriveSvg />;
   } else if (url.host.endsWith("opensquare.network")) {
-    return OpenSquareSvg;
+    return <OpenSquareSvg />;
   } else if (url.host.endsWith("dotreasury.com")) {
-    return dotreasurySvg;
+    return <DotreasurySvg />;
   } else if (url.host.endsWith("subsquare.io")) {
-    return subSquareSvg;
+    return (
+      <>
+        <ProjectIconSubsquareLight className="w-5 h-5 dark:hidden" />
+        <ProjectIconSubsquareDark className="w-5 h-5 hidden dark:block" />
+      </>
+    );
   }
 
   return LinkSvg;
