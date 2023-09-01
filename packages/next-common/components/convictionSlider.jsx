1824719e61ba395fdfc705a04a1b22de1b9bd6b9
2nthony <ant.hu@outlook.com>
2023-09-01 16:44:34 +0800

    remove label

--- packages/next-common/components/convictionSlider.jsx
+++ packages/next-common/components/convictionSlider.jsx
@@ -6,31 +6,24 @@ import last from "lodash.last";
 
 const DEFAULT_OPTIONS = [
   {
-    label: "0.1x, no lockup period",
     value: 0,
   },
   {
-    label: "1x, locked for 1 enactment period(s)",
     value: 1,
   },
   {
-    label: "2x, locked for 2 enactment period(s)",
     value: 2,
   },
   {
-    label: "3x, locked for 4 enactment period(s)",
     value: 3,
   },
   {
-    label: "4x, locked for 8 enactment period(s)",
     value: 4,
   },
   {
-    label: "5x, locked for 16 enactment period(s)",
     value: 5,
   },
   {
-    label: "6x, locked for 32 enactment period(s)",
     value: 6,
   },
 ];
