33e8e0025dedbf56400db705f7b5cfed9ca6d7f8
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-02 15:53:18 +0800

    Save referendum tally history to redux, #3219

---
+++ packages/next-common/utils/hooks/referenda/useFetchReferendaTallyHistory.js
@@ -0,0 +1,17 @@
+import nextApi from "next-common/services/nextApi";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setTallyHistory } from "next-common/store/reducers/referenda/tallyHistory";
+
+export default function useFetchReferendaTallyHistory(referendumIndex) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    nextApi
+      .fetch(`gov2/referenda/${referendumIndex}/tally-history`)
+      .then(({ result }) => {
+        if (!result) return;
+        dispatch(setTallyHistory(result));
+      });
+  }, [referendumIndex]);
+}
