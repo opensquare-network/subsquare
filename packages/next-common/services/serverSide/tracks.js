afe5727d929b4306b281cc58660fffa73b760fde
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-09 18:17:08 +0800

    Improve tracks fetching

--- packages/next-common/services/serverSide/tracks.js
+++ packages/next-common/services/serverSide/tracks.js
@@ -1,7 +1,8 @@
 import { ssrNextApi } from "next-common/services/nextApi";
 import { fellowshipTracksApi, gov2TracksApi } from "next-common/services/url";
+import getChainSettings from "next-common/utils/consts/settings";
 
-export async function fetchOpenGovTracksProps() {
+async function fetchAll() {
   const [{ result: tracks }, { result: fellowshipTracks }] = await Promise.all([
     ssrNextApi.fetch(gov2TracksApi),
     ssrNextApi.fetch(fellowshipTracksApi),
@@ -11,4 +12,24 @@ export async function fetchOpenGovTracksProps() {
     tracks: tracks ?? [],
     fellowshipTracks: fellowshipTracks ?? [],
   };
+}
+
+export async function fetchOpenGovTracksProps() {
+  const { hasReferenda, hasFellowship } = getChainSettings(process.env.CHAIN);
+  if (hasReferenda && hasFellowship) {
+    return await fetchAll();
+  }
+
+  if (hasReferenda) {
+    const { result: tracks } = await ssrNextApi.fetch(gov2TracksApi);
+    return { tracks: tracks ?? [], fellowshipTracks: [] };
+  }
+  if (hasFellowship) {
+    const { result: fellowshipTracks } = await ssrNextApi.fetch(
+      fellowshipTracksApi,
+    );
+    return { tracks: [], fellowshipTracks: fellowshipTracks ?? [] };
+  }
+
+  return { tracks: [], fellowshipTracks: [] };
 }
