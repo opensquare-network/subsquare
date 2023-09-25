c1cb5f8fff62bc421faee4b7c8b3f32bafbfc306
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-25 17:13:22 +0800

    Extract ListWrapper component, #3448

--- packages/next/components/myvotes/mobile/votesList.js
+++ packages/next/components/myvotes/mobile/votesList.js
@@ -1,25 +1,16 @@
 import VoteListItem from "./voteListItem";
-import EmptyList from "next-common/components/profile/votingHistory/mobile/emptyList";
-import LoadingList from "next-common/components/profile/votingHistory/mobile/loadingList";
 import ProxyHint from "../proxyHint";
+import ListWrapper from "./listWrapper";
 
 export default function MobileVotesList({ votes, isLoading }) {
-  if (isLoading) {
-    return <LoadingList />;
-  }
-
-  if (votes.length === 0) {
-    return <EmptyList />;
-  }
-
-  const listContent = (votes || []).map((item) => (
-    <VoteListItem key={item.referendumIndex} vote={item} />
-  ));
-
   return (
-    <div className="flex flex-col gap-[16px]">
-      <ProxyHint />
-      {listContent}
-    </div>
+    <ListWrapper isLoading={isLoading}>
+      <div className="flex flex-col gap-[16px]">
+        <ProxyHint />
+        {(votes || []).map((item) => (
+          <VoteListItem key={item.referendumIndex} vote={item} />
+        ))}
+      </div>
+    </ListWrapper>
   );
 }
