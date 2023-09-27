a85015a652b6719e6b8ce09537d60b9c9fd76d34
2nthony <ant.hu@outlook.com>
2023-09-27 15:04:16 +0800

    no comment

--- packages/next-common/components/comment/noComment.js
+++ packages/next-common/components/comment/noComment.js
@@ -1,14 +1,13 @@
-import React from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  padding: 16px 48px;
-  text-align: center;
-  font-size: 14px;
-  line-height: 140%;
-  color: var(--textTertiary);
-`;
+import { SystemEmptyBox } from "@osn/icons/subsquare";
 
 export default function NoComment() {
-  return <Wrapper>There are no comments here</Wrapper>;
+  return (
+    <div className="py-6">
+      <SystemEmptyBox className="[&_path]:stroke-textTertiary w-10 h-10 mx-auto" />
+
+      <p className="mt-2 text14Medium text-textTertiary text-center">
+        There are no comments here
+      </p>
+    </div>
+  );
 }
