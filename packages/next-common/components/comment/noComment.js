eda633083f01a9c45a3756983137f801c0627a80
2nthony <ant.hu@outlook.com>
2023-09-27 15:21:38 +0800

    refactor

--- packages/next-common/components/comment/noComment.js
+++ packages/next-common/components/comment/noComment.js
@@ -1,13 +1,5 @@
-import { SystemEmptyBox } from "@osn/icons/subsquare";
+import NoData from "../noData";
 
 export default function NoComment() {
-  return (
-    <div className="py-6">
-      <SystemEmptyBox className="[&_path]:stroke-textTertiary w-10 h-10 mx-auto" />
-
-      <p className="mt-2 text14Medium text-textTertiary text-center">
-        There are no comments here
-      </p>
-    </div>
-  );
+  return <NoData text="There are no comments here" />;
 }
