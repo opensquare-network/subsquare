309fb0ecf901ea019cd9433b6e51c7ec039965b9
Yongfeng LI <wliyongfeng@gmail.com>
2023-09-30 22:23:52 +0800

    Fix block time

--- packages/next-common/utils/hooks/index.js
+++ packages/next-common/utils/hooks/index.js
@@ -19,24 +19,24 @@ export function useBlockTime(api) {
     }
 
     const THRESHOLD = BN_THOUSAND.div(BN_TWO);
-    dispatch(
-      setBlockTime(
-        api.consts?.babe?.expectedBlockTime ||
-          // POW, eg. Kulupu
-          api.consts?.difficulty?.targetBlockTime ||
-          // Subspace
-          api.consts?.subspace?.expectedBlockTime ||
-          // Check against threshold to determine value validity
-          (api.consts?.timestamp?.minimumPeriod.gte(THRESHOLD)
-            ? // Default minimum period config
-              api.consts?.timestamp.minimumPeriod.mul(BN_TWO)
-            : api.query?.parachainSystem
-            ? // default guess for a parachain
-              DEFAULT_TIME.mul(BN_TWO)
-            : // default guess for others
-              DEFAULT_TIME),
-      ),
-    );
+    const blockTime =
+      api.consts?.babe?.expectedBlockTime ||
+      // POW, eg. Kulupu
+      api.consts?.difficulty?.targetBlockTime ||
+      // Subspace
+      api.consts?.subspace?.expectedBlockTime ||
+      // Check against threshold to determine value validity
+      (api.consts?.timestamp?.minimumPeriod.gte(THRESHOLD)
+        ? // Default minimum period config
+          api.consts?.timestamp.minimumPeriod.mul(BN_TWO)
+        : api.query?.parachainSystem
+        ? // default guess for a parachain
+          DEFAULT_TIME.mul(BN_TWO)
+        : // default guess for others
+          DEFAULT_TIME);
+    if (blockTime) {
+      dispatch(setBlockTime(blockTime.toNumber()));
+    }
   }, [api]);
 }
 
