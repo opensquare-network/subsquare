54e648d225d3f3824726ae80fd102b209daedfa4
2nthony <ant.hu@outlook.com>
2023-10-09 18:00:35 +0800

    fix imports

--- packages/next-common/components/polkassembly/comment/index.js
+++ packages/next-common/components/polkassembly/comment/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Item from "./item";
+import PolkassemblyCommentItem from "next-common/components/comment/polkassemblyCommentItem";
 import NoComment from "next-common/components/comment/noComment";
 import PolkassemblyCommentButton from "./commentButton";
 import Loading from "next-common/components/loading";
@@ -36,7 +36,7 @@ export default function PolkassemblyComments({
         <>
           <div>
             {(comments || []).map((item, idx) => (
-              <Item key={idx} data={item} />
+              <PolkassemblyCommentItem key={idx} data={item} />
             ))}
           </div>
         </>
