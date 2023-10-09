e698af6d4df23d57a1ef937e9e016831a81dcdb4
2nthony <ant.hu@outlook.com>
2023-10-09 17:24:02 +0800

    refactor: comment item

--- packages/next-common/components/comment/item.js
+++ packages/next-common/components/comment/item.js
@@ -1,10 +1,8 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import styled, { css } from "styled-components";
 import nextApi from "next-common/services/nextApi";
 import { useDispatch } from "react-redux";
 import { newErrorToast } from "next-common/store/reducers/toastSlice";
 import EditInput from "next-common/components/editInput";
-import Flex from "next-common/components/styled/flex";
 import { useIsMountedBool } from "../../utils/hooks/useIsMounted";
 import {
   HtmlPreviewer,
@@ -13,64 +11,15 @@ import {
 } from "@osn/previewer";
 import IdentityOrAddr from "../IdentityOrAddr";
 import { prettyHTML } from "../../utils/viewfuncs";
-import RichTextStyleWrapper from "../content/richTextStyleWrapper";
 import CommentActions from "../actions/commentActions";
 import copy from "copy-to-clipboard";
-import useDuration from "../../utils/hooks/useDuration";
 import { useUser } from "../../context/user";
 import useCommentsAnchor from "../../utils/hooks/useCommentsAnchor";
-import Divider from "next-common/components/styled/layout/divider";
-import clsx from "clsx";
 import { useComments, useSetComments } from "next-common/context/post/comments";
 import SystemUser from "../user/systemUser";
 import { LinkSubsquare } from "@osn/icons/subsquare";
 import Tooltip from "../tooltip";
-
-const Wrapper = styled.div`
-  position: relative;
-
-  :hover {
-    .edit {
-      display: block;
-    }
-  }
-`;
-
-const InfoWrapper = styled(Flex)`
-  min-height: 28px;
-  justify-content: space-between;
-  flex-wrap: wrap;
-`;
-
-const ContentWrapper = styled(RichTextStyleWrapper)`
-  margin: 8px 0 0 28px;
-`;
-
-const EditedLabel = styled.div`
-  margin-top: 8px;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 12px;
-  color: var(--textTertiary);
-`;
-
-const IndentWrapper = styled.div`
-  margin: 16px 0 0 28px;
-  ${(p) =>
-    p.quoted &&
-    css`
-      padding-left: 16px;
-      border-left: 3px solid var(--neutral300);
-    `};
-`;
-
-const FoldButton = styled.button`
-  color: var(--textSecondary);
-  &:hover {
-    color: var(--textPrimary);
-    cursor: pointer;
-  }
-`;
+import CommentItemTemplate from "./itemTemplate";
 
 function jumpToAnchor(anchorId) {
   var anchorElement = document.getElementById(anchorId);
@@ -96,14 +45,12 @@ export default function Item({
 }) {
   const user = useUser();
   const dispatch = useDispatch();
-  const ref = useRef();
   const refCommentTree = useRef();
   const [thumbUpLoading, setThumbUpLoading] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [loading, setLoading] = useState(false);
   const [highlight, setHighlight] = useState(false);
   const isMounted = useIsMountedBool();
-  const duration = useDuration(comment.createdAt);
   const { hasAnchor, anchor } = useCommentsAnchor();
   const [folded, setFolded] = useState(true);
   const comments = useComments();
@@ -197,131 +144,99 @@ export default function Item({
   };
 
   return (
-    <Wrapper
+    <CommentItemTemplate
       ref={refCommentTree}
+      data={comment}
+      isSecondLevel={isSecondLevel}
+      folded={folded}
+      setFolded={setFolded}
       id={comment.height}
-      className={clsx(
-        "group/comment-item first:mt-0",
-        !isSecondLevel ? "mt-8" : "mt-4",
-        highlight &&
-          clsx(
-            "bg-neutral200 -mx-12 px-12 py-4 !mt-4",
-            "max-md:-mx-6 max-md:px-6",
-            isSecondLevel && clsx("-ml-4 pl-4", "max-md:-ml-4 max-md:pl-4"),
-          ),
-      )}
-    >
-      <InfoWrapper>
-        <SystemUser user={comment.author} />
-        <div className="flex items-center gap-x-2">
-          <p className="text12Medium text-textTertiary">{duration}</p>
-          <Tooltip content="Post from SubSquare">
-            <LinkSubsquare className="w-4 h-4 [&_path]:fill-textTertiary" />
-          </Tooltip>
-        </div>
-      </InfoWrapper>
-      {!isEdit && (
+      highlight={highlight}
+      user={<SystemUser user={comment.author} />}
+      from={
+        <Tooltip content="Post from SubSquare">
+          <LinkSubsquare className="w-4 h-4 [&_path]:fill-textTertiary" />
+        </Tooltip>
+      }
+      content={
         <>
-          <ContentWrapper ref={ref}>
-            {comment.contentType === "markdown" && (
-              <MarkdownPreviewer
-                content={comment.content}
-                plugins={[renderMentionIdentityUserPlugin(<IdentityOrAddr />)]}
-              />
-            )}
-            {comment.contentType === "html" && (
-              <HtmlPreviewer
-                content={prettyHTML(comment.content)}
-                plugins={[
-                  renderMentionIdentityUserPlugin(<IdentityOrAddr />, {
-                    targetElement: { tag: "span" },
-                  }),
-                ]}
-              />
-            )}
-            {comment.createdAt !== comment.updatedAt && (
-              <EditedLabel>Edited</EditedLabel>
-            )}
-          </ContentWrapper>
-          <div className="mt-2 ml-7">
-            <CommentActions
-              setFolded={setFolded}
-              updateComment={updateTopLevelComment || updateComment}
-              scrollToNewReplyComment={
-                scrollToTopLevelCommentBottom || scrollToCommentBottom
-              }
-              replyToCommentId={replyToCommentId}
-              highlight={isLoggedIn && thumbUp}
-              noHover={!isLoggedIn || ownComment}
-              edit={ownComment}
-              setIsEdit={setIsEdit}
-              toggleThumbUp={toggleThumbUp}
-              thumbUpLoading={thumbUpLoading}
-              reactions={comment.reactions}
-              author={comment.author}
-              copy
-              onCopy={() => {
-                copy(
-                  `${window.location.origin}${window.location.pathname}${window.location.search}#${comment.height}`,
-                );
-              }}
-            />
-          </div>
-        </>
-      )}
-      {isEdit && (
-        <EditInput
-          editContent={comment.content}
-          editContentType={comment.contentType}
-          onFinishedEdit={async (reload) => {
-            if (reload) {
-              await updateComment();
-            }
-            if (isMounted()) {
-              setIsEdit(false);
-            }
-          }}
-          update={editComment}
-          loading={loading}
-          setLoading={setLoading}
-        />
-      )}
-      {comment.replies?.length > 0 && (
-        <IndentWrapper quoted>
-          <FoldButton
-            className="text14Medium"
-            onClick={() => {
-              setFolded(!folded);
-            }}
-          >
-            {folded ? `${comment.replies?.length} Replies` : "Hide Replies"}
-          </FoldButton>
-
-          {!folded
-            ? (comment.replies || []).map((item) => (
-                <Item
-                  key={item._id}
-                  data={item}
-                  replyToCommentId={replyToCommentId}
-                  isSecondLevel={true}
-                  updateTopLevelComment={updateTopLevelComment || updateComment}
-                  scrollToTopLevelCommentBottom={
-                    scrollToTopLevelCommentBottom || scrollToCommentBottom
-                  }
+          {!isEdit && (
+            <>
+              {comment.contentType === "markdown" && (
+                <MarkdownPreviewer
+                  content={comment.content}
+                  plugins={[
+                    renderMentionIdentityUserPlugin(<IdentityOrAddr />),
+                  ]}
                 />
-              ))
-            : null}
-        </IndentWrapper>
-      )}
-
-      {!isSecondLevel && (
-        <Divider
-          className={clsx(
-            "group-last/comment-item:hidden",
-            "ml-7 relative top-4",
+              )}
+              {comment.contentType === "html" && (
+                <HtmlPreviewer
+                  content={prettyHTML(comment.content)}
+                  plugins={[
+                    renderMentionIdentityUserPlugin(<IdentityOrAddr />, {
+                      targetElement: { tag: "span" },
+                    }),
+                  ]}
+                />
+              )}
+            </>
           )}
+          {isEdit && (
+            <EditInput
+              editContent={comment.content}
+              editContentType={comment.contentType}
+              onFinishedEdit={async (reload) => {
+                if (reload) {
+                  await updateComment();
+                }
+                if (isMounted()) {
+                  setIsEdit(false);
+                }
+              }}
+              update={editComment}
+              loading={loading}
+              setLoading={setLoading}
+            />
+          )}
+        </>
+      }
+      actions={
+        <CommentActions
+          setFolded={setFolded}
+          updateComment={updateTopLevelComment || updateComment}
+          scrollToNewReplyComment={
+            scrollToTopLevelCommentBottom || scrollToCommentBottom
+          }
+          replyToCommentId={replyToCommentId}
+          highlight={isLoggedIn && thumbUp}
+          noHover={!isLoggedIn || ownComment}
+          edit={ownComment}
+          setIsEdit={setIsEdit}
+          toggleThumbUp={toggleThumbUp}
+          thumbUpLoading={thumbUpLoading}
+          reactions={comment.reactions}
+          author={comment.author}
+          copy
+          onCopy={() => {
+            copy(
+              `${window.location.origin}${window.location.pathname}${window.location.search}#${comment.height}`,
+            );
+          }}
+        />
+      }
+      renderReply={(reply) => (
+        <Item
+          key={reply._id}
+          data={reply}
+          replyToCommentId={replyToCommentId}
+          isSecondLevel
+          updateTopLevelComment={updateTopLevelComment || updateComment}
+          scrollToTopLevelCommentBottom={
+            scrollToTopLevelCommentBottom || scrollToCommentBottom
+          }
         />
       )}
-    </Wrapper>
+    />
   );
 }
