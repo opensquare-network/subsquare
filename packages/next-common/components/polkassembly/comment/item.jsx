5adcaf70af3ced601e954e80e06b86cccd60b613
2nthony <ant.hu@outlook.com>
2023-10-09 16:35:31 +0800

    refactor: polkassembly comment item

--- packages/next-common/components/polkassembly/comment/item.jsx
+++ packages/next-common/components/polkassembly/comment/item.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-import styled, { css } from "styled-components";
-import Flex from "next-common/components/styled/flex";
 import {
   MarkdownPreviewer,
   renderMentionIdentityUserPlugin,
 } from "@osn/previewer";
 import IdentityOrAddr from "next-common/components/IdentityOrAddr";
 import PolkassemblyActions from "../actions";
-import RichTextStyleWrapper from "next-common/components/content/richTextStyleWrapper";
-import useDuration from "next-common/utils/hooks/useDuration";
-import Divider from "next-common/components/styled/layout/divider";
 import clsx from "clsx";
 import PolkassemblyUser from "next-common/components/user/polkassemblyUser";
 import { LinkPolkassembly } from "@osn/icons/subsquare";
@@ -18,127 +12,48 @@ import ExternalLink from "next-common/components/externalLink";
 import { getPolkassemblyLink } from "next-common/utils/polkassembly";
 import { useDetailType } from "next-common/context/page";
 import { usePost } from "next-common/context/post";
+import CommentItemTemplate from "next-common/components/comment/itemTemplate";
 
-const Wrapper = styled.div`
-  position: relative;
-`;
-
-const InfoWrapper = styled(Flex)`
-  min-height: 28px;
-  justify-content: space-between;
-  flex-wrap: wrap;
-`;
-
-const ContentWrapper = styled(RichTextStyleWrapper)`
-  margin: 8px 0 0 28px;
-`;
-
-const IndentWrapper = styled.div`
-  margin: 16px 0 0 28px;
-  ${(p) =>
-    p.quoted &&
-    css`
-      padding-left: 16px;
-      border-left: 3px solid var(--neutral300);
-    `};
-`;
-
-const FoldButton = styled.button`
-  color: var(--textSecondary);
-  &:hover {
-    color: var(--textPrimary);
-    cursor: pointer;
-  }
-`;
-
-const EditedLabel = styled.div`
-  margin-top: 8px;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 12px;
-  color: var(--textTertiary);
-`;
-
-export default function PolkassemblyCommentItem({ data, isSecondLevel }) {
-  const comment = data;
-
-  const [folded, setFolded] = React.useState(true);
-  const duration = useDuration(comment.createdAt);
+export default function PolkassemblyCommentItem({
+  data: comment,
+  isSecondLevel,
+}) {
   const type = useDetailType();
   const post = usePost();
 
   return (
-    <Wrapper
-      className={clsx(
-        "group/comment-item first:mt-0",
-        !isSecondLevel ? "mt-8" : "mt-4",
-      )}
-    >
-      <InfoWrapper>
-        <PolkassemblyUser user={comment.author} />
-        <div className="flex items-center gap-x-2">
-          <p className="text12Medium text-textTertiary">{duration}</p>
-          <Tooltip content="Post from Polkassembly">
-            <ExternalLink
-              href={`${getPolkassemblyLink(type, post)}#${data.id}`}
-              externalIcon={false}
-            >
-              <LinkPolkassembly
-                className={clsx(
-                  "w-4 h-4",
-                  "[&_path]:fill-textTertiary",
-                  "[&_path]:hover:fill-textSecondary",
-                )}
-              />
-            </ExternalLink>
-          </Tooltip>
-        </div>
-      </InfoWrapper>
-      <ContentWrapper>
+    <CommentItemTemplate
+      data={comment}
+      isSecondLevel={isSecondLevel}
+      user={<PolkassemblyUser user={comment.author} />}
+      from={
+        <Tooltip content="Post from Polkassembly">
+          <ExternalLink
+            href={`${getPolkassemblyLink(type, post)}#${comment.id}`}
+            externalIcon={false}
+          >
+            <LinkPolkassembly
+              className={clsx(
+                "w-4 h-4",
+                "[&_path]:fill-textTertiary",
+                "[&_path]:hover:fill-textSecondary",
+              )}
+            />
+          </ExternalLink>
+        </Tooltip>
+      }
+      content={
         <MarkdownPreviewer
           content={comment.content}
           plugins={[renderMentionIdentityUserPlugin(<IdentityOrAddr />)]}
         />
-        {comment.createdAt !== comment.updatedAt && (
-          <EditedLabel>Edited</EditedLabel>
-        )}
-      </ContentWrapper>
-      {!isSecondLevel && (
-        <IndentWrapper>
-          <PolkassemblyActions reactions={comment.reactions} />
-        </IndentWrapper>
+      }
+      actions={
+        !isSecondLevel && <PolkassemblyActions reactions={comment.reactions} />
+      }
+      renderReply={(reply) => (
+        <PolkassemblyCommentItem key={reply.id} data={reply} isSecondLevel />
       )}
-      {comment.replies?.length > 0 && (
-        <IndentWrapper quoted>
-          <FoldButton
-            className="text14Medium"
-            onClick={() => {
-              setFolded(!folded);
-            }}
-          >
-            {folded ? `${comment.replies?.length} Replies` : "Hide Replies"}
-          </FoldButton>
-
-          {!folded
-            ? (comment.replies || []).map((item) => (
-                <PolkassemblyCommentItem
-                  key={item.id}
-                  data={item}
-                  isSecondLevel={true}
-                />
-              ))
-            : null}
-        </IndentWrapper>
-      )}
-
-      {!isSecondLevel && (
-        <Divider
-          className={clsx(
-            "group-last/comment-item:hidden",
-            "ml-7 relative top-4",
-          )}
-        />
-      )}
-    </Wrapper>
+    />
   );
 }
