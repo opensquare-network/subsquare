9cc7b9afe3e3ed414e1c53b6d59db9a8f9bb9759
2nthony <ant.hu@outlook.com>
2023-10-10 17:38:34 +0800

    fix: child bounties table

--- packages/next/components/bounty/childBountiesTable.js
+++ packages/next/components/bounty/childBountiesTable.js
@@ -1,92 +1,12 @@
-import Row from "next-common/components/listInfo/row";
 import Anchor from "next-common/components/styled/anchor";
 import { toPrecision } from "next-common/utils";
 import Tag from "next-common/components/tags/state/tag";
 import styled from "styled-components";
-import Flex from "next-common/components/styled/flex";
-import FlexBetween from "next-common/components/styled/flexBetween";
-import Divider from "next-common/components/styled/layout/divider";
 import businessCategory from "next-common/utils/consts/business/category";
 import ValueDisplay from "next-common/components/valueDisplay";
 import { useChainSettings } from "next-common/context/chain";
-
-const ChildBountyWrapper = styled.div`
-  > div:first-child {
-    @media screen and (max-width: 768px) {
-      display: none;
-    }
-  }
-`;
-
-const ChildBounty = styled(FlexBetween)`
-  flex-grow: 1;
-
-  a {
-    flex-basis: 392px;
-    font-weight: 400;
-    white-space: pre-wrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    color: var(--textPrimary);
-  }
-
-  > div {
-    flex-basis: 240px;
-    > div:first-child {
-      flex-basis: 120px;
-      display: flex;
-      justify-content: end;
-    }
-    .symbol {
-      color: var(--textTertiary);
-    }
-  }
-  > div > span {
-    height: 20px;
-  }
-`;
-
-const ChildBountyMobile = styled(Flex)`
-  display: none;
-  @media screen and (max-width: 768px) {
-    display: flex;
-  }
-  flex-wrap: wrap;
-  padding: 12px 0;
-
-  a {
-    flex-basis: 100%;
-    padding-right: 30px;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  > div {
-    width: 100%;
-  }
-`;
-
-const SemiBold = styled.span`
-  font-weight: 500;
-`;
-
-const DividerWrapper = styled(FlexBetween)`
-  flex-wrap: wrap;
-
-  > :not(:first-child) {
-    ::before {
-      content: "·";
-      font-size: 12px;
-      color: var(--textTertiary);
-      margin: 0 8px;
-    }
-  }
-
-  > div {
-    display: flex;
-  }
-`;
+import clsx from "clsx";
+import KvList from "next-common/components/listInfo/kvList";
 
 const DomesticLink = styled.a`
   margin-top: 16px;
@@ -98,66 +18,52 @@ const DomesticLink = styled.a`
   cursor: pointer;
 `;
 
-function ChildBountiesTable({ childBounties }) {
+export default function ChildBountiesTable({ childBounties }) {
   const { decimals, symbol } = useChainSettings();
   if (!childBounties?.items || !childBounties?.items?.length) {
     return null;
   }
 
+  const listData = childBounties.items.map((bounty) => {
+    return [
+      <div
+        key={bounty.index}
+        className={clsx(
+          "flex items-center",
+          "w-full",
+          "text14Medium",
+          "max-sm:block max-sm:space-y-2",
+        )}
+      >
+        <div className="w-[160px] text-textSecondary">#{bounty.index}</div>
+        <div className="flex-1 w-full">
+          <Anchor
+            className="!text-textPrimary"
+            href={`/treasury/child-bounties/${bounty.parentBountyId}_${bounty.index}`}
+            title={bounty.title}
+          >
+            {bounty.title}
+          </Anchor>
+        </div>
+        <div className="w-[120px] text-right max-sm:text-left">
+          <ValueDisplay
+            value={toPrecision(bounty.onchainData?.value, decimals)}
+            symbol={symbol}
+          />
+        </div>
+        <div className="w-[120px] text-right max-sm:text-left">
+          <Tag
+            state={bounty.onchainData?.state?.state}
+            category={businessCategory.treasuryChildBounties}
+          />
+        </div>
+      </div>,
+    ];
+  });
+
   return (
     <div>
-      {childBounties.items.map((bounty, index) => {
-        return (
-          <ChildBountyWrapper key={index}>
-            <Row
-              row={[
-                `#${bounty.index}`,
-                <ChildBounty key="child-bounty">
-                  <Anchor
-                    href={`/treasury/child-bounties/${bounty.parentBountyId}_${bounty.index}`}
-                    title={bounty.title}
-                  >
-                    {bounty.title}
-                  </Anchor>
-                  <FlexBetween style={{ height: 23 }}>
-                    <ValueDisplay
-                      value={toPrecision(bounty.onchainData?.value, decimals)}
-                      symbol={symbol}
-                    />
-                    <Tag
-                      state={bounty.onchainData?.state?.state}
-                      category={businessCategory.treasuryChildBounties}
-                    />
-                  </FlexBetween>
-                </ChildBounty>,
-              ]}
-            />
-
-            <ChildBountyMobile key={index}>
-              <Anchor
-                href={`/treasury/child-bounties/${bounty.parentBountyId}_${bounty.index}`}
-                title={bounty.title}
-              >
-                {bounty.title}
-              </Anchor>
-              <Divider margin={8} />
-              <FlexBetween>
-                <DividerWrapper>
-                  <SemiBold>#{bounty.index}</SemiBold>
-                  <ValueDisplay
-                    value={toPrecision(bounty.onchainData?.value, decimals)}
-                    symbol={symbol}
-                  />
-                </DividerWrapper>
-                <Tag
-                  state={bounty.onchainData?.state?.state}
-                  category={businessCategory.treasuryChildBounties}
-                />
-              </FlexBetween>
-            </ChildBountyMobile>
-          </ChildBountyWrapper>
-        );
-      })}
+      <KvList data={listData} />
 
       {childBounties.total > 5 && (
         <DomesticLink
@@ -169,5 +75,3 @@ function ChildBountiesTable({ childBounties }) {
     </div>
   );
 }
-
-export default ChildBountiesTable;
