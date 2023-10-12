056d1c7944f892e990abb29baf1e480b49ff1dcb
2nthony <ant.hu@outlook.com>
2023-10-12 10:32:19 +0800

    fix: post labels

--- packages/next-common/components/postLabels.js
+++ packages/next-common/components/postLabels.js
@@ -12,7 +12,7 @@ export default function PostLabels({ labels = [] }) {
         <Link
           key={item}
           href={`/discussions?label=${encodeURIComponent(item)}`}
-          className="flex cursor-pointer px-0.5 py-2 text12Medium text-textSecondary border border-neutral400 rounded-xl"
+          className="flex cursor-pointer px-2 py-0.5 text12Medium text-textSecondary border border-neutral400 rounded-xl"
         >
           {item}
         </Link>
