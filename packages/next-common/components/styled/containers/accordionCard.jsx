ac9aff1f99bbfcd24aa216d438314ce5092c37fd
2nthony <ant.hu@outlook.com>
2023-10-17 17:10:16 +0800

    feat: Accordion Card,  #3562

---
+++ packages/next-common/components/styled/containers/accordionCard.jsx
@@ -0,0 +1,33 @@
+import { ArrowUp } from "@osn/icons/subsquare";
+import { cn } from "next-common/utils";
+import { useState } from "react";
+
+export default function AccordionCard({ children, title = "", defaultOpen }) {
+  const [open, setOpen] = useState(defaultOpen);
+
+  return (
+    <div
+      className={cn(
+        "bg-neutral100 shadow-100",
+        "border border-neutral300 rounded-xl",
+      )}
+    >
+      <div
+        className={cn(
+          "flex items-center justify-between",
+          "cursor-pointer p-6",
+          open && "pb-4",
+        )}
+        onClick={() => setOpen(!open)}
+      >
+        <h4 className="text14Bold text-textPrimary">{title}</h4>
+        <ArrowUp
+          role="button"
+          className={cn("[&_path]:stroke-textTertiary", !open && "rotate-180")}
+        />
+      </div>
+
+      <div className={cn(!open && "hidden", "p-6 pt-0")}>{children}</div>
+    </div>
+  );
+}
