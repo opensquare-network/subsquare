d5768d78a8d749a7766459549df9a4253ef7205c
Yongfeng LI <wliyongfeng@gmail.com>
2023-10-24 20:53:15 +0800

    Improve percentage view

--- packages/next-common/components/referenda/tally/support/percentage.js
+++ packages/next-common/components/referenda/tally/support/percentage.js
@@ -5,7 +5,7 @@ export default function Percentage({ perbill = 0 }) {
     return "0.0%";
   }
 
-  if (perbill > Math.pow(10, 7)) {
+  if (perbill > Math.pow(10, 6)) {
     const precision = perbill > 10 * Math.pow(10, 7) ? 1 : 2;
     return ((perbill / Math.pow(10, 9)) * 100).toFixed(precision) + "%";
   }
