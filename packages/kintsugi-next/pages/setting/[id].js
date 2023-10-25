7c190164fec400bc40bb81932f048b3436f05110
chaojun <chaojun@opensquare.network>
2023-10-25 12:46:07 +0800

    Update

--- packages/kintsugi-next/pages/setting/[id].js
+++ packages/kintsugi-next/pages/setting/[id].js
@@ -1,6 +1,6 @@
 import { getRedirectServerSidePropsByContext } from "next-common/services/serverSide";
 
-export default function Post() {
+export default function Setting() {
   return "Please visit `/settings/[id]`";
 }
 
