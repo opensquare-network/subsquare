ffcebd5116180ddb60545f8c87774204405cf708
Yongfeng LI <wliyongfeng@gmail.com>
2023-10-27 18:11:56 +0800

    Add border for tally nay value, #3578

--- packages/next/components/gov2/sidebar/tally/values/nay.js
+++ packages/next/components/gov2/sidebar/tally/values/nay.js
@@ -2,19 +2,19 @@ import { useSelector } from "react-redux";
 import NayIcon from "next-common/assets/imgs/icons/nay.svg";
 import LoadingCount from "./loadingCount";
 import SymbolValue from "./symbolValue";
-import { Row } from "next-common/components/referenda/tally/styled";
+import { BorderedRow } from "next-common/components/referenda/tally/styled";
 import { allNaySelector } from "next-common/store/reducers/referenda/votes/selectors";
 
 export default function Nay({ value = 0 }) {
   const allNay = useSelector(allNaySelector);
 
   return (
-    <Row>
+    <BorderedRow>
       <LoadingCount count={allNay.length}>
         <NayIcon />
         Nay
       </LoadingCount>
       <SymbolValue value={value} />
-    </Row>
+    </BorderedRow>
   );
 }
