7d4e41d1f54b0eb9bac3276595b4ab99ece71d7d
hyifeng <chaojun@opensquare.network>
2023-11-02 16:15:46 +0800

    Add subscription tip component on referenda page panel, #3445 (#3612)
    * Add subscription tip component on referenda page panel, #3445
    
    * Update

--- packages/next-common/components/layout/referendaLayout/index.jsx
+++ packages/next-common/components/layout/referendaLayout/index.jsx
@@ -1,7 +1,16 @@
+import { SubscribeTip, TitleExtra } from "next-common/components/overview";
 import ListLayout from "../ListLayout";
 import Gov2Summary from "next-common/components/summary/gov2Summary";
 import Gov2SummaryFooter from "next-common/components/summary/gov2SummaryFooter";
 import { useUser } from "next-common/context/user";
+
+function HeadContent() {
+  return (
+    <div className="md:hidden">
+      <SubscribeTip />
+    </div>
+  );
+}
 
 /**
  * @param {import("../ListLayout").ListLayoutProps & {summaryData: Record<string, any>}} props
@@ -11,8 +20,10 @@ export default function ReferendaLayout({ summaryData, ...props }) {
   const user = useUser();
   return (
     <ListLayout
+      titleExtra={<TitleExtra />}
       summary={<Gov2Summary summary={summaryData} />}
       summaryFooter={<Gov2SummaryFooter />}
+      headContent={<HeadContent />}
       description="All active and history referenda of various tracks."
       tabs={[
         { label: "Referenda", url: "/referenda" },
