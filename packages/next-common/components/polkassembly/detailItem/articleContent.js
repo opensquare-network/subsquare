67dd20c7d1b6dc3ddaf97194dd302cff64e905a5
2nthony <ant.hu@outlook.com>
2023-11-02 15:33:46 +0800

    polkassembly add content tabs

--- packages/next-common/components/polkassembly/detailItem/articleContent.js
+++ packages/next-common/components/polkassembly/detailItem/articleContent.js
@@ -5,8 +5,9 @@ import { MarkdownPreviewer } from "@osn/previewer";
 import RichTextStyleWrapper from "next-common/components/content/richTextStyleWrapper";
 import NoData from "next-common/components/noData";
 import { usePost } from "next-common/context/post";
-
-const Wrapper = styled(RichTextStyleWrapper)``;
+import AISummaryContent from "next-common/components/aiSummaryContent";
+import { useState } from "react";
+import Tabs from "next-common/components/tabs";
 
 const EditedLabel = styled.div`
   margin-top: 8px;
@@ -16,22 +17,56 @@ const EditedLabel = styled.div`
   color: var(--textTertiary);
 `;
 
-export default function ArticleContent({ postReactions }) {
+export default function ArticleContent({ postReactions, className = "" }) {
   const post = usePost();
 
   if (!post) {
     return null;
   }
 
+  const tabs = [
+    {
+      label: "Content",
+      content: (
+        <RichTextStyleWrapper>
+          <MarkdownPreviewer content={post.content} />
+        </RichTextStyleWrapper>
+      ),
+    },
+    post.aiSummaryContent && {
+      label: "AI Summary",
+      tooltip: "Powered by OpenAI",
+      content: (
+        <RichTextStyleWrapper>
+          <AISummaryContent />
+        </RichTextStyleWrapper>
+      ),
+    },
+  ].filter(Boolean);
+  const [activeTab, setActiveTab] = useState(tabs[0].label);
+
   return (
-    <Wrapper>
+    <div className={className}>
       {post.content === "" && (
         <NoData text={"The post has not been edited by creator."} />
       )}
+
+      {post.content && (
+        <div className="mt-6">
+          <Tabs
+            activeTabLabel={activeTab}
+            tabs={tabs}
+            onTabClick={(tab) => {
+              setActiveTab(tab.label);
+            }}
+          />
+        </div>
+      )}
+
       <MarkdownPreviewer content={post.content} />
       {post.createdAt !== post.updatedAt && <EditedLabel>Edited</EditedLabel>}
       <PostDataSource />
       <PolkassemblyActions reactions={postReactions} />
-    </Wrapper>
+    </div>
   );
 }
