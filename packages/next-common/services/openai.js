24203c165f29109645f5be59bd63164954c8d819
2nthony <ant.hu@outlook.com>
2023-11-02 14:21:51 +0800

    fix: openai

--- packages/next-common/services/openai.js
+++ packages/next-common/services/openai.js
@@ -14,15 +14,7 @@ const AZURE_OPENAI_API_KEY = process.env.NEXT_PUBLIC_AZURE_OPENAI_API_KEY;
 const openaiUrl = OPENAI_API_END_POINT + "/v1/chat/completions";
 const azureOpenaiUrl = `https://${AZURE_OPENAI_RESOURCE_NAME}.openai.azure.com/openai/deployments/${AZURE_OPENAI_DEPLOYMENT_ID}/chat/completions?api-version=${AZURE_OPENAI_API_VERSION}`;
 
-const PROMPT = `I want you to act as a Wikipedia page.
-I will give you a post content and you will write a summary of it.
-intructions you must follow:
-- Don't copy the original text
-- Don't include any code
-- Start with "This post is"
-`;
-
-const openaiModelOptions = {
+const defaultModelOptions = {
   // gpt-3.5-turbo
   // gpt-3.5-turbo-16k
   // gpt-4
@@ -31,11 +23,41 @@ const openaiModelOptions = {
   stream: false,
   // 0-1
   temperature: 0.3,
+  max_tokens: 400,
 };
 
-export async function aiSummarise({ provider = "openai", post }) {
-  let summary = "";
+async function openai({ provider = "openai", ...payload }, fetchOptions = {}) {
   let apiUrl = "";
+
+  const headers = {};
+  if (provider === "openai") {
+    apiUrl = openaiUrl;
+    headers.Authorization = `Bearer ${OPENAI_API_KEY}`;
+  }
+  if (provider === "azure-openai") {
+    apiUrl = azureOpenaiUrl;
+    headers["api-key"] = AZURE_OPENAI_API_KEY;
+  }
+
+  return await nextApi.post(
+    apiUrl,
+    {
+      ...defaultModelOptions,
+      ...payload,
+    },
+    {
+      credentials: undefined,
+      ...fetchOptions,
+      headers: {
+        ...headers,
+        ...fetchOptions.headers,
+      },
+    },
+  );
+}
+
+export async function fetchSummary({ post, type }) {
+  let summary = "";
   let content = "";
 
   // TODO: post contentType html, stripe out html tags, keep `br`
@@ -48,36 +70,23 @@ export async function aiSummarise({ provider = "openai", post }) {
     return summary;
   }
 
-  const headers = {};
-  if (provider === "openai") {
-    apiUrl = openaiUrl;
-    headers.Authorization = `Bearer ${OPENAI_API_KEY}`;
-  }
-  if (provider === "azure-openai") {
-    apiUrl = azureOpenaiUrl;
-    headers["api-key"] = AZURE_OPENAI_API_KEY;
-  }
-
-  const resp = await nextApi.post(
-    apiUrl,
-    {
-      ...openaiModelOptions,
-      messages: [
-        {
-          role: "system",
-          content: PROMPT,
-        },
-        {
-          role: "user",
-          content,
-        },
-      ],
-    },
-    {
-      headers,
-      credentials: undefined,
-    },
-  );
+  const resp = await openai({
+    messages: [
+      {
+        role: "system",
+        content: `I want you to act as a Wikipedia page.
+Summarise SubSquare ${type} post content you are provided.
+Instructions you must follow:
+- For a second-grade student in 5 bullet points
+- Don't give any redundant markdown
+`,
+      },
+      {
+        role: "user",
+        content,
+      },
+    ],
+  });
 
   if (resp.result) {
     const choices = resp.result.choices;
