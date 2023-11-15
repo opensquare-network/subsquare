073ffb5fb34d61aed43968b16910911f7a14d4a9
2nthony <ant.hu@outlook.com>
2023-11-15 14:55:19 +0800

    discussions

--- packages/next-common/components/overview/activeProposals/discussions.jsx
+++ packages/next-common/components/overview/activeProposals/discussions.jsx
@@ -1,8 +1,23 @@
 import { discussionsMenu } from "next-common/utils/consts/menu/common";
 import { getProposalPostTitleColumn } from "./columns/common";
+import normalizeDiscussionListItem from "next-common/utils/viewfuncs/discussion/normalizeDiscussionListItem";
+import { CHAIN } from "next-common/utils/constants";
+import normalizePolkassemblyDiscussionListItem from "next-common/utils/viewfuncs/discussion/normalizePaListItem";
+import { timeDurationFromNow } from "next-common/utils";
+import getChainSettings from "next-common/utils/consts/settings";
 
-// FIXME: overview, need discussions active count from server
+const lastActivityColumn = {
+  name: "Last Activity",
+  className: "w-40 text-left",
+  cellRender(data) {
+    return data.time && <span>{timeDurationFromNow(data.time)}</span>;
+  },
+};
+
 export function getActiveProposalDiscussions({ activeProposals }) {
+  const chainSettings = getChainSettings(CHAIN);
+  const subsquare = activeProposals.discussions?.subsquare;
+
   const items = [
     {
       value: "subsquare",
@@ -11,12 +26,22 @@ export function getActiveProposalDiscussions({ activeProposals }) {
         path: "overview/discussions",
         initData: activeProposals.discussions?.subsquare,
       },
+      activeCount: subsquare?.total,
+      formatter: (item) => normalizeDiscussionListItem(CHAIN, item),
+      columns: [getProposalPostTitleColumn(), lastActivityColumn],
+    },
+    chainSettings.hasPolkassemblyDiscussions && {
+      value: "polkassembly",
+      name: "Polkassembly",
+      activeCount: subsquare?.total,
+      formatter: (item) => normalizePolkassemblyDiscussionListItem(CHAIN, item),
       columns: [getProposalPostTitleColumn()],
     },
-  ];
+  ].filter(Boolean);
 
   return {
     ...discussionsMenu,
+    activeCount: subsquare?.total,
     items,
   };
 }
