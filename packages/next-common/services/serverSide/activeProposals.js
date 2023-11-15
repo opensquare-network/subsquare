b9dcb93d5abe2875de0eae871312d91c4ac407ae
2nthony <ant.hu@outlook.com>
2023-11-15 15:00:05 +0800

    revert page size

--- packages/next-common/services/serverSide/activeProposals.js
+++ packages/next-common/services/serverSide/activeProposals.js
@@ -24,7 +24,7 @@ import getChainSettings from "next-common/utils/consts/settings";
 import isMoonChain from "next-common/utils/isMoonChain";
 
 export const activeProposalFetchParams = {
-  pageSize: 3,
+  pageSize: 8,
 };
 
 async function fetcher(url) {
