86d8b103e8fbf9c513527eb6e08f1df63cd0cb88
chaojun <chaojun@opensquare.network>
2023-11-17 18:28:41 +0800

    fix: balance input, #3626

--- packages/next-common/components/extrinsic/params/balanceParam.js
+++ packages/next-common/components/extrinsic/params/balanceParam.js
@@ -1,25 +1,30 @@
 import BigNumber from "bignumber.js";
 import BalanceInput from "next-common/components/balanceInput";
 import { useChainSettings } from "next-common/context/chain";
-import { useCallback } from "react";
+import { useEffect, useState } from "react";
 
-export default function BalanceParam({ value, setValue }) {
+export default function BalanceParam({ setValue }) {
+  const [inputBalance, setInputBalance] = useState("");
   const { symbol, decimals } = useChainSettings();
-  const v = new BigNumber(value).div(Math.pow(10, decimals)).toString();
-  const _setValue = useCallback(
-    (v) => {
-      if (v === "") {
-        setValue(undefined);
-        return;
-      }
-      setValue(
-        new BigNumber(v)
-          .times(Math.pow(10, decimals))
-          .integerValue()
-          .toString(),
-      );
-    },
-    [setValue, decimals],
+
+  useEffect(() => {
+    if (inputBalance === "") {
+      setValue(undefined);
+      return;
+    }
+    setValue(
+      new BigNumber(inputBalance)
+        .times(Math.pow(10, decimals))
+        .integerValue()
+        .toString(),
+    );
+  }, [setValue, decimals, inputBalance]);
+
+  return (
+    <BalanceInput
+      value={inputBalance}
+      setValue={setInputBalance}
+      symbol={symbol}
+    />
   );
-  return <BalanceInput value={v} setValue={_setValue} symbol={symbol} />;
 }
