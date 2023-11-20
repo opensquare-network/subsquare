c7505a9365ef2a141690ef54da7698922147f2b2
2nthony <ant.hu@outlook.com>
2023-11-20 18:11:51 +0800

    div to span

--- packages/next/components/myvotes/styled.js
+++ packages/next/components/myvotes/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Flex from "next-common/components/styled/flex";
 import tw from "tailwind-styled-components";
 
-export const Title = tw.div`
+export const Title = tw.span`
   text-textPrimary text16Bold
   ${(p) => p.disabled && "text-textTertiary"}
 `;
