4274bc5e5f9241042194474119d75cf27c8d117c
2nthony <ant.hu@outlook.com>
2023-11-22 20:16:16 +0800

    deposit fellowship

--- packages/next/components/myDeposits/index.jsx
+++ packages/next/components/myDeposits/index.jsx
@@ -1,14 +1,19 @@
 import { AccountSubTabs } from "components/overview/account/subTabs";
+import { useChainSettings } from "next-common/context/chain";
 import MyDemocracyDeposits from "./democracy";
+import MyFellowshipDeposits from "./fellowship";
 import MyReferendaDeposits from "./referenda";
 
 export default function MyDeposits() {
+  const { hasReferenda, hasFellowship } = useChainSettings();
+
   return (
     <div className="space-y-6">
       <AccountSubTabs />
 
       <div className="space-y-6">
-        <MyReferendaDeposits />
+        {hasReferenda && <MyReferendaDeposits />}
+        {hasFellowship && <MyFellowshipDeposits />}
         <MyDemocracyDeposits />
       </div>
     </div>
