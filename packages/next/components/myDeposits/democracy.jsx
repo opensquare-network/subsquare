7ffe4c91793280a4ee008c5f415219b17d04743e
2nthony <ant.hu@outlook.com>
2023-11-22 15:18:37 +0800

    deposit democracy

--- packages/next/components/myDeposits/democracy.jsx
+++ packages/next/components/myDeposits/democracy.jsx
@@ -1,39 +1,83 @@
-import sumBy from "lodash.sumby";
 import ActiveProposalTemplate from "next-common/components/overview/activeProposals/activeProposalTemplate";
 import {
   getProposalPostTitleColumn,
   getStatusTagColumn,
 } from "next-common/components/overview/activeProposals/columns";
+import ValueDisplay from "next-common/components/valueDisplay";
+import { useChain, useChainSettings } from "next-common/context/chain";
 import useFetchMyDemocracyDeposits from "next-common/hooks/account/deposit/useFetchMyDemocracyDeposits";
+import nextApi from "next-common/services/nextApi";
 import { myDemocracyDepositsSelector } from "next-common/store/reducers/myOnChainData/deposits/myDemocracyDeposits";
+import { toPrecision } from "next-common/utils";
+import { EmptyList } from "next-common/utils/constants";
 import businessCategory from "next-common/utils/consts/business/category";
 import { getDemocracyMenu } from "next-common/utils/consts/menu/democracy";
+import normalizeProposalListItem from "next-common/utils/viewfuncs/democracy/normalizeProposalListItem";
 import { useSelector } from "react-redux";
 
 export default function MyDemocracyDeposits() {
   useFetchMyDemocracyDeposits();
 
+  const chain = useChain();
+  const { decimals, symbol } = useChainSettings();
   const deposits = useSelector(myDemocracyDepositsSelector);
-  console.log(deposits);
+  const activeCount = deposits?.length || 0;
 
   const menu = getDemocracyMenu();
 
   const items = [
     {
       name: "Public Proposal Deposits",
-      activeCount: deposits?.length || 0,
+      activeCount,
       columns: [
         getProposalPostTitleColumn(),
         {
           name: "Reservation",
           className: "w-40 text-right",
-          cellRender() {},
+          cellRender(data) {
+            return (
+              <ValueDisplay
+                value={toPrecision(
+                  data.depositCount * data.eachDepositValue,
+                  decimals,
+                )}
+                symbol={symbol}
+              />
+            );
+          },
         },
         getStatusTagColumn({ category: businessCategory.democracyProposals }),
       ],
+      formatter: (item) => normalizeProposalListItem(chain, item),
+      api: {
+        async fetchData() {
+          if (deposits?.length) {
+            const fetchers = deposits.map((deposit) =>
+              nextApi.fetch(`democracy/proposals/${deposit.proposalIndex}`),
+            );
+
+            const resps = await Promise.all(fetchers);
+
+            const items = resps.map((resp, idx) => {
+              return {
+                ...resp.result,
+                ...deposits[idx],
+              };
+            });
+
+            return {
+              result: {
+                items,
+                total: activeCount,
+              },
+            };
+          }
+
+          return { result: EmptyList };
+        },
+      },
     },
   ];
-  const activeCount = sumBy(items, "activeCount");
 
   return (
     <div>
