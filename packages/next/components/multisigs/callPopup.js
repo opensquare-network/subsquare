fe618724b9c0edb1757dfc34ca325a323d58e67b
chaojun <chaojun@opensquare.network>
2023-11-23 16:26:26 +0800

    refactor call detail popup, #3601

--- packages/next/components/multisigs/callPopup.js
+++ packages/next/components/multisigs/callPopup.js
@@ -1,20 +1,10 @@
-import Popup from "next-common/components/popup/wrapper/Popup";
+import useCallFromHex from "next-common/utils/hooks/useCallFromHex";
+import CallDetailPopup from "next-common/components/callDetailPopup";
 import {
-  ArgsWrapper,
   convertProposalForJsonView,
   convertProposalForTableView,
 } from "next-common/components/proposal";
-import CallTree from "next-common/components/proposal/callTree";
-import Tab from "next-common/components/tab";
-import InnerDataTable from "next-common/components/table/innerDataTable";
 import { useChain } from "next-common/context/chain";
-import useCallFromHex from "next-common/utils/hooks/useCallFromHex";
-import dynamic from "next/dynamic";
-import { useLocalStorage } from "usehooks-ts";
-
-const JsonView = dynamic(() => import("next-common/components/jsonView"), {
-  ssr: false,
-});
 
 const tabs = [
   { tabId: "tree", tabTitle: "Tree" },
@@ -29,36 +19,17 @@ export default function CallPopup({ call, callHex, blockHeight, setShow }) {
   );
   const chain = useChain();
 
-  const [selectedTabId, setSelectedTabId] = useLocalStorage(
-    "callType",
-    tabs[0].tabId,
-  );
   const tableViewData = convertProposalForTableView(call, chain);
+  const jsonViewData = convertProposalForJsonView(call, chain);
 
   return (
-    <Popup
-      title="Call Detail"
-      onClose={() => setShow(false)}
-      className="w-[650px]"
-    >
-      <Tab
-        tabs={tabs}
-        selectedTabId={selectedTabId}
-        setSelectedTabId={setSelectedTabId}
-      />
-      {selectedTabId === "tree" && (
-        <CallTree call={rawCall} isLoading={isLoadingRawCall} />
-      )}
-      {selectedTabId === "table" && (
-        <ArgsWrapper className="wrapper text-textPrimary">
-          <InnerDataTable data={tableViewData} />
-        </ArgsWrapper>
-      )}
-      {selectedTabId === "json" && (
-        <ArgsWrapper className="wrapper">
-          <JsonView src={convertProposalForJsonView(call, chain)} />
-        </ArgsWrapper>
-      )}
-    </Popup>
+    <CallDetailPopup
+      tableViewData={tableViewData}
+      jsonViewData={jsonViewData}
+      rawCall={rawCall}
+      isLoadingRawCall={isLoadingRawCall}
+      setShow={setShow}
+      tabs={tabs}
+    />
   );
 }
