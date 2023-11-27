04a67e4e46fc8f948618b1bf9e2928c0c93ea132
2nthony <ant.hu@outlook.com>
2023-11-27 15:32:15 +0800

    deposits title count

--- packages/next-common/components/overview/account/depositsTitle.jsx
+++ packages/next-common/components/overview/account/depositsTitle.jsx
@@ -1,10 +1,68 @@
+import { useSelector } from "react-redux";
 import { Title } from "./styled";
+import { myPreimageDepositsSelector } from "next-common/store/reducers/myOnChainData/deposits/myPreimageDeposits";
+import {
+  myReferendaDecisionDepositsSelector,
+  myReferendaSubmissionDepositsSelector,
+} from "next-common/store/reducers/myOnChainData/deposits/myReferendaDeposits";
+import {
+  myFellowshipDecisionDepositsSelector,
+  myFellowshipSubmissionDepositsSelector,
+} from "next-common/store/reducers/myOnChainData/deposits/myFellowshipDeposits";
+import { myDemocracyDepositsSelector } from "next-common/store/reducers/myOnChainData/deposits/myDemocracyDeposits";
+import {
+  myTreasuryBountyBondsSelector,
+  myTreasuryBountyCuratorDepositsSelector,
+  myTreasuryProposalDepositsSelector,
+  myTreasuryTipDepositsSelector,
+} from "next-common/store/reducers/myOnChainData/deposits/myTreasuryDeposits";
+import sumBy from "lodash.sumby";
 
 export default function DepositsTitle({ active }) {
+  const referendaSubmissionDeposits = useSelector(
+    myReferendaSubmissionDepositsSelector,
+  );
+  const referendaDecisionDeposits = useSelector(
+    myReferendaDecisionDepositsSelector,
+  );
+  const fellowshipSubmissionDeposits = useSelector(
+    myFellowshipSubmissionDepositsSelector,
+  );
+  const fellowshipDecisionDeposits = useSelector(
+    myFellowshipDecisionDepositsSelector,
+  );
+  const democracyDeposits = useSelector(myDemocracyDepositsSelector);
+  const treasuryProposalDeposits = useSelector(
+    myTreasuryProposalDepositsSelector,
+  );
+  const treasuryBountyBonds = useSelector(myTreasuryBountyBondsSelector);
+  const treasuryBountyCuratorDeposits = useSelector(
+    myTreasuryBountyCuratorDepositsSelector,
+  );
+  const treasuryTipDeposits = useSelector(myTreasuryTipDepositsSelector);
+  const preimageDeposits = useSelector(myPreimageDepositsSelector);
+
+  const count =
+    sumBy(
+      [
+        referendaSubmissionDeposits,
+        referendaDecisionDeposits,
+        fellowshipSubmissionDeposits,
+        fellowshipDecisionDeposits,
+        democracyDeposits,
+        treasuryProposalDeposits,
+        treasuryBountyBonds,
+        treasuryBountyCuratorDeposits,
+        treasuryTipDeposits,
+        preimageDeposits,
+      ],
+      "length",
+    ) || 0;
+
   return (
     <Title className={active ? "text-textPrimary" : "text-textTertiary"}>
       Deposits
-      {true !== null && <span className="text-textTertiary mx-1">{0}</span>}
+      {true !== null && <span className="text-textTertiary mx-1">{count}</span>}
     </Title>
   );
 }
