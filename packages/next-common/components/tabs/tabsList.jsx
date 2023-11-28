43f678660a58091271efbb5754c3fee90bcaf1b0
2nthony <ant.hu@outlook.com>
2023-11-28 15:54:16 +0800

    fix: tabs list

--- packages/next-common/components/tabs/tabsList.jsx
+++ packages/next-common/components/tabs/tabsList.jsx
@@ -11,7 +11,7 @@ export default function TabsList({
   return (
     <List
       onTabClick={onTabClick}
-      tabs={tabs.map((tab, idx) => {
+      tabs={tabs.map((tab) => {
         const itemClassName = cn(
           "block whitespace-nowrap pb-3",
           "text14Bold border-b-4 text-textPrimary",
@@ -20,24 +20,29 @@ export default function TabsList({
         const itemActiveClassName = "border-theme500 text-theme500";
         const active = tab.active ?? activeTabLabel === tab.label;
 
-        return (
-          <Tooltip key={idx} content={tab.tooltip}>
-            <div
-              className={cn(
-                itemClassName,
-                active ? itemActiveClassName : "border-transparent",
-              )}
-            >
-              {tab.label}
-              {!!tab.activeCount && (
-                <span className="ml-1 text-textTertiary text14Medium">
-                  {tab.activeCount}
-                </span>
-              )}
-              {tab.labelExtra}
-            </div>
-          </Tooltip>
-        );
+        return {
+          ...tab,
+          render() {
+            return (
+              <Tooltip content={tab.tooltip}>
+                <div
+                  className={cn(
+                    itemClassName,
+                    active ? itemActiveClassName : "border-transparent",
+                  )}
+                >
+                  {tab.label}
+                  {!!tab.activeCount && (
+                    <span className="ml-1 text-textTertiary text14Medium">
+                      {tab.activeCount}
+                    </span>
+                  )}
+                  {tab.labelExtra}
+                </div>
+              </Tooltip>
+            );
+          },
+        };
       })}
     />
   );
