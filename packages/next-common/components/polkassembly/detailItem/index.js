88c734607a558c7d593793278aa41575175fdd9a
2nthony <ant.hu@outlook.com>
2023-12-05 15:51:19 +0800

    hotfix: pa post meta layout

--- packages/next-common/components/polkassembly/detailItem/index.js
+++ packages/next-common/components/polkassembly/detailItem/index.js
@@ -2,18 +2,15 @@ import ArticleContent from "./articleContent";
 import { usePost } from "next-common/context/post";
 import DetailContentBase from "next-common/components/detail/common/detailBase";
 import PostTitle from "next-common/components/detail/common/Title";
-import PolkassemblyUser from "next-common/components/user/polkassemblyUser";
-import { toPolkassemblyDiscussionAuthor } from "next-common/utils/viewfuncs/discussion/normalizePaListItem";
-import { useChain } from "next-common/context/chain";
 import Info from "next-common/components/styled/info";
 import UpdateIcon from "next-common/assets/imgs/icons/line-chart.svg";
 import useDuration from "next-common/utils/hooks/useDuration";
+import PostMetaBase from "next-common/components/detail/container/postMeta/metaBase";
 
 export default function DetailItem({ postReactions }) {
   const post = usePost();
   const postUpdateTime = post?.updatedAt || post?.createdAt;
   const duration = useDuration(postUpdateTime);
-  const chain = useChain();
 
   if (!post) {
     return null;
@@ -23,19 +20,13 @@ export default function DetailItem({ postReactions }) {
     <DetailContentBase
       title={<PostTitle />}
       meta={
-        <div className="flex items-center justify-between flex-nowrap">
-          <div className="flex items-center">
-            <PolkassemblyUser
-              user={toPolkassemblyDiscussionAuthor(post.author, chain)}
-              fontSize={12}
-            />
-            <Info>
-              <UpdateIcon />
-              <span>{duration}</span>
-            </Info>
-            <Info>{`${post.commentsCount} Comments`}</Info>
-          </div>
-        </div>
+        <PostMetaBase>
+          <Info>
+            <UpdateIcon />
+            <span>{duration}</span>
+          </Info>
+          <Info>{`${post.commentsCount} Comments`}</Info>
+        </PostMetaBase>
       }
     >
       <ArticleContent postReactions={postReactions} />
