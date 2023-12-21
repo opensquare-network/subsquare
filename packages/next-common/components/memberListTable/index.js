d97da10dccb98017259b454b1bfa30a0032d6d15
2nthony <ant.hu@outlook.com>
2023-12-21 15:26:38 +0800

    fix: council member list

--- packages/next-common/components/memberListTable/index.js
+++ packages/next-common/components/memberListTable/index.js
@@ -1,4 +1,14 @@
+import Descriptions from "../Descriptions";
 import StyledList from "../styledList";
+import Loading from "../loading";
+import { useScreenSize } from "next-common/utils/hooks/useScreenSize";
+import { NeutralPanel } from "../styled/containers/neutralPanel";
+
+const loadingContent = (
+  <div className="mt-4 mb-2 flex justify-center">
+    <Loading size={24} />
+  </div>
+);
 
 export default function MemberListTable({
   columns = [],
@@ -7,7 +17,11 @@ export default function MemberListTable({
   noDataText = "No current members",
   ...restProps
 }) {
-  return (
+  const { sm } = useScreenSize();
+
+  return sm ? (
+    <MobileList rows={rows} columns={columns} loading={loading} />
+  ) : (
     <StyledList
       columns={columns}
       rows={rows}
@@ -15,5 +29,44 @@ export default function MemberListTable({
       noDataText={noDataText}
       {...restProps}
     />
+  );
+}
+
+function MobileList({ rows = [], columns = [], loading }) {
+  if (loading) {
+    return loadingContent;
+  }
+
+  return (
+    <NeutralPanel className="p-6">
+      {rows.map((row, idx) => {
+        const title = row[0];
+        const rest = row.slice(1);
+        const descriptionsLabels = columns.slice(1).map((col) => col.name);
+
+        const descriptionItems = rest.map((value, idx) => {
+          return {
+            label: (
+              <span className="text-textTertiary">
+                {descriptionsLabels[idx]}
+              </span>
+            ),
+            value,
+            className: "h-auto",
+          };
+        });
+
+        return (
+          <div
+            key={idx}
+            className="py-4 first:pt-0 border-b border-neutral300 space-y-3"
+          >
+            <div className="flex items-center">{title}</div>
+
+            <Descriptions items={descriptionItems} bordered={false} />
+          </div>
+        );
+      })}
+    </NeutralPanel>
   );
 }
--- packages/next/components/membersList/councilMembersList.js
+++ packages/next/components/membersList/councilMembersList.js
@@ -1,62 +1,17 @@
 import styled, { withTheme } from "styled-components";
-import {
-  bigNumber2Locale,
-  decimalPlaces,
-  toPrecision,
-} from "next-common/utils";
-import { useState } from "react";
+import { toPrecision } from "next-common/utils";
 import PrimeAddressMark from "next-common/components/primeAddressMark";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 import { useChainSettings } from "next-common/context/chain";
 import MemberListTable from "next-common/components/memberListTable";
 import AddressUser from "next-common/components/user/addressUser";
-
-const Wrapper = styled.div`
-  > :not(:first-child) {
-    margin-top: 16px;
-  }
-
-  @media screen and (max-width: 392px) {
-    .autohide {
-      display: none;
-    }
-    th.clickable {
-      color: var(--textSecondary);
-      cursor: pointer;
-      pointer-events: auto;
-    }
-  }
-`;
+import ValueDisplay from "next-common/components/valueDisplay";
 
 const Member = styled.div`
   display: flex;
   align-items: center;
   gap: 8px;
 `;
-
-const BalanceWrapper = styled.span`
-  color: var(--textPrimary);
-`;
-const SymbolWrapper = styled.span`
-  color: var(--textTertiary);
-`;
-
-function Balance({ value }) {
-  const node = useChainSettings();
-
-  return (
-    <div>
-      <BalanceWrapper>
-        {bigNumber2Locale(
-          decimalPlaces(toPrecision(value ?? 0, node.decimals), 4),
-        )}
-      </BalanceWrapper>
-      <SymbolWrapper style={{ color: "#9DA9BB", marginLeft: "8px" }}>
-        {node.symbol}
-      </SymbolWrapper>
-    </div>
-  );
-}
 
 function MembersList({
   category,
@@ -65,23 +20,24 @@ function MembersList({
   loading = false,
   hasElections = false,
 }) {
-  const [hideColumn, setHideColumn] = useState("votes");
+  const chainSettings = useChainSettings();
 
-  const columns = [{ name: "MEMBERS", style: { textAlign: "left" } }];
+  const columns = [
+    {
+      name: "Members",
+      style: { textAlign: "left" },
+    },
+  ];
 
   if (hasElections) {
     columns.push(
       {
-        name: "BACKING",
-        style: { textAlign: "right" },
-        className: hideColumn === "backing" ? "autohide" : "clickable",
-        onClick: () => setHideColumn("backing"),
+        name: "Backing",
+        style: { width: 240, textAlign: "right" },
       },
       {
-        name: "VOTES",
-        style: { textAlign: "right" },
-        className: hideColumn === "votes" ? "autohide" : "clickable",
-        onClick: () => setHideColumn("votes"),
+        name: "Votes",
+        style: { width: 160, textAlign: "right" },
       },
     );
   }
@@ -95,17 +51,23 @@ function MembersList({
     ];
 
     if (hasElections) {
-      row.push(<Balance value={item.backing} />, item.votes ?? "--");
+      row.push(
+        <ValueDisplay
+          value={toPrecision(item.backing, chainSettings.decimals)}
+          symbol={chainSettings.symbol}
+        />,
+        item.votes ?? "--",
+      );
     }
 
     return row;
   });
 
   return (
-    <Wrapper>
+    <div className="">
       <TitleContainer>{category}</TitleContainer>
       <MemberListTable columns={columns} rows={rows} loading={loading} />
-    </Wrapper>
+    </div>
   );
 }
 
