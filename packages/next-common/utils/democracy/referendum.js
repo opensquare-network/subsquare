b4a696e19f0810e70361fd9d52b7795c0db4b333
chaojun <chaojun@opensquare.network>
2023-12-21 17:48:41 +0800

    Update

--- packages/next-common/utils/democracy/referendum.js
+++ packages/next-common/utils/democracy/referendum.js
@@ -3,11 +3,14 @@ export const referendumVoteFinishedStatusArray = [
   "NotPassed",
   "Cancelled",
   "Canceled",
+  "Removed",
 ];
 
 export default function extractVoteInfo(timeline = []) {
-  const timelineStatuses = timeline.map(item => item.method);
-  const index = timelineStatuses.findIndex(status => referendumVoteFinishedStatusArray.includes(status));
+  const timelineStatuses = timeline.map((item) => item.method);
+  const index = timelineStatuses.findIndex((status) =>
+    referendumVoteFinishedStatusArray.includes(status),
+  );
   const voteFinished = index >= 0;
   let voteFinishedHeight = null;
   if (voteFinished) {
