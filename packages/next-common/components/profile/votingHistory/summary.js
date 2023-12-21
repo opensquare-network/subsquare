a6e64cd7bbb621896751a4b06cc08838c3553dec
2nthony <ant.hu@outlook.com>
2023-12-21 16:20:55 +0800

    clean

--- packages/next-common/components/profile/votingHistory/summary.js
+++ packages/next-common/components/profile/votingHistory/summary.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import SummaryItems from "next-common/components/summary/summaryItems";
-import styled from "styled-components";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import { Title } from "./styled";
 import { ModuleTab, useAvailableModuleTabs, useModuleName } from "./common";
@@ -8,18 +7,12 @@ import nextApi from "next-common/services/nextApi";
 import { usePageProps } from "next-common/context/page";
 import { useChainSettings } from "next-common/context/chain";
 
-const ValueWrapper = styled.div`
-  .value-display-symbol {
-    color: var(--textTertiary);
-  }
-`;
-
 function CountSummaryContent({ count }) {
   return <span>{(count || 0).toLocaleString()}</span>;
 }
 
 function TextSummaryContent({ value }) {
-  return <ValueWrapper>{value}</ValueWrapper>;
+  return <div>{value}</div>;
 }
 
 export default function VotingHistorySummary() {
--- packages/next-common/components/statistics/democracy/summary/index.js
+++ packages/next-common/components/statistics/democracy/summary/index.js
@@ -1,15 +1,7 @@
-import React from "react";
 import SummaryItems from "next-common/components/summary/summaryItems";
 import ValueDisplay from "next-common/components/valueDisplay";
 import { useChainSettings } from "next-common/context/chain";
 import { toPrecision } from "next-common/utils";
-import styled from "styled-components";
-
-const ValueWrapper = styled.div`
-  .value-display-symbol {
-    color: var(--textTertiary);
-  }
-`;
 
 function CountSummaryContent({ count }) {
   return <span>{(count || 0).toLocaleString()}</span>;
@@ -18,12 +10,12 @@ function CountSummaryContent({ count }) {
 function ValueSummaryContent({ value }) {
   const { voteSymbol, symbol, decimals } = useChainSettings();
   return (
-    <ValueWrapper>
+    <div>
       <ValueDisplay
         value={toPrecision(value || 0, decimals)}
         symbol={voteSymbol || symbol}
       />
-    </ValueWrapper>
+    </div>
   );
 }
 
@@ -31,11 +23,19 @@ export default function DemocracySummary({ summary }) {
   const items = [
     {
       title: "DELEGATEE",
-      content: <CountSummaryContent count={(summary?.address || summary?.addresses)?.delegatee} />,
+      content: (
+        <CountSummaryContent
+          count={(summary?.address || summary?.addresses)?.delegatee}
+        />
+      ),
     },
     {
       title: "DELEGATOR",
-      content: <CountSummaryContent count={(summary?.address || summary?.addresses)?.delegator} />,
+      content: (
+        <CountSummaryContent
+          count={(summary?.address || summary?.addresses)?.delegator}
+        />
+      ),
     },
     {
       title: "TOTAL CAPITAL",
