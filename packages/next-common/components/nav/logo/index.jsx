ef46f8dc6af799f8bfcd30d2276efeeb3c37d3c1
2nthony <ant.hu@outlook.com>
2023-12-26 16:22:31 +0800

    hotfeat: update logo event background (#3719)
    * clean
    
    * bgs
    
    * rename to event bg
    
    * delete unrelated bgs
    
    * lodash.find
    
    * rename
    
    * transparent material
    
    * update
    
    * update
    
    * fix path
    
    * add note
    
    * refactor

--- packages/next-common/components/nav/logo/index.jsx
+++ packages/next-common/components/nav/logo/index.jsx
@@ -1,6 +1,6 @@
 import { useChainSettings } from "next-common/context/chain";
 import Link from "next/link";
-import ChainLogoChristmasBackground from "./christmasBackground";
+import ChainLogoEventBackground from "./eventBackground";
 import { cn } from "next-common/utils";
 
 export default function ChainLogo({ className = "" }) {
@@ -11,23 +11,25 @@ export default function ChainLogo({ className = "" }) {
   }
 
   // NOTE: workaround, should find some React way to check is same source
-  const logo =
-    // check is same source
-    chainSettings.navLogo === chainSettings.navLogoDark ? (
-      <chainSettings.navLogo />
-    ) : (
-      <>
-        <chainSettings.navLogo className="dark:hidden" />
-        <chainSettings.navLogoDark className="hidden dark:block" />
-      </>
-    );
+  // check is same source
+  const isDarkLogoOnLightMode =
+    chainSettings.navLogo === chainSettings.navLogoDark;
+
+  const logo = isDarkLogoOnLightMode ? (
+    <chainSettings.navLogo />
+  ) : (
+    <>
+      <chainSettings.navLogo className="dark:hidden" />
+      <chainSettings.navLogoDark className="hidden dark:block" />
+    </>
+  );
 
   return (
     <div className={cn("relative", className)}>
       <Link href="/" className="z-[1]">
         {logo}
       </Link>
-      <ChainLogoChristmasBackground />
+      <ChainLogoEventBackground preferDark={isDarkLogoOnLightMode} />
     </div>
   );
 }
