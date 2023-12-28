eb1b2f9816b58b2e046cd1a0169520ee2c2e091f
chaojun <chaojun@opensquare.network>
2023-12-28 17:36:58 +0800

    fix

--- packages/next/pages/posts/create.js
+++ packages/next/pages/posts/create.js
@@ -1,20 +1,20 @@
 import PostCreate from "next-common/components/post/postCreate";
 import { useEffect } from "react";
-import { useIsLoggedIn } from "next-common/context/user";
+import { useUser } from "next-common/context/user";
 import DetailLayout from "next-common/components/layout/DetailLayout";
 import { useLoginPopup } from "next-common/hooks/useLoginPopup";
 import { getServerSidePropsWithTracks } from "next-common/services/serverSide";
 
 export default function PostCreatePage() {
-  const isLoggedIn = useIsLoggedIn();
+  const user = useUser();
   const { openLoginPopup } = useLoginPopup();
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    if (!user) {
       openLoginPopup();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoggedIn]);
+  }, [user]);
 
   const breadcrumbItems = [
     {
