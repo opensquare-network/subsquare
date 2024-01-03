9b3c5deaeabb37a3c2835d89fbce0d96dd29f7d2
Yongfeng LI <wliyongfeng@gmail.com>
2024-01-03 11:34:10 +0800

    Update democracy votes 10 times when tally changed, #3733

--- packages/next-common/hooks/democracy/tally.js
+++ packages/next-common/hooks/democracy/tally.js
@@ -5,7 +5,25 @@ import { useOnchainData } from "../../context/post";
 import useApi from "../../utils/hooks/useApi";
 import useIsMounted from "../../utils/hooks/useIsMounted";
 import { useDispatch } from "react-redux";
-import { incVotesTrigger } from "next-common/store/reducers/democracy/votes";
+import {
+  fetchVotes,
+  incVotesTrigger,
+} from "next-common/store/reducers/democracy/votes";
+import { sleep } from "next-common/utils";
+import getChainSettings from "next-common/utils/consts/settings";
+import { defaultBlockTime } from "next-common/utils/constants";
+
+async function fetchDemocracyVotes10Times(api, dispatch, referendumIndex) {
+  const blockTime =
+    getChainSettings(process.env.NEXT_PUBLIC_CHAIN).blockTime ||
+    defaultBlockTime;
+  const timers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // eslint-disable-next-line no-unused-vars
+  for (const timer of timers) {
+    dispatch(fetchVotes(api, referendumIndex));
+    await sleep(blockTime);
+  }
+}
 
 export default function useSubDemocracyTally() {
   const api = useApi();
@@ -23,23 +41,28 @@ export default function useSubDemocracyTally() {
     }
 
     let unsub;
-    api.query.democracy.referendumInfoOf(referendumIndex, (info) => {
-      if (!isMounted.current || !info || !info.isSome) {
-        return;
-      }
+    api.query.democracy
+      .referendumInfoOf(referendumIndex, (info) => {
+        if (!isMounted.current || !info || !info.isSome) {
+          return;
+        }
 
-      const unwrapped = info.unwrap();
-      if (!unwrapped.isOngoing) {
-        return;
-      }
+        const unwrapped = info.unwrap();
+        if (!unwrapped.isOngoing) {
+          return;
+        }
 
-      const ongoing = unwrapped.asOngoing;
-      setTally(ongoing.tally.toJSON());
+        const ongoing = unwrapped.asOngoing;
+        setTally(ongoing.tally.toJSON());
 
-      dispatch(incVotesTrigger());
-    }).then(result => {
-      unsub = result;
-    });
+        dispatch(incVotesTrigger());
+        fetchDemocracyVotes10Times(api, dispatch, referendumIndex).then(() => {
+          // votes updated 10 time, do nothing
+        });
+      })
+      .then((result) => {
+        unsub = result;
+      });
 
     return () => {
       if (unsub) {
