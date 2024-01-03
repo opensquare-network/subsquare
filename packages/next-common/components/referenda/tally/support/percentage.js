a879a410954fca0da90823ce59abe8a8ba44d2d1
Yongfeng LI <wliyongfeng@gmail.com>
2024-01-03 11:48:03 +0800

    Remove ‰ and keep only % for percentage view, #3738

--- packages/next-common/components/referenda/tally/support/percentage.js
+++ packages/next-common/components/referenda/tally/support/percentage.js
@@ -5,10 +5,6 @@ export default function Percentage({ perbill = 0 }) {
     return "0.0%";
   }
 
-  if (perbill > Math.pow(10, 6)) {
-    const precision = perbill > 10 * Math.pow(10, 7) ? 1 : 2;
-    return ((perbill / Math.pow(10, 9)) * 100).toFixed(precision) + "%";
-  }
-
-  return ((perbill / Math.pow(10, 8)) * 100).toFixed(1) + "‰";
+  const precision = perbill > 10 * Math.pow(10, 7) ? 1 : 2;
+  return ((perbill / Math.pow(10, 9)) * 100).toFixed(precision) + "%";
 }
