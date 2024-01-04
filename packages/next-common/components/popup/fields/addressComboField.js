3b42050fab43dbc58cb6578145701830bce95506
chaojun <chaojun@opensquare.network>
2024-01-04 13:18:35 +0800

    fix: address select filter

--- packages/next-common/components/popup/fields/addressComboField.js
+++ packages/next-common/components/popup/fields/addressComboField.js
@@ -3,13 +3,20 @@ import { useEffect, useState } from "react";
 import AddressCombo from "../../addressCombo";
 import PopupLabel from "../label";
 import { normalizeAddress } from "next-common/utils/address";
+import ChainTypes from "next-common/utils/consts/chainTypes";
+import { useChainSettings } from "next-common/context/chain";
 
 export default function AddressComboField({
   extensionAccounts,
   setAddress,
   title = "Address",
 }) {
-  const accounts = extensionAccounts.map((acc) => ({
+  const { chainType } = useChainSettings();
+  let filter = (item) => item.type !== "ethereum";
+  if (chainType === ChainTypes.ETHEREUM) {
+    filter = (item) => item.type === "ethereum";
+  }
+  const accounts = extensionAccounts.filter(filter).map((acc) => ({
     address: acc.address,
     name: acc.meta.name,
   }));
