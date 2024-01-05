eaf010d118dad2d6553df511997d2b3757392e0a
chaojun <chaojun@opensquare.network>
2024-01-05 21:44:13 +0800

    fix

--- packages/next-common/components/detail/referenda/preimageWarning.js
+++ packages/next-common/components/detail/referenda/preimageWarning.js
@@ -3,36 +3,26 @@ import useCall from "next-common/utils/hooks/useCall";
 import useApi from "next-common/utils/hooks/useApi";
 import Malicious from "next-common/components/detail/malicious";
 import { gov2State } from "next-common/utils/consts/state";
-import { useChainSettings } from "next-common/context/chain";
-
-function WarningCommon({ method }) {
-  const { proposalHash } = useOnchainData();
-  const api = useApi();
-  let [status, isLoading] = useCall(api?.query?.preimage?.[method], [
-    proposalHash,
-  ]);
-  if (isLoading || !status) {
-    return null;
-  }
-
-  if (status.isEmpty) {
-    return <Malicious>Preimage not found on chain</Malicious>;
-  }
-}
-
-function OldWarning() {
-  return <WarningCommon method="statusFor" />;
-}
 
 function Warning() {
-  return <WarningCommon method="requestStatusFor" />;
+  const { proposalHash } = useOnchainData();
+  const api = useApi();
+  const [status] = useCall(api?.query?.preimage?.statusFor, [proposalHash]);
+  const [requestStatus] = useCall(api?.query?.preimage?.requestStatusFor, [
+    proposalHash,
+  ]);
+
+  if ((status && status.isEmpty) || (requestStatus && requestStatus.isEmpty)) {
+    return <Malicious>Preimage not found on chain</Malicious>;
+  }
+
+  return null;
 }
 
 export default function PreimageWarning() {
   const state = usePostState();
   const onchainData = useOnchainData();
   const { proposalHash, proposal, inlineCall } = onchainData;
-  const { useNewPreimagePallet } = useChainSettings();
 
   const inFinalState = [
     gov2State.Rejected,
@@ -44,10 +34,6 @@ export default function PreimageWarning() {
   ].includes(state);
   if (inFinalState || !proposalHash || proposal?.inline || inlineCall) {
     return null;
-  }
-
-  if (!useNewPreimagePallet) {
-    return <OldWarning />;
   }
 
   return <Warning />;
