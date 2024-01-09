5a4fade539e6151c7991febdec58263af5f690b5
2nthony <ant.hu@outlook.com>
2024-01-09 16:32:26 +0800

    add to home page

--- packages/next-common/components/overview/overview.jsx
+++ packages/next-common/components/overview/overview.jsx
@@ -1,13 +1,16 @@
 import AccountInfo from "./accountInfo";
 import ActiveProposals from "./activeProposals";
 import { useChainSettings } from "next-common/context/chain";
+import TreasuryState from "./treasuryState";
 
 export default function Overview() {
-  const { showAccountManagementTab } = useChainSettings();
+  const { showAccountManagementTab, hasDotreasury } = useChainSettings();
 
   return (
     <div className="space-y-6">
       <AccountInfo hideManageAccountLink={showAccountManagementTab === false} />
+
+      {hasDotreasury && <TreasuryState />}
 
       <div>
         <ActiveProposals />
