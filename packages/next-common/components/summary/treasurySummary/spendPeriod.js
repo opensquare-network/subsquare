b984429405f78e569100c9334b7188aa66bede76
2nthony <ant.hu@outlook.com>
2024-01-09 16:31:28 +0800

    spend period

--- packages/next-common/components/summary/treasurySummary/spendPeriod.js
+++ packages/next-common/components/summary/treasurySummary/spendPeriod.js
@@ -1,7 +1,5 @@
-import React from "react";
 import FieldLoading from "next-common/components/icons/fieldLoading";
 import isEmpty from "lodash.isempty";
-import { cn } from "next-common/utils";
 
 export default function SpendPeriod({ summary }) {
   if (isEmpty(summary)) {
@@ -15,17 +13,17 @@ export default function SpendPeriod({ summary }) {
           {item}
         </span>
       ))}
-      {(summary?.totalPeriod || []).map((item, index) => (
-        <span
-          className={cn(
-            index % 2 === 1 ? "unit total" : "total",
-            "max-sm:hidden",
-          )}
-          key={index}
-        >
-          {item}
-        </span>
-      ))}
+
+      <div className="!ml-0 text12Medium text-textTertiary space-x-1">
+        {(summary?.totalPeriod || []).map((item, index) => (
+          <span
+            className={index % 2 === 1 ? "unit total" : "total"}
+            key={index}
+          >
+            {item}
+          </span>
+        ))}
+      </div>
     </>
   );
 }
