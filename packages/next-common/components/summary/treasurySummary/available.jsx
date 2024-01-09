1f46b40a57a339d171b07661d8ad77315f513da8
2nthony <ant.hu@outlook.com>
2024-01-09 16:32:04 +0800

    fix avaliable

--- packages/next-common/components/summary/treasurySummary/available.jsx
+++ packages/next-common/components/summary/treasurySummary/available.jsx
@@ -1,13 +1,23 @@
 import { useChainSettings } from "next-common/context/chain";
 import { abbreviateBigNumber, toPrecision } from "next-common/utils";
+import TreasurySummaryFiatPriceLabel from "./fiatPriceLabel";
+import isNil from "lodash.isnil";
 
-export default function TreasurySummaryAvailable({ free = 0 }) {
+export default function TreasurySummaryAvailable({ free = 0, fiatPrice }) {
   const { decimals, symbol } = useChainSettings();
 
+  const value = toPrecision(free || 0, decimals);
+
   return (
-    <>
-      <span>{abbreviateBigNumber(toPrecision(free || 0, decimals))}</span>
-      <span className="unit upper">{symbol}</span>
-    </>
+    <div>
+      <div className="flex gap-x-1">
+        <span>{abbreviateBigNumber(value)}</span>
+        <span className="unit upper">{symbol}</span>
+      </div>
+
+      {!isNil(fiatPrice) && (
+        <TreasurySummaryFiatPriceLabel value={value} fiatPrice={fiatPrice} />
+      )}
+    </div>
   );
 }
