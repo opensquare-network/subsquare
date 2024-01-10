e1c614ba7b37bb030d0a4c2784ced1ef8736231a
chaojun <chaojun@opensquare.network>
2024-01-10 15:00:16 +0800

    Update

--- packages/next-common/utils/hooks/useRealAddress.js
+++ packages/next-common/utils/hooks/useRealAddress.js
@@ -1,18 +1,6 @@
-import { useChain } from "next-common/context/chain";
 import { useUser } from "next-common/context/user";
-import Chains from "../consts/chains";
-import { isEthereumAddress } from "@polkadot/util-crypto";
-import { evmToSubstrateAddress } from "../hydradxUtil";
 
 export default function useRealAddress() {
   const user = useUser();
-  const chain = useChain();
-
-  const realAddress = user?.proxyAddress || user?.address;
-
-  if (chain === Chains.hydradx && isEthereumAddress(realAddress)) {
-    return evmToSubstrateAddress(realAddress);
-  }
-
-  return realAddress;
+  return user?.proxyAddress || user?.address;
 }
