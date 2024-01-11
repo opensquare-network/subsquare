5739ddca289423e99c25803c22935179ddf31e1d
2nthony <ant.hu@outlook.com>
2024-01-11 17:39:27 +0800

    fix

--- packages/next-common/components/overview/activeProposals/index.jsx
+++ packages/next-common/components/overview/activeProposals/index.jsx
@@ -24,7 +24,8 @@ export default function ActiveProposals() {
   const chainSettings = useChainSettings();
   const isPolkadotChain = chain === Chains.polkadot;
   const hasDiscussions = chainSettings.hasDiscussions !== false;
-  const { overviewSummary, activeProposals } = usePageProps();
+  const { overviewSummary, activeProposals, polkadotForumLatestTopics } =
+    usePageProps();
   const summary = overviewSummary;
   const tracks = overviewSummary?.gov2ReferendaTracks;
   const fellowshipTracks = overviewSummary?.fellowshipReferendaTracks;
@@ -32,6 +33,7 @@ export default function ActiveProposals() {
   const discussions = getActiveProposalDiscussions({
     summary,
     activeProposals,
+    polkadotForumLatestTopics,
   });
   const referenda = getActiveProposalReferenda({ tracks, activeProposals });
   const fellowship = getActiveProposalFellowship({
