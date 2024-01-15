cc1fef6efb8215f1605966d51195fba9156a793c
2nthony <ant.hu@outlook.com>
2024-01-15 12:18:25 +0800

    remove default value

--- packages/next-common/hooks/useToBeAwarded.js
+++ packages/next-common/hooks/useToBeAwarded.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function useToBeAwarded() {
   const api = useApi();
-  const [toBeAwarded, setToBeAwarded] = useState(0n);
+  const [toBeAwarded, setToBeAwarded] = useState();
 
   useEffect(() => {
     if (!api) {
