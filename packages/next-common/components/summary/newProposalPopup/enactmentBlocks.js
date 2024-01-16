799cbfe86f29f71ba7db5eddd19aa315dbf4fd02
chaojun <chaojun@opensquare.network>
2024-01-16 18:08:05 +0800

    Update

--- packages/next-common/components/summary/newProposalPopup/enactmentBlocks.js
+++ packages/next-common/components/summary/newProposalPopup/enactmentBlocks.js
@@ -1,12 +1,30 @@
 import Input from "next-common/components/input";
 import PopupLabel from "next-common/components/popup/label";
+import Tab from "next-common/components/tab";
+import { useState } from "react";
 
 export default function EnactmentBlocks() {
+  const [tabIndex, setTabIndex] = useState("After Delay");
+
   return (
     <div>
       <PopupLabel text="Enactment Blocks" />
       <div className="flex flex-col gap-[8px]">
-        <Input placeholder="0" suffix="Blocks" />
+        <Tab
+          tabs={[
+            {
+              tabId: "After Delay",
+              tabTitle: "After Delay",
+            },
+            {
+              tabId: "At Block",
+              tabTitle: "At Block",
+            },
+          ]}
+          selectedTabId={tabIndex}
+          setSelectedTabId={setTabIndex}
+        />
+        <Input placeholder="0" symbol="Blocks" />
       </div>
     </div>
   );
