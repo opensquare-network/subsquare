7af454b77de77efa6efa967eb410b332e0095b48
Yongfeng LI <wliyongfeng@gmail.com>
2024-01-20 20:19:05 +0800

    Fix forum topic url

--- packages/next-common/utils/viewfuncs/discussion/normalizePolkadotForumTopicListItem.js
+++ packages/next-common/utils/viewfuncs/discussion/normalizePolkadotForumTopicListItem.js
@@ -1,8 +1,15 @@
+import getChainSettings from "next-common/utils/consts/settings";
+import { removeTrailingSlash } from "next-common/utils/url";
+
 export default function normalizePolkadotForumTopicListItem(item) {
+  const chainSettings = getChainSettings(process.env.NEXT_PUBLIC_CHAIN);
+
   return {
     ...item,
     title: item.topic_title,
     time: item.updated_at || item.created_at,
-    detailLink: `https://forum.polkadot.network/t/${item.topic_slug}/${item.topic_id}`,
+    detailLink: `${removeTrailingSlash(chainSettings.discourseForumLink)}/t/${
+      item.topic_slug
+    }/${item.topic_id}`,
   };
 }
