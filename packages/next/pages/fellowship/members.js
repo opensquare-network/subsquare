b42b9716d8ea3284550bf6ece2f3c66195924833
Yongfeng LI <wliyongfeng@gmail.com>
2024-01-26 18:03:25 +0800

    Add rank filter on fellowship members page, #3846

--- packages/next/pages/fellowship/members.js
+++ packages/next/pages/fellowship/members.js
@@ -9,16 +9,36 @@ import {
 import { usePageProps } from "next-common/context/page";
 import useFetchFellowshipCoreMembers from "next-common/hooks/fellowship/core/useFetchFellowshipCoreMembers";
 import FellowshipCollectiveMembers from "next-common/components/fellowship/collective/list";
+import useRankFilter from "next-common/hooks/fellowship/useRankFilter";
+import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
+import { useMemo } from "react";
+import isNil from "lodash.isnil";
 
 export default function MembersPage() {
   const { fellowshipMembers } = usePageProps();
   const category = "Fellowship Members";
   const seoInfo = { title: category, desc: category };
   useFetchFellowshipCoreMembers(fellowshipMembers);
+  const ranks = [...new Set(fellowshipMembers.map((m) => m.rank))];
+  const { rank, component } = useRankFilter(ranks);
+
+  const filteredMembers = useMemo(() => {
+    if (isNil(rank)) {
+      return fellowshipMembers;
+    }
+
+    return fellowshipMembers.filter((m) => m.rank === rank);
+  }, [fellowshipMembers, rank]);
 
   return (
     <ListLayout seoInfo={seoInfo} title={category}>
-      <FellowshipCollectiveMembers members={fellowshipMembers} />
+      <div className="flex flex-col gap-y-4">
+        <TitleContainer>
+          <span>List</span>
+          {component}
+        </TitleContainer>
+        <FellowshipCollectiveMembers members={filteredMembers} />
+      </div>
     </ListLayout>
   );
 }
