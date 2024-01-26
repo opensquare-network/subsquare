0dad1551af0d81a1af2ba1840db79a24b6e898bd
2nthony <ant.hu@outlook.com>
2024-01-26 18:57:09 +0800

    fix: signal indicator animation

--- packages/next-common/components/icons/signalIndicator.jsx
+++ packages/next-common/components/icons/signalIndicator.jsx
@@ -1,27 +1,34 @@
-import {
-  SystemSignalActiveDark,
-  SystemSignalActiveLight,
-  SystemSignalInactiveDark,
-  SystemSignalInactiveLight,
-} from "@osn/icons/subsquare";
 import Tooltip from "../tooltip";
+import { cn } from "next-common/utils";
 
 export default function SignalIndicator({ active, className = "" }) {
   return (
     <Tooltip className={className} content={active ? "Active" : "Inactive"}>
-      {active && (
-        <>
-          <SystemSignalActiveLight className="dark:hidden" />
-          <SystemSignalActiveDark className="hidden dark:block" />
-        </>
-      )}
-
-      {!active && (
-        <>
-          <SystemSignalInactiveLight className="dark:hidden" />
-          <SystemSignalInactiveDark className="hidden dark:block" />
-        </>
-      )}
+      <div
+        className={cn(
+          "w-4 h-4 rounded-full bg-neutral100 relative flex items-center justify-center",
+        )}
+      >
+        <Indicator
+          className={cn(
+            active ? "bg-green500" : "bg-textDisabled",
+            active && "animate-ping",
+          )}
+        />
+        <Indicator className={cn(active ? "bg-green500" : "bg-textDisabled")} />
+      </div>
     </Tooltip>
+  );
+}
+
+function Indicator({ className = "" }) {
+  return (
+    <div
+      className={cn(
+        "absolute w-full h-full",
+        "w-1.5 h-1.5 rounded-[inherit]",
+        className,
+      )}
+    />
   );
 }
