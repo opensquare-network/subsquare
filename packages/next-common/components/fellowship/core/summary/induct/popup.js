7cf425e128b28d644d1328d5805155673c9cabf7
chaojun <chaojun@opensquare.network>
2024-01-30 16:33:36 +0800

    Implement induct popup

--- packages/next-common/components/fellowship/core/summary/induct/popup.js
+++ packages/next-common/components/fellowship/core/summary/induct/popup.js
@@ -1,27 +1,77 @@
-import SignerPopup from "next-common/components/signerPopup";
-import React from "react";
-import { emptyFunction } from "next-common/utils";
+import React, { useState } from "react";
 import useAddressInput from "next-common/components/fellowship/core/summary/induct/useAddressInput";
+import PopupWithSigner from "next-common/components/popupWithSigner";
+import Signer from "next-common/components/popup/fields/signerField";
+import { useChainSettings } from "next-common/context/chain";
+import useApi from "next-common/utils/hooks/useApi";
+import useAddressBalance from "next-common/utils/hooks/useAddressBalance";
+import { useDispatch } from "react-redux";
+import { useSignerAccount } from "next-common/components/popupWithSigner/context";
+import { PopupButtonWrapper } from "next-common/components/popup/wrapper";
+import PrimaryButton from "next-common/components/buttons/primaryButton";
+import { newErrorToast } from "next-common/store/reducers/toastSlice";
+import { sendTx, wrapWithProxy } from "next-common/utils/sendTx";
+import useIsMounted from "next-common/utils/hooks/useIsMounted";
 
-function Content() {
-  const { address, component } = useAddressInput("Who");
-  console.log("address", address);
+function Content({ onClose }) {
+  const node = useChainSettings();
+  const dispatch = useDispatch();
+  const signerAccount = useSignerAccount();
+  const [isCalling, setIsCalling] = useState(false);
+  const isMounted = useIsMounted();
+  const api = useApi();
+  const [balance, isBalanceLoading] = useAddressBalance(
+    api,
+    signerAccount?.realAddress,
+  );
+  const [signerBalance, isSignerBalanceLoading] = useAddressBalance(
+    api,
+    signerAccount?.address,
+  );
 
-  return component;
+  const { address: whoAddress, component: whoInput } = useAddressInput("Who");
+
+  const onConfirm = async () => {
+    if (!api) {
+      dispatch(newErrorToast("Chain network is not connected yet"));
+      return;
+    }
+
+    let tx = api.tx.fellowshipCore.induct(whoAddress);
+    if (signerAccount?.proxyAddress) {
+      tx = wrapWithProxy(api, tx, signerAccount.proxyAddress);
+    }
+
+    await sendTx({
+      tx,
+      dispatch,
+      setLoading: setIsCalling,
+      signerAddress: signerAccount.address,
+      isMounted,
+      onClose,
+    });
+  };
+
+  return (
+    <>
+      <Signer
+        balanceName="Balance"
+        balance={balance}
+        isBalanceLoading={isBalanceLoading}
+        signerBalance={signerBalance}
+        isSignerBalanceLoading={isSignerBalanceLoading}
+        symbol={node.symbol}
+      />
+      {whoInput}
+      <PopupButtonWrapper>
+        <PrimaryButton isLoading={isCalling} onClick={onConfirm}>
+          Confirm
+        </PrimaryButton>
+      </PopupButtonWrapper>
+    </>
+  );
 }
 
-export default function FellowshipCoreInductionPopup({
-  onClose = emptyFunction,
-}) {
-  return (
-    <SignerPopup
-      title="Induct"
-      onClose={onClose}
-      actionCallback={(api, account) =>
-        console.log("api", api, "account", account)
-      }
-    >
-      <Content />
-    </SignerPopup>
-  );
+export default function FellowshipCoreInductionPopup(props) {
+  return <PopupWithSigner title="Induct" Component={Content} {...props} />;
 }
