dbb17c52e1b254efcbd147aff3ce3dcbfca06ce4
2nthony <ant.hu@outlook.com>
2024-01-30 17:51:46 +0800

    add event filter

--- packages/next-common/components/fellowship/core/feeds/container.jsx
+++ packages/next-common/components/fellowship/core/feeds/container.jsx
@@ -1,7 +1,19 @@
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 import FellowshipCoreFeedsList from "./list";
+import Select from "next-common/components/select";
+import { useState } from "react";
 
 export default function FellowshipCoreFeedsContainer({ feeds = {} }) {
+  const [event, setEvent] = useState("");
+
+  // TODO: core feeds, event filter options
+  const options = [
+    {
+      label: "All",
+      value: "",
+    },
+  ];
+
   return (
     <>
       <TitleContainer>
@@ -13,6 +25,19 @@ export default function FellowshipCoreFeedsContainer({ feeds = {} }) {
             </span>
           )}
         </span>
+
+        <div className="text12Medium text-textPrimary flex items-center gap-x-2">
+          <div>Event</div>
+          <Select
+            className="w-20"
+            small
+            value={event}
+            options={options}
+            onChange={(option) => {
+              setEvent(option.value);
+            }}
+          />
+        </div>
       </TitleContainer>
       <div className="space-y-4 mt-4">
         <FellowshipCoreFeedsList feeds={feeds} />
