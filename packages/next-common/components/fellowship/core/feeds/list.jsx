275cba72cb1a34aef246521a2766da8ae7272092
2nthony <ant.hu@outlook.com>
2024-01-31 13:51:21 +0800

    fix: feeds responsive layout

--- packages/next-common/components/fellowship/core/feeds/list.jsx
+++ packages/next-common/components/fellowship/core/feeds/list.jsx
@@ -5,27 +5,24 @@ import FellowshipCoreFeedsListEvent from "./event";
 import tw from "tailwind-styled-components";
 import FellowshipCoreFeedsListTime from "./time";
 import FellowshipCoreFeedsListBlockLink from "./blockLink";
+import DataList from "next-common/components/dataList";
 
 const Bar = tw.div`grow w-0.5 bg-theme300 mx-auto`;
 
 export default function FellowshipCoreFeedsList({ feeds = {} }) {
+  const columns = [{ name: "event" }, { name: "block", width: 250 }];
+
   const rows = feeds?.items?.map?.((item) => {
     return [
-      <FellowshipCoreFeedsListEvent
-        key={"event"}
-        className={cn("sm:min-w-[640px] max-w-full", "max-sm:w-full")}
-        feed={item}
-      />,
-      <div key="block" className="flex items-center max-sm:gap-x-2">
+      <FellowshipCoreFeedsListEvent key="event" feed={item} className="pr-2" />,
+      <div key="block-info" className="flex items-center max-sm:gap-x-2">
         <FellowshipCoreFeedsListTime
-          key="time"
           feed={item}
           className="w-40 text-textTertiary max-sm:w-auto"
         />
         <FellowshipCoreFeedsListBlockLink
-          key={"block-link"}
           className={cn(
-            "opacity-0 group-hover/feed-item:opacity-100 max-sm:opacity-100",
+            "opacity-0 group-hover/datalist-item:opacity-100 max-sm:opacity-100",
             "text-textTertiary",
           )}
           feed={item}
@@ -36,32 +33,36 @@ export default function FellowshipCoreFeedsList({ feeds = {} }) {
 
   return (
     <SecondaryCard>
-      <div className="overflow-auto scrollbar-pretty text-textPrimary text14Medium">
-        {rows.map((row, idx) => (
-          <div key={idx} className={cn("group/feed-item", "flex gap-x-4")}>
-            <div className="flex flex-col">
-              <Bar className="group-first/feed-item:h-2.5 group-first/feed-item:grow-0 max-sm:grow-0 max-sm:h-2.5" />
+      <DataList
+        className={cn(
+          "text14Medium",
+          "[&_.datalist-head]:hidden [&_.datalist-body]:divide-y-0",
+          "[&_.datalist-item]:py-2.5",
+          "[&_.datalist-desktop-item]:items-start",
+          "[&_.datalist_.descriptions-item-label]:hidden",
+        )}
+        columns={columns}
+        rows={rows}
+        renderItem={(datalistItem, idx, arr) => {
+          const isLast = idx === arr.length - 1;
 
-              <div className="w-3 h-5 flex items-center">
-                <div className="w-3 h-3 border-[3px] border-theme500 rounded-full" />
+          return (
+            <div className="flex gap-x-4">
+              <div className="flex flex-col">
+                <Bar className="h-2.5 grow-0" />
+
+                <div className="w-3 h-5 flex items-center">
+                  <div className="w-3 h-3 border-[3px] border-theme500 rounded-full" />
+                </div>
+
+                <Bar className={cn(isLast && "bg-transparent")} />
               </div>
 
-              <Bar className="group-last/feed-item:bg-transparent" />
+              {datalistItem}
             </div>
-
-            <div
-              className={cn(
-                "flex items-center",
-                "w-full",
-                "py-2.5 max-sm:pb-5",
-                "max-sm:flex-col max-sm:items-start max-sm:gap-y-2.5",
-              )}
-            >
-              {row}
-            </div>
-          </div>
-        ))}
-      </div>
+          );
+        }}
+      />
 
       <Pagination
         page={feeds.page}
