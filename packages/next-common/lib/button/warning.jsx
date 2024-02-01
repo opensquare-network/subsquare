ad6450e67431b232870b562badfd2150b9870501
2nthony <ant.hu@outlook.com>
2024-02-01 15:46:34 +0800

    warning button

---
+++ packages/next-common/lib/button/warning.jsx
@@ -0,0 +1,19 @@
+import { cn } from "next-common/utils";
+import Button from ".";
+
+/**
+ * @param {ButtonProps} props
+ */
+export default function WarningButton(props) {
+  return (
+    <Button
+      {...props}
+      className={cn(
+        "text-textPrimaryContrast",
+        "bg-orange500 disabled:bg-orange300",
+        "border-transparent",
+        props.className,
+      )}
+    />
+  );
+}
