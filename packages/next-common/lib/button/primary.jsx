e1e5cd812b1e469aa8569bf8529d7d0e6bbc2f36
2nthony <ant.hu@outlook.com>
2024-02-01 15:37:00 +0800

    primary button

---
+++ packages/next-common/lib/button/primary.jsx
@@ -0,0 +1,20 @@
+import { cn } from "next-common/utils";
+import Button from ".";
+
+/**
+ * @param {ButtonProps} props
+ * @returns
+ */
+export default function PrimaryButton(props) {
+  return (
+    <Button
+      {...props}
+      className={cn(
+        "text-textPrimaryContrast",
+        "bg-theme500 disabled:bg-theme300",
+        "border-transparent",
+        props.className,
+      )}
+    />
+  );
+}
