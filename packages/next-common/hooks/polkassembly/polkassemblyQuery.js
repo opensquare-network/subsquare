d5327dd82cc4150fcc26f0a3f0caa9772207b470
chaojun <chaojun@opensquare.network>
2024-02-02 11:12:10 +0800

    Query polkassembly comments by the client side

---
+++ packages/next-common/hooks/polkassembly/polkassemblyQuery.js
@@ -0,0 +1,80 @@
+async function fetchGet(postUrl) {
+  const controller = new AbortController();
+  setTimeout(() => controller.abort(), 10000);
+  const resp = await fetch(postUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-network": process.env.NEXT_PUBLIC_CHAIN,
+    },
+    signal: controller.signal,
+  });
+
+  if (!resp.ok) {
+    const text = await resp.text();
+    throw new Error(text);
+  }
+
+  const result = await resp.json();
+  return result;
+}
+
+async function fetchOnChainPost(postId, proposalType) {
+  return await fetchGet(
+    `https://api.polkassembly.io/api/v1/posts/on-chain-post?postId=${postId}&proposalType=${proposalType}`,
+  );
+}
+
+async function fetchOffChainPost(postId, postType) {
+  return await fetchGet(
+    `https://api.polkassembly.io/api/v1/posts/off-chain-post?postId=${postId}&postType=${postType}`,
+  );
+}
+
+function toPolkassemblyApiPostType(postType) {
+  switch (postType) {
+    case "ReferendumV2": {
+      return "referendums_v2";
+    }
+    case "FellowshipReferendum": {
+      return "fellowship_referendums";
+    }
+    case "Tip": {
+      return "tips";
+    }
+    case "Bounty": {
+      return "bounties";
+    }
+    case "ChildBounty": {
+      return "child_bounties";
+    }
+    case "TreasuryProposal": {
+      return "treasury_proposals";
+    }
+    case "DemocracyProposal": {
+      return "democracy_proposals";
+    }
+    case "Referendum": {
+      return "referendums";
+    }
+    case "CouncilMotion": {
+      return "council_motions";
+    }
+    case "TechCommitteeProposal": {
+      return "tech_committee_proposals";
+    }
+    default: {
+      return "discussion";
+    }
+  }
+}
+
+export async function queryPostComments(postId, postType) {
+  const type = toPolkassemblyApiPostType(postType);
+
+  if (postType === "discussion") {
+    return await fetchOffChainPost(postId, type);
+  }
+
+  return await fetchOnChainPost(postId, type);
+}
