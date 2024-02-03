28fcdeaa057b3df5ec8d75865aec996aa7c014dd
chaojun <chaojun@opensquare.network>
2024-02-03 16:15:00 +0800

    refactor social links

---
+++ packages/next-common/components/iconLinks/index.jsx
@@ -0,0 +1,48 @@
+import {
+  LinkWebsite,
+  LinkTwitter,
+  LinkDiscord,
+  LinkTelegram,
+  LinkGithub,
+  LinkElement,
+  LinkMedium,
+  LinkYoutube,
+  LinkReddit,
+} from "@osn/icons/subsquare";
+import ExternalLink from "../externalLink";
+import { cn } from "next-common/utils";
+
+const iconMap = {
+  website: LinkWebsite,
+  twitter: LinkTwitter,
+  discord: LinkDiscord,
+  telegram: LinkTelegram,
+  github: LinkGithub,
+  element: LinkElement,
+  medium: LinkMedium,
+  youtube: LinkYoutube,
+  reddit: LinkReddit,
+};
+
+export default function IconLinks({ links, className }) {
+  return (
+    <ul className={cn("flex gap-x-3", className)}>
+      {links?.map?.((link) => {
+        const Icon = iconMap[link.name] || LinkWebsite;
+
+        return (
+          <li key={link.name}>
+            <ExternalLink
+              href={link.url}
+              title={link.title || link.name}
+              className="[&_path]:fill-textTertiary [&_path]:hover:fill-textSecondary"
+              externalIcon={false}
+            >
+              <Icon className="w-5 h-5" />
+            </ExternalLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
