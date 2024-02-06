f71fafefbd9c7ac849e3f3558e68c5626ad8100f
Yongfeng LI <wliyongfeng@gmail.com>
2024-02-06 22:32:08 +0800

    Change scan height subscription to loop query

--- packages/next-common/components/header/networkSwitch.js
+++ packages/next-common/components/header/networkSwitch.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import Loading from "../loading";
-import { nodesHeightSelector } from "../../store/reducers/nodeSlice";
 import useOnClickOutside from "../../utils/hooks/useOnClickOutside";
 import useWindowSize from "../../utils/hooks/useWindowSize";
 import ChainIcon from "./chainIcon";
 import { ArrowDown } from "@osn/icons/subsquare";
 import NetworkOptions from "./networkOptions";
+import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
 
 const Wrapper = styled.div`
   position: relative;
@@ -59,7 +59,7 @@ export default function NetworkSwitch({ activeNode }) {
   const [show, setShow] = useState(false);
   const ref = useRef();
   const windowSize = useWindowSize();
-  const nodesHeight = useSelector(nodesHeightSelector);
+  const nodesHeight = useSelector(chainOrScanHeightSelector);
 
   useOnClickOutside(ref, () => setShow(false));
 
