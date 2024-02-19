41fbc612c8475b6a7700f9b1b421cf5075bfdc72
Yongfeng LI <wliyongfeng@gmail.com>
2024-02-19 23:42:38 +0800

    Fix child bounties query

--- packages/next-common/services/list/fetchList.js
+++ packages/next-common/services/list/fetchList.js
@@ -1,14 +1,16 @@
 import { ssrNextApi as nextApi } from "next-common/services/nextApi";
 import { defaultPageSize, EmptyList } from "next-common/utils/constants";
 
-export async function fetchList(url, context) {
+export async function fetchList(url, context, params = {}) {
   const { page, page_size: pageSize } = context.query;
   const { result } = await nextApi.fetch(url, {
-    ...context.query,
+    ...(params || {}),
     page: page ?? 1,
     pageSize: pageSize ?? defaultPageSize,
     simple: true,
   });
+
+  console.log("result", result);
 
   return result ?? EmptyList;
 }
