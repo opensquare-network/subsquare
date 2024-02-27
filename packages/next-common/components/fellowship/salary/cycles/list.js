10364ad882e56baeeb524a8c573dc361a2e4e20d
2nthony <ant.hu@outlook.com>
2024-02-27 17:48:16 +0800

    list

--- packages/next-common/components/fellowship/salary/cycles/list.js
+++ packages/next-common/components/fellowship/salary/cycles/list.js
@@ -1,10 +1,34 @@
+import { ArrowRight } from "@osn/icons/subsquare";
+import noop from "lodash.noop";
 import DataList from "next-common/components/dataList";
 import Pagination from "next-common/components/pagination";
+import SecondaryButton from "next-common/lib/button/secondary";
+import FellowshipSalaryTimeRange from "./history/timeRange";
 
 const columns = [
   {
     name: "Index",
+    className: "w-20",
+  },
+  {
+    name: "Time Range",
+    className: "w-[240px]",
+  },
+  {
+    name: "Expenditure",
     className: "",
+  },
+  {
+    name: "Registered Paid",
+    className: "w-40 text-right",
+  },
+  {
+    name: "Unregistered Paid",
+    className: "w-40 text-right",
+  },
+  {
+    name: "",
+    className: "w-20 text-right",
   },
 ];
 
@@ -13,12 +37,34 @@ export default function FellowshipSalaryCycles({ historyCycles }) {
 
   const rows = items.map((item) => {
     const { index } = item;
-    return [index];
+    return [
+      index,
+      <FellowshipSalaryTimeRange key="time-range" cycle={item} />,
+      <div key="expenditure">expend</div>,
+      <div key="registered-paid">registered</div>,
+      <div key="unregistered-paid">unregistered</div>,
+      <div key="action">
+        <SecondaryButton
+          disabled
+          className="w-7 p-0"
+          size="small"
+          onClick={noop}
+        >
+          <ArrowRight className="w-4 h-4" />
+        </SecondaryButton>
+      </div>,
+    ];
   });
 
   return (
     <>
-      <DataList bordered columns={columns} noDataText="No Cycles" rows={rows} />
+      <DataList
+        className="text14Medium"
+        bordered
+        columns={columns}
+        noDataText="No Cycles"
+        rows={rows}
+      />
       <Pagination {...historyCycles} />
     </>
   );
