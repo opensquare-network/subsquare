385d92db467a0dc5f3eaedeefbdf9212caeb355d
2nthony <ant.hu@outlook.com>
2024-02-28 17:26:23 +0800

    feat: history cycle ui (#3908)
    * time range
    
    * list
    
    * register paid
    
    * add classname
    
    * stash
    
    * fix: list card
    
    * fix expenditure calc
    
    * fix datalist `status` assert
    
    * fix width
    
    * update mobile
    
    * fix: mobile
    
    * no need action for now

--- packages/next-common/components/fellowship/salary/cycles/list.js
+++ packages/next-common/components/fellowship/salary/cycles/list.js
@@ -1,25 +1,119 @@
 import DataList from "next-common/components/dataList";
 import Pagination from "next-common/components/pagination";
+import FellowshipSalaryExpenditure from "./history/expenditure";
+import {
+  FellowshipSalaryRegisteredPaid,
+  FellowshipSalaryUnregisteredPaid,
+} from "./history/registerPaid";
+import {
+  FellowshipSalaryTimeRange,
+  FellowshipSalaryTimeBlock,
+} from "./history/timeRange";
+import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 
-const columns = [
-  {
-    name: "Index",
-    className: "",
+const indexColumn = {
+  name: "Index",
+  width: 80,
+  cellRender(data) {
+    return data.index;
   },
+};
+
+const timeRangeColumn = {
+  name: "Time Range",
+  width: 240,
+  cellRender(data) {
+    return <FellowshipSalaryTimeRange cycle={data} />;
+  },
+};
+
+const expenditureColumn = {
+  name: "Expenditure",
+  className: "min-w-[140px]",
+  cellRender(data) {
+    return <FellowshipSalaryExpenditure cycle={data} />;
+  },
+};
+
+const registeredPaidColumn = {
+  name: "Registered Paid",
+  width: 160,
+  className: "text-right",
+  cellRender(data) {
+    return <FellowshipSalaryRegisteredPaid cycle={data} />;
+  },
+};
+
+const unregisteredPaidColumn = {
+  name: "Unregistered Paid",
+  width: 160,
+  className: "text-right",
+  cellRender(data) {
+    return <FellowshipSalaryUnregisteredPaid cycle={data} />;
+  },
+};
+
+const desktopColumns = [
+  indexColumn,
+  timeRangeColumn,
+  expenditureColumn,
+  registeredPaidColumn,
+  unregisteredPaidColumn,
+];
+
+const mobileColumns = [
+  indexColumn,
+  {
+    name: "Start Time",
+    cellRender(data) {
+      return (
+        <FellowshipSalaryTimeBlock key="start-time" indexer={data.indexer} />
+      );
+    },
+  },
+  {
+    name: "End Time",
+    cellRender(data) {
+      return (
+        <FellowshipSalaryTimeBlock key="end-time" indexer={data.endIndexer} />
+      );
+    },
+  },
+  expenditureColumn,
+  registeredPaidColumn,
+  unregisteredPaidColumn,
 ];
 
 export default function FellowshipSalaryCycles({ historyCycles }) {
   const { items = [] } = historyCycles || {};
 
-  const rows = items.map((item) => {
-    const { index } = item;
-    return [index];
+  const desktopRows = items.map((item) => {
+    return desktopColumns.map((col) => col.cellRender?.(item));
+  });
+
+  const mobileRows = items.map((item) => {
+    return mobileColumns.map((col) => col.cellRender?.(item));
   });
 
   return (
-    <>
-      <DataList bordered columns={columns} noDataText="No Cycles" rows={rows} />
-      <Pagination {...historyCycles} />
-    </>
+    <SecondaryCard>
+      <DataList
+        className="text14Medium max-sm:hidden"
+        columns={desktopColumns}
+        noDataText="No Cycles"
+        rows={desktopRows}
+      />
+
+      <DataList
+        className="text14Medium hidden max-sm:block"
+        columns={mobileColumns}
+        noDataText="No Cycles"
+        rows={mobileRows}
+      />
+
+      <div className="mt-4">
+        <Pagination {...historyCycles} />
+      </div>
+    </SecondaryCard>
   );
 }
