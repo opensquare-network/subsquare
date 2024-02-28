c292a23000ce2e0ca634d67a66453ec8ca644372
2nthony <ant.hu@outlook.com>
2024-02-28 16:38:06 +0800

    fix: mobile

--- packages/next-common/components/fellowship/salary/cycles/list.js
+++ packages/next-common/components/fellowship/salary/cycles/list.js
@@ -9,76 +9,132 @@ import {
   FellowshipSalaryRegisteredPaid,
   FellowshipSalaryUnregisteredPaid,
 } from "./history/registerPaid";
-import FellowshipSalaryTimeRange from "./history/timeRange";
+import {
+  FellowshipSalaryTimeRange,
+  FellowshipSalaryTimeBlock,
+} from "./history/timeRange";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 
-const columns = [
+const indexColumn = {
+  name: "Index",
+  width: 80,
+  cellRender(data) {
+    return data.index;
+  },
+};
+
+const timeRangeColumn = {
+  name: "Time Range",
+  width: 240,
+  cellRender(data) {
+    return <FellowshipSalaryTimeRange cycle={data} />;
+  },
+};
+
+const expenditureColumn = {
+  name: "Expenditure",
+  className: "min-w-[140px]",
+  cellRender(data) {
+    return <FellowshipSalaryExpenditure cycle={data} />;
+  },
+};
+
+const registeredPaidColumn = {
+  name: "Registered Paid",
+  width: 160,
+  className: "text-right",
+  cellRender(data) {
+    return <FellowshipSalaryRegisteredPaid cycle={data} />;
+  },
+};
+
+const unregisteredPaidColumn = {
+  name: "Unregistered Paid",
+  width: 160,
+  className: "text-right",
+  cellRender(data) {
+    return <FellowshipSalaryUnregisteredPaid cycle={data} />;
+  },
+};
+
+const actionColumn = {
+  name: "",
+  width: 80,
+  className: "text-right",
+  cellRender() {
+    return (
+      <Tooltip content="View Detail">
+        <SecondaryButton
+          disabled
+          className="w-7 p-0"
+          size="small"
+          onClick={noop}
+        >
+          <ArrowRight className="w-4 h-4" />
+        </SecondaryButton>
+      </Tooltip>
+    );
+  },
+};
+
+const desktopColumns = [
+  indexColumn,
+  timeRangeColumn,
+  expenditureColumn,
+  registeredPaidColumn,
+  unregisteredPaidColumn,
+  actionColumn,
+];
+
+const mobileColumns = [
+  indexColumn,
   {
-    name: "Index",
-    width: 80,
+    name: "Start Time",
+    cellRender(data) {
+      return (
+        <FellowshipSalaryTimeBlock key="start-time" indexer={data.indexer} />
+      );
+    },
   },
   {
-    name: "Time Range",
-    width: 240,
+    name: "End Time",
+    cellRender(data) {
+      return (
+        <FellowshipSalaryTimeBlock key="end-time" indexer={data.endIndexer} />
+      );
+    },
   },
-  {
-    name: "Expenditure",
-    className: "min-w-[140px]",
-  },
-  {
-    name: "Registered Paid",
-    width: 160,
-    className: "text-right",
-  },
-  {
-    name: "Unregistered Paid",
-    width: 160,
-    className: "text-right",
-  },
-  {
-    name: "",
-    width: 80,
-    className: "text-right",
-  },
+  expenditureColumn,
+  registeredPaidColumn,
+  unregisteredPaidColumn,
+  actionColumn,
 ];
 
 export default function FellowshipSalaryCycles({ historyCycles }) {
   const { items = [] } = historyCycles || {};
 
-  const rows = items.map((item) => {
-    const { index } = item;
-    return [
-      index,
-      <FellowshipSalaryTimeRange key="time-range" cycle={item} />,
-      <FellowshipSalaryExpenditure key="expenditure" cycle={item} />,
-      <div key="registered-paid">
-        <FellowshipSalaryRegisteredPaid cycle={item} />
-      </div>,
-      <div key="unregistered-paid">
-        <FellowshipSalaryUnregisteredPaid cycle={item} />
-      </div>,
-      <div key="action">
-        <Tooltip content="View Detail">
-          <SecondaryButton
-            disabled
-            className="w-7 p-0"
-            size="small"
-            onClick={noop}
-          >
-            <ArrowRight className="w-4 h-4" />
-          </SecondaryButton>
-        </Tooltip>
-      </div>,
-    ];
+  const desktopRows = items.map((item) => {
+    return desktopColumns.map((col) => col.cellRender?.(item));
+  });
+
+  const mobileRows = items.map((item) => {
+    return mobileColumns.map((col) => col.cellRender?.(item));
   });
 
   return (
     <SecondaryCard>
       <DataList
-        className="text14Medium"
-        columns={columns}
+        className="text14Medium max-sm:hidden"
+        columns={desktopColumns}
         noDataText="No Cycles"
-        rows={rows}
+        rows={desktopRows}
+      />
+
+      <DataList
+        className="text14Medium hidden max-sm:block"
+        columns={mobileColumns}
+        noDataText="No Cycles"
+        rows={mobileRows}
       />
 
       <div className="mt-4">
