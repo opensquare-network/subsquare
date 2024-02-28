89a86bb9f7f0d6d564215c42cbd70205ca7be8f9
2nthony <ant.hu@outlook.com>
2024-02-28 17:57:39 +0800

    feat: add all cycles link (#3911)

--- packages/next-common/components/overview/fellowship/salary/overview.js
+++ packages/next-common/components/overview/fellowship/salary/overview.js
@@ -1,11 +1,25 @@
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 import FellowshipSalaryStats from "next-common/components/overview/fellowship/salary/stats";
+import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
+import Link from "next/link";
 
 export default function FellowshipSalaryOverview() {
   return (
     <>
       <TitleContainer className="mb-4">Fellowship Salary Cycle</TitleContainer>
-      <FellowshipSalaryStats />
+
+      <SecondaryCard>
+        <FellowshipSalaryStats />
+
+        <div className="mt-2 flex justify-end">
+          <Link
+            href="/fellowship/salary"
+            className="text14Medium text-theme500"
+          >
+            All Cycles
+          </Link>
+        </div>
+      </SecondaryCard>
     </>
   );
 }
