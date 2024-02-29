27dac8c5cf19988eacc572c2b35d10a40978ebbd
2nthony <ant.hu@outlook.com>
2024-02-29 17:28:03 +0800

    revert: detail layout

--- packages/next-common/components/fellowship/salary/cycles/layout.jsx
+++ packages/next-common/components/fellowship/salary/cycles/layout.jsx
@@ -1,4 +1,6 @@
-import DetailLayout from "next-common/components/layout/DetailLayout";
+import Breadcrumbs from "next-common/components/layout/DetailLayout/breadcrumbs";
+import BaseLayout from "next-common/components/layout/baseLayout";
+import { cn } from "next-common/utils";
 
 export default function FellowshipSalaryCycleLayout({ children, ...props }) {
   const title = "Fellowship Salary Cycle";
@@ -7,14 +9,14 @@ export default function FellowshipSalaryCycleLayout({ children, ...props }) {
   const seoInfo = { title, desc };
 
   return (
-    <DetailLayout
-      title={title}
-      description={desc}
-      seoInfo={seoInfo}
-      wrapChildrenWithPanel={false}
-      {...props}
-    >
-      {children}
-    </DetailLayout>
+    <BaseLayout title={title} description={desc} seoInfo={seoInfo} {...props}>
+      <div className={cn("bg-pageBg px-6 flex-1 flex flex-col", "max-sm:px-0")}>
+        <div className="mx-auto py-6 max-w-[1200px] w-full">
+          <Breadcrumbs />
+
+          {children}
+        </div>
+      </div>
+    </BaseLayout>
   );
 }
