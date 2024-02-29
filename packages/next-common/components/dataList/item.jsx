e93865723ff2b92e942a4539dcd112e0124249e3
chaojun <chaojun@opensquare.network>
2024-02-29 12:49:30 +0800

    fix: mobile layout

--- packages/next-common/components/dataList/item.jsx
+++ packages/next-common/components/dataList/item.jsx
@@ -106,7 +106,7 @@ function MobileContent({ row = [], data, columns }) {
   return (
     <div className="datalist-mobile-item sm:hidden sm:py-4 space-y-3">
       <div className="flex">
-        <div className="flex items-center justify-between">
+        <div className="flex grow items-center justify-between">
           {first.value}
           {action && items[actionIdx]?.value}
         </div>
