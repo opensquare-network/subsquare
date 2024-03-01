c7a34cca28ccac3eb8d72c066bd5046d667fdbbb
2nthony <ant.hu@outlook.com>
2024-03-01 18:56:50 +0800

    fix: cycle registrations data list (#3928)
    * descriptions support key
    
    * datalist mobile pass key to descriptions
    
    * Revert "datalist mobile pass key to descriptions"
    
    This reverts commit 8763a95d0c0c5c6885f9f15a8a87ee5fa33c1d76.
    
    * Revert "descriptions support key"
    
    This reverts commit 4e7331674aeb41262fd3dccee17d6c2f1337a993.
    
    * fix: cycle registrations data list
    
    * fix nodata
    
    * fix: nodata text
    
    * fix: salary
    
    * add stats detail link

--- packages/next-common/components/fellowship/salary/cycles/tabsList/registrations.jsx
+++ packages/next-common/components/fellowship/salary/cycles/tabsList/registrations.jsx
@@ -1,11 +1,18 @@
+import FellowshipRank from "next-common/components/fellowship/rank";
+import { AddressUser } from "next-common/components/user";
+import ValueDisplay from "next-common/components/valueDisplay";
 import { usePageProps } from "next-common/context/page";
 import { fellowshipSalaryCycleRegistrationsApi } from "next-common/services/url";
+import { toPrecision } from "next-common/utils";
 import { useRouter } from "next/router";
+import FellowshipSalaryCycleTabRegistrationPaymentCell from "./registrationsPaymentCell";
+import { useSalaryAsset } from "next-common/hooks/useSalaryAsset";
 
 export function useFellowshipSalaryCycleRegistrationsTabItem() {
   const { registrations } = usePageProps();
   const { query } = useRouter();
   const { id } = query;
+  const { decimals, symbol } = useSalaryAsset();
 
   return {
     name: "Registrations",
@@ -13,8 +20,38 @@ export function useFellowshipSalaryCycleRegistrationsTabItem() {
     columns: [
       {
         name: "Rank",
+        width: 80,
         cellRender(data) {
-          return <div>{data.memberInfo.rank}</div>;
+          return <FellowshipRank rank={data.memberInfo.rank} />;
+        },
+      },
+      {
+        name: "Member",
+        width: 240,
+        cellRender(data) {
+          return <AddressUser add={data.who} />;
+        },
+      },
+      {
+        name: "Salary",
+        width: 160,
+        cellRender(data) {
+          return (
+            <ValueDisplay
+              value={toPrecision(data.salary, decimals)}
+              symbol={symbol}
+            />
+          );
+        },
+      },
+      {
+        name: "Payment",
+        minWidth: 240,
+        className: "min-w-[320px]",
+        cellRender(data) {
+          return (
+            <FellowshipSalaryCycleTabRegistrationPaymentCell data={data} />
+          );
         },
       },
     ],
