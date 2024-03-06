d887c7e68dc82103f3009f20d2862e3a5a7a3ebf
Yongfeng LI <wliyongfeng@gmail.com>
2024-03-06 11:43:50 +0800

    Init delegate container, #3945

---
+++ packages/next-common/components/delegation/delegate/container.jsx
@@ -0,0 +1,10 @@
+import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
+
+export default function DelegateContainer() {
+  return (
+    <TitleContainer>
+      <span>Delegates</span>
+      {/*  todo: delegates sort and filter */}
+    </TitleContainer>
+  );
+}
--- packages/next/pages/delegation/index.js
+++ packages/next/pages/delegation/index.js
@@ -1,6 +1,7 @@
 import { withCommonProps } from "next-common/lib";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 import ListLayout from "next-common/components/layout/ListLayout";
+import DelegateContainer from "next-common/components/delegation/delegate/container";
 
 export default function ReferendaPage() {
   const title = "Delegation";
@@ -8,8 +9,19 @@ export default function ReferendaPage() {
   const seoInfo = { title, desc };
 
   return (
-    <ListLayout seoInfo={seoInfo} title={title} description={seoInfo.desc}>
-      todo: content here
+    <ListLayout
+      seoInfo={seoInfo}
+      title={title}
+      description={seoInfo.desc}
+      tabs={[
+        {
+          label: "Delegates",
+          url: "/delegation",
+          exactMatch: true,
+        },
+      ]}
+    >
+      <DelegateContainer />
     </ListLayout>
   );
 }
