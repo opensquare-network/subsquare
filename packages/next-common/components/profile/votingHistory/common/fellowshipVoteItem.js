42572e632446fa90c71a191a35d56264fd118a19
chaojun <chaojun@opensquare.network>
2024-03-07 18:24:35 +0800

    Simplify code

--- packages/next-common/components/profile/votingHistory/common/fellowshipVoteItem.js
+++ packages/next-common/components/profile/votingHistory/common/fellowshipVoteItem.js
@@ -1,13 +1,10 @@
-import tw from "tailwind-styled-components";
 import { Aye, Nay } from "./voteItem";
-
-const PartialVoteItem = tw.div`flex justify-between md:w-[120px]`;
 
 export function FellowshipVoteItem({ vote }) {
   return (
-    <PartialVoteItem>
+    <div className="flex justify-between md:w-[120px]">
       {vote.isAye ? <Aye /> : <Nay />}
       <span className="text-textPrimary">{vote.votes}</span>
-    </PartialVoteItem>
+    </div>
   );
 }
