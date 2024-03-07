87b58973d67aed433f0ec7b6e41aef2e0620b69a
2nthony <ant.hu@outlook.com>
2024-03-07 16:04:54 +0800

    feat: fellowship overview link (#3955)

--- packages/next-common/components/summary/allianceOverviewSummary/index.js
+++ packages/next-common/components/summary/allianceOverviewSummary/index.js
@@ -1,5 +1,6 @@
 import Summary from "next-common/components/summary/v2/base";
 import { usePageProps } from "next-common/context/page";
+import ActiveValue from "../overviewSummary/activeValue";
 
 export default function AllianceOverviewSummary() {
   const { summary } = usePageProps();
@@ -10,11 +11,35 @@ export default function AllianceOverviewSummary() {
   return (
     <Summary
       items={[
-        { title: "Fellowship", content: fellowshipReferenda.active },
-        { title: "Motions", content: allianceMotions.active || 0 },
         {
-          title: "Announcements",
-          content: allianceAnnouncements.active || 0,
+          title: "Fellowship Referenda",
+          content: (
+            <ActiveValue
+              href={"/fellowship"}
+              tooltip="Active referenda"
+              value={fellowshipReferenda?.active || 0}
+            />
+          ),
+        },
+        {
+          title: "Alliance Motions",
+          content: (
+            <ActiveValue
+              href={"/alliance/motions"}
+              tooltip="Active motions"
+              value={allianceMotions?.active || 0}
+            />
+          ),
+        },
+        {
+          title: "Alliance Announcements",
+          content: (
+            <ActiveValue
+              href={"/alliance/announcements"}
+              tooltip="Active announcements"
+              value={allianceAnnouncements?.active || 0}
+            />
+          ),
         },
       ]}
     />
