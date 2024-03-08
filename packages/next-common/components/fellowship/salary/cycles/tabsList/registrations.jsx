15de7183b4a3d7c36e97b4cb3231d5b2df30b8df
2nthony <ant.hu@outlook.com>
2024-03-08 16:14:48 +0800

    clean mock

--- packages/next-common/components/fellowship/salary/cycles/tabsList/registrations.jsx
+++ packages/next-common/components/fellowship/salary/cycles/tabsList/registrations.jsx
@@ -12,33 +12,6 @@ export function useFellowshipSalaryCycleRegistrationsTabItem() {
   const { registrations } = usePageProps();
   const { id } = usePageProps();
 
-  registrations.items[0] = {
-    index: 1,
-    who: "16a357f5Sxab3V2ne4emGQvqJaCLeYpTMx3TCjnQhmJQ71DX",
-    beneficiary: "16a357f5Sxab3V2ne4emGQvqJaCLeYpTMx3TCjnQhmJQ71DX",
-    isRegistered: true,
-    salary: "20000000000",
-    amount: "20000000000",
-    isPaid: true,
-    indexer: {
-      blockHeight: 3201749,
-      blockHash:
-        "0xc0a6678059fa86a0ef264d2aacc137c9100149ecfc17d585b7444baf61b2d3a5",
-      blockTime: 1708084632000,
-      eventIndex: 2,
-      extrinsicIndex: 2,
-    },
-    paidIndexer: {
-      blockHeight: 3201749,
-      blockHash:
-        "0xc0a6678059fa86a0ef264d2aacc137c9100149ecfc17d585b7444baf61b2d3a5",
-      blockTime: 1708084632000,
-      eventIndex: 2,
-      extrinsicIndex: 2,
-    },
-    memberInfo: { salary: 20000000000, isActive: true, rank: 2 },
-  };
-
   const rankColumn = useFellowshipSalaryCycleRankColumn();
   const accountColumn = useFellowshipSalaryCycleAccountColumn();
   const yearlySalaryColumn = useFellowshipSalaryCycleYearlySalaryColumn();
