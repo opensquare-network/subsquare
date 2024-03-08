f93474c5dc5c5015f96532546b6437d13098e4de
Yongfeng LI <wliyongfeng@gmail.com>
2024-03-08 14:49:50 +0800

    Subscribe address democracy delegation, #3945 (#3965)

--- packages/next-common/components/summary/democracySummaryDelegation/newDelegation.js
+++ packages/next-common/components/summary/democracySummaryDelegation/newDelegation.js
@@ -1,23 +1,14 @@
-import React, { useCallback } from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../styled";
 import DelegatePopup from "next-common/components/democracy/delegatePopup";
 import MoonDelegatePopup from "next-common/components/democracy/delegatePopup/moonPopup";
 import AddSVG from "next-common/assets/imgs/icons/add.svg";
 import isMoonChain from "next-common/utils/isMoonChain";
 import useIsUseMetamask from "next-common/hooks/useIsUseMetamask";
-import { useDispatch } from "react-redux";
-import { newSuccessToast } from "next-common/store/reducers/toastSlice";
 
-export default function DemocracyNewDelegation({ refresh }) {
+export default function DemocracyNewDelegation() {
   const [showDelegatePopup, setShowDelegatePopup] = useState(false);
   const isUseMetamask = useIsUseMetamask();
-
-  const dispatch = useDispatch();
-  const onDelegateInBlock = useCallback(() => {
-    refresh();
-    dispatch(newSuccessToast("Delegate success"));
-  }, [dispatch, refresh]);
 
   let TheDelegatePopup = DelegatePopup;
   if (isMoonChain() && isUseMetamask) {
@@ -31,10 +22,7 @@ export default function DemocracyNewDelegation({ refresh }) {
         New Delegate
       </Button>
       {showDelegatePopup && (
-        <TheDelegatePopup
-          onInBlock={onDelegateInBlock}
-          onClose={() => setShowDelegatePopup(false)}
-        />
+        <TheDelegatePopup onClose={() => setShowDelegatePopup(false)} />
       )}
     </>
   );
--- packages/next-common/components/summary/democracySummaryDelegation/removeDelegation.js
+++ packages/next-common/components/summary/democracySummaryDelegation/removeDelegation.js
@@ -1,5 +1,4 @@
-import React, { useCallback } from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../styled";
 import RemoveSVG from "next-common/assets/imgs/icons/remove.svg";
 import UndelegatePopup from "./undelegatePopup";
@@ -8,27 +7,16 @@ import styled from "styled-components";
 import Tooltip from "../../tooltip";
 import isMoonChain from "next-common/utils/isMoonChain";
 import useIsUseMetamask from "next-common/hooks/useIsUseMetamask";
-import { newSuccessToast } from "next-common/store/reducers/toastSlice";
-import { useDispatch } from "react-redux";
 
 const RemoveButton = styled(Button)`
   display: flex;
   padding: 7px;
 `;
 
-export default function DemocracyRemoveDelegation({
-  ButtonComponent,
-  refresh,
-}) {
+export default function DemocracyRemoveDelegation({ ButtonComponent }) {
   const [isLoading, setIsLoading] = useState(false);
   const [showUndelegatePopup, setShowUndelegatePopup] = useState(false);
   const isUseMetamask = useIsUseMetamask();
-
-  const dispatch = useDispatch();
-  const onUndelegateInBlock = useCallback(() => {
-    refresh();
-    dispatch(newSuccessToast("Undelegated"));
-  }, [dispatch, refresh]);
 
   let TheUndelegatePopup = UndelegatePopup;
   if (isMoonChain() && isUseMetamask) {
@@ -56,7 +44,6 @@ export default function DemocracyRemoveDelegation({
       </Tooltip>
       {showUndelegatePopup && (
         <TheUndelegatePopup
-          onInBlock={onUndelegateInBlock}
           onClose={() => setShowUndelegatePopup(false)}
           isLoading={isLoading}
           setIsLoading={setIsLoading}
