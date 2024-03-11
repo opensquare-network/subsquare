8799bf199e2ff9f21696f93c60c1b5733f4cbf80
2nthony <ant.hu@outlook.com>
2024-03-11 18:54:57 +0800

    feat: isomophic nextApi

--- packages/next-common/services/nextApi.js
+++ packages/next-common/services/nextApi.js
@@ -1,9 +1,14 @@
+import { IS_SERVER } from "next-common/utils/constants";
 import Api from "./api";
 
-export default new Api(
+const clientNextApi = new Api(
   new URL("/api/", process.env.NEXT_PUBLIC_API_END_POINT).href,
 );
 
 export const ssrNextApi = new Api(
   new URL(process.env.NEXT_PUBLIC_BACKEND_API_END_POINT).href,
 );
+
+export const nextApi = IS_SERVER ? ssrNextApi : clientNextApi;
+
+export default nextApi;
