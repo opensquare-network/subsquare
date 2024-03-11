04a89ab70988b2010c8640830fbfa7021bd5c686
2nthony <ant.hu@outlook.com>
2024-03-11 19:00:24 +0800

    private ssrNextApi

--- packages/next-common/services/nextApi.js
+++ packages/next-common/services/nextApi.js
@@ -5,7 +5,7 @@ const clientNextApi = new Api(
   new URL("/api/", process.env.NEXT_PUBLIC_API_END_POINT).href,
 );
 
-export const ssrNextApi = new Api(
+const ssrNextApi = new Api(
   new URL(process.env.NEXT_PUBLIC_BACKEND_API_END_POINT).href,
 );
 
