bb9087f1132cbd24c6fcedb26b65bc242cab59d2
2nthony <ant.hu@outlook.com>
2024-03-13 16:30:38 +0800

    target field support disabled and address props

--- packages/next-common/components/democracy/delegatePopup/target.js
+++ packages/next-common/components/democracy/delegatePopup/target.js
@@ -1,6 +1,22 @@
 import React from "react";
 import AddressComboField from "next-common/components/popup/fields/addressComboField";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import Account from "next-common/components/account";
+import PopupLabel from "next-common/components/popup/label";
 
 export default function Target(props) {
-  return <AddressComboField {...props} title="Target" />;
+  const label = "Target";
+
+  if (props.disabled) {
+    return (
+      <div>
+        <PopupLabel text={label} />
+        <GreyPanel className="py-3 px-4 gap-4">
+          <Account account={{ address: props.address }} />
+        </GreyPanel>
+      </div>
+    );
+  }
+
+  return <AddressComboField {...props} title={label} />;
 }
