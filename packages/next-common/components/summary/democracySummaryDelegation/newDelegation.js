dc765f199d8bbfb977cab7dead8088965cba1c34
2nthony <ant.hu@outlook.com>
2024-03-15 15:40:08 +0800

    feat: delegation, democracy actions (#4008)
    * add disabled prop
    
    * if has delegating, then disabled the button
    
    * detail popup actions

--- packages/next-common/components/summary/democracySummaryDelegation/newDelegation.js
+++ packages/next-common/components/summary/democracySummaryDelegation/newDelegation.js
@@ -9,6 +9,7 @@ import useIsUseMetamask from "next-common/hooks/useIsUseMetamask";
 export default function DemocracyNewDelegation({
   defaultTargetAddress,
   targetDisabled,
+  disabled,
 }) {
   const [showDelegatePopup, setShowDelegatePopup] = useState(false);
   const isUseMetamask = useIsUseMetamask();
@@ -21,6 +22,7 @@ export default function DemocracyNewDelegation({
   return (
     <>
       <SecondaryButton
+        disabled={disabled}
         size="small"
         iconLeft={<SystemPlus className="w-4 h-4" />}
         onClick={() => {
