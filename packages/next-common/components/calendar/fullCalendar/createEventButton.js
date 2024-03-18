eb5a82f7ce6faf8009c730bff47a0092fa600e8c
2nthony <ant.hu@outlook.com>
2024-03-18 17:59:32 +0800

    fix: calendar event button

--- packages/next-common/components/calendar/fullCalendar/createEventButton.js
+++ packages/next-common/components/calendar/fullCalendar/createEventButton.js
@@ -9,13 +9,21 @@ export default function CreateEventButton({ disabled, onClick = noop }) {
     <Tooltip content={disabled ? "Only admins can create events" : ""}>
       <PrimaryButton
         size="small"
+        className="w-7 p-0 sm:hidden"
         disabled={disabled}
         onClick={onClick}
-        icon={
-          <SystemPlus className="[&_path]:fill-textPrimaryContrast w-4 h-4" />
-        }
       >
-        <span className="max-sm:hidden">Create Event</span>
+        <SystemPlus className="w-4 h-4" />
+      </PrimaryButton>
+
+      <PrimaryButton
+        size="small"
+        className="max-sm:hidden"
+        disabled={disabled}
+        onClick={onClick}
+        iconLeft={<SystemPlus className="w-4 h-4" />}
+      >
+        Create Event
       </PrimaryButton>
     </Tooltip>
   );
