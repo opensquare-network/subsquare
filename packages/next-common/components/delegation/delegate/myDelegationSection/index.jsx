077548690a898dea7d3ad0572a1d03af8be4fa5c
hyifeng <chaojun@opensquare.network>
2024-03-19 13:55:11 +0800

    Add announcement publish coming soon, #3945 (#4030)
    * Add announcement publish coming soon, #3945
    
    * fix: mobile layout
    
    * fix

--- packages/next-common/components/delegation/delegate/myDelegationSection/index.jsx
+++ packages/next-common/components/delegation/delegate/myDelegationSection/index.jsx
@@ -1,9 +1,11 @@
 import AllDelegationCard from "./allDelegationCard";
+import PublicAnnouncement from "./publicAnnouncement";
 
 export default function MyDelegationSection() {
   return (
-    <div>
+    <div className="flex flex-col gap-[16px]">
       <AllDelegationCard />
+      <PublicAnnouncement />
     </div>
   );
 }
---
+++ packages/next-common/components/delegation/delegate/myDelegationSection/publicAnnouncement.jsx
@@ -0,0 +1,29 @@
+import { MenuAnnouncement } from "@osn/icons/subsquare";
+import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
+import Tooltip from "next-common/components/tooltip";
+import PrimaryButton from "next-common/lib/button/primary";
+
+export default function PublicAnnouncement() {
+  return (
+    <SecondaryCard className="flex flex-row gap-[16px] max-sm:flex-col">
+      <div className="flex grow gap-[16px] sm:items-center max-sm:gap-[12px] max-sm:flex-col">
+        <div className="w-[40px] h-[40px] p-[8px] rounded-[8px] bg-theme100">
+          <MenuAnnouncement className="w-[24px] h-[24px] [&_path]:fill-theme500" />
+        </div>
+        <div className="flex flex-col grow">
+          <span className="text16Bold text-textPrimary">
+            Publish my announcement
+          </span>
+          <span className="text14Medium text-textTertiary">
+            Make a statement to get more delegators.
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center max-sm:justify-end">
+        <Tooltip content="Coming soon">
+          <PrimaryButton disabled>Publish</PrimaryButton>
+        </Tooltip>
+      </div>
+    </SecondaryCard>
+  );
+}
