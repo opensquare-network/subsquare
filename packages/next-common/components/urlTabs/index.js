fce6ac3484cbf17425f27ca9eb5f279e9de23443
2nthony <ant.hu@outlook.com>
2024-03-26 11:34:10 +0800

    fix: list layout tabs (#4059)

--- packages/next-common/components/urlTabs/index.js
+++ packages/next-common/components/urlTabs/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { isExternalLink } from "next-common/utils";
 import TabsList from "../tabsList";
 import { forwardRef } from "react";
+import { isNil } from "lodash-es";
 
 export default forwardRef(function UrlTabs({ tabs = [], ...props }, ref) {
   const router = useRouter();
@@ -15,10 +16,17 @@ export default forwardRef(function UrlTabs({ tabs = [], ...props }, ref) {
       tabs={tabs.map((tab, idx) => {
         const isExternal = isExternalLink(tab.url);
 
-        const active =
-          tab.exactMatch === false
-            ? routePath.startsWith(tab.url)
-            : routePath === tab.url;
+        let active = tab.active;
+        if (isNil(active)) {
+          if (tab.exactMatch === false) {
+            active = routePath.startsWith(tab.root || tab.url);
+          } else {
+            const urls = [tab.url, tab.root, ...(tab.urls || [])].filter(
+              Boolean,
+            );
+            active = urls.includes(routePath);
+          }
+        }
 
         return {
           ...tab,
