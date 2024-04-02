e282d15e86c9b9e809956a5c1ac6c81d194fad63
2nthony <ant.hu@outlook.com>
2024-04-02 11:52:44 +0800

    add hover for registered

--- packages/next-common/components/fellowship/salary/claimants/status.jsx
+++ packages/next-common/components/fellowship/salary/claimants/status.jsx
@@ -1,15 +1,22 @@
 import { has } from "lodash-es";
+import Tooltip from "next-common/components/tooltip";
+import { useSalaryAsset } from "next-common/hooks/useSalaryAsset";
+import { toPrecision } from "next-common/utils";
 
 export default function FellowshipSalaryMemberStatus({ status }) {
+  const { decimals, symbol } = useSalaryAsset();
+
   let content = <span className="text-textTertiary">-</span>;
+  let tooltipContent;
 
   if (has(status, "attempted")) {
     content = <span className="text-blue500">Attempted</span>;
   } else if (has(status, "registered")) {
+    tooltipContent = `${toPrecision(status.registered, decimals)} ${symbol}`;
     content = <span className="text-green500">Registered</span>;
   } else if (has(status, "nothing")) {
     content = <span className="text-textTertiary">Nothing</span>;
   }
 
-  return <div className="inline-block">{content}</div>;
+  return <Tooltip content={tooltipContent}>{content}</Tooltip>;
 }
