61cceb963363ea21a7676913a40efbd9483f1eb8
2nthony <ant.hu@outlook.com>
2024-04-03 15:42:12 +0800

    democracy delegate

--- packages/next-common/components/delegation/delegate/myDelegationSection/publicAnnouncement/democracy.jsx
+++ packages/next-common/components/delegation/delegate/myDelegationSection/publicAnnouncement/democracy.jsx
@@ -1,8 +1,10 @@
+import { AvatarContextProvider } from "next-common/context/avatar";
 import nextApi from "next-common/services/nextApi";
 import { delegationDemocracyDelegatesAddressApi } from "next-common/services/url";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useEffect, useState } from "react";
-import Delegates from "../../democracy/members";
+import MemberCardListContainer from "../../common/cardListContainer";
+import DemocracyDelegateCard from "../../democracy/card";
 import Announcement from "./announcement";
 
 export default function DemocracyAnnouncement() {
@@ -19,8 +21,16 @@ export default function DemocracyAnnouncement() {
       });
   }, [realAddress]);
 
+  const addressAvatarMap = new Map([[data?.address, data?.manifesto?.image]]);
+
   if (data) {
-    return <Delegates delegates={[data]} />;
+    return (
+      <AvatarContextProvider addressAvatarMap={addressAvatarMap}>
+        <MemberCardListContainer>
+          <DemocracyDelegateCard delegate={data} showDelegateButton={false} />
+        </MemberCardListContainer>
+      </AvatarContextProvider>
+    );
   }
 
   return <Announcement />;
