3bbe9690f6e5ab55515dcc76a73577d33d8f8831
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-07 21:56:52 +0800

    Fix less/more button attribute

--- packages/next-common/components/detail/common/PostContent.js
+++ packages/next-common/components/detail/common/PostContent.js
@@ -62,7 +62,7 @@ export default function PostContent({ post = {} }) {
           )}
         >
           <SecondaryButton
-            small
+            size="small"
             onClick={() => {
               setPostContentCollapsed(!postContentCollapsed);
             }}
