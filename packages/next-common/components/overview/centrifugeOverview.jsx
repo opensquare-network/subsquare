4b2c074d4849e7299b954dc1824f64d07497c9c8
chaojun <chaojun@opensquare.network>
2024-04-08 18:10:23 +0800

    Clean unused code, #4081

--- packages/next-common/components/overview/centrifugeOverview.jsx
+++ packages/next-common/components/overview/centrifugeOverview.jsx
@@ -1,8 +1,6 @@
 import AccountInfo from "./accountInfo";
 import RecentProposals from "./recentProposals";
 import { useChainSettings } from "next-common/context/chain";
-import WithPallet from "next-common/components/common/withPallet";
-import FellowshipSalaryOverview from "next-common/components/overview/fellowship/salary/overview";
 import CentrifugeStats from "./centrifugeStats";
 
 export default function CentrifugeOverview() {
@@ -12,18 +10,9 @@ export default function CentrifugeOverview() {
     <div className="space-y-6">
       <AccountInfo hideManageAccountLink={showAccountManagementTab === false} />
 
-      <WithPallet pallet="treasury">
-        <CentrifugeStats />
-      </WithPallet>
+      <CentrifugeStats />
 
-      <WithPallet pallet="fellowshipSalary">
-        <FellowshipSalaryOverview />
-      </WithPallet>
-
-      <div>
-        <RecentProposals />
-        {/* news */}
-      </div>
+      <RecentProposals />
     </div>
   );
 }
