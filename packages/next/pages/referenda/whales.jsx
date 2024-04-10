60af70df2b6505a183ad40a471c784bbdd886d94
2nthony <ant.hu@outlook.com>
2024-04-10 14:43:20 +0800

    fix

--- packages/next/pages/referenda/whales.jsx
+++ packages/next/pages/referenda/whales.jsx
@@ -1,12 +1,35 @@
 import ReferendaLayout from "next-common/components/layout/referendaLayout";
+import nextApi from "next-common/services/nextApi";
 import { withReferendaCommonProps } from "next-common/services/serverSide/referenda/common";
+import {
+  gov2ReferendaHistoryWhalesApi,
+  gov2ReferendaWhalesApi,
+} from "next-common/services/url";
 
-export default function ReferendaWhalesPage({ gov2ReferendaSummary }) {
+export default function ReferendaWhalesPage({ title, gov2ReferendaSummary }) {
+  const seoInfo = { title, desc: title };
+
   return (
-    <ReferendaLayout summaryData={gov2ReferendaSummary}>
+    <ReferendaLayout
+      title={title}
+      seoInfo={seoInfo}
+      summaryData={gov2ReferendaSummary}
+    >
       <div>whales</div>
     </ReferendaLayout>
   );
 }
 
-export const getServerSideProps = withReferendaCommonProps();
+export const getServerSideProps = withReferendaCommonProps(async () => {
+  const [{ result: whales }, { result: historyWhales }] = await Promise.all([
+    nextApi.fetch(gov2ReferendaWhalesApi),
+    nextApi.fetch(gov2ReferendaHistoryWhalesApi),
+  ]);
+
+  return {
+    props: {
+      whales,
+      historyWhales,
+    },
+  };
+});
