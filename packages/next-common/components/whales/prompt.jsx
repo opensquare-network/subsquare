2c4dfe2eaa70db07420cd464866d7eac6d3d64ef
2nthony <ant.hu@outlook.com>
2024-04-10 15:35:48 +0800

    fix prompt

--- packages/next-common/components/whales/prompt.jsx
+++ packages/next-common/components/whales/prompt.jsx
@@ -1,11 +1,21 @@
 import { SecondaryCard } from "../styled/containers/secondaryCard";
+import { MenuWhale } from "@osn/icons/subsquare";
+import { cn } from "next-common/utils";
 
 export default function WhalesPrompt() {
   return (
-    <SecondaryCard>
-      <ul className="list-disc text14Medium text-textPrimary">
+    <SecondaryCard
+      className={cn("flex !p-6 gap-x-4", "max-sm:flex-col max-sm:gap-y-3")}
+    >
+      <div className="w-10 h-10 bg-theme100 rounded-lg flex items-center justify-center">
+        <MenuWhale className="text-theme500" />
+      </div>
+
+      <ul className="list-disc text14Medium text-textPrimary pl-6">
         <li>
-          {"Addresses that have ever voted >= 1M DOT are defined as whales."}
+          Addresses that have ever voted{" "}
+          <span className="text-theme500">{">= 1M DOT"}</span> are defined as
+          whales.
         </li>
         <li>The list is refreshed every 10 minutes.</li>
       </ul>
