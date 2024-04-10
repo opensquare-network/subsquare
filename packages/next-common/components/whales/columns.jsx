184f328644d572cc7956ec3d06cc6e29c4b84e40
2nthony <ant.hu@outlook.com>
2024-04-10 18:03:45 +0800

    fix

--- packages/next-common/components/whales/columns.jsx
+++ packages/next-common/components/whales/columns.jsx
@@ -62,9 +62,9 @@ export const ayeNayCol = {
   cellRender(data) {
     return (
       <div className="inline-flex items-center">
-        <span className="w-12 mr-2">{data.ayeCount}</span>
+        <span className="w-12 mr-2 text-right">{data.ayeCount}</span>
         <SystemVoteAye className="inline w-5 h-5" />
-        <span className="w-12 mr-2">{data.nayCount}</span>
+        <span className="w-12 mr-2 text-right">{data.nayCount}</span>
         <SystemVoteNay className="inline w-5 h-5" />
       </div>
     );
