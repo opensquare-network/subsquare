88a1b85566531b5bd4a52e957e83862b5ba71a4b
2nthony <ant.hu@outlook.com>
2024-04-11 15:01:51 +0800

    fix caret

--- packages/next-common/components/pagination/caret.js
+++ packages/next-common/components/pagination/caret.js
@@ -24,6 +24,7 @@ export default function PageCaret({
   return (
     <Link
       shallow={shallow}
+      scroll={!shallow}
       href={`${url}?${urlParams}`}
       passHref
       legacyBehavior
