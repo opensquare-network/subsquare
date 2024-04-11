808cf5e43c7628beb5e118f72ca714c6b38cd3b2
2nthony <ant.hu@outlook.com>
2024-04-11 15:13:04 +0800

    extract

--- packages/next/pages/referenda/whales/history.jsx
+++ packages/next/pages/referenda/whales/history.jsx
@@ -1,12 +1,11 @@
 import ReferendaLayout from "next-common/components/layout/referendaLayout";
-import nextApi from "next-common/services/nextApi";
 import { withReferendaCommonProps } from "next-common/services/serverSide/referenda/common";
-import {
-  gov2ReferendaHistoryWhalesApi,
-  gov2ReferendaWhalesApi,
-} from "next-common/services/url";
 import WhalesContainer from "next-common/components/whales/container";
 import WhalesHistoryList from "next-common/components/whales/historyList";
+import {
+  getReferendaWhales,
+  getReferendaWhalesHistory,
+} from "next-common/services/serverSide/referenda/whales";
 
 export default function ReferendaWhalesHistoryPage({
   title,
@@ -31,12 +30,8 @@ export const getServerSideProps = withReferendaCommonProps(async (ctx) => {
   const page = ctx.query.page || 1;
 
   const [{ result: whales }, { result: historyWhales }] = await Promise.all([
-    nextApi.fetch(gov2ReferendaWhalesApi, {
-      pageSize: 1,
-    }),
-    nextApi.fetch(gov2ReferendaHistoryWhalesApi, {
-      page,
-    }),
+    getReferendaWhales(1, 1),
+    getReferendaWhalesHistory(page),
   ]);
 
   return {
--- packages/next/pages/referenda/whales/index.jsx
+++ packages/next/pages/referenda/whales/index.jsx
@@ -1,12 +1,11 @@
 import ReferendaLayout from "next-common/components/layout/referendaLayout";
-import nextApi from "next-common/services/nextApi";
 import { withReferendaCommonProps } from "next-common/services/serverSide/referenda/common";
-import {
-  gov2ReferendaHistoryWhalesApi,
-  gov2ReferendaWhalesApi,
-} from "next-common/services/url";
 import WhalesContainer from "next-common/components/whales/container";
 import WhalesCurrentList from "next-common/components/whales/currentList";
+import {
+  getReferendaWhales,
+  getReferendaWhalesHistory,
+} from "next-common/services/serverSide/referenda/whales";
 
 export default function ReferendaWhalesPage({ title, gov2ReferendaSummary }) {
   const seoInfo = { title, desc: title };
@@ -28,12 +27,8 @@ export const getServerSideProps = withReferendaCommonProps(async (ctx) => {
   const page = ctx.query.page || 1;
 
   const [{ result: whales }, { result: historyWhales }] = await Promise.all([
-    nextApi.fetch(gov2ReferendaWhalesApi, {
-      page,
-    }),
-    nextApi.fetch(gov2ReferendaHistoryWhalesApi, {
-      pageSize: 1,
-    }),
+    getReferendaWhales(page),
+    getReferendaWhalesHistory(1, 1),
   ]);
 
   return {
