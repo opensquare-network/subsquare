aef5bf50ffe3e71d7375fb696addf35ebcb2abc0
2nthony <ant.hu@outlook.com>
2024-04-11 18:45:48 +0800

    fix participation

--- packages/next-common/components/whales/columns/participationCol.jsx
+++ packages/next-common/components/whales/columns/participationCol.jsx
@@ -1,15 +1,25 @@
+import { usePageProps } from "next-common/context/page";
 import { toPercentage } from "next-common/utils";
 import Tooltip from "../../tooltip";
+
+function Participation({ data }) {
+  const { gov2ReferendaSummary } = usePageProps();
+
+  return (
+    <Tooltip
+      content={`Participation/Total: ${data.votesCount}/${gov2ReferendaSummary.total}`}
+      className="hover:underline"
+    >
+      {toPercentage(data.participationRate, 1).toFixed(1)}%
+    </Tooltip>
+  );
+}
 
 export const participationCol = {
   name: "Participation",
   width: 120,
   className: "text-right",
   cellRender(data) {
-    return (
-      <Tooltip content={"Participation/Total: "} className="hover:underline">
-        {toPercentage(data.participationRate, 1).toFixed(1)}%
-      </Tooltip>
-    );
+    return <Participation data={data} />;
   },
 };
