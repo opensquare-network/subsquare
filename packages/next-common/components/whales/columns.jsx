0cf8ddb66d214a8b34f4dd83c77247612fec9174
2nthony <ant.hu@outlook.com>
2024-04-11 18:32:15 +0800

    tracks

--- packages/next-common/components/whales/columns.jsx
+++ packages/next-common/components/whales/columns.jsx
@@ -8,7 +8,8 @@ import { cn } from "next-common/utils";
 import Tooltip from "../tooltip";
 import { Conviction } from "next-common/utils/referendumCommon";
 import BigNumber from "bignumber.js";
-import { map, max } from "lodash-es";
+import { filter, map, max } from "lodash-es";
+import Track from "../referenda/track/trackTag";
 
 const LOCK = Math.max(...Object.values(Conviction));
 
@@ -72,8 +73,39 @@ export const tracksCol = {
   name: "Tracks",
   width: 120,
   className: "text-right",
-  cellRender() {
-    return 1;
+  cellRender(data) {
+    const { symbol, decimals } = getChainSettings(CHAIN);
+
+    const delegations = filter(
+      data.tracksDelegations || [],
+      (d) => Number(d.delegationVotes) > 0,
+    );
+
+    return (
+      <Tooltip
+        content={
+          !!delegations.length && (
+            <ul className="text12Medium">
+              {delegations.map((d) => (
+                <li key={d.trackId}>
+                  <Track
+                    id={d.trackId}
+                    className="bg-transparent p-0 text-inherit"
+                  />{" "}
+                  <ValueDisplay
+                    className="[&_.value-display-symbol]:text-inherit"
+                    value={toPrecision(d.delegationVotes, decimals)}
+                    symbol={symbol}
+                  />
+                </li>
+              ))}
+            </ul>
+          )
+        }
+      >
+        <span>{delegations.length}</span>
+      </Tooltip>
+    );
   },
 };
 
