584a1ca14a6dee3e2999f47452fbac86a11a12c5
2nthony <ant.hu@outlook.com>
2024-04-12 19:23:16 +0800

    fix use nested

--- packages/next-common/hooks/referenda/useDecentralizedVoicesVotes.js
+++ packages/next-common/hooks/referenda/useDecentralizedVoicesVotes.js
@@ -1,14 +1,19 @@
-import { find } from "lodash-es";
+import { find, flatten } from "lodash-es";
 import { useChain } from "next-common/context/chain";
 import { usePost } from "next-common/context/post";
 import useReferendumVotingFinishHeight from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
-import { allVotesSelector } from "next-common/store/reducers/referenda/votes/selectors";
+import { allNestedVotesSelector } from "next-common/store/reducers/referenda/votes/selectors";
 import getDvAddresses from "next-common/utils/dv";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 
 export function useDecentralizedVoicesVotes() {
-  const allVotes = useSelector(allVotesSelector);
+  const { allAye, allNay, allAbstain } = useSelector(allNestedVotesSelector);
+  const allNestedVotes = useMemo(
+    () => flatten([allAye, allNay, allAbstain]),
+    [allAye, allNay, allAbstain],
+  );
+
   const chain = useChain();
   const post = usePost();
   const finishHeight = useReferendumVotingFinishHeight();
@@ -19,14 +24,16 @@ export function useDecentralizedVoicesVotes() {
     const addresses = getDvAddresses(chain, post.track, finishHeight);
 
     const dvVotes = addresses.map((address) => {
+      const vote = find(allNestedVotes, { account: address });
+
       return {
         account: address,
-        ...find(allVotes, { account: address }),
+        ...vote,
       };
     });
 
     setVotes(dvVotes);
-  }, [allVotes, chain, post.track]);
+  }, [allNestedVotes, chain, post.track]);
 
   return votes;
 }
