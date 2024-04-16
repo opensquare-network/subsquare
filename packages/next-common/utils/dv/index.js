5b73214ce78ceab31ee57faa3fd682c23fbe51aa
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-16 10:27:47 +0800

    Decentralized voices on referendum detail page, #4072 (#4101)
    * Add function to get decentralized voices addresses, #4072 (#4098)
    
    * Improve function name, #4072
    
    * feat: Add decentralized voices status to votes bubble, #4072 (#4099)
    
    * feat: Add decentrilized voices to votes bubble。note:static page (#4072)
    
    * fix: Style update for sliding bars
    
    * feat: Modify the styles according to the UI design.
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>
    
    * refactor: split components (#4104)
    
    * feat: dv data, #4072 (#4106)
    
    * dv votes hook
    
    * update
    
    * fix use nested
    
    * update
    
    * update whether to show
    
    * fix value display
    
    * dv hook
    
    * dv bar legend
    
    * fix type
    
    * dv info
    
    * fix dv should filter totalVotes
    
    * refactor
    
    * bn to percentage
    
    * update
    
    * refactor
    
    * refactor
    
    * refactor
    
    * link to votes page
    
    * change no voted to `-`
    
    * fix flicker
    
    ---------
    
    Co-authored-by: xiaokang <15184454658@sina.cn>
    Co-authored-by: 2nthony <ant.hu@outlook.com>

---
+++ packages/next-common/utils/dv/index.js
@@ -0,0 +1,26 @@
+import chainDvDelegates from "next-common/utils/dv/delegates";
+import { isNil } from "lodash-es";
+
+export default function getDvAddresses(chain, trackId, voteFinishedHeight) {
+  if (!Object.keys(chainDvDelegates).includes(chain)) {
+    return [];
+  }
+
+  const candidates = chainDvDelegates[chain];
+  return candidates
+    .filter((candidate) => {
+      const { slots = [] } = candidate;
+      return slots.some(({ start, end, trackIds = [] }) => {
+        if (!trackIds.includes(trackId)) {
+          return false;
+        }
+
+        if (isNil(voteFinishedHeight) && !isNil(start) && !isNil(end)) {
+          return false;
+        }
+
+        return !(voteFinishedHeight < start || voteFinishedHeight > end);
+      });
+    })
+    .map((candidate) => candidate.address);
+}
