110a3f1ce1a4a3834b5500ae8779f6ee19bd3ef1
2nthony <ant.hu@outlook.com>
2024-04-17 15:53:50 +0800

    refactor: trackTag, remove redundant code

--- packages/next-common/components/referenda/track/trackTag.js
+++ packages/next-common/components/referenda/track/trackTag.js
@@ -1,23 +1,27 @@
 import { usePageProps } from "next-common/context/page";
 import { startCase } from "lodash-es";
-import tw from "tailwind-styled-components";
-
-const Background = tw.span`
-  bg-neutral200
-  py-0.5 px-2
-  rounded-[10px]
-  text-textSecondary text12Medium
-`;
+import { cn } from "next-common/utils";
 
 export default function Track({ id, className = "" }) {
   const { tracks } = usePageProps();
   const track = tracks.find((track) => track.id === id);
+
+  let content;
   if (!track) {
-    return <Background className={className}>{`[${id}]`}</Background>;
+    content = `[${id}]`;
+  } else {
+    content = `[${id}] ${startCase(track.name)}`;
   }
 
-  const trackName = startCase(track.name);
   return (
-    <Background className={className}>{`[${id}] ${trackName}`}</Background>
+    <span
+      className={cn(
+        "bg-neutral200 py-0.5 px-2 rounded-[10px]",
+        "text-textSecondary text12Medium",
+        className,
+      )}
+    >
+      {content}
+    </span>
   );
 }
