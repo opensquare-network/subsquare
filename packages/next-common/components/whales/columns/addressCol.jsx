b694ecd5e54d7736eb4de8da786744daec7dcf98
2nthony <ant.hu@outlook.com>
2024-04-17 11:10:52 +0800

    refactor

--- packages/next-common/components/whales/columns/addressCol.jsx
+++ packages/next-common/components/whales/columns/addressCol.jsx
@@ -1,51 +1,11 @@
-import { SystemVoice } from "@osn/icons/subsquare";
-import { includes } from "lodash-es";
-import ExternalLink from "next-common/components/externalLink";
-import Tooltip from "next-common/components/tooltip";
-import AddressUser from "next-common/components/user/addressUser";
-import { useChain } from "next-common/context/chain";
-import getDvAddresses from "next-common/utils/dv";
-
-const WIKI_PAGE = "https://wiki.polkadot.network/docs/decentralized-voices";
-
-function Address({ data }) {
-  const chain = useChain();
-  const dvAddress = getDvAddresses(chain);
-  const isDVAddress = includes(dvAddress, data.address);
-
-  let maxWidth = 160;
-  if (isDVAddress) {
-    // 160 - icon(20) - gap(8)
-    maxWidth = 132;
-  }
-
-  return (
-    <div className="flex items-center gap-x-2">
-      <AddressUser maxWidth={maxWidth} linkToVotesPage add={data.address} />
-      {isDVAddress && (
-        <Tooltip
-          content={
-            <ExternalLink
-              className="text-textPrimaryContrast"
-              href={WIKI_PAGE}
-              externalIcon={false}
-            >
-              <span className="underline">Decentralized Voices</span>
-              <span> ↗</span>
-            </ExternalLink>
-          }
-        >
-          <SystemVoice className="w-5 h-5" />
-        </Tooltip>
-      )}
-    </div>
-  );
-}
+import AddressUserWithDVTag from "next-common/components/user/addressUserWithDV";
 
 export const addressCol = {
   name: "Address",
   className: "min-w-[160px]",
   cellRender(data) {
-    return <Address data={data} />;
+    return (
+      <AddressUserWithDVTag add={data.address} maxWidth={160} linkToVotesPage />
+    );
   },
 };
