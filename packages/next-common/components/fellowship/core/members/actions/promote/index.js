339e05fca66a40c78d2c007e3160a8a407710799
chaojun <chaojun@opensquare.network>
2024-04-18 15:04:19 +0800

    update

--- packages/next-common/components/fellowship/core/members/actions/promote/index.js
+++ packages/next-common/components/fellowship/core/members/actions/promote/index.js
@@ -1,17 +1,24 @@
 import { useSelector } from "react-redux";
 import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
 import { usePageProps } from "next-common/context/page";
+import { fellowshipCoreMembersSelector } from "next-common/store/reducers/fellowship/core";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
 
 export default function Promote({ member }) {
+  const address = useRealAddress();
   const { rank, status: { lastPromotion } = {} } = member;
+  const members = useSelector(fellowshipCoreMembersSelector);
+  const me = members.find((m) => m.address === address);
 
   const latestHeight = useSelector(chainOrScanHeightSelector);
   const { fellowshipParams } = usePageProps();
   const index = rank > 0 ? rank - 1 : 0;
   const promotionPeriod = fellowshipParams.minPromotionPeriod[index];
 
+  const higherRank = me && me.rank > rank;
+
   const gone = latestHeight - lastPromotion;
-  const canPromote = gone >= promotionPeriod;
+  const canPromote = gone >= promotionPeriod && higherRank;
 
   if (!canPromote) {
     return <span className="text14Medium text-textDisabled">Promote↗</span>;
