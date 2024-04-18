cc1db9b5f56019af30ae589214e4b7d8a2724830
chaojun <chaojun@opensquare.network>
2024-04-18 15:40:51 +0800

    Add disable tooltip to promote button, #4128

--- packages/next-common/components/fellowship/core/members/actions/promote/index.js
+++ packages/next-common/components/fellowship/core/members/actions/promote/index.js
@@ -3,10 +3,12 @@ import chainOrScanHeightSelector from "next-common/store/reducers/selectors/heig
 import { usePageProps } from "next-common/context/page";
 import { fellowshipCoreMembersSelector } from "next-common/store/reducers/fellowship/core";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import Tooltip from "next-common/components/tooltip";
 
 export default function Promote({ member }) {
   const address = useRealAddress();
   const { rank, status: { lastPromotion } = {} } = member;
+
   const members = useSelector(fellowshipCoreMembersSelector);
   const me = members.find((m) => m.address === address);
 
@@ -18,10 +20,24 @@ export default function Promote({ member }) {
   const higherRank = me && me.rank > rank;
 
   const gone = latestHeight - lastPromotion;
-  const canPromote = gone >= promotionPeriod && higherRank;
+  const promotionPeriodComplete = gone >= promotionPeriod;
+
+  const canPromote = promotionPeriodComplete && higherRank;
 
   if (!canPromote) {
-    return <span className="text14Medium text-textDisabled">Promote↗</span>;
+    let tipContent = "";
+
+    if (!higherRank) {
+      tipContent = `Only available to the members with rank > ${rank}`;
+    } else if (!promotionPeriodComplete) {
+      tipContent = `Available after ${promotionPeriod - gone} blocks`;
+    }
+
+    return (
+      <Tooltip content={tipContent}>
+        <span className="text14Medium text-textDisabled">Promote↗</span>
+      </Tooltip>
+    );
   }
 
   return (
