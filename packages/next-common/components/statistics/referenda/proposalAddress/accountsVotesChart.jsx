fd23da123ec8bd3ddbd816781fe5a73ccfdf1008
2nthony <ant.hu@outlook.com>
2024-04-19 15:04:22 +0800

    account votes

--- packages/next-common/components/statistics/referenda/proposalAddress/accountsVotesChart.jsx
+++ packages/next-common/components/statistics/referenda/proposalAddress/accountsVotesChart.jsx
@@ -1,7 +1,6 @@
 import { Bar } from "react-chartjs-2";
-import React from "react";
 import { useTheme } from "styled-components";
-import { cn, formatNum } from "next-common/utils";
+import { abbreviateBigNumber, cn } from "next-common/utils";
 export default function AccountsVotesChart({ data, className, symbol }) {
   const theme = useTheme();
   const options = {
@@ -11,6 +10,7 @@ export default function AccountsVotesChart({ data, className, symbol }) {
       duration: 0,
     },
     interaction: {
+      intersect: false,
       mode: "index",
     },
     plugins: {
@@ -24,8 +24,11 @@ export default function AccountsVotesChart({ data, className, symbol }) {
           title: () => "",
           label(item) {
             const name = item.dataset.label;
-            const count = formatNum(item.dataset.data[item.dataIndex]);
-            return `${name}: ${count} ${(symbol && symbol) || ""}`;
+            const votes = item.dataset.data;
+            const values = votes[item.dataIndex];
+            return `${name}: ${abbreviateBigNumber(values)} ${
+              (symbol && symbol) || ""
+            }`;
           },
         },
       },
@@ -49,7 +52,7 @@ export default function AccountsVotesChart({ data, className, symbol }) {
         },
       },
       y: {
-        beginAtZero: true,
+        // beginAtZero: true,
         stacked: true,
         grid: {
           display: false,
@@ -59,12 +62,14 @@ export default function AccountsVotesChart({ data, className, symbol }) {
           color: theme.neutral300,
         },
         ticks: {
-          stepSize: 500,
+          // stepSize: 500,
           color: theme.textTertiary,
         },
       },
     },
   };
+
+  // console.log(data);
 
   return (
     <div className={cn("h-[184px]", className)}>
--- packages/next-common/components/statistics/referenda/proposalAddress/accountsVotesContent.jsx
+++ packages/next-common/components/statistics/referenda/proposalAddress/accountsVotesContent.jsx
@@ -1,31 +1,36 @@
 import { StatisticsItemDiv } from "./style";
 import AccountsVotesChart from "./accountsVotesChart";
-import React from "react";
 import { useTheme } from "styled-components";
+import { useRangedVotesData } from "./useRangedVotesData";
+import { useChainSettings } from "next-common/context/chain";
+
 export default function AccountsVotesContent() {
   const theme = useTheme();
+  const { voteSymbol, symbol } = useChainSettings();
+  const { rangedLabels, ayes, nays, abstains } = useRangedVotesData();
+
   const categoryPercentage = 1;
   const barPercentage = 0.1;
   const dataVotes = {
-    labels: ["0-10K", "10-50K", "50K-1M", "1M-5M", ">5M"],
+    labels: rangedLabels,
     datasets: [
       {
         label: "Aye",
-        data: [1000, 500, 60, 520, 150],
+        data: ayes,
         backgroundColor: theme.green300,
         categoryPercentage,
         barPercentage,
       },
       {
         label: "Nay",
-        data: [500, 400, 40, 200, 100],
+        data: nays,
         backgroundColor: theme.red300,
         categoryPercentage,
         barPercentage,
       },
       {
         label: "Abstain",
-        data: [100, 100, 20, 100, 50],
+        data: abstains,
         backgroundColor: theme.neutral400,
         categoryPercentage,
         barPercentage,
@@ -38,7 +43,7 @@ export default function AccountsVotesContent() {
         <span className="text14Bold text-textPrimary">Accounts</span>
         <span className="text14Medium text-textTertiary">Votes</span>
       </div>
-      <AccountsVotesChart data={dataVotes} />
+      <AccountsVotesChart data={dataVotes} symbol={voteSymbol || symbol} />
     </StatisticsItemDiv>
   );
 }
