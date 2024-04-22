d4b4f21a0bc1b75e97fd60d9cb8ebb06cdb41306
2nthony <ant.hu@outlook.com>
2024-04-22 14:54:26 +0800

    fix current cycle card

--- packages/next-common/components/fellowship/salary/cycles/current/index.js
+++ packages/next-common/components/fellowship/salary/cycles/current/index.js
@@ -3,7 +3,6 @@ import FellowshipSalaryStats from "next-common/components/overview/fellowship/sa
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import FellowshipSalaryStatsDetailLink from "next-common/components/overview/fellowship/salary/detailLink";
 import FellowshipSalaryRegister from "next-common/components/fellowship/salary/actions/register";
-import Divider from "next-common/components/styled/layout/divider";
 
 export default function FellowshipSalaryActiveCycle() {
   return (
@@ -12,10 +11,14 @@ export default function FellowshipSalaryActiveCycle() {
 
       <SecondaryCard>
         <FellowshipSalaryStats />
-        <Divider className="mt-4" />
-        <div className="mt-4 flex items-center justify-end gap-4">
-          <FellowshipSalaryStatsDetailLink />
-          <FellowshipSalaryRegister />
+
+        <hr className="my-4" />
+
+        <div className="space-y-2">
+          <div className="flex items-center justify-end gap-4">
+            <FellowshipSalaryStatsDetailLink />
+            <FellowshipSalaryRegister />
+          </div>
         </div>
       </SecondaryCard>
     </>
