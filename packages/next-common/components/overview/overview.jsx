eec60761b103e248a843cd27ae4a8de325098113
xiaokang <15184454658@sina.cn>
2024-04-24 22:47:02 +0800

    Add a induction panel on polkadot collectives home page, #4113 (#4126)
    * feat: completed induction and registration button only UI
    
    * fix: collectives show button
    
    * feat: membersInduction completed only UI
    
    * listlayout remove `prop.button`
    
    * feat: overview member induction (#4151)
    
    * move file
    
    * induct, button props
    
    * members induction
    
    * hide panel if not logged-in
    
    * with pallet
    
    * clean redundant code
    
    * fix: should move the tooltip into induct button
    
    * fix responsive
    
    * Improve text, #4113
    
    * use can induct member
    
    * refactor
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>
    
    ---------
    
    Co-authored-by: 2nthony <ant.hu@outlook.com>
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

--- packages/next-common/components/overview/overview.jsx
+++ packages/next-common/components/overview/overview.jsx
@@ -4,13 +4,17 @@ import { useChainSettings } from "next-common/context/chain";
 import TreasuryStats from "./treasuryStats";
 import WithPallet from "next-common/components/common/withPallet";
 import FellowshipSalaryOverview from "next-common/components/overview/fellowship/salary/overview";
+import MembersInduction from "./fellowship/membersInduction";
 
 export default function Overview() {
   const { showAccountManagementTab } = useChainSettings();
-
   return (
     <div className="space-y-6">
       <AccountInfo hideManageAccountLink={showAccountManagementTab === false} />
+
+      <WithPallet pallet="fellowshipCore">
+        <MembersInduction className="mt-4" />
+      </WithPallet>
 
       <WithPallet pallet="treasury">
         <TreasuryStats />
