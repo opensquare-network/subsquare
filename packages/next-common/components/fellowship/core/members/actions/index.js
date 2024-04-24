2d810bdb36944f50a5b8150dd24946b966569cb2
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-24 22:40:43 +0800

    Add todo, #4155

--- packages/next-common/components/fellowship/core/members/actions/index.js
+++ packages/next-common/components/fellowship/core/members/actions/index.js
@@ -1,6 +1,7 @@
 import Bump from "next-common/components/fellowship/core/members/actions/bump";
 
 export default function Actions({ member }) {
+  // todo: add activeness setting menu
   return (
     <div className="leading-5 flex justify-between mt-6">
       <span className="text14Medium text-textPrimary">Actions</span>
