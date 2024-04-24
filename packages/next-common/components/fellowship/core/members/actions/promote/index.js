8d8755367d89abce55c4858f505f4f96d3dd4964
chaojun <chaojun@opensquare.network>
2024-04-24 17:25:09 +0800

    Add promote popup, #4128

---
+++ packages/next-common/components/fellowship/core/members/actions/promote/index.js
@@ -0,0 +1,66 @@
+import { useSelector } from "react-redux";
+import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
+import { usePageProps } from "next-common/context/page";
+import { fellowshipCoreMembersSelector } from "next-common/store/reducers/fellowship/core";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import Tooltip from "next-common/components/tooltip";
+import { useState } from "react";
+import PromoteFellowshipMemberPopup from "next-common/components/summary/newProposalQuickStart/PromoteFellowshipMemberPopup";
+
+export default function Promote({ member }) {
+  const [showPromotePopup, setShowPromotePopup] = useState(false);
+  const address = useRealAddress();
+  const { rank, status: { lastPromotion } = {} } = member;
+
+  const members = useSelector(fellowshipCoreMembersSelector);
+
+  const latestHeight = useSelector(chainOrScanHeightSelector);
+  const { fellowshipParams } = usePageProps();
+
+  const isMaxRank = rank >= 6;
+
+  if (isMaxRank) {
+    return;
+  }
+
+  const me = members.find((m) => m.address === address);
+  const higherRank = me && me.rank > rank;
+
+  const index = rank > 0 ? rank - 1 : 0;
+  const promotionPeriod = fellowshipParams.minPromotionPeriod[index];
+  const gone = latestHeight - lastPromotion;
+  const promotionPeriodComplete = gone >= promotionPeriod;
+  const canPromote = promotionPeriodComplete && higherRank;
+  if (!canPromote) {
+    let tipContent = "";
+
+    if (!higherRank) {
+      tipContent = `Only available to the members with rank > ${rank}`;
+    } else if (!promotionPeriodComplete) {
+      tipContent = `Available after ${promotionPeriod - gone} blocks`;
+    }
+
+    return (
+      <Tooltip content={tipContent}>
+        <span className="text14Medium text-textDisabled">Promote↗</span>
+      </Tooltip>
+    );
+  }
+
+  return (
+    <>
+      <span
+        className="text14Medium text-theme500 cursor-pointer"
+        onClick={() => setShowPromotePopup(true)}
+      >
+        Promote↗
+      </span>
+      {showPromotePopup && (
+        <PromoteFellowshipMemberPopup
+          member={member}
+          onClose={() => setShowPromotePopup(false)}
+        />
+      )}
+    </>
+  );
+}
---
+++ packages/next-common/components/summary/newProposalQuickStart/PromoteFellowshipMemberPopup/index.js
@@ -0,0 +1,114 @@
+import AddressComboField from "next-common/components/popup/fields/addressComboField";
+import RankField from "next-common/components/popup/fields/rankField";
+import { PopupButtonWrapper } from "next-common/components/popup/wrapper";
+import PopupWithSigner from "next-common/components/popupWithSigner";
+import {
+  useExtensionAccounts,
+  useSignerAccount,
+} from "next-common/components/popupWithSigner/context";
+import SignerWithBalance from "next-common/components/signerPopup/signerWithBalance";
+import { useContextApi } from "next-common/context/api";
+import PrimaryButton from "next-common/lib/button/primary";
+import { incPreImagesTrigger } from "next-common/store/reducers/preImagesSlice";
+import useIsMounted from "next-common/utils/hooks/useIsMounted";
+import { sendTx, wrapWithProxy } from "next-common/utils/sendTx";
+import { useCallback, useState } from "react";
+import { useDispatch } from "react-redux";
+import EnactmentBlocks from "../../newProposalPopup/enactmentBlocks";
+import { newErrorToast } from "next-common/store/reducers/toastSlice";
+import { useRouter } from "next/router";
+
+function PopupContent({ member, onClose }) {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [enactment, setEnactment] = useState();
+  const isMounted = useIsMounted();
+  const api = useContextApi();
+  const signerAccount = useSignerAccount();
+  const extensionAccounts = useExtensionAccounts();
+  const [toRank, setToRank] = useState(member?.rank + 1);
+  const [memberAddress, setMemberAddress] = useState(member?.address);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const doPromote = useCallback(async () => {
+    if (!api || !memberAddress) {
+      return;
+    }
+
+    if (toRank > 6) {
+      dispatch(newErrorToast("Invalid rank"));
+      return;
+    }
+
+    const proposal = api.tx.fellowshipCore.promote(memberAddress, toRank);
+    let tx = api.tx.fellowshipReferenda.submit(
+      { FellowshipOrigins: `PromoteTo${toRank}Dan` },
+      { Inline: proposal.method.toHex() },
+      enactment,
+    );
+    if (signerAccount?.proxyAddress) {
+      tx = wrapWithProxy(api, tx, signerAccount.proxyAddress);
+    }
+
+    setIsLoading(true);
+    try {
+      await sendTx({
+        tx,
+        setLoading: setIsLoading,
+        dispatch,
+        signerAccount,
+        isMounted,
+        section: "fellowshipReferenda",
+        method: "Submitted",
+        onInBlock: (eventData) => {
+          if (!eventData) {
+            return;
+          }
+          const [referendumIndex] = eventData;
+          router.push(`/fellowship/referenda/${referendumIndex}`);
+        },
+        onFinalized: () => dispatch(incPreImagesTrigger()),
+      });
+    } catch (error) {
+      dispatch(newErrorToast(error.message));
+      setIsLoading(false);
+    }
+  }, [
+    api,
+    onClose,
+    toRank,
+    memberAddress,
+    enactment,
+    signerAccount,
+    dispatch,
+    isMounted,
+    router,
+  ]);
+
+  return (
+    <>
+      <SignerWithBalance />
+      <AddressComboField
+        title="Member"
+        extensionAccounts={extensionAccounts}
+        defaultAddress={memberAddress}
+        setAddress={setMemberAddress}
+      />
+      <RankField rank={toRank} setRank={setToRank} />
+      <EnactmentBlocks setEnactment={setEnactment} />
+      <PopupButtonWrapper>
+        <PrimaryButton loading={isLoading} onClick={doPromote}>
+          Confirm
+        </PrimaryButton>
+      </PopupButtonWrapper>
+    </>
+  );
+}
+
+export default function PromoteFellowshipMemberPopup({ member, onClose }) {
+  return (
+    <PopupWithSigner title="Promote Fellowship Member" onClose={onClose} wide>
+      <PopupContent member={member} onClose={onClose} />
+    </PopupWithSigner>
+  );
+}
