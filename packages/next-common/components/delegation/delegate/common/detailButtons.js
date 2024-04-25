483c6414dce632fc535242253d5d577ec1cb1a31
chaojun <chaojun@opensquare.network>
2024-04-25 16:18:23 +0800

    Clean up unused code, #4053

--- packages/next-common/components/delegation/delegate/common/detailButtons.js
+++ packages/next-common/components/delegation/delegate/common/detailButtons.js
@@ -1,32 +1,8 @@
 import SecondaryButton from "next-common/lib/button/secondary";
 import { SystemSubtract } from "@osn/icons/subsquare";
-// import { useState } from "react";
-// import AnnouncementEditPopup from "../AnnouncementEditPopup";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useState } from "react";
 import RevokePopup from "./revokePopup";
-
-// export function EditButton({ address }) {
-//   const [showEdit, setShowEdit] = useState(false);
-//   return (
-//     <>
-//       <SecondaryButton
-//         size="small"
-//         iconLeft={<SystemEdit2 className="w-4 h-4" />}
-//         onClick={() => setShowEdit(true)}
-//       >
-//         Edit
-//       </SecondaryButton>
-//       {showEdit && (
-//         <AnnouncementEditPopup
-//           title="Edit"
-//           onClose={() => setShowEdit(false)}
-//           address={address}
-//         />
-//       )}
-//     </>
-//   );
-// }
 
 export function RevokeButton({ address }) {
   const [showRevotePopup, setShowRevotePopup] = useState(false);
@@ -57,7 +33,6 @@ export default function DetailButtons({ address }) {
   return (
     isMyDelegate && (
       <div className="flex gap-[8px]">
-        {/* <EditButton address={address} /> */}
         <RevokeButton address={address} />
       </div>
     )
