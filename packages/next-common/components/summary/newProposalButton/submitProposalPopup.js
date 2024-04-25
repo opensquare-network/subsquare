291f85baf5fc546a4a9acdcef3c53b1187053278
chaojun <chaojun@opensquare.network>
2024-04-25 18:21:29 +0800

    Add button new treasury proposal, #4112

--- packages/next-common/components/summary/newProposalButton/submitProposalPopup.js
+++ packages/next-common/components/summary/newProposalButton/submitProposalPopup.js
@@ -2,6 +2,29 @@ import { useState } from "react";
 import NewProposalPopup from "../newProposalPopup";
 import { usePageProps } from "next-common/context/page";
 import SubmitProposalPopupCommon from "./common";
+import { ArrowRight } from "@osn/icons/subsquare";
+import { cn } from "../../../utils";
+
+function QuickStartButton({ title, onClick }) {
+  return (
+    <div
+      className={cn(
+        "flex items-center pl-[16px] py-[10px] pr-[10px] gap-[8px]",
+        "cursor-pointer rounded-full border border-neutral400 hover:border-neutral500",
+      )}
+      onClick={onClick}
+    >
+      <span className="text-textPrimary text14Medium">{title}</span>
+      <div className="inline-flex">
+        <ArrowRight
+          className="[&_path]:stroke-textTertiary"
+          width={20}
+          height={20}
+        />
+      </div>
+    </div>
+  );
+}
 
 export default function SubmitProposalPopup({ onClose }) {
   const { period } = usePageProps();
@@ -21,6 +44,13 @@ export default function SubmitProposalPopup({ onClose }) {
           preimageLength={preimageLength}
         />
       }
-    />
+    >
+      <div className="flex flex-col gap-[8px] mt-[24px]">
+        <h6 className="text-textPrimary text14Bold">Quick Start</h6>
+        <div className="flex flex-wrap">
+          <QuickStartButton title="Create a treasury proposal" />
+        </div>
+      </div>
+    </SubmitProposalPopupCommon>
   );
 }
