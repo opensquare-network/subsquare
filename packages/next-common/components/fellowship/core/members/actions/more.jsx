93d15f1d09267b2f3983ffc6105a8ed848ec80c9
2nthony <ant.hu@outlook.com>
2024-04-26 11:37:15 +0800

    add more button

---
+++ packages/next-common/components/fellowship/core/members/actions/more.jsx
@@ -0,0 +1,51 @@
+import { SystemMore } from "@osn/icons/subsquare";
+import SignalIndicator from "next-common/components/icons/signalIndicator";
+import {
+  OptionItem,
+  OptionWrapper,
+} from "next-common/components/internalDropdown/styled";
+import { useRef, useState } from "react";
+import { useClickAway } from "react-use";
+
+function ActivationItem({ member }) {
+  const { isActive } = member.status;
+
+  return (
+    <OptionItem>
+      <div className="flex items-center">
+        <SignalIndicator
+          showTooltip={false}
+          active={!isActive}
+          className="w-6 h-6 mr-2"
+        />
+        {isActive ? "Unactive" : "Active"}
+      </div>
+    </OptionItem>
+  );
+}
+
+export default function More({ member }) {
+  const [show, setShow] = useState(false);
+  const ref = useRef();
+
+  useClickAway(ref, () => {
+    setShow(false);
+  });
+
+  return (
+    <div ref={ref} className="relative">
+      <SystemMore
+        className="w-5 h-5 text-textTertiary cursor-pointer"
+        onClick={() => {
+          setShow(true);
+        }}
+      />
+
+      {show && (
+        <OptionWrapper>
+          <ActivationItem member={member} />
+        </OptionWrapper>
+      )}
+    </div>
+  );
+}
