3ad45b01dadba4521348935c7eaf74fb294764c6
2nthony <ant.hu@outlook.com>
2024-04-26 14:27:17 +0800

    fix menu color

--- packages/next-common/components/fellowship/core/members/actions/more.jsx
+++ packages/next-common/components/fellowship/core/members/actions/more.jsx
@@ -4,6 +4,7 @@ import {
   OptionItem,
   OptionWrapper,
 } from "next-common/components/internalDropdown/styled";
+import { cn } from "next-common/utils";
 import { useRef, useState } from "react";
 import { useClickAway } from "react-use";
 
@@ -35,7 +36,10 @@ export default function More({ member }) {
   return (
     <div ref={ref} className="relative">
       <SystemMore
-        className="w-5 h-5 text-textTertiary cursor-pointer"
+        className={cn(
+          "w-5 h-5 text-textTertiary cursor-pointer",
+          show && "text-textSecondary",
+        )}
         onClick={() => {
           setShow(true);
         }}
