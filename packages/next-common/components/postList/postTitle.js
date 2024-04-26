41646e2badf2af52549000056d57ceebbc3a0ca5
袁康 <15184454658@sina.cn>
2024-04-26 10:29:37 +0800

    feat: warn icon move position

--- packages/next-common/components/postList/postTitle.js
+++ packages/next-common/components/postList/postTitle.js
@@ -3,8 +3,6 @@ import ReasonLink from "../reasonLink";
 import Link from "next/link";
 import { cn, isExternalLink } from "next-common/utils";
 import React from "react";
-import Tooltip from "next-common/components/tooltip";
-import WarningIcon from "next-common/assets/imgs/icons/warning.svg";
 export default function ListPostTitle({
   data = {},
   href,
@@ -17,7 +15,7 @@ export default function ListPostTitle({
     <div
       title={title}
       className={cn(
-        "flex-1 overflow-hidden text-textPrimary text16Medium align-middle",
+        "flex-1 overflow-hidden text-textPrimary text16Medium",
         ellipsis && "flex",
         className,
       )}
@@ -37,14 +35,6 @@ export default function ListPostTitle({
       >
         {title}
       </Link>
-      {data?.isMalicious && (
-        <Tooltip
-          className="align-middle ml-2 mt-[2px]"
-          content="Warning: Malicious proposal!"
-        >
-          <WarningIcon className="align-middle" />
-        </Tooltip>
-      )}
       <ReasonLink text={data.title} hideText={true} />
     </div>
   );
