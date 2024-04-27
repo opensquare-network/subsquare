239d9de50a1a8fcf6d6a495ae5e8cb1e55723e1b
Yongfeng LI <wliyongfeng@gmail.com>
2024-04-27 21:26:17 +0800

    Move escrow page to kintsugi next package, #4164

---
+++ packages/kintsugi-next/pages/escrow/index.js
@@ -0,0 +1,49 @@
+import { withCommonProps } from "next-common/lib";
+import { useChainSettings } from "next-common/context/chain";
+import ListLayout from "next-common/components/layout/ListLayout";
+import OverviewSummary from "next-common/components/summary/overviewSummary";
+import { useUser } from "next-common/context/user";
+import { HeadContent, TitleExtra } from "next-common/components/overview";
+import Escrow from "next-common/components/escrow/index";
+import useAccountUrl from "next-common/hooks/account/useAccountUrl";
+
+export default function EscrowPage() {
+  const chainSettings = useChainSettings();
+  const user = useUser();
+  const url = useAccountUrl();
+
+  const tabs = [
+    {
+      label: "Overview",
+      url: "/",
+    },
+    {
+      label: "Escrow",
+      url: "/escrow",
+      exactMatch: false,
+    },
+  ];
+
+  if (user?.address && chainSettings.showAccountManagementTab !== false) {
+    tabs.push({
+      label: "Account",
+      url,
+    });
+  }
+
+  return (
+    <ListLayout
+      title={chainSettings.name}
+      titleExtra={<TitleExtra />}
+      seoInfo={{ title: "" }}
+      description={chainSettings.description}
+      headContent={<HeadContent />}
+      summary={<OverviewSummary />}
+      tabs={tabs}
+    >
+      <Escrow />
+    </ListLayout>
+  );
+}
+
+export const getServerSideProps = withCommonProps();
