59e6b8fbd6ee32637538d590574dca6a0cd9a7a1
2nthony <ant.hu@outlook.com>
2024-04-30 16:30:22 +0800

    add do web3 login hook

---
+++ packages/next-common/hooks/connect/doWeb3Login.js
@@ -0,0 +1,47 @@
+import { newErrorToast } from "next-common/store/reducers/toastSlice";
+import { useLoginPopup } from "../useLoginPopup";
+import { useDispatch } from "react-redux";
+import { encodeAddressToChain } from "next-common/services/address";
+import {
+  LoginResult,
+  setLoginResult,
+} from "next-common/store/reducers/userSlice";
+import { useConnectedAccountContext } from "next-common/context/connectedAccount";
+import { useAsyncFn } from "react-use";
+import { useChain } from "next-common/context/chain";
+
+export function useDoWeb3Login() {
+  const chain = useChain();
+  const dispatch = useDispatch();
+  const { closeLoginPopup } = useLoginPopup();
+  const { connect: connectAccount } = useConnectedAccountContext();
+
+  const [state, doWeb3Login] = useAsyncFn(
+    async ({ account, wallet }) => {
+      if (!account?.address) {
+        dispatch(newErrorToast("Please select an account"));
+        return;
+      }
+
+      try {
+        const address = encodeAddressToChain(account.address, chain);
+        const accountInfo = {
+          address,
+          wallet,
+        };
+        await connectAccount(accountInfo);
+        dispatch(setLoginResult(LoginResult.Connected));
+
+        closeLoginPopup();
+      } catch (e) {
+        dispatch(newErrorToast(e.message));
+      }
+    },
+    [chain, connectAccount],
+  );
+
+  return {
+    isLoading: state.loading,
+    doWeb3Login,
+  };
+}
