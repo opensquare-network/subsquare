968ac8cad460e24e6caad2c3a6fcaf7ffc324d83
2nthony <ant.hu@outlook.com>
2024-04-30 17:10:58 +0800

    wagmi, remove wallets

--- packages/next-common/context/wagmi/index.jsx
+++ packages/next-common/context/wagmi/index.jsx
@@ -1,13 +1,10 @@
 import { WagmiProvider as Provider } from "wagmi";
 import { http, createConfig } from "wagmi";
 import { mainnet, base } from "wagmi/chains";
-import { safe, walletConnect } from "wagmi/connectors";
-
-const projectId = "subsquare";
 
 const wagmiConfig = createConfig({
   chains: [mainnet, base],
-  connectors: [walletConnect({ projectId }), safe()],
+  connectors: [],
   transports: {
     [mainnet.id]: http(),
     [base.id]: http(),
