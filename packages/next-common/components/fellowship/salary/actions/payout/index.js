451d0ad3472c0bc982090999e3afe95a94ca9566
chaojun <chaojun@opensquare.network>
2024-05-06 18:26:44 +0800

    Add more checks, #4156

--- packages/next-common/components/fellowship/salary/actions/payout/index.js
+++ packages/next-common/components/fellowship/salary/actions/payout/index.js
@@ -7,20 +7,36 @@ import { fellowshipSalaryStatusSelector } from "next-common/store/reducers/fello
 import useFellowshipSalaryPeriods from "next-common/hooks/fellowship/salary/useFellowshipSalaryPeriods";
 import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
 import { isNil } from "lodash-es";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import useFellowshipCollectiveMembers from "next-common/hooks/fellowship/collective/useFellowshipCollectiveMembers";
 
 export default function FellowshipSalaryRegister() {
   const [showPopup, setShowPopup] = useState(false);
+  const address = useRealAddress();
+  const members = useFellowshipCollectiveMembers();
+  const memberAddrs = (members || []).map((item) => item.address);
+  const isCollectiveMember = memberAddrs.includes(address);
 
-  const { cycleStart } = useSelector(fellowshipSalaryStatusSelector);
+  const { cycleStart } = useSelector(fellowshipSalaryStatusSelector) || {};
   const { registrationPeriod } = useFellowshipSalaryPeriods();
   const payoutStart = cycleStart + registrationPeriod || null;
   const latestHeight = useSelector(chainOrScanHeightSelector);
   const isStarted =
     !isNil(latestHeight) && !isNil(payoutStart) && latestHeight >= payoutStart;
 
-  if (!isStarted) {
+  const disabled = !address || !isStarted || !isCollectiveMember;
+  let tooltipText = null;
+  if (!address) {
+    tooltipText = "Connect your address please";
+  } else if (!isCollectiveMember) {
+    tooltipText = "Not a collective member";
+  } else if (!isStarted) {
+    tooltipText = "The payout period is not started";
+  }
+
+  if (disabled) {
     return (
-      <Tooltip content={"The payout period is not started"}>
+      <Tooltip content={tooltipText}>
         <PrimaryButton size="small" disabled>
           Payout
         </PrimaryButton>
