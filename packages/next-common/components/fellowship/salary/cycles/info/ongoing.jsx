ca0aa7c01624ac691bf6e8b199a265fa5d01a2a8
chaojun <chaojun@opensquare.network>
2024-05-07 10:14:09 +0800

    Also display payout button on cycle detail, #4156

--- packages/next-common/components/fellowship/salary/cycles/info/ongoing.jsx
+++ packages/next-common/components/fellowship/salary/cycles/info/ongoing.jsx
@@ -14,6 +14,7 @@ import SalaryStatsTotalPeriodItem from "../summary/totalPeriodItem";
 import SalaryStatsBlockTimeItem from "../summary/blockTimeItem";
 import { DesktopPlaceHolderItem } from "next-common/components/overview/fellowship/salary/stats";
 import SalaryStatsTimeRemainItem from "../summary/timeRemainItem";
+import FellowshipSalaryPayout from "next-common/components/fellowship/salary/actions/payout";
 
 export default function FellowshipSalaryCycleDetailInfoOngoing({ cycle = {} }) {
   const [navCollapsed] = useNavCollapsed();
@@ -76,6 +77,7 @@ export default function FellowshipSalaryCycleDetailInfoOngoing({ cycle = {} }) {
         <FellowshipSalaryMyStatus />
         <div className="flex items-center justify-end gap-4">
           <FellowshipSalaryRegister />
+          <FellowshipSalaryPayout />
         </div>
       </div>
     </PrimaryCard>
