6cb02163f6155e309a0664495fe93601bb08514b
2nthony <ant.hu@outlook.com>
2024-05-07 16:07:02 +0800

    fix: evm content back to button display

--- packages/next-common/components/login/evm/content.jsx
+++ packages/next-common/components/login/evm/content.jsx
@@ -3,10 +3,12 @@ import { ArrowCircleLeft } from "@osn/icons/subsquare";
 import WalletOption from "next-common/components/wallet/walletOption";
 import { setConnectPopupView } from "next-common/store/reducers/connectPopupSlice";
 import { CONNECT_POPUP_VIEWS } from "next-common/utils/constants";
+import isMixedChain from "next-common/utils/isMixedChain";
 import { useDispatch } from "react-redux";
 
 export default function LoginEVMContent() {
   const dispatch = useDispatch();
+  const showBack = isMixedChain();
 
   return (
     <div className="space-y-6">
@@ -15,20 +17,22 @@ export default function LoginEVMContent() {
         <span className="text-theme500">EVM Address</span>
       </h3>
 
-      <div className="grid grid-cols-2 gap-2 max-sm:grid-cols-1">
-        <WalletOption
-          installed
-          role="button"
-          onClick={() => {
-            dispatch(setConnectPopupView(CONNECT_POPUP_VIEWS.WEB3));
-          }}
-        >
-          <div className="flex items-center">
-            <ArrowCircleLeft className="text-textSecondary" />
-            Back to Substrate
-          </div>
-        </WalletOption>
-      </div>
+      {showBack && (
+        <div className="grid grid-cols-2 gap-2 max-sm:grid-cols-1">
+          <WalletOption
+            installed
+            role="button"
+            onClick={() => {
+              dispatch(setConnectPopupView(CONNECT_POPUP_VIEWS.WEB3));
+            }}
+          >
+            <div className="flex items-center">
+              <ArrowCircleLeft className="text-textSecondary" />
+              Back to Substrate
+            </div>
+          </WalletOption>
+        </div>
+      )}
 
       <LoginEVMForm />
     </div>
