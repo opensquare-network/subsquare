108a998bc947618ed0ec71f08104ae1fba15a2a6
2nthony <ant.hu@outlook.com>
2024-05-07 15:57:23 +0800

    evm add back to substrate

--- packages/next-common/components/login/evm/content.jsx
+++ packages/next-common/components/login/evm/content.jsx
@@ -1,12 +1,34 @@
 import LoginEVMForm from "./form";
+import { ArrowCircleLeft } from "@osn/icons/subsquare";
+import WalletOption from "next-common/components/wallet/walletOption";
+import { setConnectPopupView } from "next-common/store/reducers/connectPopupSlice";
+import { CONNECT_POPUP_VIEWS } from "next-common/utils/constants";
+import { useDispatch } from "react-redux";
 
 export default function LoginEVMContent() {
+  const dispatch = useDispatch();
+
   return (
     <div className="space-y-6">
       <h3 className="text20Bold text-textPrimary">
         <span>{"Connect with "}</span>
         <span className="text-theme500">EVM Address</span>
       </h3>
+
+      <div className="grid grid-cols-2 gap-2 max-sm:grid-cols-1">
+        <WalletOption
+          installed
+          role="button"
+          onClick={() => {
+            dispatch(setConnectPopupView(CONNECT_POPUP_VIEWS.WEB3));
+          }}
+        >
+          <div className="flex items-center">
+            <ArrowCircleLeft className="text-textSecondary" />
+            Back to Substrate
+          </div>
+        </WalletOption>
+      </div>
 
       <LoginEVMForm />
     </div>
