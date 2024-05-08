23cd33c6901c8aa6ce1547dec164d80402aed0dd
2nthony <ant.hu@outlook.com>
2024-05-08 14:25:32 +0800

    evm add metamask coinbasewallet as popular wallet options

--- packages/next-common/hooks/connect/useEVMWalletOptions.js
+++ packages/next-common/hooks/connect/useEVMWalletOptions.js
@@ -1,9 +1,15 @@
-import { filter, find } from "lodash-es";
+import { filter, find, uniqBy } from "lodash-es";
 import { useChainSettings } from "next-common/context/chain";
 import ChainTypes from "next-common/utils/consts/chainTypes";
-import { allWallets } from "next-common/utils/consts/connect";
+import {
+  allWallets,
+  coinbaseWallet,
+  metamask,
+} from "next-common/utils/consts/connect";
 import WalletTypes from "next-common/utils/consts/walletTypes";
 import { useConnect } from "wagmi";
+
+const popularWallets = [metamask, coinbaseWallet];
 
 export function useEVMWalletOptions() {
   const { connectors } = useConnect();
@@ -17,22 +23,28 @@ export function useEVMWalletOptions() {
     return true;
   });
 
-  const options = supportedConnectors.map((connector) => {
-    const found = find(allWallets, (w) => {
-      return (
-        w.title.toLowerCase() === connector.name.toLowerCase() ||
-        w.extensionName.toLowerCase() === connector.name.toLowerCase()
-      );
-    });
-    const logo = found?.logo;
+  const options = uniqBy(
+    [
+      ...supportedConnectors.map((connector) => {
+        const found = find(allWallets, (w) => {
+          return (
+            w.title.toLowerCase() === connector.name.toLowerCase() ||
+            w.extensionName.toLowerCase() === connector.name.toLowerCase()
+          );
+        });
+        const logo = found?.logo;
 
-    return {
-      title: connector.name,
-      extensionName: connector.name.toLowerCase(),
-      logo,
-      connector,
-    };
-  });
+        return {
+          title: connector.name,
+          extensionName: connector.name.toLowerCase(),
+          logo,
+          connector,
+        };
+      }),
+      ...popularWallets,
+    ],
+    "extensionName",
+  );
 
   return options;
 }
