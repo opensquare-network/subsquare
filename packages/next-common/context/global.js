704aa922f232d2cf5a62bfa5d00cfa4c78d474ae
2nthony <ant.hu@outlook.com>
2024-05-10 14:51:30 +0800

    clean

--- packages/next-common/context/global.js
+++ packages/next-common/context/global.js
@@ -39,7 +39,6 @@ export default function GlobalProvider({
                       <WagmiProvider>
                         <ReactQueryClientProvider>
                           {children}
-                          {/**/}
                         </ReactQueryClientProvider>
                       </WagmiProvider>
                     </SignetContextProvider>
