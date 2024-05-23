70cf1f13a9374cedda341a231c9cd76f4f5b0714
2nthony <ant.hu@outlook.com>
2024-05-23 14:54:44 +0800

    fix: conflicts chart config

--- packages/next-common/components/charts/globalConfig.js
+++ packages/next-common/components/charts/globalConfig.js
@@ -1,10 +1,13 @@
 import { Tooltip } from "chart.js";
 import Chart from "chart.js/auto";
 import Annotation from "chartjs-plugin-annotation";
+import gradient from "chartjs-plugin-gradient";
+import "./adapterDayjs";
 
 Chart.defaults.font.family =
   "Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif";
 
+Chart.register(gradient);
 Chart.register(Annotation);
 
 Tooltip.positioners.followMouse = function (elements, eventPosition) {
