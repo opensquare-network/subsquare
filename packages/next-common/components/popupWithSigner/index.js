3595aea42b184322612b48a719e351aa9ecff413
2nthony <ant.hu@outlook.com>
2024-05-30 15:55:48 +0800

    fix signer login popup

--- packages/next-common/components/popupWithSigner/index.js
+++ packages/next-common/components/popupWithSigner/index.js
@@ -1,14 +1,13 @@
-import React from "react";
 import { useUser } from "next-common/context/user";
 import WalletTypes from "next-common/utils/consts/walletTypes";
 import MaybePolkadotSigner from "./maybePolkadotSigner";
 import MaybeMetamaskSigner from "./maybeMetamaskSigner";
 import MaybeSignetSigner from "./maybeSignetSigner";
-import SelectWalletPopup from "../selectWallet";
 import { useConnectedAccountContext } from "next-common/context/connectedAccount";
 import CanBeAnyWalletSigner from "./canBeAnyWalletSigner";
 import { isKeyRegisteredUser } from "next-common/utils";
 import { PopupParamsProvider, usePopupParams } from "./context";
+import LoginPopup from "../login/popup";
 
 function PopupImpl({ children }) {
   const user = useUser();
@@ -17,7 +16,7 @@ function PopupImpl({ children }) {
   const { onClose } = usePopupParams();
 
   if (!user) {
-    return <SelectWalletPopup onClose={onClose} />;
+    return <LoginPopup onClose={onClose} />;
   }
 
   if (!isKeyRegisteredUser(user) && !connectedAccount) {
--- packages/next-common/components/popupWithSigner/maybeSignerConnected.js
+++ packages/next-common/components/popupWithSigner/maybeSignerConnected.js
@@ -1,20 +1,6 @@
-import React from "react";
-import { useUser } from "../../context/user";
-import { isSameAddress } from "../../utils";
-import SelectWalletPopup from "../selectWallet";
-import { SignerContextProvider, usePopupParams } from "./context";
+import { SignerContextProvider } from "./context";
 
 export default function MaybeSignerConnected({ children, extensionAccounts }) {
-  const user = useUser();
-  const { onClose } = usePopupParams();
-
-  if (
-    !user?.address ||
-    !extensionAccounts?.find((acc) => isSameAddress(acc.address, user?.address))
-  ) {
-    return <SelectWalletPopup onClose={onClose} />;
-  }
-
   return (
     <SignerContextProvider extensionAccounts={extensionAccounts}>
       {children}
