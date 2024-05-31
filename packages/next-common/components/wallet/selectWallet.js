7e6fe15afe6409b9f22d94eb60b19f94383b0102
2nthony <ant.hu@outlook.com>
2024-05-31 15:00:32 +0800

    select wallet add evm support

--- packages/next-common/components/wallet/selectWallet.js
+++ packages/next-common/components/wallet/selectWallet.js
@@ -3,17 +3,55 @@ import { NovaWallet } from "./novaWallet";
 import WalletTypes from "next-common/utils/consts/walletTypes";
 import { noop } from "lodash-es";
 import { SignetWallet } from "./signetWallet";
+import WalletOption from "./walletOption";
 
 export default function SelectWallet({
   wallets,
   selectedWallet,
   setSelectWallet,
   onSelect = noop,
+  beforeWallets,
   extraWallets,
 }) {
+  function handleClick(wallet) {
+    onSelect(wallet);
+  }
+
   return (
     <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-2">
+      {beforeWallets}
+
       {(wallets || []).map((wallet, index) => {
+        // evm
+        if (wallet.connector) {
+          let icon;
+          if (wallet.logo) {
+            icon = <wallet.logo className="w-6 h-6" />;
+          } else if (wallet.connector?.icon) {
+            icon = <img src={wallet.connector?.icon} className="w-6 h-6" />;
+          }
+
+          const installed = !!wallet.connector;
+
+          return (
+            <WalletOption
+              key={wallet.extensionName}
+              installed={installed}
+              logo={icon}
+              title={wallet.title}
+              selected={selectedWallet?.connector?.id === wallet.connector?.id}
+              onClick={() => {
+                if (!installed) {
+                  return;
+                }
+
+                handleClick(wallet);
+              }}
+            />
+          );
+        }
+
+        // substrate
         const selected = wallet.extensionName === selectedWallet;
 
         if (wallet.extensionName === WalletTypes.NOVA) {
@@ -23,7 +61,7 @@ export default function SelectWallet({
               wallet={wallet}
               onClick={async () => {
                 setSelectWallet(wallet?.extensionName);
-                onSelect();
+                handleClick(wallet);
               }}
               selected={selected}
             />
@@ -37,7 +75,7 @@ export default function SelectWallet({
               wallet={wallet}
               onClick={async () => {
                 setSelectWallet(wallet?.extensionName);
-                onSelect();
+                handleClick(wallet);
               }}
               selected={selected}
             />
@@ -50,7 +88,7 @@ export default function SelectWallet({
             wallet={wallet}
             onClick={async () => {
               setSelectWallet(wallet?.extensionName);
-              onSelect();
+              handleClick(wallet);
             }}
             selected={selected}
           />
