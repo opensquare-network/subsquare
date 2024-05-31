5122056d8544945274959c3b7adb60d1a5497a38
chaojun <chaojun@opensquare.network>
2024-05-31 20:09:59 +0800

    Update USDx amount, #4223

--- packages/next-common/components/summary/newProposalQuickStart/createUSDxTreasuryProposalPopup/treasuryBalance.js
+++ packages/next-common/components/summary/newProposalQuickStart/createUSDxTreasuryProposalPopup/treasuryBalance.js
@@ -1,22 +1,28 @@
+import BigNumber from "bignumber.js";
 import Loading from "next-common/components/loading";
 import ValueDisplay from "next-common/components/valueDisplay";
+import { useChain, useChainSettings } from "next-common/context/chain";
+import useFetch from "next-common/hooks/useFetch";
 import { toPrecisionNumber } from "next-common/utils";
-import { SYMBOL_DECIMALS } from "next-common/utils/consts/asset";
 
 export default function TreasuryBalance({ symbol, isLoading, value }) {
+  const chain = useChain();
+  const { decimals } = useChainSettings();
+  const amount = toPrecisionNumber(value, decimals);
+
+  const { loading: isPriceLoading, value: priceData } = useFetch(
+    `https://${chain}-api.dotreasury.com/overview`,
+  );
+  const latestSymbolPrice = priceData?.latestSymbolPrice;
+  const usdxAmount = new BigNumber(amount).times(latestSymbolPrice).toFixed();
+
   return (
     <div className="flex items-center gap-[8px] text12Bold text-textPrimary [&_.value-display-symbol]:text-textPrimary">
       <span className="text12Medium text-textTertiary">Treasury Balance</span>
-      {isLoading ? (
+      {isLoading || isPriceLoading ? (
         <Loading size={16} />
       ) : (
-        <ValueDisplay
-          value={toPrecisionNumber(
-            value,
-            SYMBOL_DECIMALS[symbol.toUpperCase()],
-          )}
-          symbol={symbol}
-        />
+        <ValueDisplay value={usdxAmount} symbol={symbol} />
       )}
     </div>
   );
