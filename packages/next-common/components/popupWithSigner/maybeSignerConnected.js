1d25b67ab201a0fb890cf9bfd178d24476618446
2nthony <ant.hu@outlook.com>
2024-06-05 15:49:56 +0800

    fix: maybe signer connected, from can be any wallet signer

--- packages/next-common/components/popupWithSigner/maybeSignerConnected.js
+++ packages/next-common/components/popupWithSigner/maybeSignerConnected.js
@@ -1,6 +1,19 @@
-import { SignerContextProvider } from "./context";
+import { useUser } from "../../context/user";
+import { isSameAddress } from "../../utils";
+import LoginPopup from "../login/popup";
+import { SignerContextProvider, usePopupParams } from "./context";
 
 export default function MaybeSignerConnected({ children, extensionAccounts }) {
+  const user = useUser();
+  const { onClose } = usePopupParams();
+
+  if (
+    !user?.address ||
+    !extensionAccounts?.find((acc) => isSameAddress(acc.address, user?.address))
+  ) {
+    return <LoginPopup onClose={onClose} showRegister={false} />;
+  }
+
   return (
     <SignerContextProvider extensionAccounts={extensionAccounts}>
       {children}
