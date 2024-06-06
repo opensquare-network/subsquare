e7b3a8a1779a578a4bf14dc57ac719fbc3b21d27
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-06 17:10:34 +0800

    Fix seo title

--- packages/next-common/hooks/usePageTitle.js
+++ packages/next-common/hooks/usePageTitle.js
@@ -1,10 +1,10 @@
-import { useChain } from "../context/chain";
+import { useChainSettings } from "../context/chain";
 import { capitalize } from "lodash-es";
 
 /**
  * @description Return page title with chain name.
  */
 export default function usePageTitle(suffix = "") {
-  const chain = useChain();
-  return `${capitalize(chain)} ${suffix}`;
+  const { name } = useChainSettings();
+  return `${capitalize(name)} ${suffix}`;
 }
