844c54b7caacd8f160b56f5b0923779282012149
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-11 16:35:43 +0800

    Update decentralized voices cohort 1 end

--- packages/next-common/utils/dv/delegates/polkadot.js
+++ packages/next-common/utils/dv/delegates/polkadot.js
@@ -6,7 +6,7 @@ export const polkadotDvDelegates = [
     slots: [
       {
         start: 19653189,
-        end: null,
+        end: 21157749,
         trackIds: treasuryTrackIds,
       },
     ],
@@ -16,29 +16,29 @@ export const polkadotDvDelegates = [
     slots: [
       {
         start: 19653189,
-        end: null,
+        end: 21157749,
         trackIds: treasuryTrackIds,
       },
     ],
   },
   {
     address: "12s6UMSSfE2bNxtYrJc6eeuZ7UxQnRpUzaAh1gPQrGNFnE8h", // Polkadotters
-    slots: [{ start: 19653237, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 19653237, end: 21157738, trackIds: treasuryTrackIds }],
   },
   {
     address: "15fTH34bbKGMUjF1bLmTqxPYgpg481imThwhWcQfCyktyBzL", // 🏔 HELIKON 🏔
-    slots: [{ start: 19653237, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 19653237, end: 21157738, trackIds: treasuryTrackIds }],
   },
   {
     address: "1ZSPR3zNg5Po3obkhXTPR95DepNBzBZ3CyomHXGHK9Uvx6w", // W1ZSPR3(William from Talisman)
-    slots: [{ start: 19653264, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 19653264, end: 21157744, trackIds: treasuryTrackIds }],
   },
   {
     address: "153YD8ZHD9dRh82U419bSCB5SzWhbdAFzjj4NtA5pMazR2yC", // SAXEMBERG
-    slots: [{ start: 19653264, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 19653264, end: 21157744, trackIds: treasuryTrackIds }],
   },
   {
     address: "12mP4sjCfKbDyMRAEyLpkeHeoYtS5USY4x34n9NMwQrcEyoh", // Polkaworld
-    slots: [{ start: 19653280, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 19653280, end: 21157754, trackIds: treasuryTrackIds }],
   },
 ];
