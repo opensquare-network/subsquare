8341b13eed3282a588b892164808f3eec722b5e1
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-11 16:48:49 +0800

    Update decentralized voices cohort 1 end for kusama

--- packages/next-common/utils/dv/delegates/kusama.js
+++ packages/next-common/utils/dv/delegates/kusama.js
@@ -3,26 +3,26 @@ import { treasuryTrackIds } from "./common";
 export const kusamaDvDelegates = [
   {
     address: "GqC37KSFFeGAoL7YxSeP1YDwr85WJvLmDDQiSaprTDAm8Jj", // Adam_Clay_Steeber
-    slots: [{ start: 22045831, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 22045831, end: 23551311, trackIds: treasuryTrackIds }],
   },
   {
     address: "Dm4uKxZJZHJbpZpfnYPiHnbgyHWKMU1s5h6X7kqjfYv1Xkk", // Alex PromoTeam Validator
-    slots: [{ start: 22045831, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 22045831, end: 23551311, trackIds: treasuryTrackIds }],
   },
   {
     address: "EocabFvqttEamwQKoFyQxLPnx9HWDdVDS9wwrUX1aKKbJ5g", // Alzymologist
-    slots: [{ start: 22045841, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 22045841, end: 23551317, trackIds: treasuryTrackIds }],
   },
   {
     address: "FDL99LDYERjevxPnXBjNGHZv13FxCGHrqh2N5zWQXx1finf", // Georgii / Space Invader
-    slots: [{ start: 22045841, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 22045841, end: 23551317, trackIds: treasuryTrackIds }],
   },
   {
     address: "FcjmeNzPk3vgdENm1rHeiMCxFK96beUoi2kb59FmCoZtkGF", // Staker Space
-    slots: [{ start: 22045955, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 22045955, end: 23551323, trackIds: treasuryTrackIds }],
   },
   {
     address: "J9FdcwiNLso4hcJFTeQvy7f7zszGhKoVh5hdBM2qF7joJQa", // Ivy voter collective
-    slots: [{ start: 22045955, end: null, trackIds: treasuryTrackIds }],
+    slots: [{ start: 22045955, end: 23551323, trackIds: treasuryTrackIds }],
   },
 ];
