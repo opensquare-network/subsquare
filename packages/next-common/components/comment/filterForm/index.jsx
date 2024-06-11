a56287c2627945c2118ff9cd29a6263919a3d6e2
2nthony <ant.hu@outlook.com>
2024-06-11 19:08:59 +0800

    loading

--- packages/next-common/components/comment/filterForm/index.jsx
+++ packages/next-common/components/comment/filterForm/index.jsx
@@ -1,9 +1,15 @@
+import { useVotesLoading } from "next-common/hooks/useVotesLoading";
 import CommentsFilterFormFilter from "./filter";
 import CommentsFilterFormSorter from "./sorter";
+import Loading from "next-common/components/loading";
 
 export default function CommentsFilterForm() {
+  const loading = useVotesLoading();
+
   return (
     <div className="flex items-center gap-x-2">
+      {loading && <Loading size={16} />}
+
       <CommentsFilterFormSorter />
       <CommentsFilterFormFilter />
     </div>
