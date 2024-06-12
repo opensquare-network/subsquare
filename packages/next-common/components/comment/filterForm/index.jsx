b9ef114395d82f319a5b08d7149e0478fa13dcc8
2nthony <ant.hu@outlook.com>
2024-06-12 17:28:59 +0800

    now only support filter in referenda page

--- packages/next-common/components/comment/filterForm/index.jsx
+++ packages/next-common/components/comment/filterForm/index.jsx
@@ -2,16 +2,26 @@ import { useVotesLoading } from "next-common/hooks/useVotesLoading";
 import CommentsFilterFormFilter from "./filter";
 import CommentsFilterFormSorter from "./sorter";
 import Loading from "next-common/components/loading";
+import { useDetailType } from "next-common/context/page";
+import { detailPageCategory } from "next-common/utils/consts/business/category";
 
 export default function CommentsFilterForm() {
   const loading = useVotesLoading();
+  const detailType = useDetailType();
 
-  return (
-    <div className="flex items-center gap-x-2">
-      {loading && <Loading size={16} />}
+  if (
+    detailType === detailPageCategory.GOV2_REFERENDUM ||
+    detailType === detailPageCategory.DEMOCRACY_REFERENDUM
+  ) {
+    return (
+      <div className="flex items-center gap-x-2">
+        {loading && <Loading size={16} />}
 
-      <CommentsFilterFormSorter />
-      <CommentsFilterFormFilter />
-    </div>
-  );
+        <CommentsFilterFormSorter />
+        <CommentsFilterFormFilter />
+      </div>
+    );
+  }
+
+  return null;
 }
