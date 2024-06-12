4582e4e73c959b9c18018072f6072c17ff9827dd
2nthony <ant.hu@outlook.com>
2024-06-12 11:16:44 +0800

    refactor

--- packages/next-common/hooks/useVotesLoading.js
+++ packages/next-common/hooks/useVotesLoading.js
@@ -2,6 +2,7 @@ import { useDetailType } from "next-common/context/page";
 import { votesLoadingSelector as democracyVotesLoadingSelector } from "next-common/store/reducers/democracy/votes/selectors";
 import { votesLoadingSelector as referendaVotesLoadingSelector } from "next-common/store/reducers/referenda/votes/selectors";
 import { detailPageCategory } from "next-common/utils/consts/business/category";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 
 export function useVotesLoading() {
@@ -9,12 +10,13 @@ export function useVotesLoading() {
   const referendaLoading = useSelector(referendaVotesLoadingSelector);
   const democracyLoading = useSelector(democracyVotesLoadingSelector);
 
-  let loading;
-  if (detailType === detailPageCategory.GOV2_REFERENDUM) {
-    loading = referendaLoading;
-  } else if (detailType === detailPageCategory.DEMOCRACY_REFERENDUM) {
-    loading = democracyLoading;
-  }
+  const loading = useMemo(() => {
+    if (detailType === detailPageCategory.GOV2_REFERENDUM) {
+      return referendaLoading;
+    } else if (detailType === detailPageCategory.DEMOCRACY_REFERENDUM) {
+      return democracyLoading;
+    }
+  }, [detailType, referendaLoading, democracyLoading]);
 
   return loading;
 }
