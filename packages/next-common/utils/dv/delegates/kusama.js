b964a07c37f287abd249b19a8cac3a87e6bb2ac4
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-12 11:33:34 +0800

    Update polkadot cohort 2

--- packages/next-common/utils/dv/delegates/kusama.js
+++ packages/next-common/utils/dv/delegates/kusama.js
@@ -26,3 +26,14 @@ export const kusamaDvDelegates = [
     slots: [{ start: 22045955, end: 23551323, trackIds: treasuryTrackIds }],
   },
 ];
+
+// cohort 2:
+// FDL99LDYERjevxPnXBjNGHZv13FxCGHrqh2N5zWQXx1finf  0xGeorgii | SpaceInvader
+// Dm4uKxZJZHJbpZpfnYPiHnbgyHWKMU1s5h6X7kqjfYv1Xkk Alex | PromoTeam
+// todo: Bruno Škvorc
+// CbNFBz4eykqiGqwYTfzRcYZGDh8xhbwwy4QaeiS4ctEPvXn Lorena Fabris
+// Ftuq9bHvQb5NiU5JA7q79fxYn9FVBeRjNBHL3RH5raN9qck  KSM Community Collective
+// DDCNPp8oeYBcBM44b32iSse4t4yfTnDJbjQxohF59Fo23EF Luke Schoen
+// todo: Roger Le
+// todo: Thomas Rivier | Bifrost
+// EtommijqrHDFWFvBxP515oUbkXx9vK2qohzrmwpCXbU7Yx2 Tommi/Hitchhooker | Rotko.net
--- packages/next-common/utils/dv/delegates/polkadot.js
+++ packages/next-common/utils/dv/delegates/polkadot.js
@@ -5,8 +5,15 @@ export const polkadotDvDelegates = [
     address: "13EyMuuDHwtq5RD6w3psCJ9WvJFZzDDion6Fd2FVAqxz1g7K", // ChaosDao OpenGov
     slots: [
       {
+        // cohort 1
         start: 19653189,
         end: 21157749,
+        trackIds: treasuryTrackIds,
+      },
+      {
+        // cohort 2
+        start: 21172801,
+        end: null,
         trackIds: treasuryTrackIds,
       },
     ],
@@ -35,10 +42,45 @@ export const polkadotDvDelegates = [
   },
   {
     address: "153YD8ZHD9dRh82U419bSCB5SzWhbdAFzjj4NtA5pMazR2yC", // SAXEMBERG
-    slots: [{ start: 19653264, end: 21157744, trackIds: treasuryTrackIds }],
+    slots: [
+      { start: 19653264, end: 21157744, trackIds: treasuryTrackIds },
+      { start: 21172834, end: null, trackIds: treasuryTrackIds }, // cohort 2
+    ],
   },
   {
     address: "12mP4sjCfKbDyMRAEyLpkeHeoYtS5USY4x34n9NMwQrcEyoh", // Polkaworld
     slots: [{ start: 19653280, end: 21157754, trackIds: treasuryTrackIds }],
+  },
+  {
+    address: "1CaXBXVGNbey352w7ydA1A2yDyNQLshycom8Zyj69v5eRNK", // BRA_16 Collective
+    slots: [{ start: 21172801, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "14Gn7SEmCgMX7Ukuppnw5TRjA7pao2HFpuJo39frB42tYLEh", // Ezio Rojas
+    slots: [{ start: 21172820, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "16XYgDGN6MxvdmjhRsHLT1oqQVDwGdEPVQqC42pRXiZrE8su", // Irina Karagyaur
+    slots: [{ start: 21173594, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "15TzZpYZa2rwfBNKhkDzuU1JApgACxD3m6pcaNt4SZneYTV5", // Mexican Collective
+    slots: [{ start: 21172820, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "12BJTP99gUerdvBhPobiTvrWwRaj1i5eFHN9qx51JWgrBtmv", // oneblock
+    slots: [{ start: 21173594, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "13mZThJSNdKUyVUjQE9ZCypwJrwdvY8G5cUCpS9Uw4bodh4t", // polkassembly
+    slots: [{ start: 21172851, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "13Bf8PY8dks2EecNFW7hrqJ7r1aj7iEFFUudFJFvoprXdUiH", // Scytale Digital
+    slots: [{ start: 21172851, end: null, trackIds: treasuryTrackIds }], // cohort 2
+  },
+  {
+    address: "12pXignPnq8sZvPtEsC3RdhDLAscqzFQz97pX2tpiNp3xLqo", // Lucky Friday Labs
+    slots: [{ start: 21172834, end: null, trackIds: treasuryTrackIds }], // cohort 2
   },
 ];
