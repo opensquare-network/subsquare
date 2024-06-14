4b26a61342db6d2c6147292233ee1f767f8888b3
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-14 10:43:31 +0800

    Dynamic import delegate and undelegate popup, #4268

--- packages/next-common/components/summary/democracyAllMyDelegationPopup/remove.js
+++ packages/next-common/components/summary/democracyAllMyDelegationPopup/remove.js
@@ -4,9 +4,19 @@ import { useDispatch } from "react-redux";
 import { incMyReferendaDelegationsTrigger } from "next-common/store/reducers/myOnChainData/referenda/myReferendaDelegations";
 import useIsUseMetamask from "next-common/hooks/useIsUseMetamask";
 import isMoonChain from "next-common/utils/isMoonChain";
-import UndelegatePopup from "../delegation/undelegatePopup";
-import MoonUndelegatePopup from "../delegation/undelegatePopup/moonPopup";
 import Tooltip from "next-common/components/tooltip";
+import dynamic from "next/dynamic";
+
+const UndelegatePopup = dynamic(() => import("../delegation/undelegatePopup"), {
+  ssr: false,
+});
+
+const MoonUndelegatePopup = dynamic(
+  () => import("../delegation/undelegatePopup/moonPopup"),
+  {
+    ssr: false,
+  },
+);
 
 export default function RemoveDelegation({ trackId }) {
   const dispatch = useDispatch();
