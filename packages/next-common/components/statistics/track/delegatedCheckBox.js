05432b04786fe8775ff29c7ef4219411078c21ee
2nthony <ant.hu@outlook.com>
2024-06-17 15:15:14 +0800

    fix: duplicated checkbox

--- packages/next-common/components/statistics/track/delegatedCheckBox.js
+++ packages/next-common/components/statistics/track/delegatedCheckBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CheckBox from "next-common/components/checkBox";
+import Checkbox from "next-common/components/checkbox";
 import FlexBetweenCenter from "next-common/components/styled/flexBetweenCenter";
 import styled from "styled-components";
 
@@ -11,7 +11,7 @@ const Wrapper = styled(FlexBetweenCenter)`
 export default function DelegatedCheckBox({ setChecked, checked }) {
   return (
     <Wrapper onClick={() => setChecked(!checked)}>
-      <CheckBox checked={checked} />
+      <Checkbox checked={checked} className="w-4 h-4" />
       <span className="text12Medium text-textPrimary">Delegated</span>
     </Wrapper>
   );
