56b1ad87ee2cc964415ec3f832f756c714ae43b3
2nthony <ant.hu@outlook.com>
2024-06-20 17:36:11 +0800

    popup container forward ref

--- packages/next-common/components/popup/wrapper/container.jsx
+++ packages/next-common/components/popup/wrapper/container.jsx
@@ -1,12 +1,14 @@
 import { cn } from "next-common/utils";
+import { forwardRef } from "react";
 
 /**
  * @param {React.HTMLAttributes<HTMLDivElement>} props
  */
-export default function PopupContainer(props) {
+function PopupContainer(props, ref) {
   return (
     <div
       {...props}
+      ref={ref}
       className={cn(
         "fixed inset-0 bg-black/25 flex justify-center items-start overflow-auto overscroll-y-none",
         props.className,
@@ -14,3 +16,5 @@ export default function PopupContainer(props) {
     />
   );
 }
+
+export default forwardRef(PopupContainer);
