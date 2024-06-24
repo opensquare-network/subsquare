4e40c95767ce9ff6ca9d95ff70daa6d3604c2fce
chaojun <chaojun@opensquare.network>
2024-06-24 09:57:09 +0800

    Clean unused code

--- packages/next-common/components/sima/comment/user.jsx
+++ packages/next-common/components/sima/comment/user.jsx
@@ -1,19 +1,7 @@
-import { useDetailType } from "next-common/context/page";
-import { detailPageCategory } from "next-common/utils/consts/business/category";
-import DVTag from "next-common/components/detail/common/DVTag";
 import AddressUser from "next-common/components/user/addressUser";
 
 export default function CommentUser({ address }) {
-  const type = useDetailType();
-
-  if (type === detailPageCategory.GOV2_REFERENDUM) {
-    return (
-      <div className="flex items-center gap-x-2">
-        <AddressUser add={address} />
-        <DVTag address={address} />
-      </div>
-    );
-  }
+  //TODO: GOV2_REFERENDUM DVTag display
 
   return <AddressUser add={address} />;
 }
