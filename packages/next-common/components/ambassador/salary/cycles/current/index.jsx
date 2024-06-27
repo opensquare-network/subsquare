4ea967739b1f804f4c22f57069d0a9cffc76a435
2nthony <ant.hu@outlook.com>
2024-06-27 18:44:02 +0800

    if no current status, hide current cycle panel

--- packages/next-common/components/ambassador/salary/cycles/current/index.jsx
+++ packages/next-common/components/ambassador/salary/cycles/current/index.jsx
@@ -1,9 +1,18 @@
 // import AmbassadorSalaryStatsActiveCycleDetailLink from "next-common/components/overview/ambassador/salary/detailLink/activeCycle";
+import { isNil } from "lodash-es";
 import AmbassadorSalaryStats from "next-common/components/overview/ambassador/salary/stats";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
+import { ambassadorSalaryStatusSelector } from "next-common/store/reducers/ambassador/salary";
+import { useSelector } from "react-redux";
 
 export default function AmbassadorSalaryCurrentCycle() {
+  const stats = useSelector(ambassadorSalaryStatusSelector);
+
+  if (isNil(stats)) {
+    return null;
+  }
+
   return (
     <div>
       <TitleContainer className="mb-4">Current Cycle</TitleContainer>
