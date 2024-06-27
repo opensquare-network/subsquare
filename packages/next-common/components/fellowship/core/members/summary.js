04ad9181f769da01f3b8b36793ed255745c8869a
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-27 11:51:56 +0800

    Init ambassador core page, #4314 (#4326)

--- packages/next-common/components/fellowship/core/members/summary.js
+++ packages/next-common/components/fellowship/core/members/summary.js
@@ -1,39 +1,15 @@
 import { useSelector } from "react-redux";
 import { fellowshipCoreMembersSelector } from "next-common/store/reducers/fellowship/core";
-import { isNil } from "lodash-es";
-import LoadableContent from "next-common/components/common/loadableContent";
 import { fellowshipCollectiveMembersSelector } from "next-common/store/reducers/fellowship/collective";
-import SummaryLayout from "next-common/components/summary/layout/layout";
-import SummaryItem from "next-common/components/summary/layout/item";
+import CollectivesCoreSummary from "next-common/components/collectives/core/summary";
 
 export default function FellowshipCoreMembersSummary() {
-  const fellowshipMembers = useSelector(fellowshipCollectiveMembersSelector);
+  const collectiveMembers = useSelector(fellowshipCollectiveMembersSelector);
   const coreMembers = useSelector(fellowshipCoreMembersSelector);
-  const candidates = (coreMembers || []).filter((m) => m.rank <= 0);
-  const total = (coreMembers || []).length;
-
-  const isLoading = isNil(coreMembers);
-
   return (
-    <SummaryLayout>
-      <SummaryItem title="Total">
-        <LoadableContent isLoading={isLoading}>{total}</LoadableContent>
-      </SummaryItem>
-      <SummaryItem title="Members">
-        <LoadableContent isLoading={isLoading}>
-          {total - candidates.length}
-        </LoadableContent>
-      </SummaryItem>
-      <SummaryItem title="Candidates">
-        <LoadableContent isLoading={isLoading}>
-          {candidates.length}
-        </LoadableContent>
-      </SummaryItem>
-      <SummaryItem title="Not Inducted">
-        <LoadableContent isLoading={isLoading}>
-          {fellowshipMembers?.length - total}
-        </LoadableContent>
-      </SummaryItem>
-    </SummaryLayout>
+    <CollectivesCoreSummary
+      coreMembers={coreMembers}
+      collectiveMembers={collectiveMembers}
+    />
   );
 }
--- packages/next-common/components/pages/ambassador/core.js
+++ packages/next-common/components/pages/ambassador/core.js
@@ -1,1 +1,10 @@
-export default function AmbassadorCoreMembersPage() {}
+import AmbassadorMembersLoadable from "next-common/components/pages/ambassador/loadable";
+import AmbassadorCoreCommon from "next-common/components/ambassador/core/common";
+
+export default function AmbassadorCoreMembersPage() {
+  return (
+    <AmbassadorMembersLoadable>
+      <AmbassadorCoreCommon></AmbassadorCoreCommon>
+    </AmbassadorMembersLoadable>
+  );
+}
