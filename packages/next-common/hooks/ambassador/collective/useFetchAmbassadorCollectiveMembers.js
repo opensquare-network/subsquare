85cb83b6418ff31c14d256255da47e3f6ad1e443
Yongfeng LI <wliyongfeng@gmail.com>
2024-06-27 11:09:39 +0800

    Add hooks to get ambassador members, #4314

--- packages/next-common/hooks/ambassador/collective/useFetchAmbassadorCollectiveMembers.js
+++ packages/next-common/hooks/ambassador/collective/useFetchAmbassadorCollectiveMembers.js
@@ -1,21 +1,21 @@
 import { useContextApi } from "next-common/context/api";
 import { useDispatch, useSelector } from "react-redux";
-import { fellowshipCoreMembersTriggerSelector } from "next-common/store/reducers/fellowship/core";
+import { ambassadorCollectiveMembersTriggerSelector } from "next-common/store/reducers/ambassador/collective";
 import { useEffect } from "react";
 import { setFellowshipCollectiveMembers } from "next-common/store/reducers/fellowship/collective";
 import { normalizeRankedCollectiveEntries } from "next-common/utils/rankedCollective/normalize";
 
-export default function useFetchFellowshipMembers() {
+export default function useFetchAmbassadorCollectiveMembers() {
   const api = useContextApi();
-  const trigger = useSelector(fellowshipCoreMembersTriggerSelector);
+  const trigger = useSelector(ambassadorCollectiveMembersTriggerSelector);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!api?.query?.fellowshipCollective?.members) {
+    if (!api?.query?.ambassadorCollective?.members) {
       return;
     }
 
-    api.query.fellowshipCollective.members
+    api.query.ambassadorCollective.members
       .entries()
       .then((collectiveEntries) => {
         const collectiveMembers =
