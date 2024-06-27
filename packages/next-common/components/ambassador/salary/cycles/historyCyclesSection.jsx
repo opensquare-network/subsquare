39b21c437af7261c7efbdc118a67a475003b1e55
2nthony <ant.hu@outlook.com>
2024-06-27 17:26:29 +0800

    init ambassador salary cycle panel and list #4314

---
+++ packages/next-common/components/ambassador/salary/cycles/historyCyclesSection.jsx
@@ -0,0 +1,22 @@
+import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
+import { usePageProps } from "next-common/context/page";
+import FellowshipSalaryCycles from "next-common/components/fellowship/salary/cycles/list";
+
+export default function AmbassadorHistoryCyclesSection() {
+  const { historyCycles } = usePageProps();
+
+  return (
+    <>
+      <TitleContainer className="my-4">History Cycles</TitleContainer>
+
+      <div className="space-y-4 mt-4">
+        <FellowshipSalaryCycles
+          historyCycles={historyCycles}
+          resolveActionColLink={(data) =>
+            `/ambassador/salary/cycles/${data.index}`
+          }
+        />
+      </div>
+    </>
+  );
+}
