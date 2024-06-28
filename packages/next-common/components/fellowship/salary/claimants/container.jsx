76c347b104a14645e491f2aa08944214926abc4d
2nthony <ant.hu@outlook.com>
2024-06-28 15:18:15 +0800

    refactor fellowship claimants list

--- packages/next-common/components/fellowship/salary/claimants/container.jsx
+++ packages/next-common/components/fellowship/salary/claimants/container.jsx
@@ -1,11 +1,20 @@
+import { useFellowSalaryClaimantsData } from "next-common/hooks/fellowship/salary/useFellowshipSalaryClaimantsData";
 import FellowshipSalaryActiveCycle from "../cycles/current";
-import FellowshipSalaryClaimants from "./list";
+import FellowshipSalaryClaimantsList from "./list";
+import { usePageProps } from "next-common/context/page";
 
 export default function FellowshipSalaryClaimantsContainer() {
+  const claimants = useFellowSalaryClaimantsData();
+  const { fellowshipParams, fellowshipMembers } = usePageProps();
+
   return (
     <div className="space-y-6">
       <FellowshipSalaryActiveCycle />
-      <FellowshipSalaryClaimants />
+      <FellowshipSalaryClaimantsList
+        claimants={claimants}
+        params={fellowshipParams}
+        members={fellowshipMembers}
+      />
     </div>
   );
 }
