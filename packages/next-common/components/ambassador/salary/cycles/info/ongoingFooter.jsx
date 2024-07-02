b4efe487fc28d07770b28825990f9936d9869f45
2nthony <ant.hu@outlook.com>
2024-07-02 15:32:54 +0800

    ambassador salary cycle ongoing panel footer actions #4314

--- packages/next-common/components/ambassador/salary/cycles/info/ongoingFooter.jsx
+++ packages/next-common/components/ambassador/salary/cycles/info/ongoingFooter.jsx
@@ -1,12 +1,16 @@
+import FellowshipSalaryBump from "next-common/components/fellowship/salary/actions/bump";
+import AmbassadorSalaryPayout from "../../actions/payout";
+import AmbassadorSalaryRegister from "../../actions/register";
+
 export default function AmbassadorSalaryCycleDetailInfoOngoingFooter() {
   return (
     <div className="space-y-2">
       {/* my status */}
 
       <div className="flex flex-wrap items-center justify-end gap-x-4 gap-y-2">
-        {/* bump */}
-        {/* register */}
-        {/* payout */}
+        <FellowshipSalaryBump />
+        <AmbassadorSalaryRegister />
+        <AmbassadorSalaryPayout />
       </div>
     </div>
   );
