6d528813b022451bd838d31b8cad02c590c56462
chaojun <chaojun@opensquare.network>
2024-07-03 16:55:40 +0800

    fix: discussion comment reply, #4266

--- packages/next-common/sima/components/post/context/commentActionsProvider.js
+++ packages/next-common/sima/components/post/context/commentActionsProvider.js
@@ -2,13 +2,13 @@ import CommentActionsContext from "next-common/sima/context/commentActions";
 import {
   useCreateDiscussionComment,
   useCreateDiscussionCommentReply,
-  useGetDiscussionComment,
 } from "next-common/sima/actions/comment";
 import { useDiscussionCommentUpVote } from "next-common/sima/actions/upVote";
 import { useDiscussionCommentCancelUpVote } from "next-common/sima/actions/cancelUpVote";
+import { useGetOffChainComment } from "next-common/noSima/actions/comment";
 
 export function DiscussionCommentActionsContextProvider({ children }) {
-  const getComment = useGetDiscussionComment();
+  const getComment = useGetOffChainComment();
   const createPostComment = useCreateDiscussionComment();
   const createCommentReply = useCreateDiscussionCommentReply();
   const upVoteComment = useDiscussionCommentUpVote();
