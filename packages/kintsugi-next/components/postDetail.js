3d1e606ca18bb9906ab795447bb079be313fbde1
chaojun <chaojun@opensquare.network>
2024-07-04 16:23:29 +0800

    fix: kintsugi discussion, #4266

--- packages/kintsugi-next/components/postDetail.js
+++ packages/kintsugi-next/components/postDetail.js
@@ -2,6 +2,8 @@ import { usePost } from "next-common/context/post";
 import DetailItem from "components/detailItem";
 import ContentWithComment from "next-common/components/detail/common/contentWithComment";
 import SimaPostDetail from "next-common/sima/components/post/postDetail";
+import { OffChainArticleActionsProvider } from "next-common/noSima/context/articleActionsProvider";
+import { OffChainCommentActionsProvider } from "next-common/noSima/context/commentActionsProvider";
 
 export default function PostDetail() {
   const post = usePost();
@@ -11,8 +13,12 @@ export default function PostDetail() {
   }
 
   return (
-    <ContentWithComment>
-      <DetailItem />
-    </ContentWithComment>
+    <OffChainArticleActionsProvider>
+      <OffChainCommentActionsProvider>
+        <ContentWithComment>
+          <DetailItem />
+        </ContentWithComment>
+      </OffChainCommentActionsProvider>
+    </OffChainArticleActionsProvider>
   );
 }
