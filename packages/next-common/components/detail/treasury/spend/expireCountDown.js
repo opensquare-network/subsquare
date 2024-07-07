cd8539932aaf91df06628f770c000dc3b274c61c
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-07 22:12:21 +0800

    Add valid and expire count down for treasury spend, #4332

---
+++ packages/next-common/components/detail/treasury/spend/expireCountDown.js
@@ -0,0 +1,47 @@
+import { useOnchainData, usePostState } from "next-common/context/post";
+import { useSelector } from "react-redux";
+import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
+import { useEstimateBlocksTime } from "next-common/utils/hooks";
+import { isNil } from "lodash-es";
+import { CountDownWrapper } from "next-common/components/detail/common/styled";
+import CountDown from "next-common/components/_CountDown";
+import React from "react";
+import Malicious from "next-common/components/detail/malicious";
+import useBlockTimestamp from "next-common/hooks/common/useBlockTimestamp";
+import { timeDurationFromNow } from "next-common/utils";
+
+export default function TreasurySpendExpireCountdown() {
+  const { meta } = useOnchainData() || {};
+  const { validFrom, expireAt } = meta || {};
+  const latestHeight = useSelector(chainOrScanHeightSelector);
+  const estimatedBlocksTime = useEstimateBlocksTime(expireAt - latestHeight);
+  const { timestamp } = useBlockTimestamp(expireAt);
+  const state = usePostState();
+  if (
+    isNil(expireAt) ||
+    isNil(validFrom) ||
+    ["Paid", "Processed"].includes(state)
+  ) {
+    return null;
+  }
+
+  if (latestHeight >= expireAt) {
+    return (
+      <Malicious>
+        Already expired {timestamp ? timeDurationFromNow(timestamp) : null}
+      </Malicious>
+    );
+  }
+
+  const text = `Expire in ${estimatedBlocksTime}`;
+  return (
+    <CountDownWrapper>
+      <CountDown
+        numerator={latestHeight - validFrom}
+        denominator={expireAt - validFrom}
+        tooltipContent={text}
+      />
+      <span>{text}</span>
+    </CountDownWrapper>
+  );
+}
