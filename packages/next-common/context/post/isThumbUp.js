b11d3c377f7f3f34385d864468ba18fb3c5e5841
chaojun <chaojun@opensquare.network>
2024-07-08 09:36:14 +0800

    Simplify, #4260

--- packages/next-common/context/post/isThumbUp.js
+++ packages/next-common/context/post/isThumbUp.js
@@ -1,21 +1,6 @@
-import { useConnectedAccount } from "../connectedAccount";
-import { useUser } from "../user";
-import { usePost } from "./index";
+import { useMyUpVote } from "./useMyUpVote";
 
 export function useIsThumbUp() {
-  const user = useUser();
-  const post = usePost();
-  const connectedAccount = useConnectedAccount();
-
-  if (!user && !connectedAccount) {
-    return false;
-  }
-
-  return (
-    post?.reactions?.findIndex(
-      (r) =>
-        r.user?.username === user.username ||
-        r.proposer === connectedAccount.address,
-    ) > -1
-  );
+  const myUpVote = useMyUpVote();
+  return !!myUpVote;
 }
