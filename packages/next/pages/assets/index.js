e05d2c0061016f50519749f2c049af839d0ff182
chaojun <chaojun@opensquare.network>
2024-07-09 16:20:32 +0800

    Init empty assethub page, #4354

--- packages/next/pages/assets/index.js
+++ packages/next/pages/assets/index.js
@@ -1,8 +1,21 @@
 import { withCommonProps } from "next-common/lib";
+import { useChainSettings } from "next-common/context/chain";
+import ListLayout from "next-common/components/layout/ListLayout";
+import { HeadContent, TitleExtra } from "next-common/components/overview";
 import Chains from "next-common/utils/consts/chains";
 
-export default function AssetHubPage() {
-  return "assets page";
+export default function HomePage() {
+  const chainSettings = useChainSettings();
+
+  return (
+    <ListLayout
+      title={chainSettings.name}
+      titleExtra={<TitleExtra />}
+      seoInfo={{ title: "" }}
+      description={chainSettings.description}
+      headContent={<HeadContent />}
+    ></ListLayout>
+  );
 }
 
 export const getServerSideProps = withCommonProps(async () => {
