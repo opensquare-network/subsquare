4492f66ee7eb4fb3b1fac2e358280c37118887eb
2nthony <ant.hu@outlook.com>
2024-07-10 11:26:40 +0800

    ambassador info tag

---
+++ packages/next-common/components/profile/ambassadorInfo.jsx
@@ -0,0 +1,36 @@
+import { isNil } from "lodash-es";
+import { useChainSettings } from "next-common/context/chain";
+import { CommonTag } from "../tags/state/styled";
+import { getRankColor } from "next-common/utils/fellowship/getRankColor";
+import { useAmbassadorMemberRank } from "next-common/hooks/ambassador/useAmbassadorMemberRank";
+
+function AmbassadorInfoImpl({ address }) {
+  const ambassadorRank = useAmbassadorMemberRank(address);
+
+  if (isNil(ambassadorRank)) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center my-2">
+      <CommonTag
+        className="py-1"
+        style={{
+          color: getRankColor(ambassadorRank),
+          backgroundColor: getRankColor(ambassadorRank, 0.1),
+        }}
+      >
+        Ambassador #{ambassadorRank}
+      </CommonTag>
+    </div>
+  );
+}
+
+export default function AmbassadorInfo({ address }) {
+  const { modules } = useChainSettings();
+  if (!modules.ambassador) {
+    return null;
+  }
+
+  return <AmbassadorInfoImpl address={address} />;
+}
