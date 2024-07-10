a1bd3e30e7a79ce9b799fa5581b420a3cb1c2c67
2nthony <ant.hu@outlook.com>
2024-07-10 15:01:45 +0800

    feat: user profile fellowship/ambassador tag info #4191 (#4370)
    * extract rank color util
    
    * refactor common tag
    
    * fellowship info tag
    
    * ambassador info tag
    
    * add tag info to bio
    
    * rename file
    
    * refactor, pass pallet
    
    * tweak
    
    * unified tag info

---
+++ packages/next-common/hooks/fellowship/useFellowshipMemberRank.js
@@ -0,0 +1,25 @@
+import { useContextApi } from "next-common/context/api";
+import { useEffect, useState } from "react";
+
+export function useFellowshipMemberRank(
+  address,
+  pallet = "fellowshipCollective",
+) {
+  const [rank, setRank] = useState(null);
+  const api = useContextApi();
+
+  useEffect(() => {
+    if (!api || !api.query[pallet]) {
+      return;
+    }
+
+    api.query[pallet].members(address).then((resp) => {
+      if (!resp.isNone) {
+        const json = resp.value.toJSON();
+        setRank(json.rank);
+      }
+    });
+  }, [api, address, pallet]);
+
+  return rank;
+}
