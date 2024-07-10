153bab835c1fea465f572ceb535bbc30495254ea
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-10 17:45:54 +0800

    Use BigInt to calculate native asset, #4354

--- packages/next/components/assets/useAssets.js
+++ packages/next/components/assets/useAssets.js
@@ -108,9 +108,9 @@ export default function useAssets() {
 
     let unsubNativeBalance;
     api.query.system
-      .account(address, (data) => {
-        const { free, reserved } = data.data.toJSON();
-        setNativeBalance(new BigNumber(free).plus(reserved).toFixed());
+      .account(address, ({data}) => {
+        const {free, reserved} = data;
+        setNativeBalance((free.toBigInt() + reserved.toBigInt()).toString());
       })
       .then((result) => (unsubNativeBalance = result));
 
