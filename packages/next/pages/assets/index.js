4bf5e7c6f9be181abd3ce94975a1e7224ebf59bc
chaojun <chaojun@opensquare.network>
2024-07-10 09:32:17 +0800

    Add asset list page header, #4354

--- packages/next/pages/assets/index.js
+++ packages/next/pages/assets/index.js
@@ -1,19 +1,19 @@
 import { withCommonProps } from "next-common/lib";
 import { useChainSettings } from "next-common/context/chain";
 import usePageTitle from "next-common/hooks/usePageTitle";
-import BaseLayout from "next-common/components/layout/baseLayout";
 import Chains from "next-common/utils/consts/chains";
 import NoWalletConnected from "components/assets/noWalletConnected";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import WalletAssetList from "components/assets/walletAssetList";
 
-function AssetsContent() {
+function AssetsContent({ seoInfo }) {
   const address = useRealAddress();
 
   if (!address) {
-    return <NoWalletConnected />;
+    return <NoWalletConnected seoInfo={seoInfo} />;
   }
 
-  return null;
+  return <WalletAssetList seoInfo={seoInfo} />;
 }
 
 export default function AssetsPage() {
@@ -24,11 +24,7 @@ export default function AssetsPage() {
     desc: chainSettings.description,
   };
 
-  return (
-    <BaseLayout seoInfo={seoInfo}>
-      <AssetsContent />
-    </BaseLayout>
-  );
+  return <AssetsContent seoInfo={seoInfo} />;
 }
 
 export const getServerSideProps = withCommonProps(async () => {
