78588fcad632af4b4a26de96dff9e000f010d708
chaojun <chaojun@opensquare.network>
2024-07-11 17:20:11 +0800

    Add ID column to asset list, #4354

--- packages/next-common/components/profile/assets/assetsList.js
+++ packages/next-common/components/profile/assets/assetsList.js
@@ -2,13 +2,14 @@ import React from "react";
 import ScrollerX from "next-common/components/styled/containers/scrollerX";
 import { MapDataList } from "next-common/components/dataList";
 import {
+  colId,
   colName,
   colToken,
   colTotal,
   colTransferrable,
 } from "next-common/components/assets/assetsList";
 
-const columnsDef = [colToken, colName, colTotal, colTransferrable];
+const columnsDef = [colToken, colId, colName, colTotal, colTransferrable];
 
 export default function ProfileAssetsList({ assets }) {
   return (
