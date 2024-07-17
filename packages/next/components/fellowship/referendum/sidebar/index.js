d633f25f7180395f4e6f134234279dbf2680e0d5
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-17 22:26:52 +0800

    Check permissions for collective vote, #4419 (#4423)
    * Check collective referenda vote permission, #4419
    
    * Fix lint, #4419
    
    * Not show rank info on fellowship vote popup, #4419
    
    * Fix lint, #4419
    
    * Fix lint
    
    * Show tooltip when checking vote permissions, #4419

--- packages/next/components/fellowship/referendum/sidebar/index.js
+++ packages/next/components/fellowship/referendum/sidebar/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { RightBarWrapper } from "next-common/components/layout/sidebar/rightBarWrapper";
 import FellowshipTally from "./tally";
 import Gov2Status from "../../../gov2/sidebar/status";
@@ -11,6 +11,63 @@ import { useChainSettings } from "next-common/context/chain";
 import HowOpenGovWorks from "next-common/components/howOpenGovWorks";
 import { VoteSuccessfulProvider } from "next-common/components/vote";
 import VoteSuccessfulPopup from "../votePopup/voteSuccessful";
+import { isNil, isUndefined, noop } from "lodash-es";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import useSubCollectiveRank from "next-common/hooks/collectives/useSubCollectiveRank";
+import { useRankedCollectivePallet } from "next-common/context/collectives/collectives";
+import {
+  getAmbassadorMinRankOfClass,
+  getFellowshipMinRankOfClass,
+} from "next-common/context/post/fellowship/useMaxVoters";
+import { useTrack } from "next-common/context/post/gov2/track";
+import Tooltip from "next-common/components/tooltip";
+
+function useMinRank() {
+  const { id: trackId } = useTrack();
+
+  const collectivePallet = useRankedCollectivePallet();
+  if ("fellowshipCollective" === collectivePallet) {
+    return getFellowshipMinRankOfClass(trackId);
+  } else if ("ambassadorCollective" === collectivePallet) {
+    return getAmbassadorMinRankOfClass(trackId);
+  } else {
+    throw new Error(`Can not get min rank of class for pallet ${collectivePallet}`);
+  }
+}
+
+function CollectiveVote({ onClick = noop }) {
+  const address = useRealAddress();
+  const collectivePallet = useRankedCollectivePallet();
+  const { rank, loading } = useSubCollectiveRank(address, collectivePallet);
+  const minRank = useMinRank();
+  const disabled = loading || isNil(rank) || rank < minRank;
+  const text = loading ? "Checking permissions" : "Vote";
+
+  const tooltipText = useMemo(() => {
+    if (loading) {
+      return "Checking permissions";
+    }
+
+    if (!isUndefined(rank) && rank < minRank) {
+      return `Only members with rank >= ${minRank} can vote`;
+    }
+
+    return null;
+  }, [loading, rank, minRank]);
+
+  return (
+    <Tooltip content={tooltipText}>
+      <PrimaryButton
+        loading={loading}
+        disabled={disabled}
+        style={{ width: "100%" }}
+        onClick={onClick}
+      >
+        {text}
+      </PrimaryButton>
+    </Tooltip>
+  );
+}
 
 export default function FellowshipReferendumSideBar() {
   const post = usePost();
@@ -24,16 +81,7 @@ export default function FellowshipReferendumSideBar() {
       <Gov2Status />
       <FellowshipTally />
       {isVoting && !hideActionButtons && (
-        <InlineWrapper>
-          <PrimaryButton
-            style={{ width: "100%" }}
-            onClick={() => {
-              setShowVote(true);
-            }}
-          >
-            Vote
-          </PrimaryButton>
-        </InlineWrapper>
+        <CollectiveVote onClick={() => setShowVote(true)} />
       )}
       <VoteSuccessfulProvider VoteSuccessfulPopup={VoteSuccessfulPopup}>
         {showVote && (
