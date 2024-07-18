643b863f0699acdff067b8ed69f7394f38321152
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-18 18:24:33 +0800

    Refactor useSubscribeNativeBalance

--- packages/next-common/utils/hooks/useSubAddressBalance.js
+++ packages/next-common/utils/hooks/useSubAddressBalance.js
@@ -2,7 +2,7 @@ import BigNumber from "bignumber.js";
 import { useState } from "react";
 import useSubStorage from "next-common/hooks/common/useSubStorage";
 
-export default function useSubAddressBalance(api, address) {
+export default function useSubAddressBalance(address) {
   const [balance, setBalance] = useState(0);
   const { loading } = useSubStorage("system", "account", [address], (account) => {
     const balance = new BigNumber(account.data.free.toJSON())
