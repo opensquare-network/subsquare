ea020e6d9dbf912cb44c992f54ea9bec220b76c8
2nthony <ant.hu@outlook.com>
2024-07-22 15:09:37 +0800

    refactor: wallet option #4416 (#4451)

--- packages/next-common/components/wallet/walletOption.jsx
+++ packages/next-common/components/wallet/walletOption.jsx
@@ -1,69 +1,68 @@
 import React from "react";
 import { cn } from "next-common/utils";
-import Loading from "../loading";
+import SecondaryButton from "next-common/lib/button/secondary";
+import ExternalLink from "../externalLink";
+import { SystemLink, SystemLoadingDots } from "@osn/icons/subsquare";
 
 /**
  * @param {{
  *  selected: boolean
  *  installed: boolean
+ *  installUrl?: string
  *  logo: React.ReactElement
  *  title: string
- *  loading?: boolean
- *  notInstalledContent?: React.ReactElement
- *  extraContent?: React.ReactElement
- * } & React.HTMLAttributes<HTMLDivElement>} props
+ * } & ButtonProps} props
  */
 export default function WalletOption({
   selected,
   installed,
+  installUrl,
   logo,
   title,
   loading,
-  notInstalledContent,
-  extraContent: extraContentProp,
   ...props
 }) {
-  let extraContent;
-  if (loading) {
-    extraContent = <Loading />;
-  } else if (!installed) {
-    extraContent = notInstalledContent || (
-      <div className="text12Medium">Not installed</div>
-    );
-  } else {
-    extraContent = extraContentProp;
-  }
-
   const disabled = !installed || loading;
 
   return (
-    <div
-      role="button"
+    <SecondaryButton
       {...props}
       className={cn(
-        "flex justify-between items-center px-4 py-2.5",
-        // compat with old logic
-        "[&>div]:gap-x-4",
-        "text14Bold text-textPrimary",
-        "rounded-lg border",
-
-        !disabled
-          ? "border-neutral400 hover:bg-neutral300 hover:border-neutral300"
-          : "cursor-default bg-neutral200 border-neutral200 select-none text-textTertiary", // disabled
+        "flex justify-start",
+        "!text14Bold text-textPrimary",
+        "disabled:bg-neutral200 disabled:border-neutral200 disabled:select-none disabled:text-textTertiary",
         selected && "bg-neutral300 border-neutral300",
-
-        // wallet logo svg
-        "[&_svg]:w-[24px] [&_svg]:h-[24px]",
-
+        // icon left(wallet logo)
+        "[&_.button-icon-left]:mr-3",
+        "[&_.button-icon-left_svg]:w-6 [&_.button-icon-left_svg]:h-6",
+        // large size button
+        "py-[9px] h-12 pl-[11px]",
         props.className,
       )}
+      disabled={disabled}
+      iconLeft={logo}
     >
-      <div className="flex items-center">
-        {logo}
-        <div>{title}</div>
-      </div>
-
-      {extraContent}
-    </div>
+      <span className="inline-flex justify-between items-center w-full">
+        {title}
+        {loading ? (
+          <SystemLoadingDots className="w-5 h-5" />
+        ) : (
+          !installed && (
+            <span className="text12Medium">
+              Not installed
+              {installUrl && (
+                <ExternalLink
+                  externalIcon={false}
+                  className="ml-3"
+                  href={installUrl}
+                >
+                  <SystemLink className="inline-flex w-5 h-5 text-theme500" />
+                </ExternalLink>
+              )}
+            </span>
+          )
+        )}
+      </span>
+    </SecondaryButton>
   );
 }
