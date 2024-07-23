1f1e8e6a1da4fd79ee276f901f7a0bd839e39271
2nthony <ant.hu@outlook.com>
2024-07-23 16:13:17 +0800

    revert dynamic

--- packages/next-common/components/login/globalPopup.jsx
+++ packages/next-common/components/login/globalPopup.jsx
@@ -1,11 +1,7 @@
 import { useLoginPopup } from "next-common/hooks/useLoginPopup";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import dynamicClientOnly from "next-common/lib/dynamic/clientOnly";
-
-const LoginPopup = dynamicClientOnly(() =>
-  import("next-common/components/login/globalPopup"),
-);
+import LoginPopup from "next-common/components/login/popup";
 
 export default function LoginGlobalPopup() {
   const { loginPopupOpen, closeLoginPopup } = useLoginPopup();
