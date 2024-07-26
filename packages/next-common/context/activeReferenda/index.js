df487d390713af6994045625d43ff78cb1b88f6f
Chaojun Huang <chaojun@opensquare.network>
2024-07-26 12:26:32 +0800

    Show how many evidences(member submitted) need to be processed, #4318 (#4478)
    * Show how many evidences(member submitted) need to be processed, #4318
    
    * Add loading state, #4318
    
    * fix
    
    * Not show warning panel when there is none, #4318 (#4479)
    
    * Guard evidences variable length, #4318
    
    * Improve evidences return value, #4318 (#4480)
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

--- packages/next-common/context/activeReferenda/index.js
+++ packages/next-common/context/activeReferenda/index.js
@@ -7,15 +7,20 @@ const ActiveReferendaContext = createContext();
 export default ActiveReferendaContext;
 
 export function ActiveReferendaProvider({ pallet, children }) {
-  const activeReferenda = useOnChainActiveReferenda(pallet);
+  const { activeReferenda, isLoading } = useOnChainActiveReferenda(pallet);
 
   return (
-    <ActiveReferendaContext.Provider value={activeReferenda}>
+    <ActiveReferendaContext.Provider value={{ activeReferenda, isLoading }}>
       {children}
     </ActiveReferendaContext.Provider>
   );
 }
 
-export function useActiveReferenda() {
+export function useActiveReferendaContext() {
   return useContext(ActiveReferendaContext);
+}
+
+export function useActiveReferenda() {
+  const { activeReferenda } = useActiveReferendaContext();
+  return activeReferenda;
 }
