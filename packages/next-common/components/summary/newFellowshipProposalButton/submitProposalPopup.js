e1cf49332ab50648aa81b03ef456c44e928e4427
2nthony <ant.hu@outlook.com>
2024-07-29 17:46:23 +0800

    new retain proposal template popup #4282 (#4493)
    * rename prop to `rank`
    
    * fix
    
    * fix: track name
    
    * retain template popup
    
    * rename files

--- packages/next-common/components/summary/newFellowshipProposalButton/submitProposalPopup.js
+++ packages/next-common/components/summary/newFellowshipProposalButton/submitProposalPopup.js
@@ -7,7 +7,8 @@ import { usePageProps } from "next-common/context/page";
 import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
 import { NewRemarkReferendumInnerPopup } from "../newProposalQuickStart/createSystemRemarkProposalPopup";
 import { QuickStart } from "next-common/components/preImages/createPreimagePopup";
-import NewFellowshipCoreMemberReferendumInnerPopup from "../newProposalQuickStart/createFellowshipCoreMemberProposalPopup";
+import NewFellowshipCoreMemberPromoteReferendumInnerPopup from "../newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberPromotePopup";
+import NewFellowshipCoreMemberRetainReferendumInnerPopup from "../newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberRetainPopup";
 
 export default function SubmitFellowshipProposalPopup({ onClose }) {
   const { period } = usePageProps();
@@ -16,11 +17,14 @@ export default function SubmitFellowshipProposalPopup({ onClose }) {
 
   const [showMemberPromotionPopup, setShowMemberPromotionPopup] =
     useState(false);
+  const [showRetainPopup, setShowRetainPopup] = useState(false);
   const [showNewRemarkPopup, setShowNewRemarkPopup] = useState(false);
 
   let content;
   if (showMemberPromotionPopup) {
-    content = <NewFellowshipCoreMemberReferendumInnerPopup />;
+    content = <NewFellowshipCoreMemberPromoteReferendumInnerPopup />;
+  } else if (showRetainPopup) {
+    content = <NewFellowshipCoreMemberRetainReferendumInnerPopup />;
   } else if (showNewRemarkPopup) {
     content = <NewRemarkReferendumInnerPopup />;
   } else {
@@ -38,10 +42,17 @@ export default function SubmitFellowshipProposalPopup({ onClose }) {
       >
         <QuickStart>
           <ChoiceButton
-            name="Member Promotion"
+            name="Promote"
             description="Creating a proposal for getting promoted to a higher rank"
             onClick={() => {
               setShowMemberPromotionPopup(true);
+            }}
+          />
+          <ChoiceButton
+            name="Retain"
+            description="Creating a proposal for retaining current rank"
+            onClick={() => {
+              setShowRetainPopup(true);
             }}
           />
           <ChoiceButton
