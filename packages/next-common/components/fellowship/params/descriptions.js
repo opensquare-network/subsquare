91855b83a45330138793da4a12a5b21094137646
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-29 18:12:51 +0800

    Update

---
+++ packages/next-common/components/fellowship/params/descriptions.js
@@ -0,0 +1,10 @@
+export default function CollectiveParamsDescriptions({ rank, params }) {
+  const {
+    activeSalary = [],
+    passiveSalary = [],
+    demotionPeriod = [],
+    minPromotionPeriod = [],
+  } = params ?? {};
+
+
+}
--- packages/next-common/components/fellowship/params/list.jsx
+++ packages/next-common/components/fellowship/params/list.jsx
@@ -64,7 +64,7 @@ export default function FellowshipParamsList({ rank, params }) {
       columns={columns}
       noDataText="No Params"
       rows={rows}
-      highlightedIndexes={[rank]}
+      highlightedIndexes={[rank - 1]}
     />
   );
 }
