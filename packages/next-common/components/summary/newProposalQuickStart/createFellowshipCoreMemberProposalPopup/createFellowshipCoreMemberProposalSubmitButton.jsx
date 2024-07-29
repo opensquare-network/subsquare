55358cac72a888a0daedbcf5ab1c8691b34b21f5
2nthony <ant.hu@outlook.com>
2024-07-29 15:41:06 +0800

    only rank >= 3

--- packages/next-common/components/summary/newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberProposalSubmitButton.jsx
+++ packages/next-common/components/summary/newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberProposalSubmitButton.jsx
@@ -7,6 +7,12 @@ import { getEventData } from "next-common/utils/sendTx";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import useTrackDetail from "../../newProposalPopup/useTrackDetail";
+import useFetchFellowshipCoreMembers from "next-common/hooks/fellowship/core/useFetchFellowshipCoreMembers";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import { useSelector } from "react-redux";
+import { fellowshipCoreMembersSelector } from "next-common/store/reducers/fellowship/core";
+import { find } from "lodash-es";
+import Tooltip from "next-common/components/tooltip";
 
 export default function CreateFellowshipCoreMemberProposalSubmitButton({
   trackId,
@@ -14,6 +20,13 @@ export default function CreateFellowshipCoreMemberProposalSubmitButton({
   who,
   toRank,
 }) {
+  useFetchFellowshipCoreMembers();
+  const members = useSelector(fellowshipCoreMembersSelector);
+  const realAddress = useRealAddress();
+  const me = find(members, { address: realAddress });
+
+  const myRankOk = me && me.rank >= 3;
+
   const api = useContextApi();
   const onClose = usePopupOnClose();
   const router = useRouter();
@@ -21,7 +34,7 @@ export default function CreateFellowshipCoreMemberProposalSubmitButton({
   const listPageType = useListPageType();
   const origin = track?.origin;
 
-  const disabled = !who || !toRank;
+  const disabled = !myRankOk || !who || !toRank;
 
   let corePallet = "fellowshipCore";
   let referendaPallet = "fellowshipReferenda";
@@ -45,19 +58,23 @@ export default function CreateFellowshipCoreMemberProposalSubmitButton({
   }, [api, who, referendaPallet, toRank]);
 
   return (
-    <TxSubmissionButton
-      disabled={disabled}
-      title="Create Preimage"
-      getTxFunc={getTxFunc}
-      onClose={onClose}
-      onInBlock={(events) => {
-        const eventData = getEventData(events, referendaPallet, "Submitted");
-        if (!eventData) {
-          return;
-        }
-        const [referendumIndex] = eventData;
-        router.push(`/${listPageType}/${referendumIndex}`);
-      }}
-    />
+    <Tooltip
+      content={!myRankOk && "Only available to the members with rank >= 3"}
+    >
+      <TxSubmissionButton
+        disabled={disabled}
+        title="Create Preimage"
+        getTxFunc={getTxFunc}
+        onClose={onClose}
+        onInBlock={(events) => {
+          const eventData = getEventData(events, referendaPallet, "Submitted");
+          if (!eventData) {
+            return;
+          }
+          const [referendumIndex] = eventData;
+          router.push(`/${listPageType}/${referendumIndex}`);
+        }}
+      />
+    </Tooltip>
   );
 }
