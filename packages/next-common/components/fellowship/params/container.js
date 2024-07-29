bd7cd969613244358f8f61f8d292fc7d23b7edf4
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-29 17:58:01 +0800

    Refactor params page with useRankFilter, #4475

--- packages/next-common/components/fellowship/params/container.js
+++ packages/next-common/components/fellowship/params/container.js
@@ -1,40 +1,17 @@
-import { useState } from "react";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
-import Select from "next-common/components/select";
 import FellowshipParamsList from "next-common/components/fellowship/params/list";
 import FellowshipParamsOffBoardTimeoutCard from "next-common/components/fellowship/params/off-boardTimeout";
+import useRankFilter from "next-common/hooks/fellowship/useRankFilter";
 
 export default function FellowshipCoreParamsContainer({ params }) {
-  const options = [
-    {
-      label: "-",
-      value: "",
-    },
-    ...(params?.activeSalary || []).map((_, idx) => {
-      return {
-        label: idx + 1,
-        value: idx,
-      };
-    }),
-  ];
-  const [rank, setRank] = useState(options[0].value);
+  const ranks = (params?.activeSalary || []).map((_, idx) => idx);
+  const { rank, component } = useRankFilter(ranks, "-");
 
   return (
     <>
       <TitleContainer>
         <span>Params</span>
-        <div className="text12Medium text-textPrimary flex items-center gap-x-2">
-          <div>Rank</div>
-          <Select
-            className="w-20"
-            small
-            value={rank}
-            options={options}
-            onChange={(option) => {
-              setRank(option.value);
-            }}
-          />
-        </div>
+        {component}
       </TitleContainer>
       <div className="space-y-4 mt-4">
         <FellowshipParamsList rank={rank} params={params} />
