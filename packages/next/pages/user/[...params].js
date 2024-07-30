0a0ec5ee322fa854d3c7a40530424ce124627af4
2nthony <ant.hu@outlook.com>
2024-07-30 17:24:11 +0800

    revert [skip ci]

--- packages/next/pages/user/[...params].js
+++ packages/next/pages/user/[...params].js
@@ -3,7 +3,6 @@ import nextApi from "next-common/services/nextApi";
 import Profile from "next-common/components/profile";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 import { tryConvertToSubstrateAddress } from "next-common/utils/mixedChainUtil";
-import { fellowshipParamsApi } from "next-common/services/url";
 
 export default Profile;
 
@@ -14,14 +13,9 @@ export const getServerSideProps = withCommonProps(async (context) => {
 
   const maybeAddress = tryConvertToSubstrateAddress(id);
 
-  const [
-    { result: userSummary },
-    { result: user },
-    { result: fellowshipParams = {} },
-  ] = await Promise.all([
+  const [{ result: userSummary }, { result: user }] = await Promise.all([
     nextApi.fetch(`users/${maybeAddress}/counts`),
     nextApi.fetch(`users/${maybeAddress}`),
-    nextApi.fetch(fellowshipParamsApi),
   ]);
   const tracksProps = await fetchOpenGovTracksProps();
 
@@ -32,7 +26,6 @@ export const getServerSideProps = withCommonProps(async (context) => {
       user: user ?? {},
       route: context.query?.params?.slice(1)?.join("/") ?? "",
       ...tracksProps,
-      fellowshipParams,
     },
   };
 });
