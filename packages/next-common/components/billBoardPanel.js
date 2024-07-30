1e2aa5bace6b68bb901dc2ece8722731ba7be1cc
Chaojun Huang <chaojun@opensquare.network>
2024-07-30 11:00:45 +0800

    Extract multi prompts panel component (#4498)
    * Extract multi prompts panel component
    
    * Rename to BillBoardPanel

---
+++ packages/next-common/components/billBoardPanel.js
@@ -0,0 +1,28 @@
+import { cn } from "next-common/utils";
+import { SecondaryCard } from "./styled/containers/secondaryCard";
+
+export default function BillBoardPanel({ className, icon, items = [] }) {
+  if (!items.length) {
+    return null;
+  }
+
+  return (
+    <SecondaryCard
+      className={cn(
+        "flex !p-6 gap-x-4",
+        "max-sm:flex-col max-sm:gap-y-3",
+        className,
+      )}
+    >
+      <div className="w-10 h-10 bg-theme100 rounded-lg flex items-center justify-center">
+        {icon}
+      </div>
+
+      <ul className="list-disc text14Medium text-textPrimary pl-6">
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </SecondaryCard>
+  );
+}
