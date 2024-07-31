cd98a25d29e7eca3434a7e570aa1fdb4cd376921
Yongfeng LI <wliyongfeng@gmail.com>
2024-07-31 21:32:50 +0800

    Guard loading for track status panel, #4240

--- packages/next/components/referenda/tracks/tracksStatus/tracksStatusPanel.js
+++ packages/next/components/referenda/tracks/tracksStatus/tracksStatusPanel.js
@@ -6,26 +6,27 @@ import { BucketProvider } from "./useBucket";
 
 export default function TracksStatusPanel() {
   const { tracks, isLoading } = useGroupedReferenda();
+  if (isLoading) {
+    return <SecondaryCard>
+      <div className="flex justify-center">
+        <Loading size={16} />
+      </div>
+    </SecondaryCard>
+  }
 
   return (
     <SecondaryCard>
-      {isLoading ? (
-        <div className="flex justify-center">
-          <Loading size={16} />
+      <BucketProvider>
+        <div className="m-[-24px]">
+          {(tracks || []).map((track) => (
+            <TrackStatusItem
+              key={track.trackId}
+              trackId={track.trackId}
+              {...track.referenda}
+            />
+          ))}
         </div>
-      ) : (
-        <BucketProvider>
-          <div className="m-[-24px]">
-            {(tracks || []).map((track) => (
-              <TrackStatusItem
-                key={track.trackId}
-                trackId={track.trackId}
-                {...track.referenda}
-              />
-            ))}
-          </div>
-        </BucketProvider>
-      )}
+      </BucketProvider>
     </SecondaryCard>
   );
 }
