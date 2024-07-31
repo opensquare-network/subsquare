aedebc9bf708b7af699ee664fb4ea9c78106028b
2nthony <ant.hu@outlook.com>
2024-07-31 16:17:59 +0800

    createFellowshipCoreFeedsRows

--- packages/next-common/components/fellowship/core/feeds/list.jsx
+++ packages/next-common/components/fellowship/core/feeds/list.jsx
@@ -3,30 +3,22 @@ import { orderBy } from "lodash-es";
 import FellowshipFeedSuffix from "next-common/components/fellowship/feeds/suffix";
 import FellowshipFeedsPanel from "next-common/components/fellowship/feeds/list";
 
-export default function FellowshipCoreFeedsList({
-  feeds = {},
-  noDataText,
-  bordered,
-}) {
+export function createFellowshipCoreFeedsRows(feeds) {
   const orderedItems = orderBy(
     feeds?.items || [],
     ["indexer.blockHeight", "indexer.eventIndex"],
     ["desc", "desc"],
   );
 
-  const rows = orderedItems?.map?.((item) => {
+  return orderedItems?.map?.((item) => {
     return [
       <FellowshipCoreFeedsListEvent key="event" feed={item} className="pr-2" />,
       <FellowshipFeedSuffix key="block-info" indexer={item?.indexer} />,
     ];
   });
+}
 
-  return (
-    <FellowshipFeedsPanel
-      feeds={feeds}
-      rows={rows}
-      noDataText={noDataText}
-      bordered={bordered}
-    />
-  );
+export default function FellowshipCoreFeedsList({ feeds = {} }) {
+  const rows = createFellowshipCoreFeedsRows(feeds?.items);
+  return <FellowshipFeedsPanel feeds={feeds} rows={rows} />;
 }
