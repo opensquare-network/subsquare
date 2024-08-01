2072a4bea71f2eca3ca219c4a2d4497c00f33dcf
chaojun <chaojun@opensquare.network>
2024-08-01 11:40:44 +0800

    fix: use context pallet, #4240

--- packages/next-common/hooks/referenda/useOnChainReferendaTracks.js
+++ packages/next-common/hooks/referenda/useOnChainReferendaTracks.js
@@ -1,18 +1,20 @@
 import { useContextApi } from "next-common/context/api";
+import { useReferendaPallet } from "next-common/context/referenda/pallet";
 import { useMemo } from "react";
 
 export default function useOnChainReferendaTracks() {
   const api = useContextApi();
+  const pallet = useReferendaPallet();
 
   const tracks = useMemo(() => {
     if (!api) {
       return [];
     }
-    return (api?.consts?.referenda?.tracks || []).map(([id, detail]) => ({
+    return (api?.consts[pallet]?.tracks || []).map(([id, detail]) => ({
       id: id.toNumber(),
       ...detail.toJSON(),
     }));
-  }, [api]);
+  }, [api, pallet]);
 
   return {
     tracks,
