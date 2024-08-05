4e4dc250bae1101303f4f37dadf12d95479026e0
leocs2417 <leochen.sat24@gmail.com>
2024-08-05 11:46:24 +0800

    feat: Swap the line style of Threshold and Current

--- packages/next-common/components/charts/thresholdCurve/legend/gov2TallyLegend.js
+++ packages/next-common/components/charts/thresholdCurve/legend/gov2TallyLegend.js
@@ -3,14 +3,14 @@ import LegendItem from "../../legend/item";
 export default function ThresholdCurvesGov2TallyLegend() {
   return (
     <div className="flex justify-center">
-      <LegendItem color="var(--purple500)">Support</LegendItem>
-      <LegendItem dashed color="var(--purple500)">
-        Current Support
-      </LegendItem>
-      <LegendItem color="var(--green500)">Approval</LegendItem>
       <LegendItem dashed color="var(--green500)">
-        Current Approval
+        Approval
       </LegendItem>
+      <LegendItem color="var(--green500)">Current Approval</LegendItem>
+      <LegendItem dashed color="var(--purple500)">
+        Support
+      </LegendItem>
+      <LegendItem color="var(--purple500)">Current Support</LegendItem>
     </div>
   );
 }
