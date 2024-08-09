ecbbb503eaf112d45cd41f8623e5bc31d040be90
leochen <leochen.sat24@gmail.com>
2024-08-09 18:43:54 +0800

    feat: Fellowship Statistics Expenditure. #4329 (#4555)
    * feat: add Fellowship Statistics Cycles.
    
    * chore: rm console.log

--- packages/next-common/components/fellowship/statistics/collectives.jsx
+++ packages/next-common/components/fellowship/statistics/collectives.jsx
@@ -1,24 +1,9 @@
-import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
-import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
-import SummaryItem from "next-common/components/summary/layout/item";
-import SummaryLayout from "next-common/components/summary/layout/layout";
-
-function SummaryPanel() {
-  return (
-    <SecondaryCard>
-      <SummaryLayout>
-        <SummaryItem title="Total Spent">123</SummaryItem>
-        <SummaryItem title="Spent Circles">234</SummaryItem>
-      </SummaryLayout>
-    </SecondaryCard>
-  );
-}
+import StatisticsExpenditure from "next-common/components/fellowship/statistics/expenditure";
 
 export default function FellowshipCollectivesStatistics() {
   return (
-    <div className="flex flex-col gap-[16px]">
-      <TitleContainer>Expenditure</TitleContainer>
-      <SummaryPanel />
+    <div className="flex flex-col gap-y-4">
+      <StatisticsExpenditure />
     </div>
   );
 }
---
+++ packages/next-common/components/fellowship/statistics/expenditure/summary/index.jsx
@@ -0,0 +1,14 @@
+import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
+import SummaryItem from "next-common/components/summary/layout/item";
+import SummaryLayout from "next-common/components/summary/layout/layout";
+
+export default function StatisticsExpenditureSummary({ value = [], loading }) {
+  return (
+    <SecondaryCard>
+      <SummaryLayout>
+        <SummaryItem title="Total Spent">123</SummaryItem>
+        <SummaryItem title="Spent Circles">234</SummaryItem>
+      </SummaryLayout>
+    </SecondaryCard>
+  );
+}
