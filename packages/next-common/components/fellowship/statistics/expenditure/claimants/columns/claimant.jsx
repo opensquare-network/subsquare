7690ac7ba7d59956eaf50eb653adc25b9c12b2b5
leocs2417 <leochen.sat24@gmail.com>
2024-08-12 18:11:35 +0800

    feat: rm useless claimant props

--- packages/next-common/components/fellowship/statistics/expenditure/claimants/columns/claimant.jsx
+++ packages/next-common/components/fellowship/statistics/expenditure/claimants/columns/claimant.jsx
@@ -3,8 +3,6 @@ import AddressUser from "next-common/components/user/addressUser";
 export function useStatisticsClaimantColumn() {
   return {
     name: "Claimant",
-    flexGrow: 1,
-    minWidth: 240,
     cellRender(data) {
       return <AddressUser key={data.who} add={data.who} />;
     },
