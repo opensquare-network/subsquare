4fff680bc9f9512f14466348a84d7cba7a2fb14d
chaojun <chaojun@opensquare.network>
2024-08-15 13:11:52 +0800

    refactor opengov remove vote

--- packages/next-common/components/myReferendumVote/removeReferendaVotePopup/index.js
+++ packages/next-common/components/myReferendumVote/removeReferendaVotePopup/index.js
@@ -1,58 +1,23 @@
-import React, { useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
-
-import { useMountedState } from "react-use";
-import { newErrorToast } from "next-common/store/reducers/toastSlice";
-import { sendTx, wrapWithProxy } from "next-common/utils/sendTx";
-import SignerPopup from "next-common/components/signerPopup";
+import React, { useCallback } from "react";
+import SignerPopupV2 from "next-common/components/signerPopup/indexV2";
+import { useContextApi } from "next-common/context/api";
 
 export default function RemoveReferendaVotePopup({
   trackId,
   referendumIndex,
   onClose,
 }) {
-  const dispatch = useDispatch();
-  const isMounted = useMountedState();
-  const [isLoading, setIsLoading] = useState(false);
+  const api = useContextApi();
 
-  const showErrorToast = useCallback(
-    (message) => dispatch(newErrorToast(message)),
-    [dispatch],
-  );
-
-  const doRemoveVote = useCallback(
-    async (api, signerAccount) => {
-      if (!api) {
-        return showErrorToast("Chain network is not connected yet");
-      }
-
-      if (!signerAccount) {
-        return showErrorToast("Please login first");
-      }
-
-      let tx = api.tx.convictionVoting.removeVote(trackId, referendumIndex);
-      if (signerAccount?.proxyAddress) {
-        tx = wrapWithProxy(api, tx, signerAccount.proxyAddress);
-      }
-
-      await sendTx({
-        tx,
-        setLoading: setIsLoading,
-        dispatch,
-        onClose,
-        signerAccount,
-        isMounted,
-      });
-    },
-    [dispatch, isMounted, showErrorToast, onClose, trackId, referendumIndex],
-  );
+  const getTxFunc = useCallback(async () => {
+    return api.tx.convictionVoting.removeVote(trackId, referendumIndex);
+  }, [api, trackId, referendumIndex]);
 
   return (
-    <SignerPopup
+    <SignerPopupV2
       title="Remove Vote"
-      actionCallback={doRemoveVote}
+      getTxFunc={getTxFunc}
       onClose={onClose}
-      isLoading={isLoading}
     />
   );
 }
