ce39e4e98c7be8e0c8663488ee7cfa67b3c870be
chaojun <chaojun@opensquare.network>
2024-08-15 15:54:47 +0800

    refactor second popup

--- packages/next-common/components/publicProposal/second/popup/index.js
+++ packages/next-common/components/publicProposal/second/popup/index.js
@@ -1,61 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import PopupWithSigner from "../../../popupWithSigner";
-import PopupContent from "./popupContent";
-import { sendTx, wrapWithProxy } from "next-common/utils/sendTx";
-import { newErrorToast } from "next-common/store/reducers/toastSlice";
+import { useDispatch } from "react-redux";
+import Signer from "next-common/components/popup/fields/signerField";
+import { newErrorToast } from "../../../../store/reducers/toastSlice";
+import DepositRequired from "./depositRequired";
+import useDeposit from "./useDeposit";
+import { isNil, noop } from "lodash-es";
+import SecondPopupInputTimes from "./inputTimes";
+import { useSignerAccount } from "next-common/components/popupWithSigner/context";
+import useIsLoaded from "next-common/hooks/useIsLoaded";
+import { useChainSettings } from "next-common/context/chain";
+import { usePopupParams } from "next-common/components/popupWithSigner/context";
+import { useContextApi } from "next-common/context/api";
+import TxSubmissionButton from "next-common/components/common/tx/txSubmissionButton";
 
-export async function submitSubstrateExtrinsic({
-  api,
-  proposalIndex,
-  depositorUpperBound,
-  times,
-  dispatch,
-  setLoading,
-  onSubmitted,
-  onInBlock,
-  onClose,
-  signerAccount,
-  isMounted,
-}) {
-  let tx = null;
-  try {
-    if (api.tx.democracy?.second?.meta.args.length < 2) {
-      tx = api.tx.democracy.second(proposalIndex);
-    } else {
-      tx = api.tx.democracy.second(proposalIndex, depositorUpperBound || 1);
+function PopupContent() {
+  const {
+    proposalIndex,
+    depositorUpperBound,
+    depositRequired,
+    onInBlock = noop,
+    useAddressVotingBalance,
+  } = usePopupParams();
+  const dispatch = useDispatch();
+  const signerAccount = useSignerAccount();
+  const node = useChainSettings();
+
+  const [submitDisabled, setSubmitDisabled] = useState(false);
+  const api = useContextApi();
+  const [balance, loadingBalance] = useAddressVotingBalance(
+    api,
+    signerAccount?.realAddress,
+  );
+  const isBalanceLoaded = useIsLoaded(loadingBalance);
+  const [signerBalance, isSignerBalanceLoading] = useAddressVotingBalance(
+    api,
+    signerAccount?.address,
+  );
+  const {
+    deposit,
+    balanceInsufficient,
+    isLoading: isCheckingDeposit,
+  } = useDeposit(depositRequired, balance, isBalanceLoaded);
+  const [times, setTimes] = useState(1);
+
+  const getTxFunc = async () => {
+    if (isNil(proposalIndex)) {
+      return;
     }
-  } catch (e) {
-    return dispatch(newErrorToast(e.message));
-  }
 
-  if (times > 1) {
-    const txs = Array.from({ length: times }).fill(tx);
-    tx = api.tx.utility.batch(txs);
-  }
+    let tx = null;
+    try {
+      if (api.tx.democracy?.second?.meta.args.length < 2) {
+        tx = api.tx.democracy.second(proposalIndex);
+      } else {
+        tx = api.tx.democracy.second(proposalIndex, depositorUpperBound || 1);
+      }
+    } catch (e) {
+      return dispatch(newErrorToast(e.message));
+    }
 
-  if (signerAccount?.proxyAddress) {
-    tx = wrapWithProxy(api, tx, signerAccount.proxyAddress);
-  }
+    if (times > 1) {
+      const txs = Array.from({ length: times }).fill(tx);
+      tx = api.tx.utility.batch(txs);
+    }
 
-  await sendTx({
-    tx,
-    dispatch,
-    setLoading,
-    onInBlock,
-    onSubmitted,
-    onClose,
-    signerAccount,
-    isMounted,
-  });
+    return tx;
+  };
+
+  return (
+    <>
+      <Signer
+        balanceName="Voting balance"
+        balance={balance}
+        isBalanceLoading={loadingBalance}
+        signerBalance={signerBalance}
+        isSignerBalanceLoading={isSignerBalanceLoading}
+        symbol={node.voteSymbol}
+      />
+      <DepositRequired
+        deposit={deposit}
+        balanceInsufficient={balanceInsufficient}
+      />
+      <SecondPopupInputTimes
+        times={times}
+        setTimes={setTimes}
+        currentTimes={depositorUpperBound}
+        setSubmitDisabled={setSubmitDisabled}
+      />
+      <TxSubmissionButton
+        getTxFunc={getTxFunc}
+        disabled={balanceInsufficient || isCheckingDeposit || submitDisabled}
+        onInBlock={onInBlock}
+      />
+    </>
+  );
 }
 
 export default function Popup(props) {
   return (
-    <PopupWithSigner
-      title="Second"
-      submitExtrinsic={submitSubstrateExtrinsic}
-      {...props}
-    >
+    <PopupWithSigner title="Second" {...props}>
       <PopupContent />
     </PopupWithSigner>
   );
