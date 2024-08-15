7c05c8b9fda0f716390140260670dbe382c76778
chaojun <chaojun@opensquare.network>
2024-08-15 11:53:52 +0800

    refactor undelegate popup

--- packages/next-common/components/summary/delegation/undelegatePopup/index.js
+++ packages/next-common/components/summary/delegation/undelegatePopup/index.js
@@ -1,50 +1,26 @@
 import React from "react";
 import { noop } from "lodash-es";
-import { sendTx, wrapWithProxy } from "next-common/utils/sendTx";
-import PopupCommon from "./popupCommon";
-
-export async function submitSubstrateExtrinsic({
-  api,
-  trackId,
-  dispatch,
-  setLoading,
-  onInBlock,
-  signerAccount,
-  isMounted,
-  onClose,
-}) {
-  let tx = api.tx.convictionVoting.undelegate(trackId);
-
-  if (signerAccount?.proxyAddress) {
-    tx = wrapWithProxy(api, tx, signerAccount.proxyAddress);
-  }
-
-  await sendTx({
-    tx,
-    dispatch,
-    setLoading,
-    onInBlock,
-    signerAccount,
-    isMounted,
-    onClose,
-  });
-}
+import { useCallback } from "react";
+import SignerPopupV2 from "next-common/components/signerPopup/indexV2";
+import { useContextApi } from "next-common/context/api";
 
 export default function UndelegatePopup({
   trackId,
   onClose,
-  isLoading,
-  setIsLoading = noop,
   onInBlock = noop,
 }) {
+  const api = useContextApi();
+
+  const getTxFunc = useCallback(async () => {
+    return api.tx.convictionVoting.undelegate(trackId);
+  }, [api, trackId]);
+
   return (
-    <PopupCommon
-      trackId={trackId}
+    <SignerPopupV2
+      title="Undelegate"
+      getTxFunc={getTxFunc}
       onClose={onClose}
-      isLoading={isLoading}
-      setIsLoading={setIsLoading}
       onInBlock={onInBlock}
-      submitExtrinsic={submitSubstrateExtrinsic}
     />
   );
 }
