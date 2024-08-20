cc066e320f78c5157a74df652073a7be50d1d454
leocs2417 <leochen.sat24@gmail.com>
2024-08-20 17:49:30 +0800

    feat: revert reset ValidFromField

--- packages/next-common/components/popup/fields/validFromField.js
+++ packages/next-common/components/popup/fields/validFromField.js
@@ -60,12 +60,6 @@ export default function ValidFromField({ title = "", value, setValue }) {
     }
   }, [isEditable]);
 
-  useEffect(() => {
-    return () => {
-      setValue("");
-    };
-  }, []);
-
   return (
     <div>
       <div className="flex justify-between items-center mb-[8px]">
