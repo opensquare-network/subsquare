3fe880345a6c423918504494e2c7fef3c07255e6
2nthony <ant.hu@outlook.com>
2024-08-21 19:06:17 +0800

    refactor: use fetch function

--- packages/next-common/components/collectives/core/updateFunc.js
+++ packages/next-common/components/collectives/core/updateFunc.js
@@ -1,15 +1,16 @@
 import { useCallback } from "react";
 import getChainSettings from "next-common/utils/consts/settings";
 import { defaultBlockTime } from "next-common/utils/constants";
-import { incFellowshipCoreMembersTrigger } from "next-common/store/reducers/fellowship/core";
 import { sleep } from "next-common/utils";
 import { useDispatch } from "react-redux";
 import { useCollectivesContext } from "next-common/context/collectives/collectives";
 import { incAmbassadorCoreMembersTrigger } from "next-common/store/reducers/ambassador/core";
+import useFellowshipCoreMembers from "next-common/hooks/fellowship/core/useFellowshipCoreMembers";
 
 export default function useCoreFellowshipUpdateFunc() {
   const dispatch = useDispatch();
   const section = useCollectivesContext();
+  const { fetch } = useFellowshipCoreMembers();
 
   return useCallback(async () => {
     const blockTime =
@@ -20,11 +21,11 @@ export default function useCoreFellowshipUpdateFunc() {
     // eslint-disable-next-line no-unused-vars
     for (const timer of timers) {
       if ("fellowship" === section) {
-        dispatch(incFellowshipCoreMembersTrigger());
+        await fetch();
       } else if ("ambassador" === section) {
         dispatch(incAmbassadorCoreMembersTrigger());
       }
       await sleep(blockTime);
     }
-  }, [dispatch]);
+  }, [dispatch, fetch, section]);
 }
