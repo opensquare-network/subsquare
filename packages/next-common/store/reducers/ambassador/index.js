d652699b9c6ceb21901f7c756ff1785b5bd3ef82
2nthony <ant.hu@outlook.com>
2024-08-21 16:39:15 +0800

    refactor: delete ambassador referenda info store #4585

--- packages/next-common/store/reducers/ambassador/index.js
+++ packages/next-common/store/reducers/ambassador/index.js
@@ -2,12 +2,10 @@ import ambassadorCore from "./core";
 import ambassadorCollective from "./collective";
 import ambassadorSalaryClaimants from "./claimants";
 import ambassadorSalary from "./salary";
-import ambassadorReferendumInfo from "./info";
 
 export default {
   ambassadorCore,
   ambassadorCollective,
   ambassadorSalaryClaimants,
   ambassadorSalary,
-  ambassadorReferendumInfo,
 };
