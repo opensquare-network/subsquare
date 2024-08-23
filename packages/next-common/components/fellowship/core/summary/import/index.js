0726c8834b5ecc5386e4ccef391597134e526ffc
2nthony <ant.hu@outlook.com>
2024-08-23 15:07:02 +0800

    revert: summary import popup

--- packages/next-common/components/fellowship/core/summary/import/index.js
+++ packages/next-common/components/fellowship/core/summary/import/index.js
@@ -7,7 +7,9 @@ import dynamicPopup from "next-common/lib/dynamic/popup";
 import useFellowshipCoreMembers from "next-common/hooks/fellowship/core/useFellowshipCoreMembers";
 import { useFellowshipCollectiveMembers } from "next-common/hooks/fellowship/core/useFellowshipCollectiveMembers";
 
-const FellowshipCoreImportPopup = dynamicPopup(() => import("./popup"));
+const FellowshipCoreImportPopup = dynamicPopup(() =>
+  import("next-common/components/fellowship/core/summary/import/popup"),
+);
 
 export default function Import() {
   const [showPopup, setShowPopup] = useState(false);
