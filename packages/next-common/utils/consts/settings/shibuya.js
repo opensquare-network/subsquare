ef8b4971fded5274a8f2fa9a2d29bedbb718b13a
chaojun <chaojun@opensquare.network>
2024-08-23 13:55:33 +0800

    Reuse astar setting for shibuya, #4559

--- packages/next-common/utils/consts/settings/shibuya.js
+++ packages/next-common/utils/consts/settings/shibuya.js
@@ -1,8 +1,8 @@
 import Chains from "../chains";
 import capitalize from "../../capitalize";
 import MenuGroups from "./menuGroups";
-import { defaultPostLabels } from "./common";
 import dynamic from "next/dynamic";
+import astarSettings from "./astar";
 
 const ProjectIconShibuyaDark = dynamic(() =>
   import("@osn/icons/subsquare/ProjectIconShibuyaDark"),
@@ -28,78 +28,19 @@ const nodes = [
   },
 ];
 
-const links = [
-  {
-    name: "website",
-    url: "https://astar.network/",
-  },
-  {
-    name: "twitter",
-    url: "https://twitter.com/AstarNetwork",
-  },
-  {
-    name: "discord",
-    url: "https://discord.gg/astarnetwork",
-  },
-  {
-    name: "telegram",
-    url: "https://t.me/PlasmOfficial",
-  },
-  {
-    name: "github",
-    url: "https://github.com/AstarNetwork",
-  },
-  {
-    name: "youtube",
-    url: "https://www.youtube.com/c/AstarNetwork",
-  },
-];
-
 const shibuya = {
+  ...astarSettings,
   value: Chains.shibuya,
   name: capitalize(Chains.shibuya),
   identity: Chains.shibuya,
   symbol: "SBY",
-  decimals: 18,
-  blockTime: 12000,
-  ss58Format: 5,
   endpoints: nodes,
   avatar: ProjectIconShibuyaLight,
   darkAvatar: ProjectIconShibuyaDark,
   navLogo: ProjectLogoShibuyaLight,
   navLogoDark: ProjectLogoShibuyaDark,
-  navPreferDark: true,
-  links,
   group: MenuGroups.Solochain,
-  postLabels: defaultPostLabels,
-  hasSubscan: true,
-  hasDiscussions: true,
-  hasTechComm: true,
-  hasTipsModule: false,
   description: "Shibuya is the testnet of Shiden (a sister chain of Astar).",
-  modules: {
-    democracy: true,
-    referenda: false,
-  },
-  cssVarsLight: {
-    theme100: "rgba(0,117,255,0.10)",
-    theme300: "rgba(0,117,255,0.40)",
-    theme500: "rgba(0,117,255,1)",
-    navigationBg: "rgba(2,6,23,1)",
-    navigationActive: "rgba(255,255,255,0.06)",
-    navigationBorder: "rgba(255,255,255,0.08)",
-    navigationText: "var(--textPrimaryContrast)",
-    navigationTextTertiary: "var(--textTertiaryContrast)",
-    navigationIcon: "var(--textSecondaryContrast)",
-  },
-  cssVarsDark: {
-    theme100: "rgba(0,117,255,0.10)",
-    theme300: "rgba(0,117,255,0.40)",
-    theme500: "rgba(0,117,255,1)",
-    navigationBg: "rgba(2,6,23,1)",
-    navigationActive: "rgba(38,41,56,1)",
-    navigationBorder: "var(--neutral300)",
-  },
 };
 
 export default shibuya;
