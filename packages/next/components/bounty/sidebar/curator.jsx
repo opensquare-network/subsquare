7a5b68e065afda6126aa6c51196bc3ac62058a8d
leocs2417 <leochen.sat24@gmail.com>
2024-08-26 16:17:59 +0800

    chore: add badge & MultisigAccounts

--- packages/next/components/bounty/sidebar/curator.jsx
+++ packages/next/components/bounty/sidebar/curator.jsx
@@ -3,7 +3,6 @@ import Loading from "next-common/components/loading";
 import { SecondaryCardDetail } from "next-common/components/styled/containers/secondaryCard";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 import { BorderedRow } from "next-common/components/referenda/tally/styled";
-import { useOnchainData } from "next-common/context/post";
 import AddressUser from "next-common/components/user/addressUser";
 import IndentPanel from "next-common/components/callTreeView/indentPanel";
 import CuratorLinks from "./links";
@@ -16,10 +15,35 @@ function MultisigAccounts() {
   // TODO:
   // if MultisigAccounts null, return null;
   // return IndentPanel
+  const mockCurator = "";
+
   return (
     <div className="pl-3">
-      <IndentPanel>123</IndentPanel>
+      <IndentPanel>
+        <AddressUser key={mockCurator} add={mockCurator} className="my-1" />
+        <AddressUser key={mockCurator} add={mockCurator} className="my-1" />
+        <AddressUser key={mockCurator} add={mockCurator} className="my-1" />
+        <AddressUser key={mockCurator} add={mockCurator} className="my-1" />
+      </IndentPanel>
     </div>
+  );
+}
+
+function ThresholdSignatoryBadge() {
+  // TODO
+  const badgeValue = "3/7";
+  return (
+    <span className="py-[2px] px-[8px] rounded-[10px] text12Medium bg-theme100 text-theme500">
+      {badgeValue}
+    </span>
+  );
+}
+
+function CuratorTitle() {
+  return (
+    <Title className="!px-0">
+      <span>Curator</span>
+    </Title>
   );
 }
 
@@ -36,16 +60,12 @@ function BountySidebarCurator() {
 
   return (
     <SecondaryCardDetail>
-      <Title className="!px-0">
-        <span>Curator</span>
-        {isLoading && (
-          <div>
-            <Loading size={16} />
-          </div>
-        )}
-      </Title>
+      <CuratorTitle />
       <BorderedRow>
-        <AddressUser key={mockCurator} add={mockCurator} />
+        <div className="flex items-center space-x-2">
+          <AddressUser key={mockCurator} add={mockCurator} />
+          <ThresholdSignatoryBadge />
+        </div>
       </BorderedRow>
       <CuratorLinks address={mockCurator} showCouncilorLink={true} />
       <MultisigAccounts />
