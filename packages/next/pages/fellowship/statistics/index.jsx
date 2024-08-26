a9c103092796df03ef1ef8e003edb2f7c5c8c256
2nthony <ant.hu@outlook.com>
2024-08-26 14:14:07 +0800

    fix: fellowship statistics collective provider

--- packages/next/pages/fellowship/statistics/index.jsx
+++ packages/next/pages/fellowship/statistics/index.jsx
@@ -2,15 +2,18 @@ import ListLayout from "next-common/components/layout/ListLayout";
 import { withCommonProps } from "next-common/lib";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 import FellowshipCollectivesStatistics from "next-common/components/fellowship/statistics/collectives";
+import CollectivesProvider from "next-common/context/collectives/collectives";
 
 export default function FellowshipStatisticsPage() {
   const category = "Fellowship Statistics";
   const seoInfo = { title: category, desc: category };
 
   return (
-    <ListLayout seoInfo={seoInfo} title={category}>
-      <FellowshipCollectivesStatistics />
-    </ListLayout>
+    <CollectivesProvider section="fellowship">
+      <ListLayout seoInfo={seoInfo} title={category}>
+        <FellowshipCollectivesStatistics />
+      </ListLayout>
+    </CollectivesProvider>
   );
 }
 
