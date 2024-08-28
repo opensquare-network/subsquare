e264088bf591c8acb2b8a46cf8a528c01f1ef5ae
2nthony <ant.hu@outlook.com>
2024-08-28 14:48:19 +0800

    support show evidence markdown content #4607 (#4610)
    * feat: support evidence content
    
    * extract fellowship evidence content as component
    
    * tweak
    
    * core member view evidence detail
    
    * evidence card list

---
+++ packages/next-common/components/collectives/core/evidenceContent.jsx
@@ -0,0 +1,94 @@
+import { SystemLink, SystemLoading, SystemWarning } from "@osn/icons/subsquare";
+import { MarkdownPreviewer } from "@osn/previewer";
+import WarningInfoPanel from "next-common/components/summary/styled/warningInfoPanel";
+import { useIpfsContent } from "next-common/hooks/useIpfsContent";
+import { cn, isHash } from "next-common/utils";
+import { getCidByEvidence } from "next-common/utils/collective/getCidByEvidence";
+import { hexToString } from "@polkadot/util";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import Tooltip from "next-common/components/tooltip";
+import ExternalLink from "next-common/components/externalLink";
+import getIpfsLink from "next-common/utils/env/ipfsEndpoint";
+
+export default function FellowshipEvidenceContent({
+  loading,
+  wish,
+  evidence,
+  className = "",
+}) {
+  const notFound = !wish && !evidence;
+
+  let content;
+
+  if (loading) {
+    content = <LoadingContent />;
+  } else if (notFound) {
+    content = (
+      <WarningInfoPanel className="justify-center">
+        <SystemWarning className="w-5 h-5" />
+        {"Warning: can't find evidence on chain."}
+      </WarningInfoPanel>
+    );
+  } else if (!isHash(evidence)) {
+    content = <MarkdownPreviewer content={hexToString(evidence)} />;
+  } else {
+    const cid = getCidByEvidence(evidence);
+
+    content = (
+      <>
+        <GreyPanel className="justify-center gap-x-2 text14Medium text-textPrimary py-2.5 px-4 max-w-full">
+          <div className="line-clamp-1 break-all">{cid}</div>
+          <Tooltip content="IPFS Link">
+            <ExternalLink
+              href={getIpfsLink(cid)}
+              externalIcon={false}
+              className="text-textTertiary hover:text-textSecondary"
+            >
+              <SystemLink className="w-4 h-4" />
+            </ExternalLink>
+          </Tooltip>
+        </GreyPanel>
+
+        <IpfsEvidenceContent cid={cid} />
+      </>
+    );
+  }
+
+  return (
+    <div className={cn("space-y-4", className)}>
+      <h4 className="text14Bold text-textPrimary">
+        Evidence{wish && ` for ${wish}`}
+      </h4>
+
+      {content}
+    </div>
+  );
+}
+
+function LoadingContent() {
+  return (
+    <div className="flex items-center justify-center gap-x-2 text-textTertiary text14Medium">
+      <SystemLoading className="w-5 h-5" />
+      Fetching...
+    </div>
+  );
+}
+
+function IpfsEvidenceContent({ cid }) {
+  const { value, loading, error } = useIpfsContent(cid);
+
+  if (loading) {
+    return <LoadingContent />;
+  }
+
+  if (error) {
+    return (
+      <WarningInfoPanel className="justify-center">
+        <SystemWarning className="w-5 h-5" />
+        {"Warning: can't load the evidence content."}
+      </WarningInfoPanel>
+    );
+  }
+
+  return <MarkdownPreviewer content={value} />;
+}
--- packages/next-common/components/detail/fellowship/evidence.jsx
+++ packages/next-common/components/detail/fellowship/evidence.jsx
@@ -1,89 +1,20 @@
-import { SystemLink, SystemLoading, SystemWarning } from "@osn/icons/subsquare";
-import { MarkdownPreviewer } from "@osn/previewer";
-import ExternalLink from "next-common/components/externalLink";
-import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
-import WarningInfoPanel from "next-common/components/summary/styled/warningInfoPanel";
-import Tooltip from "next-common/components/tooltip";
+import FellowshipEvidenceContent from "next-common/components/collectives/core/evidenceContent";
 import { useCoreFellowshipPallet } from "next-common/context/collectives/collectives";
 import { useOnchainData } from "next-common/context/post";
 import { useReferendumFellowshipCoreEvidence } from "next-common/context/post/fellowship/useReferendumFellowshipCoreEvidence";
-import { useIpfsContent } from "next-common/hooks/useIpfsContent";
-import { getCidByEvidence } from "next-common/utils/collective/getCidByEvidence";
-import getIpfsLink from "next-common/utils/env/ipfsEndpoint";
-
-function ContentLoading() {
-  return (
-    <div className="flex items-center justify-center gap-x-2 text-textTertiary text14Medium">
-      <SystemLoading className="w-5 h-5" />
-      Fetching...
-    </div>
-  );
-}
-
-function IpfsContent({ cid }) {
-  const { value, loading, error } = useIpfsContent(cid);
-
-  if (loading) {
-    return <ContentLoading />;
-  }
-
-  if (error) {
-    return (
-      <WarningInfoPanel className="justify-center">
-        <SystemWarning className="w-5 h-5" />
-        {"Warning: can't load the evidence content."}
-      </WarningInfoPanel>
-    );
-  }
-
-  return <MarkdownPreviewer content={value} />;
-}
 
 function FellowshipReferendaDetailEvidenceImpl() {
   const { wish, evidence, loading } = useReferendumFellowshipCoreEvidence();
-  const notFound = !wish && !evidence;
-
-  const cid = getCidByEvidence(evidence);
-
-  let content;
-  if (loading) {
-    content = <ContentLoading />;
-  } else if (notFound) {
-    content = (
-      <WarningInfoPanel className="justify-center">
-        <SystemWarning className="w-5 h-5" />
-        {"Warning: can't find evidence on chain."}
-      </WarningInfoPanel>
-    );
-  } else {
-    content = (
-      <>
-        <GreyPanel className="justify-center gap-x-2 text14Medium text-textPrimary py-2.5 px-4 max-w-full">
-          <div className="line-clamp-1 break-all">{cid}</div>
-          <Tooltip content="IPFS Link">
-            <ExternalLink
-              href={getIpfsLink(cid)}
-              externalIcon={false}
-              className="text-textTertiary hover:text-textSecondary"
-            >
-              <SystemLink className="w-4 h-4" />
-            </ExternalLink>
-          </Tooltip>
-        </GreyPanel>
-
-        <IpfsContent cid={cid} />
-      </>
-    );
-  }
 
   return (
     <div className="mt-4 space-y-4">
-      <hr className="my-4" />
-      <h4 className="text14Bold text-textPrimary">
-        Evidence{wish && ` for ${wish}`}
-      </h4>
+      <hr />
 
-      {content}
+      <FellowshipEvidenceContent
+        wish={wish}
+        evidence={evidence}
+        loading={loading}
+      />
     </div>
   );
 }
