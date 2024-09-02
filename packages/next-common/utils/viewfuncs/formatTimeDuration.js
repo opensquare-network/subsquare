ca6ec05aec12e6cb87938ff4f93a417d0446374f
2nthony <ant.hu@outlook.com>
2024-09-02 20:13:29 +0800

    tweak formatTimeDuration slice

--- packages/next-common/utils/viewfuncs/formatTimeDuration.js
+++ packages/next-common/utils/viewfuncs/formatTimeDuration.js
@@ -5,7 +5,10 @@ import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
 
-export function formatTimeDuration(input) {
+export function formatTimeDuration(
+  input,
+  { slice = 2, withUnitSpace = false } = {},
+) {
   const duration = dayjs.duration(input);
 
   const yr = duration.years();
@@ -15,20 +18,33 @@ export function formatTimeDuration(input) {
   const min = duration.minutes();
   const s = duration.seconds();
 
-  let slice = 1;
-  if (yr <= 0 && mo <= 0 && d < 3) {
-    slice = 2;
+  if (yr < 1 && mo < 1 && d < 3) {
+    slice = Math.max(slice, 2);
   }
 
   return [
-    yr > 0 ? `${yr}yr${yr > 1 ? "s" : ""}` : null,
-    mo > 0 ? `${mo}mo${mo > 1 ? "s" : ""}` : null,
-    d > 0 ? `${d}d` : null,
-    hr > 0 ? `${hr}hr${hr > 1 ? "s" : ""}` : null,
-    min > 0 ? `${min}min${min > 1 ? "s" : ""}` : null,
-    s > 0 ? `${s}s` : null,
+    buildTimeUnit(yr, "yr", { withUnitSpace }),
+    buildTimeUnit(mo, "mo", { withUnitSpace }),
+    buildTimeUnit(d, "d", { withUnitSpace, withPluralSuffix: false }),
+    buildTimeUnit(hr, "hr", { withUnitSpace }),
+    buildTimeUnit(min, "min", { withUnitSpace }),
+    buildTimeUnit(s, "s", { withUnitSpace, withPluralSuffix: false }),
   ]
     .filter(Boolean)
     .slice(0, slice)
     .join(" ");
+}
+
+function buildTimeUnit(
+  value,
+  unit,
+  { withPluralSuffix = true, withUnitSpace = false } = {},
+) {
+  if (value <= 0) {
+    return null;
+  }
+
+  return `${value}${withUnitSpace ? " " : ""}${unit}${
+    withPluralSuffix && value > 1 ? "s" : ""
+  }`;
 }
