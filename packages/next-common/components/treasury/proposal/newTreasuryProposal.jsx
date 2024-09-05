9460dc514662f24ebd4bec79cea21126eadbfe58
leocs2417 <leochen.sat24@gmail.com>
2024-09-05 17:24:57 +0800

    feat: optimize NewTreasuryProposal

--- packages/next-common/components/treasury/proposal/newTreasuryProposal.jsx
+++ packages/next-common/components/treasury/proposal/newTreasuryProposal.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import PrimaryButton from "next-common/lib/button/primary";
 import NewTreasuryProposalButton from "./newTreasuryProposalButton";
 import { getEventData } from "next-common/utils/sendTransaction";
 import {
   useProposalsSection,
   useProposalsParams,
 } from "next-common/context/treasury/proposals";
+
+const SystemPlus = dynamic(() => import("@osn/icons/subsquare/SystemPlus"));
+
+const NewTreasuryProposalPopup = dynamic(() => import("./popup"));
+
+function NewTreasuryProposalButton({ treasuryPallet, onInBlock }) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <>
+      <PrimaryButton
+        size="small"
+        icon={
+          <SystemPlus className="w-4 h-4 [&_path]:fill-textPrimaryContrast" />
+        }
+        onClick={() => setShowPopup(true)}
+      >
+        New Proposal
+      </PrimaryButton>
+      {showPopup && (
+        <NewTreasuryProposalPopup
+          treasuryPallet={treasuryPallet}
+          onInBlock={onInBlock}
+          onClose={() => setShowPopup(false)}
+        />
+      )}
+    </>
+  );
+}
 
 export default function NewTreasuryProposal() {
   const router = useRouter();
