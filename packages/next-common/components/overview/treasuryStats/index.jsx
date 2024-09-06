1b04683e854b9216f1cf8139da11bc217b55c62c
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-06 14:58:58 +0800

    Fix treasury data on overview page, #4654

--- packages/next-common/components/overview/treasuryStats/index.jsx
+++ packages/next-common/components/overview/treasuryStats/index.jsx
@@ -1,6 +1,7 @@
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 import TreasurySummary from "next-common/components/summary/treasurySummary";
+import { TreasuryProvider } from "next-common/context/treasury";
 
 export default function TreasuryStats() {
   return (
@@ -8,7 +9,9 @@ export default function TreasuryStats() {
       <TitleContainer className="mb-4">Treasury Stats</TitleContainer>
 
       <SecondaryCard>
-        <TreasurySummary />
+        <TreasuryProvider>
+          <TreasurySummary />
+        </TreasuryProvider>
       </SecondaryCard>
     </div>
   );
--- packages/next-common/context/treasury/index.js
+++ packages/next-common/context/treasury/index.js
@@ -1,8 +1,9 @@
 import { createContext, useContext } from "react";
 
 const TreasuryContext = createContext();
+const defaultPallet = "treasury";
 
-export function TreasuryProvider({ pallet = "treasury", children }) {
+export function TreasuryProvider({ pallet = defaultPallet, children }) {
   return (
     <TreasuryContext.Provider value={{ pallet }}>
       {children}
@@ -12,6 +13,10 @@ export function TreasuryProvider({ pallet = "treasury", children }) {
 
 export function useTreasuryPallet() {
   const { pallet } = useContext(TreasuryContext) || {};
+  if (!pallet) {
+    throw new Error(`No treasury provider set when useTreasuryPallet`);
+  }
+
   return pallet;
 }
 
