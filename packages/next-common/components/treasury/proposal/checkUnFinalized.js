c0c233c7b9cd3f5cbe0eb496ff2366dab134fd09
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-06 18:33:26 +0800

    Fix treasury proposal api for un-finalized check, #4654

--- packages/next-common/components/treasury/proposal/checkUnFinalized.js
+++ packages/next-common/components/treasury/proposal/checkUnFinalized.js
@@ -2,13 +2,15 @@ import React from "react";
 import nextApi from "next-common/services/nextApi";
 import CheckUnFinalizedBase from "next-common/components/checkUnFinalizedBase";
 import { useTreasuryPallet } from "next-common/context/treasury";
+import { kebabCase } from "lodash-es";
 
 export default function CheckUnFinalized({ id }) {
   const pallet = useTreasuryPallet();
+
   return (
     <CheckUnFinalizedBase
       onChainDataFetcher={async (api) => api.query[pallet]?.proposals(id)}
-      serverPostFetcher={() => nextApi.fetch(`${pallet}/proposals/${id}`)}
+      serverPostFetcher={() => nextApi.fetch(`${kebabCase(pallet)}/proposals/${id}`)}
     />
   );
 }
