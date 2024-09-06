d886b152ed0caa6df2cc07c669e22e8cc4811a80
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-06 12:31:09 +0800

    Fix treasury next burn and spend period, #4654

--- packages/next-common/utils/hooks/useTreasuryBurn.js
+++ packages/next-common/utils/hooks/useTreasuryBurn.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { useMountedState } from "react-use";
 import BigNumber from "bignumber.js";
+import { useTreasuryPallet } from "next-common/context/treasury";
 
 export default function useTreasuryBurn(api, free) {
   const [burn, setBurn] = useState("0");
   const isMounted = useMountedState();
+  const pallet = useTreasuryPallet();
 
   useEffect(() => {
-    if (api && api.consts.treasury && isMounted()) {
+    if (api && api.consts[pallet] && isMounted()) {
       const burn = api.consts.treasury.burn;
       const toBurn = new BigNumber(burn.toNumber())
         .dividedBy(Math.pow(10, 6))
