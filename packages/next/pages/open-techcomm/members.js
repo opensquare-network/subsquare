c1917cb4a369f6375ee153ebedeede4c8545be44
2nthony <ant.hu@outlook.com>
2024-09-09 11:29:46 +0800

    open tc

--- packages/next/pages/open-techcomm/members.js
+++ packages/next/pages/open-techcomm/members.js
@@ -2,22 +2,25 @@ import MembersNoElections from "components/council/membersNoElections";
 import { detailPageCategory } from "next-common/utils/consts/business/category";
 import ListLayout from "next-common/components/layout/ListLayout";
 import { getServerSidePropsWithTracks } from "next-common/services/serverSide";
+import CollectiveProvider from "next-common/context/collective";
 
 export default function Members() {
   const category = "Open Tech. Comm. Members";
   const seoInfo = { title: category, desc: category };
 
   return (
-    <ListLayout
-      seoInfo={seoInfo}
-      title={category}
-      description="Open technical committee members"
-    >
-      <MembersNoElections
-        category={category}
-        type={detailPageCategory.OPEN_TECH_COMM_PROPOSAL}
-      />
-    </ListLayout>
+    <CollectiveProvider pallet="openTechCommitteeCollective">
+      <ListLayout
+        seoInfo={seoInfo}
+        title={category}
+        description="Open technical committee members"
+      >
+        <MembersNoElections
+          category={category}
+          type={detailPageCategory.OPEN_TECH_COMM_PROPOSAL}
+        />
+      </ListLayout>
+    </CollectiveProvider>
   );
 }
 
