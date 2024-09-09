20f3be98ec8fdcda61d970a95efad6705ce735c7
chaojun <chaojun@opensquare.network>
2024-09-09 15:49:04 +0800

    Add fellowship spend info, #4679

--- packages/next-common/components/detail/fellowship/fellowshipReferendumTreasurySpendNavigation.js
+++ packages/next-common/components/detail/fellowship/fellowshipReferendumTreasurySpendNavigation.js
@@ -1,6 +1,6 @@
+import React from "react";
 import { useOnchainData } from "next-common/context/post";
 import { isNil } from "lodash-es";
-import React from "react";
 import Link from "next/link";
 import { NavigationWrapper } from "next-common/components/detail/navigation/navigators";
 import TriangleRight from "next-common/assets/imgs/icons/arrow-triangle-right.svg";
