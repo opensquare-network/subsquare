058ab36743af0fc92b9367a8c2efa77b872c291e
2nthony <ant.hu@outlook.com>
2024-09-09 15:41:56 +0800

    clean

--- packages/next-common/hooks/useCouncilName.js
+++
@@ -1,9 +0,0 @@
-import { useChain } from "../context/chain";
-import { usePageProperties } from "next-common/context/page";
-import toApiCouncil from "next-common/utils/toApiCouncil";
-
-export default function useCouncilName(detailType) {
-  const chain = useChain();
-  const { type } = usePageProperties();
-  return toApiCouncil(chain, detailType || type);
-}
