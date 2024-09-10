4021bc065d6eda6b0e9b7a379badd90218d82078
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-10 18:06:41 +0800

    Init decision warning component, #4664

---
+++ packages/next/components/gov2/sidebar/status/warning.js
@@ -0,0 +1,43 @@
+import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
+import { useSelector } from "react-redux";
+import { useDecidingSince } from "next-common/context/post/gov2/referendum";
+import { useDecision } from "next-common/context/post/gov2/track";
+import { useDecisionBlocks, useDecisionEnd } from "./useDecisionPercentage";
+import { useApprovalThreshold } from "next-common/context/post/gov2/threshold";
+import { useReferendumTally } from "next-common/hooks/referenda/useReferendumInfo";
+import BigNumber from "bignumber.js";
+import { toPrecision } from "next-common/utils";
+import { useDecimals } from "next-common/context/chain";
+
+function WarningText() {
+  const approvalThreshold = useApprovalThreshold();
+  const tally = useReferendumTally();
+  const decimals = useDecimals();
+  let needNayVotes = BigNumber(tally.ayes).div(approvalThreshold).minus(tally.nays).minus(tally.nays).toString();
+  if (BigNumber(needNayVotes).lt(0)) {
+    needNayVotes = 0;
+  }
+
+  // fixme: normalize nay votes number
+  return <div className="">
+    Will be rejected immediately with {toPrecision(needNayVotes, decimals, 2)} nay votes.
+  </div>
+}
+
+export default function DecisionWarning() {
+  const latestHeight = useSelector(chainOrScanHeightSelector);
+  const decidingSince = useDecidingSince();
+  const decisionPeriod = useDecision();
+  const decisionBlocks = useDecisionBlocks();
+  const end = useDecisionEnd();
+
+  if (
+    decisionBlocks <= decisionPeriod ||
+    latestHeight <= decidingSince + decisionPeriod ||
+    latestHeight >= end
+  ) {
+    return null;
+  }
+
+  return <WarningText />;
+}
