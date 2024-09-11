d5fba3e8e36da184fe59c4aaa1a84c45c23a6dee
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-11 11:44:43 +0800

    Simplify BountyClaim, #4667

--- packages/next-common/components/treasury/bounty/claim/index.jsx
+++ packages/next-common/components/treasury/bounty/claim/index.jsx
@@ -1,32 +1,20 @@
 import { usePostState } from "next-common/context/post";
 import ClaimedInfo from "./ClaimedInfo";
 import Claim from "./claim";
-import { useOnchainData } from "next-common/context/post";
-import { useChainSettings } from "next-common/context/chain";
-import useSubStorage from "next-common/hooks/common/useSubStorage";
 
-export default function BountyClaim() {
+function GuardClaimed({ children }) {
   const state = usePostState();
-  const { hideActionButtons } = useChainSettings();
-  const onChain = useOnchainData();
-
-  const { bountyIndex } = onChain;
-  const { loading, result: onChainStorage } = useSubStorage("bounties", "bounties", [bountyIndex]);
-
-  if (loading || !onChainStorage?.isSome) {
-    return null;
-  }
-
-  if (
-    !["PendingPayout", "Claimed"].includes(onChain?.state?.state) ||
-    hideActionButtons
-  ) {
-    return null;
-  }
-
   if ("Claimed" === state) {
     return <ClaimedInfo />;
+  } else {
+    return children;
   }
+}
 
-  return <Claim />;
+export default function BountyClaim() {
+  return (
+    <GuardClaimed>
+      <Claim />
+    </GuardClaimed>
+  );
 }
