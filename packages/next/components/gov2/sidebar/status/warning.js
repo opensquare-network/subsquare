ee045893032c445c7c278ccf5208bd9c19d43903
leocs2417 <leochen.sat24@gmail.com>
2024-09-11 11:39:07 +0800

    normalize nay votes number

--- packages/next/components/gov2/sidebar/status/warning.js
+++ packages/next/components/gov2/sidebar/status/warning.js
@@ -8,20 +8,39 @@ import { useReferendumTally } from "next-common/hooks/referenda/useReferendumInf
 import BigNumber from "bignumber.js";
 import { toPrecision } from "next-common/utils";
 import { useDecimals } from "next-common/context/chain";
+import ValueDisplay from "next-common/components/valueDisplay";
 
 function WarningText() {
   const approvalThreshold = useApprovalThreshold();
   const tally = useReferendumTally();
   const decimals = useDecimals();
-  let needNayVotes = BigNumber(tally.ayes).div(approvalThreshold).minus(tally.nays).minus(tally.nays).toString();
+  let needNayVotes = BigNumber(tally.ayes)
+    .div(approvalThreshold)
+    .minus(tally.nays)
+    .minus(tally.nays)
+    .toString();
   if (BigNumber(needNayVotes).lt(0)) {
     needNayVotes = 0;
   }
 
-  // fixme: normalize nay votes number
-  return <div className="flex text14Medium text-orange500 bg-orange100 rounded-[8px] py-2.5 px-4">
-    Will be rejected immediately with over {toPrecision(needNayVotes, decimals, 2)} nay votes.
-  </div>;
+  if (isNaN(needNayVotes)) {
+    return null;
+  }
+
+  return (
+    <div className="inline-flex text14Medium text-orange500 bg-orange100 rounded-[8px] py-2.5 px-4">
+      <div className="inline">
+        Will be rejected immediately with over&nbsp;
+        <span className="inline hover:cursor-pointer">
+          <ValueDisplay
+            value={toPrecision(needNayVotes, decimals, 2)}
+            symbol={""}
+          />
+        </span>
+        &nbsp;nay votes.
+      </div>
+    </div>
+  );
 }
 
 export default function DecisionWarning() {
