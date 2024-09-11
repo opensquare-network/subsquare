8a5b3ac0c2b229800fb51cc360859374fe724700
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-11 12:39:10 +0800

    Refactor BountyClaim, #4667

--- packages/next-common/components/treasury/bounty/claim/index.jsx
+++ packages/next-common/components/treasury/bounty/claim/index.jsx
@@ -2,19 +2,11 @@ import { usePostState } from "next-common/context/post";
 import ClaimedInfo from "./ClaimedInfo";
 import Claim from "./claim";
 
-function GuardClaimed({ children }) {
+export default function BountyClaim() {
   const state = usePostState();
   if ("Claimed" === state) {
     return <ClaimedInfo />;
-  } else {
-    return children;
   }
-}
 
-export default function BountyClaim() {
-  return (
-    <GuardClaimed>
-      <Claim />
-    </GuardClaimed>
-  );
+  return <Claim />;
 }
