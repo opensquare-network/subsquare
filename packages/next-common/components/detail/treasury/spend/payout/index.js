848ea42012021d571b2c5731fdb3548fe1272fc1
Yongfeng LI <wliyongfeng@gmail.com>
2024-09-12 15:37:42 +0800

    Show claim and beneficiary info on sidebar, #4702

--- packages/next-common/components/detail/treasury/spend/payout/index.js
+++ packages/next-common/components/detail/treasury/spend/payout/index.js
@@ -1,12 +1,14 @@
 import { RightBarWrapper } from "next-common/components/layout/sidebar/rightBarWrapper";
 import Meta from "next-common/components/detail/treasury/spend/payout/meta";
 import TreasurySpendPay from "next-common/components/detail/treasury/spend/payout/pay";
+import PayHint from "next-common/components/detail/treasury/spend/payout/paidInfo";
 
 export default function TreasurySpendPayout() {
   return (
     <RightBarWrapper>
       <Meta />
       <TreasurySpendPay />
+      <PayHint />
     </RightBarWrapper>
   );
 }
