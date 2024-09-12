9f0f9cc2884defdd456a2d22730918bb1969e91c
2nthony <ant.hu@outlook.com>
2024-09-12 15:46:55 +0800

    only collective council member can create proposal

--- packages/next-common/components/summary/newCouncilMotionProposalButton/index.jsx
+++ packages/next-common/components/summary/newCouncilMotionProposalButton/index.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import NewButton from "../newProposalButton/newButton";
 import NewCouncilMotionProposalPopup from "./newCouncilMotionProposalPopup";
+import Tooltip from "next-common/components/tooltip";
+import useIsCouncilMember from "next-common/utils/hooks/useIsCouncilMember";
 
 export default function NewCouncilMotionProposalButton() {
   const [showPopup, setShowPopup] = useState(false);
+  const isCouncilMember = useIsCouncilMember();
 
   return (
     <>
-      <NewButton setShowPopup={setShowPopup} />
+      <Tooltip
+        content={
+          !isCouncilMember ? "Only council members can create proposals" : null
+        }
+      >
+        <NewButton setShowPopup={setShowPopup} disabled={!isCouncilMember} />
+      </Tooltip>
 
       {showPopup && (
         <NewCouncilMotionProposalPopup
