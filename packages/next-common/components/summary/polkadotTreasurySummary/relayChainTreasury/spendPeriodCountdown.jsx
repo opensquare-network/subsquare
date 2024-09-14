83dbd441a434b41bbbafc7b17a777c6245e24ec3
leocs2417 <leochen.sat24@gmail.com>
2024-09-14 16:53:19 +0800

    countdown percentage precision, #4686

--- packages/next-common/components/summary/polkadotTreasurySummary/relayChainTreasury/spendPeriodCountdown.jsx
+++ packages/next-common/components/summary/polkadotTreasurySummary/relayChainTreasury/spendPeriodCountdown.jsx
@@ -1,12 +1,15 @@
 import Tooltip from "next-common/components/tooltip";
 import CountDown from "next-common/components/_CountDown";
+import { toPercentage } from "next-common/utils";
 
 export default function SpendPeriodCountdown({ summary }) {
+  const progress = summary?.progress ?? 0;
+  const percentage = toPercentage(progress / 100, 2);
   return (
-    <Tooltip content={`${summary?.progress ?? 0}%`} className={"z-10"}>
+    <Tooltip content={`${percentage}%`} className={"z-10"}>
       <span className="ml-2">
         <CountDown
-          numerator={summary?.progress ?? 0}
+          numerator={percentage}
           denominator={100}
           backgroundColor="var(--theme100)"
           foregroundColor="var(--theme500)"
