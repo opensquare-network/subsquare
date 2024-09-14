590b3d4517715ed417cce6b26a505e73b946ca12
leocs2417 <leochen.sat24@gmail.com>
2024-09-14 11:43:35 +0800

    modify useSubStorage optionsApi func, #4686

--- packages/next-common/components/summary/polkadotTreasurySummary/common/useSubscribeAssetHubAssets.js
+++ packages/next-common/components/summary/polkadotTreasurySummary/common/useSubscribeAssetHubAssets.js
@@ -1,36 +1,27 @@
 import { useState, useEffect } from "react";
 import { useAssetHubApi } from "next-common/context/assetHub";
 import { toPrecision } from "next-common/utils";
+import useSubStorage from "next-common/hooks/common/useSubStorage";
 
 export function useSubscribeAssetHubAssets(assetId, address) {
   const api = useAssetHubApi();
   const [free, setFree] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { loading, result } = useSubStorage(
+    "assets",
+    "account",
+    [assetId, address],
+    { api },
+  );
+
   useEffect(() => {
-    if (!api || !address) {
-      return;
-    }
+    if (loading) return;
 
-    const fetchAssetInfo = async () => {
-      setIsLoading(true);
-      try {
-        const data = await api.query.assets.account(assetId, address);
-        const freeAsset = data?.toJSON()?.balance || 0;
-        setFree(toPrecision(freeAsset, 6));
-      } catch (error) {
-        console.error("Error fetching asset info:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchAssetInfo();
-
-    const interval = setInterval(fetchAssetInfo, 60000);
-
-    return () => clearInterval(interval);
-  }, [api, assetId, address]);
+    const freeAsset = result?.toJSON()?.balance || 0;
+    setFree(toPrecision(freeAsset, 6));
+    setIsLoading(loading);
+  }, [loading, result]);
 
   return { free, isLoading };
 }
--- packages/next-common/components/summary/polkadotTreasurySummary/common/useSubscribeAssetHubTreasuryFree.js
+++ packages/next-common/components/summary/polkadotTreasurySummary/common/useSubscribeAssetHubTreasuryFree.js
@@ -1,35 +1,23 @@
 import { useState, useEffect } from "react";
 import { useAssetHubApi } from "next-common/context/assetHub";
+import useSubStorage from "next-common/hooks/common/useSubStorage";
 
 export function useSubscribeFellowshipTreasuryFree(address) {
   const api = useAssetHubApi();
   const [free, setFree] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
+  const { loading, result } = useSubStorage("system", "account", [address], {
+    api,
+  });
+
   useEffect(() => {
-    if (!api || !address) {
-      return;
-    }
+    if (loading) return;
 
-    const fetchAssetInfo = async () => {
-      setIsLoading(true);
-      try {
-        const res = await api.query.system.account(address);
-        const freeData = res?.data?.free?.toJSON() || 0;
-        setFree(freeData);
-      } catch (error) {
-        console.error("Error fetching asset info:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchAssetInfo();
-
-    const interval = setInterval(fetchAssetInfo, 60000);
-
-    return () => clearInterval(interval);
-  }, [api, address]);
+    const freeData = result?.data?.free?.toJSON() || 0;
+    setFree(freeData);
+    setIsLoading(loading);
+  }, [loading, result]);
 
   return { free, isLoading };
 }
