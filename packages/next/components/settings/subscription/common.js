b6d2c73f2400b61e530f76e5ccb26c3bd3129b63
2nthony <ant.hu@outlook.com>
2024-09-18 17:41:11 +0800

    fix

--- packages/next/components/settings/subscription/common.js
+++ packages/next/components/settings/subscription/common.js
@@ -1,9 +1,5 @@
 export function checkSubMenu(menus, menuName) {
-  const chain = process.env.NEXT_PUBLIC_CHAIN;
   const menu = menus?.find((item) => item.name === menuName);
-  const isMenuActive =
-    menu &&
-    !menu.excludeToChains?.includes(chain) &&
-    !menu.archivedToChains?.includes(chain);
+  const isMenuActive = menu;
   return { isMenuActive, menu };
 }
