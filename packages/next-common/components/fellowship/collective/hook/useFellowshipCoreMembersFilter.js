7a7766404fc049c9bcf5b243151d0ae9325a0a4e
leocs2417 <leochen.sat24@gmail.com>
2024-09-18 18:30:48 +0800

    useFellowshipCoreMembersFilter rank, #4718

--- packages/next-common/components/fellowship/collective/hook/useFellowshipCoreMembersFilter.js
+++ packages/next-common/components/fellowship/collective/hook/useFellowshipCoreMembersFilter.js
@@ -2,19 +2,29 @@ import { isNil } from "lodash-es";
 import { useMemo } from "react";
 import useFellowshipCoreOnlySwitch from "./useFellowshipCoreOnlySwitch";
 import useSubCoreCollectivesMember from "next-common/hooks/collectives/useSubCoreCollectivesMember";
+import usePeriodSelect, {
+  DemotionPeriodAboutToExpire,
+  DemotionPeriodExpired,
+  Promotable,
+} from "next-common/components/pages/fellowship/usePeriodSelect";
+import usePeriodFilterFn from "next-common/components/pages/fellowship/usePeriodFilterFn";
+import { useRouterRankFilter } from "next-common/hooks/fellowship/useRankFilter";
 
-function isFellowshipCoreOnly(item) {
-  const { member: memberStatus, isLoading } = useSubCoreCollectivesMember(
-    item.address,
-  );
-  return !isNil(memberStatus) && !isLoading;
+function getMemberStatus(item) {
+  const { member, isLoading } = useSubCoreCollectivesMember(item.address);
+  return {
+    status: member,
+    isLoading,
+  };
 }
 
 function handleFilterMembers(members) {
   return members.map((item) => {
+    const { status, isLoading } = getMemberStatus(item);
     return {
       ...item,
-      isFellowshipOnly: isFellowshipCoreOnly(item),
+      status,
+      isFellowshipOnly: !isNil(status) && !isLoading,
     };
   });
 }
@@ -24,21 +34,55 @@ export default function useFellowshipCoreMembersFilter(members) {
     useFellowshipCoreOnlySwitch();
   const membersWithStatus = handleFilterMembers(members);
 
+  const { periodFilter, component: PeriodFilterComponent } = usePeriodSelect();
+
+  const ranks = [...new Set(membersWithStatus.map((m) => m.rank))];
+  const { rank, component: RankFilterComponent } = useRouterRankFilter(ranks);
+  // TODO: need params
+  // const {
+  //   filterDemotionAboutToExpireFn,
+  //   filterDemotionExpiredFn,
+  //   filterPromotableFn,
+  // } = usePeriodFilterFn();
+
   const filteredMembers = useMemo(() => {
     if (isNil(membersWithStatus)) return;
 
     let filteredMembers = membersWithStatus;
+
+    // if (periodFilter === DemotionPeriodAboutToExpire) {
+    //   filteredMembers = filterDemotionAboutToExpireFn(filteredMembers);
+    // } else if (periodFilter === DemotionPeriodExpired) {
+    //   filteredMembers = filterDemotionExpiredFn(filteredMembers);
+    // } else if (periodFilter === Promotable) {
+    //   filteredMembers = filterPromotableFn(filteredMembers);
+    // }
 
     if (isFellowshipCoreOnly) {
       filteredMembers = filteredMembers.filter((member) => {
         return member.isFellowshipOnly;
       });
     }
-    return filteredMembers;
+
+    if (isNil(rank)) {
+      return filteredMembers;
+    } else {
+      return filteredMembers.filter((m) => m.rank === rank);
+    }
   }, [membersWithStatus, isFellowshipCoreOnly]);
+
+  console.log(":::::membersWithStatus", membersWithStatus);
+
+  const component = (
+    <div className="flex flex-wrap max-sm:flex-col sm:items-center gap-[12px] max-sm:gap-[8px] ml-[24px]">
+      {FellowshipCoreSwitch}
+      {PeriodFilterComponent}
+      {RankFilterComponent}
+    </div>
+  );
 
   return {
     filteredMembers,
-    component: FellowshipCoreSwitch,
+    component,
   };
 }
--- packages/next/pages/fellowship/members.js
+++ packages/next/pages/fellowship/members.js
@@ -1,55 +1,38 @@
 import ListLayout from "next-common/components/layout/ListLayout";
 import { usePageProps } from "next-common/context/page";
 import FellowshipCollectiveMembers from "next-common/components/fellowship/collective/list";
-import useRankFilter from "next-common/hooks/fellowship/useRankFilter";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
-import { useMemo } from "react";
-import { isNil } from "lodash-es";
 import getFellowshipMembersServerSideProps from "next-common/services/serverSide/fellowship/members";
 import useFellowshipCoreMembersFilter from "next-common/components/fellowship/collective/hook/useFellowshipCoreMembersFilter";
+import CollectivesProvider from "next-common/context/collectives/collectives";
 
-// TODO: move into next-common/components/
 export default function MembersPage() {
   const { fellowshipMembers } = usePageProps();
   const category = "Fellowship Members";
   const seoInfo = { title: category, desc: category };
-  const ranks = [...new Set(fellowshipMembers.map((m) => m.rank))];
-  const { rank, component } = useRankFilter(ranks);
 
   const { filteredMembers, component: FilterComponent } =
     useFellowshipCoreMembersFilter(fellowshipMembers);
 
-  const membersCount = filteredMembers?.length;
-
-  // const filteredMembers = useMemo(() => {
-  //   // TODO: read status by isFellowshipOnly
-  //   if (true) {
-  //     return membersWithStatus.filter((m) => m.isFellowshipOnly);
-  //   }
-
-  //   if (isNil(rank)) {
-  //     return membersWithStatus;
-  //   }
-
-  //   return membersWithStatus.filter((m) => m.rank === rank);
-  // }, [membersWithStatus, rank]);
+  const membersCount = filteredMembers?.length || 0;
 
   return (
-    <ListLayout seoInfo={seoInfo} title={category}>
-      <div className="flex flex-col gap-y-4">
-        <TitleContainer>
-          <span>
-            List
-            <span className="text-textTertiary text14Medium ml-1">
-              {membersCount}
+    <CollectivesProvider section="fellowship">
+      <ListLayout seoInfo={seoInfo} title={category}>
+        <div className="flex flex-col gap-y-4">
+          <TitleContainer>
+            <span>
+              List
+              <span className="text-textTertiary text14Medium ml-1">
+                {membersCount}
+              </span>
             </span>
-          </span>
-          {FilterComponent}
-          {component}
-        </TitleContainer>
-        <FellowshipCollectiveMembers members={filteredMembers} />
-      </div>
-    </ListLayout>
+            {FilterComponent}
+          </TitleContainer>
+          <FellowshipCollectiveMembers members={filteredMembers} />
+        </div>
+      </ListLayout>
+    </CollectivesProvider>
   );
 }
 
