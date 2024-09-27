d6180b45924e11a109735336c525b84dc8e1385c
Chaojun Huang <chaojun@opensquare.network>
2024-09-27 14:52:01 +0800

    Implement collectives fellowship referenda statistics, #4192 (#4788)
    * Implement collectives fellowship referenda statistics, #4192
    
    * refactor, #4192
    
    * refactor, #4192
    
    * fix, #4192
    
    * clean unused param, #4192

--- packages/next-common/components/statistics/referenda/summary/index.js
+++ packages/next-common/components/statistics/referenda/summary/index.js
@@ -2,6 +2,7 @@ import { StatisticsTitle } from "../../styled";
 import { DoughnutChart } from "./doughnetChart";
 import TrackList from "./trackList";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
+import { ReferendaTrackColors } from "./colors";
 
 export default function ReferendaSummary({ summary }) {
   return (
@@ -9,7 +10,10 @@ export default function ReferendaSummary({ summary }) {
       <StatisticsTitle>Referendum Count</StatisticsTitle>
       <div className="flex flex-wrap gap-6">
         <TrackList trackReferendaCounts={summary?.trackReferendaCounts} />
-        <DoughnutChart trackReferendaCounts={summary?.trackReferendaCounts} />
+        <DoughnutChart
+          trackReferendaCounts={summary?.trackReferendaCounts}
+          trackColors={ReferendaTrackColors}
+        />
       </div>
     </SecondaryCard>
   );
