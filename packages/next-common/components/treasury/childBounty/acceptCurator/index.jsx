0e20a6b98a534a62c28d09a1e3cc198859ceec9d
2nthony <ant.hu@outlook.com>
2024-09-30 15:31:57 +0800

    fix, only curator can see this button

--- packages/next-common/components/treasury/childBounty/acceptCurator/index.jsx
+++ packages/next-common/components/treasury/childBounty/acceptCurator/index.jsx
@@ -1,12 +1,16 @@
 import PrimaryButton from "next-common/lib/button/primary";
 import { useAcceptCuratorPopup } from "./useAcceptCuratorPopup";
-import { usePostState } from "next-common/context/post";
+import { useOnchainData, usePostState } from "next-common/context/post";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
 
 export default function ChildBountyAcceptCurator() {
+  const address = useRealAddress();
+  const onchainData = useOnchainData();
+  const { curator } = onchainData || {};
   const postState = usePostState();
   const { showPopup, component } = useAcceptCuratorPopup();
 
-  if (postState !== "CuratorProposed") {
+  if (curator !== address && postState !== "CuratorProposed") {
     return null;
   }
 
