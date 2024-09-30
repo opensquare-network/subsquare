680ed8e84165e3500b0c3d9370d9c6aa6889277b
2nthony <ant.hu@outlook.com>
2024-09-30 17:13:36 +0800

    fix: if not the curator, disabled and show tooltip

--- packages/next-common/components/treasury/childBounty/acceptCurator/index.jsx
+++ packages/next-common/components/treasury/childBounty/acceptCurator/index.jsx
@@ -3,28 +3,50 @@ import useSubStorage from "next-common/hooks/common/useSubStorage";
 import PrimaryButton from "next-common/lib/button/primary";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useAcceptCuratorPopup } from "./useAcceptCuratorPopup";
+import Tooltip from "next-common/components/tooltip";
+import { isNil } from "lodash-es";
+import { isSameAddress } from "next-common/utils";
+import AddressUser from "next-common/components/user/addressUser";
 
 export default function ChildBountyAcceptCurator() {
   const address = useRealAddress();
   const onchainData = useOnchainData();
   const { parentBountyId, index: childBountyId } = onchainData;
+  const { showPopup, component } = useAcceptCuratorPopup();
+
   const { result, loading } = useSubStorage("childBounties", "childBounties", [
     parentBountyId,
     childBountyId,
   ]);
   const status = result?.toJSON() || {};
-  const curatorProposed = status.curatorProposed || {};
-  const { curator } = curatorProposed;
 
-  const { showPopup, component } = useAcceptCuratorPopup();
-
-  if (loading || curator !== address) {
+  if (loading || isNil(status.curatorProposed)) {
     return null;
   }
 
+  const curatorProposed = status.curatorProposed || {};
+  const { curator } = curatorProposed;
+
+  const disabled = !isSameAddress(curator, address);
+
   return (
-    <div>
+    <Tooltip
+      content={
+        disabled ? (
+          <div className="flex items-center gap-x-2">
+            Only{" "}
+            <AddressUser
+              addressClassName="!text-textPrimaryContrast"
+              add={curator}
+              noEvent
+            />
+            can accept curator
+          </div>
+        ) : null
+      }
+    >
       <PrimaryButton
+        disabled={disabled}
         className="w-full"
         onClick={() => {
           showPopup();
@@ -34,6 +56,6 @@ export default function ChildBountyAcceptCurator() {
       </PrimaryButton>
 
       {component}
-    </div>
+    </Tooltip>
   );
 }
