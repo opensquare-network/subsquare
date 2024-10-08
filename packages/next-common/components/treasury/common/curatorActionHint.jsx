3bea2279fc9ba5868f167a56765948c6b08f9b09
2nthony <ant.hu@outlook.com>
2024-10-08 18:31:01 +0800

    rename

--- packages/next-common/components/treasury/common/curatorActionHint.jsx
+++ packages/next-common/components/treasury/common/curatorActionHint.jsx
@@ -2,7 +2,7 @@ import ExternalLink from "next-common/components/externalLink";
 import { cn } from "next-common/utils";
 import { mimir } from "next-common/utils/consts/connect";
 
-export default function CuratorActionHint({ className = "" }) {
+export default function BountyCuratorActionHint({ className = "" }) {
   return (
     <div className={cn("text14Medium text-textTertiary", className)}>
       For multisig curators, we suggest to do actions with{" "}
