f84fad4685aa8357056e4aa1ac825165430a9be4
chaojun <chaojun@opensquare.network>
2024-10-09 14:45:52 +0800

    Spend beneficiary shows deleted account, #4817

---
+++ packages/next/components/gov2/business/beneficiaryJsonViewButton.jsx
@@ -0,0 +1,41 @@
+import { InfoDocs } from "@osn/icons/subsquare";
+import Tooltip from "next-common/components/tooltip";
+import { useOnchainData } from "next-common/context/post";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import Popup from "next-common/components/popup/wrapper/Popup";
+
+const JsonView = dynamic(() => import("next-common/components/jsonView"), {
+  ssr: false,
+});
+
+function BeneficiaryJsonView({ onClose }) {
+  const { proposal } = useOnchainData();
+  const beneficiary = proposal?.call?.args?.find(
+    (item) => item.name === "beneficiary",
+  );
+
+  return (
+    <Popup title="Beneficiary Detail" onClose={onClose} className="w-[650px]">
+      <JsonView src={beneficiary} />
+    </Popup>
+  );
+}
+
+export default function BeneficiaryJsonViewButton() {
+  const [showDetail, setShowDetail] = useState(false);
+
+  return (
+    <>
+      <Tooltip content="Beneficiary Detail">
+        <InfoDocs
+          className="cursor-pointer w-[16px] h-[16px]"
+          onClick={() => setShowDetail(true)}
+        />
+      </Tooltip>
+      {showDetail && (
+        <BeneficiaryJsonView onClose={() => setShowDetail(false)} />
+      )}
+    </>
+  );
+}
