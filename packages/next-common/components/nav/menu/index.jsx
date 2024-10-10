55602a133d01c4465215d9aebd56b19acb4cd941
2nthony <ant.hu@outlook.com>
2024-10-10 10:56:35 +0800

    refactor

--- packages/next-common/components/nav/menu/index.jsx
+++ packages/next-common/components/nav/menu/index.jsx
@@ -1,45 +1,22 @@
 import { ArrowCircleLeft } from "@osn/icons/subsquare";
 import { useNavCollapsed } from "next-common/context/nav";
 import { usePageProps } from "next-common/context/page";
-import { getNavMenu } from "next-common/utils/consts/menu";
-import { createStateContext } from "react-use";
-import NavMenuDivider from "../divider";
+import { getMainMenu } from "next-common/utils/consts/menu";
+import { createGlobalState } from "react-use";
 import NavMenuEntrance from "./entrance";
-import NavMenuItem from "./item";
 
-const [useNavMenuView, NavMenuViewProvider] = createStateContext({
+export const useNavMenuView = createGlobalState({
   view: "main",
   menu: null,
 });
 
-export { useNavMenuView };
-
 export default function NavMenu() {
-  return (
-    <NavMenuViewProvider>
-      <NavMenuImpl />
-    </NavMenuViewProvider>
-  );
-}
-
-function NavMenuImpl() {
   const [collapsed] = useNavCollapsed();
-
-  const [{ view }] = useNavMenuView();
-
-  return (
-    <>
-      {view === "main" && <MainMenu collapsed={collapsed} />}
-      {view === "subspace" && <SubSpaceMenu collapsed={collapsed} />}
-    </>
-  );
-}
-
-function MainMenu({ collapsed }) {
+  const [navMenuView, setNavMenuView] = useNavMenuView();
   const { tracks, fellowshipTracks, summary, detail, ambassadorTracks } =
     usePageProps();
 
-  const menu = getNavMenu({
+  const mainMenu = getMainMenu({
     tracks,
     fellowshipTracks,
     ambassadorTracks,
@@ -47,40 +24,30 @@ function MainMenu({ collapsed }) {
     currentTrackId: detail?.track,
   });
 
+  let menu = [];
+  if (navMenuView.view === "main") {
+    menu = mainMenu;
+  } else if (navMenuView.view === "subspace") {
+    menu = [
+      {
+        name: "Back",
+        icon: <ArrowCircleLeft />,
+        onClick() {
+          setNavMenuView({ view: "main" });
+        },
+      },
+      { type: "divider" },
+      ...(navMenuView.menu || []),
+    ];
+  }
+
   return (
-    <div>
-      {menu.map((m) => (
-        <NavMenuEntrance key={m.name} {...m} collapsed={collapsed} />
-      ))}
-    </div>
-  );
-}
-
-function SubSpaceMenu({ collapsed }) {
-  const [{ menu }, setNavMenuView] = useNavMenuView();
-
-  return (
-    <div>
-      <ul>
-        <li>
-          <NavMenuItem
-            item={{
-              icon: <ArrowCircleLeft />,
-              name: "Back",
-            }}
-            onClick={() => {
-              setNavMenuView({ view: "main" });
-            }}
-            collapsed={collapsed}
-          />
-        </li>
-      </ul>
-
-      <NavMenuDivider />
-
-      {menu?.map((m) => (
-        <NavMenuEntrance key={m.name} {...m} collapsed={collapsed} />
-      ))}
-    </div>
+    <ul>
+      <li>
+        {menu.map((m) => (
+          <NavMenuEntrance key={m.name} {...m} collapsed={collapsed} />
+        ))}
+      </li>
+    </ul>
   );
 }
