9effdbb6a13fcf64aa569b7706a12e74a0bdfc2f
Yongfeng LI <wliyongfeng@gmail.com>
2024-10-12 12:14:31 +0800

    Create hook to get referenda voting balance, #3923

---
+++ packages/next-common/hooks/referenda/useReferendaVotingBalance.js
@@ -0,0 +1,23 @@
+import useSubStorage from "next-common/hooks/common/useSubStorage";
+import { useMemo } from "react";
+
+export default function useReferendaVotingBalance(api, address) {
+  const { loading, result } = useSubStorage("system", "account", [address], {
+    api,
+  });
+
+  return useMemo(() => {
+    if (loading || !result) {
+      return {
+        isLoading: loading,
+        balance: null,
+      }
+    }
+
+    const balanceBig = result.data.free.toBigInt() + result.data.reserved.toBigInt();
+    return {
+      isLoading: loading,
+      balance: balanceBig.toString(),
+    }
+  }, [result, loading]);
+}
