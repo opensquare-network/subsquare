56459e16f2783b00895cf83e67e06d89847ec951
2nthony <ant.hu@outlook.com>
2024-10-14 18:45:13 +0800

    coretime overview connect api

--- packages/next/pages/coretime/index.jsx
+++ packages/next/pages/coretime/index.jsx
@@ -2,12 +2,31 @@ import ListLayout from "next-common/components/layout/ListLayout";
 import { withCommonProps } from "next-common/lib";
 import CoretimeSalePanel from "next-common/components/coretime/salePanel";
 import CoretimeSalesHistorySection from "next-common/components/coretime/salesHistorySection";
+import { createStore } from "next-common/store";
+import ChainProvider, { useChainSettings } from "next-common/context/chain";
+import ApiProvider from "next-common/context/api";
+import { Provider } from "react-redux";
+
+const chain = "coretime";
+const store = createStore({ chain });
+
 export default function CoretimePage() {
   return (
-    <ListLayout
-      title="Coretime"
-      description="A revolutionary approach to accessing the right amount of blockspace for every stage of growth."
-    >
+    <Provider store={store}>
+      <ChainProvider chain={chain}>
+        <ApiProvider>
+          <CoretimePageImpl />
+        </ApiProvider>
+      </ChainProvider>
+    </Provider>
+  );
+}
+
+function CoretimePageImpl() {
+  const { name, description } = useChainSettings();
+
+  return (
+    <ListLayout title={name} description={description}>
       <div className="space-y-6">
         <CoretimeSalePanel />
         <CoretimeSalesHistorySection />
