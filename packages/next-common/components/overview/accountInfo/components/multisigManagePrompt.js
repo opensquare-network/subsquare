f4d58c468c8be3aeb615fbc9f4360e433c0523b6
leochen <leochen.sat24@gmail.com>
2024-10-21 15:14:15 +0800

    Modify prompt content, #4466

--- packages/next-common/components/overview/accountInfo/components/multisigManagePrompt.js
+++ packages/next-common/components/overview/accountInfo/components/multisigManagePrompt.js
@@ -11,14 +11,52 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import getChainSettings from "next-common/utils/consts/settings";
+import { usePathname } from "next/navigation";
+import { myMultisigsSelector } from "next-common/store/reducers/multisigSlice";
+
+const getNeedApprovalCount = (multisigs, address) => {
+  const needApprovalItems = multisigs?.filter((item) => {
+    return (
+      item.state?.name === "Approving" && !item?.approvals.includes(address)
+    );
+  });
+
+  return needApprovalItems?.length || 0;
+};
+
+function ManageLink() {
+  const pathname = usePathname();
+  if (pathname.startsWith("/account/multisigs")) {
+    return null;
+  }
+
+  return (
+    <>
+      &nbsp;Manage&nbsp;
+      <Link className="underline" href={"/account/multisigs"}>
+        here
+      </Link>
+    </>
+  );
+}
 
 function PromptContent() {
   const dispatch = useDispatch();
   const chain = useChain();
   const realAddress = useRealAddress();
   const myMultisigsCount = useSelector(myMultisigsCountSelector) || 0;
+  const myMultisigs = useSelector(myMultisigsSelector);
+  const { items: multisigs = [], total = 0 } = myMultisigs || {};
 
   const settings = getChainSettings(chain);
+
+  const needApprovalCount = useMemo(() => {
+    if (total === 0) {
+      return 0;
+    }
+
+    return getNeedApprovalCount(multisigs, realAddress);
+  }, [multisigs, total, realAddress]);
 
   useEffect(() => {
     if (settings?.multisigApiPrefix) {
@@ -32,23 +70,20 @@ function PromptContent() {
     }
 
     const manageContent = myMultisigsCount > 1 ? "them" : "it";
-    const transactionContent =
-      myMultisigsCount > 1 ? "transactions" : "transaction";
+    const transactionContent = myMultisigsCount > 1 ? "multisigs" : "multisig";
 
     return (
       <Prompt
         cacheKey={CACHE_KEY.multisigPromptVisible}
         type={PromptTypes.NEUTRAL}
       >
-        You have {myMultisigsCount} multisig {transactionContent} to sign.
-        Manage &nbsp;
-        {manageContent}&nbsp;
-        <Link className="underline" href={"/account/multisigs"}>
-          here
-        </Link>
+        You have {myMultisigsCount} active {transactionContent}, &nbsp;
+        {needApprovalCount} of &nbsp;
+        {manageContent} need your approval.
+        <ManageLink />
       </Prompt>
     );
-  }, [myMultisigsCount, settings]);
+  }, [myMultisigsCount, needApprovalCount, settings]);
 
   return promptContent;
 }
