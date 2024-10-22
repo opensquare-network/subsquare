2e380ded312ebb095b3e34e65307a1390d5b5392
chaojun <chaojun@opensquare.network>
2024-10-22 15:29:26 +0800

    fix: proxy select popup, #3909

--- packages/next-common/components/selectProxyAccountPopup/index.js
+++ packages/next-common/components/selectProxyAccountPopup/index.js
@@ -2,15 +2,18 @@ import { GeneralProxiesProvider } from "next-common/context/proxy";
 import Popup from "../popup/wrapper/Popup";
 import { ProxiedAccounts } from "../switchSignerPopup";
 import { noop } from "lodash-es";
+import SignerPopupWrapper from "../popupWithSigner/signerPopupWrapper";
 
 export default function SelectProxyAccountPopup({ onClose, onSelect = noop }) {
   return (
-    <Popup title="Select Address" className="w-[640px]" onClose={onClose}>
-      <GeneralProxiesProvider>
-        <div className="flex flex-col gap-[24px]">
-          <ProxiedAccounts onSelect={onSelect} />
-        </div>
-      </GeneralProxiesProvider>
-    </Popup>
+    <SignerPopupWrapper onClose={onClose}>
+      <Popup title="Select Address" className="w-[640px]" onClose={onClose}>
+        <GeneralProxiesProvider>
+          <div className="flex flex-col gap-[24px]">
+            <ProxiedAccounts onSelect={onSelect} />
+          </div>
+        </GeneralProxiesProvider>
+      </Popup>
+    </SignerPopupWrapper>
   );
 }
