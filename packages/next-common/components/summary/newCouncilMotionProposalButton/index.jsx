559c5cc303eba4552ea8f457ad5613a6fa515364
leochen <leochen.sat24@gmail.com>
2024-10-23 14:07:52 +0800

    Remove disable of new proposal button, #4910

--- packages/next-common/components/summary/newCouncilMotionProposalButton/index.jsx
+++ packages/next-common/components/summary/newCouncilMotionProposalButton/index.jsx
@@ -1,8 +1,6 @@
-import Tooltip from "next-common/components/tooltip";
 import dynamicPopup from "next-common/lib/dynamic/popup";
 import { useState } from "react";
 import NewButton from "../newProposalButton/newButton";
-import useIsCollectiveMember from "next-common/utils/hooks/collectives/useIsCollectiveMember";
 
 const NewCouncilMotionProposalPopup = dynamicPopup(() =>
   import("./newCouncilMotionProposalPopup"),
@@ -10,16 +8,10 @@ const NewCouncilMotionProposalPopup = dynamicPopup(() =>
 
 export default function NewCouncilMotionProposalButton() {
   const [showPopup, setShowPopup] = useState(false);
-  const { isMember } = useIsCollectiveMember();
 
   return (
     <>
-      <Tooltip
-        content={!isMember ? "Only council members can create proposal" : null}
-      >
-        <NewButton setShowPopup={setShowPopup} disabled={!isMember} />
-      </Tooltip>
-
+      <NewButton setShowPopup={setShowPopup} />
       {showPopup && (
         <NewCouncilMotionProposalPopup
           onClose={() => {
