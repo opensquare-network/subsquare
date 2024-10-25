eca6465cc16f2ea996ef9b19f83fabb4796401d8
leochen <leochen.sat24@gmail.com>
2024-10-25 18:29:54 +0800

    Initialize coretime sales detail page, #4946 (#4947)
    * Init sales details structure, #4946
    
    * Modify history graphql, #4946
    
    * Init detail page content, #4946
    
    * Rename GET_CORETIME_HISTORY_SALES, #4946

---
+++ packages/next/pages/coretime/sales/[id].jsx
@@ -0,0 +1,100 @@
+import { withCommonProps } from "next-common/lib";
+import { createStore } from "next-common/store";
+import ChainProvider from "next-common/context/chain";
+import ApiProvider from "next-common/context/api";
+import { Provider } from "react-redux";
+import { commonReducers } from "next-common/store/reducers";
+import { CHAIN } from "next-common/utils/constants";
+import getChainSettings from "next-common/utils/consts/settings";
+import queryCoretimeCurrentSale from "next-common/services/gql/coretime/currentSale";
+import { CoretimeActiveSaleProvider } from "next-common/context/coretime/sale";
+import queryCoretimeConfiguration from "next-common/services/gql/coretime/configuration";
+import queryCoretimeStatus from "next-common/services/gql/coretime/status";
+import CoretimeCommonProvider from "next-common/context/coretime/common";
+import useLoopCoretimeScanHeight from "next-common/hooks/coretime/useLoopCoretimeScanHeight";
+import RelayInfoProvider from "next-common/context/relayInfo";
+import getMetaDesc from "next-common/utils/post/getMetaDesc";
+import { getBannerUrl } from "next-common/utils/banner";
+import { PostProvider, usePost } from "next-common/context/post";
+import CoretimeSalesDetail from "next-common/components/detail/coretime/sales";
+import DetailLayout from "next-common/components/layout/DetailLayout";
+
+const isCoretimeSupported = !!getChainSettings(CHAIN).modules?.coretime;
+
+let chain;
+let store;
+
+if (isCoretimeSupported) {
+  chain = `${CHAIN}-coretime`;
+  store = createStore({
+    chain,
+    reducer: commonReducers,
+  });
+}
+
+export default function CoretimePage({ detail }) {
+  if (!isCoretimeSupported) {
+    return null;
+  }
+
+  return (
+    <RelayInfoProvider>
+      <Provider store={store}>
+        <ChainProvider chain={chain}>
+          <ApiProvider>
+            <CoretimeCommonProvider>
+              <CoretimeActiveSaleProvider>
+                <PostProvider post={detail}>
+                  <CoretimeSalesDetailPageImpl />
+                </PostProvider>
+              </CoretimeActiveSaleProvider>
+            </CoretimeCommonProvider>
+          </ApiProvider>
+        </ChainProvider>
+      </Provider>
+    </RelayInfoProvider>
+  );
+}
+
+function CoretimeSalesDetailPageImpl() {
+  useLoopCoretimeScanHeight();
+
+  const post = usePost();
+
+  const desc = getMetaDesc(post);
+
+  const seoInfo = {
+    title: post?.title,
+    desc,
+    ogImage: getBannerUrl(post?.bannerCid),
+  };
+
+  return (
+    <DetailLayout seoInfo={seoInfo}>
+      <div className="space-y-6">
+        <CoretimeSalesDetail />
+      </div>
+    </DetailLayout>
+  );
+}
+
+export const getServerSideProps = async (ctx) => {
+  if (!isCoretimeSupported) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return withCommonProps(async () => {
+    const sale = await queryCoretimeCurrentSale();
+    const configuration = await queryCoretimeConfiguration();
+    const status = await queryCoretimeStatus();
+    return {
+      props: {
+        coretimeSale: sale,
+        configuration,
+        status,
+      },
+    };
+  })(ctx);
+};
