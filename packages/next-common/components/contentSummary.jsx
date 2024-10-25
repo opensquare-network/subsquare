da30ce1ccb8b244667b20e5563f6627a6a63366b
leochen <leochen.sat24@gmail.com>
2024-10-25 16:56:39 +0800

    Remove powered by OpenAI, #4948

--- packages/next-common/components/contentSummary.jsx
+++ packages/next-common/components/contentSummary.jsx
@@ -8,14 +8,6 @@ export default function ContentSummary() {
   return (
     <div>
       <MarkdownPreviewer content={post.contentSummary?.summary} />
-
-      <div className="mt-4 text12Medium text-textTertiary flex items-center">
-        Powered by
-        <span className="ml-2">
-          <LinkChatgpt className="mr-1 inline-flex w-4 h-4 [&_path]:fill-textTertiary" />
-          OpenAI
-        </span>
-      </div>
     </div>
   );
 }
