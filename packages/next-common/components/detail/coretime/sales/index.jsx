580f1033b264fd8fa5a35ef1108e3b0d810398c4
leochen <leochen.sat24@gmail.com>
2024-10-25 18:14:55 +0800

    Init detail page content, #4946

--- packages/next-common/components/detail/coretime/sales/index.jsx
+++ packages/next-common/components/detail/coretime/sales/index.jsx
@@ -1,3 +1,12 @@
+import CoretimeSalePanel from "next-common/components/coretime/salePanel";
+import CoretimeSalesHistorySection from "next-common/components/coretime/salesHistorySection";
+
+// TODO: fetch target sale detail by data?.id.
 export default function CoretimeSalesDetail() {
-  return <div>sales details content</div>;
+  return (
+    <div className="space-y-6">
+      <CoretimeSalePanel />
+      <CoretimeSalesHistorySection />
+    </div>
+  );
 }
