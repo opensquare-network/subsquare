18395a51b5e21090d508bb9112350e98f67b5b2a
chaojun <chaojun@opensquare.network>
2024-10-28 17:55:55 +0800

    Different sales history tab order for different phase, #4832

--- packages/next-common/components/coretime/salesHistorySection/index.jsx
+++ packages/next-common/components/coretime/salesHistorySection/index.jsx
@@ -5,34 +5,72 @@ import SalesHistoryPurchases from "./purchases";
 import SalesHistoryRenewals from "./renewals";
 import { useState } from "react";
 import { usePageProps } from "next-common/context/page";
+import useCoretimeSalePhase, {
+  Phases,
+} from "next-common/context/coretime/hooks/useCoretimeSalePhase";
+import Loading from "next-common/components/loading";
 
-export default function CoretimeSalesHistorySection() {
-  const { coretimeSale } = usePageProps();
-  const [activeTabLabel, setActiveTabLabel] = useState("Purchases");
-
+function SalesHistoryWrapper({ children }) {
   return (
     <div>
       <TitleContainer className="mb-4">Sales History</TitleContainer>
-      <NeutralPanel className="p-6">
-        <Tabs
-          activeTabLabel={activeTabLabel}
-          onTabClick={(tab) => {
-            setActiveTabLabel(tab.label);
-          }}
-          tabs={[
-            {
-              label: "Renewals",
-              activeCount: coretimeSale?.renewalCount,
-              content: <SalesHistoryRenewals />,
-            },
-            {
-              label: "Purchases",
-              activeCount: coretimeSale?.purchaseCount,
-              content: <SalesHistoryPurchases />,
-            },
-          ]}
-        />
-      </NeutralPanel>
+      <NeutralPanel className="p-6">{children}</NeutralPanel>
     </div>
+  );
+}
+
+function SalesHistoryList({ phase }) {
+  const { coretimeSale } = usePageProps();
+
+  const isInterludePhase = phase === Phases.Interlude;
+
+  const [activeTabLabel, setActiveTabLabel] = useState(
+    isInterludePhase ? "Purchases" : "Renewals",
+  );
+
+  const renewalsTabInfo = {
+    label: "Renewals",
+    activeCount: coretimeSale?.renewalCount,
+    content: <SalesHistoryRenewals />,
+  };
+
+  const purchasesTabInfo = {
+    label: "Purchases",
+    activeCount: coretimeSale?.purchaseCount,
+    content: <SalesHistoryPurchases />,
+  };
+
+  const tabs = isInterludePhase
+    ? [purchasesTabInfo, renewalsTabInfo]
+    : [renewalsTabInfo, purchasesTabInfo];
+
+  return (
+    <Tabs
+      activeTabLabel={activeTabLabel}
+      onTabClick={(tab) => {
+        setActiveTabLabel(tab.label);
+      }}
+      tabs={tabs}
+    />
+  );
+}
+
+export default function CoretimeSalesHistorySection() {
+  const { isLoading, phase } = useCoretimeSalePhase();
+
+  if (isLoading) {
+    return (
+      <SalesHistoryWrapper>
+        <div className="flex items-center justify-center">
+          <Loading size={20} />
+        </div>
+      </SalesHistoryWrapper>
+    );
+  }
+
+  return (
+    <SalesHistoryWrapper>
+      <SalesHistoryList phase={phase} />
+    </SalesHistoryWrapper>
   );
 }
