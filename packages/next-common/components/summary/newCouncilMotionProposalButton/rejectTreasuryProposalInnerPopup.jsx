f59e2245521b51c2eeda87a9b421140c72c4e795
Chaojun Huang <chaojun@opensquare.network>
2024-10-29 18:11:44 +0800

    Add rejectProposal template and enable proposal templates for council, #4979 (#4986)

--- packages/next-common/components/summary/newCouncilMotionProposalButton/rejectTreasuryProposalInnerPopup.jsx
+++ packages/next-common/components/summary/newCouncilMotionProposalButton/rejectTreasuryProposalInnerPopup.jsx
@@ -1,21 +1,21 @@
 import Popup from "next-common/components/popup/wrapper/Popup";
 import TreasuryProposalPopupContent from "./common/treasuryProposalPopupContent";
 
-export default function ApproveTreasuryProposalInnerPopup({
+export default function RejectTreasuryProposalInnerPopup({
   onClose,
   isMember,
 }) {
   return (
     <Popup
       className="!w-[640px]"
-      title="Approve Treasury Proposal"
+      title="Reject Treasury Proposal"
       maskClosable={false}
       onClose={onClose}
     >
       <TreasuryProposalPopupContent
         onClose={onClose}
         isMember={isMember}
-        treasuryProposalAction="approveProposal"
+        treasuryProposalAction="rejectProposal"
       />
     </Popup>
   );
