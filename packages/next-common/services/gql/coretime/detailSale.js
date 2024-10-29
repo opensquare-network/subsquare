69e6efafb573a87175a836ab7c006dd81c70996b
leochen <leochen.sat24@gmail.com>
2024-10-29 18:31:50 +0800

    Modify common vars to string, #4985

--- packages/next-common/services/gql/coretime/detailSale.js
+++ packages/next-common/services/gql/coretime/detailSale.js
@@ -1,11 +1,12 @@
 import { coretimeClient } from "next-common/hooks/apollo";
 import { gql } from "@apollo/client";
-import { INFO_FIELDS, INDEXER_FIELDS } from "./common";
+import { INFO_FIELDS, INDEXER_FIELDS, COMMON_SALE_FIELDS } from "./common";
 
 const GET_CORETIME_SALE = gql`
   query MyQuery($id: Int!) {
     coretimeSale(id: $id) {
       isFinal
+      ${COMMON_SALE_FIELDS}
       info {
         ${INFO_FIELDS}
       }
