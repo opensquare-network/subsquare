11bb5eae86cd2e6710b42c3da21735925794be63
Yongfeng LI <wliyongfeng@gmail.com>
2024-10-31 21:58:27 +0800

    Fix core time sale end calculation, #4832

--- packages/next-common/context/coretime/hooks/useCoretimeSaleEnd.js
+++ packages/next-common/context/coretime/hooks/useCoretimeSaleEnd.js
@@ -15,7 +15,7 @@ export default function useCoretimeSaleEnd() {
   const relayHeight = useRelayHeight();
   const sale = useCoretimeSale();
   const configuration = useCoretimeConfiguration();
-  const { info: { regionEnd } = {}, isFinal, endIndexer } = sale;
+  const { info: { regionBegin } = {}, isFinal, endIndexer } = sale;
   const coretimeHeight = useSelector(chainOrScanHeightSelector);
   if (isFinal && endIndexer) {
     return {
@@ -28,15 +28,8 @@ export default function useCoretimeSaleEnd() {
     };
   }
 
-  const advanced = relayHeight + configuration.advanceNotice;
-  const currentSlice = Math.floor(advanced / CORETIME_TIMESLICE_PERIOD);
-
-  const sliceGap = regionEnd - currentSlice;
-  const regionBlocksGap = sliceGap * CORETIME_TIMESLICE_PERIOD; // coretime 12s, relay 6s, so divide 2.
-
-  const currentSliceRemainingBlocks = CORETIME_TIMESLICE_PERIOD - advanced % CORETIME_TIMESLICE_PERIOD;
-  const relayBlocksGap = regionBlocksGap + currentSliceRemainingBlocks;
-
+  const relayEndBlock = (regionBegin + 1) * CORETIME_TIMESLICE_PERIOD - configuration.advanceNotice;
+  const relayBlocksGap = relayEndBlock - relayHeight;
   const coretimeBlocksGap = Math.ceil(relayBlocksGap / 2);
   return {
     isLoading: false,
