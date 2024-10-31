1d492c33ba52e949d7d9d705c69e01ce1e890cd0
2nthony <ant.hu@outlook.com>
2024-10-31 11:52:01 +0800

    fix period progress percentage

--- packages/next-common/components/coretime/salePanel/chart/periodProgress.jsx
+++ packages/next-common/components/coretime/salePanel/chart/periodProgress.jsx
@@ -1,6 +1,8 @@
 import { SystemQuestion } from "@osn/icons/subsquare";
 import Progress from "next-common/components/progress";
+import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
 import { cn, toPercentage } from "next-common/utils";
+import { useSelector } from "react-redux";
 
 export default function CoretimeSalePanelChartPeriodProgress({
   className = "",
@@ -21,23 +23,44 @@ export default function CoretimeSalePanelChartPeriodProgress({
         style={{
           width: `${renewalWidth}%`,
         }}
+        renewalBlocks={renewalBlocks}
+        initBlockHeight={initBlockHeight}
       />
 
       <SalePeriodProgress
         fixedStart={fixedStart}
         saleStart={saleStart}
         saleBlocks={saleBlocks}
+        endBlockHeight={endBlockHeight}
       />
     </div>
   );
 }
 
-function RenewalPeriodProgress({ style }) {
+function RenewalPeriodProgress({
+  style,
+  renewalBlocks,
+  saleStart,
+  initBlockHeight,
+}) {
+  const chainHeight = useSelector(chainOrScanHeightSelector);
+
+  let percentage = 0;
+  if (chainHeight >= saleStart) {
+    percentage = 100;
+  } else if (chainHeight >= initBlockHeight) {
+    const gone = chainHeight - initBlockHeight;
+    percentage = toPercentage(gone / renewalBlocks, 3);
+  }
+
   return (
     <div className="text12Medium space-y-2" style={style}>
       <div>Renewal Period</div>
-      {/* TODO: renewal period percentage*/}
-      <Progress percentage={66} fg="var(--theme300)" bg="var(--neutral200)" />
+      <Progress
+        percentage={percentage}
+        fg="var(--theme300)"
+        bg="var(--neutral200)"
+      />
       <div className="text-textTertiary flex items-center gap-1">
         Interlude <SystemQuestion className="inline-flex w-3 h-3" />
       </div>
@@ -45,14 +68,32 @@ function RenewalPeriodProgress({ style }) {
   );
 }
 
-function SalePeriodProgress({ fixedStart, saleStart, saleBlocks }) {
+function SalePeriodProgress({
+  fixedStart,
+  saleStart,
+  saleBlocks,
+  endBlockHeight,
+}) {
+  const chainHeight = useSelector(chainOrScanHeightSelector);
   const fixedPosition = toPercentage((fixedStart - saleStart) / saleBlocks, 3);
+
+  let percentage = 0;
+  if (chainHeight >= endBlockHeight) {
+    percentage = 100;
+  } else if (chainHeight >= saleStart) {
+    const gone = chainHeight - saleStart;
+    percentage = toPercentage(gone / saleBlocks, 3);
+  }
 
   return (
     <div className="grow text12Medium space-y-2">
       <div>Sale Period</div>
       <div className="relative space-y-2">
-        <Progress fg="var(--theme300)" bg="var(--neutral200)" />
+        <Progress
+          percentage={percentage}
+          fg="var(--theme300)"
+          bg="var(--neutral200)"
+        />
         <div className="text-textTertiary flex items-center gap-1">
           Price Discovery
           <SystemQuestion className="inline-flex w-3 h-3" />
