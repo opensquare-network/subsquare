50821da572f6aec884a44cc53bdcd27f1f16884b
leochen <leochen.sat24@gmail.com>
2024-10-31 11:27:39 +0800

    Remove useEffect, #4999

--- packages/next-common/context/coretime/sale/detailSubscriber.js
+++ packages/next-common/context/coretime/sale/detailSubscriber.js
@@ -1,36 +1,26 @@
 import queryCoretimeDetailSale from "next-common/services/gql/coretime/detailSale";
-import { useEffect, useState } from "react";
 import { useInterval } from "react-use";
 import { useChainSettings } from "next-common/context/chain";
 import { useSharedCoretimeSale } from "./provider";
 
 export default function CoretimeDetailSaleSubscriber({ children }) {
   const [detailSale, setDetailSale] = useSharedCoretimeSale();
-  const [saleData, setSaleData] = useState(null);
   const { blockTime } = useChainSettings();
   const { id, isFinal } = detailSale;
 
   const pollInterval = parseInt(blockTime) || 12000;
   useInterval(async () => {
-    if (isFinal) {
+    if (isFinal || !id) {
       return;
     }
 
     try {
       const newSaleData = await queryCoretimeDetailSale(id);
-      setSaleData(newSaleData);
+      setDetailSale(newSaleData);
     } catch (err) {
       throw new Error("Subscribe coretime detailSale failed.");
     }
   }, pollInterval);
-
-  useEffect(() => {
-    if (isFinal || !saleData) {
-      return;
-    }
-
-    setDetailSale(saleData);
-  }, [setDetailSale, saleData, isFinal]);
 
   return children;
 }
