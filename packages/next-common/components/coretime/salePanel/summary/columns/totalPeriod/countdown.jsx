4ea0580416c297ee60e5acbde0f397fc756c7703
leochen <leochen.sat24@gmail.com>
2024-11-01 16:13:06 +0800

    Hide TotalPeriodCountdown on mobile device, #5009

--- packages/next-common/components/coretime/salePanel/summary/columns/totalPeriod/countdown.jsx
+++ packages/next-common/components/coretime/salePanel/summary/columns/totalPeriod/countdown.jsx
@@ -1,9 +1,18 @@
 import CountDown from "next-common/components/summary/countDown";
 import { cn } from "next-common/utils";
+import { useNavCollapsed } from "next-common/context/nav";
 
 export default function TotalPeriodCountdown({ percentage, className = "" }) {
+  const [navCollapsed] = useNavCollapsed();
+
   return (
-    <div className={cn("flex", className)}>
+    <div
+      className={cn(
+        "flex",
+        navCollapsed ? "max-sm:hidden" : "max-md:hidden",
+        className,
+      )}
+    >
       <CountDown percent={percentage} />
     </div>
   );
--- packages/next-common/components/coretime/salePanel/summary/common.jsx
+++ packages/next-common/components/coretime/salePanel/summary/common.jsx
@@ -6,7 +6,7 @@ export function SummaryColumnGap({ children }) {
 
 export function Item({ label = "", value, valueClassName }) {
   return (
-    <div className="flex items-center flex-wrap gap-x-1 text12Medium text-textTertiary">
+    <div className="flex items-center gap-x-1 text12Medium text-textTertiary">
       <div>{label}</div>
       <div className={cn("text-textPrimary", valueClassName)}>{value}</div>
     </div>
