bf9901563aeec9d1ed3b5a3fbc264dcfa2d69c6b
leochen <leochen.sat24@gmail.com>
2024-11-01 15:00:50 +0800

    Show period countdown on mobile device, #5009

--- packages/next-common/components/coretime/salePanel/summary/columns/salePeriod/countdown.jsx
+++ packages/next-common/components/coretime/salePanel/summary/columns/salePeriod/countdown.jsx
@@ -1,18 +1,9 @@
 import CountDown from "next-common/components/summary/countDown";
-import { useNavCollapsed } from "next-common/context/nav";
 import { cn } from "next-common/utils";
 
 export default function SalePeriodCountdown({ percentage, className = "" }) {
-  const [navCollapsed] = useNavCollapsed();
-
   return (
-    <div
-      className={cn(
-        "flex",
-        navCollapsed ? "max-sm:hidden" : "max-md:hidden",
-        className,
-      )}
-    >
+    <div className={cn("flex", className)}>
       <CountDown percent={percentage} />
     </div>
   );
