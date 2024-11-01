22efe1b3d82a9150fd1e488771b6808d242c7e4f
leochen <leochen.sat24@gmail.com>
2024-11-01 16:54:31 +0800

    Fix: flickering caused by re-rendering, #5009

--- packages/next-common/components/coretime/salePanel/summary/columns/totalPeriod/index.jsx
+++ packages/next-common/components/coretime/salePanel/summary/columns/totalPeriod/index.jsx
@@ -1,56 +1,76 @@
+import { useMemo, memo } from "react";
 import SummaryItem from "next-common/components/summary/layout/item";
 import { SummaryColumnGap } from "../../common";
 import TotalPeriodCountdown from "./countdown";
 import FieldLoading from "next-common/components/icons/fieldLoading";
 import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
 import { useSelector } from "react-redux";
-import useCoretimeSaleEnd from "next-common/context/coretime/hooks/useCoretimeSaleEnd";
 import { getCountDownProgress } from "../currentPhase/common";
-import useCoretimeSale from "next-common/context/coretime/sale/provider";
 import { blockTimeSelector } from "next-common/store/reducers/chainSlice";
 import { useRemainingTime } from "next-common/components/remaining";
 import EndAt from "./endAt";
 import PassedTime from "./passedTime";
 import TotalTime from "./totalTime";
+import { usePageProps } from "next-common/context/page";
+import useCoretimeSaleEnd from "next-common/context/coretime/hooks/useCoretimeSaleEnd";
 
 function getEndBlocksTime(initBlocksTime, blockTime, blockGap) {
   return initBlocksTime + blockGap * blockTime;
 }
 
+function SalePeriodContent({
+  passedBlockGap,
+  remaining,
+  totalBlockGap,
+  endBlocksTime,
+}) {
+  return (
+    <div className="space-y-1">
+      <PassedTime passedBlockGap={passedBlockGap} remaining={remaining} />
+      <TotalTime totalBlockGap={totalBlockGap} />
+      <EndAt endTime={endBlocksTime} />
+    </div>
+  );
+}
+
+const MemoizedSalePeriodContent = memo(SalePeriodContent);
+
 export default function TotalPeriod() {
-  const { initIndexer = {} } = useCoretimeSale();
+  const { coretimeSale } = usePageProps();
+  const { initIndexer = {} } = coretimeSale;
+  const initHeight = initIndexer?.blockHeight;
   const { isLoading, indexer: endIndexer } = useCoretimeSaleEnd();
 
-  const blockTime = useSelector(blockTimeSelector);
   const chainHeight = useSelector(chainOrScanHeightSelector);
+  const blockTime = useSelector(blockTimeSelector);
 
-  const initHeight = initIndexer?.blockHeight;
   const endHeight = endIndexer?.blockHeight;
 
-  const totalBlockGap = endHeight - initHeight;
-  const passedBlockGap = chainHeight - initHeight;
-  const remainingBlocksGap = endHeight - chainHeight;
+  const totalBlockGap = useMemo(
+    () => endHeight - initHeight,
+    [endHeight, initHeight],
+  );
+  const passedBlockGap = useMemo(
+    () => chainHeight - initHeight,
+    [chainHeight, initHeight],
+  );
+  const remainingBlocksGap = useMemo(
+    () => endHeight - chainHeight,
+    [endHeight, chainHeight],
+  );
 
   const remaining = useRemainingTime(remainingBlocksGap);
+  const percentage = useMemo(
+    () => getCountDownProgress(initHeight, chainHeight, endHeight),
+    [initHeight, chainHeight, endHeight],
+  );
 
-  const percentage = getCountDownProgress(initHeight, chainHeight, endHeight);
-  const endBlocksTime = getEndBlocksTime(
-    initIndexer?.blockTime,
-    blockTime,
-    totalBlockGap,
+  const endBlocksTime = useMemo(
+    () => getEndBlocksTime(initIndexer?.blockTime, blockTime, totalBlockGap),
+    [initIndexer, blockTime, totalBlockGap],
   );
 
   const isFieldLoading = !initHeight || !endHeight || isLoading;
-
-  const SalePeriodContent = () => {
-    return (
-      <div className="space-y-1">
-        <PassedTime passedBlockGap={passedBlockGap} remaining={remaining} />
-        <TotalTime totalBlockGap={totalBlockGap} />
-        <EndAt endTime={endBlocksTime} />
-      </div>
-    );
-  };
 
   return (
     <SummaryColumnGap>
@@ -58,7 +78,16 @@ export default function TotalPeriod() {
         title="Total Period"
         suffix={<TotalPeriodCountdown percentage={percentage} />}
       >
-        {isFieldLoading ? <FieldLoading /> : <SalePeriodContent />}
+        {isFieldLoading ? (
+          <FieldLoading />
+        ) : (
+          <MemoizedSalePeriodContent
+            passedBlockGap={passedBlockGap}
+            remaining={remaining}
+            totalBlockGap={totalBlockGap}
+            endBlocksTime={endBlocksTime}
+          />
+        )}
       </SummaryItem>
     </SummaryColumnGap>
   );
