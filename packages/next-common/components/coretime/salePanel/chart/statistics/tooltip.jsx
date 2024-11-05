5ad3943bec9fe1dc8f24a04f3747e7068f17671f
2nthony <ant.hu@outlook.com>
2024-11-05 18:18:54 +0800

    add triangle

--- packages/next-common/components/coretime/salePanel/chart/statistics/tooltip.jsx
+++ packages/next-common/components/coretime/salePanel/chart/statistics/tooltip.jsx
@@ -1,6 +1,8 @@
 import AddressUser from "next-common/components/user/addressUser";
 import { useChainSettings } from "next-common/context/chain";
 
+const TRIANGLE_SIZE = 8;
+
 export default function CoretimeSalePanelChartStatisticsTooltip({
   x,
   y,
@@ -13,12 +15,15 @@ export default function CoretimeSalePanelChartStatisticsTooltip({
     return null;
   }
 
+  const offsetLeft = x - TRIANGLE_SIZE - 3;
+  const offsetTop = y - TRIANGLE_SIZE - 3;
+
   return (
     <div
       className="absolute z-50 pointer-events-none -translate-x-1/2 -translate-y-full"
       style={{
-        left: x,
-        top: y,
+        left: offsetLeft,
+        top: offsetTop,
       }}
     >
       <div className="rounded py-1.5 px-3 text12Normal text-textPrimaryContrast bg-tooltipBg">
@@ -43,6 +48,13 @@ export default function CoretimeSalePanelChartStatisticsTooltip({
             />
           </div>
         )}
+        <div
+          className="absolute left-1/2 bottom-0 bg-tooltipBg rotate-45 -translate-x-1/2 translate-y-1/2"
+          style={{
+            width: TRIANGLE_SIZE,
+            height: TRIANGLE_SIZE,
+          }}
+        />
       </div>
     </div>
   );
