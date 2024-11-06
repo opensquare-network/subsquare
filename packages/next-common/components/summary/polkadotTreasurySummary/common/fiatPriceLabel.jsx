5b3f93d98ea1349148da75e660a9305cc9d08ae5
leochen <leochen.sat24@gmail.com>
2024-11-06 17:35:40 +0800

    Add mythtoken balance into total balance, #5016

--- packages/next-common/components/summary/polkadotTreasurySummary/common/fiatPriceLabel.jsx
+++ packages/next-common/components/summary/polkadotTreasurySummary/common/fiatPriceLabel.jsx
@@ -6,20 +6,28 @@ import useFiatPrice from "next-common/hooks/useFiatPrice";
 import { toPrecision } from "next-common/utils";
 import { SYMBOL_DECIMALS } from "next-common/utils/consts/asset";
 
-// TODO: fiat price plus mythTokenBalance
+// TODO: fetch Myth Token fiat price
+const mythTokenFiatPrice = 0.2168;
+
 export default function FiatPriceLabel({
   free = 0,
   usdcBalance = 0,
   usdtBalance = 0,
-  // mythTokenBalance = 0,
+  mythTokenBalance = 0,
 }) {
   const { price: fiatPrice } = useFiatPrice();
   const { decimals } = useChainSettings();
+
+  const mythTokenFiatValue = BigNumber(mythTokenBalance || 0)
+    .dividedBy(Math.pow(10, SYMBOL_DECIMALS.MYTH))
+    .multipliedBy(mythTokenFiatPrice);
+
   const totalPrice = BigNumber(free || 0)
     .dividedBy(Math.pow(10, decimals))
     .multipliedBy(fiatPrice)
     .plus(toPrecision(usdcBalance, SYMBOL_DECIMALS.USDC))
-    .plus(toPrecision(usdtBalance, SYMBOL_DECIMALS.USDT));
+    .plus(toPrecision(usdtBalance, SYMBOL_DECIMALS.USDT))
+    .plus(mythTokenFiatValue);
 
   return (
     <div>
