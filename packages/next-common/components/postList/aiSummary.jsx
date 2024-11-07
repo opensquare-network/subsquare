b654decb90c360e53331c734ff3cba124ac137ad
2nthony <ant.hu@outlook.com>
2024-11-07 18:12:43 +0800

    revert markdown

--- packages/next-common/components/postList/aiSummary.jsx
+++ packages/next-common/components/postList/aiSummary.jsx
@@ -1,6 +1,7 @@
 import { SystemAiSummary } from "@osn/icons/subsquare";
 import Tooltip from "../tooltip";
 import DesktopOnly from "../responsive/desktopOnly";
+import { MarkdownPreviewer } from "@osn/previewer";
 
 export default function PostListAISummary({ data }) {
   const content = data?.contentSummary?.summary;
@@ -12,7 +13,14 @@ export default function PostListAISummary({ data }) {
   return (
     <DesktopOnly>
       <div className="flex items-center">
-        <Tooltip content={content}>
+        <Tooltip
+          content={
+            <MarkdownPreviewer
+              className="!text-inherit max-w-4xl"
+              content={content}
+            />
+          }
+        >
           <SystemAiSummary className="w-4 h-4 text-textTertiary hover:text-textSecondary hover:cursor-pointer stroke-current stroke-[0.5]" />
         </Tooltip>
       </div>
