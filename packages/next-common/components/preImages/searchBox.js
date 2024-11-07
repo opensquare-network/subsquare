43c77e70f7614ffc3a56a4df64b12229c4f49554
leochen <leochen.sat24@gmail.com>
2024-11-07 18:17:37 +0800

    Debounce the Search Input, #5050

--- packages/next-common/components/preImages/searchBox.js
+++ packages/next-common/components/preImages/searchBox.js
@@ -1,6 +1,8 @@
 import { SystemSearch } from "@osn/icons/subsquare";
 import InputOrigin from "next-common/components/input";
 import styled from "styled-components";
+import { useEffect } from "react";
+import { debounce } from "lodash-es";
 
 const Input = styled(InputOrigin)`
   width: 100%;
@@ -13,16 +15,35 @@ export default function SearchBox({
   value,
   setValue,
   placeholder = "Search hash",
+  isDebounce = false,
+  debounceDelay = 500,
 }) {
+  const handleDebouncedChange = debounce((newValue) => {
+    setValue(newValue);
+  }, debounceDelay);
+
+  useEffect(() => {
+    // Clean up debounce on unmount to avoid memory leaks
+    return () => handleDebouncedChange.cancel();
+  }, [handleDebouncedChange]);
+
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+
+    if (isDebounce) {
+      handleDebouncedChange(newValue);
+    } else {
+      setValue(newValue);
+    }
+  };
+
   return (
     <div className="flex items-center gap-x-2 max-md:w-full text12Normal">
       <Input
         placeholder={placeholder}
         prefix={<SystemSearch className="text-textTertiary w-5 h-5" />}
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
+        defaultValue={value}
+        onChange={handleChange}
       />
     </div>
   );
