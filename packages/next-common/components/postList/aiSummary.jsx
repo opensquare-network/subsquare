b0ca325f5f30bdc1c2b921d2bca95cfb4f58293f
2nthony <ant.hu@outlook.com>
2024-11-08 14:15:03 +0800

    use hover card

--- packages/next-common/components/postList/aiSummary.jsx
+++ packages/next-common/components/postList/aiSummary.jsx
@@ -1,7 +1,8 @@
 import { SystemAiSummary } from "@osn/icons/subsquare";
-import Tooltip from "../tooltip";
 import DesktopOnly from "../responsive/desktopOnly";
 import { MarkdownPreviewer } from "@osn/previewer";
+import * as HoverCard from "@radix-ui/react-hover-card";
+import { cn } from "next-common/utils";
 
 export default function PostListAISummary({ data }) {
   const content = data?.contentSummary?.summary;
@@ -13,16 +14,30 @@ export default function PostListAISummary({ data }) {
   return (
     <DesktopOnly>
       <div className="flex items-center max-sm:hidden">
-        <Tooltip
-          content={
-            <MarkdownPreviewer
-              className="!text-inherit max-w-4xl"
-              content={content}
-            />
-          }
-        >
-          <SystemAiSummary className="w-4 h-4 text-textTertiary hover:text-textSecondary hover:cursor-pointer stroke-current stroke-[0.5]" />
-        </Tooltip>
+        <HoverCard.Root openDelay={0} closeDelay={100}>
+          <HoverCard.Trigger>
+            <SystemAiSummary className="w-4 h-4 text-textTertiary hover:text-textSecondary hover:cursor-pointer stroke-current stroke-[0.5]" />
+          </HoverCard.Trigger>
+
+          <HoverCard.Portal>
+            <HoverCard.Content
+              side="right"
+              sideOffset={0}
+              align="end"
+              className="pl-2 z-50"
+            >
+              <MarkdownPreviewer
+                content={content}
+                className={cn(
+                  "max-w-[480px]",
+                  "!bg-neutral100 shadow-200",
+                  "px-4 py-3 rounded-lg",
+                  "border border-neutral300",
+                )}
+              />
+            </HoverCard.Content>
+          </HoverCard.Portal>
+        </HoverCard.Root>
       </div>
     </DesktopOnly>
   );
