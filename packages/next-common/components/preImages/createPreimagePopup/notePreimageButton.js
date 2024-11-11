7923dbfec11e0f46e9dc927578ca4c82c2bd4428
Chaojun Huang <chaojun@opensquare.network>
2024-11-11 15:14:38 +0800

    fix: treasury preimage template on collectives preimages, #5061 (#5062)

--- packages/next-common/components/preImages/createPreimagePopup/notePreimageButton.js
+++ packages/next-common/components/preImages/createPreimagePopup/notePreimageButton.js
@@ -10,6 +10,7 @@ export default function NotePreimageButton({ notePreimageTx }) {
 
   return (
     <TxSubmissionButton
+      disabled={!notePreimageTx}
       getTxFunc={() => notePreimageTx}
       onClose={onClose}
       onInBlock={() => {
