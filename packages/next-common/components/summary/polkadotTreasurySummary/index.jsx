fb4dc28727954f14f16a6c0bf4eaa7b5ea0e19d7
leochen <leochen.sat24@gmail.com>
2024-11-12 16:34:52 +0800

    Add CollapsePanel, #5051

--- packages/next-common/components/summary/polkadotTreasurySummary/index.jsx
+++ packages/next-common/components/summary/polkadotTreasurySummary/index.jsx
@@ -1,27 +1,25 @@
-import SummaryLayout from "next-common/components/summary/layout/layout";
 import TotalTreasury from "./totalTreasury";
 import RelayChainTreasury from "./relayChainTreasury";
 import TreasuryOnAssetHub from "./treasuryOnAssetHub";
 import TreasuryOnHydration from "./treasuryOnHydration";
-import { cn } from "next-common/utils";
 import { PolkadotTreasurySummaryProvider } from "./context";
 import { TreasuryOnHydrationProvider } from "./context/treasuryOnHydration";
 import FellowshipTreasuryOnAssetHub from "./fellowshipTreasuryOnAssetHub";
 import Loans from "./loans";
 import Bounties from "./bounties";
+import CollapsePanel from "next-common/components/summary/polkadotTreasurySummary/common/collapsePanel";
 
 function PolkadotTreasurySummaryInContext() {
   return (
-    <div className={cn("max-sm:grid-cols-1")}>
-      <TotalTreasury />
-      <SummaryLayout className={cn("max-sm:grid-cols-1")}>
+    <div className="max-sm:grid-cols-1 space-y-4">
+      <CollapsePanel labelItem={<TotalTreasury />}>
         <RelayChainTreasury />
         <TreasuryOnAssetHub />
         <TreasuryOnHydration />
         <Bounties />
         <FellowshipTreasuryOnAssetHub />
         <Loans />
-      </SummaryLayout>
+      </CollapsePanel>
     </div>
   );
 }
