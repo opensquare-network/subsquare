4f91e9f05f39ec40aee16d78d1a830f73c665580
leochen <leochen.sat24@gmail.com>
2024-11-18 18:13:46 +0800

    add loading for useCoretimeSaleIsInterlude, #4832 (#5071)

--- packages/next-common/context/coretime/hooks/useCoretimeSaleIsInterlude.js
+++ packages/next-common/context/coretime/hooks/useCoretimeSaleIsInterlude.js
@@ -1,14 +1,31 @@
 import chainOrScanHeightSelector from "next-common/store/reducers/selectors/height";
 import useCoretimeSale from "next-common/context/coretime/sale/provider";
 import { useSelector } from "react-redux";
+import { isNil } from "lodash-es";
 
 export default function useCoretimeSaleIsInterlude() {
   const chainHeight = useSelector(chainOrScanHeightSelector);
-  const sale = useCoretimeSale();
+  const sale = useCoretimeSale() || {};
   const {
     initIndexer: { blockHeight: initBlockHeight } = {},
     info: { saleStart } = {},
-  } = sale || {};
+  } = sale;
 
-  return chainHeight >= initBlockHeight && chainHeight <= saleStart;
+  const isLoading = isNil(chainHeight);
+
+  const isHistoricalSale = "endIndexer" in sale;
+  if (isHistoricalSale) {
+    return {
+      isLoading: false,
+      isInterludePhase: false,
+    };
+  }
+
+  const isInterludePhase =
+    chainHeight >= initBlockHeight && chainHeight <= saleStart;
+
+  return {
+    isLoading,
+    isInterludePhase,
+  };
 }
