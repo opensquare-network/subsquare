89d206c0575979d4e34371009832bd7fde57e877
leochen <leochen.sat24@gmail.com>
2024-11-27 17:07:51 +0800

    Modify useProfileOnChainProxies, #5093

--- packages/next-common/components/profile/proxy/hooks/useProfileOnChainProxies.js
+++ packages/next-common/components/profile/proxy/hooks/useProfileOnChainProxies.js
@@ -1,7 +1,9 @@
-import useSubStorage from "next-common/hooks/common/useSubStorage";
+import { useContextApi } from "next-common/context/api";
+import useCall from "next-common/utils/hooks/useCall";
 
 export default function useProfileOnChainProxies(address) {
-  const { result, loading } = useSubStorage("proxy", "proxies", [address]);
+  const api = useContextApi();
+  const { value, loaded } = useCall(api?.query.proxy?.proxies, [address]);
 
-  return { proxies: result?.toJSON(), loading };
+  return { proxies: value?.toJSON(), loading: !loaded };
 }
