6e77938c90265c7afa2becd184cad1e975b14680
2nthony <ant.hu@outlook.com>
2024-11-27 17:12:47 +0800

    fix overview page

--- packages/next/pages/assethub/index.jsx
+++ packages/next/pages/assethub/index.jsx
@@ -1,14 +1,16 @@
 import { AssetMetadataProvider } from "next-common/components/assets/context/assetMetadata";
 import NoWalletConnected from "next-common/components/assets/noWalletConnected";
-import ListLayout from "next-common/components/layout/ListLayout";
+import WalletAssetList from "next-common/components/assets/walletAssetList";
 import ApiProvider from "next-common/context/api";
-import ChainProvider, { useChainSettings } from "next-common/context/chain";
+import ChainProvider from "next-common/context/chain";
 import RelayInfoProvider from "next-common/context/relayInfo";
 import { withCommonProps } from "next-common/lib";
 import { createStore } from "next-common/store";
 import { commonReducers } from "next-common/store/reducers";
+import multiAccountsSlice from "next-common/store/reducers/multiAccountsSlice";
 import { CHAIN } from "next-common/utils/constants";
 import getChainSettings from "next-common/utils/consts/settings";
+import useExistentialDeposit from "next-common/utils/hooks/chain/useExistentialDeposit";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { Provider } from "react-redux";
 
@@ -21,7 +23,10 @@ if (isAssetHubSupported) {
   chain = `${CHAIN}-assethub`;
   store = createStore({
     chain,
-    reducer: commonReducers,
+    reducer: {
+      ...commonReducers,
+      multiAccounts: multiAccountsSlice,
+    },
   });
 }
 
@@ -42,20 +47,14 @@ export default function AssetHubPage() {
 }
 
 function AssetHubOverviewPageImpl() {
-  const { description } = useChainSettings();
   const realAddress = useRealAddress();
+  useExistentialDeposit();
 
   if (!realAddress) {
     return <NoWalletConnected />;
   }
 
-  return (
-    <ListLayout title="Asset Hub" description={description}>
-      <div className="space-y-6">
-        <div>Asset Hub page</div>
-      </div>
-    </ListLayout>
-  );
+  return <WalletAssetList />;
 }
 
 export const getServerSideProps = async (ctx) => {
