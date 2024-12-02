c854ec6f18c1649d44fd5325329024532dfd14f0
Chaojun Huang <chaojun@opensquare.network>
2024-12-02 16:20:52 +0800

    Astar council threshold is 2/3, #5089 (#5121)
    * Astar council threshold is 2/3(Shibuya is 1/2), #5089
    
    * fix, #5089

--- packages/next-common/components/summary/newCouncilMotionProposalButton/common/councilProposeButton.jsx
+++ packages/next-common/components/summary/newCouncilMotionProposalButton/common/councilProposeButton.jsx
@@ -1,18 +1,30 @@
 import TxSubmissionButton from "next-common/components/common/tx/txSubmissionButton";
 import { useContextApi } from "next-common/context/api";
+import { useChain } from "next-common/context/chain";
 import { useCollectivePallet } from "next-common/context/collective";
+import Chains from "next-common/utils/consts/chains";
 import useCollectiveMembers from "next-common/utils/hooks/collectives/useCollectiveMembers";
 import { getEventData } from "next-common/utils/sendTransaction";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 
+function getThreshold(members, chain) {
+  if (chain === Chains.shibuya) {
+    return Math.ceil(members?.length / 2);
+  } else if (chain === Chains.astar) {
+    return Math.ceil((members?.length * 2) / 3);
+  }
+  throw new Error(`Unsupported chain: ${chain}`);
+}
+
 export default function CouncilProposeButton({ getTxFunc, loading, disabled }) {
   const router = useRouter();
   const api = useContextApi();
   const pallet = useCollectivePallet();
+  const chain = useChain();
 
   const { members } = useCollectiveMembers();
-  const threshold = Math.floor(members?.length / 2) + 1;
+  const threshold = getThreshold(members, chain);
 
   const getProposeTxFunc = useCallback(() => {
     if (!api) {
