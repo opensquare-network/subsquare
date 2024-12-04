90c996b59c982d580b1c29925d14369be6c601c2
leochen <leochen.sat24@gmail.com>
2024-12-04 18:33:48 +0800

    init addProxy options, #5130

--- packages/next-common/components/myProxies/common/operations/addProxy.jsx
+++ packages/next-common/components/myProxies/common/operations/addProxy.jsx
@@ -1,29 +1,92 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import PrimaryButton from "next-common/lib/button/primary";
 import { SystemAddProxy } from "@osn/icons/subsquare";
 import { ChoiceButton } from "next-common/components/summary/newProposalButton/common";
 import Popup from "next-common/components/popup/wrapper/Popup";
 import PopupWithSigner from "next-common/components/popupWithSigner";
 import SignerWithBalance from "next-common/components/signerPopup/signerWithBalance";
+import TxSubmissionButton from "next-common/components/common/tx/txSubmissionButton";
+import { useSignerAccount } from "next-common/components/popupWithSigner/context";
+import { useContextApi } from "next-common/context/api";
+import AdvanceSettings from "next-common/components/summary/newProposalQuickStart/common/advanceSettings";
+import useAddressComboField from "next-common/components/preImages/createPreimagePopup/fields/useAddressComboField";
+import Select from "next-common/components/select";
 
-function AddProxyForm({ props }) {
+function ProxyTypeSelector({ proxyType, setProxyType }) {
+  const options = [
+    {
+      value: "Any",
+      label: "Any",
+    },
+    {
+      value: "NonTransfer",
+      label: "NonTransfer",
+    },
+    {
+      value: "Governance",
+      label: "Governance",
+    },
+    {
+      value: "Staking",
+      label: "Staking",
+    },
+  ];
   return (
-    <PopupWithSigner title="Add Proxy" {...props} wide>
-      <div className="space-y-4">
-        <SignerWithBalance title="Account" />
-        {/* Add form content for proxy account here */}
-        {/* Including:
-          1. Proxy account address input
-          2. Proxy type selection
-          3. Delay block number setting (if needed)
-          4. Advanced settings (if needed)
-        */}
-      </div>
+    <>
+      <span className="text-textPrimary my-[12px] w-[144px]">Type</span>
+      <Select
+        small
+        className="w-full h-[40px] text12Medium"
+        value={proxyType}
+        options={options}
+        onChange={({ value }) => {
+          setProxyType(value);
+        }}
+      />
+    </>
+  );
+}
+
+// TODO: 1. proxy type optioins
+// TODO: 2. delay options
+// TODO: 3. advance settings
+function PopupContent({ onClose }) {
+  const api = useContextApi();
+  const signerAccount = useSignerAccount();
+  const { value: proxyAccount, component: proxyAccountField } =
+    useAddressComboField({ title: "Proxy Account" });
+  const [proxyType, setProxyType] = useState("Any");
+  // const [delay, setDelay] = useState(0);
+  const delay = 0;
+
+  const getTxFunc = useCallback(() => {
+    if (!api || !signerAccount?.realAddress || !proxyAccount) {
+      return;
+    }
+
+    return api.tx.proxy.addProxy(proxyAccount, proxyType, delay);
+  }, [api, signerAccount, proxyAccount, proxyType, delay]);
+
+  return (
+    <div className="space-y-4">
+      <SignerWithBalance title="Account" />
+      {proxyAccountField}
+      <ProxyTypeSelector proxyType={proxyType} setProxyType={setProxyType} />
+      <AdvanceSettings></AdvanceSettings>
+      <TxSubmissionButton getTxFunc={getTxFunc} onClose={onClose} />
+    </div>
+  );
+}
+
+function AddProxyPopup({ onClose }) {
+  return (
+    <PopupWithSigner title="Add Proxy" onClose={onClose} wide>
+      <PopupContent />
     </PopupWithSigner>
   );
 }
 
-function ProxyTypeSelector({ onSelect, onClose }) {
+function OperationSelector({ onSelect, onClose }) {
   return (
     <Popup wide className="!w-[640px]" title="Set Proxy" onClose={onClose}>
       <div className="flex flex-col !mt-[24px] gap-[16px]">
@@ -38,26 +101,29 @@ function ProxyTypeSelector({ onSelect, onClose }) {
 }
 
 export default function AddProxy() {
-  const [showTypeSelector, setShowTypeSelector] = useState(false);
+  const [showOperationSelector, setShowOperationSelector] = useState(false);
   const [showAddForm, setShowAddForm] = useState(false);
 
   return (
     <>
-      <PrimaryButton size="small" onClick={() => setShowTypeSelector(true)}>
+      <PrimaryButton
+        size="small"
+        onClick={() => setShowOperationSelector(true)}
+      >
         Set Proxy
       </PrimaryButton>
 
-      {showTypeSelector && (
-        <ProxyTypeSelector
+      {showOperationSelector && (
+        <OperationSelector
           onSelect={() => {
             setShowAddForm(true);
-            setShowTypeSelector(false);
+            setShowOperationSelector(false);
           }}
-          onClose={() => setShowTypeSelector(false)}
+          onClose={() => setShowOperationSelector(false)}
         />
       )}
 
-      {showAddForm && <AddProxyForm onClose={() => setShowAddForm(false)} />}
+      {showAddForm && <AddProxyPopup onClose={() => setShowAddForm(false)} />}
     </>
   );
 }
