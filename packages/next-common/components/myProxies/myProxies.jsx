129712ae789f991429f73c2c9ba2fc6901f7e4fa
leochen <leochen.sat24@gmail.com>
2024-12-04 15:57:04 +0800

    remove paginations from myProxies, #5125

--- packages/next-common/components/myProxies/myProxies.jsx
+++ packages/next-common/components/myProxies/myProxies.jsx
@@ -1,5 +1,4 @@
 import { MapDataList } from "next-common/components/dataList";
-import { defaultPageSize } from "next-common/utils/constants";
 import { useEffect, useState } from "react";
 import {
   delegateeColumn,
@@ -8,19 +7,13 @@ import {
   removeColumn,
 } from "./common/columns";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
-import usePaginationComponent from "next-common/components/pagination/usePaginationComponent";
 import HeaderPrompt from "next-common/components/profile/proxy/common/headerPrompt";
 import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
 import { useMyProxiesContext } from "./context/myProxies";
 
 export default function MyProxies() {
   const [dataList, setDataList] = useState([]);
-  const { proxies, total, loading } = useMyProxiesContext();
-
-  const { page, component: pageComponent } = usePaginationComponent(
-    total,
-    defaultPageSize,
-  );
+  const { proxies, loading } = useMyProxiesContext();
 
   const delayBlockOrTimeColumn = useDelayBlockOrTimeColumn();
   const columns = [
@@ -35,10 +28,8 @@ export default function MyProxies() {
       return;
     }
 
-    const startIndex = (page - 1) * defaultPageSize;
-    const endIndex = startIndex + defaultPageSize;
-    setDataList(proxies?.slice(startIndex, endIndex));
-  }, [proxies, page, loading]);
+    setDataList(proxies);
+  }, [proxies, loading]);
 
   return (
     <SecondaryCard className="space-y-4">
@@ -54,7 +45,6 @@ export default function MyProxies() {
           columnsDef={columns}
           data={dataList}
         />
-        {total > 0 && pageComponent}
       </SignerPopupWrapper>
     </SecondaryCard>
   );
--- packages/next-common/components/profile/proxy/myProxies.jsx
+++ packages/next-common/components/profile/proxy/myProxies.jsx
@@ -1,5 +1,4 @@
 import { MapDataList } from "next-common/components/dataList";
-import { defaultPageSize } from "next-common/utils/constants";
 import { useEffect, useState } from "react";
 import {
   delegateeColumn,
@@ -8,17 +7,11 @@ import {
 } from "./common/columns";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import { useMyProxiesContext } from "./context/myProxies";
-import usePaginationComponent from "next-common/components/pagination/usePaginationComponent";
 import HeaderPrompt from "./common/headerPrompt";
 
 export default function MyProxies() {
-  const { data, total, loading } = useMyProxiesContext();
+  const { data, loading } = useMyProxiesContext();
   const [dataList, setDataList] = useState([]);
-
-  const { page, component: pageComponent } = usePaginationComponent(
-    total,
-    defaultPageSize,
-  );
 
   const delayBlockOrTimeColumn = useDelayBlockOrTimeColumn();
   const columns = [delegateeColumn, typeColumn, delayBlockOrTimeColumn];
@@ -28,10 +21,8 @@ export default function MyProxies() {
       return;
     }
 
-    const startIndex = (page - 1) * defaultPageSize;
-    const endIndex = startIndex + defaultPageSize;
-    setDataList(data?.slice(startIndex, endIndex));
-  }, [data, page, loading]);
+    setDataList(data);
+  }, [data, loading]);
 
   return (
     <SecondaryCard className="space-y-4">
@@ -46,7 +37,6 @@ export default function MyProxies() {
         columnsDef={columns}
         data={dataList}
       />
-      {total > 0 && pageComponent}
     </SecondaryCard>
   );
 }
