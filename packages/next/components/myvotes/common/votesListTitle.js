9f6c8e122938aed17c3025e5305c468d32c6cc19
2nthony <hi@2nthony.com>
2024-12-04 20:11:57 +0800

    Revert "fix: tabs component a11y (#5115)" (#5140)
    This reverts commit ba92b5bf1d6d3ac0e3fc043498836eaae89652c0.

--- packages/next/components/myvotes/common/votesListTitle.js
+++ packages/next/components/myvotes/common/votesListTitle.js
@@ -2,7 +2,7 @@ import { Title } from "../styled";
 
 export default function VotesListTitle({ length, disabled }) {
   return (
-    <div className="flex gap-[8px]" role="button">
+    <div className="flex gap-[8px]">
       <Title disabled={disabled}>On-chain Votes</Title>
       <span className="text-textTertiary text16Medium">{length || 0}</span>
     </div>
