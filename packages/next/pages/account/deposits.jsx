dc29adeb8821f776c98995d672b627cea9e96a26
leochen <leochen.sat24@gmail.com>
2024-12-06 15:22:29 +0800

    add provider into account deposits, #5148

--- packages/next/pages/account/deposits.jsx
+++ packages/next/pages/account/deposits.jsx
@@ -1,11 +1,14 @@
 import MyDeposits from "next-common/components/myDeposits";
 import AccountLayout from "next-common/components/layout/AccountLayout";
 import { getServerSideProps as getServerSidePropsFn } from "./votes";
+import { MyProxiesProvider } from "next-common/components/myProxies/context/myProxies";
 
 export default function AccountDepositsPage() {
   return (
     <AccountLayout>
-      <MyDeposits />
+      <MyProxiesProvider>
+        <MyDeposits />
+      </MyProxiesProvider>
     </AccountLayout>
   );
 }
