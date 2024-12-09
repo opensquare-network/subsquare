486f5f4b503dd4cb9f1022bf0190d3c10c2ed9a2
Yongfeng LI <wliyongfeng@gmail.com>
2024-12-09 18:05:49 +0800

    Use statescan graphql for kusama/polkadot, #5158 (#5161)

--- packages/next-common/services/multisig/url.js
+++ packages/next-common/services/multisig/url.js
@@ -1,7 +1,11 @@
 import getChainSettings from "next-common/utils/consts/settings";
+import Chains from "next-common/utils/consts/chains";
 
 export default function getMultisigApiUrl(chain) {
   const settings = getChainSettings(chain);
+  if ([Chains.kusama, Chains.polkadot].includes(chain)) {
+    return `https://${settings.graphqlApiSubDomain}.statescan.io/graphql`;
+  }
   if (!settings?.multisigApiPrefix) {
     throw new Error(`Can not find multisig settings for ${chain}`);
   }
