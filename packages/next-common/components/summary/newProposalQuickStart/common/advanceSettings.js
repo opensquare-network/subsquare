78ff3014bd0bfc675bf4b311d5603801c8d1352d
leochen <leochen.sat24@gmail.com>
2024-12-10 11:25:56 +0800

    Modify cursor pointer, #5164

--- packages/next-common/components/summary/newProposalQuickStart/common/advanceSettings.js
+++ packages/next-common/components/summary/newProposalQuickStart/common/advanceSettings.js
@@ -7,11 +7,11 @@ export default function AdvanceSettings({ children }) {
   const [showAdvanced, setShowAdvanced] = useState(false);
   return (
     <>
-      <div className="cursor-pointer flex items-center justify-center w-full gap-4">
+      <div className="flex items-center justify-center w-full gap-4">
         <Divider className="flex-1" />
         <div
           onClick={() => setShowAdvanced(!showAdvanced)}
-          className="inline-flex items-center gap-1.5 border border-neutral400 rounded-[6px] py-1.5 pl-3 pr-1.5"
+          className="cursor-pointer inline-flex items-center gap-1.5 border border-neutral400 rounded-[6px] py-1.5 pl-3 pr-1.5"
         >
           <span className="text12Medium text-textPrimary">
             Advanced Settings
