7779c963810b01b7af7327e599efee30c1a46da8
leochen <leochen.sat24@gmail.com>
2024-12-10 11:21:37 +0800

    Improve advanced text on some popups, #5164

--- packages/next-common/components/summary/newProposalQuickStart/common/advanceSettings.js
+++ packages/next-common/components/summary/newProposalQuickStart/common/advanceSettings.js
@@ -1,17 +1,29 @@
 import { useState } from "react";
+import { ArrowDown } from "@osn/icons/subsquare";
+import { cn } from "next-common/utils";
+import Divider from "next-common/components/styled/layout/divider";
 
 export default function AdvanceSettings({ children }) {
   const [showAdvanced, setShowAdvanced] = useState(false);
   return (
     <>
-      <div className="flex items-center justify-between">
-        <span className="text12Bold text-textPrimary">Advanced</span>
-        <span
-          className="cursor-pointer text12Medium text-theme500"
+      <div className="cursor-pointer flex items-center justify-center w-full gap-4">
+        <Divider className="flex-1" />
+        <div
           onClick={() => setShowAdvanced(!showAdvanced)}
+          className="inline-flex items-center gap-1.5 border border-neutral400 rounded-[6px] py-1.5 pl-3 pr-1.5"
         >
-          {showAdvanced ? "Hide" : "Settings"}
-        </span>
+          <span className="text12Medium text-textPrimary">
+            Advanced Settings
+          </span>
+          <ArrowDown
+            className={cn(
+              showAdvanced && "rotate-180",
+              "w-4 h-4 [&_path]:stroke-textTertiary",
+            )}
+          />
+        </div>
+        <Divider className="flex-1" />
       </div>
       <div className={showAdvanced ? "space-y-4" : "hidden"}>{children}</div>
     </>
