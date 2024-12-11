4a58116beede3eb93443679a042529619c75f063
leochen <leochen.sat24@gmail.com>
2024-12-11 14:49:18 +0800

    add useSubAssetBalanceDeps, #5180

--- packages/next-common/components/assets/useSubAssetBalance.js
+++ packages/next-common/components/assets/useSubAssetBalance.js
@@ -1,4 +1,63 @@
 import useSubStorage from "next-common/hooks/common/useSubStorage";
+import useAllAssetMetadata from "next-common/components/assets/context/assetMetadata";
+import { useMemo, useState, useEffect } from "react";
+import { useContextApi } from "next-common/context/api";
+import BigNumber from "bignumber.js";
+
+export function useMultiAccountKeyDeps(address) {
+  const [allMetadata] = useAllAssetMetadata();
+  const multiAccountKey = useMemo(
+    () => allMetadata?.map((item) => [item.assetId, address]),
+    [allMetadata, address],
+  );
+  return multiAccountKey;
+}
+
+export function useSubAssetBalanceDeps(address) {
+  const api = useContextApi();
+  const multiAccountKey = useMultiAccountKeyDeps(address);
+  const [multiAccounts, setMultiAccounts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!api || !multiAccountKey) {
+      return;
+    }
+
+    api.query.assets.account
+      .multi(multiAccountKey)
+      .then((result) => {
+        const multiAccounts = result
+          ?.map((option) => {
+            if (option.isNone) {
+              return null;
+            }
+
+            const { balance, status = {} } = option.unwrap();
+            const { isFrozen } = status;
+
+            return {
+              balance: balance.toJSON(),
+              status: {
+                isFrozen,
+              },
+            };
+          })
+          .filter(Boolean)
+          .filter((item) => !new BigNumber(item.balance || 0).isZero());
+
+        setMultiAccounts(multiAccounts);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [api, multiAccountKey]);
+
+  return {
+    multiAccounts,
+    loading,
+  };
+}
 
 export default function useSubAssetBalance(assetId, address) {
   const { result, loading } = useSubStorage("assets", "account", [
