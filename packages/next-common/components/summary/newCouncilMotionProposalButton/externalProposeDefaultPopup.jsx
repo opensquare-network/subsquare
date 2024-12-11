2190188624ae0d1df2e635126919486e3d282203
Chaojun Huang <chaojun@opensquare.network>
2024-12-11 10:59:15 +0800

    Should be 100% threshold for main council of astar/shibuya (#5187)
    * should be 100% threshold for main council of astar/shibuya
    
    * fix and refactor
    
    * Rename variable

--- packages/next-common/components/summary/newCouncilMotionProposalButton/externalProposeDefaultPopup.jsx
+++ packages/next-common/components/summary/newCouncilMotionProposalButton/externalProposeDefaultPopup.jsx
@@ -1,11 +1,25 @@
+import Chains from "next-common/utils/consts/chains";
 import ExternalProposeVoteThresholdPopup from "./common/externalProposeVoteThresholdPopup";
+import { useCollectivePallet } from "next-common/context/collective";
+import { useChain } from "next-common/context/chain";
+import useCollectiveMembers from "next-common/utils/hooks/collectives/useCollectiveMembers";
 
 export default function ExternalProposeDefaultPopup({ isMember, onClose }) {
+  const chain = useChain();
+  const { members } = useCollectiveMembers();
+  const pallet = useCollectivePallet();
+
+  let threshold = null;
+  if ([Chains.shibuya, Chains.astar].includes(chain) && pallet === "council") {
+    threshold = members?.length;
+  }
+
   return (
     <ExternalProposeVoteThresholdPopup
       method="externalProposeDefault"
       isMember={isMember}
       onClose={onClose}
+      threshold={threshold}
     />
   );
 }
