fa8856c747880aab828bf5bdd697ad4c71828c01
2nthony <ant.hu@outlook.com>
2024-12-13 10:44:59 +0800

    add Paseo assethub to network node #5136

--- packages/next-common/utils/constants.js
+++ packages/next-common/utils/constants.js
@@ -32,6 +32,7 @@ import astar from "next-common/utils/consts/settings/astar";
 import zkverifyTestnet from "./consts/settings/zkverifyTestnet";
 import paseo from "./consts/settings/paseo";
 import ajuna from "./consts/settings/ajuna";
+import paseoAssetHub from "./consts/settings/paseoAssetHub";
 
 const optionalNodes =
   process.env.NEXT_PUBLIC_DEVELOPMENT === "true" ? [development] : [];
@@ -72,6 +73,7 @@ export const nodes = [
   vara,
   zkverifyTestnet,
   paseo,
+  paseoAssetHub,
   ...optionalNodes,
 ];
 
