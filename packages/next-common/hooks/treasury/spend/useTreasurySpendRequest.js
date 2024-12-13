7c6f1457e0d37a61b82385d66d32e31d86bb0a0f
Yongfeng LI <wliyongfeng@gmail.com>
2024-12-13 23:47:08 +0800

    Fix treasury spend meta with null assetKind

--- packages/next-common/hooks/treasury/spend/useTreasurySpendRequest.js
+++ packages/next-common/hooks/treasury/spend/useTreasurySpendRequest.js
@@ -3,12 +3,13 @@ import {
   getParachainIdByMeta,
 } from "next-common/utils/treasury/spend/usdCheck";
 import getSpendBeneficiaryFromMeta from "next-common/utils/treasury/spend/beneficiary";
-import { useChain } from "next-common/context/chain";
+import { useChain, useChainSettings } from "next-common/context/chain";
 
 export default function useTreasurySpendRequest(meta) {
   const chain = useChain();
+  const chainSettings = useChainSettings();
   const parachain = getParachainIdByMeta(meta);
-  const asset = getAssetByMeta(meta);
+  const asset = getAssetByMeta(meta, chainSettings);
   const beneficiary = getSpendBeneficiaryFromMeta(meta, chain);
 
   return {
