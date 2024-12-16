32d7d6b2865eb39c158bd8acb6d3e40d3a0bf115
2nthony <ant.hu@outlook.com>
2024-12-16 15:37:17 +0800

    `CoreFellowshipMemberInfoWrapper` props

--- packages/next-common/components/collectives/core/member/infoWrapper.js
+++ packages/next-common/components/collectives/core/member/infoWrapper.js
@@ -1,3 +1,16 @@
-export default function CoreFellowshipMemberInfoWrapper({ children }) {
-  return <div className="grow truncate basis-1/2">{children}</div>;
+import { cn } from "next-common/utils";
+
+/**
+ * @param {import("react").HTMLAttributes<HTMLDivElement>} props
+ */
+export default function CoreFellowshipMemberInfoWrapper({
+  children,
+  className,
+  ...props
+}) {
+  return (
+    <div className={cn("grow truncate basis-1/2", className)} {...props}>
+      {children}
+    </div>
+  );
 }
