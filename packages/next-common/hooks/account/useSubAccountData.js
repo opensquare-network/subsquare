5239710bf95b2c8651ae9dd1e0937e0cc92fb1a6
2nthony <ant.hu@outlook.com>
2024-12-18 15:12:06 +0800

    useSubAccountData #5190
    formerly `useOnChainAccountData`

---
+++ packages/next-common/hooks/account/useSubAccountData.js
@@ -0,0 +1,51 @@
+// useOnChainAccountData.js
+
+import { useContextApi } from "next-common/context/api";
+import useSubStorage from "../common/useSubStorage";
+import { useEffect, useMemo, useState } from "react";
+import { every, values } from "lodash-es";
+
+export function useSubAccountData(address) {
+  const [value, setValue] = useState(null);
+
+  const api = useContextApi();
+  const { result: account } = useSubStorage("system", "account", [address]);
+
+  function update(newVal) {
+    setValue((val) => {
+      return { ...val, ...newVal };
+    });
+  }
+
+  useEffect(() => {
+    if (account) {
+      update({ account });
+    }
+  }, [account]);
+
+  useEffect(() => {
+    if (!api || !address || !account) {
+      return;
+    }
+
+    api.derive.balances.all(address, (balanceAll) => {
+      update({ balanceAll });
+    });
+
+    api.derive.staking.account(address, (stakingInfo) => {
+      update({ stakingInfo });
+    });
+  }, [account, address, api]);
+
+  const data = useMemo(() => {
+    if (every(values(value), Boolean)) {
+      return value;
+    }
+
+    return null;
+  }, [value]);
+
+  const loading = data === null;
+
+  return { data, isLoading: loading };
+}
