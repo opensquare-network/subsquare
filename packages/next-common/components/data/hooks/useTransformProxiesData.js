fcdb7d69cab9a3c95ca466887bef92683f26f98c
leochen <leochen.sat24@gmail.com>
2024-12-19 11:38:08 +0800

    fix: lint error, #5239

--- packages/next-common/components/data/hooks/useTransformProxiesData.js
+++ packages/next-common/components/data/hooks/useTransformProxiesData.js
@@ -30,7 +30,7 @@ export default function useTransformProxiesData(data = [], initialLoading) {
     setTimeout(() => {
       setLoading(false);
     }, 300);
-  }, [data]);
+  }, [data, initialLoading]);
 
   return { result, loading };
 }
