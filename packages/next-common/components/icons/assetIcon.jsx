ccca9ac9e794ba10180473c75a681619a2a0c7dc
leochen <leochen.sat24@gmail.com>
2024-12-19 14:22:08 +0800

    Fix: show usdt icon

--- packages/next-common/components/icons/assetIcon.jsx
+++ packages/next-common/components/icons/assetIcon.jsx
@@ -1,8 +1,14 @@
 import { find } from "lodash-es";
 import knownPolkadotAssetHubAssets from "../assets/known/polkadot";
 
+function transformSymbol(symbol) {
+  return symbol === "USDT" ? "USDt" : symbol;
+}
+
 export default function AssetIcon({ symbol, className = "" }) {
-  const foundAsset = find(knownPolkadotAssetHubAssets, { symbol });
+  const foundAsset = find(knownPolkadotAssetHubAssets, {
+    symbol: transformSymbol(symbol),
+  });
 
   return foundAsset?.icon && <foundAsset.icon className={className} />;
 }
