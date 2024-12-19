342aa882a5223265f4282587c3f1913f4eb0c9bb
leochen <leochen.sat24@gmail.com>
2024-12-19 16:50:55 +0800

    ProxyExplorerHeader, #5239

--- packages/next-common/components/data/proxies/index.jsx
+++ packages/next-common/components/data/proxies/index.jsx
@@ -1,10 +1,12 @@
 import ProxyExplorerTable from "./table";
 import { AllProxiesProvider } from "next-common/components/data/context/allProxies";
+import ProxyExplorerHeader from "./header";
 
 export default function ProxyExplorer() {
   return (
     <>
       <AllProxiesProvider>
+        <ProxyExplorerHeader />
         <ProxyExplorerTable />
       </AllProxiesProvider>
     </>
