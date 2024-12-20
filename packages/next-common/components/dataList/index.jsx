75906cb5cac68b3fd91c1ee2e4674e2a848647e7
leochen <leochen.sat24@gmail.com>
2024-12-20 15:11:22 +0800

    remove useless code, #5239

--- packages/next-common/components/dataList/index.jsx
+++ packages/next-common/components/dataList/index.jsx
@@ -182,28 +182,3 @@ export function MapDataList({ data, columnsDef, getRowKey, ...props }) {
 
   return <DataList columns={columnsDef} rows={rows} {...props} />;
 }
-
-export function TreeMapDataList({
-  treeKey = "children",
-  data,
-  columnsDef,
-  getRowKey,
-  ...props
-}) {
-  const rows = (data || []).map((item, index) => {
-    const row = columnsDef.map(({ render }) => render(item));
-    row.key = getRowKey ? getRowKey(item) : index;
-    return row;
-  });
-
-  return (
-    <DataList
-      columns={columnsDef}
-      rows={rows}
-      {...props}
-      treeKey={treeKey}
-      treeData={data}
-      tree={true}
-    />
-  );
-}
