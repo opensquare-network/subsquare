d315a970437579faae10cc543a6d7f4fc401cf08
leochen <leochen.sat24@gmail.com>
2024-12-23 18:26:11 +0800

    add debounce & modify search filter, #5256

--- packages/next-common/components/data/hooks/useSearchByAddressIdentity.js
+++ packages/next-common/components/data/hooks/useSearchByAddressIdentity.js
@@ -2,6 +2,8 @@ import { useChainSettings } from "next-common/context/chain";
 import { fetchIdentity } from "next-common/services/identity";
 import { getIdentityDisplay } from "next-common/utils/identity";
 import { useEffect, useMemo, useState } from "react";
+import { isAddress } from "@polkadot/util-crypto";
+import { useDebounce } from "react-use";
 
 async function fetchBatchIdentities(identityChain, accounts = []) {
   const results = await Promise.all(
@@ -27,7 +29,16 @@ export default function useSearchByAddressIdentity(
   allProxies = [],
 ) {
   const [identityMapping, setIdentityMapping] = useState({});
+  const [debouncedSearchValue, setDebouncedSearchValue] = useState("");
   const { identity: identityChain } = useChainSettings();
+
+  useDebounce(
+    () => {
+      setDebouncedSearchValue(searchValue);
+    },
+    500,
+    [searchValue],
+  );
 
   useEffect(() => {
     const allAccounts = new Set();
@@ -48,23 +59,28 @@ export default function useSearchByAddressIdentity(
   }, [allProxies, identityChain]);
 
   return useMemo(() => {
-    const search = (searchValue || "").toLowerCase();
+    const search = (debouncedSearchValue || "").toLowerCase();
+
     if (!search) {
       return allProxies;
     }
 
-    return allProxies?.filter(({ delegator, items }) => {
-      const addressMatch =
-        delegator.toLowerCase().includes(search) ||
-        items.some(({ delegatee }) => delegatee.toLowerCase().includes(search));
+    if (isAddress(debouncedSearchValue)) {
+      return allProxies?.filter(
+        ({ delegator, items }) =>
+          delegator.toLowerCase() === search ||
+          items.some(({ delegatee }) => delegatee.toLowerCase() === search),
+      );
+    }
 
+    return allProxies?.filter(({ delegator, items }) => {
       const identityMatch =
         identityMapping[delegator]?.includes(search) ||
         items.some(({ delegatee }) =>
           identityMapping[delegatee]?.includes(search),
         );
 
-      return addressMatch || identityMatch;
+      return identityMatch;
     });
-  }, [searchValue, allProxies, identityMapping]);
+  }, [debouncedSearchValue, allProxies, identityMapping]);
 }
