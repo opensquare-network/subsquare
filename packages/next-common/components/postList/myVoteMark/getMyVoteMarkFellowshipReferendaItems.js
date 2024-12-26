dc465ab66cf02d43877e686af03cfd1885d98d11
2nthony <ant.hu@outlook.com>
2024-12-26 14:17:28 +0800

    remove redundant assertion

--- packages/next-common/components/postList/myVoteMark/getMyVoteMarkFellowshipReferendaItems.js
+++ packages/next-common/components/postList/myVoteMark/getMyVoteMarkFellowshipReferendaItems.js
@@ -1,12 +1,6 @@
 // packages/next-common/utils/hooks/fellowship/useFellowshipVotes.js `normalizeVotingRecord`
 
-import { isNil } from "lodash-es";
-
 export function getMyVoteMarkFellowshipReferendaItems(myVote) {
-  if (isNil(myVote)) {
-    return null;
-  }
-
   const { vote } = myVote;
 
   return [
