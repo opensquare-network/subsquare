8fd777434a117c45b4368a170f97d55d18210d88
2nthony <ant.hu@outlook.com>
2025-01-03 10:24:36 +0800

    test: add multi relative links

--- packages/next-common/utils/post/ensurePolkassemblyRelativeLink.test.js
+++ packages/next-common/utils/post/ensurePolkassemblyRelativeLink.test.js
@@ -39,4 +39,15 @@ describe("transform polkassembly relative link", () => {
       '<a href="https://polkadot.polkassembly.io/referenda/1357">link</a>',
     );
   });
+
+  it("multi relative links", () => {
+    expect(
+      ensurePolkassemblyRelativeLink(
+        '<a href="../referenda/1357">referenda link</a><a href="../post/1234">post link</a>',
+        "polkadot",
+      ),
+    ).toBe(
+      '<a href="https://polkadot.polkassembly.io/referenda/1357">referenda link</a><a href="https://polkadot.polkassembly.io/post/1234">post link</a>',
+    );
+  });
 });
