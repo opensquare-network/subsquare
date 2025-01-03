35c854e39f123a150873954989fe4d2c1a9b3410
2nthony <hi@2nthony.com>
2025-01-03 18:43:24 +0800

    fix: polkassembly post content relative link url #5306 (#5321)
    * ensure polkassembly post content url
    
    * Revert "ensure polkassembly post content url"
    
    This reverts commit 234a47ad856be4abeb336617c3e8f034032d649f.
    
    * fix: polkassembly post content relative link url
    
    * extract util and add tests
    
    * refactor
    
    * rename
    
    * test: add multi relative links

---
+++ packages/next-common/utils/post/ensurePolkassemblyRelativeLink.test.js
@@ -0,0 +1,53 @@
+/* eslint-disable quotes */
+import { describe, expect, it } from "vitest";
+import { ensurePolkassemblyRelativeLink } from "./ensurePolkassemblyRelativeLink";
+
+describe("transform polkassembly relative link", () => {
+  it("no href attr, replace nothing", () => {
+    expect(
+      ensurePolkassemblyRelativeLink('<a class="pa-link">link</a>', "polkadot"),
+    ).toBe('<a class="pa-link">link</a>');
+  });
+
+  it("relative post", () => {
+    // double quotes
+    expect(
+      ensurePolkassemblyRelativeLink(
+        '<a class="pa-link" href="../post/2592" target="_blank">link</a>',
+        "polkadot",
+      ),
+    ).toBe(
+      '<a class="pa-link" href="https://polkadot.polkassembly.io/post/2592" target="_blank">link</a>',
+    );
+
+    // single quotes
+    expect(
+      ensurePolkassemblyRelativeLink(
+        "<a href='../post/2592'>link</a>",
+        "polkadot",
+      ),
+    ).toBe("<a href='https://polkadot.polkassembly.io/post/2592'>link</a>");
+  });
+
+  it("relative referenda", () => {
+    expect(
+      ensurePolkassemblyRelativeLink(
+        '<a href="../referenda/1357">link</a>',
+        "polkadot",
+      ),
+    ).toBe(
+      '<a href="https://polkadot.polkassembly.io/referenda/1357">link</a>',
+    );
+  });
+
+  it("multi relative links", () => {
+    expect(
+      ensurePolkassemblyRelativeLink(
+        '<a href="../referenda/1357">referenda link</a><a href="../post/1234">post link</a>',
+        "polkadot",
+      ),
+    ).toBe(
+      '<a href="https://polkadot.polkassembly.io/referenda/1357">referenda link</a><a href="https://polkadot.polkassembly.io/post/1234">post link</a>',
+    );
+  });
+});
