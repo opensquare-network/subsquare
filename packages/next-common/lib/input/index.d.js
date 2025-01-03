ad5da1edf8c65c5e042e03f86c4bdefd4b32b0b5
2nthony <ant.hu@outlook.com>
2025-01-03 20:20:22 +0800

    feat(lib): init input #5323

---
+++ packages/next-common/lib/input/index.d.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {Omit<React.InputHTMLAttributes, 'size' | 'prefix'> & {
+ * prefix?: React.ReactNode
+ * suffix?: React.ReactNode
+ * pre?: React.ReactNode
+ * post?: React.ReactNode
+ * error?: React.ReactNode
+ * size?: 'small'
+ * symbol?: string
+ * }} InputProps
+ */
