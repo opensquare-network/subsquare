73f3f137bcb54e397d996fd33406699f423f31e4
2nthony <ant.hu@outlook.com>
2025-01-06 17:24:39 +0800

    feat: number input, formerly currency input

---
+++ packages/next-common/lib/input/number/index.d.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {{
+ * allowDecimals?: boolean
+ * controls?: boolean
+ * keyboard?: boolean
+ * step?: number | string
+ * min?: number | string
+ * max?: number | string
+ * } & InputProps} NumberInputProps
+ */
