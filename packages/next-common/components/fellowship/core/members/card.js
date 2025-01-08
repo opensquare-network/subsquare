eb7a88d57e82a59b1c0546c8bd0c0e0babc35fe8
chaojun <chaojun@opensquare.network>
2025-01-08 11:10:34 +0800

    Show empty non-core card, #5315

--- packages/next-common/components/fellowship/core/members/card.js
+++ packages/next-common/components/fellowship/core/members/card.js
@@ -1,22 +1,13 @@
-import React, { useMemo } from "react";
+import React from "react";
 import Actions from "next-common/components/fellowship/core/members/actions";
-import useSubFellowshipCoreMember from "next-common/hooks/fellowship/core/useSubFellowshipCoreMember";
 import CoreFellowshipMemberCard from "next-common/components/collectives/core/member/card";
+import NonCoreFellowshipMemberCard from "./nonCoreCard";
 
-export default function FellowshipCoreMemberCard({
-  member: _member = {},
-  params = {},
-}) {
-  const { member: statusFromStorage } = useSubFellowshipCoreMember(
-    _member.address,
-  );
-
-  const member = useMemo(() => {
-    return {
-      ..._member,
-      status: statusFromStorage || _member.status || {},
-    };
-  }, [_member, statusFromStorage]);
+export default function FellowshipCoreMemberCard({ member = {}, params = {} }) {
+  const { isFellowshipOnly } = member;
+  if (!isFellowshipOnly) {
+    return <NonCoreFellowshipMemberCard member={member} params={params} />;
+  }
 
   return (
     <CoreFellowshipMemberCard
