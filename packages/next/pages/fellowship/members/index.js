b819a861b66cb55bbb6b6a9894e3dcfb08f335c2
chaojun <chaojun@opensquare.network>
2025-01-10 16:07:26 +0800

    fix: refactor, #5315

--- packages/next/pages/fellowship/members/index.js
+++ packages/next/pages/fellowship/members/index.js
@@ -1,30 +1,6 @@
 import FellowshipMembersPage from "next-common/components/pages/fellowship/members";
-import { withCommonProps } from "next-common/lib";
-import nextApi from "next-common/services/nextApi";
-import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
-import {
-  fellowshipMembersApiUri,
-  fellowshipParamsApi,
-} from "next-common/services/url";
+import getFellowshipMembersServerSideProps from "next-common/services/serverSide/fellowship/members";
 
 export default FellowshipMembersPage;
 
-export const getServerSideProps = withCommonProps(async () => {
-  const [
-    tracksProps,
-    { result: fellowshipParams = {} },
-    { result: fellowshipMembers = [] },
-  ] = await Promise.all([
-    fetchOpenGovTracksProps(),
-    nextApi.fetch(fellowshipParamsApi),
-    nextApi.fetch(fellowshipMembersApiUri),
-  ]);
-
-  return {
-    props: {
-      ...tracksProps,
-      fellowshipParams,
-      fellowshipMembers,
-    },
-  };
-});
+export const getServerSideProps = getFellowshipMembersServerSideProps;
