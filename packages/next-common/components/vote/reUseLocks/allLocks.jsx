642c35ff84da1bb59bc77395d4f214c2e8a89de7
leochen <leochen.sat24@gmail.com>
2025-01-14 18:02:35 +0800

    add common lockButton, #5252

--- packages/next-common/components/vote/reUseLocks/allLocks.jsx
+++ packages/next-common/components/vote/reUseLocks/allLocks.jsx
@@ -1,32 +1,17 @@
-import { useChainSettings } from "next-common/context/chain";
-import ValueDisplay from "next-common/components/valueDisplay";
-import { toPrecision } from "next-common/utils";
 import { useUserAccountInfo } from "next-common/context/user/account";
+import LockButton from "./lockButton";
 
 const AllLocks = ({ reUseAllLocks }) => {
-  const { symbol, decimals, voteSymbol } = useChainSettings();
   const { info, isLoading } = useUserAccountInfo();
   const lockedBalance = info?.data?.lockedBalance;
 
-  if (lockedBalance == 0 || isLoading) {
-    return null;
-  }
-
-  const value = toPrecision(lockedBalance, decimals);
-
   return (
-    <div
-      className="bg-neutral200 py-1.5 px-4 rounded-lg text14Medium space-x-2 hover:bg-neutral300"
-      role="button"
-      onClick={() => reUseAllLocks(value)}
-    >
-      <span className="text-textTertiary">Reuse all locks</span>
-      <ValueDisplay
-        className="text-textPrimary"
-        value={value}
-        symbol={voteSymbol || symbol}
-      />
-    </div>
+    <LockButton
+      isLoading={isLoading}
+      balance={lockedBalance}
+      label="Reuse all locks"
+      onClick={reUseAllLocks}
+    />
   );
 };
 
--- packages/next-common/components/vote/reUseLocks/govLocks.jsx
+++ packages/next-common/components/vote/reUseLocks/govLocks.jsx
@@ -1,34 +1,18 @@
-import { useChainSettings } from "next-common/context/chain";
-import ValueDisplay from "next-common/components/valueDisplay";
-import { toPrecision } from "next-common/utils";
 import useQueryGovernanceLock from "next-common/hooks/referenda/useQueryGovernanceLock";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import LockButton from "./lockButton";
 
 const GovLocks = ({ reUseGovLocks }) => {
-  const { symbol, decimals, voteSymbol } = useChainSettings();
   const address = useRealAddress();
-
   const { balance, isLoading } = useQueryGovernanceLock(address);
 
-  if (balance == 0 || isLoading) {
-    return null;
-  }
-
-  const value = toPrecision(balance, decimals);
-
   return (
-    <div
-      className="bg-neutral200 py-1.5 px-4 rounded-lg text14Medium space-x-2 hover:bg-neutral300"
-      role="button"
-      onClick={() => reUseGovLocks(value)}
-    >
-      <span className="text-textTertiary">Reuse governance lock</span>
-      <ValueDisplay
-        className="text-textPrimary"
-        value={value}
-        symbol={voteSymbol || symbol}
-      />
-    </div>
+    <LockButton
+      isLoading={isLoading}
+      balance={balance}
+      label="Reuse governance lock"
+      onClick={reUseGovLocks}
+    />
   );
 };
 
