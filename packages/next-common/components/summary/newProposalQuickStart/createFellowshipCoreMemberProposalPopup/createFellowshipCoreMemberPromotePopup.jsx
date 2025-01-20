af20215e4fc84c01d8cd03015794ccc092c08037
leochen <leochen.sat24@gmail.com>
2025-01-20 11:13:50 +0800

    auto fill toRank, #5271

--- packages/next-common/components/summary/newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberPromotePopup.jsx
+++ packages/next-common/components/summary/newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberPromotePopup.jsx
@@ -12,6 +12,8 @@ import { ActiveReferendaProvider } from "next-common/context/activeReferenda";
 import useRelatedPromotionReferenda from "next-common/hooks/fellowship/useRelatedPromotionReferenda";
 import { ReferendaWarningMessage } from "./common";
 import WarningInfoPanel from "next-common/components/summary/styled/warningInfoPanel";
+import useFellowshipCoreMembers from "next-common/hooks/fellowship/core/useFellowshipCoreMembers";
+import { find } from "lodash-es";
 
 export function NotAvailableMemberPrompt() {
   return (
@@ -23,6 +25,7 @@ export function NotAvailableMemberPrompt() {
 }
 
 function NewFellowshipCoreMemberPromoteReferendumInnerPopupImpl() {
+  const { members } = useFellowshipCoreMembers();
   const { onClose } = usePopupParams();
   const {
     value: who,
@@ -35,7 +38,8 @@ function NewFellowshipCoreMemberPromoteReferendumInnerPopupImpl() {
   const { value: enactment, component: enactmentField } =
     useEnactmentBlocksField();
 
-  const [toRank, setToRank] = useState();
+  const targetMember = find(members, { address: who });
+  const toRank = targetMember?.rank;
 
   const trackName = getTrackNameFromRank(toRank);
 
@@ -45,7 +49,7 @@ function NewFellowshipCoreMemberPromoteReferendumInnerPopupImpl() {
   return (
     <Popup title="New Promote Proposal" onClose={onClose}>
       {whoField}
-      <RankField title="To Rank" rank={toRank} setRank={setToRank} />
+      <RankField title="To Rank" rank={toRank} readOnly />
       <ReferendaWarningMessage
         isLoading={isLoading}
         relatedReferenda={relatedReferenda}
