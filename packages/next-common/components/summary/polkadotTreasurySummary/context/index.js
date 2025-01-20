23c9ab15c1f17f3b5849d82a86c52f709d19b85e
leochen <leochen.sat24@gmail.com>
2025-01-20 14:14:13 +0800

    Update treasury centrifuge loan to 1.5M, #5402 (#5403)

--- packages/next-common/components/summary/polkadotTreasurySummary/context/index.js
+++ packages/next-common/components/summary/polkadotTreasurySummary/context/index.js
@@ -89,7 +89,7 @@ export function PolkadotTreasurySummaryProvider({ children }) {
         isUsdtTreasuryBalanceOnAssetHubLoading,
         usdcTreasuryBalanceOnAssetHub,
         isUsdcTreasuryBalanceOnAssetHubLoading,
-        loanCentrifugeUsdcBalance: 3000000000000,
+        loanCentrifugeUsdcBalance: 1500000000000,
         loanBifrostDotBalance: 5000000000000000,
         loadPendulumDotBalance: 500000000000000,
         loanHydrationDotBalance: 10000000000000000,
