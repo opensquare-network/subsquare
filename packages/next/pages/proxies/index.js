025254b481ad12c4cc3447f44b1c84d0101bc844
leochen <leochen.sat24@gmail.com>
2025-01-20 20:07:17 +0800

    add data page backgroud pattern and linear-gradient, #5399

--- packages/next/pages/proxies/index.js
+++ packages/next/pages/proxies/index.js
@@ -1,6 +1,7 @@
 import BaseLayout from "next-common/components/layout/baseLayout";
 import ProxyExplorer from "next-common/components/data/proxies";
 import { getServerSidePropsWithTracks } from "next-common/services/serverSide/serverSidePropsWithTracks";
+import { cn } from "next-common/utils";
 
 export default function ProxiesPage() {
   const title = "Proxy Explorer";
@@ -8,7 +9,17 @@ export default function ProxiesPage() {
 
   return (
     <BaseLayout seoInfo={seoInfo}>
-      <div className="flex-1">
+      <div
+        className={cn(
+          "flex-1",
+          "[background:var(--event-light-background)]",
+          "dark:[background:var(--event-dark-background)]",
+        )}
+        style={{
+          "--event-light-background": `url("/project-page-bg-data-light.svg"), repeat, linear-gradient(180deg, #FDFDFD 0%, #F6F7FA 100%)`,
+          "--event-dark-background": `url("/project-page-bg-data-dark.svg"), repeat, linear-gradient(180deg, #212433 0%, #1E2130 100%)`,
+        }}
+      >
         <div className={"px-6 py-6 mx-auto max-w-[1200px] max-sm:px-0"}>
           <ProxyExplorer />
         </div>
