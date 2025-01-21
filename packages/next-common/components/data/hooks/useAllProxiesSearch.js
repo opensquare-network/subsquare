32597b63897858be4f24a00ce32903c9c6544574
2nthony <ant.hu@outlook.com>
2025-01-21 15:13:16 +0800

    refactor: proxies search input

--- packages/next-common/components/data/hooks/useAllProxiesSearch.js
+++ packages/next-common/components/data/hooks/useAllProxiesSearch.js
@@ -5,27 +5,7 @@ import {
 } from "next-common/utils/router";
 import { useRouter } from "next/router";
 import { SystemSearch } from "@osn/icons/subsquare";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  margin: 16px 24px 0 !important;
-  display: flex;
-  border-radius: 8px;
-  border: 1px solid var(--neutral400);
-  background: var(--neutral100);
-`;
-
-const Input = styled.input`
-  flex-grow: 1;
-  border: none;
-  outline: none;
-  background: transparent;
-
-  color: var(--textPrimary);
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-`;
+import Input from "next-common/lib/input";
 
 export default function useAllProxiesSearch() {
   const router = useRouter();
@@ -44,16 +24,15 @@ export default function useAllProxiesSearch() {
   return {
     search: querySearch,
     component: (
-      <Wrapper>
-        <div className="flex p-[8px] [&_path]:fill-textTertiary">
-          <SystemSearch width={24} height={24} />
-        </div>
-        <Input
-          placeholder="Search by identity name or address"
-          defaultValue={querySearch || ""}
-          onChange={handleInputChange}
-        />
-      </Wrapper>
+      <Input
+        className="mt-4 mx-6"
+        prefix={
+          <SystemSearch width={24} height={24} className="text-textTertiary" />
+        }
+        placeholder="Search by identity name or address"
+        defaultValue={querySearch || ""}
+        onChange={handleInputChange}
+      />
     ),
   };
 }
