42cc619393d20e278233fb8d338d684e81a87ff8
leochen <leochen.sat24@gmail.com>
2025-01-24 17:47:57 +0800

    fix: lint error, #5416

--- packages/next-common/hooks/useFiatPrice.js
+++ packages/next-common/hooks/useFiatPrice.js
@@ -105,7 +105,7 @@ export function useFiatPriceBySymbolSnapshot(symbol) {
 
   useEffect(() => {
     getPrice({ variables: { symbol } });
-  }, [getPrice]);
+  }, [getPrice, symbol]);
 
   const price = find(data?.prices, {
     symbol,
