bf95161905741b1e41fa87e1af55a70f3687ac78
leochen <leochen.sat24@gmail.com>
2025-02-05 18:34:27 +0800

    remove loop query, #5441

--- packages/next-common/context/collectives/fellowshipVotes.js
+++ packages/next-common/context/collectives/fellowshipVotes.js
@@ -3,10 +3,8 @@ import { query } from "next-common/utils/hooks/fellowship/useFellowshipVotes";
 import { useReferendumVotingFinishIndexer } from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
 import { useOnchainData } from "next-common/context/post";
 import { isNil } from "lodash-es";
-import { useSelector } from "react-redux";
 import { partition } from "lodash-es";
 import { useContextApi } from "next-common/context/api";
-import { latestHeightSelector } from "next-common/store/reducers/chainSlice";
 
 function useSubFellowshipVotes(pollIndex, indexer) {
   const api = useContextApi();
@@ -15,25 +13,9 @@ function useSubFellowshipVotes(pollIndex, indexer) {
     allNay: [],
   });
   const [isLoading, setIsLoading] = useState(true);
-  const currentHeight = useSelector(latestHeightSelector);
 
   useEffect(() => {
-    if (!api || isNil(pollIndex) || !isNil(indexer)) {
-      return;
-    }
-
-    query(api, pollIndex, indexer?.blockHash)
-      .then((votes) => {
-        const [allAye = [], allNay = []] = partition(votes, (v) => v.isAye);
-        setFellowshipVotes({ allAye, allNay });
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, [api, pollIndex, indexer, currentHeight]);
-
-  useEffect(() => {
-    if (!api || isNil(pollIndex) || isNil(indexer)) {
+    if (!api || isNil(pollIndex)) {
       return;
     }
 
