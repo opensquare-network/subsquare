9b07bb876af3a4ebead6ad6107dd66f64f0753fc
leochen <leochen.sat24@gmail.com>
2025-02-06 18:04:53 +0800

    optimize style, #5160

--- packages/next-common/components/globalNotification/notification.jsx
+++ packages/next-common/components/globalNotification/notification.jsx
@@ -3,11 +3,7 @@ import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
 import { SystemClose } from "@osn/icons/subsquare";
 import { isNil } from "lodash-es";
 
-export default function Notification({
-  cacheKey,
-  children,
-  expires = 15,
-}) {
+export default function Notification({ cacheKey, children, expires = 15 }) {
   const [visible, setVisible] = useCookieValue(cacheKey, true);
 
   if (!visible) {
@@ -15,22 +11,26 @@ export default function Notification({
   }
 
   return (
-    <div className="w-full bg-theme500 flex justify-center px-6">
-      <div className="max-w-[1200px] w-full">
-        <GreyPanel className="bg-theme500 text14Medium py-2.5 px-6 flex items-center justify-center rounded-none text-textPrimaryContrast relative">
-          <div>{children}</div>
-          <SystemClose
-            className="w-5 h-5 absolute max-sm:right-0 right-[54px]"
-            role="button"
-            onClick={() => {
-              const cookieOptions = {};
-              if (!isNil(expires)) {
-                cookieOptions.expires = expires;
-              }
-              setVisible(false, cookieOptions);
-            }}
-          />
-        </GreyPanel>
+    <div className="w-full bg-theme500 flex justify-center">
+      <div className="px-6 py-2.5 mx-auto max-w-[1200px] max-sm:px-0 flex-1">
+        <div className="w-full px-6">
+          <GreyPanel className="flex justify-between items-start flex-row bg-theme500 text14Medium rounded-none text-textPrimaryContrast">
+            <div className="flex-1 justify-center items-center text-center">
+              {children}
+            </div>
+            <SystemClose
+              className="w-5 h-5 shrink-0 mr-[6px]"
+              role="button"
+              onClick={() => {
+                const cookieOptions = {};
+                if (!isNil(expires)) {
+                  cookieOptions.expires = expires;
+                }
+                setVisible(false, cookieOptions);
+              }}
+            />
+          </GreyPanel>
+        </div>
       </div>
     </div>
   );
