415376a6c90f71570f204a9ed382ed0c0d9c05e5
2nthony <ant.hu@outlook.com>
2025-02-07 15:44:32 +0800

    refactor: extract `useFellowshipCoreMemberProposalSubmitTx` hook #5444

--- packages/next-common/components/summary/newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberProposalSubmitButton.jsx
+++ packages/next-common/components/summary/newProposalQuickStart/createFellowshipCoreMemberProposalPopup/createFellowshipCoreMemberProposalSubmitButton.jsx
@@ -1,18 +1,15 @@
 import TxSubmissionButton from "next-common/components/common/tx/txSubmissionButton";
-import { useContextApi } from "next-common/context/api";
 import { getEventData } from "next-common/utils/sendTransaction";
 import { useRouter } from "next/router";
-import { useCallback } from "react";
 import useFellowshipCoreMembers from "next-common/hooks/fellowship/core/useFellowshipCoreMembers";
 import { useSignerAccount } from "next-common/components/popupWithSigner/context";
 import { find } from "lodash-es";
 import Tooltip from "next-common/components/tooltip";
 import {
   useCollectivesSection,
-  useCoreFellowshipPallet,
-  useRankedCollectivePallet,
   useReferendaFellowshipPallet,
 } from "next-common/context/collectives/collectives";
+import { useFellowshipCoreMemberProposalSubmitTx } from "next-common/hooks/fellowship/core/useFellowshipCoreMemberProposalSubmitTx";
 
 export default function CreateFellowshipCoreMemberProposalSubmitButton({
   disabled,
@@ -31,57 +28,21 @@ export default function CreateFellowshipCoreMemberProposalSubmitButton({
 
   const myRankOk = me && me.rank >= 3;
 
-  const api = useContextApi();
-
   const router = useRouter();
   const section = useCollectivesSection();
-  const corePallet = useCoreFellowshipPallet();
   const referendaPallet = useReferendaFellowshipPallet();
-  const collectivePallet = useRankedCollectivePallet();
 
   const buttonDisabled = disabled || !myRankOk || !who || !rank;
 
-  const getTxFunc = useCallback(async () => {
-    if (!api || buttonDisabled || !action) {
-      return;
-    }
-
-    const proposal = api.tx[corePallet][action](who, rank);
-    const submitTx = api.tx[referendaPallet].submit(
-      { FellowshipOrigins: trackName },
-      { Inline: proposal.method.toHex() },
-      enactment,
-    );
-
-    const referendumCount = await api.query[referendaPallet].referendumCount();
-    const targetReferendumIndex = referendumCount.toNumber();
-
-    const optionsTxs = [
-      checkDecisionDeposit &&
-        api.tx[referendaPallet].placeDecisionDeposit(targetReferendumIndex),
-      checkVoteAye &&
-        api.tx[collectivePallet].vote(targetReferendumIndex, true),
-    ].filter(Boolean);
-
-    if (optionsTxs.length) {
-      return api.tx.utility.batch([submitTx, ...optionsTxs]);
-    }
-
-    return submitTx;
-  }, [
-    api,
-    buttonDisabled,
-    action,
-    corePallet,
-    who,
+  const submitTxFunc = useFellowshipCoreMemberProposalSubmitTx({
     rank,
-    referendaPallet,
-    collectivePallet,
+    who,
+    action,
     trackName,
     enactment,
     checkDecisionDeposit,
     checkVoteAye,
-  ]);
+  });
 
   return (
     <Tooltip
@@ -90,7 +51,9 @@ export default function CreateFellowshipCoreMemberProposalSubmitButton({
       <TxSubmissionButton
         disabled={buttonDisabled}
         title="Create Preimage"
-        getTxFunc={getTxFunc}
+        getTxFunc={() => {
+          return submitTxFunc;
+        }}
         onInBlock={({ events }) => {
           const eventData = getEventData(events, referendaPallet, "Submitted");
           if (!eventData) {
