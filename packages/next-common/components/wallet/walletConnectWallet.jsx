f70b1c2b9131ebfa7354de30013a46f8e6e1b071
2nthony <ant.hu@outlook.com>
2025-02-12 15:58:34 +0800

    fix walletconnect connect behaviour #5285

--- packages/next-common/components/wallet/walletConnectWallet.jsx
+++ packages/next-common/components/wallet/walletConnectWallet.jsx
@@ -1,18 +1,30 @@
 import { useWalletConnect } from "next-common/context/walletconnect";
+import { useLoginPopup } from "next-common/hooks/useLoginPopup";
 import WalletOption from "./walletOption";
+import { useWeb3Login } from "next-common/hooks/connect/useWeb3Login";
 
-export default function WalletConnectWallet({ wallet, onClick }) {
+export default function WalletConnectWallet({ wallet }) {
   const { fetchAddresses } = useWalletConnect();
+  const { closeLoginPopup } = useLoginPopup();
+  const [web3Login] = useWeb3Login();
+
+  function handleConnect() {
+    closeLoginPopup();
+
+    fetchAddresses().then((addresses) => {
+      web3Login({
+        account: { address: addresses[0] },
+        wallet: wallet.extensionName,
+      });
+    });
+  }
 
   return (
     <WalletOption
       installed
       logo={<wallet.logo className={wallet.title} alt={wallet.title} />}
       title={wallet.title}
-      onClick={() => {
-        fetchAddresses();
-        onClick?.();
-      }}
+      onClick={handleConnect}
     />
   );
 }
