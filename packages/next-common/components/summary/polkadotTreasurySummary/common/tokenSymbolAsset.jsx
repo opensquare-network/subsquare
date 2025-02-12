f8cc18e9a5fc45d07f912804f982bd5812c1d309
leochen <leochen.sat24@gmail.com>
2025-02-12 15:44:52 +0800

    TokenSymbolAsset optimize, #5448

--- packages/next-common/components/summary/polkadotTreasurySummary/common/tokenSymbolAsset.jsx
+++ packages/next-common/components/summary/polkadotTreasurySummary/common/tokenSymbolAsset.jsx
@@ -2,6 +2,7 @@ import AssetIcon from "next-common/components/icons/assetIcon";
 import ValueDisplay from "next-common/components/valueDisplay";
 import { cn } from "next-common/utils";
 import { useNativeTokenIcon } from "next-common/components/assets/known";
+import { useMemo } from "react";
 
 export default function TokenSymbolAsset({
   amount,
@@ -11,13 +12,13 @@ export default function TokenSymbolAsset({
 }) {
   const NativeAssetIcon = useNativeTokenIcon();
 
+  const TokenIcon = useMemo(() => {
+    return type === "native" ? NativeAssetIcon : AssetIcon;
+  }, [type, NativeAssetIcon]);
+
   return (
     <div className="flex items-center gap-x-2">
-      {type === "native" ? (
-        <NativeAssetIcon symbol={symbol} className="w-4 h-4 flex-none" />
-      ) : (
-        <AssetIcon symbol={symbol} className="w-4 h-4 flex-none" />
-      )}
+      <TokenIcon symbol={symbol} className="w-4 h-4 flex-none" />
       <ValueDisplay
         value={amount}
         symbol={symbol}
