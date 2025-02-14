5d2225ab15295b7f099bec41e68471987eaabcea
chaojun <chaojun@opensquare.network>
2025-02-14 14:46:46 +0800

    Rename files, #5343

---
+++ packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/context/myDemotionExpiration.js
@@ -0,0 +1,43 @@
+import { createContext, useContext } from "react";
+import useMyDemotionPeriodExpiration from "../hooks/useMyDemotionPeriodExpiration";
+import { useContextMyMemberData } from "./myMemberData";
+
+export const MyDemotionExpirationContext = createContext();
+
+function CheckMyDemotionExpiration({ children, memberData }) {
+  const { isDemotionExpiring } = useMyDemotionPeriodExpiration(memberData);
+
+  return (
+    <MyDemotionExpirationContext.Provider
+      value={{
+        isLoading: false,
+        isDemotionExpiring,
+      }}
+    >
+      {children}
+    </MyDemotionExpirationContext.Provider>
+  );
+}
+
+export default function MyDemotionExpirationProvider({ children }) {
+  const { memberData, isLoading: isMemberDataLoading } =
+    useContextMyMemberData();
+
+  if (isMemberDataLoading) {
+    return (
+      <MyDemotionExpirationContext.Provider value={{ isLoading: true }}>
+        {children}
+      </MyDemotionExpirationContext.Provider>
+    );
+  }
+
+  return (
+    <CheckMyDemotionExpiration memberData={memberData}>
+      {children}
+    </CheckMyDemotionExpiration>
+  );
+}
+
+export function useContextMyDemotionExpiration() {
+  return useContext(MyDemotionExpirationContext);
+}
