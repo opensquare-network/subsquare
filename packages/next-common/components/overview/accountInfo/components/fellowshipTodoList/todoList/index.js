f99e5a58cc5ef979987a2b33a47977a478311222
Yongfeng LI <wliyongfeng@gmail.com>
2025-02-17 15:24:31 +0800

    Add OnlyLowRankMembers guard component, #5343

--- packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/index.js
+++ packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/index.js
@@ -2,6 +2,7 @@ import RetentionEvidenceSubmissionTodo from "./retentionEvidenceSubmissionTodo";
 import DemotionExpirationTodo from "./demotionExpirationTodo";
 import useTodoListCount from "../hooks/useTodoListCount";
 import MembershipReferendaTodoForLowerRank from "./membershipReferendaTodoForLowerRank";
+import { useContextMyRank } from "next-common/components/overview/accountInfo/components/fellowshipTodoList/context/myMemberData";
 
 function TodoListWrapper({ children }) {
   const count = useTodoListCount();
@@ -11,12 +12,24 @@ function TodoListWrapper({ children }) {
   return <div className="flex flex-col mt-[16px] gap-[4px]">{children}</div>;
 }
 
+function OnlyLowRankMembers({ children }) {
+  const myRank = useContextMyRank();
+  // todo: ambassador may have a different rank threshold
+  if (myRank < 3) {
+    return null;
+  }
+
+  return children;
+}
+
 export default function TodoList() {
   return (
     <TodoListWrapper>
       <RetentionEvidenceSubmissionTodo />
       <DemotionExpirationTodo />
-      <MembershipReferendaTodoForLowerRank />
+      <OnlyLowRankMembers>
+        <MembershipReferendaTodoForLowerRank />
+      </OnlyLowRankMembers>
     </TodoListWrapper>
   );
 }
