8d2c39f852b80346398306357a807ac013bd9e61
chaojun <chaojun@opensquare.network>
2025-02-17 17:18:43 +0800

    Remove demotion expiring check for referenda todo, #5343

--- packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/hooks/useShouldShowMembershipReferendaTodoForLowerRank.js
+++ packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/hooks/useShouldShowMembershipReferendaTodoForLowerRank.js
@@ -1,15 +1,13 @@
 import { useContextMyMembershipReferenda } from "../context/myMembershipReferenda";
-import { useContextMyDemotionExpiration } from "../context/myDemotionExpiration";
 import { useContextMyEvidence } from "../context/myEvidence";
 import { useContextMyRank } from "../context/myMemberData";
 
 export default function useShouldShowMembershipReferendaTodoForLowerRank() {
-  const { isDemotionExpiring } = useContextMyDemotionExpiration();
   const { evidence } = useContextMyEvidence();
   const { myMembershipReferenda } = useContextMyMembershipReferenda();
   const myRank = useContextMyRank();
 
-  if (!isDemotionExpiring || !evidence) {
+  if (!evidence) {
     return false;
   }
 
