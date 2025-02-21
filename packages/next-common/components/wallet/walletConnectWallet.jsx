dabe92ccf971136b319e2be6d4ce56cdc66ab03e
2nthony <ant.hu@outlook.com>
2025-02-22 00:48:00 +0800

    fix: add loading to walletconnect wallet button

--- packages/next-common/components/wallet/walletConnectWallet.jsx
+++ packages/next-common/components/wallet/walletConnectWallet.jsx
@@ -1,12 +1,16 @@
+import { isNil } from "lodash-es";
+import { useWalletConnect } from "next-common/context/walletconnect";
 import { useWeb3WalletView } from "next-common/hooks/connect/useWeb3WalletView";
 import WalletOption from "./walletOption";
 
 export default function WalletConnectWallet({ wallet }) {
   const { setView } = useWeb3WalletView();
+  const { provider } = useWalletConnect();
 
   return (
     <WalletOption
       installed
+      loading={isNil(provider)}
       logo={<wallet.logo className={wallet.title} alt={wallet.title} />}
       title={wallet.title}
       onClick={() => {
