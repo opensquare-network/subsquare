b5505aa020d00a740629be9ffd18c1dd49c8c975
chaojun <chaojun@opensquare.network>
2025-02-21 09:56:08 +0800

    Clean unused file, #5343

--- packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/context/hooks/salary.js
+++
@@ -1,7 +0,0 @@
-import { useContextSalaryStats } from "../salaryStats";
-import { useIsInSalaryRegistrationPeriod } from "next-common/hooks/fellowship/salary/useIsInSalaryRegistrationPeriod";
-
-export default function useIsSalaryRegistrationOpening() {
-  const salaryStats = useContextSalaryStats();
-  return useIsInSalaryRegistrationPeriod(salaryStats);
-}
