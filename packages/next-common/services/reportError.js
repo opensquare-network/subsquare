1e700a53f886912e01cded368c6676826abe0fd3
2nthony <ant.hu@outlook.com>
2025-02-25 15:01:31 +0800

    fix report error api url #5466

--- packages/next-common/services/reportError.js
+++ packages/next-common/services/reportError.js
@@ -1,7 +1,7 @@
 import nextApi from "./nextApi";
 
 export async function reportError(errorData) {
-  await nextApi.post("/report-error", {
+  await nextApi.post("report-error", {
     msg_type: "text",
     content: {
       text: JSON.stringify(errorData, null, 2),
