f99b6df05d957a87fa7dd7d1cfb61f0174db6211
2nthony <hi@2nthony.com>
2025-02-27 16:22:21 +0800

    custom error page #5541 (#5550)
    * bump icons
    
    * move contact links to constants
    
    * Revert "move contact links to constants"
    
    This reverts commit 4507f4a136d853de51980c8f4a81a38dcba37740.
    
    * Custom error page #5541

--- packages/next/pages/_error.jsx
+++ packages/next/pages/_error.jsx
@@ -1,8 +1,53 @@
-import React, { useEffect } from "react";
-import Error from "next/error";
-import { CHAIN } from "next-common/utils/constants";
+import {
+  ImgErrorDark,
+  ImgErrorLight,
+  LinkElement,
+  LinkEmail,
+  LinkTelegram,
+} from "@osn/icons/subsquare";
+import ExternalLink from "next-common/components/externalLink";
+import ThemeModeProvider from "next-common/context/theme";
+import SecondaryButton from "next-common/lib/button/secondary";
 import { reportClientError } from "next-common/services/reportClientError";
+import { CHAIN } from "next-common/utils/constants";
+import Link from "next/link";
+import { useEffect } from "react";
 
+const contactLinks = [
+  {
+    name: "Telegram",
+    link: "https://t.me/opensquare",
+    icon: LinkTelegram,
+  },
+  {
+    name: "Element",
+    link: "https://app.element.io/#/room/#opensquare:matrix.org",
+    icon: LinkElement,
+  },
+  {
+    name: "Email",
+    link: "mailto:yongfeng@opensquare.network",
+    icon: LinkEmail,
+  },
+];
+
+function getErrorReason(statusCode) {
+  const reasons = {
+    500: {
+      title: "Internal Server Error",
+      description:
+        "The server encountered an error and could not complete your request",
+    },
+  };
+
+  return (
+    reasons[statusCode] || {
+      title: "Application Error",
+      description:
+        "A client-side exception has occurred (see the browser console for more information)",
+    }
+  );
+}
 function ErrorPage({ statusCode, err, isServerError, reqUrl }) {
   useEffect(() => {
     if (err) {
@@ -19,7 +64,44 @@ function ErrorPage({ statusCode, err, isServerError, reqUrl }) {
     }
   }, [err, isServerError, reqUrl, statusCode]);
 
-  return <Error statusCode={statusCode} />;
+  const { title, description } = getErrorReason(statusCode);
+
+  return (
+    <ThemeModeProvider defaultThemeMode="system">
+      <div className="h-screen flex flex-col items-center justify-center gap-y-6 text-textPrimary text14Medium">
+        <ImgErrorLight className="dark:hidden" />
+        <ImgErrorDark className="hidden dark:block" />
+
+        <div className="flex flex-col items-center">
+          <div className="text20Bold">
+            {statusCode} {title}
+          </div>
+          <div className="mt-2 text-textTertiary">{description}</div>
+        </div>
+
+        <Link href="/">
+          <SecondaryButton>Go Back Home</SecondaryButton>
+        </Link>
+
+        <div className="flex flex-col items-center gap-y-2">
+          <div className="text-textSecondary">Contact Support</div>
+          <div className="flex items-center gap-x-2">
+            {contactLinks.map((contact) => {
+              return (
+                <ExternalLink
+                  externalIcon={false}
+                  href={contact.link}
+                  key={contact.name}
+                >
+                  <contact.icon className="w-5 h-5 text-textTertiary" />
+                </ExternalLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </ThemeModeProvider>
+  );
 }
 
 ErrorPage.getInitialProps = async ({ req, res, err }) => {
