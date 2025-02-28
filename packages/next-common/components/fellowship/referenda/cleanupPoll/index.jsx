182ae7ce8151c17ad6a19c18f6cbf98ec2f5395b
leochen <leochen.sat24@gmail.com>
2025-02-28 16:49:53 +0800

    add sign popup, #5540

--- packages/next-common/components/fellowship/referenda/cleanupPoll/index.jsx
+++ packages/next-common/components/fellowship/referenda/cleanupPoll/index.jsx
@@ -1,75 +1,42 @@
 import Button from "next-common/lib/button";
-import { useCallback, useState, useMemo } from "react";
+import { useState, useMemo } from "react";
 import Tooltip from "next-common/components/tooltip";
 import ReferendumVotingProvider, {
   useReferendumVoting,
-  fetchFellowshipReferendumVotes2Times,
 } from "next-common/context/fellowship/referendumVoting";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
-import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
-import useTxSubmission from "next-common/components/common/tx/useTxSubmission";
-import { useContextApi } from "next-common/context/api";
-import { useRankedCollectivePallet } from "next-common/context/collectives/collectives";
 import { useReferendumVotingFinishIndexer } from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
 import { isNil } from "lodash-es";
 import { useOnchainData } from "next-common/context/post";
-import { useDispatch } from "react-redux";
-import { newSuccessToast } from "next-common/store/reducers/toastSlice";
+import dynamicPopup from "next-common/lib/dynamic/popup";
 
-const successToastContent =
-  "Votes in storage have been cleaned up. This button will disappear in a few seconds.";
+const CleanupPopup = dynamicPopup(() => import("./popup"));
 
 function CleanupPollButton() {
-  const { votes, isLoading, fetch } = useReferendumVoting();
-  const { referendumIndex: pollIndex } = useOnchainData();
+  const { votes, isLoading } = useReferendumVoting();
   const address = useRealAddress();
-  const [isDisabled, setIsDisabled] = useState(false);
-  const api = useContextApi();
-  const pallet = useRankedCollectivePallet();
-  const dispatch = useDispatch();
+  const [showPopup, setShowPopup] = useState(false);
 
-  const isShowCleanup = useMemo(() => {
+  const showCleanup = useMemo(() => {
     return votes?.length > 0 && !isLoading && address;
   }, [votes, isLoading, address]);
 
-  const getTxFunc = useCallback(() => {
-    if (!api || !pallet) {
-      return;
-    }
-    setIsDisabled(true);
-
-    return api?.tx?.[pallet]?.cleanupPoll(pollIndex, votes.length);
-  }, [api, pallet, pollIndex, votes]);
-
-  const onInBlock = () => {
-    dispatch(newSuccessToast(successToastContent));
-    fetchFellowshipReferendumVotes2Times(fetch).then(() => {
-      // updated 2 time, do nothing
-    });
-  };
-
-  const { doSubmit } = useTxSubmission({
-    getTxFunc,
-    onInBlock,
-    onCancelled: () => setIsDisabled(false),
-  });
-
-  if (!isShowCleanup) {
+  if (!showCleanup) {
     return null;
   }
 
   return (
-    <Tooltip content="Clean up votes from storage, no gas">
-      <Button
-        className={
-          "w-full h-[40px] rounded-lg bg-neutral100 border border-neutral400"
-        }
-        onClick={doSubmit}
-        disabled={isDisabled}
-      >
-        Cleanup Poll
-      </Button>
-    </Tooltip>
+    <>
+      <Tooltip content="Clean up votes from storage, no gas">
+        <Button
+          className="w-full h-[40px] rounded-lg bg-neutral100 border border-neutral400"
+          onClick={() => setShowPopup(true)}
+        >
+          Cleanup Poll
+        </Button>
+      </Tooltip>
+      {showPopup && <CleanupPopup onClose={() => setShowPopup(false)} />}
+    </>
   );
 }
 
@@ -88,9 +55,7 @@ export default function FellowshipReferendumCleanupPoll() {
   return (
     <MakesureReferendumFinishedGuard>
       <ReferendumVotingProvider>
-        <SignerPopupWrapper>
-          <CleanupPollButton />
-        </SignerPopupWrapper>
+        <CleanupPollButton />
       </ReferendumVotingProvider>
     </MakesureReferendumFinishedGuard>
   );
