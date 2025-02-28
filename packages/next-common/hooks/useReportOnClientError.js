616d5ebba45738712dd661769a108a7f48b526e0
2nthony <ant.hu@outlook.com>
2025-02-28 10:30:19 +0800

    clean log

--- packages/next-common/hooks/useReportOnClientError.js
+++ packages/next-common/hooks/useReportOnClientError.js
@@ -2,8 +2,6 @@ import { reportClientError } from "next-common/services/reportClientError";
 import { CHAIN, IS_PRODUCTION } from "next-common/utils/constants";
 import { useEffect } from "react";
 
-console.info("IS_PRODUCTION", IS_PRODUCTION);
-
 export function useReportOnClientError() {
   useEffect(() => {
     if (typeof window !== "undefined" && IS_PRODUCTION) {
