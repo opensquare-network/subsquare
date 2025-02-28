5a36219e4c833705f9ba6c865b33f57f3978b7eb
2nthony <ant.hu@outlook.com>
2025-03-01 04:32:30 +0800

    disable client onerror report #5466

--- packages/next/pages/_app.js
+++ packages/next/pages/_app.js
@@ -17,7 +17,6 @@ import SystemVersionUpgrade from "next-common/components/systemVersionUpgrade";
 import "@osn/previewer/styles.css";
 import "next-common/styles/markdown.css";
 import useInitMimir from "next-common/hooks/useInitMimir";
-import { useReportOnClientError } from "next-common/hooks/useReportOnClientError";
 
 NProgress.configure({
   minimum: 0.3,
@@ -45,7 +44,6 @@ function MyApp({ Component, pageProps }) {
   }
 
   useInitMimir();
-  useReportOnClientError();
 
   const {
     connectedAccount,
