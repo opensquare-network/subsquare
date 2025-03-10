6884c5425f23cefdba21924bcecfa25e3e7fddbd
chaojun <chaojun@opensquare.network>
2025-03-10 15:53:36 +0800

    fix: detail import, #5548

--- packages/next-common/components/fellowship/applications/articleContent.js
+++ packages/next-common/components/fellowship/applications/articleContent.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import styled from "styled-components";
-import ArticleActions from "./actions/articleActions";
-import Divider from "./styled/layout/divider";
-import NonEdited from "./detail/common/NonEdited";
-import PostContent from "./detail/common/PostContent";
-import { usePost } from "../context/post";
+import ArticleActions from "next-common/components/actions/articleActions";
+import Divider from "next-common/components//styled/layout/divider";
+import NonEdited from "next-common/components//detail/common/NonEdited";
+import PostContent from "next-common/components//detail/common/PostContent";
+import { usePost } from "next-common/context/post";
 import { isPostEdited } from "next-common/utils/post";
-import Tabs from "./tabs";
-import ContentSummary from "./contentSummary";
-import PostDataSource from "./postDataSource";
+import Tabs from "next-common/components/tabs";
+import ContentSummary from "next-common/components//contentSummary";
+import PostDataSource from "next-common/components//postDataSource";
 
 const Wrapper = styled.div`
   :hover {
