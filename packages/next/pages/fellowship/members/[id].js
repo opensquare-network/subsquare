4e55fdbe94a114540cfc982c2d95df556a9c1977
Yongfeng LI <wliyongfeng@gmail.com>
2025-03-10 19:53:39 +0800

    Init fellowship member detail page, #5316

---
+++ packages/next/pages/fellowship/members/[id].js
@@ -0,0 +1,33 @@
+import { withCommonProps } from "next-common/lib";
+import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
+import nextApi from "next-common/services/nextApi";
+import {
+  fellowshipMembersApiUri,
+  fellowshipParamsApi,
+} from "next-common/services/url";
+import FellowshipMemberPage from "next-common/components/pages/fellowship/member";
+
+export default FellowshipMemberPage;
+
+export const getServerSideProps = withCommonProps(async (context) => {
+  const { id } = context.query;
+
+  const [
+    tracksProps,
+    { result: fellowshipMembers },
+    { result: fellowshipParams = {} },
+  ] = await Promise.all([
+    fetchOpenGovTracksProps(),
+    nextApi.fetch(fellowshipMembersApiUri),
+    nextApi.fetch(fellowshipParamsApi),
+  ]);
+
+  return {
+    props: {
+      id,
+      ...tracksProps,
+      fellowshipMembers: fellowshipMembers ?? null,
+      fellowshipParams,
+    },
+  };
+});
