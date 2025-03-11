c6db3963653b8b04502cc1892174bc3d7ead2ffa
chaojun <chaojun@opensquare.network>
2025-03-11 17:15:23 +0800

    Update

--- packages/next-common/components/overview/accountInfo/components/extensionUpdatePrompt.js
+++ packages/next-common/components/overview/accountInfo/components/extensionUpdatePrompt.js
@@ -161,6 +161,7 @@ export default function ExtensionUpdatePrompt({ isWithCache = true }) {
 
         const isOk = await extension.metadata.provide(def);
         alert("after metadata.provide");
+        alert(JSON.stringify({ isOk }));
 
         if (isOk) {
           alert("trigger1");
