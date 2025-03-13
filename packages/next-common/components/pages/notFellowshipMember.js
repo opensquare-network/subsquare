bf84a29f02b70387c0b517dc570dbfabe1f3b466
Chaojun Huang <chaojun@opensquare.network>
2025-03-13 16:55:14 +0800

    Add not a fellowship member page, #5316 (#5603)
    * Add not a fellowship member page, #5316
    
    * refactor, #5316
    
    * fix: mobile text center, #5316
    
    * Update, #5316

---
+++ packages/next-common/components/pages/notFellowshipMember.js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import { ImgNotMemberLight } from "@osn/icons/subsquare";
+import BaseLayout from "next-common/components/layout/baseLayout";
+import SecondaryButton from "next-common/lib/button/secondary";
+import { useCollectivesSection } from "next-common/context/collectives/collectives";
+import { upperFirst } from "lodash-es";
+
+export default function NotFellowshipMember() {
+  const router = useRouter();
+  const section = useCollectivesSection();
+
+  return (
+    <BaseLayout>
+      <div className="flex items-center justify-center grow px-[16px]">
+        <div className="flex flex-col items-center gap-[24px]">
+          <ImgNotMemberLight />
+          <div className="flex flex-col gap-[8px] items-center">
+            <h1 className="text-textPrimary text20Bold">
+              Not a {upperFirst(section)} member
+            </h1>
+            <h2 className="text-center text-textTertiary text14Medium">
+              You are currently visiting an account that is not a {section}{" "}
+              member yet
+            </h2>
+          </div>
+          <SecondaryButton onClick={() => router.push(`/${section}/members`)}>
+            Check All Members
+          </SecondaryButton>
+        </div>
+      </div>
+    </BaseLayout>
+  );
+}
