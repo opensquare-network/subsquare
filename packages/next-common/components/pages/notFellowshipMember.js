ae859d45224942c350d4b7d825d27d755e954c63
chaojun <chaojun@opensquare.network>
2025-03-13 16:22:48 +0800

    fix: mobile text center, #5316

--- packages/next-common/components/pages/notFellowshipMember.js
+++ packages/next-common/components/pages/notFellowshipMember.js
@@ -11,15 +11,15 @@ export default function NotFellowshipMember() {
 
   return (
     <BaseLayout>
-      <div className="flex items-center justify-center w-full h-full">
+      <div className="flex items-center justify-center grow mx-[16px]">
         <div className="flex flex-col items-center gap-[24px]">
           <ImgNotMemberLight />
           <div className="flex flex-col gap-[8px] items-center">
             <h1 className="text-textPrimary text20Bold">
               Not a {upperFirst(section)} member
             </h1>
-            <h2 className="text-textTertiary text14Medium">
-              You are currently visiting an account that is not a {section}
+            <h2 className="text-center text-textTertiary text14Medium">
+              You are currently visiting an account that is not a {section}{" "}
               member yet
             </h2>
           </div>
