a9cb6d0f9b2c3ec582c49c1bde6bc06a8bfb281c
chaojun <chaojun@opensquare.network>
2025-03-13 10:11:30 +0800

    fix, #5548

--- packages/next-common/utils/viewfuncs/fellowshipApplication/normalizeFellowshipApplicationListItem.js
+++ packages/next-common/utils/viewfuncs/fellowshipApplication/normalizeFellowshipApplicationListItem.js
@@ -1,7 +1,6 @@
 export default function normalizeFellowshipApplicationListItem(item) {
   return {
     ...item,
-    index: item.applicationUid,
     time: item.lastActivityAt,
     detailLink: `/fellowship/applications/${item.applicationUid}`,
   };
