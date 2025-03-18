edc3030f2423d7775c4ce29353ef64afa0d0bc9a
wolyshaw <wolyshaw@users.noreply.github.com>
2025-03-18 09:41:57 +0800

    perf: Membership

--- packages/next-common/components/pages/fellowship/member/fellowshipMember/membership.js
+++ packages/next-common/components/pages/fellowship/member/fellowshipMember/membership.js
@@ -11,79 +11,46 @@ import { fellowshipStatisticsUsersApi } from "next-common/services/url";
 import { useMembersWithStatus } from "next-common/components/fellowship/collective/hook/useFellowshipCoreMembersFilter";
 import CoreFellowshipMemberDemotionPeriod from "next-common/components/collectives/core/member/demotionPeriod";
 import CoreFellowshipMemberPromotionPeriod from "next-common/components/collectives/core/member/promotionPeriod";
+import { useIsMobile } from "next-common/components/overview/accountInfo/components/accountBalances";
 import { CardTitle } from "./styled";
 import { useMemo } from "react";
 import FellowshipRank from "next-common/components/fellowship/rank";
 import Divider from "next-common/components/styled/layout/divider";
 import Period from "next-common/components/fellowship/params/period";
+import { cn } from "next-common/utils";
+import WindowSizeProvider from "next-common/context/windowSize";
 
 export default function Membership() {
   const { fellowshipMembers, id, fellowshipParams } = usePageProps();
   const filterMember = useMemo(() => {
     return fellowshipMembers.filter((member) => member.address === id);
   }, [fellowshipMembers, id]);
-  const {
-    demotionPeriod = [],
-    minPromotionPeriod = [],
-    offboardTimeout,
-  } = fellowshipParams ?? {};
 
   const { membersWithStatus, isLoading } = useMembersWithStatus(filterMember);
 
-  const ActiveMember = membersWithStatus?.[0];
-
-  const demotionBlocks = useMemo(() => {
-    return ActiveMember?.rank <= 0
-      ? offboardTimeout
-      : demotionPeriod[rankToIndex(ActiveMember?.rank)];
-  }, [ActiveMember?.rank, demotionPeriod, offboardTimeout]);
-
-  const promotionPeriod = useMemo(() => {
-    return minPromotionPeriod[ActiveMember?.rank] || 0;
-  }, [ActiveMember?.rank, minPromotionPeriod]);
+  const activeMember = membersWithStatus?.[0];
 
   return (
-    <SecondaryCard>
-      <CardTitle>Membership</CardTitle>
+    <WindowSizeProvider>
+      <SecondaryCard>
+        <CardTitle>Membership</CardTitle>
 
-      <div className="gap-y-3 flex flex-col">
-        <ProfileFellowshipStatisticsInfoImpl rank={ActiveMember?.rank} />
+        <div className="gap-y-4 flex flex-col">
+          <ProfileFellowshipStatisticsInfoImpl
+            rank={activeMember?.rank}
+            isRankLoading={isLoading || isNil(activeMember)}
+          />
 
-        <Divider />
+          <Divider />
 
-        <LoadableContent isLoading={isLoading || isNil(ActiveMember)}>
-          <div className="flex gap-x-4">
-            <div className="flex-1">
-              <CoreFellowshipMemberDemotionPeriod
-                lastProof={ActiveMember?.status?.lastProof}
-                rank={ActiveMember?.rank}
-                params={fellowshipParams}
-                titleClassName="text14Medium text-textPrimary"
-                progressClassName="h-[0.375rem]"
-              />
-              <span className="text-textTertiary text12Medium block mt-1">
-                <Period blocks={demotionBlocks} />
-              </span>
-            </div>
-            <span className="border-r border-[var(--neutral200)] w-0 block" />
-            <div className="flex-1">
-              {ActiveMember?.rank > 0 && (
-                <CoreFellowshipMemberPromotionPeriod
-                  lastPromotion={ActiveMember?.status?.lastPromotion}
-                  rank={ActiveMember?.rank}
-                  params={fellowshipParams}
-                  titleClassName="text14Medium text-textPrimary"
-                  progressClassName="h-[0.375rem]"
-                />
-              )}
-              <span className="text-textTertiary text12Medium block mt-1">
-                <Period blocks={promotionPeriod} />
-              </span>
-            </div>
-          </div>
-        </LoadableContent>
-      </div>
-    </SecondaryCard>
+          <PeriodProgress
+            fellowshipParams={fellowshipParams}
+            activeMember={activeMember}
+            isLoading={isLoading || isNil(activeMember)}
+          />
+        </div>
+      </SecondaryCard>
+    </WindowSizeProvider>
   );
 }
 
@@ -101,14 +68,14 @@ function useUserStatisticsData(address) {
   }, [address, statisticsApi]);
 }
 
-function ProfileFellowshipStatisticsInfoImpl({ rank }) {
+function ProfileFellowshipStatisticsInfoImpl({ rank, isRankLoading }) {
   const { id: address } = usePageProps();
   const { value, loading } = useUserStatisticsData(address);
 
   return (
     <SummaryLayout>
       <SummaryItem title="Rank">
-        <LoadableContent isLoading={loading || isNil(value?.totalPaid)}>
+        <LoadableContent isLoading={isRankLoading}>
           <FellowshipRank rank={rank} />
         </LoadableContent>
       </SummaryItem>
@@ -128,5 +95,64 @@ function ProfileFellowshipStatisticsInfoImpl({ rank }) {
         </LoadableContent>
       </SummaryItem>
     </SummaryLayout>
+  );
+}
+
+function PeriodProgress({ activeMember, fellowshipParams, isLoading }) {
+  const isMobile = useIsMobile();
+  const {
+    demotionPeriod = [],
+    minPromotionPeriod = [],
+    offboardTimeout,
+  } = fellowshipParams ?? {};
+
+  const demotionBlocks = useMemo(() => {
+    return activeMember?.rank <= 0
+      ? offboardTimeout
+      : demotionPeriod[rankToIndex(activeMember?.rank)];
+  }, [activeMember?.rank, demotionPeriod, offboardTimeout]);
+
+  const promotionPeriod = useMemo(() => {
+    return minPromotionPeriod[activeMember?.rank] || 0;
+  }, [activeMember?.rank, minPromotionPeriod]);
+
+  return (
+    <div className={cn("flex gap-x-4", { "flex-col gap-y-4": isMobile })}>
+      <div className="flex-1">
+        <LoadableContent isLoading={isLoading}>
+          <CoreFellowshipMemberDemotionPeriod
+            lastProof={activeMember?.status?.lastProof}
+            rank={activeMember?.rank}
+            params={fellowshipParams}
+            titleClassName="text14Medium text-textPrimary"
+            progressClassName="h-[0.375rem]"
+          />
+          <span className="text-textTertiary text12Medium flex mt-1">
+            <Period blocks={demotionBlocks} suffix=" remaining" />
+          </span>
+        </LoadableContent>
+      </div>
+      <span
+        className={cn("border-r border-[var(--neutral200)] w-0 block", {
+          "w-full border-t": isMobile,
+        })}
+      />
+      <div className="flex-1">
+        <LoadableContent isLoading={isLoading}>
+          {activeMember?.rank > 0 && (
+            <CoreFellowshipMemberPromotionPeriod
+              lastPromotion={activeMember?.status?.lastPromotion}
+              rank={activeMember?.rank}
+              params={fellowshipParams}
+              titleClassName="text14Medium text-textPrimary"
+              progressClassName="h-[0.375rem]"
+            />
+          )}
+          <span className="text-textTertiary text12Medium flex mt-1">
+            <Period blocks={promotionPeriod} suffix=" remaining" />
+          </span>
+        </LoadableContent>
+      </div>
+    </div>
   );
 }
