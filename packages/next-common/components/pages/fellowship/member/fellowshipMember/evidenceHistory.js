4cca2b362d728b765f950c836e032463435bf6ba
wolyshaw <wolyshaw@users.noreply.github.com>
2025-03-19 14:45:42 +0800

    Implement the "Evidence" card in fellowship member detail page #5316 (#5626)
    * feat: Add evidence wish
    
    * fix: style and hidden history
    
    * add wish history and edit "EvidenceList" component
    
    * Merge highly similar components "FellowshipEvidenceContent"
    
    * change empty text
    
    * add "Related Referendum"
    
    * perf: evidence content load and view

---
+++ packages/next-common/components/pages/fellowship/member/fellowshipMember/evidenceHistory.js
@@ -0,0 +1,16 @@
+import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
+import ProfileFellowshipCoreEvidence from "next-common/components/profile/fellowship/core/evidence";
+import { noop } from "lodash-es";
+
+export default function EvidenceHistory() {
+  return (
+    <SecondaryCard>
+      <ProfileFellowshipCoreEvidence
+        setEvidenceCount={noop}
+        className="grid-cols-2"
+        noDateText="No evidences"
+        popupTitle="Wish Detail"
+      />
+    </SecondaryCard>
+  );
+}
