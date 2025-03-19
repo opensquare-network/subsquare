cbc928e3f1c4cbaf5362f89ad1e0320d9e6c676e
wolyshaw <wolyshaw@users.noreply.github.com>
2025-03-19 16:47:50 +0800

    add "Membership" tab #5627

--- packages/next-common/components/pages/fellowship/member/fellowshipMember/coreActivities.js
+++ packages/next-common/components/pages/fellowship/member/fellowshipMember/coreActivities.js
@@ -1,10 +1,33 @@
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
-import { CardTitle } from "./styled";
+import ProfileFellowshipCoreFeeds from "next-common/components/profile/fellowship/core/feeds";
+import Tabs from "next-common/components/tabs";
+import { useState } from "react";
 
 export default function CoreActivities() {
+  const tabs = [
+    {
+      label: "Feeds",
+      value: "feeds",
+      content: (
+        <ProfileFellowshipCoreFeeds
+          noDataText="No current activities"
+          showUserInfo={false}
+        />
+      ),
+    },
+  ];
+
+  const [activeTabValue, setActiveTabValue] = useState(tabs[0].value);
+
   return (
     <SecondaryCard>
-      <CardTitle>Core Activities</CardTitle>
+      <Tabs
+        tabs={tabs}
+        activeTabValue={activeTabValue}
+        onTabClick={(tab) => {
+          setActiveTabValue(tab.value);
+        }}
+      />
     </SecondaryCard>
   );
 }
