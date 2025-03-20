65545a74577f561e1d6b8544d982ab4eee5b504d
leochen <leochen.sat24@gmail.com>
2025-03-20 14:22:31 +0800

    Test tempalte, #5563

--- packages/next-common/components/collectives/core/actions/more/submitEvidenceItem/popup.jsx
+++ packages/next-common/components/collectives/core/actions/more/submitEvidenceItem/popup.jsx
@@ -18,6 +18,44 @@ import { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useCoreFellowshipPallet } from "next-common/context/collectives/collectives";
 
+// default Fellowship Evidence template
+const fellowshipEvidenceTemplate = `# Argument-0000: Retention at/Promotion to Rank ____
+
+## Member details
+- Date of initial induction:
+- Date of last report: 
+- Area(s) of Expertise/Interest: 
+
+## Reporting period
+- Start date: YYYY/MM/DD
+- End date: YYYY/MM/DD
+
+## Argument
+*Explain why your contributions in relation to the Polkadot SDK are worthy of retention/promotion. Refer to the terms in Section 6 of the [Manifesto](https://github.com/polkadot-fellows/manifesto/blob/main/manifesto.pdf) and provide links to relevant content (i.e code, articles, media, etc.) to show that you are meeting all the requirements.*
+
+Below are some examples on how to write an argument for:
+- Retention: [Rank 1](https://collectives.subsquare.io/fellowship/referenda/289), [Rank 2](https://collectives.subsquare.io/fellowship/referenda/282), [Rank 3](https://collectives.subsquare.io/fellowship/referenda/244)
+- Promotion [Rank 1](https://collectives.subsquare.io/fellowship/referenda/272), [Rank 2](https://collectives.subsquare.io/fellowship/referenda/306), [Rank 3](https://collectives.subsquare.io/fellowship/referenda/255).
+
+## Voting record
+*Provide your voting record in relation to required thresholds for your rank.* 
+
+|  Ranks | Activity thresholds | Agreement thresholds | Member's voting activities | Comments |
+|---|---|---|---|---|
+|III|70%   |100%  |I have voted on x out of xx referenda in which I was eligible to vote (i.e xx % voting activity). Out of xx referenda in which members of higher ranks were in complete agreement, I have voted in line with the consensus x times (i.e xx % voting agreement).  |*This is an example.* |
+|I  |90%   |N/A   |   |  |
+|II |80%   |N/A   |   |  |
+|III|70%   |100%  |   |  |
+|IV |60%   |90%   |   |  |
+|V  |50%   |80%   |   |  |
+|VI |40%   |70%   |   |  |
+
+## Misc
+
+- [ ] Question(s): 
+- [ ] Concern(s): 
+- [ ] Comment(s): `;
+
 function WishChoice({ title, description, checked, onClick = noop }) {
   return (
     <div
@@ -49,9 +87,20 @@ function Content() {
   const { component } = useSigner("Address");
   const [evidence, setEvidence] = useState("");
   const [wish, setWish] = useState("retention");
+  const [template, setTemplate] = useState("");
   const { uploading, upload } = useUploadToIpfs();
   const api = useContextApi();
   const pallet = useCoreFellowshipPallet();
+
+  const handleTemplateChange = (e) => {
+    const value = e.target.value;
+    setTemplate(value);
+    if (value === "fellowship") {
+      setEvidence(fellowshipEvidenceTemplate);
+    } else {
+      setEvidence("");
+    }
+  };
 
   const getTxFunc = useCallback(async () => {
     const { error, result } = await upload(
@@ -113,11 +162,21 @@ function Content() {
             </span>
           }
         />
+        <div className="mb-2">
+          <select
+            value={template}
+            onChange={handleTemplateChange}
+            className="w-full p-2 border border-neutral400 rounded-[4px] text14Medium text-textPrimary"
+          >
+            <option value="">Use template (Optional)</option>
+            <option value="fellowship">Fellowship Evidence Template</option>
+          </select>
+        </div>
         <Editor
           value={evidence}
           onChange={setEvidence}
           contentType={"markdown"}
-          minHeight={100}
+          minHeight={300}
         />
       </div>
 
