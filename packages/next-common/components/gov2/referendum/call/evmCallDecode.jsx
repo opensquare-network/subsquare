febe869fcceaac50ce2f792ff98506fc8a943b69
wolyshaw <wolyshaw@users.noreply.github.com>
2025-03-26 15:15:38 +0800

    add showmore & mobile style #5632

--- packages/next-common/components/gov2/referendum/call/evmCallDecode.jsx
+++ packages/next-common/components/gov2/referendum/call/evmCallDecode.jsx
@@ -8,24 +8,67 @@ import { decodeInput } from "next-common/utils/evm/decodeInput";
 import { contractAddressMapiing } from "next-common/utils/evm/importAbi";
 import { useState } from "react";
 import { useAsync } from "react-use";
+import { useIsMobile } from "next-common/components/overview/accountInfo/components/accountBalances";
+import WindowSizeProvider from "next-common/context/windowSize";
+import styled from "styled-components";
 
 const CallDetailPopup = dynamicPopup(() => import("../../../callDetailPopup"));
 
+const separateNumber = 5;
+
+const NameTag = styled(CommonTag)`
+  background-color: var(--neutral200);
+  color: var(--textPrimary);
+  white-space: nowrap;
+`;
+
 export default function EvmCallInputDecode({ evmCallInputs }) {
+  const [showMore, setShowMore] = useState(false);
+  const shouldCollapsed = evmCallInputs?.length > separateNumber;
+
   return (
-    <div className="flex flex-col gap-y-2">
-      {evmCallInputs.map((item) => (
-        <EvmCallInputDecodeItem
-          key={item.target + item.input}
-          input={item.input}
-          target={item.target}
-        />
-      ))}
-    </div>
+    <WindowSizeProvider>
+      <div className="flex flex-col">
+        <div className="flex flex-col gap-y-2">
+          {evmCallInputs.slice(0, separateNumber).map((item) => (
+            <EvmCallInputDecodeItem
+              key={item.target + item.input}
+              input={item.input}
+              target={item.target}
+            />
+          ))}
+          {showMore &&
+            shouldCollapsed &&
+            evmCallInputs
+              ?.slice(separateNumber)
+              .map((item) => (
+                <EvmCallInputDecodeItem
+                  key={item.target + item.input}
+                  input={item.input}
+                  target={item.target}
+                />
+              ))}
+        </div>
+        {shouldCollapsed && (
+          <div className="mt-4">
+            <span
+              role="button"
+              className="text12Medium text-theme500"
+              onClick={() => {
+                setShowMore(!showMore);
+              }}
+            >
+              Show {showMore ? "Less" : "More"}
+            </span>
+          </div>
+        )}
+      </div>
+    </WindowSizeProvider>
   );
 }
 
 function EvmCallInputDecodeItem({ input, target }) {
+  const isMobile = useIsMobile();
   const [detailPopupVisible, setDetailPopupVisible] = useState(false);
   const name = contractAddressMapiing[target]?.name;
   const { value, error, loading } = useAsync(
@@ -43,40 +86,28 @@ function EvmCallInputDecodeItem({ input, target }) {
   }
 
   return (
-    <div className="flex gap-x-2">
-      <CommonTag
-        style={{
-          backgroundColor: "var(--neutral200)",
-          color: "var(--textPrimary)",
-        }}
-      >
-        {name}
-      </CommonTag>
+    <div
+      className={cn("flex gap-2", {
+        "flex-wrap": isMobile,
+      })}
+    >
+      <NameTag>{name}</NameTag>
+      {success && <span className="text-textTertiary text14Medium">·</span>}
       {success && (
-        <>
-          <span className="text-textTertiary text14Medium">·</span>
-          <CommonTag
-            style={{
-              backgroundColor: "var(--neutral200)",
-              color: "var(--textPrimary)",
-            }}
-          >
-            {decode?.method}
-          </CommonTag>
-        </>
-      )}
-      {success && (
-        <Tooltip content="Call Detail">
-          <InfoDocs
-            role="button"
-            className={cn(
-              "w-4 h-4 relative top-[0.5px]",
-              "[&_path]:stroke-textTertiary [&_path]:hover:stroke-textSecondary",
-              "[&_path]:fill-textTertiary [&_path]:hover:fill-textSecondary",
-            )}
-            onClick={() => setDetailPopupVisible(true)}
-          />
-        </Tooltip>
+        <div className="flex gap-2">
+          <NameTag>{decode?.method}</NameTag>
+          <Tooltip content="Call Detail">
+            <InfoDocs
+              role="button"
+              className={cn(
+                "w-4 h-4 relative top-[0.5px]",
+                "[&_path]:stroke-textTertiary [&_path]:hover:stroke-textSecondary",
+                "[&_path]:fill-textTertiary [&_path]:hover:fill-textSecondary",
+              )}
+              onClick={() => setDetailPopupVisible(true)}
+            />
+          </Tooltip>
+        </div>
       )}
       {detailPopupVisible && success && (
         <CallDetailPopup
