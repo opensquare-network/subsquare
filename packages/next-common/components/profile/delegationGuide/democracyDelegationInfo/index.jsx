2938a665987f0f8550cfb4a1b652ed746dc2b65b
leochen <leochen.sat24@gmail.com>
2025-03-31 18:23:40 +0800

    DemocracyDelegationInfo, #5666

--- packages/next-common/components/profile/delegationGuide/democracyDelegationInfo/index.jsx
+++ packages/next-common/components/profile/delegationGuide/democracyDelegationInfo/index.jsx
@@ -1,22 +1,51 @@
 import { NoDelegationInfo, CommonDelegationInfoPanel } from "../delegationInfo";
 import useSubDemocracyDelegating from "next-common/utils/hooks/referenda/useSubDemocracyDelegating";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import BigNumber from "bignumber.js";
+import { useChainSettings } from "next-common/context/chain";
+import { toPrecision } from "next-common/utils";
+import { ConvictionSupport } from "next-common/utils/referendumCommon";
+import Tooltip from "next-common/components/tooltip";
+import ValueDisplay from "next-common/components/valueDisplay";
 
 export default function DemocracyDelegationInfo() {
+  const { decimals, symbol } = useChainSettings();
   const realAddress = useRealAddress();
   const { delegating, isLoading } = useSubDemocracyDelegating(realAddress);
+
   if (isLoading) {
     return null;
   }
 
-  if (delegating?.length === 0) {
+  if (!delegating) {
     return <NoDelegationInfo />;
   }
 
-  // TODO: democracy delegation info
+  const conviction = ConvictionSupport[delegating?.conviction] || 0;
+  const votes = new BigNumber(delegating?.balance || 0)
+    .times(conviction)
+    .toString();
+  const votesDisplay = toPrecision(votes || 0, decimals);
+
   return (
     <CommonDelegationInfoPanel>
-      DemocracyDelegationInfo
+      <span>
+        You are delegating your
+        <Tooltip
+          content={
+            <>
+              <div>
+                Capital:&nbsp;
+                <ValueDisplay value={votesDisplay} symbol={symbol} />
+              </div>
+              <div>Conviction:&nbsp;{conviction}x</div>
+            </>
+          }
+        >
+          <span className="mx-1 text-textSecondary">{votesDisplay} Votes</span>
+        </Tooltip>
+        to this account.
+      </span>
     </CommonDelegationInfoPanel>
   );
 }
