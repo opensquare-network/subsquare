fda0a41951f7ff08b25ef8106d120820e290a821
Chaojun Huang <chaojun@opensquare.network>
2025-04-02 11:42:44 +0800

    Add member promotion/retention todo, #5580 (#5691)
    * Add member promotion/retention todo, #5580
    
    * Rename, #5580
    
    * Update, #5580
    
    * Clean unused function, #5580

--- packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/demotionExpirationTodo.js
+++ packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/demotionExpirationTodo.js
@@ -3,15 +3,14 @@ import { TodoContent, TodoTag, TodoWrapper } from "./styled";
 import { useDemotionExpiredMembers } from "../context/hooks/expired";
 import dynamicPopup from "next-common/lib/dynamic/popup";
 import ActionButton from "./actionButton";
-import { useHasDemotionExpirationTodo } from "../hooks/useHasTodo";
 
 const BatchBumpPopup = dynamicPopup(() => import("./bumpAllPopup"));
 
 export default function DemotionExpirationTodo() {
   const [showBumpAllPopup, setShowBumpAllPopup] = useState(false);
   const { members } = useDemotionExpiredMembers();
-  const hasTodo = useHasDemotionExpirationTodo();
-  if (!hasTodo) {
+
+  if (!members?.length) {
     return null;
   }
 
