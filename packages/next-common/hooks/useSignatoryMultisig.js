dc38023ec19002f660cefec0ae4f1d524c370326
leochen <leochen.sat24@gmail.com>
2025-04-02 11:07:33 +0800

    Add proxies & multisig data hooks, #5689 (#5694)
    * fetch proxies hooks, #5689
    
    * useSignatoryMultisig, #5689

---
+++ packages/next-common/hooks/useSignatoryMultisig.js
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client";
+import { useMultisigQuery } from "next-common/hooks/apollo";
+import { useEffect, useState } from "react";
+
+const GET_MULTISIG_ADDRESSES = gql`
+  query MyQuery($limit: Int!, $offset: Int!, $signatory: String) {
+    multisigAddresses(limit: $limit, offset: $offset, signatory: $signatory) {
+      limit
+      multisigAddresses {
+        address
+        signatories
+        threshold
+      }
+      offset
+      total
+    }
+  }
+`;
+
+export default function useSignatoryMultisig(address) {
+  const [result, setResult] = useState(null);
+  const { data, loading } = useMultisigQuery(GET_MULTISIG_ADDRESSES, {
+    variables: {
+      signatory: address,
+      limit: 100,
+      offset: 0,
+    },
+  });
+
+  useEffect(() => {
+    if (loading || !address) {
+      return;
+    }
+
+    const { multisigAddress } = data || {};
+    setResult(multisigAddress);
+  }, [address, data, loading]);
+
+  return {
+    result,
+    loading,
+  };
+}
