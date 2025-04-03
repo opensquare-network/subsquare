27fc54505cce840ff5eca586736be75229574e35
leochen <leochen.sat24@gmail.com>
2025-04-03 11:57:23 +0800

    NoRelationshipsTip, #5656

--- packages/next-common/components/relationshipPopup/index.jsx
+++ packages/next-common/components/relationshipPopup/index.jsx
@@ -4,6 +4,16 @@ import useConversionRelationshipNode from "next-common/hooks/useConversionRelati
 import Popup from "../popup/wrapper/Popup";
 import Indications from "./indications";
 import Relationship from "./relationship";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import { useMemo } from "react";
+
+function NoRelationshipsTip() {
+  return (
+    <GreyPanel className="justify-start gap-x-2 text14Medium text-textSecondary py-2.5 px-4 max-w-full">
+      This account has no relationships with proxy and multisig.
+    </GreyPanel>
+  );
+}
 
 export default function RelationshipPopup({
   title = "Relatives",
@@ -12,12 +22,21 @@ export default function RelationshipPopup({
 }) {
   const { isLoading, nodes, edges } = useConversionRelationshipNode();
 
+  const showNoRelationshipsTip = useMemo(() => {
+    if (isLoading) {
+      return false;
+    }
+
+    return nodes?.length === 1 && edges?.length === 0;
+  }, [isLoading, nodes, edges]);
+
   return (
     <Popup
       className={cn("w-[960px]", className)}
       title={title}
       onClose={onClose}
     >
+      {showNoRelationshipsTip && <NoRelationshipsTip />}
       <Relationship loading={isLoading} nodes={nodes} edges={edges} />
       <Indications />
     </Popup>
