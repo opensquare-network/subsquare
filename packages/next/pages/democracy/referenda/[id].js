d454ca7007f3b7f09bb1cb3c1c70391a806408c0
quinn-gao <quinnn.gao@gmail.com>
2025-04-10 15:08:30 +0800

    refactor: DemocracyReferendaDetailMultiTabs components

--- packages/next/pages/democracy/referenda/[id].js
+++ packages/next/pages/democracy/referenda/[id].js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { withCommonProps } from "next-common/lib";
 import nextApi from "next-common/services/nextApi";
 import { EmptyList } from "next-common/utils/constants";
@@ -13,10 +13,8 @@ import DemocracyReferendaDetail from "next-common/components/detail/Democracy/re
 import DetailLayout from "next-common/components/layout/DetailLayout";
 import useDemocracyVotesFromServer from "next-common/utils/hooks/referenda/useDemocracyVotesFromServer";
 import { clearVotes } from "next-common/store/reducers/democracy/votes";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import useSubscribePostDetail from "next-common/hooks/useSubscribePostDetail";
-import DetailMultiTabs from "next-common/components/detail/detailMultiTabs";
-import useInlineCall from "next-common/components/democracy/metadata/useInlineCall";
 import { fetchDetailComments } from "next-common/services/detail";
 import { getNullDetailProps } from "next-common/services/detail/nullDetail";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
@@ -24,43 +22,18 @@ import ContentWithComment from "next-common/components/detail/common/contentWith
 import { usePageProps } from "next-common/context/page";
 import useSubDemocracyReferendumStatus from "next-common/hooks/democracy/useSubDemocracyReferendumStatus";
 import useSetReferendumStatus from "next-common/hooks/democracy/useSetReferendumStatus";
-import { referendumStatusSelector } from "next-common/store/reducers/referendumSlice";
 import { useContextApi } from "next-common/context/api";
-import dynamicClientOnly from "next-common/lib/dynamic/clientOnly";
 import MaybeSimaContent from "next-common/components/detail/maybeSimaContent";
-import DemocracyReferendumCallProvider from "next-common/context/democracy/referenda/call";
-
-const Timeline = dynamicClientOnly(() =>
-  import("components/referenda/timeline"),
-);
-const ReferendumMetadata = dynamicClientOnly(() =>
-  import("next-common/components/democracy/metadata"),
-);
-const ReferendumCall = dynamicClientOnly(() =>
-  import("next-common/components/democracy/call"),
-);
-const DemocracyReferendaVotesBubble = dynamicClientOnly(() =>
-  import("next-common/components/democracy/referendum/votesBubble"),
-);
+import DemocracyReferendaDetailMultiTabs from "components/tabs/democracyReferendaDetailMultiTabs";
 
 function ReferendumContent() {
   const post = usePost();
-
-  const onchainData = post?.onchainData;
+  const api = useContextApi();
   const dispatch = useDispatch();
-
-  const { timeline = [], preImage } = onchainData;
 
   useSubscribePostDetail(post?.referendumIndex);
   useSetReferendumStatus();
   useSubDemocracyReferendumStatus(post?.referendumIndex);
-
-  const api = useContextApi();
-  const referendumStatus = useSelector(referendumStatusSelector);
-  const proposal = referendumStatus?.proposal;
-
-  const { call: inlineCall } = useInlineCall(timeline, proposal);
-  const call = preImage?.call || inlineCall;
 
   useMaybeFetchElectorate(post?.onchainData, api);
   useDemocracyVotesFromServer(post.referendumIndex);
@@ -76,35 +49,8 @@ function ReferendumContent() {
     <MaybeSimaContent>
       <ContentWithComment>
         <DemocracyReferendaDetail />
-
         <Vote referendumIndex={post?.referendumIndex} />
-
-        <DetailMultiTabs
-          call={
-            (call || inlineCall) && (
-              <DemocracyReferendumCallProvider>
-                <ReferendumCall
-                  call={call || inlineCall}
-                  shorten={post?.onchainData?.preImage?.shorten}
-                  onchainData={post?.onchainData}
-                />
-              </DemocracyReferendumCallProvider>
-            )
-          }
-          metadata={
-            <ReferendumMetadata
-              proposer={post?.proposer}
-              status={referendumStatus ?? {}}
-              call={
-                post?.onchainData?.preImage?.call || post?.onchainData?.call
-              }
-              shorten={post?.onchainData?.preImage?.shorten}
-              onchainData={post?.onchainData}
-            />
-          }
-          timeline={<Timeline />}
-          votesBubble={<DemocracyReferendaVotesBubble />}
-        />
+        <DemocracyReferendaDetailMultiTabs />
       </ContentWithComment>
     </MaybeSimaContent>
   );
