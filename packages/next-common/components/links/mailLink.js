61cc98719c44ec3b0d7d398acd5059cbfb15adbe
leochen <leochen.sat24@gmail.com>
2025-04-10 14:55:41 +0800

    account links style, #5738

--- packages/next-common/components/links/mailLink.js
+++ packages/next-common/components/links/mailLink.js
@@ -2,5 +2,10 @@ import { LinkEmail } from "@osn/icons/subsquare";
 import IconLink from "./iconLink";
 
 export default function MailLink({ email }) {
-  return <IconLink icon={<LinkEmail />} href={`mailto:${email}`} />;
+  return (
+    <IconLink
+      icon={<LinkEmail className="w-5 h-5" />}
+      href={`mailto:${email}`}
+    />
+  );
 }
--- packages/next-common/components/links/webLink.js
+++ packages/next-common/components/links/webLink.js
@@ -3,5 +3,10 @@ import IconLink from "./iconLink";
 import { ensureProtocol } from "next-common/utils/url";
 
 export default function WebLink({ website }) {
-  return <IconLink icon={<LinkWebsite />} href={ensureProtocol(website)} />;
+  return (
+    <IconLink
+      icon={<LinkWebsite className="w-5 h-5" />}
+      href={ensureProtocol(website)}
+    />
+  );
 }
