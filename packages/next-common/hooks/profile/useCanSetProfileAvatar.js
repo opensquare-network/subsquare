b34b6a8a9361e8bf0405deb044d0a01eff0625fa
leochen <leochen.sat24@gmail.com>
2025-04-11 15:31:15 +0800

    optimize useCanSetProfileAvatar, #5749

--- packages/next-common/hooks/profile/useCanSetProfileAvatar.js
+++ packages/next-common/hooks/profile/useCanSetProfileAvatar.js
@@ -1,29 +1,57 @@
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import useProfileAddress from "next-common/components/profile/useProfileAddress";
 import { isSameAddress } from "next-common/utils";
-import useProfileOnChainProxies from "next-common/components/profile/proxy/hooks/useProfileOnChainProxies";
+import { useContextApi } from "next-common/context/api";
 
 export default function useCanSetProfileAvatar() {
   const profileAddress = useProfileAddress();
   const realAddress = useRealAddress();
-  const { proxies = [], loading } = useProfileOnChainProxies(profileAddress);
+  const api = useContextApi();
 
-  return useMemo(() => {
-    if (!realAddress || !profileAddress || loading || !proxies[0]?.length) {
-      return false;
+  const isSelf = useMemo(
+    () => isSameAddress(realAddress, profileAddress),
+    [realAddress, profileAddress],
+  );
+
+  const [isProxyAccount, setIsProxyAccount] = useState(false);
+
+  useEffect(() => {
+    if (
+      isSelf ||
+      !api ||
+      !api?.query?.proxy?.proxies ||
+      !realAddress ||
+      !profileAddress
+    ) {
+      setIsProxyAccount(false);
+      return;
     }
 
-    const isSelf = isSameAddress(realAddress, profileAddress);
+    api?.query?.proxy
+      ?.proxies(profileAddress)
+      .then((result) => {
+        const data = result?.toJSON?.() || [];
+        const isProxy = data[0]?.some(({ delegate }) =>
+          isSameAddress(realAddress, delegate),
+        );
+
+        setIsProxyAccount(isProxy);
+      })
+      .catch(() => {
+        setIsProxyAccount(false);
+      });
+  }, [api, realAddress, profileAddress, isSelf]);
+
+  return useMemo(() => {
+    if (!realAddress || !profileAddress) {
+      return false;
+    }
 
     if (isSelf) {
       return true;
     }
 
-    const isProxyAccount = proxies[0].some(({ delegate }) =>
-      isSameAddress(realAddress, delegate),
-    );
-
     return isProxyAccount;
-  }, [realAddress, profileAddress, proxies, loading]);
+  }, [realAddress, profileAddress, isSelf, isProxyAccount]);
 }
