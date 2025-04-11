8f638e7ad26f1b85632dd504cf7d595938b84aba
leochen <leochen.sat24@gmail.com>
2025-04-11 18:34:05 +0800

    Improve pure proxy style

--- packages/next-common/components/relationshipPopup/userNode.jsx
+++ packages/next-common/components/relationshipPopup/userNode.jsx
@@ -46,35 +46,41 @@ ${(p) => {
 }}
 `;
 
-function AddressLabelWithBadge({ data }) {
+function AddressLabel({ data }) {
   return (
-    <>
-      <AddressUser
-        add={data?.address || ""}
-        className="flex text14Medium text-textPrimary"
-        maxWidth={200}
-        showAvatar={false}
-        noTooltip
-      />
-      {data.isPure && (
-        <Tooltip
-          content={
-            <Link
-              className="underline relative z-20"
-              style={{ pointerEvents: "all" }}
-              href="https://wiki.polkadot.network/learn/learn-proxies-pure/"
-              target="_blank"
-            >
-              Pure Proxy↗
-            </Link>
-          }
+    <AddressUser
+      add={data?.address || ""}
+      className="flex text14Medium text-textPrimary"
+      maxWidth={200}
+      showAvatar={false}
+      noTooltip
+    />
+  );
+}
+
+function PureBadge({ data, className = "" }) {
+  if (!data?.isPure) {
+    return null;
+  }
+
+  return (
+    <Tooltip
+      content={
+        <Link
+          className="underline relative z-20"
+          style={{ pointerEvents: "all" }}
+          href="https://wiki.polkadot.network/learn/learn-proxies-pure/"
+          target="_blank"
         >
-          <span className="inline-block h-5 leading-5 bg-neutral200 text-textSecondary text12Medium px-2 rounded-[0.625rem]">
-            Pure
-          </span>
-        </Tooltip>
-      )}
-    </>
+          Pure Proxy↗
+        </Link>
+      }
+      className={className}
+    >
+      <span className="inline-block h-5 leading-5 bg-neutral200 text-textSecondary text12Medium px-2 rounded-[0.625rem]">
+        Pure
+      </span>
+    </Tooltip>
   );
 }
 
@@ -87,13 +93,18 @@ function SelfNode({ data }) {
         className="bg-no-repeat bg-cover w-60 h-[40px] rounded-t-[12px]"
         style={{ backgroundImage: `url(${bannerUrl})` }}
       >
+        <PureBadge
+          data={data}
+          className="inline-flex absolute h-5 right-2 top-2"
+        />
+
         <div className="w-full relative top-[20px] flex justify-center">
           <UserAvatar address={data?.address} badge={data.badge} />
         </div>
       </div>
       <div className="px-4 py-2 mt-5">
         <div className="flex items-center justify-between h-5">
-          <AddressLabelWithBadge data={data} />
+          <AddressLabel data={data} />
         </div>
         <DisplayUserAddress
           showLinks={false}
@@ -112,7 +123,8 @@ function RelativeUserNode({ data }) {
       <UserAvatar address={data?.address} badge={data.badge} />
       <div className="flex-1">
         <div className="flex items-center justify-between h-6">
-          <AddressLabelWithBadge data={data} />
+          <AddressLabel data={data} />
+          <PureBadge data={data} />
         </div>
         <DisplayUserAddress
           showLinks={false}
