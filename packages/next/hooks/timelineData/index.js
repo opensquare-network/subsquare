c949e6a1a97ad87caa15f781785b169fb1418d84
quinn-gao <quinnn.gao@gmail.com>
2025-04-15 14:59:02 +0800

    fix: add timeline tabswitch in DemocracyReferendaProposalsDetailMultiTabs

--- packages/next/hooks/timelineData/index.js
+++ packages/next/hooks/timelineData/index.js
@@ -2,3 +2,4 @@ export { default as useReferendumTimelineData } from "./useReferendumTimelineDat
 export { default as useAllianceAnnouncementTimelineData } from "./useAllianceAnnouncementTimelineData";
 export { default as useDemocracyExternalProposalTimelineData } from "./useDemocracyExternalProposalTimelineData";
 export { default as useDemocracyPublicProposalTimelineData } from "./useDemocracyPublicProposalTimelineData";
+export { default as useDemocracyReferendaProposalTimelineData } from "./useDemocracyReferendaProposalTimelineData";
