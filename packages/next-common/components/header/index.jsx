f851eaea908bd339b102bc65c62567b8219b5a9d
qiaochao <qiaochao@icourt.cc>
2025-04-15 14:39:57 +0800

    feat: add a judgment on whether there is a referenda of chain

--- packages/next-common/components/header/index.jsx
+++ packages/next-common/components/header/index.jsx
@@ -2,17 +2,18 @@ import { useChainSettings } from "next-common/context/chain";
 import HeaderAccount from "./headerAccount";
 import NetworkSwitch from "./networkSwitch";
 import NodeSwitch from "./nodeSwitch";
-// import SearchInput from "./searchInput";
+import SearchInput from "./searchInput";
 import SearchInputWithPopup from "./searchInputWithPopup";
 export default function Header() {
   const chainSettings = useChainSettings();
+  const {
+    modules: { referenda: hasReferenda },
+  } = chainSettings;
 
   return (
     <header className="py-4 px-6 flex gap-x-6 border-b border-neutral300 bg-neutral100">
       <div className="flex-1">
-        {/* TOOD: add judgement on using which one input */}
-        {/* <SearchInput /> */}
-        <SearchInputWithPopup />
+        {hasReferenda ? <SearchInputWithPopup /> : <SearchInput />}
       </div>
 
       <div className="flex gap-x-2 relative">
