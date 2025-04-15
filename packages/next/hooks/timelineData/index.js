b4650173e7d30e8c97094a0a1cd59cda44f65fb8
quinn-gao <quinnn.gao@gmail.com>
2025-04-15 14:44:27 +0800

    fix: add timeline tabswitch in DemocracyPublicProposalsDetailMultiTabs

--- packages/next/hooks/timelineData/index.js
+++ packages/next/hooks/timelineData/index.js
@@ -1,3 +1,4 @@
 export { default as useReferendumTimelineData } from "./useReferendumTimelineData";
 export { default as useAllianceAnnouncementTimelineData } from "./useAllianceAnnouncementTimelineData";
 export { default as useDemocracyExternalProposalTimelineData } from "./useDemocracyExternalProposalTimelineData";
+export { default as useDemocracyPublicProposalTimelineData } from "./useDemocracyPublicProposalTimelineData";
---
+++ packages/next/hooks/timelineData/useDemocracyPublicProposalTimelineData.js
@@ -0,0 +1,45 @@
+import { getTimelineStatus } from "next-common/utils/index";
+import { detailPageCategory } from "next-common/utils/consts/business/category";
+import { useTimelineData } from "next-common/context/post";
+import formatTime from "next-common/utils/viewfuncs/formatDate";
+import { useMemo } from "react";
+import SymbolBalance from "next-common/components/values/symbolBalance";
+import sortTimeline from "next-common/utils/timeline/sort";
+
+function makePublicProposalTimelineData(timeline) {
+  const getTimelineData = (args, method) => {
+    switch (method) {
+      case "Proposed":
+        return {
+          Index: `#${args.index}`,
+        };
+      case "Tabled":
+        return {
+          "Referendum Index": `#${args.referendumIndex}`,
+          Deposit: <SymbolBalance value={args.deposit} />,
+          Depositors: (args.depositors || []).length,
+        };
+    }
+    return args;
+  };
+
+  const timelineData = (timeline || []).map((item) => {
+    return {
+      time: formatTime(item.indexer.blockTime),
+      indexer: item.indexer,
+      status: getTimelineStatus(
+        detailPageCategory.DEMOCRACY_PROPOSAL,
+        item.method ?? item.name,
+      ),
+      data: getTimelineData(item.args, item.method ?? item.name),
+    };
+  });
+  sortTimeline(timelineData);
+
+  return timelineData;
+}
+
+export default function useDemocracyPublicProposalTimelineData() {
+  const timeline = useTimelineData();
+  return useMemo(() => makePublicProposalTimelineData(timeline), [timeline]);
+}
