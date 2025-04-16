bf15489463431355ac12258f532f6ad26ef714b4
qiaochao <qiaochao@icourt.cc>
2025-04-16 10:25:37 +0800

    feat: padding change

--- packages/next-common/components/header/search/common/commonList.js
+++ packages/next-common/components/header/search/common/commonList.js
@@ -4,7 +4,7 @@ import DataList from "next-common/components/dataList";
 function CommonList({ data, isLoading, title, ItemBox }) {
   return (
     <div className="pt-2">
-      <p className="h-9 pl-4 flex items-center text12Medium text-textTertiary">
+      <p className="h-9 px-4 flex items-center text12Medium text-textTertiary">
         {title}
       </p>
       <DataList
@@ -15,7 +15,7 @@ function CommonList({ data, isLoading, title, ItemBox }) {
           <ItemBox key={idx} row={rows[idx]} />
         )}
         className="max-h-[525px] overflow-auto"
-        contentClassName="border-0 divide-y-0"
+        contentClassName="border-0 divide-y-0 px-2"
         titleClassName="border-0 pb-0"
       />
     </div>
