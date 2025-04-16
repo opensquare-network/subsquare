03909397265d6ad1ec0c58de5773a97394a40e7c
qiaochao <qiaochao@icourt.cc>
2025-04-16 19:59:18 +0800

    feat: request performance balancing

--- packages/next-common/components/header/search/popup/index.js
+++ packages/next-common/components/header/search/popup/index.js
@@ -1,6 +1,6 @@
 import Popup from "next-common/components/popup/wrapper/Popup";
 import { cn } from "next-common/utils";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReminderInput from "next-common/components/header/search/popup/reminderInput";
 import InputInSearchPopup from "next-common/components/header/search/popup/input";
 import LoadingSkeleton from "next-common/components/header/search/popup/loadingSkeleton";
@@ -31,17 +31,44 @@ function SearchPopup({ onClose }) {
     fetch(searchValue);
   });
 
+  const throttleTimer = React.useRef();
   const throttleSearch = useRefCallback(
-    throttle((value) => {
-      if (value.length > 2) {
-        handleSearch();
-      }
-    }, 3000),
+    throttle(
+      (value) => {
+        if (value.length > 2) {
+          handleSearch();
+        }
+      },
+      1000,
+      { leading: true, trailing: false },
+    ),
   );
 
-  React.useEffect(() => {
-    throttleSearch(searchValue);
-  }, [searchValue, throttleSearch]);
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    if (searchValue.length === 0) {
+      setReferenda(null);
+      if (throttleTimer.current) {
+        clearTimeout(throttleTimer.current);
+        throttleTimer.current = null;
+      }
+      abortController.abort();
+      return;
+    }
+
+    throttleTimer.current = setTimeout(() => {
+      throttleSearch(searchValue);
+    }, 300);
+
+    return () => {
+      if (throttleTimer.current) {
+        clearTimeout(throttleTimer.current);
+      }
+      abortController.abort();
+      setReferenda(null);
+    };
+  }, [searchValue, setReferenda, throttleSearch]);
 
   return (
     <Popup className="p-0" onClose={onClose}>
@@ -66,10 +93,12 @@ function SearchPopup({ onClose }) {
             <LoadingSkeleton />
           </Wrapper>
         )}
-        {referenda?.length > 0 && (
+        {Array.isArray(referenda) && referenda.length > 0 && (
           <ReferendaList data={referenda} onClose={onClose} />
         )}
-        {referenda?.length === 0 && !isLoading && <NoResult />}
+        {Array.isArray(referenda) && referenda.length === 0 && !isLoading && (
+          <NoResult />
+        )}
       </div>
     </Popup>
   );
