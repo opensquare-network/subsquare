990e8d98edc094d3a1733264a1893a3b7b0e59d6
quinn-gao <quinnn.gao@gmail.com>
2025-04-18 00:03:15 +0800

    fix: change create NewPreimage process

--- packages/next-common/components/summary/newProposalButton/submitProposalPopup.js
+++ packages/next-common/components/summary/newProposalButton/submitProposalPopup.js
@@ -1,54 +1,12 @@
-import { useCallback } from "react";
-import { NewProposalInnerPopup } from "../newProposalPopup";
-import { usePageProps } from "next-common/context/page";
-import { NewPreimageButton, NewProposalFromPreimageButton } from "./common";
-import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
-import ForwardPopupProvider, {
-  useForwardPopupContext,
-} from "next-common/context/forwardPopup";
-import Popup from "next-common/components/popup/wrapper/Popup";
-import { NewPreimageInnerPopup } from "next-common/components/preImages/newPreimagePopup";
-import ReferendaProposalQuickStart from "./referendaProposalQuickStart";
 import {
   useStepContainer,
   StepContainerProvider,
 } from "next-common/context/stepContainer";
-
-function NewPreimage() {
-  const { period } = usePageProps();
-  const { setForwardPopup } = useForwardPopupContext();
-
-  const onPreimageCreated = useCallback(
-    (hash, length) => {
-      setForwardPopup(
-        <NewProposalInnerPopup
-          track={period}
-          preimageHash={hash}
-          preimageLength={length}
-        />,
-      );
-    },
-    [period, setForwardPopup],
-  );
-
-  return (
-    <NewPreimageButton
-      onClick={() =>
-        setForwardPopup(<NewPreimageInnerPopup onCreated={onPreimageCreated} />)
-      }
-    />
-  );
-}
-
-function NewProposalFromPreImage() {
-  const { period } = usePageProps();
-  const { setForwardPopup } = useForwardPopupContext();
-  return (
-    <NewProposalFromPreimageButton
-      onClick={() => setForwardPopup(<NewProposalInnerPopup track={period} />)}
-    />
-  );
-}
+import CreateFormPreImage from "./createFromPreImage";
+import Popup from "next-common/components/popup/wrapper/Popup";
+import ForwardPopupProvider from "next-common/context/forwardPopup";
+import ReferendaProposalQuickStart from "./referendaProposalQuickStart";
+import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
 
 export default function SubmitProposalPopup({ onClose }) {
   return (
@@ -89,11 +47,7 @@ function SubmitProposal() {
   return (
     <>
       <ReferendaProposalQuickStart />
-      <div className="flex flex-col gap-[12px]">
-        <h6 className="text-textPrimary text14Bold ">Create from a Preimage</h6>
-        <NewPreimage />
-        <NewProposalFromPreImage />
-      </div>
+      <CreateFormPreImage />
     </>
   );
 }
