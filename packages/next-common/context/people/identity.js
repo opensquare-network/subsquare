d445d7b55973643a545df367bb832327d73719a3
leochen <leochen.sat24@gmail.com>
2025-04-21 17:23:33 +0800

    modify context, #5787

--- packages/next-common/context/people/identity.js
+++ packages/next-common/context/people/identity.js
@@ -1,11 +1,35 @@
 import useSubMyIdentityInfo from "next-common/hooks/people/useSubMyIdentityInfo";
 import { createContext, useContext } from "react";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import { isSameAddress } from "next-common/utils";
+import { useChain } from "next-common/context/chain";
+import {
+  isKusamaChain,
+  isPolkadotChain,
+  isPaseoChain,
+} from "next-common/utils/chain";
 
 const PeopleIdentityContext = createContext();
 
 export default PeopleIdentityContext;
 
 export function PeopleIdentityProvider({ children }) {
+  const chain = useChain();
+
+  if (isKusamaChain(chain) || isPolkadotChain(chain) || isPaseoChain(chain)) {
+    return <IdentityProvider>{children}</IdentityProvider>;
+  }
+
+  return (
+    <PeopleIdentityContext.Provider
+      value={{ identityInfo: null, isLoading: false }}
+    >
+      {children}
+    </PeopleIdentityContext.Provider>
+  );
+}
+
+function IdentityProvider({ children }) {
   const { result, isLoading } = useSubMyIdentityInfo();
 
   return (
@@ -15,6 +39,13 @@ export function PeopleIdentityProvider({ children }) {
   );
 }
 
-export function usePeopleIdentityContext() {
-  return useContext(PeopleIdentityContext) || {};
+export function usePeopleIdentityContext(address = "") {
+  const result = useContext(PeopleIdentityContext) || {};
+  const connectedAddress = useRealAddress();
+
+  if (isSameAddress(address, connectedAddress)) {
+    return result || null;
+  }
+
+  return null;
 }
