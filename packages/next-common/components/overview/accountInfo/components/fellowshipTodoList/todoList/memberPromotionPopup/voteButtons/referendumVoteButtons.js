5fa529664ae9bbfb5ba7a9fd97f865ccdcd50c19
Chaojun Huang <chaojun@opensquare.network>
2025-04-22 18:13:57 +0800

    Vote buttons on fellowship members page wish column, #5767 (#5819)
    * There should be vote buttons on fellowship members page wish column, #5767
    
    * fix, #5767
    
    * fix, #5767
    
    * fix, #5767
    
    * Improve text, #5767
    
    * Improve text, #5767
    
    * refactor, #5767
    
    * refactor, #5767

--- packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/memberPromotionPopup/voteButtons/referendumVoteButtons.js
+++ packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/memberPromotionPopup/voteButtons/referendumVoteButtons.js
@@ -4,8 +4,12 @@ import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { cn } from "next-common/utils";
 import { useSubFellowshipVote } from "next-common/utils/hooks/fellowship/useFellowshipVote";
 import VoteButton from "./voteButton";
+import SecondaryButton from "next-common/lib/button/secondary";
+import { useMyVotesChangedContext } from "../../../context/myVotesChanged";
+import { newSuccessToast } from "next-common/store/reducers/toastSlice";
+import { useDispatch } from "react-redux";
 
-function MyVote({ referendumIndex }) {
+export function MyVote({ referendumIndex }) {
   const realAddress = useRealAddress();
   const { result: myVote } = useSubFellowshipVote(referendumIndex, realAddress);
   const vote = myVote?.toJSON();
@@ -16,7 +20,7 @@ function MyVote({ referendumIndex }) {
 
   const tooltipContent = (
     <ul>
-      <li>Vote: {"aye" in vote ? "Aye" : "Nay"}</li>
+      <li>My Vote: {"aye" in vote ? "Aye" : "Nay"}</li>
       <li>Votes: {"aye" in vote ? vote.aye : vote.nay}</li>
     </ul>
   );
@@ -35,16 +39,58 @@ function MyVote({ referendumIndex }) {
   );
 }
 
+function SecondaryButtonWrapper({ disabled, onClick, children }) {
+  return (
+    <SecondaryButton
+      disabled={disabled}
+      className={cn("p-[6px]", disabled && "[&_svg_path]:stroke-textDisabled")}
+      size="small"
+      onClick={onClick}
+    >
+      {children}
+    </SecondaryButton>
+  );
+}
+
 export default function ReferendumVoteButtons({ referendumIndex }) {
+  const dispatch = useDispatch();
+  const { triggerMyVotesChanged } = useMyVotesChangedContext();
+
   return (
     <div className="flex gap-[12px] h-[31px] items-center justify-end">
       <MyVote referendumIndex={referendumIndex} />
 
-      <VoteButton referendumIndex={referendumIndex} voteAye={false}>
+      <VoteButton
+        referendumIndex={referendumIndex}
+        voteAye={false}
+        ButtonComponent={SecondaryButtonWrapper}
+        callbacks={{
+          onInBlock: () => {
+            dispatch(newSuccessToast("Vote successfully"));
+            triggerMyVotesChanged();
+          },
+          onFinalized: () => {
+            triggerMyVotesChanged();
+          },
+        }}
+      >
         <SystemVoteNay className="w-[16px]" />
       </VoteButton>
 
-      <VoteButton referendumIndex={referendumIndex} voteAye={true}>
+      <VoteButton
+        referendumIndex={referendumIndex}
+        voteAye={true}
+        ButtonComponent={SecondaryButtonWrapper}
+        callbacks={{
+          onInBlock: () => {
+            dispatch(newSuccessToast("Vote successfully"));
+            triggerMyVotesChanged();
+          },
+          onFinalized: () => {
+            triggerMyVotesChanged();
+          },
+        }}
+      >
         <SystemVoteAye className="w-[16px]" />
       </VoteButton>
     </div>
