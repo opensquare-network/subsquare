7f3b663021035a5049ce98a052553a6b689f662d
quinn-gao <53474899+quinn-gaoo@users.noreply.github.com>
2025-04-22 18:48:17 +0800

    Optimize the referenda voting dialog prompt, #5796 (#5817)
    * fix: change corywriting, add same tootips in vote dialog content, #5796
    
    * fix: solve the compatibility issue of pop-up box styles, #5781 (#5813)
    
    Co-authored-by: qiaochao <qiaochao@icourt.cc>
    
    * fix: distinguish between pc and mobile, and switch to reset, #5781 (#5814)
    
    * fix: distinguish between pc and mobile, and switch to reset, #5781
    
    * feat: optimized code, #5781
    
    * fix: return to the initial code, #5781
    
    ---------
    
    Co-authored-by: qiaochao <qiaochao@icourt.cc>
    
    * fix: use symbol variable instead of DOT, #5796
    
    ---------
    
    Co-authored-by: Zack <752716886@qq.com>
    Co-authored-by: qiaochao <qiaochao@icourt.cc>

--- packages/next-common/components/popup/fields/convictionField.js
+++ packages/next-common/components/popup/fields/convictionField.js
@@ -6,6 +6,7 @@ import ConvictionSlider from "next-common/components/convictionSlider";
 import { useChainSettings } from "next-common/context/chain";
 import { calcVotes } from "next-common/utils/democracy/votes/passed/common";
 import NumberWithComma from "next-common/components/numberWithComma";
+import Tooltip from "next-common/components/tooltip";
 
 export default function ConvictionField({
   balance = 0,
@@ -15,7 +16,7 @@ export default function ConvictionField({
   titleTooltip = "",
   module,
 }) {
-  const [time, isLoading] = useVoteLockTime(conviction, module);
+  const [time, isLoading, date] = useVoteLockTime(conviction, module);
   const chainSettings = useChainSettings();
   const symbol = chainSettings.voteSymbol || chainSettings.symbol;
 
@@ -36,7 +37,7 @@ export default function ConvictionField({
       <StatusWrapper className="flex-col gap-y-1">
         <div className="flex justify-between w-full">
           <div className="text-textSecondary">
-            <span>Votes</span>
+            <span>Total Votes</span>
           </div>
           <div className="result">
             <NumberWithComma
@@ -47,17 +48,23 @@ export default function ConvictionField({
         </div>
         <div className="flex justify-between w-full">
           <div className="text-textSecondary">
-            <span>Locked For</span>
+            <span>Lock Duration</span>
           </div>
-          <div className="result">{time ? "≈ " + time : 0}</div>
+          <div className="result">
+            {time ? (
+              <Tooltip content={`End on ${date}`}>{"≈ " + time}</Tooltip>
+            ) : (
+              0
+            )}
+          </div>
         </div>
 
         <hr className="w-full my-2" />
 
         <div className="text12Medium text-textTertiary">
-          Your voted {chainSettings.voteSymbol || chainSettings.symbol} will be
-          locked during the whole voting period of this referendum. The
-          conviction lock will start counting down when the voting period ends.
+          Your {chainSettings.voteSymbol || chainSettings.symbol} will stay
+          locked for the entire voting period. After the vote ends, it remains
+          locked for an additional period based on your selected multiplier.
         </div>
       </StatusWrapper>
     );
@@ -65,7 +72,11 @@ export default function ConvictionField({
 
   return (
     <div>
-      <PopupLabel text={title} titleTooltip={titleTooltip} />
+      <PopupLabel
+        text={title}
+        tooltip={titleTooltip}
+        tooltipContentClassName="max-w-[240px]"
+      />
       <ConvictionSlider value={conviction} setValue={setConviction} />
       {lockingPeriod && <div className="mt-2">{lockingPeriod}</div>}
     </div>
