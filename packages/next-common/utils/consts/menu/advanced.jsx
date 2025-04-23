6b2bfc0c3da7b767bacaebe32a37ec19b5999ac9
quinn-gao <quinnn.gao@gmail.com>
2025-04-23 17:29:13 +0800

    fix: hidden empty advance menu, #5802

--- packages/next-common/utils/consts/menu/advanced.jsx
+++ packages/next-common/utils/consts/menu/advanced.jsx
@@ -1,11 +1,13 @@
 import { MenuAdvanced } from "@osn/icons/subsquare";
 
 export default function getAdvancedMenu(advanceMenu = []) {
-  return {
-    name: "Advanced",
-    value: "advanced",
-    type: "advanced",
-    icon: <MenuAdvanced />,
-    items: advanceMenu,
-  };
+  return advanceMenu.length
+    ? {
+        name: "Advanced",
+        value: "advanced",
+        type: "advanced",
+        icon: <MenuAdvanced />,
+        items: advanceMenu,
+      }
+    : null;
 }
