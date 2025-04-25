8d8c3cce232f173619e556ca5cfc90e02eea2ff6
wolyshaw <wolyshaw@users.noreply.github.com>
2025-04-25 16:01:36 +0800

    Add identity registrars page #5828 (#5838)
    * add "Registrars" #5828
    
    * add empty status #5828
    
    * add total #5828
    
    * fix const name #5828
    
    * split "TableHeader" component #5828

--- packages/next-common/components/data/common/tableHeader.jsx
+++ packages/next-common/components/data/common/tableHeader.jsx
@@ -2,22 +2,43 @@ import useMyRelatedSwitch from "next-common/components/data/common/useMyRelatedS
 import useSearchComponent from "next-common/components/data/common/useSearchComponent";
 import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 
-export default function TableHeader({ total, loading, showMyRelated = true }) {
-  const { component: MyRelatedSwitchComponent } = useMyRelatedSwitch();
+export default function TableHeader({
+  title = "List",
+  total,
+  loading,
+  showMyRelated = true,
+}) {
   const { component: SearchBoxComponent } = useSearchComponent();
+
+  const HeaderContent = showMyRelated ? TableNameSwitch : TableName;
 
   return (
     <div>
-      <TitleContainer>
-        <span>
-          List
-          <span className="text-textTertiary text16Medium ml-1">
-            {!loading && total}
-          </span>
-        </span>
-        {showMyRelated && MyRelatedSwitchComponent}
-      </TitleContainer>
+      <HeaderContent title={title} total={total} loading={loading} />
       {SearchBoxComponent}
     </div>
+  );
+}
+
+export function TableName({ title, total, loading }) {
+  return (
+    <TitleContainer>
+      <span className="inline-flex items-center">
+        {title}
+        <span className="text-textTertiary text16Medium ml-1">
+          {!loading && total}
+        </span>
+      </span>
+    </TitleContainer>
+  );
+}
+
+export function TableNameSwitch({ title, total, loading }) {
+  const { component: MyRelatedSwitchComponent } = useMyRelatedSwitch();
+  return (
+    <TitleContainer>
+      <TableName title={title} total={total} loading={loading} />
+      {MyRelatedSwitchComponent}
+    </TitleContainer>
   );
 }
