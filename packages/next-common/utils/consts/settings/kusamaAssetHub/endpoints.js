6791866d3e0d0e4ee94ac9a360b374f77ebdcf8f
Yongfeng LI <wliyongfeng@gmail.com>
2025-04-25 21:08:12 +0800

    Refactor kusama chain config

--- packages/next-common/utils/consts/settings/kusamaAssetHub/endpoints.js
+++ packages/next-common/utils/consts/settings/kusamaAssetHub/endpoints.js
@@ -1,13 +1,13 @@
 const kusamaAssetHubNodes = [
   { name: "Parity", url: "wss://kusama-asset-hub-rpc.polkadot.io" },
-  { name: "IBP1", url: "wss://sys.ibp.network/statemine" },
+  { name: "IBP1", url: "wss://sys.ibp.network/asset-hub-kusama" },
   {
     name: "RadiumBlock",
     url: "wss://statemine.public.curie.radiumblock.co/ws",
   },
-  { name: "Dwellir", url: "wss://statemine-rpc.dwellir.com" },
+  { name: "Dwellir", url: "wss://asset-hub-kusama-rpc.dwellir.com" },
   { name: "Dwellir Tunisia", url: "wss://statemine-rpc-tn.dwellir.com" },
-  { name: "IBP2", url: "wss://sys.dotters.network/statemine" },
+  { name: "IBP2", url: "wss://asset-hub-kusama.dotters.network/" },
   { name: "LuckyFriday", url: "wss://rpc-asset-hub-kusama.luckyfriday.io" },
   { name: "Stakeworld", url: "wss://ksm-rpc.stakeworld.io/assethub" },
 ];
