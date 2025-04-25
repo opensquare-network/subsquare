2019a99b27cea4163c64e55b69d684d69b291bb1
wolyshaw <wolyshaw@users.noreply.github.com>
2025-04-25 18:33:37 +0800

    Fix sub identity display name #5848 (#5849)
    * fix sub name display #5848
    
    * fix name #5848
    
    * fix name #5848
    
    * clean identity display name fix#5848
    
    * wait submit #5848
    
    * check value #5848

--- packages/next-common/hooks/people/useSubMyIdentityInfo.js
+++ packages/next-common/hooks/people/useSubMyIdentityInfo.js
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { useContextApi } from "next-common/context/api";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import useSubStorage from "next-common/hooks/common/useSubStorage";
 
@@ -38,8 +39,60 @@ function convertIdentityInfo(identity) {
   };
 }
 
-export default function useSubMyIdentityInfo() {
+function useSuperOfIdentityDisplayName(identity) {
   const address = useRealAddress();
+  const api = useContextApi();
+  const [subDisplay, setSubDisplay] = useState(null);
+
+  useEffect(() => {
+    if (!api || !identity || identity?.display) {
+      return;
+    }
+
+    async function fetchIdentityInfo() {
+      const superOfResult = await api.query.identity
+        ?.superOf(address)
+        .then((superOf) => {
+          if (superOf?.isSome) {
+            const [parentAddress, subDisplay] =
+              superOf.unwrap()?.toHuman() || [];
+            return {
+              subDisplay: subDisplay?.Raw,
+              parentAddress,
+            };
+          }
+        });
+
+      if (superOfResult?.isNone || !superOfResult?.parentAddress) {
+        return;
+      }
+
+      const identityResult = await api.query.identity
+        ?.identityOf(superOfResult.parentAddress)
+        .then((parentResult) => {
+          if (!parentResult?.isNone) {
+            return convertIdentityInfo(parentResult);
+          }
+        });
+      if (identityResult.display && superOfResult.subDisplay) {
+        setSubDisplay(`${identityResult.display}/${superOfResult.subDisplay}`);
+      }
+    }
+
+    try {
+      fetchIdentityInfo();
+    } catch (error) {
+      console.error(error);
+    }
+  }, [api, identity, address]);
+
+  return {
+    result: subDisplay,
+    isLoading: false,
+  };
+}
+
+function useAddressIdentityInfo(address) {
   const { result, loading: isLoading } = useSubStorage(
     "identity",
     "identityOf",
@@ -55,7 +108,19 @@ export default function useSubMyIdentityInfo() {
   }, [result]);
 
   return {
-    isLoading,
     result: identity,
+    isLoading,
+  };
+}
+
+export default function useSubMyIdentityInfo() {
+  const address = useRealAddress();
+  const { result, isLoading } = useAddressIdentityInfo(address);
+  const { result: superResult } = useSuperOfIdentityDisplayName(result);
+
+  return {
+    isLoading,
+    result: result,
+    displayName: (result.display || superResult) ?? null,
   };
 }
--- packages/next-common/hooks/useIdentityInfo.js
+++ packages/next-common/hooks/useIdentityInfo.js
@@ -21,7 +21,7 @@ export function useChainAddressIdentityInfo(chain, address) {
   const cachedIdentity = getCachedIdentity(identityChain, encodedAddress);
   const [identity, setIdentity] = useState(cachedIdentity);
   const [isLoading, setIsLoading] = useState(true);
-  const { result: myIdentityInfo } = useSubMyIdentityInfo();
+  const { result: myIdentityInfo, displayName } = useSubMyIdentityInfo();
   const realAddress = useRealAddress();
 
   useEffect(() => {
@@ -38,7 +38,7 @@ export function useChainAddressIdentityInfo(chain, address) {
           const myIdentityIsEmpty = Object.values(myIdentityInfo).every(
             (item) => isNil(item),
           );
-          if (myIdentityIsEmpty) {
+          if (myIdentityIsEmpty && !displayName) {
             setIdentity(null);
             return;
           }
@@ -56,11 +56,19 @@ export function useChainAddressIdentityInfo(chain, address) {
             };
           }
 
+          if (
+            !peopleIdentity?.info?.display &&
+            displayName &&
+            peopleIdentity?.info
+          ) {
+            peopleIdentity.info.display = displayName;
+          }
+
           setIdentity(peopleIdentity);
         })
         .finally(() => setIsLoading(false));
     }
-  }, [address, identityChain, myIdentityInfo, chain, realAddress]);
+  }, [address, identityChain, myIdentityInfo, displayName, chain, realAddress]);
 
   return {
     identity,
