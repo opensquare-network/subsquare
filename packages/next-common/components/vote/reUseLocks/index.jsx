ea1b0087b32d1f3b7068a83dcbb25e90d0cdfb75
quinn-gao <quinnn.gao@gmail.com>
2025-04-28 15:01:03 +0800

    fix: referendum vote popu style, #5796

--- packages/next-common/components/vote/reUseLocks/index.jsx
+++ packages/next-common/components/vote/reUseLocks/index.jsx
@@ -3,7 +3,7 @@ import GovLocks from "./govLocks";
 
 const ReUseLocks = ({ reUseGovLocks, reUseAllLocks }) => {
   return (
-    <div className="grid grid-cols-2 w-full gap-2 max-sm:grid-cols-1">
+    <div className="w-full flex flex-wrap gap-2 md:flex-row">
       <GovLocks reUseGovLocks={reUseGovLocks} />
       <AllLocks reUseAllLocks={reUseAllLocks} />
     </div>
