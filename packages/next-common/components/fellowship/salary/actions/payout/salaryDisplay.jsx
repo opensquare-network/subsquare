439df68395ca2bf55d5e34cf19a9f6af8ada7752
qiaochao <qiaochao@icourt.cc>
2025-04-28 10:44:36 +0800

    feat: add title for salary display, #5808

--- packages/next-common/components/fellowship/salary/actions/payout/salaryDisplay.jsx
+++ packages/next-common/components/fellowship/salary/actions/payout/salaryDisplay.jsx
@@ -5,26 +5,31 @@ import { toPrecision } from "next-common/utils";
 
 function SalaryDisplay({ className = "", value = 0, decimals, symbol }) {
   return (
-    <div
-      className={cn(
-        "flex",
-        "items-center",
-        "rounded-[8px]",
-        "bg-neutral200",
-        "justify-between",
-        "text-textPrimary",
-        "text14Medium",
-        "border-[1px]",
-        "border-neutral400",
-        className,
-      )}
-    >
-      <ValueDisplay
-        className="flex-1 px-4 py-2.5"
-        key="value"
-        value={toPrecision(value, decimals)}
-      />
-      <span className="px-4 py-2.5 border-l border-neutral300">{symbol}</span>
+    <div>
+      <p className="flex items-center pb-2 text-textPrimary text14Medium font-bold">
+        salary
+      </p>
+      <p
+        className={cn(
+          "flex",
+          "items-center",
+          "rounded-[8px]",
+          "bg-neutral200",
+          "justify-between",
+          "text-textPrimary",
+          "text14Medium",
+          "border-[1px]",
+          "border-neutral400",
+          className,
+        )}
+      >
+        <ValueDisplay
+          className="flex-1 px-4 py-2.5"
+          key="value"
+          value={toPrecision(value, decimals)}
+        />
+        <span className="px-4 py-2.5 border-l border-neutral300">{symbol}</span>
+      </p>
     </div>
   );
 }
