72c7fda5ce26f1d4bb7434018d20d7329ec5c9be
leochen <leochen.sat24@gmail.com>
2025-04-28 11:43:41 +0800

    Fix: ci error, #5757

--- packages/next-common/services/multisig/url.js
+++ packages/next-common/services/multisig/url.js
@@ -1,12 +1,14 @@
 import getChainSettings from "next-common/utils/consts/settings";
 import Chains from "next-common/utils/consts/chains";
-import { getRelayChain } from "next-common/utils/chain";
 
 export default function getMultisigApiUrl(chain) {
   const settings = getChainSettings(chain);
-  const relayChain = getRelayChain(chain);
 
-  if ([Chains.kusama, Chains.polkadot, Chains.paseo].includes(relayChain)) {
+  if (
+    [Chains.kusama, Chains.polkadot, Chains.paseo].includes(
+      settings?.identity || chain,
+    )
+  ) {
     return `https://${settings.graphqlApiSubDomain}.statescan.io/graphql`;
   }
   if (!settings?.multisigApiPrefix) {
