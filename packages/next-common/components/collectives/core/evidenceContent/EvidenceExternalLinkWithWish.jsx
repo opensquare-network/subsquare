204a775c87f14e8e3897f472fdf353421e8a290f
qiaochao <qiaochao@icourt.cc>
2025-04-29 10:32:59 +0800

    fix: fix some style issues, #5863

--- packages/next-common/components/collectives/core/evidenceContent/EvidenceExternalLinkWithWish.jsx
+++ packages/next-common/components/collectives/core/evidenceContent/EvidenceExternalLinkWithWish.jsx
@@ -11,15 +11,15 @@ function EvidenceExternalLinkWithWish({ className = "", wish = "", cid = "" }) {
       {wish && (
         <p className="flex items-center flex-grow-0 pr-4  text14Bold">
           <span>{`Evidence for ${wish}`}</span>
-          <span className="border-r-[1px] border-neutral400 h-[12px] w-4"></span>
+          <span className="border-r-[1px] border-neutral400 h-4 w-4"></span>
           <span className="p-0 pl-4 h-full flex items-center">
-            <Tooltip content="IPFS Link" className="w-4 h-4">
+            <Tooltip content="IPFS Link" className="w-5 h-5">
               <ExternalLink
                 href={getIpfsLink(cid)}
                 externalIcon={false}
                 className="text-textTertiary hover:text-textSecondary"
               >
-                <LinkIpfs className="w-4 h-4" />
+                <LinkIpfs className="w-5 h-5" />
               </ExternalLink>
             </Tooltip>
           </span>
