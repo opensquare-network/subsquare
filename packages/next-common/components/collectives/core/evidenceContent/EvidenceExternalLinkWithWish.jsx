1207683554ddad930354f5a12e2b023445bb8935
qiaochao <qiaochao@icourt.cc>
2025-04-29 11:02:19 +0800

    fix: modify the return logic of component, #5863

--- packages/next-common/components/collectives/core/evidenceContent/EvidenceExternalLinkWithWish.jsx
+++ packages/next-common/components/collectives/core/evidenceContent/EvidenceExternalLinkWithWish.jsx
@@ -6,25 +6,27 @@ import getIpfsLink from "next-common/utils/env/ipfsEndpoint";
 import React from "react";
 
 function EvidenceExternalLinkWithWish({ className = "", wish = "", cid = "" }) {
+  if (!wish || !cid) {
+    return null;
+  }
+
   return (
     <div className={cn(className)}>
-      {wish && (
-        <p className="flex items-center flex-grow-0 pr-4  text14Bold">
-          <span>{`Evidence for ${wish}`}</span>
-          <span className="border-r-[1px] border-neutral400 h-4 w-4"></span>
-          <span className="p-0 pl-4 h-full flex items-center">
-            <Tooltip content="IPFS Link" className="w-5 h-5">
-              <ExternalLink
-                href={getIpfsLink(cid)}
-                externalIcon={false}
-                className="text-textTertiary hover:text-textSecondary"
-              >
-                <LinkIpfs className="w-5 h-5" />
-              </ExternalLink>
-            </Tooltip>
-          </span>
-        </p>
-      )}
+      <p className="flex items-center flex-grow-0 pr-4  text14Bold">
+        <span>{`Evidence for ${wish}`}</span>
+        <span className="border-r-[1px] border-neutral400 h-4 w-4"></span>
+        <span className="p-0 pl-4 h-full flex items-center">
+          <Tooltip content="IPFS Link" className="w-5 h-5">
+            <ExternalLink
+              href={getIpfsLink(cid)}
+              externalIcon={false}
+              className="text-textTertiary hover:text-textSecondary"
+            >
+              <LinkIpfs className="w-5 h-5" />
+            </ExternalLink>
+          </Tooltip>
+        </span>
+      </p>
     </div>
   );
 }
