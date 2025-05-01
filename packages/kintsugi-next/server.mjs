86b885484d1a79117926542cfede775412f18c51
chaojun <chaojun@opensquare.network>
2025-05-02 00:45:55 +0800

    Revert "fix: startup script"
    This reverts commit 4b82158ca91ae0bb4badd54e37eb1ce9a20e316b.

--- packages/kintsugi-next/server.mjs
+++ packages/kintsugi-next/server.mjs
@@ -1,28 +1,59 @@
-import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 import nextEnv from "@next/env";
+import httpProxy from "http-proxy";
+import { createServer } from "http";
+const { loadEnvConfig } = nextEnv;
 
 // Load next env
 const projectDir = process.cwd();
-nextEnv.loadEnvConfig(projectDir);
+loadEnvConfig(projectDir);
+
+const ssrUrl = new URL(process.env.NEXT_PUBLIC_BACKEND_API_END_POINT);
+const proxy = httpProxy.createProxyServer({
+  target: {
+    protocol: ssrUrl.protocol,
+    host: ssrUrl.hostname,
+    port: ssrUrl.port,
+  },
+  changeOrigin: true,
+});
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const nextHandler = app.getRequestHandler();
 
-const PORT = parseInt(process.env.PORT, 10);
+const PORT = process.env.PORT;
 if (!PORT) {
   console.info("PORT is not defined");
   process.exit();
 }
 
-// Start production server
 app.prepare().then(() => {
-  createServer((req, res) => {
+  const httpServer = createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    nextHandler(req, res, parsedUrl);
-  }).listen(PORT, (err) => {
+    const { pathname } = parsedUrl;
+
+    if (pathname.startsWith("/api/")) {
+      req.url = req.url.replace(/^\/api\//, ssrUrl.pathname);
+      proxy.web(req, res, { autoRewrite: false });
+    } else if (pathname.startsWith("/socket.io/")) {
+      proxy.web(req, res, { autoRewrite: false });
+    } else {
+      nextHandler(req, res, parsedUrl);
+    }
+  });
+
+  httpServer.on("upgrade", function (req, socket, head) {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    if (pathname.startsWith("/socket.io/")) {
+      proxy.ws(req, socket, head);
+    }
+  });
+
+  httpServer.listen(PORT, (err) => {
     if (err) throw err;
     console.info(`> Ready on http://127.0.0.1:${PORT}`);
   });
