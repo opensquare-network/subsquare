c1c0aecb094c5e1876f4430418d35d7bc8955b8f
leochen <leochen.sat24@gmail.com>
2025-05-07 15:30:02 +0800

    matchJudgementStatus, #5904

--- packages/next-common/hooks/people/useOnchainPeopleIdentityList.js
+++ packages/next-common/hooks/people/useOnchainPeopleIdentityList.js
@@ -2,6 +2,33 @@ import { useState, useEffect } from "react";
 import { useContextApi } from "next-common/context/api";
 import useCall from "next-common/utils/hooks/useCall";
 
+function matchJudgementStatus(storageValue) {
+  const unwrappedValue = storageValue?.unwrap() || {};
+  const { judgements } = Array.isArray(unwrappedValue)
+    ? unwrappedValue[0]
+    : unwrappedValue;
+
+  if (judgements?.length >= 1) {
+    if (
+      judgements?.toString()?.includes("knownGood") ||
+      judgements?.toString()?.includes("reasonable")
+    ) {
+      return "verified";
+    } else if (
+      judgements?.toString()?.includes("erroneous") ||
+      judgements?.toString()?.includes("lowQuality") ||
+      judgements?.toString()?.includes("unknown") ||
+      judgements?.toString()?.includes("outOfDate")
+    ) {
+      return "erroneous";
+    } else {
+      return "unverified";
+    }
+  } else {
+    return "unverified";
+  }
+}
+
 export default function useOnchainPeopleIdentityList() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState(null);
@@ -18,25 +45,7 @@ export default function useOnchainPeopleIdentityList() {
 
     const results = (value || []).map(([storageKey, storageValue]) => {
       const address = storageKey?.args[0]?.toString();
-      const { judgements = [] } = storageValue?.unwrap() || {};
-      let status = "";
-
-      if (judgements?.length >= 1) {
-        if (judgements[0][1]?.isKnownGood || judgements[0][1]?.isReasonable) {
-          status = "verified";
-        } else if (
-          judgements[0][1]?.isErroneous ||
-          judgements[0][1]?.isLowQuality ||
-          judgements[0][1]?.isUnknown ||
-          judgements[0][1]?.isOutOfDate
-        ) {
-          status = "erroneous";
-        } else {
-          status = "unverified";
-        }
-      } else {
-        status = "unverified";
-      }
+      const status = matchJudgementStatus(storageValue);
 
       return {
         address,
