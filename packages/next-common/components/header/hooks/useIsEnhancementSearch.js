1280e24d8365bc28269e22b23f602c604e0b20cb
qiaochao <qiaochao@icourt.cc>
2025-05-08 15:16:06 +0800

    fix: simplify the deconstruction logic, #5928

--- packages/next-common/components/header/hooks/useIsEnhancementSearch.js
+++ packages/next-common/components/header/hooks/useIsEnhancementSearch.js
@@ -1,12 +1,13 @@
 import { useChainSettings } from "next-common/context/chain";
 
 function useIsEnhancementSearch() {
-  const chainSettings = useChainSettings();
   const {
-    modules: { referenda: hasReferenda, democracy = {}, treasury = {} },
-  } = chainSettings;
-  const { referenda: hasDemocracyReferenda } = democracy;
-  const { bounties: hasBounties, childBounties: hasChildBounties } = treasury;
+    modules: {
+      referenda: hasReferenda,
+      democracy: { referenda: hasDemocracyReferenda } = {},
+      treasury: { bounties: hasBounties, childBounties: hasChildBounties } = {},
+    },
+  } = useChainSettings();
   return (
     hasReferenda || hasDemocracyReferenda || hasBounties || hasChildBounties
   );
