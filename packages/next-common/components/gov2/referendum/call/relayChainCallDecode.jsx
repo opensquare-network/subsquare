8f4f4a8a6ab7b4253ac2b005a1b31d09c3be3086
wolyshaw <wolyshaw@users.noreply.github.com>
2025-05-14 17:57:22 +0800

    add destApi & realyChainBlockNumber #5975

--- packages/next-common/components/gov2/referendum/call/relayChainCallDecode.jsx
+++ packages/next-common/components/gov2/referendum/call/relayChainCallDecode.jsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { isObject } from "lodash-es";
 import { DecodeCallItem } from "./decodeItem";
 import Row from "next-common/components/listInfo/row";
 import DecodeCallList from "./decodeList";
+import getChainSettings from "next-common/utils/consts/settings";
+import {
+  getBlockApiByHeight,
+  getOriginApi,
+} from "next-common/services/chain/api";
+import { useAsync } from "react-use";
+import { CHAIN } from "next-common/utils/constants";
+import { getRelayChain } from "next-common/utils/chain";
+import { useEffect, useMemo, useState } from "react";
+import { useContextApi } from "next-common/context/api";
+import { useOnchainData } from "next-common/context/post";
+
+const apiMap = new Map();
 
 export default function RelayChainCall({ call }) {
   const { value: relayChainDecodes } = useRelayChainCallDecodeType(call);
@@ -33,33 +45,140 @@ export default function RelayChainCall({ call }) {
   );
 }
 
-export async function extractRelayChainInputsWithContext(data) {
-  const encodedResults = [];
-
-  async function findEncoded(item) {
-    if (!isObject(item)) {
-      return;
-    }
-
-    if (Array.isArray(item)) {
-      await Promise.all(item.map(findEncoded));
-      return;
-    }
-
-    if (item.encoded && typeof item.encoded === "string") {
-      encodedResults.push(item.encoded);
-    }
-
-    await Promise.all(Object.values(item).map(findEncoded));
-  }
-
-  await findEncoded(data);
-  return encodedResults;
+function useReferendaIsActived() {
+  const onchainData = useOnchainData();
+  return useMemo(() => {
+    const name = onchainData?.state?.name;
+    return ["DecisionDepositPlaced", "DecisionStarted"].includes(name);
+  }, [onchainData]);
 }
 
-export function useRelayChainCallDecodeType() {
+function useRealyChainBlockNumber() {
+  const api = useContextApi();
+  const onchainData = useOnchainData();
+  const isActived = useReferendaIsActived();
+  const [relayChainBlockNumber, setRelayChainBlockNumber] = useState();
+
+  useEffect(() => {
+    if (!isActived && api) {
+      const confirmedTl = onchainData.timeline.find(
+        (tl) => tl.name === "Submitted",
+      );
+      const blockHeight = confirmedTl.indexer.blockHeight;
+      if (blockHeight) {
+        getBlockApiByHeight(api, blockHeight)
+          .then((atApi) =>
+            atApi?.query?.parachainSystem.lastRelayChainBlockNumber(),
+          )
+          .then((res) => res.toNumber())
+          .then((relayNumber) => setRelayChainBlockNumber(relayNumber));
+      }
+    }
+  }, [api, isActived, onchainData]);
+
   return {
-    value: [],
+    relayChainBlockNumber,
+  };
+}
+
+export function useRelayChainCallDecodeType(call) {
+  const { value: encodeCalls } = useAsync(() => extractXcmContext(call));
+  const [results, setResults] = useState([]);
+  const { relayChainBlockNumber } = useRealyChainBlockNumber();
+
+  useEffect(() => {
+    const decodeResults = [];
+    if (encodeCalls?.length && relayChainBlockNumber) {
+      for (const { destChainId, encodedCalls: calls } of encodeCalls) {
+        getDestApi(destChainId)
+          ?.then((api) => {
+            return getBlockApiByHeight(api, relayChainBlockNumber);
+          })
+          .then((apiAt) => {
+            if (apiAt && calls?.length) {
+              for (const call of calls) {
+                const result = apiAt?.registry?.createType("Call", call);
+                if (result) {
+                  decodeResults.push(result.toHuman?.());
+                }
+              }
+              setResults(decodeResults);
+              clearDestApi();
+            }
+          });
+      }
+    }
+  }, [encodeCalls, relayChainBlockNumber]);
+
+  return {
+    value: results,
     loading: false,
   };
+}
+
+export async function extractXcmContext(data) {
+  if (!data || data.section !== "polkadotXcm") {
+    return { error: "Invalid polkadotXcm.send JSON" };
+  }
+
+  const dest = data.args[0]?.value;
+  const message = data.args[1]?.value;
+  const destChainId = parseDestChainId(dest);
+
+  const encodedCalls = [];
+  if (message.v4 && Array.isArray(message.v4)) {
+    message.v4.forEach((instruction) => {
+      if (instruction.transact?.call?.encoded) {
+        encodedCalls.push(instruction.transact.call.encoded);
+      }
+    });
+  }
+
+  return [
+    {
+      destChainId,
+      encodedCalls,
+    },
+  ];
+}
+
+function parseDestChainId(dest) {
+  if (!dest.v4) return null;
+  const { parents, interior } = dest.v4;
+
+  if (parents === 1 && interior.here === null) {
+    return "0";
+  }
+  if (parents === 1 && interior.X1?.Parachain) {
+    return interior.X1.Parachain.toString();
+  }
+  if (parents === 1 && interior.X2?.[0]?.Parachain) {
+    return interior.X2[0].Parachain.toString();
+  }
+  return null;
+}
+
+function getDestApi(destChainId) {
+  const chainMap = {
+    0: getRelayChain(CHAIN),
+  };
+  const chain = chainMap[destChainId];
+  if (!chain) {
+    return null;
+  }
+  const { endpoints } = getChainSettings(chain);
+  const endpoint = endpoints[0]?.url;
+
+  if (!apiMap.get(endpoint)) {
+    apiMap.set(endpoint, getOriginApi(chain, endpoint));
+  }
+
+  return apiMap.get(endpoint);
+}
+
+function clearDestApi() {
+  apiMap.forEach(async (api) => {
+    (await api)?.disconnect();
+  });
+  apiMap.clear();
 }
