2f9edae55fb84d29713ede4457a57c8235205b4a
qiaochao <qiaochao@icourt.cc>
2025-05-14 18:23:09 +0800

    fix: remove polkaassembly package in collectives, #5793

--- packages/collectives-next/pages/polkassembly/post/[id].js
+++
@@ -1,9 +0,0 @@
-import { getRedirectServerSideProps } from "next-common/services/serverSide";
-
-export default function Post() {
-  return "Please visit `/polkassembly/posts/[id]`";
-}
-
-export const getServerSideProps = getRedirectServerSideProps(
-  (id) => `/polkassembly/posts/${id}`,
-);
