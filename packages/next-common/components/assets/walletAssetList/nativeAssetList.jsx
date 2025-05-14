28674c95689c9f50ac98e6d22bd8a97bf1d8997f
chaojun <chaojun@opensquare.network>
2025-05-14 18:17:17 +0800

    Revert "fix: cross-chain transport popup disappear"
    This reverts commit 903e686d1a64d5ea799a237e4aa948cc580126ee.

--- packages/next-common/components/assets/walletAssetList/nativeAssetList.jsx
+++ packages/next-common/components/assets/walletAssetList/nativeAssetList.jsx
@@ -2,11 +2,11 @@ import ScrollerX from "next-common/components/styled/containers/scrollerX";
 import { MapDataList } from "next-common/components/dataList";
 import ParaChainTeleportButton from "../paraChainTeleportButton";
 import { useNativeTokenIcon } from "next-common/components/assets/known";
-import { formatBalance } from "next-common/components/assets/assetsList";
+import {
+  colTotal,
+  colTransferrable,
+} from "next-common/components/assets/assetsList";
 import { useMyNativeAsset } from "next-common/hooks/assets/useMyNativeAsset";
-import BalanceDisplay from "../balanceDisplay";
-import { useSubBalanceInfo } from "next-common/hooks/balance/useSubBalanceInfo";
-import useRealAddress from "next-common/utils/hooks/useRealAddress";
 
 function TokenSymbol({ symbol }) {
   const NativeAssetIcon = useNativeTokenIcon();
@@ -14,30 +14,6 @@ function TokenSymbol({ symbol }) {
     <div className="flex gap-[8px] items-center text14Medium text-textPrimary">
       <NativeAssetIcon width={24} height={24} /> {symbol}
     </div>
-  );
-}
-
-function Transferrable({ transferrable, decimals }) {
-  const address = useRealAddress();
-  const { value } = useSubBalanceInfo(address);
-  return (
-    <span className="text14Medium text-textPrimary">
-      <BalanceDisplay
-        balance={formatBalance(value?.transferrable || transferrable, decimals)}
-      />
-    </span>
-  );
-}
-
-function Balance({ balance, decimals }) {
-  const address = useRealAddress();
-  const { value } = useSubBalanceInfo(address);
-  return (
-    <span className="text14Medium text-textPrimary">
-      <BalanceDisplay
-        balance={formatBalance(value?.balance || balance, decimals)}
-      />
-    </span>
   );
 }
 
@@ -51,26 +27,6 @@ const teleport = {
   name: "",
   style: { textAlign: "right", width: "80px", minWidth: "80px" },
   render: () => <ParaChainTeleportButton />,
-};
-
-const colTotal = {
-  name: "Total",
-  style: { textAlign: "right", width: "160px", minWidth: "160px" },
-  render: (item) => (
-    <Balance key="total" balance={item.balance} decimals={item.decimals} />
-  ),
-};
-
-const colTransferrable = {
-  name: "Transferrable",
-  style: { textAlign: "right", width: "160px", minWidth: "160px" },
-  render: (item) => (
-    <Transferrable
-      key="transferrable"
-      transferrable={item.transferrable}
-      decimals={item.decimals}
-    />
-  ),
 };
 
 const columnsDef = [colToken, colTotal, colTransferrable, teleport];
