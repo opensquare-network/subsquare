0d50203d7f33075c4782623a2feec55e9ffcb8d9
Yongfeng LI <wliyongfeng@gmail.com>
2025-05-16 17:43:17 +0800

    Remove useless pages for collectives, #5793 (#5992)

--- packages/collectives-next/pages/privacy.js
+++
@@ -1,6 +0,0 @@
-import Privacy from "next-common/components/pages/privacy";
-import { getServerSidePropsWithTracks } from "next-common/services/serverSide";
-
-export default Privacy;
-
-export const getServerSideProps = getServerSidePropsWithTracks;
--- packages/collectives-next/pages/verify.js
+++
@@ -1,6 +0,0 @@
-import Verify from "next-common/components/pages/verify";
-import { withCommonProps } from "next-common/lib";
-
-export default Verify;
-
-export const getServerSideProps = withCommonProps();
