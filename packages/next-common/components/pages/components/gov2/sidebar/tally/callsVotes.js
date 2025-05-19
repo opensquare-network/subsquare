e3ab522826b86b9e970230778edbf6816dca13d7
quinn-gao <quinnn.gao@gmail.com>
2025-05-19 18:09:22 +0800

    fix: add tooltip in referenda detail Tally sidebar, #5982

--- packages/next-common/components/pages/components/gov2/sidebar/tally/callsVotes.js
+++ packages/next-common/components/pages/components/gov2/sidebar/tally/callsVotes.js
@@ -1,6 +1,7 @@
 import { Button } from "./styled";
 import { useState } from "react";
 import dynamicPopup from "next-common/lib/dynamic/popup";
+import Tooltip from "next-common/components/tooltip";
 
 const OpenGovCallsVotesPopup = dynamicPopup(() =>
   import("./openGovCallsVotesPopup"),
@@ -11,7 +12,12 @@ export default function CallsVotes() {
 
   return (
     <>
-      <Button onClick={() => setShowCallsVotes(true)}>Calls</Button>
+      <Tooltip
+        contentClassName="max-w-[240px]"
+        content="Shows the on-chain transaction that the proposal will execute."
+      >
+        <Button onClick={() => setShowCallsVotes(true)}>Calls</Button>
+      </Tooltip>
       {showCallsVotes && (
         <OpenGovCallsVotesPopup setShowVoteList={setShowCallsVotes} />
       )}
