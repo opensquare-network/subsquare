f33db39bf32e7e84478a4a05df58c7257ecd16ed
qiaochao <qiaochao@icourt.cc>
2025-05-19 13:38:48 +0800

    feat: show the specific data on track tooltip, #5959

--- packages/next-common/components/referenda/trackPanel/trackStreamlinedDetails.jsx
+++ packages/next-common/components/referenda/trackPanel/trackStreamlinedDetails.jsx
@@ -1,10 +1,55 @@
 import React, { memo } from "react";
 import useTrackDetail from "next-common/components/summary/newProposalPopup/useTrackDetail";
 import { startCase } from "lodash-es";
-import { Wrapper, LineBox, LineTitle, LineValue, Divider } from "./lineItem.js";
+import { Wrapper, LineBox, LineTitle, LineValue, Divider } from "./lineItem";
+import useTrackSummary from "./hooks/useTrackSummary";
+import { useAsync } from "react-use";
+import { isNil } from "lodash-es";
+import { useChainSettings } from "next-common/context/chain";
+import { useSelector } from "react-redux";
+import { blockTimeSelector } from "next-common/store/reducers/chainSlice";
+import { estimateBlocksTime, toPrecision } from "next-common/utils";
+import ValueDisplay from "next-common/components/valueDisplay";
+import { SummaryGreyText } from "next-common/components/summary/styled";
 
 function TrackStreamlinedDetails({ trackId }) {
-  const { id, name, description } = useTrackDetail(trackId);
+  const trackDetails = useTrackDetail(trackId);
+  const { summary, fetchTrackSummary } = useTrackSummary();
+  const { decimals, symbol } = useChainSettings();
+  const blockTime = useSelector(blockTimeSelector);
+  useAsync(async () => {
+    fetchTrackSummary(trackId);
+  }, []);
+  if (isNil(trackDetails)) return null;
+
+  const {
+    id,
+    name,
+    description,
+    maxDeciding,
+    preparePeriod,
+    decisionPeriod,
+    confirmPeriod,
+    decisionDeposit,
+    minEnactmentPeriod,
+  } = trackDetails;
+
+  const preparePeriodBlockTime = estimateBlocksTime(
+    preparePeriod,
+    blockTime,
+  ).split(" ");
+  const decisionPeriodBlockTime = estimateBlocksTime(
+    decisionPeriod,
+    blockTime,
+  ).split(" ");
+  const confirmPeriodBlockTime = estimateBlocksTime(
+    confirmPeriod,
+    blockTime,
+  ).split(" ");
+  const minEnactPeriodBlockTime = estimateBlocksTime(
+    minEnactmentPeriod,
+    blockTime,
+  ).split(" ");
 
   return (
     <Wrapper className="max-w-[320px]">
@@ -16,37 +61,49 @@ function TrackStreamlinedDetails({ trackId }) {
       <LineBox>
         <LineTitle title="Capacity" />
         <LineValue>
-          <span> 1</span>
+          <span>
+            {(summary?.decidingCount || 0) + (summary?.confirmingCount || 0)}
+          </span>
+          <SummaryGreyText> / {maxDeciding}</SummaryGreyText>
         </LineValue>
       </LineBox>
       <LineBox>
         <LineTitle title="Confirm Period" />
         <LineValue>
-          <span> 2</span>
+          <span>{confirmPeriodBlockTime?.[0] || 0}</span>
+          <SummaryGreyText> {confirmPeriodBlockTime[1]}</SummaryGreyText>
         </LineValue>
       </LineBox>
       <LineBox>
         <LineTitle title="Prepare Period" />
         <LineValue>
-          <span> 3</span>
+          <span>{preparePeriodBlockTime?.[0] || 0}</span>
+          <SummaryGreyText> {preparePeriodBlockTime[1]}</SummaryGreyText>
         </LineValue>
       </LineBox>
       <LineBox>
         <LineTitle title="Decision Period" />
         <LineValue>
-          <span> 4</span>
+          <span>{decisionPeriodBlockTime?.[0] || 0}</span>
+          <SummaryGreyText> {decisionPeriodBlockTime[1]}</SummaryGreyText>
         </LineValue>
       </LineBox>
       <LineBox>
         <LineTitle title="Min Enact Period" />
         <LineValue>
-          <span> 5</span>
+          <span>{minEnactPeriodBlockTime?.[0] || 0}</span>
+          <SummaryGreyText> {minEnactPeriodBlockTime[1]}</SummaryGreyText>
         </LineValue>
       </LineBox>
       <LineBox>
         <LineTitle title="Decision Deposit" />
         <LineValue>
-          <span> 6</span>
+          <span>
+            <ValueDisplay
+              value={toPrecision(decisionDeposit, decimals)}
+              symbol={symbol}
+            />
+          </span>
         </LineValue>
       </LineBox>
     </Wrapper>
