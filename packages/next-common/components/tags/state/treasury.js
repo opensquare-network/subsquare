3f2dc0b0b2e422e56c4aa3be796590f6476d7a7f
quinn-gao <quinnn.gao@gmail.com>
2025-05-20 17:41:46 +0800

    fix: add tooltip in treasury , #5911

--- packages/next-common/components/tags/state/treasury.js
+++ packages/next-common/components/tags/state/treasury.js
@@ -6,6 +6,7 @@ import {
   PositiveTag,
   StartTag,
 } from "./styled";
+import Tooltip from "next-common/components/tooltip";
 
 const stateTagMap = {
   Proposed: StartTag,
@@ -14,6 +15,15 @@ const stateTagMap = {
   Rejected: NegativeTag,
   ApproveVoting: PositiveTag,
   RejectVoting: NegativeTag,
+};
+
+const treasuryTooltipMap = {
+  Proposed: "Submitted and waiting to enter voting",
+  Awarded: "Proposal has been granted a reward",
+  Approved: "Vote passed but action not yet executed",
+  Rejected: "Vote failed and proposal not pass",
+  ApproveVoting: "Voting process has started to approve the proposal",
+  RejectVoting: "Voting process has started to reject the proposal",
 };
 
 const tipStateTagMap = {
@@ -27,6 +37,20 @@ const tipStateTagMap = {
   Tip: ActiveTag,
   "Tip Retracted": NegativeTag,
   "Tip Closed": ClosedTag,
+};
+
+const tipTooltipMap = {
+  Tipping: "Tipping process is open for contributions",
+  Retracted: "Tip proposal has been withdrawn",
+  Closed: "Tipping process has ended",
+  Slashed: "Funds have been penalized or removed due to misbehavior",
+
+  "Report Awesome": "Proposal reported as worthy of a tip",
+  tipNew: "A new tip suggestion has been submitted",
+  Tip: "A proposal open for decision-making",
+  "Tip Retracted": "Tip suggestion has been withdrawn",
+  "Tip Closed": "Tipping process has ended",
+  Removed: "Proposal has been removed from the process",
 };
 
 const bountyStateTagMap = {
@@ -56,6 +80,33 @@ const bountyStateTagMap = {
   RejectVoting: NegativeTag,
 };
 
+const bountyTooltipMap = {
+  Proposed: "Submitted and waiting to enter voting",
+  Approved: "Vote passed but action not yet executed",
+  Funded: "Proposal has been granted funding",
+  Active: "Funding has been approved and is currently in use",
+  PendingPayout: "Awaiting payout after approval",
+  Rejected: "Vote failed and proposal not pass",
+  Canceled: "Proposal was canceled before voting started",
+  Claimed: "Funds have been claimed by the recipient",
+
+  "Propose Bounty": "Bounty has been proposed and awaits approval",
+  BountyBecameActive: "Bounty has been approved and is now active",
+  acceptCurator: "Curator has accepted the responsibility",
+  BountyExtended: "Bounty duration or scope has been extended",
+  BountyAwarded: "Bounty has been granted to a contributor",
+  BountyClaimed: "Bounty reward has been claimed",
+  BountyRejected: "Bounty has been rejected",
+  BountyApproved: "Bounty has been approved",
+  CuratorUnassigned: "Curator role has been revoked or declined",
+  CuratorProposed: "A curator candidate has been nominated",
+
+  // in timeline
+  CloseVoting: "Voting period has ended",
+  ApproveVoting: "Voting process has started to approve the proposal",
+  RejectVoting: "Voting process has started to reject the proposal",
+};
+
 const childBountyStateTagMap = {
   Added: StartTag,
   CuratorProposed: ActiveTag,
@@ -71,6 +122,21 @@ const childBountyStateTagMap = {
   Awarded: PositiveTag,
 };
 
+const childBountyTooltipMap = {
+  Added: "Child bounty has been added to the bounty",
+  CuratorProposed: "A curator has been proposed for a child bounty",
+  Active: "Child bounty has been approved and is now active",
+  PendingPayout: "Awaiting payout after approval",
+  Rejected: "Child Bounty has been rejected",
+  Canceled: "Proposal was canceled before voting started",
+  Claimed: "Child bounty reward has been claimed",
+
+  // in timeline
+  proposeCurator: "A curator candidate has been nominated",
+  acceptCurator: "Curator has accepted the responsibility",
+  Awarded: "Proposal has been granted a reward",
+};
+
 const spendStateTagMap = {
   Approved: StartTag,
   Paid: PositiveTag,
@@ -78,9 +144,20 @@ const spendStateTagMap = {
   Voided: NegativeTag,
 };
 
+const spendTooltipMap = {
+  Approved: "Vote passed but action not yet executed",
+  Paid: "Funds have been disbursed",
+  Processed: "Proposal or transaction has been fully processed",
+  Voided: "Proposal or payment has been voided and invalidated",
+};
+
 export function TreasuryTag({ state }) {
   const Tag = stateTagMap[state] || ClosedTag;
-  return <Tag>{state}</Tag>;
+  return (
+    <Tooltip className="cursor-pointer" content={treasuryTooltipMap[state]}>
+      <Tag>{state}</Tag>
+    </Tooltip>
+  );
 }
 
 export function TipTag({ state }) {
@@ -89,20 +166,36 @@ export function TipTag({ state }) {
     Tag = ActiveTag;
   }
 
-  return <Tag>{state}</Tag>;
+  return (
+    <Tooltip className="cursor-pointer" content={tipTooltipMap[state]}>
+      <Tag>{state}</Tag>
+    </Tooltip>
+  );
 }
 
 export function BountyTag({ state }) {
   let Tag = bountyStateTagMap[state] || ClosedTag;
-  return <Tag>{state}</Tag>;
+  return (
+    <Tooltip className="cursor-pointer" content={bountyTooltipMap[state]}>
+      <Tag>{state}</Tag>
+    </Tooltip>
+  );
 }
 
 export function ChildBountyTag({ state }) {
   let Tag = childBountyStateTagMap[state] || ClosedTag;
-  return <Tag>{state}</Tag>;
+  return (
+    <Tooltip className="cursor-pointer" content={childBountyTooltipMap[state]}>
+      <Tag>{state}</Tag>
+    </Tooltip>
+  );
 }
 
 export function SpendTag({ state }) {
   let Tag = spendStateTagMap[state] || ClosedTag;
-  return <Tag>{state}</Tag>;
+  return (
+    <Tooltip className="cursor-pointer" content={spendTooltipMap[state]}>
+      <Tag>{state}</Tag>
+    </Tooltip>
+  );
 }
