7fc5e6d7b9d7d804f5d62a1081d04c050f9fa3ad
chaojun <chaojun@opensquare.network>
2025-05-21 12:20:48 +0800

    fix: more data fetch from backend api

--- packages/next-common/components/pages/components/gov2/checkUnFinalized.js
+++ packages/next-common/components/pages/components/gov2/checkUnFinalized.js
@@ -1,5 +1,5 @@
 import React from "react";
-import nextApi from "next-common/services/nextApi";
+import { backendApi } from "next-common/services/nextApi";
 import CheckUnFinalizedBase from "next-common/components/checkUnFinalizedBase";
 
 export default function CheckUnFinalized({ id }) {
@@ -8,7 +8,7 @@ export default function CheckUnFinalized({ id }) {
       onChainDataFetcher={async (api) =>
         api.query.referenda?.referendumInfoFor(id)
       }
-      serverPostFetcher={() => nextApi.fetch(`gov2/referendums/${id}`)}
+      serverPostFetcher={() => backendApi.fetch(`gov2/referendums/${id}`)}
     />
   );
 }
--- packages/next-common/services/serverSide/referenda/common.js
+++ packages/next-common/services/serverSide/referenda/common.js
@@ -1,6 +1,6 @@
 import { withCommonProps } from "next-common/lib";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
-import nextApi from "next-common/services/nextApi";
+import { backendApi } from "next-common/services/nextApi";
 import { gov2ReferendumsSummaryApi } from "next-common/services/url";
 import { merge, noop } from "lodash-es";
 
@@ -8,7 +8,7 @@ export function withReferendaCommonProps(fn = noop) {
   return withCommonProps(async (context) => {
     const [tracksProps, { result: gov2ReferendaSummary }] = await Promise.all([
       fetchOpenGovTracksProps(),
-      nextApi.fetch(gov2ReferendumsSummaryApi),
+      backendApi.fetch(gov2ReferendumsSummaryApi),
     ]);
 
     const res = await fn?.(context);
