5a467041c33293ef86f0589c248ed058c9d59ddb
quinn-gao <quinnn.gao@gmail.com>
2025-05-21 18:15:50 +0800

    fix: get submission deposit and tip , #6010

--- packages/next-common/components/summary/newProposalQuickStart/common/signingTip.js
+++ packages/next-common/components/summary/newProposalQuickStart/common/signingTip.js
@@ -1,8 +1,26 @@
+import { useContextApi } from "next-common/context/api";
+import useAddressBalance from "next-common/utils/hooks/useAddressBalance";
+import { useSignerAccount } from "next-common/components/popupWithSigner/context";
+
 export default function SigningTip() {
+  const api = useContextApi();
+  const signerAccount = useSignerAccount();
+
+  const [balance] = useAddressBalance(api, signerAccount?.realAddress);
+
+  const submissionDeposit = api.consts.referenda?.submissionDeposit.toString();
+
   return (
-    <div className="bg-neutral200 rounded-lg px-4 py-2.5 text14Medium text-textSecondary">
-      You&apos;ll be redirected to the referendum detail page after transaction
-      submission
-    </div>
+    <>
+      <div className="bg-neutral200 rounded-lg px-4 py-2.5 text14Medium text-textSecondary">
+        You&apos;ll be redirected to the referendum detail page after
+        transaction submission
+      </div>
+      {balance < submissionDeposit && (
+        <div className="bg-red100 rounded-lg px-4 py-2.5 text14Medium text-textSecondary">
+          You haven&apos;t enough balance for the referendum submission deposit.
+        </div>
+      )}
+    </>
   );
 }
