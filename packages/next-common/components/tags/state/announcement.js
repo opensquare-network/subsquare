216fe0da496d07ff0093952b05896e9d9a537efe
quinn-gao <quinnn.gao@gmail.com>
2025-05-21 10:52:24 +0800

    fix: remove announcement  status tag tooltip, #5911

--- packages/next-common/components/tags/state/announcement.js
+++ packages/next-common/components/tags/state/announcement.js
@@ -1,22 +1,12 @@
 import { ClosedTag, NegativeTag, PositiveTag } from "./styled";
 import React from "react";
-import Tooltip from "next-common/components/tooltip";
 
 const tagMap = {
   Announced: PositiveTag,
   Removed: NegativeTag,
 };
 
-const tooltipContent = {
-  Announced: "Proposal has been publicly declared",
-  Removed: "Proposal has been removed from the process",
-};
-
 export default function AnnouncementTag({ state }) {
   const Tag = tagMap[state] || ClosedTag;
-  return (
-    <Tooltip content={tooltipContent[state]}>
-      <Tag>{state}</Tag>
-    </Tooltip>
-  );
+  return <Tag>{state}</Tag>;
 }
