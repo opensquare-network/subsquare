51e42e2653a50a38a61ae85e69066e8079b167ea
chaojun <chaojun@opensquare.network>
2025-05-22 11:00:07 +0800

    Simplify API check

--- packages/next-common/api/disabledApi.js
+++ packages/next-common/api/disabledApi.js
@@ -1,24 +1,5 @@
 const disabledApiRoutes = [/^\/api\/gov2\/referendums/];
 
-function trimEndSlash(url) {
-  return url.replace(/\/+$/, "");
-}
-
 export function isApiDisabled(req) {
-  const isDisabledApi = disabledApiRoutes.some((route) => req.url.match(route));
-  if (!isDisabledApi) {
-    return false;
-  }
-
-  const isRequestFromSubsquare = req.headers.referer.startsWith(
-    trimEndSlash(process.env.NEXT_PUBLIC_API_END_POINT),
-  );
-  if (isRequestFromSubsquare) {
-    console.error(
-      "ERROR: Unexpected disabled api request from subsquare frontend, please check!",
-    );
-    return false;
-  }
-
-  return true;
+  return disabledApiRoutes.some((route) => req.url?.match?.(route));
 }
