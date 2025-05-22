d433a7aaa833d08776bdc2b786f658a136d16bf2
leochen <leochen.sat24@gmail.com>
2025-05-22 17:50:02 +0800

    optimize isAssetHubMigrated, #6023

--- packages/next-common/utils/consts/isAssetHubMigrated.js
+++ packages/next-common/utils/consts/isAssetHubMigrated.js
@@ -3,5 +3,5 @@ import getChainSettings from "./settings";
 export function isAssetHubMigrated() {
   const chain = process.env.NEXT_PUBLIC_CHAIN;
   const setting = getChainSettings(chain);
-  return setting.assetHubMigrated;
+  return setting.assetHubMigrated ?? false;
 }
