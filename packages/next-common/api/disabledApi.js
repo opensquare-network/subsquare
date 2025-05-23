cef31008618a5c7c3b2ee71007da4fdbd97f268a
Chaojun Huang <chaojun@opensquare.network>
2025-05-23 11:21:15 +0800

    Check if the request is initiated from subsquare (#6054)

--- packages/next-common/api/disabledApi.js
+++ packages/next-common/api/disabledApi.js
@@ -1,5 +1,26 @@
 const disabledApiRoutes = [/^\/api\/gov2\/referendums/];
 
+function trimEndSlash(url) {
+  return url.replace(/\/+$/, "");
+}
+
 export function isApiDisabled(req) {
-  return disabledApiRoutes.some((route) => req.url?.match?.(route));
+  const isDisabledApi = disabledApiRoutes.some((route) =>
+    req.url?.match?.(route),
+  );
+  if (!isDisabledApi) {
+    return false;
+  }
+
+  const isRequestFromSubsquare = req.headers?.referer?.startsWith?.(
+    trimEndSlash(process.env.NEXT_PUBLIC_API_END_POINT),
+  );
+  if (isRequestFromSubsquare) {
+    console.error(
+      "ERROR: Unexpected disabled api request from subsquare frontend, please check!",
+    );
+    return false;
+  }
+
+  return true;
 }
