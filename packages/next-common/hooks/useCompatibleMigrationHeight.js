264294ce76454bb1ce94a4048402004f742a56fe
wolyshaw <wolyshaw@users.noreply.github.com>
2025-05-23 17:10:42 +0800

    use api #6046

--- packages/next-common/hooks/useCompatibleMigrationHeight.js
+++ packages/next-common/hooks/useCompatibleMigrationHeight.js
@@ -1,19 +1,33 @@
+import { useEffect } from "react";
+import { createGlobalState } from "react-use";
 import { useReferendumVotingFinishIndexer } from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
-import useSubStorage from "./common/useSubStorage";
 import { isContentMigrated } from "next-common/context/migration/conditionalApi";
 import useChainOrScanHeight from "./height";
+import { useContextApi } from "next-common/context/api";
+
+const useCompatibleMigrationHeightState = createGlobalState();
 
 export function useCompatibleMigrationHeight() {
+  const [compatibleMigrationHeight, setCompatibleMigrationHeight] =
+    useCompatibleMigrationHeightState();
+  const api = useContextApi();
   const height = useChainOrScanHeight();
   const indexer = useReferendumVotingFinishIndexer();
   const isMigrated = isContentMigrated(indexer);
-  const { result: relayChainHeight } = useSubStorage(
-    "parachainSystem",
-    "lastRelayChainBlockNumber",
-  );
+
+  useEffect(() => {
+    if (!api || !isMigrated) {
+      return;
+    }
+    api?.query?.parachainSystem
+      ?.lastRelayChainBlockNumber()
+      .then((relayParent) => {
+        setCompatibleMigrationHeight(relayParent.toNumber());
+      });
+  }, [api, isMigrated, height, setCompatibleMigrationHeight]);
 
   if (isMigrated) {
-    return relayChainHeight;
+    return compatibleMigrationHeight;
   }
 
   return height;
