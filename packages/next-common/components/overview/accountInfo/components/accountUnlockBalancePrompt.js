ce32a722ea84f746c30502aff4bb2603f4b5b16f
quinn-gao <quinnn.gao@gmail.com>
2025-05-23 13:56:16 +0800

    fix: unlock tove balance, #6010

--- packages/next-common/components/overview/accountInfo/components/accountUnlockBalancePrompt.js
+++ packages/next-common/components/overview/accountInfo/components/accountUnlockBalancePrompt.js
@@ -1,19 +1,28 @@
 import useVoteBalance from "next-common/hooks/account/useVoteBalance";
 import { toPrecision } from "next-common/utils";
 import { useChainSettings } from "next-common/context/chain";
-import Prompt from "./prompt";
 import Link from "next/link";
 import useFetchMyReferendaVoting from "next-common/components/myvotes/referenda/useFetchMyReferendaVoting";
+import { createGlobalState } from "react-use";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import { PromptTypes, colorStyle } from "next-common/components/scrollPrompt";
+import { SystemClose } from "@osn/icons/subsquare";
 
+const useMode = createGlobalState(true);
 export default function AccountUnlockBalancePrompt() {
+  const [visible, setVisible] = useMode(true);
   useFetchMyReferendaVoting();
   const { unlockBalance } = useVoteBalance();
   const { symbol, decimals } = useChainSettings();
   return (
-    unlockBalance !== "0" && (
-      <Prompt>
+    unlockBalance !== "0" &&
+    visible && (
+      <GreyPanel
+        className="text14Medium py-2.5 px-4 justify-between"
+        style={colorStyle[PromptTypes]}
+      >
         <div className="text-textSecondary">
-          You have {toPrecision(unlockBalance, decimals)} {symbol} expired votes
+          You have {toPrecision(unlockBalance, decimals)} expired {symbol}{" "}
           available to unlock, manage{" "}
           <Link
             className="underline text14Medium font-[700]"
@@ -23,7 +32,14 @@ export default function AccountUnlockBalancePrompt() {
           </Link>
           .
         </div>
-      </Prompt>
+        <SystemClose
+          className="w-5 h-5 text-textSecondary"
+          role="button"
+          onClick={() => {
+            setVisible(false);
+          }}
+        />
+      </GreyPanel>
     )
   );
 }
