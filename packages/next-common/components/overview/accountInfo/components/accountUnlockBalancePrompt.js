1d866dc3c77ee1b3a9ed7ea093d730f6743a4f60
quinn-gao <53474899+quinn-gaoo@users.noreply.github.com>
2025-05-23 17:39:30 +0800

    Notify user to unlock expired votes, #6030 (#6056)
    * fix: add expired votes tip, #6030
    
    * fix: unlock tove balance, #6010
    
    * fix: code review, #6030
    
    * fix: optimize useVoteBalance, #6030
    
    * fix: change className, #6030

---
+++ packages/next-common/components/overview/accountInfo/components/accountUnlockBalancePrompt.js
@@ -0,0 +1,41 @@
+import useVoteBalance from "next-common/hooks/account/useVoteBalance";
+import { toPrecision } from "next-common/utils";
+import { useChainSettings } from "next-common/context/chain";
+import Link from "next/link";
+import useFetchMyReferendaVoting from "next-common/components/myvotes/referenda/useFetchMyReferendaVoting";
+import { createGlobalState } from "react-use";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import { PromptTypes, colorStyle } from "next-common/components/scrollPrompt";
+import { SystemClose } from "@osn/icons/subsquare";
+
+const useMode = createGlobalState(true);
+export default function AccountUnlockBalancePrompt() {
+  const [visible, setVisible] = useMode(true);
+  useFetchMyReferendaVoting();
+  const { unlockBalance } = useVoteBalance();
+  const { symbol, decimals } = useChainSettings();
+  if (unlockBalance.isZero() || !visible) return null;
+
+  return (
+    <GreyPanel
+      className="text14Medium py-2.5 px-4 justify-between"
+      style={colorStyle[PromptTypes]}
+    >
+      <div className="text-textSecondary">
+        You have {toPrecision(unlockBalance.toString(), decimals)} expired{" "}
+        {symbol} available to unlock, manage{" "}
+        <Link className="underline text14Bold " href={"/account/votes"}>
+          here
+        </Link>
+        .
+      </div>
+      <SystemClose
+        className="w-5 h-5 text-textSecondary"
+        role="button"
+        onClick={() => {
+          setVisible(false);
+        }}
+      />
+    </GreyPanel>
+  );
+}
