916d7bfbd9dcfd0136483b91f7474cea26eae619
wolyshaw <wolyshaw@users.noreply.github.com>
2025-05-23 17:52:57 +0800

    use "relayScanHeight" #6046

--- packages/next-common/hooks/useCompatibleMigrationHeight.js
+++ packages/next-common/hooks/useCompatibleMigrationHeight.js
@@ -1,33 +1,16 @@
-import { useEffect } from "react";
-import { createGlobalState } from "react-use";
 import { useReferendumVotingFinishIndexer } from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
 import { isHistoricalContent } from "next-common/context/migration/conditionalApi";
 import useChainOrScanHeight from "./height";
-import { useContextApi } from "next-common/context/api";
-
-const useCompatibleMigrationHeightState = createGlobalState();
+import { usePageProperties } from "next-common/context/page";
 
 export function useCompatibleMigrationHeight() {
-  const [compatibleMigrationHeight, setCompatibleMigrationHeight] =
-    useCompatibleMigrationHeightState();
-  const api = useContextApi();
+  const { relayScanHeight } = usePageProperties();
   const height = useChainOrScanHeight();
   const indexer = useReferendumVotingFinishIndexer();
   const isHistorical = isHistoricalContent(indexer);
 
-  useEffect(() => {
-    if (!api || !isHistorical) {
-      return;
-    }
-    api?.query?.parachainSystem
-      ?.lastRelayChainBlockNumber()
-      .then((relayParent) => {
-        setCompatibleMigrationHeight(relayParent.toNumber());
-      });
-  }, [api, isHistorical, height, setCompatibleMigrationHeight]);
-
   if (isHistorical) {
-    return compatibleMigrationHeight;
+    return relayScanHeight;
   }
 
   return height;
