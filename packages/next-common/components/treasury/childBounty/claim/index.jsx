9ad4fde794e439d55f7d41d0cfb5c7c7361d8d5c
Yongfeng LI <wliyongfeng@gmail.com>
2025-05-26 16:45:10 +0800

    Refactor child bounty sidebar components

--- packages/next-common/components/treasury/childBounty/claim/index.jsx
+++ packages/next-common/components/treasury/childBounty/claim/index.jsx
@@ -2,11 +2,15 @@ import { usePostState } from "next-common/context/post";
 import ClaimedInfo from "./ClaimedInfo";
 import Claim from "./claim";
 
-export default function ChildBountyClaim() {
+export function ChildBountyClaimed() {
   const state = usePostState();
   if ("Claimed" === state) {
     return <ClaimedInfo />;
+  } else {
+    return null;
   }
+}
 
+export default function ChildBountyClaim() {
   return <Claim />;
 }
