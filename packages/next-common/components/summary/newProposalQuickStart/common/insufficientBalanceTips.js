a03eb62eb161102ba6386e0f035f8199ac5adf98
Yongfeng LI <wliyongfeng@gmail.com>
2025-05-26 23:34:59 +0800

    Refactor, #6010

--- packages/next-common/components/summary/newProposalQuickStart/common/insufficientBalanceTips.js
+++ packages/next-common/components/summary/newProposalQuickStart/common/insufficientBalanceTips.js
@@ -4,11 +4,10 @@ import { SystemNo, SystemYes } from "@osn/icons/subsquare";
 import { useMemo } from "react";
 import { toPrecision } from "next-common/utils";
 import { useChainSettings } from "next-common/context/chain";
-import usePreimageDeposit, {
-  getPreimageDeposit,
+import useChainPreimageDepositSettings, {
+  usePreimageDeposit,
 } from "next-common/hooks/useChainPreimageDeposit";
 import useAccountTransferrable from "next-common/hooks/useAccountTransferrable";
-import { BN, BN_ZERO } from "@polkadot/util";
 
 function YesOrNo({ yes = true }) {
   if (yes) {
@@ -18,56 +17,66 @@ function YesOrNo({ yes = true }) {
   }
 }
 
-export default function InsufficientBalanceTips({ byteLength }) {
+function DepositItem({ text, deposit, yes }) {
+  const { decimals, symbol } = useChainSettings();
+
+  return (
+    <div className="flex items-center justify-between">
+      <div>
+        {text}&nbsp;
+        <span>
+          {toPrecision(deposit, decimals)} {symbol}
+        </span>
+      </div>
+      <YesOrNo yes={yes} />
+    </div>
+  );
+}
+
+// Guard that we only show it when we have preimage deposit chain settings
+function PreimageDepositSettingGuard({ children }) {
+  const settings = useChainPreimageDepositSettings();
+  return settings ? children : null;
+}
+
+export function InsufficientBalanceTipsInner({ byteLength }) {
   const api = useContextApi();
   const signerAccount = useSignerAccount();
-  const node = useChainSettings();
-  const preimage = usePreimageDeposit();
-
   const { transferrable } = useAccountTransferrable(
     api,
     signerAccount?.realAddress,
   );
-  const transferrableBalance = new BN(transferrable);
-
-  const preimageDeposit = useMemo(() => {
-    return preimage && byteLength
-      ? getPreimageDeposit(preimage, byteLength)
-      : BN_ZERO;
-  }, [byteLength, preimage]);
-
+  const preimageDeposit = usePreimageDeposit(byteLength);
   const submissionDeposit = useMemo(
-    () => api.consts?.referenda?.submissionDeposit || BN_ZERO,
+    () => api.consts?.referenda?.submissionDeposit?.toString() || 0,
     [api],
   );
 
-  const isPreimageDepositSufficient = preimageDeposit.lt(transferrableBalance);
-  const isSubmissionDepositSufficient = submissionDeposit
-    .add(preimageDeposit)
-    .lt(transferrableBalance);
+  const isPreimageDepositSufficient =
+    BigInt(preimageDeposit) < BigInt(transferrable);
+  const isSubmissionDepositSufficient =
+    BigInt(submissionDeposit) + BigInt(preimageDeposit) < BigInt(transferrable);
 
   return (
-    preimage && (
-      <div className="bg-neutral200 rounded-lg px-4 py-2.5 text14Medium text-textSecondary space-y-2">
-        <div className="flex items-center justify-between">
-          <div>
-            Preimage Deposit:{" "}
-            <span>
-              {toPrecision(preimageDeposit, node.decimals)} {node.symbol}
-            </span>
-          </div>
-          <YesOrNo yes={isPreimageDepositSufficient} />
-        </div>
-        <div className="flex items-center justify-between">
-          <div>
-            Submission Deposit:{" "}
-            <span>
-              {toPrecision(submissionDeposit, node.decimals)} {node.symbol}
-            </span>
-          </div>
-          <YesOrNo yes={isSubmissionDepositSufficient} />
-        </div>
-      </div>
-    )
+    <div className="bg-neutral200 rounded-lg px-4 py-2.5 text14Medium text-textSecondary space-y-2">
+      <DepositItem
+        text="Preimage Deposit:"
+        deposit={preimageDeposit}
+        yes={isPreimageDepositSufficient}
+      />
+      <DepositItem
+        text="Submission Deposit:"
+        deposit={submissionDeposit}
+        yes={isSubmissionDepositSufficient}
+      />
+    </div>
+  );
+}
+
+export default function InsufficientBalanceTips({ byteLength }) {
+  return (
+    <PreimageDepositSettingGuard>
+      <InsufficientBalanceTipsInner byteLength={byteLength} />
+    </PreimageDepositSettingGuard>
   );
 }
--- packages/next-common/hooks/useChainPreimageDeposit.js
+++ packages/next-common/hooks/useChainPreimageDeposit.js
@@ -1,7 +1,7 @@
 import { BN } from "@polkadot/util";
-import { useChainSettings } from "next-common/context/chain";
+import { useChain, useChainSettings } from "next-common/context/chain";
 
-export default function usePreimageDeposit() {
+export default function useChainPreimageDepositSettings() {
   const { preimage } = useChainSettings();
   if (preimage) {
     const { baseDeposit = 0, byteDeposit = 0 } = preimage;
@@ -13,6 +13,20 @@ export default function usePreimageDeposit() {
   return preimage;
 }
 
-export function getPreimageDeposit(preimage, length = 1) {
-  return preimage.baseDeposit.add(preimage.byteDeposit?.mul(new BN(length)));
+export function usePreimageDeposit(byteLen = 0) {
+  const chain = useChain();
+  const settings = useChainPreimageDepositSettings();
+  if (!settings) {
+    throw new Error(`No preimage deposit settings for ${chain}`);
+  }
+
+  if (BigInt(byteLen) <= 0) {
+    return 0;
+  }
+
+  const { baseDeposit, byteDeposit } = settings;
+  return (
+    BigInt(baseDeposit) +
+    BigInt(byteDeposit) * BigInt(byteLen)
+  ).toString();
 }
