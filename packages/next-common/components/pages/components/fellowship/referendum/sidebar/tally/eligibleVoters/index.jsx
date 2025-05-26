38ab5d9919c849b08bf14d956ae94b8a1478b547
leochen <leochen.sat24@gmail.com>
2025-05-26 14:44:00 +0800

    apply conditionApi to useCollectiveEligibleVoters, #6059

--- packages/next-common/components/pages/components/fellowship/referendum/sidebar/tally/eligibleVoters/index.jsx
+++ packages/next-common/components/pages/components/fellowship/referendum/sidebar/tally/eligibleVoters/index.jsx
@@ -2,19 +2,24 @@ import { memo, useState } from "react";
 import { Button } from "next-common/components/pages/components/gov2/sidebar/tally/styled";
 import dynamicPopup from "next-common/lib/dynamic/popup";
 import FellowshipVotesProvider from "next-common/context/collectives/fellowshipVotes";
+import { MigrationConditionalApiProvider } from "next-common/context/migration/conditionalApi";
+import { useReferendumVotingFinishIndexer } from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
 
 const EligibleVotersPopup = dynamicPopup(() => import("./popup"));
 
 function EligibleVoters() {
   const [showEligibleVoters, setShowEligibleVoters] = useState(false);
+  const indexer = useReferendumVotingFinishIndexer();
 
   return (
     <>
       <Button onClick={() => setShowEligibleVoters(true)}>All votes</Button>
       {showEligibleVoters && (
-        <FellowshipVotesProvider>
-          <EligibleVotersPopup onClose={() => setShowEligibleVoters(false)} />
-        </FellowshipVotesProvider>
+        <MigrationConditionalApiProvider indexer={indexer}>
+          <FellowshipVotesProvider>
+            <EligibleVotersPopup onClose={() => setShowEligibleVoters(false)} />
+          </FellowshipVotesProvider>
+        </MigrationConditionalApiProvider>
       )}
     </>
   );
