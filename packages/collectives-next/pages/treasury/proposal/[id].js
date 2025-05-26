3a2463cc03f7a075a70da0dd9fbf000530fa7cbb
qiaochao <qiaochao@icourt.cc>
2025-05-26 10:50:23 +0800

    fix: remove the redundant files and retain the required spend id, #6051

--- packages/collectives-next/pages/treasury/proposal/[id].js
+++
@@ -1,9 +0,0 @@
-import { getRedirectServerSideProps } from "next-common/services/serverSide";
-
-export default function Proposal() {
-  return "Please visit `/treasury/proposals/[id]`";
-}
-
-export const getServerSideProps = getRedirectServerSideProps(
-  (id) => `/treasury/proposals/${id}`,
-);
--- packages/collectives-next/pages/treasury/tip/[id].js
+++
@@ -1,9 +0,0 @@
-import { getRedirectServerSideProps } from "next-common/services/serverSide";
-
-export default function Tip() {
-  return "Please visit `/treasury/tips/[id]`";
-}
-
-export const getServerSideProps = getRedirectServerSideProps(
-  (id) => `/treasury/tips/${id}`,
-);
