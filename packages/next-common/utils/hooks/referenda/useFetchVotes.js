38c4e9159a54dd2c1e36132936c06f331fc91635
leochen <leochen.sat24@gmail.com>
2025-05-28 14:03:06 +0800

    kintsugi democracy referendum votes api, #6059

--- packages/next-common/utils/hooks/referenda/useFetchVotes.js
+++ packages/next-common/utils/hooks/referenda/useFetchVotes.js
@@ -3,12 +3,12 @@ import { useDispatch, useSelector } from "react-redux";
 import extractVoteInfo from "../../democracy/referendum";
 import { fetchVotes } from "next-common/store/reducers/democracy/votes";
 import { votesTriggerSelector } from "next-common/store/reducers/democracy/votes/selectors";
-import { useContextApi } from "next-common/context/api";
+import { useConditionalContextApi } from "next-common/context/migration/conditionalApi";
 
 export default function useFetchVotes(referendum) {
   const { voteFinishedHeight } = extractVoteInfo(referendum?.timeline);
   const referendumIndex = referendum?.referendumIndex;
-  const api = useContextApi();
+  const api = useConditionalContextApi();
   const voterTriggerCount = useSelector(votesTriggerSelector);
   const dispatch = useDispatch();
 
