1fe783e38dab47a23d5829e3d75f78f35f00c98e
quinn-gao <quinnn.gao@gmail.com>
2025-05-28 17:36:18 +0800

    use reset page test client report

--- packages/next/pages/reset.js
+++ packages/next/pages/reset.js
@@ -1,6 +1,28 @@
 import { withCommonProps } from "next-common/lib";
 import Reset from "next-common/components/pages/reset";
+import { useState } from "react";
 
-export default Reset;
+export default function Login() {
+  const [data, setData] = useState([1, 2, 3, 4, 5]);
+  return (
+    <>
+      <h1>test</h1>
+      {data.map((item) => (
+        <div key={item}>
+          <p>{item}</p>
+        </div>
+      ))}
+      <button
+        onClick={() => {
+          setData({});
+        }}
+      >
+        create error
+      </button>
+    </>
+  );
+}
+
+// export default Reset;
 
 export const getServerSideProps = withCommonProps();
