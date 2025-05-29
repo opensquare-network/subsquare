5e4bd7b98ab6d5d35b0bf894bac3e2436e2b5d8b
quinn-gao <quinnn.gao@gmail.com>
2025-05-29 18:17:44 +0800

    fix: update previous button style

--- packages/next-common/components/summary/newProposalButton/previousButton.js
+++ packages/next-common/components/summary/newProposalButton/previousButton.js
@@ -4,7 +4,7 @@ import { ArrowLineLeft } from "@osn/icons/subsquare";
 export default function PreviousButton({ onClick, isLoading }) {
   return (
     <Button
-      className={`gap-2 border-neutral400 hover:border-neutral500 text-textPrimary ${
+      className={`gap-2 p-2 pr-4 border-neutral400 hover:border-neutral500 text-textPrimary ${
         isLoading
           ? " cursor-not-allowed text-textDisabled border-neutral300"
           : ""
