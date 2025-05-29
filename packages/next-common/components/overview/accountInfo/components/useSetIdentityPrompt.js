3eea94fe1f8353f6e60e391a6c98678350d1ad02
wolyshaw <wolyshaw@users.noreply.github.com>
2025-05-29 16:55:52 +0800

    Add identity prompts on the account panel, #6070 (#6107)
    * add identity tips #6070
    
    * add requestJudgementPrompt #6070
    
    * fix cache key #6070
    
    * fix style and switch tab #6070
    
    * merge prompt #6070
    
    * key #6070
    
    * Improve copywriting, #6070
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

---
+++ packages/next-common/components/overview/accountInfo/components/useSetIdentityPrompt.js
@@ -0,0 +1,88 @@
+import Link from "next/link";
+import { PromptTypes } from "next-common/components/scrollPrompt";
+import { CACHE_KEY } from "next-common/utils/constants";
+import { useCookieValue } from "next-common/utils/hooks/useCookieValue";
+import { useMemo } from "react";
+import { useChainSettings } from "next-common/context/chain";
+import { useRouter } from "next/router";
+import useIdentityInfo from "next-common/hooks/useIdentityInfo";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+
+const identityPage = "/people";
+const judgementPage = "/people?tab=judgements";
+
+export default function useSetIdentityPrompt() {
+  const router = useRouter();
+  const chainSettings = useChainSettings();
+  const address = useRealAddress();
+  const { hasIdentity, identity } = useIdentityInfo(address);
+  const pathName = router.pathname;
+  const { modules } = chainSettings;
+  const supportedPeople = modules?.people;
+
+  const isPeoplePage = pathName?.startsWith(identityPage);
+  const isJudgementPage = router.asPath?.startsWith(judgementPage);
+
+  const isNotVerified = useMemo(() => {
+    return identity?.info?.status === "NOT_VERIFIED";
+  }, [identity?.info?.status]);
+
+  const cacheKey = useMemo(
+    () =>
+      isNotVerified
+        ? CACHE_KEY.requestJudgementPrompt
+        : CACHE_KEY.setIdentityPromptVisible,
+    [isNotVerified],
+  );
+
+  const [visible, setVisible] = useCookieValue(cacheKey, true);
+
+  return useMemo(() => {
+    if (!visible || !supportedPeople) {
+      return {};
+    }
+
+    let message;
+    if (hasIdentity && isNotVerified && !isJudgementPage) {
+      message = (
+        <div>
+          Your on-chain identity is not verified yet. Request judgements&nbsp;
+          <Link className="underline text14Medium" href={judgementPage}>
+            here
+          </Link>
+          .
+        </div>
+      );
+    } else if (!hasIdentity && !isPeoplePage) {
+      message = (
+        <div>
+          Set your on-chain identity&nbsp;
+          <Link className="underline text14Medium" href={identityPage}>
+            here
+          </Link>
+          .
+        </div>
+      );
+    }
+
+    if (message) {
+      return {
+        key: cacheKey,
+        message,
+        type: PromptTypes.INFO,
+        close: () => setVisible(false, { expires: 15 }),
+      };
+    }
+
+    return {};
+  }, [
+    cacheKey,
+    hasIdentity,
+    isJudgementPage,
+    isNotVerified,
+    isPeoplePage,
+    setVisible,
+    supportedPeople,
+    visible,
+  ]);
+}
