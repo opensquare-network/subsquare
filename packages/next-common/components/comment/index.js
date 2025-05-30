8da897a63a453661b3a3c08fd3b6ea90f5c135af
quinn-gao <quinnn.gao@gmail.com>
2025-05-30 14:30:25 +0800

    fix:  code semanticization, #6116

--- packages/next-common/components/comment/index.js
+++ packages/next-common/components/comment/index.js
@@ -4,46 +4,19 @@ import { TitleContainer } from "../styled/containers/titleContainer";
 import { cn } from "next-common/utils";
 import PolkassemblyCommentItem from "./polkassemblyCommentItem";
 import CommentsFilter from "./filter";
-import { Skeleton } from "../skeleton";
-import Divider from "../styled/layout/divider";
-
-const CommentSkeleton = () => {
-  const item = (
-    <>
-      <div className="py-4">
-        <div className="flex gap-2">
-          <Skeleton className="w-5 h-5 rounded-full" />
-          <div className="flex justify-between flex-1">
-            <Skeleton className="w-20 h-5 rounded-[4px]" />
-            <Skeleton className="w-20 h-5 rounded-[4px]" />
-          </div>
-        </div>
-        <div className="pl-7 pt-4 space-y-2">
-          <Skeleton className="w-full h-5 rounded-[4px]" />
-          <Skeleton className="w-full h-5 rounded-[4px]" />
-          <Skeleton className="w-20 h-5 rounded-[4px]" />
-        </div>
-      </div>
-      <div className="pl-7">
-        <Divider height={2} className="h-[2px]" />
-      </div>
-    </>
-  );
-
-  return (
-    <>
-      {item}
-      {item}
-    </>
-  );
-};
+import CommentSkeleton from "./commentSkeleton";
 
 export default function Comments({ data: commentsData, loading }) {
   const { items } = commentsData;
 
   let content;
   if (loading) {
-    content = <CommentSkeleton />;
+    content = (
+      <>
+        <CommentSkeleton />
+        <CommentSkeleton />
+      </>
+    );
   } else if (items?.length > 0) {
     content = (
       <div>
