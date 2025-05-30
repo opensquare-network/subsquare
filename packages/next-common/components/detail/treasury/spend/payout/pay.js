564082be1f794c107904b7094219571d8ac5ed64
wolyshaw <wolyshaw@users.noreply.github.com>
2025-05-30 09:36:13 +0800

    add still payout #6064

--- packages/next-common/components/detail/treasury/spend/payout/pay.js
+++ packages/next-common/components/detail/treasury/spend/payout/pay.js
@@ -4,18 +4,14 @@ import { useMemo, useState } from "react";
 import dynamicPopup from "next-common/lib/dynamic/popup";
 import useSubTreasurySpend from "next-common/hooks/treasury/spend/useSubTreasurySpend";
 import { has } from "lodash-es";
+import { cn } from "next-common/utils";
+import useChainOrScanHeight from "next-common/hooks/height";
 
 const Popup = dynamicPopup(() => import("./popup"));
 
 export default function TreasurySpendPay() {
-  const { index } = useOnchainData() || {};
-  const onchainStatus = useSubTreasurySpend(index);
   const [showPopup, setShowPopup] = useState(false);
   const state = usePostState();
-
-  const disabled = useMemo(() => {
-    return !has(onchainStatus?.status, "pending");
-  }, [onchainStatus]);
 
   if (["Paid", "Processed"].includes(state)) {
     return null;
@@ -23,14 +19,45 @@ export default function TreasurySpendPay() {
 
   return (
     <>
-      <PrimaryButton
-        className="w-full"
-        disabled={disabled}
-        onClick={() => setShowPopup(true)}
-      >
-        Payout
-      </PrimaryButton>
+      <PayoutContent setShowPopup={setShowPopup} />
       {showPopup && <Popup onClose={() => setShowPopup(false)} />}
     </>
+  );
+}
+
+function PayoutContent({ setShowPopup }) {
+  const { index, meta } = useOnchainData() || {};
+  const latestHeight = useChainOrScanHeight();
+  const onchainStatus = useSubTreasurySpend(index);
+  const { expireAt } = meta || {};
+  const disabled = useMemo(() => {
+    return !has(onchainStatus?.status, "pending");
+  }, [onchainStatus]);
+
+  if (expireAt && latestHeight >= expireAt) {
+    return (
+      <div className="text14Medium text-textSecondary">
+        This treasury spend is expired,{" "}
+        <span
+          className={cn(
+            "text-theme500 cursor-pointer font-bold",
+            disabled && "text-theme300 cursor-not-allowed",
+          )}
+          onClick={() => setShowPopup(true)}
+        >
+          still payout.
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <PrimaryButton
+      className="w-full"
+      disabled={disabled}
+      onClick={() => setShowPopup(true)}
+    >
+      Payout
+    </PrimaryButton>
   );
 }
