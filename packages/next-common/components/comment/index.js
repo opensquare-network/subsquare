36cc8f72180fd753e4994f8213938bd752aff097
quinn-gao <quinnn.gao@gmail.com>
2025-05-30 13:59:58 +0800

    fix: add skeleton loading in comment, #6116

--- packages/next-common/components/comment/index.js
+++ packages/next-common/components/comment/index.js
@@ -3,19 +3,47 @@ import NoComment from "./noComment";
 import { TitleContainer } from "../styled/containers/titleContainer";
 import { cn } from "next-common/utils";
 import PolkassemblyCommentItem from "./polkassemblyCommentItem";
-import Loading from "../loading";
 import CommentsFilter from "./filter";
+import { Skeleton } from "../skeleton";
+import Divider from "../styled/layout/divider";
+
+const CommitSkeleton = () => {
+  const item = (
+    <>
+      <div className="py-4">
+        <div className="flex gap-2">
+          <Skeleton className=" w-5 h-5 rounded-full" />
+          <div className="flex justify-between flex-1">
+            <Skeleton className=" w-20 h-5 rounded-[4px]" />
+            <Skeleton className=" w-20 h-5 rounded-[4px]" />
+          </div>
+        </div>
+        <div className="pl-7 pt-4 space-y-2">
+          <Skeleton className=" w-full h-5 rounded-[4px]" />
+          <Skeleton className=" w-full h-5 rounded-[4px]" />
+          <Skeleton className=" w-20 h-5 rounded-[4px]" />
+        </div>
+      </div>
+      <div className="pl-7">
+        <Divider height={2} className="h-[2px]" />
+      </div>
+    </>
+  );
+
+  return (
+    <>
+      {item}
+      {item}
+    </>
+  );
+};
 
 export default function Comments({ data: commentsData, loading }) {
   const { items } = commentsData;
 
   let content;
   if (loading) {
-    content = (
-      <div className="flex justify-center py-5">
-        <Loading size={14} />
-      </div>
-    );
+    content = <CommitSkeleton />;
   } else if (items?.length > 0) {
     content = (
       <div>
