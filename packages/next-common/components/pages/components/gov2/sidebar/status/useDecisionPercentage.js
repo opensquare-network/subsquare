7939404fde3fc69ce6810ebfb3b2723dbdb1854b
Yongfeng LI <wliyongfeng@gmail.com>
2025-06-03 18:42:02 +0800

    Fix decision status for approved referenda, #5994

--- packages/next-common/components/pages/components/gov2/sidebar/status/useDecisionPercentage.js
+++ packages/next-common/components/pages/components/gov2/sidebar/status/useDecisionPercentage.js
@@ -7,24 +7,28 @@ import { isNil } from "lodash-es";
 import { useOnchainData, usePostState } from "next-common/context/post";
 import { gov2State } from "next-common/utils/consts/state";
 import useChainOrScanHeight from "next-common/hooks/height";
+import { useMemo } from "react";
 
 export function useDecisionEnd() {
   const trackDecision = useDecision();
   const decidingSince = useDecidingSince();
   const confirming = useConfirming();
   const state = usePostState();
-
-  const normalCase = decidingSince + trackDecision;
   const onchain = useOnchainData();
 
-  if (onchain.rejected) {
-    return Math.max(normalCase, onchain.rejected[0]);
-  } else if (gov2State.Confirming === state) {
-    // note: referendum can still be deciding even after decision period gone, when it's still confirming.
-    return Math.max(normalCase, confirming || 0);
-  }
-
-  return normalCase;
+  return useMemo(() => {
+    const normalCase = decidingSince + trackDecision;
+    if (onchain?.rejected) {
+      return Math.max(normalCase, onchain.rejected[0]);
+    } else if (onchain?.approved) {
+      return Math.max(normalCase, onchain.approved[0]);
+    } else if (gov2State.Confirming === state) {
+      // note: referendum can still be deciding even after decision period gone, when it's still confirming.
+      return Math.max(normalCase, confirming || 0);
+    } else {
+      return normalCase;
+    }
+  }, [onchain, state, decidingSince, trackDecision]);
 }
 
 export function useDecisionBlocks() {
