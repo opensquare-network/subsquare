9850414813f26a89baa65cc8c1ea9d985678ee02
wolyshaw <wolyshaw@users.noreply.github.com>
2025-06-03 11:00:22 +0800

    fix supported check #6101

--- packages/next-common/components/referenda/trackPanel/trackDescriptionTooltip.jsx
+++ packages/next-common/components/referenda/trackPanel/trackDescriptionTooltip.jsx
@@ -2,13 +2,14 @@ import { isNil } from "lodash-es";
 import useTrackDetail from "next-common/components/summary/newProposalPopup/useTrackDetail";
 import Tooltip from "next-common/components/tooltip";
 import { usePageProperties } from "next-common/context/page";
-import { listPageCategory } from "next-common/utils/consts/business/category";
+import { supportedTrackPageCategory } from "next-common/utils/consts/business/category";
 
 export function MaybeTrackDescriptionTooltip({ trackId, children }) {
   const { listPageType } = usePageProperties();
 
-  const isSupportedTrackPage =
-    Object.values(listPageCategory).includes(listPageType);
+  const isSupportedTrackPage = Object.values(
+    supportedTrackPageCategory,
+  ).includes(listPageType);
 
   if (isSupportedTrackPage) {
     return (
