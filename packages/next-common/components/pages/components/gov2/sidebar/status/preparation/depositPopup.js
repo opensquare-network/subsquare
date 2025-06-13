9fdb7a606d225ec0ff1f154fccdd3fe678da7538
wolyshaw <wolyshaw@users.noreply.github.com>
2025-06-13 11:49:42 +0800

    balance #6176

--- packages/next-common/components/pages/components/gov2/sidebar/status/preparation/depositPopup.js
+++ packages/next-common/components/pages/components/gov2/sidebar/status/preparation/depositPopup.js
@@ -5,15 +5,22 @@ import { toPrecision } from "next-common/utils";
 import React, { useCallback } from "react";
 import PopupLabel from "next-common/components/popup/label";
 import Input from "next-common/lib/input";
-import SignerWithBalance from "next-common/components/signerPopup/signerWithBalance";
+import Signer from "next-common/components/popup/fields/signerField";
 import TxSubmissionButton from "next-common/components/common/tx/txSubmissionButton";
 import { usePopupParams } from "next-common/components/popupWithSigner/context";
 import { useContextApi } from "next-common/context/api";
 import { useReferendaPallet } from "next-common/context/referenda/pallet";
 import CurrencyInput from "next-common/components/currencyInput";
+import BalanceProvider, {
+  useBalanceContext,
+} from "next-common/context/balance";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import { colorStyle, PromptTypes } from "next-common/components/scrollPrompt";
+import BigNumber from "bignumber.js";
 
 function PopupContent() {
   const { onClose } = usePopupParams();
+  const { transferrable, loading } = useBalanceContext() || {};
   const api = useContextApi();
   const node = useChainSettings();
   const { referendumIndex, trackInfo: track } = usePostOnChainData();
@@ -25,9 +32,16 @@ function PopupContent() {
     }
   }, [api, referendumIndex, pallet]);
 
+  const isEnough = BigNumber(transferrable).gte(track.decisionDeposit);
+
   return (
     <>
-      <SignerWithBalance />
+      <Signer
+        showTransferable
+        balance={transferrable}
+        isBalanceLoading={loading}
+        balanceName="Available Balance"
+      />
       <div>
         <PopupLabel text={"Referendum ID"} />
         <Input value={referendumIndex} disabled={true} />
@@ -40,7 +54,18 @@ function PopupContent() {
           symbol={node?.symbol}
         />
       </div>
-
+      <GreyPanel
+        className="text14Medium px-4 py-[10px]"
+        style={
+          isEnough
+            ? colorStyle[PromptTypes.SUCCESS]
+            : colorStyle[PromptTypes.WARNING]
+        }
+      >
+        {isEnough
+          ? "Your free balance is enough to pay deposits."
+          : "Your free balance is not enough to pay deposits."}
+      </GreyPanel>
       <TxSubmissionButton getTxFunc={getTxFunc} onClose={onClose} />
     </>
   );
@@ -49,7 +74,9 @@ function PopupContent() {
 export default function DepositPopup(props) {
   return (
     <PopupWithSigner title="Place decision deposit" {...props}>
-      <PopupContent />
+      <BalanceProvider>
+        <PopupContent />
+      </BalanceProvider>
     </PopupWithSigner>
   );
 }
