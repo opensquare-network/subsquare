6dc4b023beff133fff7d3bd3d2d696c8ad1c7426
leochen <leochen.sat24@gmail.com>
2025-06-17 11:56:08 +0800

    Init NayAyeSwitch, #6182

--- packages/next-common/components/charts/thresholdCurve/gov2TallyPopup/index.js
+++ packages/next-common/components/charts/thresholdCurve/gov2TallyPopup/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import Popup from "../../../popup/wrapper/Popup";
 import { noop } from "lodash-es";
 import "../../globalConfig";
@@ -15,6 +15,7 @@ import Flex from "next-common/components/styled/flex";
 import HowOpenGovWorks from "next-common/components/howOpenGovWorks";
 import ConfirmationEstimation from "next-common/components/charts/thresholdCurve/gov2TallyPopup/confirmationEstimation";
 import ThresholdVotesCard from "../thresholdCards/votes";
+import NayAyeSwitch from "./nayAyeSwitch";
 
 function PopupChartContent({ isFellowship = false }) {
   if (isFellowship) {
@@ -33,9 +34,19 @@ export default function ThresholdCurvesGov2TallyPopup({
 }) {
   const approvalThreshold = useApprovalThreshold();
   const supportThreshold = useSupportThreshold();
+  const [showAyeNay, setShowAyeNay] = useState(true);
 
   return (
-    <Popup title="Threshold Curves" className="w-[960px]" onClose={closeFunc}>
+    <Popup
+      title="Threshold Curves"
+      className="w-[960px]"
+      onClose={closeFunc}
+      extra={
+        !isFellowship && (
+          <NayAyeSwitch isOn={showAyeNay} setIsOn={setShowAyeNay} />
+        )
+      }
+    >
       <PopupChartContent isFellowship={isFellowship} />
       <ThresholdCurvesGov2TallyLegend isFellowship={isFellowship} />
 
