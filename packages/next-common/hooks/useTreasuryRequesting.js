ccee8384d5b92bc62c69dc82b62ab350dc8cb815
wolyshaw <wolyshaw@users.noreply.github.com>
2025-06-18 16:32:47 +0800

    add isSupported limit #6160

--- packages/next-common/hooks/useTreasuryRequesting.js
+++ packages/next-common/hooks/useTreasuryRequesting.js
@@ -1,19 +1,29 @@
 import { fetchTreasuryRequesting } from "next-common/services/fetchTreasuryRequesting";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useFiatPriceSnapshot } from "./useFiatPrice";
 import getChainSettings from "next-common/utils/consts/settings";
 import { toPrecision } from "next-common/utils";
 import BigNumber from "bignumber.js";
 import { SYMBOL_DECIMALS } from "next-common/utils/consts/asset";
 import { CHAIN } from "next-common/utils/constants";
+import { useChainSettings } from "next-common/context/chain";
 
 export default function useTreasuryRequesting() {
   const { price, loading: priceLoading } = useFiatPriceSnapshot();
   const [requesting, setRequesting] = useState([]);
   const [confirming, setConfirming] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const { modules } = useChainSettings();
+
+  const isSupported = useMemo(() => {
+    return modules?.referenda?.displayTreasuryRequesting;
+  }, [modules]);
 
   useEffect(() => {
+    if (!isSupported) {
+      return;
+    }
+
     setLoading(true);
     fetchTreasuryRequesting()
       .then((data) => {
@@ -26,7 +36,7 @@ export default function useTreasuryRequesting() {
       .finally(() => {
         setLoading(false);
       });
-  }, []);
+  }, [isSupported]);
 
   return {
     loading: loading || priceLoading,
