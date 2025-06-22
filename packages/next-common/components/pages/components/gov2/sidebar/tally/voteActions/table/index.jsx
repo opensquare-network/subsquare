736054ef7fdf66a6c38eb06ffcd72c234843d3d4
Yongfeng LI <wliyongfeng@gmail.com>
2025-06-22 15:10:17 +0800

    Remove pagination, #4694

--- packages/next-common/components/pages/components/gov2/sidebar/tally/voteActions/table/index.jsx
+++ packages/next-common/components/pages/components/gov2/sidebar/tally/voteActions/table/index.jsx
@@ -1,46 +1,13 @@
-import { useState, useMemo, useEffect } from "react";
 import useQueryVoteActions from "../useQueryVoteActions";
 import { useOnchainData } from "next-common/context/post";
 import { desktopColumns, mobileColumns } from "./columns";
 import { MapDataList } from "next-common/components/dataList";
 import ScrollerX from "next-common/components/styled/containers/scrollerX";
-import Pagination from "next-common/components/pagination";
 import { cn } from "next-common/utils";
 
 export default function VoteActionsTable() {
   const { referendumIndex } = useOnchainData();
   const { loading, voteActions } = useQueryVoteActions(referendumIndex);
-  const [page, setPage] = useState(1);
-  const pageSize = 5;
-
-  useEffect(() => {
-    setPage(1);
-  }, [voteActions]);
-
-  const onPageChange = (e, target) => {
-    e.preventDefault();
-    setPage(target);
-  };
-
-  const pagination = useMemo(
-    () => ({
-      page,
-      pageSize,
-      total: voteActions?.length || 0,
-      onPageChange,
-    }),
-    [page, pageSize, voteActions?.length],
-  );
-
-  const sliceFrom = useMemo(
-    () => (pagination.page - 1) * pageSize,
-    [pageSize, pagination.page],
-  );
-  const sliceTo = sliceFrom + pageSize;
-
-  const pageItems = useMemo(() => {
-    return voteActions?.slice(sliceFrom, sliceTo) || [];
-  }, [voteActions, sliceFrom, sliceTo]);
 
   return (
     <>
@@ -48,7 +15,7 @@ export default function VoteActionsTable() {
         <MapDataList
           className="max-md:hidden"
           columnsDef={desktopColumns}
-          data={pageItems}
+          data={voteActions}
           loading={loading}
           noDataText="No data"
           contentClassName="!max-h-max"
@@ -60,12 +27,11 @@ export default function VoteActionsTable() {
             "[&_.datalist_.descriptions-item-value]:w-full",
           )}
           columnsDef={mobileColumns}
-          data={pageItems}
+          data={voteActions}
           loading={loading}
           noDataText="No data"
         />
       </ScrollerX>
-      {voteActions?.length > 0 && <Pagination {...pagination} />}
     </>
   );
 }
