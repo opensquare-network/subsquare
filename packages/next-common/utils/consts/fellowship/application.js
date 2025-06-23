6780feb911ec65bf1244910e3baeeb70090af5dd
Yongfeng LI <wliyongfeng@gmail.com>
2025-06-23 11:41:02 +0800

    Improve copywriting for state change confirmation, #6194

--- packages/next-common/utils/consts/fellowship/application.js
+++ packages/next-common/utils/consts/fellowship/application.js
@@ -24,16 +24,16 @@ export const terminateApplicationInfo = {
   [finalStateMap.Rejected]: {
     title: "Reject Application",
     confirmText:
-      "This will reject the application, and you will not be able to reverse this decision.",
+      "This will set this application as rejected, and the state can not be reversed.",
   },
   [finalStateMap.Invalid]: {
     title: "Invalidate Application",
     confirmText:
-      "This will invalidate the application. You will not be able to restore it.",
+      "This will set this application as invalid, and the state can not be reversed.",
   },
   [finalStateMap.Timeout]: {
     title: "Time Out Application",
     confirmText:
-      "This will time out the application. You will not be able to reopen it.",
+      "This will set this application as timedout, and the state can not be reversed.",
   },
 };
