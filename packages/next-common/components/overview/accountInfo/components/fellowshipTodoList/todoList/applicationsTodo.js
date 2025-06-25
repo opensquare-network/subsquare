56e59ce602ee1b27143aecc7d64e0beb59ddfc8e
wolyshaw <wolyshaw@users.noreply.github.com>
2025-06-25 22:49:20 +0800

    Fellowship: add candidate applications todo on account panel, #6195 (#6229)
    * add applications todo #6195
    
    * use link #6195
    
    * permission #6195
    
    * Improve copywriting, #6195
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

---
+++ packages/next-common/components/overview/accountInfo/components/fellowshipTodoList/todoList/applicationsTodo.js
@@ -0,0 +1,37 @@
+import useIsAdmin from "next-common/hooks/useIsAdmin";
+import ActionButton from "./actionButton";
+import { TodoContent, TodoTag, TodoWrapper } from "./styled";
+import { usePageProps } from "next-common/context/page";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useIsCollectivesMember } from "../context/hooks/mine";
+import pluralize from "pluralize";
+
+export default function ApplicationsTodo() {
+  const router = useRouter();
+  const { summary } = usePageProps();
+  const isCollectivesMember = useIsCollectivesMember();
+  const { active = 0 } = summary?.fellowshipApplications || {};
+  const isAdmin = useIsAdmin();
+  const ApplicationsPage = "/fellowship/applications";
+  const hasPermission = isAdmin || isCollectivesMember;
+
+  if (!hasPermission || active <= 0) {
+    return null;
+  }
+
+  return (
+    <TodoWrapper>
+      <TodoTag>Membership</TodoTag>
+      <TodoContent>
+        <Link className="text-theme500 cursor-pointer" href={ApplicationsPage}>
+          {active} {pluralize("application", active)}
+        </Link>
+        &nbsp;to join fellowship need your review.&nbsp;
+        <ActionButton onClick={() => router.push(ApplicationsPage)}>
+          Check All
+        </ActionButton>
+      </TodoContent>
+    </TodoWrapper>
+  );
+}
