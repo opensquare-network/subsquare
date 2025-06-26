db0dfa442182dc818765a154157c7789d32f6772
wolyshaw <wolyshaw@users.noreply.github.com>
2025-06-26 11:36:28 +0800

    use tw #6234

--- packages/next-common/components/styled/linkButton.js
+++ packages/next-common/components/styled/linkButton.js
@@ -1,16 +1,11 @@
 import Link from "next/link";
-import { cn } from "next-common/utils";
+import tw from "tailwind-styled-components";
 
-export default function LinkButton({ children, className, ...props }) {
-  return (
-    <Link
-      className={cn(
-        "text-textPrimary text12Medium border border-neutral400 rounded-[6px] px-[11px] py-[5px] h-7 hover:border-neutral500",
-        className,
-      )}
-      {...props}
-    >
-      {children}
-    </Link>
-  );
-}
+const LinkButtonStyled = tw(Link)`
+  text-textPrimary text12Medium
+  border border-neutral400
+  rounded-[6px] px-[11px] py-[5px] h-7
+  hover:border-neutral500
+`;
+
+export default LinkButtonStyled;
