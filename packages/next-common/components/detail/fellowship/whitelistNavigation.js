b33fca6701e43b6c0ecfe1e02201038c43783488
Chaojun Huang <chaojun@opensquare.network>
2025-06-30 14:22:50 +0800

    Add whitelist navigation to collectives, #6143 (#6244)
    * Add whitelist navigation to collectives, #6143
    
    * fix, #6143
    
    * Improve copywriting, #6143
    
    * fix: link reload when scan height change, #6143
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

--- packages/next-common/components/detail/fellowship/whitelistNavigation.js
+++ packages/next-common/components/detail/fellowship/whitelistNavigation.js
@@ -2,6 +2,11 @@ import React from "react";
 import { useOnchainData } from "../../../context/post";
 import { NavigationWrapper } from "../navigation/navigators";
 import Link from "next/link";
+import { useAsync } from "react-use";
+import Loading from "next-common/components/loading";
+import { useChain } from "next-common/context/chain";
+import Chains from "next-common/utils/consts/chains";
+import useObjectMemo from "next-common/hooks/useObjectMemo";
 
 export function ReferendumLink({ referendumIndex }) {
   return (
@@ -19,20 +24,100 @@ function OpenGovReferendum({ referendumIndex }) {
   );
 }
 
-export default function FellowshipWhitelistNavigation() {
+function PolkadotOpenGovReferendum({ referendumIndex }) {
+  return (
+    <div>
+      <Link href={`https://polkadot.subsquare.io/referenda/${referendumIndex}`}>
+        {`Referenda #${referendumIndex}`}
+      </Link>
+    </div>
+  );
+}
+
+function FellowshipWhitelistBar() {
   const onchainData = useOnchainData();
   const { openGovReferenda = [] } = onchainData;
 
-  if (openGovReferenda.length <= 0) {
+  if (!openGovReferenda || openGovReferenda.length === 0) {
     return null;
   }
 
   return (
     <NavigationWrapper>
-      Executed by &nbsp;
+      Execution &nbsp;
       {openGovReferenda.map((openGovIndex) => (
         <OpenGovReferendum key={openGovIndex} referendumIndex={openGovIndex} />
       ))}
     </NavigationWrapper>
   );
+}
+
+function LoadingNavigationWrapper() {
+  return (
+    <NavigationWrapper>
+      <Loading size={20} />
+    </NavigationWrapper>
+  );
+}
+
+function useWhitelistLinkedReferenda(whitelistedHashesByXcm) {
+  const whitelistedHashesByXcmDep = useObjectMemo(whitelistedHashesByXcm);
+
+  return useAsync(async () => {
+    if (!whitelistedHashesByXcmDep || whitelistedHashesByXcmDep.length === 0) {
+      return [];
+    }
+    const queryParams = (whitelistedHashesByXcmDep || [])
+      .map((hash) => `hash=${hash}`)
+      .join("&");
+    const url = `https://polkadot-api.subsquare.io/gov2/referenda/whitelist-related?${queryParams}`;
+
+    const responses = await fetch(url);
+    if (!responses.ok) {
+      throw new Error("Failed to fetch whitelist-related referenda");
+    }
+    return responses.json();
+  }, [whitelistedHashesByXcmDep]);
+}
+
+function FellowshipWhitelistBarByXcm() {
+  const onchainData = useOnchainData();
+  const { whitelistedHashesByXcm } = onchainData;
+  const { value: referenda, loading } = useWhitelistLinkedReferenda(
+    whitelistedHashesByXcm,
+  );
+
+  if (!whitelistedHashesByXcm || whitelistedHashesByXcm.length === 0) {
+    return [];
+  }
+
+  if (loading && !referenda) {
+    return <LoadingNavigationWrapper />;
+  }
+
+  if (!referenda || referenda.length === 0) {
+    return null;
+  }
+
+  return (
+    <NavigationWrapper>
+      Execution &nbsp;
+      {referenda.map((referendum) => (
+        <PolkadotOpenGovReferendum
+          key={referendum.referendumIndex}
+          referendumIndex={referendum.referendumIndex}
+        />
+      ))}
+    </NavigationWrapper>
+  );
+}
+
+export default function FellowshipWhitelistNavigation() {
+  const chain = useChain();
+
+  if (chain === Chains.collectives) {
+    return <FellowshipWhitelistBarByXcm />;
+  }
+
+  return <FellowshipWhitelistBar />;
 }
