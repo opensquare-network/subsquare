5ee944e4995133d6284481ca73763125d119a00d
quinn-gao <quinnn.gao@gmail.com>
2025-06-30 11:55:39 +0800

    fix: add application state tooltip, #6217

--- packages/next-common/components/tags/state/fellowshipApplication.js
+++ packages/next-common/components/tags/state/fellowshipApplication.js
@@ -4,6 +4,7 @@ import {
   finalStateMap,
   formattedViewMap,
 } from "next-common/utils/consts/fellowship/application";
+import Tooltip from "next-common/components/tooltip";
 
 const fellowshipApplicationTagMap = {
   [finalStateMap.New]: ActiveTag,
@@ -13,11 +14,30 @@ const fellowshipApplicationTagMap = {
   [finalStateMap.Timeout]: ClosedTag,
 };
 
+const tooltipsTextMap = {
+  [finalStateMap.Inducted]: "The applicant has been inducted as a candidate",
+  [finalStateMap.Rejected]: "The application was rejected",
+  [finalStateMap.Invalid]:
+    "The proposal content format is not valid or miss necessary info",
+  [finalStateMap.Timeout]:
+    "The applicant failed to get inducted winthin 30 days after application",
+};
+
 export default function FellowshipApplicationTag({ state }) {
   const Tag = fellowshipApplicationTagMap[state] || ClosedTag;
 
   if (!state || !formattedViewMap[state]) {
     return null;
+  }
+
+  const content = tooltipsTextMap[state];
+
+  if (content) {
+    return (
+      <Tooltip content={content}>
+        <Tag className="cursor-pointer">{formattedViewMap[state]}</Tag>
+      </Tooltip>
+    );
   }
 
   return <Tag>{formattedViewMap[state]}</Tag>;
