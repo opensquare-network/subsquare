36457fad527cb19e4bcfc14162d2aec8771aa660
leochen <leochen.sat24@gmail.com>
2025-07-08 18:35:00 +0800

    useCanAppendReferendaPost, #6280

--- packages/next-common/hooks/useCanAppendReferendaPost.js
+++ packages/next-common/hooks/useCanAppendReferendaPost.js
@@ -4,13 +4,19 @@ import { usePost } from "next-common/context/post";
 
 export default function useCanAppendReferendaPost() {
   const address = useRealAddress();
-  const { authors } = usePost();
+  const { authors, refToPost } = usePost();
+  const postType = refToPost?.postType;
 
   return useMemo(() => {
-    if (!address || !authors || authors?.length === 0) {
+    if (
+      !address ||
+      !authors ||
+      authors?.length === 0 ||
+      postType !== "referendaReferendum"
+    ) {
       return false;
     }
 
     return authors?.includes(address);
-  }, [address, authors]);
+  }, [address, authors, postType]);
 }
