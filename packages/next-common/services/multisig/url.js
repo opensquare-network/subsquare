6d140786027263f4ad9b6df21fe34a3a73757051
leochen <leochen.sat24@gmail.com>
2025-07-09 17:45:34 +0800

    Add AssetHub migration multisig API setting (#6233)
    * Add westend assethub migrated multisigApiPrefix
    
    * Westend multisig settings

--- packages/next-common/services/multisig/url.js
+++ packages/next-common/services/multisig/url.js
@@ -1,5 +1,6 @@
 import getChainSettings from "next-common/utils/consts/settings";
 import Chains from "next-common/utils/consts/chains";
+import { isAssetHubMigrated } from "next-common/utils/consts/isAssetHubMigrated";
 
 export default function getMultisigApiUrl(chain) {
   const settings = getChainSettings(chain);
@@ -13,6 +14,10 @@ export default function getMultisigApiUrl(chain) {
   }
   if (!settings?.multisigApiPrefix) {
     throw new Error(`Can not find multisig settings for ${chain}`);
+  }
+
+  if (isAssetHubMigrated(chain)) {
+    return `https://${settings.multisigApiPrefix}.statescan.io/graphql`;
   }
 
   return `https://${settings.multisigApiPrefix}-multisig-api.statescan.io/graphql`;
