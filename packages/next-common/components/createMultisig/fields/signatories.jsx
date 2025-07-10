814763dda49be0e1f0b9a8f11f14d392d4af7885
wolyshaw <wolyshaw@users.noreply.github.com>
2025-07-10 18:26:46 +0800

    add unique address #4507

--- packages/next-common/components/createMultisig/fields/signatories.jsx
+++ packages/next-common/components/createMultisig/fields/signatories.jsx
@@ -6,11 +6,18 @@ import { useExtensionAccounts } from "next-common/components/popupWithSigner/con
 import { SystemPlus, SystemTrash } from "@osn/icons/subsquare";
 import Button from "next-common/lib/button";
 import { useSignatories } from "../context/signatories";
+import { useMemo } from "react";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import { colorStyle, PromptTypes } from "next-common/components/scrollPrompt";
+import { uniq } from "lodash-es";
 
 export default function SignatoriesField() {
-  const extensionAccounts = useExtensionAccounts();
-  const { signatories, setSignatory, addSignatory, removeSignatory } =
-    useSignatories();
+  const { signatories, addSignatory } = useSignatories();
+  const showAccountError = useMemo(
+    () => uniq(signatories).length !== signatories.length,
+    [signatories],
+  );
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -18,25 +25,21 @@ export default function SignatoriesField() {
       <IndentPanel className="flex flex-col gap-2">
         <MaybeProxySigner noSwitch={true} />
         {signatories.map((address, index) => (
-          <AddressCombo
+          <SignatoriesItem
             key={`signatory-${index}`}
             address={address}
-            accounts={extensionAccounts}
-            setAddress={(add) => setSignatory(add, index)}
-            rightContent={
-              <span
-                className="cursor-pointer"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  removeSignatory(index);
-                }}
-              >
-                <SystemTrash className="text-red500" size={16} />
-              </span>
-            }
+            index={index}
           />
         ))}
       </IndentPanel>
+      {showAccountError && (
+        <GreyPanel
+          style={colorStyle[PromptTypes.ERROR]}
+          className="text14Medium px-4 py-2.5"
+        >
+          The signatories must be unique.
+        </GreyPanel>
+      )}
       <div className="flex justify-end">
         <Button
           onClick={addSignatory}
@@ -48,5 +51,45 @@ export default function SignatoriesField() {
         </Button>
       </div>
     </div>
+  );
+}
+
+function SignatoriesItem({ address, index }) {
+  const realAddress = useRealAddress();
+  const extensionAccounts = useExtensionAccounts();
+  const { removeSignatory, signatories, setSignatory } = useSignatories();
+
+  const allowedAccounts = useMemo(
+    () => extensionAccounts.filter((item) => item.address !== realAddress),
+    [extensionAccounts, realAddress],
+  );
+
+  const accountOptions = useMemo(
+    () =>
+      allowedAccounts.filter(
+        (item) =>
+          item.address === address || !signatories.includes(item.address),
+      ),
+    [allowedAccounts, address, signatories],
+  );
+
+  return (
+    <AddressCombo
+      key={`signatory-${index}`}
+      address={address}
+      accounts={accountOptions}
+      setAddress={(add) => setSignatory(add, index)}
+      rightContent={
+        <span
+          className="cursor-pointer"
+          onClick={(e) => {
+            e.stopPropagation();
+            removeSignatory(index);
+          }}
+        >
+          <SystemTrash className="text-red500" size={16} />
+        </span>
+      }
+    />
   );
 }
