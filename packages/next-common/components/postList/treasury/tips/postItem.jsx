5bca88701d4f2b6ce853ad8a1d3785ab2a4e9043
quinn-gao <quinnn.gao@gmail.com>
2025-07-10 16:43:11 +0800

    refactor: Treasury Tips PostList component, #6297

---
+++ packages/next-common/components/postList/treasury/tips/postItem.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Wrapper,
+  Footer,
+  FooterWrapper,
+  HeadWrapper,
+  ContentWrapper,
+} from "next-common/components/postList/styled";
+import {
+  PostItemBannner,
+  PostItemTitle,
+  PostItemUser,
+  PostItemTime,
+  PostItemCommentCount,
+  PostItemMalicious,
+  PostItemAISummary,
+  PostItemTitleValue,
+} from "next-common/components/postList/common";
+import Divider from "next-common/components/styled/layout/divider";
+import { TipTag } from "next-common/components/tags/state/treasury";
+import TreasurySpendsCountDown from "next-common/components/postList/treasury/spends/countdown";
+
+export default function PostItem({ data }) {
+  return (
+    <>
+      <Wrapper>
+        <ContentWrapper>
+          <HeadWrapper>
+            <PostItemTitle data={data} href={data?.detailLink} />
+            <PostItemTitleValue data={data} />
+          </HeadWrapper>
+          <Divider margin={12} />
+          <FooterWrapper>
+            <Footer>
+              <PostItemUser data={data} />
+              <PostItemTime
+                data={data}
+                elapseIcon={<TreasurySpendsCountDown data={data} />}
+              />
+              <PostItemCommentCount data={data} />
+              <PostItemMalicious isMalicious={data?.isMalicious} />
+              <PostItemAISummary data={data} />
+            </Footer>
+            <TipTag state={data.status} />
+          </FooterWrapper>
+        </ContentWrapper>
+        <PostItemBannner bannerCid={data?.bannerCid} />
+      </Wrapper>
+    </>
+  );
+}
