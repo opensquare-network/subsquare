3d2fca01d806a310e78db2481f49b8f78ca08c1d
wolyshaw <wolyshaw@users.noreply.github.com>
2025-07-11 10:44:18 +0800

    address user #6292

--- packages/next-common/components/user/addressUser.js
+++ packages/next-common/components/user/addressUser.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React, { memo, useMemo } from "react";
 import { UnStyledIdentity } from "../Identity";
 import Link from "next/link";
 import { AvatarWrapper, UserWrapper } from "./styled";
@@ -34,38 +34,53 @@ export function AddressUserImpl({
   const chain = useChain();
   const displayAddress = tryConvertToEvmAddress(address);
 
-  const userIdentity = hasIdentity ? (
-    <UnStyledIdentity
-      identity={identity}
-      maxWidth={maxWidth}
-      ellipsis={ellipsis}
-      identityIconClassName={identityIconClassName}
-    />
-  ) : (
-    <AddressDisplay
-      address={displayAddress}
-      maxWidth={maxWidth}
-      noTooltip={noTooltip}
-      ellipsis={ellipsis}
-    />
+  const userIdentity = useMemo(
+    () =>
+      hasIdentity ? (
+        <UnStyledIdentity
+          identity={identity}
+          maxWidth={maxWidth}
+          ellipsis={ellipsis}
+          identityIconClassName={identityIconClassName}
+        />
+      ) : (
+        <AddressDisplay
+          address={displayAddress}
+          maxWidth={maxWidth}
+          noTooltip={noTooltip}
+          ellipsis={ellipsis}
+        />
+      ),
+    [
+      hasIdentity,
+      identity,
+      maxWidth,
+      ellipsis,
+      identityIconClassName,
+      displayAddress,
+      noTooltip,
+    ],
   );
 
-  let userIdentityLink;
-  if (!needHref) {
-    userIdentityLink = userIdentity;
-  } else if (isExternalLink(link)) {
-    userIdentityLink = (
-      <ExternalLink externalIcon={false} href={link}>
-        {userIdentity}
-      </ExternalLink>
-    );
-  } else {
+  const userIdentityLink = useMemo(() => {
+    if (!needHref) {
+      return userIdentity;
+    }
+
+    if (isExternalLink(link)) {
+      return (
+        <ExternalLink externalIcon={false} href={link}>
+          {userIdentity}
+        </ExternalLink>
+      );
+    }
+
     let href = `/user/${displayAddress}${link}`;
     if (isAssetHubChain(chain)) {
       href = `/assethub${href}`;
     }
 
-    userIdentityLink = (
+    return (
       <Link
         href={href}
         onClick={(e) => {
@@ -75,7 +90,7 @@ export function AddressUserImpl({
         {userIdentity}
       </Link>
     );
-  }
+  }, [needHref, userIdentity, link, chain, displayAddress]);
 
   return (
     <UserWrapper noEvent={noEvent} className={className}>
