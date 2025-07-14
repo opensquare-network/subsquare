4c8533fde7346bb0484b1a53004588cbbfddacc9
quinn-gao <quinnn.gao@gmail.com>
2025-07-14 17:44:48 +0800

    refactor:  bounties PostList  renaming TreasuryBountiesPostList, add TreasuryBountiesPostList to user pofile posted list, #6297

--- packages/next-common/components/postList/treasyrybountiesPostList/index.jsx
+++ packages/next-common/components/postList/treasyrybountiesPostList/index.jsx
@@ -1,22 +1,23 @@
 import Pagination from "next-common/components/pagination/index.js";
-import ListTitleBar from "../../../listTitleBar";
+import ListTitleBar from "../../listTitleBar";
 import MaybeEmpty from "next-common/components/emptyList";
 import businessCategory from "next-common/utils/consts/business/category";
-import { ListWrapper } from "../../styled";
+import { ListWrapper } from "../styled";
 import PostItem from "./postItem";
-import NewBountyButton from "next-common/components/treasury/bounty/newBountyButton";
 
 export default function TreasuryBountiesPostList({
   titleCount = null,
   items,
   pagination,
+  titleExtra,
 }) {
   return (
     <ListWrapper>
       <ListTitleBar
         title="List"
         titleCount={titleCount}
-        titleExtra={<NewBountyButton />}
+        titleExtra={titleExtra}
+        link="/treasury/bounties"
       />
       <MaybeEmpty items={items} type={businessCategory.treasuryBounties}>
         {items.map((data, index) => (
