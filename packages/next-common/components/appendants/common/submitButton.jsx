4bc8013d1870256fb312da7b64069abeb2ff7dac
leochen <leochen.sat24@gmail.com>
2025-07-15 15:51:41 +0800

    Remove disabled from button, #6315

--- packages/next-common/components/appendants/common/submitButton.jsx
+++ packages/next-common/components/appendants/common/submitButton.jsx
@@ -1,26 +1,24 @@
 import PrimaryButton from "next-common/lib/button/primary";
 import Tooltip from "next-common/components/tooltip";
 
-export function CreateButton({ onClick, loading, disabled, content }) {
+export function CreateButton({ onClick, loading, content }) {
   const isEmpty = content === "" || content === "<p><br></p>";
-  const isDisabled = disabled || isEmpty;
 
   return (
     <Tooltip content={isEmpty ? "Cannot submit empty content" : ""}>
-      <PrimaryButton loading={loading} onClick={onClick} disabled={isDisabled}>
+      <PrimaryButton loading={loading} onClick={onClick} disabled={isEmpty}>
         Submit
       </PrimaryButton>
     </Tooltip>
   );
 }
 
-export function UpdateButton({ onClick, loading, disabled, content }) {
+export function UpdateButton({ onClick, loading, content }) {
   const isEmpty = content === "" || content === "<p><br></p>";
-  const isDisabled = disabled || isEmpty;
 
   return (
     <Tooltip content={isEmpty ? "Cannot submit empty content" : ""}>
-      <PrimaryButton loading={loading} onClick={onClick} disabled={isDisabled}>
+      <PrimaryButton loading={loading} onClick={onClick} disabled={isEmpty}>
         Update
       </PrimaryButton>
     </Tooltip>
