a0d92b40d0a7120ce1b87a7ec61769f7514bf578
leochen <leochen.sat24@gmail.com>
2025-07-15 16:47:42 +0800

    Common SubmitButton, #6315

--- packages/next-common/components/appendants/common/submitButton.jsx
+++ packages/next-common/components/appendants/common/submitButton.jsx
@@ -1,26 +1,36 @@
 import PrimaryButton from "next-common/lib/button/primary";
 import Tooltip from "next-common/components/tooltip";
 
-export function CreateButton({ onClick, loading, content }) {
+function SubmitButton({ onClick, loading, content, buttonText }) {
   const isEmpty = content === "" || content === "<p><br></p>";
 
   return (
     <Tooltip content={isEmpty ? "Cannot submit empty content" : ""}>
       <PrimaryButton loading={loading} onClick={onClick} disabled={isEmpty}>
-        Submit
+        {buttonText}
       </PrimaryButton>
     </Tooltip>
   );
 }
 
-export function UpdateButton({ onClick, loading, content }) {
-  const isEmpty = content === "" || content === "<p><br></p>";
-
+export function CreateButton({ onClick, loading, content }) {
   return (
-    <Tooltip content={isEmpty ? "Cannot submit empty content" : ""}>
-      <PrimaryButton loading={loading} onClick={onClick} disabled={isEmpty}>
-        Update
-      </PrimaryButton>
-    </Tooltip>
+    <SubmitButton
+      buttonText="Create"
+      onClick={onClick}
+      loading={loading}
+      content={content}
+    />
+  );
+}
+
+export function UpdateButton({ onClick, loading, content }) {
+  return (
+    <SubmitButton
+      buttonText="Update"
+      onClick={onClick}
+      loading={loading}
+      content={content}
+    />
   );
 }
