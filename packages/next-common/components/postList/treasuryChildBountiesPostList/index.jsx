eb65f9477ce3c01254876533caa150e618b805e0
quinn-gao <quinnn.gao@gmail.com>
2025-07-15 14:29:59 +0800

    refactor: child bounties PostList renaming TreasuryChildBountiesPostList, #6297

--- packages/next-common/components/postList/treasuryChildBountiesPostList/index.jsx
+++ packages/next-common/components/postList/treasuryChildBountiesPostList/index.jsx
@@ -1,14 +1,22 @@
 import Pagination from "next-common/components/pagination/index.js";
-import ListTitleBar from "../../../listTitleBar";
+import ListTitleBar from "../../listTitleBar";
 import MaybeEmpty from "next-common/components/emptyList";
 import businessCategory from "next-common/utils/consts/business/category";
-import { ListWrapper } from "../../styled";
+import { ListWrapper } from "../styled";
 import PostItem from "./postItem";
 
-export default function PostList({ titleCount = null, items, pagination }) {
+export default function TreasuryChildBountiesPostList({
+  titleCount = null,
+  items,
+  pagination,
+}) {
   return (
     <ListWrapper>
-      <ListTitleBar title="List" titleCount={titleCount} />
+      <ListTitleBar
+        title="List"
+        titleCount={titleCount}
+        link="/treasury/child-bounties"
+      />
       <MaybeEmpty items={items} type={businessCategory.treasuryChildBounties}>
         {items.map((data, index) => (
           <PostItem key={index} data={data} />
