fc4f3414d040cc16dac30ba22601c6fe98c813d5
quinn-gao <quinnn.gao@gmail.com>
2025-07-15 18:10:49 +0800

    refactor: Improve naming of refrenda post list, #6263

--- packages/next-common/components/postList/referendaPostList/index.jsx
+++ packages/next-common/components/postList/referendaPostList/index.jsx
@@ -1,37 +1,26 @@
 import React from "react";
 import { ListWrapper } from "next-common/components/postList/styled";
 import MaybeEmpty from "next-common/components/emptyList";
-import ReferendaListFilter from "next-common/components/referenda/list/filter";
 import ListTitleBar from "next-common/components/listTitleBar";
 import Pagination from "next-common/components/pagination/index.js";
 import businessCategory from "next-common/utils/consts/business/category";
-import NewProposalButton from "next-common/components/summary/newProposalButton";
 import PostItem from "./postItem";
 
-function ListTitle({ isUnVotedOnlyLoading, total }) {
-  return (
-    <ListTitleBar
-      title="List"
-      titleCount={isUnVotedOnlyLoading ? "Filtering un-voted..." : total}
-      titleExtra={
-        <div className="flex items-center gap-x-2">
-          <ReferendaListFilter isUnVotedOnlyLoading={isUnVotedOnlyLoading} />
-          <NewProposalButton />
-        </div>
-      }
-    />
-  );
-}
-
-export default function PostList({
+export default function ReferendaPostList({
   items,
   pagination,
-  isUnVotedOnlyLoading,
-  total,
+  titleCount,
+  titleExtra,
+  link,
 }) {
   return (
     <ListWrapper>
-      <ListTitle isUnVotedOnlyLoading={isUnVotedOnlyLoading} total={total} />
+      <ListTitleBar
+        title="List"
+        link={link}
+        titleCount={titleCount}
+        titleExtra={titleExtra}
+      />
       <MaybeEmpty items={items} type={businessCategory.openGovReferenda}>
         {items.map((data, index) => (
           <PostItem key={index} data={data} />
