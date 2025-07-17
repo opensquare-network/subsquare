c7b311a44b0b80ac4e880084fee0af22435786ad
quinn-gao <quinnn.gao@gmail.com>
2025-07-17 15:09:53 +0800

    refactor: democracy public proposals post list, #6350

---
+++ packages/next-common/components/postList/democracyPublicProposalsPostList/postItem.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  Wrapper,
+  Footer,
+  FooterWrapper,
+  HeadWrapper,
+  ContentWrapper,
+} from "next-common/components/postList/styled";
+import {
+  PostItemBannner,
+  PostItemTitle,
+  PostItemUser,
+  PostItemTime,
+  PostItemCommentCount,
+  PostItemMalicious,
+  PostItemAISummary,
+  PostItemTitleValue,
+} from "next-common/components/postList/common";
+import Divider from "next-common/components/styled/layout/divider";
+import { DemocracyProposalTag } from "next-common/components/tags/state/democracy";
+
+export default function PostItem({ data }) {
+  return (
+    <>
+      <Wrapper>
+        <ContentWrapper>
+          <HeadWrapper>
+            <PostItemTitle data={data} href={data?.detailLink} />
+            <PostItemTitleValue data={data} />
+          </HeadWrapper>
+          <Divider margin={12} />
+          <FooterWrapper>
+            <Footer>
+              <PostItemUser data={data} />
+              <PostItemTime data={data} />
+              <PostItemCommentCount data={data} />
+              <PostItemMalicious isMalicious={data?.isMalicious} />
+              <PostItemAISummary data={data} />
+            </Footer>
+            <DemocracyProposalTag state={data.state} />
+          </FooterWrapper>
+        </ContentWrapper>
+        <PostItemBannner bannerCid={data?.bannerCid} />
+      </Wrapper>
+    </>
+  );
+}
