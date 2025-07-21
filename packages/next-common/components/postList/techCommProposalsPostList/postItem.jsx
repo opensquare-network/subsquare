64b66b35f9d7fdbb0ba3e79acc93462feb533d63
quinn-gao <quinnn.gao@gmail.com>
2025-07-21 16:21:31 +0800

    refactor: simplify PostItem component structure and remove unnecessary fragments, #6357

--- packages/next-common/components/postList/techCommProposalsPostList/postItem.jsx
+++ packages/next-common/components/postList/techCommProposalsPostList/postItem.jsx
@@ -19,40 +19,35 @@ import {
 } from "next-common/components/postList/common";
 import Divider from "next-common/components/styled/layout/divider";
 import { CollectiveTag } from "next-common/components/tags/state/collective";
-import Post from "../post";
-import businessCategory from "next-common/utils/consts/business/category";
 import MotionElapse from "next-common/components/motionElapse";
 import { getMotionStateArgs } from "next-common/utils/collective/result";
 
 export default function PostItem({ data }) {
   const args = getMotionStateArgs(data.onchainData.state);
   return (
-    <>
-      <Wrapper>
-        <ContentWrapper>
-          <HeadWrapper>
-            <PostItemTitle data={data} href={data?.detailLink} />
-            <PostItemTitleValue data={data} />
-          </HeadWrapper>
-          <Divider margin={12} />
-          <FooterWrapper>
-            <Footer>
-              <PostItemUser data={data} />
-              <PostItemDemocracyTag isDemocracy={data.isDemocracy} />
-              <PostItemTime
-                data={data}
-                elapseIcon={<MotionElapse motion={data.onchainData} />}
-              />
-              <PostItemCommentCount data={data} />
-              <PostItemMalicious isMalicious={data?.isMalicious} />
-              <PostItemAISummary data={data} />
-            </Footer>
-            <CollectiveTag state={data.status} args={args} />
-          </FooterWrapper>
-        </ContentWrapper>
-        <PostItemBanner bannerCid={data?.bannerCid} />
-      </Wrapper>
-      <Post data={data} type={businessCategory.tcProposals} href={""} />
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <HeadWrapper>
+          <PostItemTitle data={data} href={data?.detailLink} />
+          <PostItemTitleValue data={data} />
+        </HeadWrapper>
+        <Divider margin={12} />
+        <FooterWrapper>
+          <Footer>
+            <PostItemUser data={data} />
+            <PostItemDemocracyTag isDemocracy={data.isDemocracy} />
+            <PostItemTime
+              data={data}
+              elapseIcon={<MotionElapse motion={data.onchainData} />}
+            />
+            <PostItemCommentCount data={data} />
+            <PostItemMalicious isMalicious={data?.isMalicious} />
+            <PostItemAISummary data={data} />
+          </Footer>
+          <CollectiveTag state={data.status} args={args} />
+        </FooterWrapper>
+      </ContentWrapper>
+      <PostItemBanner bannerCid={data?.bannerCid} />
+    </Wrapper>
   );
 }
