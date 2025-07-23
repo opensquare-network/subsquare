2f951126f27504a07f3fb0c56a66ad6e3500a079
quinn-gao <quinnn.gao@gmail.com>
2025-07-23 10:16:19 +0800

    refactor: simplify useReferendumCurveData by use useBlockSteps and useDecisionBlocks, #6380

--- packages/next-common/utils/hooks/referenda/detail/useReferendumCurveData.js
+++ packages/next-common/utils/hooks/referenda/detail/useReferendumCurveData.js
@@ -1,28 +1,16 @@
 import { useTrack } from "next-common/context/post/gov2/track";
-import { useDecidingSince } from "next-common/context/post/gov2/referendum";
-import useReferendumVotingFinishHeight from "next-common/context/post/referenda/useReferendumVotingFinishHeight";
-import { useSelector } from "react-redux";
-import { blockTimeSelector } from "next-common/store/reducers/chainSlice";
 import { range } from "lodash-es";
 import {
   getTrackApprovalCurve,
   getTrackSupportCurve,
 } from "next-common/context/post/gov2/curve";
+import { useBlockSteps, useDecisionBlocks } from "./useReferendumBlocks";
 
 // used for curve chart on OpenGov referendum detail page.
 export default function useReferendumCurveData() {
   const track = useTrack();
-  const decidingSince = useDecidingSince();
-  const finishedHeight = useReferendumVotingFinishHeight();
-  const blockTime = useSelector(blockTimeSelector);
-
-  let decisionBlocks = track.decisionPeriod;
-  if (finishedHeight && finishedHeight > decidingSince + track.decisionPeriod) {
-    decisionBlocks = finishedHeight - decidingSince;
-  }
-
-  const oneHour = 3600 * 1000;
-  const blockStep = oneHour / blockTime; // it means the blocks between 2 dots.
+  const blockStep = useBlockSteps();
+  const decisionBlocks = useDecisionBlocks();
   const hours = decisionBlocks / blockStep;
   const labels = range(hours + 1);
 
