3cbbc1e959ca8a10b6e9285371d9b51909995037
quinn-gao <quinnn.gao@gmail.com>
2025-07-24 15:35:15 +0800

    refactor: enhance CustomXTickLabels component with improved tick label calculation and visibility handling, #6386

--- packages/next-common/components/charts/thresholdCurve/curveChartCustomXTickLabels.js
+++ packages/next-common/components/charts/thresholdCurve/curveChartCustomXTickLabels.js
@@ -1,43 +1,54 @@
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
+
+const PREPARING_THRESHOLD = 60;
 
 export default function CustomXTickLabels({
-  decisionIndex,
-  labelsLength,
-  showAyeNay,
-  chartArea,
+  decisionIndex = 0,
+  labelsLength = 0,
+  showAyeNay = false,
+  chartArea = {},
 }) {
+  const preparingRef = useRef(null);
+
+  // tick bar style
   const style = useMemo(() => {
-    const { width = 0, left = 0, right = 0 } = chartArea || {};
+    const { width = 0, left = 0, right = 0 } = chartArea;
     return {
-      paddingLeft: left + "px",
-      paddingRight: showAyeNay ? right - width : 2 + "px",
+      paddingLeft: `${left}px`,
+      paddingRight: showAyeNay ? `${Math.max(right - width, 0)}px` : "2px",
     };
   }, [chartArea, showAyeNay]);
+
+  //deciding ticks
+  const tickLabels = useMemo(() => {
+    return [
+      0,
+      Math.floor((labelsLength - decisionIndex) / 3),
+      Math.floor(((labelsLength - decisionIndex) / 3) * 2),
+      labelsLength - decisionIndex - 1,
+    ];
+  }, [labelsLength, decisionIndex]);
 
   return (
     <div className="w-full" style={style}>
       <div className="flex items-center w-full overflow-hidden text12Medium font-normal text-textSecondary">
-        {decisionIndex > 80 ? (
-          <div
-            className="flex-shrink-0"
-            style={{
-              width: (decisionIndex / labelsLength) * 100 + "%",
-            }}
-          >
-            Preparing
-          </div>
-        ) : null}
-        <div className="flex flex-wrap w-full justify-between ">
-          <div className="flex w-full justify-between">
-            <div className="">0hs</div>
-            <div className=" ">
-              {Math.floor((labelsLength - decisionIndex) / 3)}hs
-            </div>
-            <div className=" ">
-              {Math.floor(((labelsLength - decisionIndex) / 3) * 2)}hs
-            </div>
-            <div className=" ">{labelsLength - decisionIndex - 1}hs</div>
-          </div>
+        <div
+          className="flex-shrink-0 overflow-hidden"
+          ref={preparingRef}
+          style={{
+            width: `${(decisionIndex / labelsLength) * 100}%`,
+            visibility:
+              preparingRef.current?.offsetWidth < PREPARING_THRESHOLD
+                ? "hidden"
+                : "initial",
+          }}
+        >
+          Preparing
+        </div>
+        <div className="flex w-full justify-between">
+          {tickLabels.map((label, idx) => (
+            <div key={idx}>{label}hs</div>
+          ))}
         </div>
       </div>
     </div>
