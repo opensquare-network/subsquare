b0bebbbfb4a3eb55d986af349bd4f4afe68bfb7b
quinn-gao <quinnn.gao@gmail.com>
2025-07-28 14:58:03 +0800

    feat: add FellowshipThresholdCurvesGov2TallyPopup component, #6403

---
+++ packages/next-common/components/charts/thresholdCurve/gov2TallyPopup/fellowship.js
@@ -0,0 +1,53 @@
+import Popup from "../../../popup/wrapper/Popup";
+import { noop } from "lodash-es";
+import "../../globalConfig";
+import ThresholdCurvesGov2TallyLegend from "../legend/gov2TallyLegend";
+import {
+  useApprovalThreshold,
+  useSupportThreshold,
+} from "../../../../context/post/gov2/threshold";
+import ThresholdSupportCard from "../thresholdCards/support";
+import ThresholdApprovalCard from "../thresholdCards/approval";
+import FellowshipCurveChart from "next-common/components/charts/thresholdCurve/fellowshipCurveChart";
+import Flex from "next-common/components/styled/flex";
+import HowOpenGovWorks from "next-common/components/howOpenGovWorks";
+import ConfirmationEstimation from "next-common/components/charts/thresholdCurve/gov2TallyPopup/confirmationEstimation";
+
+export default function FellowshipThresholdCurvesGov2TallyPopup({
+  closeFunc = noop,
+  supportPerbill = 0,
+  supportPercentage = 0,
+  approvalPercentage = 0,
+}) {
+  const approvalThreshold = useApprovalThreshold();
+  const supportThreshold = useSupportThreshold();
+
+  return (
+    <Popup title="Threshold Curves" className="w-[960px]" onClose={closeFunc}>
+      <FellowshipCurveChart />
+      <ThresholdCurvesGov2TallyLegend showAyeNay={false} />
+
+      <Flex className="flex max-sm:flex-col grow gap-[16px]">
+        <ThresholdApprovalCard
+          approvalThreshold={approvalThreshold}
+          approvalPercentage={approvalPercentage}
+        />
+
+        <ThresholdSupportCard
+          supportThreshold={supportThreshold}
+          supportPerbill={supportPerbill}
+          supportPercentage={supportPercentage}
+        />
+      </Flex>
+
+      <ConfirmationEstimation
+        approvePercentage={approvalPercentage}
+        supportPercentage={supportPercentage}
+      />
+
+      <div className="mt-[16px]">
+        <HowOpenGovWorks anchor="referenda" />
+      </div>
+    </Popup>
+  );
+}
