19a6aba80037de807e2beed3af7ab5e48dafd357
chaojun <chaojun@opensquare.network>
2025-07-28 11:55:49 +0800

    fix: origin address proxy tag, #4507

--- packages/next-common/hooks/useOnChainProxy.js
+++ packages/next-common/hooks/useOnChainProxy.js
@@ -1,20 +1,20 @@
 import { useContextApi } from "next-common/context/api";
-import { useUser } from "next-common/context/user";
 import { isSameAddress } from "next-common/utils";
 import useCall from "next-common/utils/hooks/useCall";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useMemo } from "react";
 
 export default function useOnChainProxyInfo(address) {
   const api = useContextApi();
-  const user = useUser();
+  const realAddress = useRealAddress();
   const { value: proxyInfo } = useCall(api?.query?.proxy?.proxies, [address]);
   return useMemo(() => {
-    if (!proxyInfo || !user) {
+    if (!proxyInfo || !realAddress) {
       return null;
     }
     const [proxies] = proxyInfo;
     return (proxies || []).find((proxy) =>
-      isSameAddress(proxy.delegate.toString(), user?.address),
+      isSameAddress(proxy.delegate.toString(), realAddress),
     );
-  }, [proxyInfo, user]);
+  }, [proxyInfo, realAddress]);
 }
