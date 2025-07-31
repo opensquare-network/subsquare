99c617073d12c69731bb0154c79d917f29dd71fe
wolyshaw <wolyshaw@users.noreply.github.com>
2025-07-31 14:02:22 +0800

    Show fellowship tab on profile page for history members, #6418 (#6424)
    * history member #6418
    
    * is history #6418
    
    * improve feeds api #6418
    
    * use server data #6418
    
    * optimization parameters #6418
    
    * default page #6418

--- packages/collectives-next/pages/user/[...params].js
+++ packages/collectives-next/pages/user/[...params].js
@@ -1,10 +1,13 @@
 import { withCommonProps } from "next-common/lib";
 import { backendApi } from "next-common/services/nextApi";
 import Profile from "next-common/components/profile";
+import { defaultPageSize, EmptyList } from "next-common/utils/constants";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 import { tryConvertToSubstrateAddress } from "next-common/utils/mixedChainUtil";
 import {
+  ambassadorCoreFeedsApiUri,
   ambassadorMembersApiUri,
+  fellowshipCoreFeedsApiUri,
   fellowshipMembersApiUri,
 } from "next-common/services/url";
 
@@ -13,20 +16,31 @@ export default Profile;
 export const getServerSideProps = withCommonProps(async (context) => {
   const {
     params: [id],
+    page = 1,
   } = context.query;
 
   const maybeAddress = tryConvertToSubstrateAddress(id);
+
+  const queryFeedsParams = {
+    page: page,
+    page_size: defaultPageSize,
+    who: id,
+  };
 
   const [
     { result: userSummary },
     { result: user },
     { result: fellowshipMembers },
     { result: ambassadorMembers },
+    { result: fellowshipFeeds },
+    { result: ambassadorFeeds },
   ] = await Promise.all([
     backendApi.fetch(`users/${maybeAddress}/counts`),
     backendApi.fetch(`users/${maybeAddress}`),
     backendApi.fetch(fellowshipMembersApiUri),
     backendApi.fetch(ambassadorMembersApiUri),
+    backendApi.fetch(fellowshipCoreFeedsApiUri, queryFeedsParams),
+    backendApi.fetch(ambassadorCoreFeedsApiUri, queryFeedsParams),
   ]);
   const tracksProps = await fetchOpenGovTracksProps();
 
@@ -36,6 +50,8 @@ export const getServerSideProps = withCommonProps(async (context) => {
       userSummary: userSummary ?? {},
       fellowshipMembers: fellowshipMembers ?? null,
       ambassadorMembers: ambassadorMembers ?? null,
+      fellowshipFeeds: fellowshipFeeds ?? EmptyList,
+      ambassadorFeeds: ambassadorFeeds ?? EmptyList,
       user: user ?? {},
       route: context.query?.params?.slice(1)?.join("/") ?? "",
       ...tracksProps,
--- packages/next-common/components/profile/tabs/useProfileCollectivesTabs.js
+++ packages/next-common/components/profile/tabs/useProfileCollectivesTabs.js
@@ -3,23 +3,44 @@ import { isCollectivesChain } from "next-common/utils/chain";
 import useProfileAddress from "next-common/components/profile/useProfileAddress";
 import { TabTitle } from "./";
 import { usePageProps } from "next-common/context/page";
+import { EmptyList } from "next-common/utils/constants";
+
+function useShowFellowshipTab() {
+  const address = useProfileAddress();
+  const { fellowshipMembers = [], fellowshipFeeds = EmptyList } =
+    usePageProps();
+  const fellowshipMember = fellowshipMembers?.find(
+    (member) => member.address === address,
+  );
+  const isFellowshipHistoryMember = fellowshipFeeds?.items?.length > 0;
+
+  return fellowshipMember || isFellowshipHistoryMember;
+}
+
+function useShowAmbassadorTab() {
+  const address = useProfileAddress();
+  const { ambassadorMembers = [], ambassadorFeeds = EmptyList } =
+    usePageProps();
+  const ambassadorMember = ambassadorMembers?.find(
+    (member) => member.address === address,
+  );
+  const isAmbassadorHistoryMember = ambassadorFeeds?.items?.length > 0;
+
+  return ambassadorMember || isAmbassadorHistoryMember;
+}
 
 export function useProfileCollectivesTabs() {
   const chain = useChain();
   const address = useProfileAddress();
-  const { fellowshipMembers = [], ambassadorMembers = [] } = usePageProps();
-  const fellowshipMember = fellowshipMembers.find(
-    (member) => member.address === address,
-  );
-  const ambassadorMember = ambassadorMembers.find(
-    (member) => member.address === address,
-  );
+  const showFellowshipTab = useShowFellowshipTab();
+  const showAmbassadorTab = useShowAmbassadorTab();
+
   if (!isCollectivesChain(chain)) {
     return [];
   }
 
   const tabs = [];
-  if (fellowshipMember) {
+  if (showFellowshipTab) {
     tabs.push({
       label({ active }) {
         return <TabTitle active={active}>Fellowship</TabTitle>;
@@ -29,7 +50,7 @@ export function useProfileCollectivesTabs() {
       exactMatch: false,
     });
   }
-  if (ambassadorMember) {
+  if (showAmbassadorTab) {
     tabs.push({
       label({ active }) {
         return <TabTitle active={active}>Ambassador</TabTitle>;
