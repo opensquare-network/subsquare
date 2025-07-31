bd32d130c783029570a60b1a6c58992f77a20682
chaojun <chaojun@opensquare.network>
2025-07-31 16:08:15 +0800

    fix: multisig weight incorrect, #4507

--- packages/next-common/utils/hooks/common/useWeight.js
+++ packages/next-common/utils/hooks/common/useWeight.js
@@ -35,6 +35,7 @@ export function convertWeight(weight) {
 
 export default function useWeight(call) {
   const api = useContextApi();
+  const [isLoading, setIsLoading] = useState(true);
   const isMounted = useMountedState();
   const [state, setState] = useState(() =>
     objectSpread(
@@ -47,17 +48,14 @@ export default function useWeight(call) {
 
   useEffect(() => {
     if (!api || !call || !api?.call?.transactionPaymentApi) {
-      setState((prev) => objectSpread({}, prev, EMPTY_STATE));
       return;
     }
 
     nextTick(async () => {
       try {
-        const { v1Weight, v2Weight } = convertWeight(
-          (await api.tx(call).paymentInfo(ZERO_ACCOUNT)).weight,
-        );
-
-        isMounted() &&
+        const paymentInfo = await api.tx(call).paymentInfo(ZERO_ACCOUNT);
+        const { v1Weight, v2Weight } = convertWeight(paymentInfo.weight);
+        if (isMounted()) {
           setState((prev) =>
             objectSpread({}, prev, {
               encodedCallLength: call.encodedLength,
@@ -66,11 +64,13 @@ export default function useWeight(call) {
               weight: prev.isWeightV2 ? v2Weight : v1Weight,
             }),
           );
+          setIsLoading(false);
+        }
       } catch (error) {
         console.error(error);
       }
     });
   }, [api, call, isMounted]);
 
-  return state;
+  return { state, isLoading };
 }
