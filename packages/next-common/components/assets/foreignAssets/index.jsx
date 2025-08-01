1fca9c3f228c84bef070d6baf7bf04fc65f84e6f
leochen <leochen.sat24@gmail.com>
2025-08-01 16:21:11 +0800

    optimize ForeignAssets entry, #6274

--- packages/next-common/components/assets/foreignAssets/index.jsx
+++ packages/next-common/components/assets/foreignAssets/index.jsx
@@ -4,29 +4,26 @@ import {
   ForeignAssetsProvider,
   useForeignAssets,
 } from "next-common/context/foreignAssets";
+import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 
 function ForeignAssetsHeader() {
   const { count } = useForeignAssets();
 
   return (
-    <div className="pl-6 inline-flex items-center">
-      <span className="text16Bold text-textPrimary">Foreign Asset</span>
-      <span className="text16Medium text-textTertiary ml-1">{count}</span>
-    </div>
+    <TitleContainer className="justify-start gap-x-1">
+      Foreign Asset
+      <span className="text16Medium text-textTertiary">{count}</span>
+    </TitleContainer>
   );
 }
 
 export default function ForeignAssets() {
   return (
     <ForeignAssetsProvider>
-      <div className="mb-1">
-        <ForeignAssetsHeader />
-        <div className="mt-4">
-          <SecondaryCard>
-            <ForeignAssetsTable />
-          </SecondaryCard>
-        </div>
-      </div>
+      <ForeignAssetsHeader />
+      <SecondaryCard>
+        <ForeignAssetsTable />
+      </SecondaryCard>
     </ForeignAssetsProvider>
   );
 }
