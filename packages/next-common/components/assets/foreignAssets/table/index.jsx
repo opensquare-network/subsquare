236c7a27379f72b0dd0e9da33eb6962989826695
leochen <leochen.sat24@gmail.com>
2025-08-01 14:54:45 +0800

    use MapDataList, #6274

--- packages/next-common/components/assets/foreignAssets/table/index.jsx
+++ packages/next-common/components/assets/foreignAssets/table/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForeignAssets } from "next-common/context/foreignAssets";
 import ScrollerX from "next-common/components/styled/containers/scrollerX";
-import DataList from "next-common/components/dataList";
+import { MapDataList } from "next-common/components/dataList";
 import { foreignAssetsColumnsDef } from "./columns/index";
 import { SingleForeignAssetProvider } from "next-common/context/foreignAssets/singleForeignAsset";
 
@@ -20,18 +20,6 @@ function ForeignAssetListItem({ DataListItem, idx, rows, assets, loading }) {
 export default function ForeignAssetsTable() {
   const { assets, loading } = useForeignAssets();
 
-  const columns = foreignAssetsColumnsDef.map((col) => ({
-    name: col.name,
-    style: col.style,
-    className: col.className,
-  }));
-
-  const rows = (assets || []).map((item, index) => {
-    const row = foreignAssetsColumnsDef.map(({ render }) => render(item));
-    row.key = item.assetId || index;
-    return row;
-  });
-
   const renderItem = (DataListItem, idx, rows) => (
     <ForeignAssetListItem
       DataListItem={DataListItem}
@@ -45,9 +33,9 @@ export default function ForeignAssetsTable() {
 
   return (
     <ScrollerX>
-      <DataList
-        columns={columns}
-        rows={rows}
+      <MapDataList
+        columnsDef={foreignAssetsColumnsDef}
+        data={assets}
         loading={loading}
         renderItem={renderItem}
         noDataText="No current foreign assets"
