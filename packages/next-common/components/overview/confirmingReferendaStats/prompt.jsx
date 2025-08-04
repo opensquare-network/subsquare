bf0d96de781b823599265116860de5afa05e4132
quinn-gao <quinnn.gao@gmail.com>
2025-08-04 10:44:12 +0800

    fix: correct style bug in OpenGov warning on overview page, #6447

--- packages/next-common/components/overview/confirmingReferendaStats/prompt.jsx
+++ packages/next-common/components/overview/confirmingReferendaStats/prompt.jsx
@@ -13,22 +13,20 @@ export default function Prompt({
       className="text14Medium py-2.5 px-4 justify-between"
       style={colorStyle[PromptTypes.INFO]}
     >
-      <div className="inline-flex">
+      <div className="inline-flex flex-wrap">
         <span className="mr-2">OpenGov:</span>
-        <span>There&nbsp;{confirmingCount > 1 ? "are" : "is"}</span>
+        There<span>{confirmingCount > 1 ? "are" : "is"}</span>
         <span className="text14Bold">&nbsp;{confirmingCount}&nbsp;</span>
-        <span>
-          referenda confirming{requesting}. Check{" "}
-          {confirmingCount > 1 ? "them" : "it"}
-          &nbsp;
-        </span>
-        <Link className="underline text14Bold" href="/referenda">
+        referenda confirming{requesting}. Check{" "}
+        {confirmingCount > 1 ? "them" : "it"}
+        &nbsp;
+        <Link className="inline-block underline text14Bold" href="/referenda">
           here
         </Link>
         <span>.</span>
       </div>
       <SystemClose
-        className="w-5 h-5 text-theme500"
+        className="w-5 h-5 text-theme500 flex-shrink-0 ml-2"
         role="button"
         onClick={onClose}
       />
