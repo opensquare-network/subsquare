258a2b0da8ca2295fe21f9616cfcda7d4a4c5506
chaojun <chaojun@opensquare.network>
2025-08-06 16:48:50 +0800

    refactor

--- packages/next-common/components/multisigs/actions/composeCallPopup/proposeWithExtrinsic.jsx
+++ packages/next-common/components/multisigs/actions/composeCallPopup/proposeWithExtrinsic.jsx
@@ -1,30 +1,12 @@
 import { ExtrinsicFieldWithLoading } from "next-common/components/popup/fields/extrinsicField";
-import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
-import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 import { useCallback, useState } from "react";
 import { useContextApi } from "next-common/context/api";
 import { blake2AsHex } from "@polkadot/util-crypto";
 import TxSubmissionButton from "next-common/components/common/tx/txSubmissionButton";
+import CallHash from "./callHash";
 
 const defaultSectionName = "system";
 const defaultMethodName = "setCode";
-
-function CallHash({ callHash }) {
-  if (!callHash) {
-    return null;
-  }
-
-  return (
-    <div>
-      <TitleContainer className="text14Bold px-0 pb-2">
-        Call Hash
-      </TitleContainer>
-      <GreyPanel className="justify-start break-all gap-x-2 text14Medium text-textPrimary py-2.5 px-4 max-w-full">
-        {callHash}
-      </GreyPanel>
-    </div>
-  );
-}
 
 export default function ProposeWithExtrinsic() {
   const api = useContextApi();
