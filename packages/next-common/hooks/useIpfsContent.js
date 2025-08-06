b56692a93d8950f9c218517a76e51d26170137ba
wolyshaw <wolyshaw@users.noreply.github.com>
2025-08-06 16:37:37 +0800

    add timeout #5864

--- packages/next-common/hooks/useIpfsContent.js
+++ packages/next-common/hooks/useIpfsContent.js
@@ -1,13 +1,24 @@
 import getIpfsLink from "next-common/utils/env/ipfsEndpoint";
 import { useAsync } from "react-use";
 
+const REQUEST_TIMEOUT = 30000; // 30s
+
 export function useIpfsContent(cid) {
   return useAsync(async () => {
-    const resp = await fetch(getIpfsLink(cid));
-    if (resp.ok) {
-      return resp.text();
-    } else {
-      throw new Error(resp.statusText);
-    }
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => {
+        reject(new Error("Request timeout"));
+      }, REQUEST_TIMEOUT);
+    });
+
+    const fetchPromise = fetch(getIpfsLink(cid)).then((resp) => {
+      if (resp.ok) {
+        return resp.text();
+      } else {
+        throw new Error(resp.statusText);
+      }
+    });
+
+    return Promise.race([fetchPromise, timeoutPromise]);
   }, [cid]);
 }
