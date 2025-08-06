cc02cbfb158b6d4820fffd1198d800b4b427d3af
wolyshaw <wolyshaw@users.noreply.github.com>
2025-08-06 14:02:37 +0800

    optimized conditions #5864

--- packages/next-common/utils/fellowship/getEvidenceTitle.js
+++ packages/next-common/utils/fellowship/getEvidenceTitle.js
@@ -1,9 +1,11 @@
+import { isNil } from "lodash-es";
+
 export default function getEvidenceTitle({ wish, rank, title }) {
   if (title) {
     return title;
   }
 
-  if (wish && rank) {
+  if (!isNil(wish) && !isNil(rank)) {
     const adposition = wish === "Retention" ? "at" : "to";
     const realRankValue = wish === "Promotion" ? rank + 1 : rank;
     return `${wish} ${adposition} Rank ${realRankValue}`;
