295db16fa803686ba55d4a312aa53019d8cb72b1
wolyshaw <wolyshaw@users.noreply.github.com>
2025-08-07 10:23:45 +0800

    add state tag #5864

--- packages/next-common/components/pages/fellowship/member/evidenceRelatedReferenda.js
+++ packages/next-common/components/pages/fellowship/member/evidenceRelatedReferenda.js
@@ -7,12 +7,18 @@ import MyVote from "./fellowshipMember/myVote";
 import tw from "tailwind-styled-components";
 import { useCollectivesContext } from "next-common/context/collectives/collectives";
 import useFetch from "next-common/hooks/useFetch";
-import { gov2VotingStates } from "next-common/utils/consts/state";
+import {
+  gov2FinalState,
+  gov2State,
+  gov2VotingStates,
+} from "next-common/utils/consts/state";
 import CollectivesProvider from "next-common/context/collectives/collectives";
 import { ActiveReferendaProvider } from "next-common/context/activeReferenda";
 import CollectivesMembersProvider from "next-common/components/overview/accountInfo/components/fellowshipTodoList/context/collectivesMember";
 import { isNil } from "lodash-es";
 import { useMemo } from "react";
+import { Gov2ReferendaTag } from "next-common/components/tags/state/gov2";
+import { getGov2ReferendumStateArgs } from "next-common/utils/gov2/result";
 
 const CreateReferendumAndVoteArea = tw(CreateReferendumAndVote)`
   !w-full
@@ -93,8 +99,6 @@ function ReferendumVoteItem({ referendumIndex }) {
     `/api/${section}/referenda/${referendumIndex}`,
   );
 
-  const isVoting = gov2VotingStates.includes(detail?.state?.name);
-
   return (
     <div className="flex items-center justify-between w-full text14Medium gap-x-4 max-sm:flex-col max-sm:gap-y-3">
       <div className="flex flex-col gap-[4px] flex-1 min-w-0 w-full min-h-[40px] justify-center">
@@ -105,9 +109,25 @@ function ReferendumVoteItem({ referendumIndex }) {
         />
         <MyVote referendumIndex={referendumIndex} />
       </div>
-      {isVoting && (
-        <ReferendumVoteButtonsArea referendumIndex={referendumIndex} />
-      )}
+      <ReferendumVoteItemRight
+        state={detail?.state}
+        referendumIndex={referendumIndex}
+      />
     </div>
   );
+}
+
+function ReferendumVoteItemRight({ state, referendumIndex }) {
+  const isVoting = gov2VotingStates.includes(state?.name);
+
+  const isFinal = [...gov2FinalState, gov2State.Executed].includes(state?.name);
+
+  if (isVoting) {
+    return <ReferendumVoteButtonsArea referendumIndex={referendumIndex} />;
+  } else if (isFinal) {
+    const args = getGov2ReferendumStateArgs(state);
+    return <Gov2ReferendaTag state={state?.name} args={args} />;
+  } else {
+    return null;
+  }
 }
