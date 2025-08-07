c38889add7cfb653202f1c8a0d39b53dc870e5de
wolyshaw <wolyshaw@users.noreply.github.com>
2025-08-07 15:10:41 +0800

    fix signer popup #6442

--- packages/next-common/components/popup/fields/signerField.js
+++ packages/next-common/components/popup/fields/signerField.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { isNil } from "lodash-es";
 import { toPrecision } from "next-common/utils";
 import PopupLabelWithBalance from "next-common/components/popup/balanceLabel";
@@ -72,8 +72,6 @@ export default function Signer({
   showTransferable = false,
   supportedMultisig = true,
 }) {
-  const isBalanceUnavailable = useMemo(() => isNil(balance), [balance]);
-
   return (
     <SignerWrapper
       title={title}
@@ -84,7 +82,7 @@ export default function Signer({
       showTransferable={showTransferable}
     >
       <MaybeProxySigner
-        noSwitch={noSwitchSigner || isBalanceUnavailable}
+        noSwitch={noSwitchSigner}
         supportedMultisig={supportedMultisig}
       />
     </SignerWrapper>
