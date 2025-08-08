52c62a009183daaa646fc64fa1cd010c7e397094
wolyshaw <wolyshaw@users.noreply.github.com>
2025-08-08 09:38:14 +0800

    Sort evidences by active first, and then rank in ASC order #6471 (#6474)
    * sort by rank #6471
    
    * annotation #6471

--- packages/next-common/components/fellowship/evidences/useEvidencesSort.js
+++ packages/next-common/components/fellowship/evidences/useEvidencesSort.js
@@ -1,3 +1,4 @@
+import { isNil } from "lodash-es";
 import { isSameAddress } from "next-common/utils";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useMemo } from "react";
@@ -7,15 +8,31 @@ export default function useEvidencesSort(evidences) {
 
   return useMemo(() => {
     const newEvidencesArray = evidences || [];
-    // Move my evidences to the first, keep the order of other evidences
     newEvidencesArray.sort((a, b) => {
       if (isSameAddress(a.address, realAddress)) {
         return -1;
       } else if (isSameAddress(b.address, realAddress)) {
         return 1;
-      } else {
-        return 0;
       }
+
+      if (b.activeEvidence && !a.activeEvidence) {
+        return 1;
+      }
+      if (!b.activeEvidence && a.activeEvidence) {
+        return -1;
+      }
+
+      if (!isNil(b.rank) && !isNil(a.rank)) {
+        return a.rank - b.rank;
+      }
+      if (!isNil(b.rank)) {
+        return 1;
+      }
+      if (!isNil(a.rank)) {
+        return -1;
+      }
+
+      return a.address < b.address ? -1 : 1;
     });
     return newEvidencesArray;
   }, [evidences, realAddress]);
