2b25b307909ad06914cd9047e52daf031c9125a2
wolyshaw <wolyshaw@users.noreply.github.com>
2025-08-11 10:47:30 +0800

    add new evidence button #6468

--- packages/next-common/components/pages/fellowship/evidencesList.js
+++ packages/next-common/components/pages/fellowship/evidencesList.js
@@ -2,10 +2,15 @@ import { TitleContainer } from "next-common/components/styled/containers/titleCo
 import FellowshipEvidencesTable from "next-common/components/fellowship/evidences/table";
 import useEvidencesSort from "next-common/components/fellowship/evidences/useEvidencesSort";
 import useEvidenceFilter from "next-common/components/pages/fellowship/useEvidenceFilter";
+import SubmitEvidencePopup from "next-common/components/collectives/core/actions/more/submitEvidenceItem/popup";
 import { usePageProps } from "next-common/context/page";
+import PrimaryButton from "next-common/lib/button/primary";
+import { useState } from "react";
+import { SystemPlus } from "@osn/icons/subsquare";
 
 export default function FellowshipEvidencesList() {
   const { evidences } = usePageProps();
+  const [showSubmitEvidencePopup, setShowSubmitEvidencePopup] = useState(false);
 
   const { component: EvidenceFilterComponent, filteredEvidences } =
     useEvidenceFilter(evidences);
@@ -24,10 +29,26 @@ export default function FellowshipEvidencesList() {
             </span>
           </span>
         </TitleContainer>
-        {EvidenceFilterComponent}
+        <div className="flex items-center gap-3 max-md:pl-6">
+          {EvidenceFilterComponent}
+          <PrimaryButton
+            onClick={() => setShowSubmitEvidencePopup(true)}
+            size="small"
+            iconLeft={<SystemPlus className="w-4 h-4" />}
+          >
+            New Evidence
+          </PrimaryButton>
+        </div>
       </div>
 
       <FellowshipEvidencesTable evidences={filteredEvidences} />
+      {showSubmitEvidencePopup && (
+        <SubmitEvidencePopup
+          onClose={() => {
+            setShowSubmitEvidencePopup(false);
+          }}
+        />
+      )}
     </>
   );
 }
