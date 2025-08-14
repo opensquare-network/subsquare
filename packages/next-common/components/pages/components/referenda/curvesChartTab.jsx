0f1c0e034bfa8b7c48348216e51e9eb15d87fb8d
quinn-gao <quinnn.gao@gmail.com>
2025-08-14 16:13:05 +0800

    fix: refactor CurvesChartTab, #6502

--- packages/next-common/components/pages/components/referenda/curvesChartTab.jsx
+++ packages/next-common/components/pages/components/referenda/curvesChartTab.jsx
@@ -5,27 +5,20 @@ import { CurvePopupExpander } from "next-common/components/gov2/referendum/curve
 import ReferendaCurveChart from "next-common/components/charts/thresholdCurve/referendaCurveChart";
 
 export default function CurvesChartTab() {
+  const showVoteActions = useShowVoteActions();
   const [showVoter, setShowVoter] = useState(true);
   return (
     <>
-      <Header showVoter={showVoter} setShowVoter={setShowVoter} />
+      <div className="flex items-center justify-between">
+        <span className="text-textPrimary text14Bold">Threshold Curves</span>
+        <div className="flex items-center gap-3">
+          {showVoteActions && (
+            <AvatarSwitch value={showVoter} onChange={setShowVoter} />
+          )}
+          <CurvePopupExpander />
+        </div>
+      </div>
       <ReferendaCurveChart showVoter={showVoter} />
     </>
-  );
-}
-
-function Header({ showVoter, setShowVoter }) {
-  const showVoteActions = useShowVoteActions();
-
-  return (
-    <div className="flex items-center justify-between">
-      <span className="text-textPrimary text14Bold">Threshold Curves</span>
-      <div className="flex items-center gap-3">
-        {showVoteActions && (
-          <AvatarSwitch value={showVoter} onChange={setShowVoter} />
-        )}
-        <CurvePopupExpander />
-      </div>
-    </div>
   );
 }
