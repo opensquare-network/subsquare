8556cf74bffe67f8a85be8928aa007880b4a1510
leochen <leochen.sat24@gmail.com>
2025-08-15 10:46:59 +0800

    useProfileForeignAssets, #6500

--- packages/next-common/components/profile/foreignAssets/useProfileForeignAssets.js
+++ packages/next-common/components/profile/foreignAssets/useProfileForeignAssets.js
@@ -1,27 +1,7 @@
-import { useMemo } from "react";
-import { encodeAddress } from "@polkadot/util-crypto";
-import { useChain } from "next-common/context/chain";
-import { getChainSs58Format } from "next-common/utils/chain";
 import useForeignAssets from "next-common/hooks/foreignAssets/useForeignAssets";
 
 export default function useProfileForeignAssets(address) {
-  const chain = useChain();
-
-  const realAddress = useMemo(() => {
-    if (!address || !chain) {
-      return null;
-    }
-
-    try {
-      const ss58Format = getChainSs58Format(chain);
-      return encodeAddress(address, ss58Format);
-    } catch (error) {
-      console.error("Failed to encode address:", error);
-      return null;
-    }
-  }, [address, chain]);
-
-  const { assets, loading } = useForeignAssets(realAddress);
+  const { assets, loading } = useForeignAssets(address);
 
   return { assets, loading };
 }
