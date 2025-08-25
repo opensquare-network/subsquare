28441e42b5cb4d3fee898788b427903f54784ab8
quinn-gao <quinnn.gao@gmail.com>
2025-08-25 16:48:41 +0800

    fix: PolkassemblyUser componet style, #6555

--- packages/next-common/components/postList/polkassemblyDiscussionPostList/postItem.jsx
+++ packages/next-common/components/postList/polkassemblyDiscussionPostList/postItem.jsx
@@ -9,13 +9,13 @@ import {
 import {
   PostItemBanner,
   PostItemTitle,
-  PostItemUser,
   PostItemTime,
   PostItemCommentCount,
   PostItemMalicious,
   PostItemAISummary,
 } from "next-common/components/postList/common";
 import Divider from "next-common/components/styled/layout/divider";
+import PolkassemblyUser from "next-common/components/user/polkassemblyUser";
 
 export default function PostItem({ data }) {
   return (
@@ -27,7 +27,10 @@ export default function PostItem({ data }) {
         <Divider margin={12} />
         <FooterWrapper>
           <Footer>
-            <PostItemUser data={data} />
+            <PolkassemblyUser
+              className="text12Medium text-textPrimary"
+              user={data?.author}
+            />
             <PostItemTime data={data} />
             <PostItemCommentCount data={data} />
             <PostItemMalicious isMalicious={data?.isMalicious} />
