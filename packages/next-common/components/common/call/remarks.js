a8386835312a6ea1a35d582172aeb478d881a51e
quinn-gao <quinnn.gao@gmail.com>
2025-08-25 17:23:29 +0800

    fix: add support for displaying remarks in Gov2 referendum call component, #6531

--- packages/next-common/components/common/call/remarks.js
+++ packages/next-common/components/common/call/remarks.js
@@ -1,5 +1,3 @@
-import { hexIsValidUTF8 } from "next-common/utils/utf8validate";
-import { hexToString } from "@polkadot/util";
 import React from "react";
 import { MarkdownPreviewer } from "@osn/previewer";
 import styled from "styled-components";
@@ -13,43 +11,7 @@ const RemarkWrapper = styled.div`
   }
 `;
 
-function isRemark(section, method) {
-  return "system" === section && ["remark", "remarkWithEvent"].includes(method);
-}
-
-function findRemarkCalls(call) {
-  const { section, method, args } = call || {};
-  if (isRemark(section, method)) {
-    return [call];
-  }
-
-  if (
-    "utility" === section &&
-    ["batch", "forceBatch"].includes(method) &&
-    args
-  ) {
-    const calls = args[0].value;
-    return calls.filter((call) => isRemark(call.section, call.method));
-  }
-
-  return [];
-}
-
-function extractRemarks(call = {}) {
-  const remarkCalls = findRemarkCalls(call);
-  const remarks = [];
-  for (const call of remarkCalls) {
-    const value = call.args[0].value;
-    if (hexIsValidUTF8(value)) {
-      remarks.push(hexToString(value));
-    }
-  }
-
-  return remarks;
-}
-
-export default function extractRemarkMetaFields(call = {}) {
-  let remarks = extractRemarks(call);
+export default function extractRemarkMetaFields(remarks) {
   const multipleRemark = remarks.length > 1;
 
   return remarks?.map((item, index) => {
