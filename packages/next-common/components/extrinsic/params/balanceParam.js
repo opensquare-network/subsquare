dffb61dc2e121c667290e277248d85e5956b81ee
chaojun <chaojun@opensquare.network>
2025-08-28 15:42:21 +0800

    Not use input with symbol in some cases, #6562

--- packages/next-common/components/extrinsic/params/balanceParam.js
+++ packages/next-common/components/extrinsic/params/balanceParam.js
@@ -1,9 +1,41 @@
 import BigNumber from "bignumber.js";
 import CurrencyInput from "next-common/components/currencyInput";
 import { useChainSettings } from "next-common/context/chain";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { CallContext } from "../context";
 
-export default function BalanceParam({ title, setValue }) {
+function SimpleBalanceParam({ title, setValue }) {
+  const [inputText, setInputText] = useState("");
+
+  useEffect(
+    (data) => {
+      const bn = new BigNumber(inputText);
+
+      if (bn.isNaN() || bn.isLessThan(0)) {
+        setValue({
+          isValid: false,
+          data,
+        });
+        return;
+      }
+
+      setValue({
+        isValid: true,
+        data: bn.integerValue().toString(),
+      });
+    },
+    [setValue, inputText],
+  );
+
+  return (
+    <>
+      {title}
+      <CurrencyInput value={inputText} onValueChange={setInputText} />
+    </>
+  );
+}
+
+function NativeTokenBalanceParam({ title, setValue }) {
   const [inputText, setInputText] = useState("");
   const { symbol, decimals } = useChainSettings();
 
@@ -37,4 +69,14 @@ export default function BalanceParam({ title, setValue }) {
       />
     </>
   );
+}
+
+export default function BalanceParam({ title, setValue }) {
+  const { section } = useContext(CallContext);
+
+  if (["balances"].includes(section)) {
+    return <NativeTokenBalanceParam title={title} setValue={setValue} />;
+  }
+
+  return <SimpleBalanceParam title={title} setValue={setValue} />;
 }
