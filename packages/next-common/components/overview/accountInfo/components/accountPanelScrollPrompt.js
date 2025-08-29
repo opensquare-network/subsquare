6658a6a1a1369a1fc9065cfb5ea369f1b627a7d4
quinn-gao <quinnn.gao@gmail.com>
2025-08-29 17:31:53 +0800

    refactor: account panel prompts to use new component structure and improve prompt handling, #6573

--- packages/next-common/components/overview/accountInfo/components/accountPanelScrollPrompt.js
+++ packages/next-common/components/overview/accountInfo/components/accountPanelScrollPrompt.js
@@ -1,53 +1,20 @@
-import { useEffect, useState } from "react";
-import useDelegationPrompt from "./useDelegationPrompt";
-import useSetAvatarPrompt from "./useSetAvatarPrompt";
-import { isEmpty } from "lodash-es";
-import ScrollPrompt from "next-common/components/scrollPrompt";
-import useSetIdentityPrompt from "./useSetIdentityPrompt";
-import useMultisigPrompt from "./useMultisigPrompt";
-import useAssetHubManagePrompt from "./useAssetHubManagePrompt";
-import { AssetHubMetadataProvider } from "../context/assetHubMetadataContext";
+import { DelegationPrompt } from "./useDelegationPrompt";
+import { AvatarPrompt } from "./useSetAvatarPrompt";
+import { AccountPanelScroll } from "next-common/components/scrollPrompt";
+import { IdentityPrompt } from "./useSetIdentityPrompt";
+import { MultisigPrompt } from "./useMultisigPrompt";
+import AssetHubManagePrompt from "./useAssetHubManagePrompt";
 
 export default function AccountPanelScrollPrompt() {
   return (
-    <AssetHubMetadataProvider>
-      <AccountPanelScrollPromptImpl />
-    </AssetHubMetadataProvider>
-  );
-}
-
-function AccountPanelScrollPromptImpl() {
-  const delegationPrompt = useDelegationPrompt();
-  const setAvatarPrompt = useSetAvatarPrompt();
-  const setIdentityPrompt = useSetIdentityPrompt();
-  const multisigPrompt = useMultisigPrompt();
-  const assetHubManagePrompt = useAssetHubManagePrompt();
-
-  const [prompts, setPrompts] = useState([]);
-
-  useEffect(() => {
-    setPrompts(
-      [
-        multisigPrompt,
-        delegationPrompt,
-        setAvatarPrompt,
-        setIdentityPrompt,
-        assetHubManagePrompt,
-      ].filter((item) => !isEmpty(item)),
-    );
-  }, [
-    assetHubManagePrompt,
-    delegationPrompt,
-    multisigPrompt,
-    setAvatarPrompt,
-    setIdentityPrompt,
-  ]);
-
-  return (
-    <ScrollPrompt
-      prompts={prompts}
-      setPrompts={setPrompts}
-      pageSize={prompts.length > 2 ? 2 : 1}
+    <AccountPanelScroll
+      items={[
+        DelegationPrompt,
+        AvatarPrompt,
+        IdentityPrompt,
+        MultisigPrompt,
+        AssetHubManagePrompt,
+      ]}
     />
   );
 }
--- packages/next-common/components/scrollPrompt/index.js
+++ packages/next-common/components/scrollPrompt/index.js
@@ -1,8 +1,9 @@
 import { SystemClose } from "@osn/icons/subsquare";
 import { cn } from "next-common/utils";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { useAnimate } from "framer-motion";
+import { animate, useAnimate } from "framer-motion";
 import { useWindowSize } from "react-use";
+import { Fragment } from "react";
 
 export const PromptTypes = {
   INFO: "info",
@@ -151,6 +152,121 @@ export default function ScrollPrompt({
           );
         })}
       </div>
+    </div>
+  );
+}
+
+export function ScrollPromptItemWrapper({ prompt }) {
+  const { width } = useWindowSize();
+  const isMobile = width < 768;
+  return (
+    <div
+      key={prompt.key}
+      className={cn(
+        "flex justify-between items-center rounded-lg",
+        "text14Medium py-2.5 px-4 flex-shrink-0",
+        isMobile ? "h-[60px]" : "h-[40px]",
+      )}
+      style={colorStyle[prompt.type || PromptTypes.NEUTRAL]}
+    >
+      <div>{prompt.message}</div>
+      <SystemClose
+        className="w-5 h-5 flex-shrink-0"
+        role="button"
+        onClick={prompt.close}
+      />
+    </div>
+  );
+}
+
+export function AccountPanelScroll({ items }) {
+  const wrapperRef = useRef();
+  const { width } = useWindowSize();
+  const isMobile = width < 768;
+  const [total, setTotal] = useState(0);
+  const pauseRef = useRef(false);
+
+  const pageSize = total > 2 ? 2 : 1;
+
+  const wrapperHeight = useMemo(() => {
+    if (!total) {
+      return 0;
+    }
+    if (isMobile) {
+      return pageSize * MOBILE_ITEM_HEIGHT + ITEM_GAP * (pageSize - 1);
+    }
+    return pageSize * ITEM_HEIGHT + ITEM_GAP * (pageSize - 1);
+  }, [isMobile, pageSize, total]);
+
+  const [random, setRandom] = useState(1);
+  useEffect(() => {
+    setTotal(wrapperRef.current.children.length / 2);
+  }, [random]);
+
+  const marginTop = useMemo(() => {
+    if (isMobile) {
+      return (MOBILE_ITEM_HEIGHT + ITEM_GAP) * 1;
+    }
+    return (ITEM_HEIGHT + ITEM_GAP) * 1;
+  }, [isMobile]);
+
+  const indexRef = useRef(0);
+
+  useEffect(() => {
+    if (total < 2) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      if (pauseRef.current || !wrapperRef.current) {
+        return;
+      }
+      if (indexRef.current > total) {
+        wrapperRef.current?.scrollTo({
+          top: 0,
+        });
+        indexRef.current = 1;
+      }
+      const scrollTop = wrapperRef.current?.scrollTop;
+
+      animate(0, 100, {
+        duration: 1,
+        times: [0, 1],
+        onUpdate: (v) => {
+          wrapperRef.current?.scrollTo({
+            top: (v / 100) * marginTop + scrollTop,
+          });
+        },
+      });
+
+      indexRef.current++;
+    }, 6500);
+    return () => clearInterval(interval);
+  }, [marginTop, total]);
+
+  return (
+    <div
+      ref={wrapperRef}
+      style={{
+        height: wrapperHeight + "px",
+        display: total ? "" : "none",
+      }}
+      className=" overflow-y-hidden flex flex-col gap-1"
+      onMouseEnter={() => (pauseRef.current = true)}
+      onMouseLeave={() => (pauseRef.current = false)}
+    >
+      {[...items, ...items].map((Item, index) => {
+        return (
+          <Fragment key={index}>
+            <Item
+              key={random}
+              onClose={() => {
+                setRandom(random + 1);
+              }}
+            />
+          </Fragment>
+        );
+      })}
     </div>
   );
 }
