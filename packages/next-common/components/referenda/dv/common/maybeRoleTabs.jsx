7628026f25e31882ea86329584050f38983823ca
wolyshaw <wolyshaw@users.noreply.github.com>
2025-09-03 17:58:46 +0800

    add tab numer #6213

--- packages/next-common/components/referenda/dv/common/maybeRoleTabs.jsx
+++ packages/next-common/components/referenda/dv/common/maybeRoleTabs.jsx
@@ -3,11 +3,13 @@ import Tabs from "next-common/components/tabs";
 import useFormattedDelegates from "next-common/hooks/referenda/useFormattedDelegates";
 import { isNil } from "lodash-es";
 import { useDvDelegateGuardians } from "next-common/context/referenda/dv";
+import { usePageProps } from "next-common/context/page";
 
 export function RoleTabsImpl({
   component: Component,
   formattedDelegates = [],
 }) {
+  const { cohort } = usePageProps();
   const [activeTabValue, setActiveTabValue] = useState("delegate");
 
   const tabs = useMemo(() => {
@@ -24,14 +26,24 @@ export function RoleTabsImpl({
         value: "delegate",
         label: "DAO",
         content: <Component delegates={filteredDelegates} />,
+        labelExtra: (
+          <span className="text-textTertiary text14Medium before:content-['·'] before:mx-1">
+            {cohort?.delegateCnt}
+          </span>
+        ),
       },
       {
         value: "guardian",
         label: "Guardian",
         content: <Component delegates={filteredDelegates} />,
+        labelExtra: (
+          <span className="text-textTertiary text14Medium before:content-['·'] before:mx-1">
+            {cohort?.guardianCnt}
+          </span>
+        ),
       },
     ];
-  }, [activeTabValue, formattedDelegates]);
+  }, [activeTabValue, formattedDelegates, cohort]);
 
   return (
     <Tabs
