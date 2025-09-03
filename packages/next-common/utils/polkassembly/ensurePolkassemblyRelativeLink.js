bda87fddcfd0201333c177775f7bf2f47d78e09e
leochen <leochen.sat24@gmail.com>
2025-09-03 11:38:52 +0800

    Fix postContent format

--- packages/next-common/utils/polkassembly/ensurePolkassemblyRelativeLink.js
+++ packages/next-common/utils/polkassembly/ensurePolkassemblyRelativeLink.js
@@ -1,5 +1,5 @@
 export function ensurePolkassemblyRelativeLink(postContent = "", chain = "") {
-  return postContent.replace(
+  return String(postContent)?.replace(
     /href=(['"])\.\./g,
     (_, $1) => `href=${$1}https://${chain}.polkassembly.io`,
   );
