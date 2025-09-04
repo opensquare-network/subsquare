66430f27c586de43941a6bc619d43a324209f563
wolyshaw <wolyshaw@users.noreply.github.com>
2025-09-04 09:43:51 +0800

    Fix cohorts delegates #6213

--- packages/next-common/components/referenda/dv/cohorts/delegateCol.jsx
+++ packages/next-common/components/referenda/dv/cohorts/delegateCol.jsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
 import { LinkMedium } from "@osn/icons/subsquare";
 import { isNil } from "lodash-es";
+import BigNumber from "bignumber.js";
 
 export default function DelegateCol({ row }) {
   if (isNil(row)) {
     return null;
   }
 
+  const count = BigNumber(row.delegateCnt)
+    .plus(row.guardianCnt || 0)
+    .toString();
+
   return (
     <span className="flex items-center justify-end gap-x-1">
-      {row.delegateCnt}
+      {count}
       {row.announcementLink && (
         <Link
           target="_blank"
