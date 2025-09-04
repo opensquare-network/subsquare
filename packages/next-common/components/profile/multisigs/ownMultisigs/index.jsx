d1de879dc4c2d2be634513c85b60a0f8746f1756
leochen <leochen.sat24@gmail.com>
2025-09-04 14:00:37 +0800

    As multisig empty, #6599

--- packages/next-common/components/profile/multisigs/ownMultisigs/index.jsx
+++ packages/next-common/components/profile/multisigs/ownMultisigs/index.jsx
@@ -4,16 +4,18 @@ import { useState, useMemo } from "react";
 import MultisigSummary from "next-common/components/profile/multisigs/ownMultisigs/summary";
 // import useProfileAddress from "next-common/components/profile/useProfileAddress";
 // import dynamicClientOnly from "next-common/lib/dynamic/clientOnly";
+import { useMultisigContext } from "next-common/components/profile/multisigs/context";
+import Loading from "next-common/components/loading";
 
-function Multisigs() {
+function MultisigsTabContent() {
   return <div>Multisigs</div>;
 }
 
-function Signatories() {
+function SignatoriesTabContent() {
   return <div>Signatories</div>;
 }
 
-function OwnMultisigsImpl() {
+function OwnMultisigsTabs() {
   const [activeTabValue, setActiveTabValue] = useState("multisigs");
   // const address = useProfileAddress();
 
@@ -22,12 +24,12 @@ function OwnMultisigsImpl() {
       {
         value: "multisigs",
         label: "Multisigs",
-        content: <Multisigs />,
+        content: <MultisigsTabContent />,
       },
       {
         value: "signatories",
         label: "Signatories",
-        content: <Signatories />,
+        content: <SignatoriesTabContent />,
       },
     ],
     [],
@@ -42,14 +44,46 @@ function OwnMultisigsImpl() {
   );
 }
 
-// TODO: if not a multisig address -> always show this tab(show the null card)
-export default function OwnMultisigs() {
+function OwnMultisigsContent() {
   return (
     <div className="flex flex-col gap-[18px]">
       <MultisigSummary />
       <SecondaryCard>
-        <OwnMultisigsImpl />
+        <OwnMultisigsTabs />
       </SecondaryCard>
     </div>
   );
+}
+
+function Empty() {
+  return (
+    <div className="flex flex-col gap-[18px]">
+      <SecondaryCard className="flex items-center justify-center">
+        <span className="text-textTertiary text14Medium">
+          {"Can't find multisig records of this address on chain"}
+        </span>
+      </SecondaryCard>
+    </div>
+  );
+}
+
+function LoadingWrapper() {
+  return (
+    <div className="flex grow mt-2 justify-center items-center">
+      <Loading size={20} />
+    </div>
+  );
+}
+
+export default function OwnMultisigs() {
+  const { multisig, loading } = useMultisigContext();
+  if (loading) {
+    return <LoadingWrapper />;
+  }
+
+  if (!multisig) {
+    return <Empty />;
+  }
+
+  return <OwnMultisigsContent />;
 }
