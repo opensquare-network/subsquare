018096fca31539ba4af3835fcf62e157e3374207
wolyshaw <wolyshaw@users.noreply.github.com>
2025-09-07 01:22:20 +0800

    fix tab style #6213

--- packages/next-common/components/layout/referendaLayout/tabTitle.jsx
+++ packages/next-common/components/layout/referendaLayout/tabTitle.jsx
@@ -4,7 +4,7 @@ export default function TabTitle({ active, className = "", children }) {
   return (
     <span
       className={cn(
-        "flex items-center cursor-pointer pb-3 text14Bold border-b-4 whitespace-nowrap hover:text-theme500",
+        "flex items-center cursor-pointer pb-3 text14Bold border-b-4 whitespace-nowrap hover:text-theme500 text-textPrimary",
         className,
         {
           "text-theme500 border-theme500": active,
