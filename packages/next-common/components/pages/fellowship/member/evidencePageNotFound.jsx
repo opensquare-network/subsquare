4a95ea3ba80cab79a0a1d6a762bea5c175236b7f
leochen <leochen.sat24@gmail.com>
2025-09-10 11:57:25 +0800

    Not redirect users to 404 but show 404 component if no evidence found, #6640

---
+++ packages/next-common/components/pages/fellowship/member/evidencePageNotFound.jsx
@@ -0,0 +1,53 @@
+import NotFoundDetail from "next-common/components/notFoundDetail";
+import AddressUser from "next-common/components/user/addressUser";
+import { useRouter } from "next/router";
+import WindowSizeProvider from "next-common/context/windowSize";
+import { useIsMobile } from "next-common/components/overview/accountInfo/components/accountBalances";
+
+function EvidencePageNotFoundImpl({ props }) {
+  const router = useRouter();
+  const routerPath = router.asPath.split("/");
+  const evidenceId = routerPath[routerPath?.length - 1];
+  const isMobile = useIsMobile();
+
+  return (
+    <NotFoundDetail
+      customId={`#${evidenceId}`}
+      breadcrumbItems={[
+        isMobile
+          ? null
+          : {
+              path: "/fellowship",
+              content: "Fellowship",
+            },
+        {
+          path: "/fellowship/members",
+          content: "Members",
+        },
+        {
+          content: (
+            <AddressUser
+              add={props.who}
+              showAvatar={false}
+              link="/fellowship"
+            />
+          ),
+          className: "flex",
+        },
+        isMobile
+          ? null
+          : {
+              content: "Evidences",
+            },
+      ].filter(Boolean)}
+    />
+  );
+}
+
+export default function EvidencePageNotFound({ props }) {
+  return (
+    <WindowSizeProvider>
+      <EvidencePageNotFoundImpl props={props} />
+    </WindowSizeProvider>
+  );
+}
