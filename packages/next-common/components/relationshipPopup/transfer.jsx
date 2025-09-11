8da00571a77abc5e08daff071cd32e3f1555052b
wolyshaw <wolyshaw@users.noreply.github.com>
2025-09-11 15:56:42 +0800

    move node #6507

--- packages/next-common/components/relationshipPopup/transfer.jsx
+++ packages/next-common/components/relationshipPopup/transfer.jsx
@@ -114,19 +114,25 @@ const createNodes = (
 
   function dragstarted(event, d) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+    const t = d3.zoomTransform(svg.node());
+    const [x, y] = t.invert([event.x, event.y]);
+    d.fx = x;
+    d.fy = y;
   }
 
   function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
+    const t = d3.zoomTransform(svg.node());
+    const [x, y] = t.invert([event.x, event.y]);
+    d.fx = x;
+    d.fy = y;
   }
 
   function dragended(event, d) {
     if (!event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
+    const t = d3.zoomTransform(svg.node());
+    const [x, y] = t.invert([event.x, event.y]);
+    d.fx = x;
+    d.fy = y;
   }
 
   return node;
