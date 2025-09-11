a6b90acbeebf425eac6c10b327e82e82d5bff476
leochen <leochen.sat24@gmail.com>
2025-09-11 15:46:16 +0800

    Rename, #3929

--- packages/next-common/components/contentTranslations.jsx
+++ packages/next-common/components/contentTranslations.jsx
@@ -3,14 +3,14 @@ import TranslationsSelect from "./translationsSelect";
 import { useState } from "react";
 
 export default function ContentTranslations() {
-  // selectedLauguage value: SOURCE | SC | ES | RU
-  const [selectedLauguage, setSelectedLauguage] = useState("");
+  // selectedLanguage value: SOURCE | SC | ES | RU
+  const [selectedLanguage, setSelectedLauguage] = useState("");
 
   return (
     <div className="py-2.5 pl-4 pr-2.5 mb-4 bg-neutral200 rounded-[8px] flex items-center">
       <SystemTranslate className="w-6 h-6 mr-4 text-textTertiary" />
       <TranslationsSelect
-        selectedLauguage={selectedLauguage}
+        selectedLanguage={selectedLanguage}
         onSelect={(value) => setSelectedLauguage(value)}
       />
     </div>
