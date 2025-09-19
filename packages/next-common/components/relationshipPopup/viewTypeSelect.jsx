8986385e6381c93ad3c6d0b734ba7808636e20d0
wolyshaw <wolyshaw@users.noreply.github.com>
2025-09-19 15:27:38 +0800

    options #6507

--- packages/next-common/components/relationshipPopup/viewTypeSelect.jsx
+++ packages/next-common/components/relationshipPopup/viewTypeSelect.jsx
@@ -4,27 +4,38 @@ import {
   VIEW_TYPE,
 } from "next-common/context/relationship/selectViewType";
 import { noop } from "lodash-es";
+import { useMemo } from "react";
+import { useChain } from "next-common/context/chain";
+import { isPolkadotChain } from "next-common/utils/chain";
 
 export default function ViewTypeSelect({ setViewType = noop, className = "" }) {
   const { viewType } = useRelationshipViewTypeState();
+  const chain = useChain();
+
+  const options = useMemo(() => {
+    return [
+      {
+        label: "Common",
+        value: VIEW_TYPE.COMMON,
+      },
+      {
+        label: "Delegation",
+        value: VIEW_TYPE.DELEGATION,
+      },
+      isPolkadotChain(chain)
+        ? {
+            label: "Transfer",
+            value: VIEW_TYPE.TRANSFER,
+          }
+        : null,
+    ].filter(Boolean);
+  }, [chain]);
+
   return (
     <Select
       small
       className={className}
-      options={[
-        {
-          label: "Common",
-          value: VIEW_TYPE.COMMON,
-        },
-        {
-          label: "Delegation",
-          value: VIEW_TYPE.DELEGATION,
-        },
-        {
-          label: "Transfer",
-          value: VIEW_TYPE.TRANSFER,
-        },
-      ]}
+      options={options}
       value={viewType}
       onChange={(item) => setViewType(item.value)}
     />
