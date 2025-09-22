24a57e6ee477150393dabb59ea3eb766752886b3
quinn-gao <quinnn.gao@gmail.com>
2025-09-22 11:59:23 +0800

    fix: only polkadot has news managements, #3627

--- packages/next/pages/news/management.jsx
+++ packages/next/pages/news/management.jsx
@@ -1,8 +1,18 @@
 import NewsManagementPage from "next-common/components/news/newsManagementPage";
 import { withCommonProps } from "next-common/lib";
+import { CHAIN } from "next-common/utils/constants";
+import { isPolkadotChain } from "next-common/utils/chain";
 export default NewsManagementPage;
 
 export const getServerSideProps = withCommonProps(async () => {
+  if (!isPolkadotChain(CHAIN)) {
+    return {
+      redirect: {
+        permanent: true,
+        destination: "/404",
+      },
+    };
+  }
   return {
     props: {},
   };
