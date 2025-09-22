5640dbb5601ffcc1c5de85237e7a8fdeb20e2c7f
wolyshaw <wolyshaw@users.noreply.github.com>
2025-09-22 17:23:08 +0800

    split #6668

--- packages/next-common/components/multisigs/actions/composeCallPopup/index.jsx
+++ packages/next-common/components/multisigs/actions/composeCallPopup/index.jsx
@@ -1,25 +1,11 @@
-import { ChoiceButton } from "next-common/components/summary/newProposalButton/common";
 import Popup from "next-common/components/popup/wrapper/Popup";
-import {
-  usePopupParams,
-  useSignerContext,
-} from "next-common/components/popupWithSigner/context";
-import SignerWithBalance from "next-common/components/signerPopup/signerWithBalance";
 import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
-import { useState } from "react";
-import { useMount } from "react-use";
-import { SystemNewProposal } from "@osn/icons/subsquare";
+
 import {
   useStepContainer,
   StepContainerProvider,
 } from "next-common/context/stepContainer";
-import ComposeCallTabs from "./composeCallTabs";
-import ProposeWithInputHex from "./proposeWithInputHex";
-import ProposeWithExtrinsic from "./proposeWithExtrinsic";
-import Transfer from "./templates/transfer";
-import BatchTransfer from "./templates/batchTransfer";
-import AddProxy from "./templates/addProxy";
-import RemoveProxy from "./templates/removeProxy";
+import QuickStart from "./quickStart";
 
 export default function ComposeCallPopup({ onClose, multisig }) {
   return (
@@ -35,33 +21,6 @@ export default function ComposeCallPopup({ onClose, multisig }) {
         <PopupContent onClose={onClose} />
       </StepContainerProvider>
     </SignerPopupWrapper>
-  );
-}
-
-export function ComposeCallPopupImpl() {
-  const { multisig } = usePopupParams();
-  const [formType, setFormType] = useState("set");
-
-  const { setSelectedProxyAddress, setMultisig } = useSignerContext();
-
-  useMount(() => {
-    setSelectedProxyAddress();
-    setMultisig(multisig);
-  });
-
-  return (
-    <div className="flex flex-col gap-[8px]">
-      <SignerWithBalance noSwitchSigner />
-      <ComposeCallTabs formType={formType} setFormType={setFormType} />
-
-      <div className={formType === "set" ? "hidden" : ""}>
-        <ProposeWithInputHex />
-      </div>
-
-      <div className={formType === "input" ? "hidden" : ""}>
-        <ProposeWithExtrinsic />
-      </div>
-    </div>
   );
 }
 
@@ -81,66 +40,3 @@ const PopupContent = ({ onClose }) => {
     </Popup>
   );
 };
-
-function QuickStart() {
-  const { goNext } = useStepContainer();
-  return (
-    <>
-      <ChoiceButton
-        description="Builds a call to transfer tokens"
-        icon={<SystemNewProposal className="text-textTertiary" />}
-        buttonSuffix="Transfer"
-        onClick={() => {
-          goNext({
-            title: "Transfer",
-            component: Transfer,
-          });
-        }}
-      />
-      <ChoiceButton
-        description="Builds a call to batch transfer tokens"
-        icon={<SystemNewProposal className="text-textTertiary" />}
-        buttonSuffix="Batch Transfer"
-        onClick={() => {
-          goNext({
-            title: "Batch Transfer",
-            component: BatchTransfer,
-          });
-        }}
-      />
-      <ChoiceButton
-        description="Builds a call to add a proxy with a given type and delay"
-        icon={<SystemNewProposal className="text-textTertiary" />}
-        buttonSuffix="Add Proxy"
-        onClick={() => {
-          goNext({
-            title: "Add Proxy",
-            component: AddProxy,
-          });
-        }}
-      />
-      <ChoiceButton
-        description="Builds a call to remove a proxy with a given type and delay"
-        icon={<SystemNewProposal className="text-textTertiary" />}
-        buttonSuffix="Remove Proxy"
-        onClick={() => {
-          goNext({
-            title: "Remove Proxy",
-            component: RemoveProxy,
-          });
-        }}
-      />
-      <ChoiceButton
-        description="Allows building any Propose call manually."
-        icon={<SystemNewProposal className="text-textTertiary" />}
-        buttonSuffix="Propose Call"
-        onClick={() => {
-          goNext({
-            title: "Propose Call",
-            component: ComposeCallPopupImpl,
-          });
-        }}
-      />
-    </>
-  );
-}
---
+++ packages/next-common/components/multisigs/actions/composeCallPopup/quickStart.jsx
@@ -0,0 +1,72 @@
+import { ChoiceButton } from "next-common/components/summary/newProposalButton/common";
+
+import { SystemNewProposal } from "@osn/icons/subsquare";
+import Transfer from "./templates/transfer";
+import BatchTransfer from "./templates/batchTransfer";
+import AddProxy from "./templates/addProxy";
+import RemoveProxy from "./templates/removeProxy";
+import { useStepContainer } from "next-common/context/stepContainer";
+import ComposeCallPopupImpl from "./composeCall";
+
+export default function QuickStart() {
+  const { goNext } = useStepContainer();
+  return (
+    <>
+      <ChoiceButton
+        description="Builds a call to transfer tokens"
+        icon={<SystemNewProposal className="text-textTertiary" />}
+        buttonSuffix="Transfer"
+        onClick={() => {
+          goNext({
+            title: "Transfer",
+            component: Transfer,
+          });
+        }}
+      />
+      <ChoiceButton
+        description="Builds a call to batch transfer tokens"
+        icon={<SystemNewProposal className="text-textTertiary" />}
+        buttonSuffix="Batch Transfer"
+        onClick={() => {
+          goNext({
+            title: "Batch Transfer",
+            component: BatchTransfer,
+          });
+        }}
+      />
+      <ChoiceButton
+        description="Builds a call to add a proxy with a given type and delay"
+        icon={<SystemNewProposal className="text-textTertiary" />}
+        buttonSuffix="Add Proxy"
+        onClick={() => {
+          goNext({
+            title: "Add Proxy",
+            component: AddProxy,
+          });
+        }}
+      />
+      <ChoiceButton
+        description="Builds a call to remove a proxy with a given type and delay"
+        icon={<SystemNewProposal className="text-textTertiary" />}
+        buttonSuffix="Remove Proxy"
+        onClick={() => {
+          goNext({
+            title: "Remove Proxy",
+            component: RemoveProxy,
+          });
+        }}
+      />
+      <ChoiceButton
+        description="Allows building any Propose call manually."
+        icon={<SystemNewProposal className="text-textTertiary" />}
+        buttonSuffix="Propose Call"
+        onClick={() => {
+          goNext({
+            title: "Propose Call",
+            component: ComposeCallPopupImpl,
+          });
+        }}
+      />
+    </>
+  );
+}
