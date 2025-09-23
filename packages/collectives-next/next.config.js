18490e135a8dc239ac83c52b6ca82b69861e14b0
quinn-gao <quinnn.gao@gmail.com>
2025-09-23 18:36:43 +0800

    fix: update lib

--- packages/collectives-next/next.config.js
+++ packages/collectives-next/next.config.js
@@ -27,6 +27,9 @@ const config = {
     ];
   },
   webpack(config, { dev }) {
+    // Fix MetaMask SDK React Native dependency issue
+    config.resolve.alias["@react-native-async-storage/async-storage"] = false;
+
     // Treat warnings as errors if we're not in development.
     if (!dev) {
       config.optimization.minimizer = config.optimization.minimizer || [];
