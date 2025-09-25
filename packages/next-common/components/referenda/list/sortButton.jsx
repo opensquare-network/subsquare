bffa0cc51a38d5e049e15d71965cbda284134a9e
quinn-gao <53474899+quinn-gaoo@users.noreply.github.com>
2025-09-25 16:17:41 +0800

    Add referenda list desc sort, #6628 (#6684)
    * feat: referenda list desc sort, #6628
    
    * Improve sort copywriting, #6682
    
    ---------
    
    Co-authored-by: Yongfeng LI <wliyongfeng@gmail.com>

---
+++ packages/next-common/components/referenda/list/sortButton.jsx
@@ -0,0 +1,58 @@
+import { ArrowUp, ArrowDown } from "@osn/icons/subsquare";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { pickBy, omit } from "lodash-es";
+import Tooltip from "next-common/components/tooltip";
+import { useMemo } from "react";
+
+export default function SortButton() {
+  const router = useRouter();
+  const sort = router.query?.sort || "";
+
+  const query = useMemo(() => {
+    const query = { ...router.query };
+    query.page = 0;
+    if (sort === "") {
+      query.sort = "index_desc";
+    } else if (sort === "index_desc") {
+      query.sort = "";
+    }
+
+    return pickBy(omit(query, ["page"]), Boolean);
+  }, [router.query, sort]);
+
+  const tooltipText =
+    sort === ""
+      ? "Click to sort referenda by index descending"
+      : "Click to clear sorting";
+
+  return (
+    <Tooltip content={tooltipText}>
+      <Link
+        href={{
+          pathname: "/referenda",
+          query: query,
+        }}
+        shallow
+        onClick={(e) => {
+          e.preventDefault();
+          router.replace({ query });
+        }}
+      >
+        <button
+          size="small"
+          className="h-7 w-7 flex overflow-hidden justify-center items-center bg-neutral100 border-neutral400 hover:border-neutral500 rounded-md border"
+        >
+          {sort === "index_desc" ? (
+            <ArrowDown className="w-5 h-5 translate-y-0" />
+          ) : (
+            <div className="flex flex-col ">
+              <ArrowDown className="w-5 h-5 translate-y-[70%]" />
+              <ArrowUp className="w-5 h-5 -translate-y-[70%]" />
+            </div>
+          )}
+        </button>
+      </Link>
+    </Tooltip>
+  );
+}
