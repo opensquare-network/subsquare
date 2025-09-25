c1eb59f1d7006880e19798b93c918e8031fd8ecc
leochen <leochen.sat24@gmail.com>
2025-09-25 16:28:15 +0800

    lp total

--- packages/next-common/hooks/ecoAssets/hydration/useLPTotal.js
+++ packages/next-common/hooks/ecoAssets/hydration/useLPTotal.js
@@ -1,1 +1,44 @@
-// Farm LP Total Balance
+import { useMemo } from "react";
+import useAccountPositions from "./common/useAccountPosition";
+import useLiquidityPositionData from "./common/useLiquidityPositionData";
+import BigNumber from "bignumber.js";
+
+function useOmnipoolPositionsData(address) {
+  const { data: accountPositions, isLoading } = useAccountPositions(address);
+  const { liquidityPositions } = accountPositions ?? {};
+
+  const positionIds = liquidityPositions?.map((position) => position.assetId);
+
+  const { getData } = useLiquidityPositionData(positionIds);
+
+  const data = useMemo(() => {
+    return (liquidityPositions ?? []).reduce((acc, position) => {
+      const data = getData(position);
+      if (data) acc.push(data);
+      return acc;
+    }, []);
+  }, [getData, liquidityPositions]);
+
+  return {
+    data,
+    isLoading,
+  };
+}
+
+export default function useLPTotal(address) {
+  const { data, isLoading } = useOmnipoolPositionsData(address);
+
+  const balance = useMemo(
+    () =>
+      data.reduce(
+        (acc, { valueDisplay }) => BigNumber(acc).plus(valueDisplay).toString(),
+        "0",
+      ),
+    [data],
+  );
+
+  return {
+    balance,
+    isLoading,
+  };
+}
--- packages/next-common/hooks/ecoAssets/hydration/useTotalAssetsBalance.js
+++ packages/next-common/hooks/ecoAssets/hydration/useTotalAssetsBalance.js
@@ -1,13 +1,15 @@
 import useAssetsTotal from "./useAssetsTotal";
 import useFarmTotal from "./useFarmTotal";
+import useLPTotal from "./useLPTotal";
 
 export default async function useTotalAssetsBalance() {
-  const address = "12sNU8BXivMj1xQmcd4T39ugCyHjmhir8jkPqfAw5ZDESrx4";
+  const address = "158svTMgfWrejQGjt3NNa1Pvhgm8ngMj5cn912Gwr47RoUK7";
   const { balance, isLoading } = useAssetsTotal(address);
+  const { balance: lpBalance, isLoading: lpIsLoading } = useLPTotal(address);
   const { balance: farmBalance, isLoading: farmIsLoading } = useFarmTotal(address);
   console.log("::::farmBalance", farmBalance, farmIsLoading);
 
   console.log("::::balance", balance, isLoading);
-  // console.log("::::lpBalance", lpBalance);
+  console.log("::::lpBalance", lpBalance, lpIsLoading);
   // return { balance, isLoading };
 }
