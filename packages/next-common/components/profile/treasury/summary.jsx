832ba097ec5bc8986cb69b80bed3b59d05745895
wolyshaw <wolyshaw@users.noreply.github.com>
2025-10-10 15:53:56 +0800

    fix link & copywriting #6702

--- packages/next-common/components/profile/treasury/summary.jsx
+++ packages/next-common/components/profile/treasury/summary.jsx
@@ -1,8 +1,11 @@
+import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import SummaryItem from "next-common/components/summary/layout/item";
 import SummaryLayout from "next-common/components/summary/layout/layout";
+import { AddressUser } from "next-common/components/user";
 import ValueDisplay from "next-common/components/valueDisplay";
 import { usePageProps } from "next-common/context/page";
+import { CHAIN } from "next-common/utils/constants";
 import Link from "next/link";
 
 export default function ProfileTreasurySummary() {
@@ -10,6 +13,19 @@ export default function ProfileTreasurySummary() {
 
   return (
     <SecondaryCard>
+      <GreyPanel className="text14Medium text-gray500 py-2.5 px-4 max-w-full mb-4 max-sm:!block gap-x-1 flex-wrap">
+        <AddressUser add={beneficiariesSummary?.address} /> is a beneficiary of{" "}
+        {CHAIN} treasury. Check all beneficiaries{" "}
+        <Link
+          href={`https://${CHAIN}.dotreasury.com/#/beneficiaries`}
+          target="_blank"
+          rel="noreferrer"
+          className="inline-flex text-theme500 underline"
+        >
+          here
+        </Link>
+        .
+      </GreyPanel>
       <SummaryLayout>
         <SummaryItem title="Total Awarded">
           <ValueDisplay
@@ -17,18 +33,6 @@ export default function ProfileTreasurySummary() {
             symbol=""
             prefix="$"
           />
-          <p className="text12Medium text-textTertiary !ml-0">
-            Check all beneficiaries{" "}
-            <Link
-              href="https://kusama.dotreasury.com/#/beneficiaries"
-              target="_blank"
-              rel="noreferrer"
-              className="inline-flex text-theme500 underline"
-            >
-              here
-            </Link>
-            .{" "}
-          </p>
         </SummaryItem>
       </SummaryLayout>
     </SecondaryCard>
