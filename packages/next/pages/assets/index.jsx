6abf6ff0cc6af87e38830a7c82802dce257a2c8e
leochen <leochen.sat24@gmail.com>
2025-10-10 11:59:39 +0800

    remove useless code from assets overview page, #6714

--- packages/next/pages/assets/index.jsx
+++ packages/next/pages/assets/index.jsx
@@ -1,66 +1,16 @@
 import { AssetMetadataProvider } from "next-common/components/assets/context/assetMetadata";
 import NoWalletConnected from "next-common/components/assets/noWalletConnected";
 import WalletAssetList from "next-common/components/assets/walletAssetList";
-import ApiProvider from "next-common/context/api";
-import ChainProvider from "next-common/context/chain";
 import { RelayChainApiProvider } from "next-common/context/relayChain";
-import RelayInfoProvider from "next-common/context/relayInfo";
 import { withCommonProps } from "next-common/lib";
-import { createStore } from "next-common/store";
-import { commonReducers } from "next-common/store/reducers";
-import multiAccountsSlice from "next-common/store/reducers/multiAccountsSlice";
 import { CHAIN } from "next-common/utils/constants";
 import getChainSettings from "next-common/utils/consts/settings";
 import useExistentialDeposit from "next-common/utils/hooks/chain/useExistentialDeposit";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
-import { Provider } from "react-redux";
-import { isAssetHubMigrated } from "next-common/utils/consts/isAssetHubMigrated";
 
 export const isAssetHubSupported = !!getChainSettings(CHAIN).modules?.assethub;
 
-let chain;
-let store;
-
-if (isAssetHubSupported) {
-  chain = `${CHAIN}-assethub`;
-  store = createStore({
-    chain,
-    reducer: {
-      ...commonReducers,
-      multiAccounts: multiAccountsSlice,
-    },
-  });
-}
-
-function ConditionRelayInfoProvider({ children }) {
-  if (isAssetHubMigrated()) {
-    return children;
-  }
-
-  return <RelayInfoProvider>{children}</RelayInfoProvider>;
-}
-
-export function AssetHubPageProvider({ children }) {
-  return (
-    <ConditionRelayInfoProvider>
-      <Provider store={store}>
-        <ChainProvider chain={chain}>
-          <ApiProvider>{children}</ApiProvider>
-        </ChainProvider>
-      </Provider>
-    </ConditionRelayInfoProvider>
-  );
-}
-
 export default function AssetHubPage() {
-  return (
-    <AssetHubPageProvider>
-      <AssetHubOverviewPageImpl />
-    </AssetHubPageProvider>
-  );
-}
-
-function AssetHubOverviewPageImpl() {
   const realAddress = useRealAddress();
   useExistentialDeposit();
 
@@ -70,6 +20,7 @@ function AssetHubOverviewPageImpl() {
 
   return (
     <AssetMetadataProvider>
+      {/* Remove useRelayChainApi (Polkadot AHM completed) */}
       <RelayChainApiProvider>
         <WalletAssetList />
       </RelayChainApiProvider>
