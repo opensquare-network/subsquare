369dd04b92b9d6e7e7d119b05e9ee631ec26efc3
leochen <leochen.sat24@gmail.com>
2025-10-13 10:40:58 +0800

    Use conditional api for useFellowshipMember, #6720

--- packages/next-common/hooks/fellowship/useFellowshipMember.js
+++ packages/next-common/hooks/fellowship/useFellowshipMember.js
@@ -1,10 +1,15 @@
 import useSubStorage from "next-common/hooks/common/useSubStorage";
+import { useConditionalContextApi } from "next-common/context/migration/conditionalApi";
 
 export function useFellowshipMember(address, pallet = "fellowshipCollective") {
+  const api = useConditionalContextApi();
   const { result: member, loading: isLoading } = useSubStorage(
     pallet,
     "members",
     [address],
+    {
+      api,
+    },
   );
   return {
     member,
