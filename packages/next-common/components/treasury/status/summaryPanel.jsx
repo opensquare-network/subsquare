80635f740900c794c6514d4146a52326ba39e662
wolyshaw <wolyshaw@users.noreply.github.com>
2025-10-14 14:47:10 +0800

    improve summary #6705

--- packages/next-common/components/treasury/status/summaryPanel.jsx
+++ packages/next-common/components/treasury/status/summaryPanel.jsx
@@ -1,9 +1,11 @@
 import { useChain } from "next-common/context/chain";
-import { isPolkadotChain } from "next-common/utils/chain";
-import PolkadotTreasuryStatsOnProposal from "next-common/components/treasury/common/polkadotTreasuryStatsOnProposal";
-import TreasurySummary from "next-common/components/summary/treasurySummary";
+import { isKusamaChain, isPolkadotChain } from "next-common/utils/chain";
 import { PromptTypes } from "next-common/components/scrollPrompt";
 import dynamic from "next/dynamic";
+import KusamaTreasuryProvider from "next-common/context/treasury/kusamaTreasury";
+import KusamaTreasurySummary from "next-common/components/summary/kusamaTreasurySummary";
+import PolkadotTreasuryProvider from "next-common/context/treasury/polkadotTreasury";
+import PolkadotTreasurySummary from "next-common/components/summary/polkadotTreasurySummary";
 
 const TotalRequestingAssets = dynamic(
   () => import("next-common/components/totalRequestingAssets"),
@@ -14,11 +16,25 @@ const TotalRequestingAssets = dynamic(
 
 export default function TreasuryStatusSummaryPanel() {
   const chain = useChain();
-  const summaryPanel = isPolkadotChain(chain) ? (
-    <PolkadotTreasuryStatsOnProposal />
-  ) : (
-    <TreasurySummary />
-  );
+
+  let summaryPanel = null;
+  if (isPolkadotChain(chain)) {
+    summaryPanel = (
+      <PolkadotTreasuryProvider>
+        <PolkadotTreasurySummary />
+      </PolkadotTreasuryProvider>
+    );
+  } else if (isKusamaChain(chain)) {
+    summaryPanel = (
+      <KusamaTreasuryProvider>
+        <KusamaTreasurySummary />
+      </KusamaTreasuryProvider>
+    );
+  }
+
+  if (!summaryPanel) {
+    return null;
+  }
 
   return (
     <div className="flex flex-col gap-y-4">
