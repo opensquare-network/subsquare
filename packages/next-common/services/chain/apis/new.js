c015226428aef83d3a31a50e798008280702f20b
chaojun <chaojun@opensquare.network>
2025-10-16 15:48:28 +0800

    fix

--- packages/next-common/services/chain/apis/new.js
+++ packages/next-common/services/chain/apis/new.js
@@ -1,23 +1,13 @@
 import Chains from "next-common/utils/consts/chains";
 import getOptions from "next-common/services/chain/apis/options";
-import getChainSettings from "next-common/utils/consts/settings";
 
 const apiMap = new Map();
-
-function maybeSetHasher(chain, api) {
-  const { chainApi: { hasher } = {} } = getChainSettings(chain);
-  if (hasher) {
-    api.registry.setHasher(hasher);
-  }
-}
 
 async function newApiPromise(chain, endpoint) {
   const ApiPromise = (await import("@polkadot/api")).ApiPromise;
 
   const options = await getOptions(chain, endpoint);
-  const api = new ApiPromise(options);
-  maybeSetHasher(chain, api);
-  return api;
+  return new ApiPromise(options);
 }
 
 export default async function newApi(chain, endpoint) {
