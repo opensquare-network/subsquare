52debd5cb33072123c4dbf8af549f899f4dba8c1
quinn-gao <53474899+quinn-gaoo@users.noreply.github.com>
2025-10-21 17:49:45 +0800

    Add title attribute to the hash column on whitelist page and preimages page (#6771)
    * fix: copyable add title
    
    * fix: copyable add title

--- packages/next-common/components/preImages/fields.js
+++ packages/next-common/components/preImages/fields.js
@@ -12,8 +12,14 @@ export function Hash({ hash, proposal, setShowArgumentsDetail }) {
   return (
     <div className="flex justify-between">
       <div className="flex items-center">
-        <Copyable className="max-md:flex max-md:items-center" copyText={hash}>
-          <span className="text14Medium text-textPrimary inline-block w-[96px] truncate">
+        <Copyable
+          className="max-md:flex max-md:items-center flex items-center"
+          copyText={hash}
+        >
+          <span
+            title={hash}
+            className="text14Medium text-textPrimary inline-block w-[96px] truncate"
+          >
             {hash}
           </span>
         </Copyable>
