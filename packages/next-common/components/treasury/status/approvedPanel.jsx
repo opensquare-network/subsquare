091342194dc683012c58f2ab0803dbb3078f2fb3
wolyshaw <wolyshaw@users.noreply.github.com>
2025-10-28 15:23:30 +0800

    slim, #6705

--- packages/next-common/components/treasury/status/approvedPanel.jsx
+++ packages/next-common/components/treasury/status/approvedPanel.jsx
@@ -3,15 +3,21 @@ import ApprovedProposal from "./summarys/approvedProposal";
 import ApprovedSpend from "./summarys/approvedSpend";
 import { NeutralPanel } from "next-common/components/styled/containers/neutralPanel";
 import SpendPeriod from "./summarys/spendPeriod";
+import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 
 export default function ApprovedPanel() {
   return (
-    <NeutralPanel className="p-6 space-y-4">
-      <SummaryLayout>
-        <ApprovedSpend />
-        <ApprovedProposal />
-        <SpendPeriod />
-      </SummaryLayout>
-    </NeutralPanel>
+    <>
+      <TitleContainer className="justify-start">
+        <span>Approved</span>
+      </TitleContainer>
+      <NeutralPanel className="p-6 space-y-4">
+        <SummaryLayout>
+          <ApprovedSpend />
+          <ApprovedProposal />
+          <SpendPeriod />
+        </SummaryLayout>
+      </NeutralPanel>
+    </>
   );
 }
--- packages/next/pages/treasury/index.js
+++ packages/next/pages/treasury/index.js
@@ -5,7 +5,6 @@ import TreasuryStatusSummaryPanel from "next-common/components/treasury/status/s
 import TreasuryStatusTabContent from "next-common/components/treasury/status/tabContent";
 import ApprovedPanel from "next-common/components/treasury/status/approvedPanel";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
-import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
 
 const seoInfo = { title: "Treasury Status", desc: "Treasury Status" };
 
@@ -18,9 +17,6 @@ export default function TreasuryStatusPage() {
         summary={<TreasuryStatusSummaryPanel />}
       >
         <div className="flex flex-col gap-y-4">
-          <TitleContainer className="justify-start">
-            <span>Approved</span>
-          </TitleContainer>
           <ApprovedPanel />
           <TreasuryStatusTabContent />
         </div>
