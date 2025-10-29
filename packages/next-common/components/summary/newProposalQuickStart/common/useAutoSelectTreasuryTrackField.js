e8bb01c6f50b05e8c7a26722fc8541047cd0c6ae
chaojun <chaojun@opensquare.network>
2025-10-29 16:23:49 +0800

    Handle case of over maximum value fellowship referenda can approve, #6796

--- packages/next-common/components/summary/newProposalQuickStart/common/useAutoSelectTreasuryTrackField.js
+++ packages/next-common/components/summary/newProposalQuickStart/common/useAutoSelectTreasuryTrackField.js
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { usePageProps } from "next-common/context/page";
 import AutoSelectTreasuryTrack from "next-common/components/popup/fields/autoSelectTreasuryTrack";
 import { isCollectivesChain } from "next-common/utils/chain";
-import { useChain } from "next-common/context/chain";
+import { useChain, useChainSettings } from "next-common/context/chain";
+
+export const AutoSelectTreasuryTrackErrors = {
+  OverMax: "OverMax",
+};
 
 export default function useAutoSelectTreasuryTrackField(
   requestAmount,
@@ -10,6 +14,7 @@ export default function useAutoSelectTreasuryTrackField(
 ) {
   const chain = useChain();
   const { tracks: referendaTracks, fellowshipTracks } = usePageProps();
+  const { treasuryProposalTracks } = useChainSettings();
   let tracks = referendaTracks;
   if (isCollectivesChain(chain)) {
     tracks = fellowshipTracks;
@@ -17,8 +22,21 @@ export default function useAutoSelectTreasuryTrackField(
   const defaultTrackId = defaultValue || tracks[0]?.id;
   const [trackId, setTrackId] = useState(defaultTrackId);
 
+  const error = useMemo(() => {
+    if (
+      requestAmount &&
+      !treasuryProposalTracks.some(
+        (track) => track.max >= parseFloat(requestAmount),
+      )
+    ) {
+      return AutoSelectTreasuryTrackErrors.OverMax;
+    }
+    return null;
+  }, [treasuryProposalTracks, requestAmount]);
+
   return {
     value: trackId,
+    error,
     component: (
       <AutoSelectTreasuryTrack
         requestAmount={requestAmount}
