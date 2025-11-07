9490f807284222af8700eac397c26384081b64c8
wolyshaw <wolyshaw@users.noreply.github.com>
2025-11-07 17:05:00 +0800

    Correct treasury status after AHM, #6823 (#6825)
    * improve summary item, #6823
    
    * slim, #6823
    
    * slim, #6823
    
    * fix total, #6823
    
    * rename, #6823
    
    * return account, #6823

--- packages/next-common/components/summary/kusamaTreasurySummary/treasuryStatus/index.jsx
+++ packages/next-common/components/summary/kusamaTreasurySummary/treasuryStatus/index.jsx
@@ -1,19 +1,17 @@
 import LoadableContent from "next-common/components/common/loadableContent";
 import SummaryItem from "next-common/components/summary/layout/item";
 import SummaryLabelItem from "next-common/components/summary/polkadotTreasurySummary/common/summaryLabelItem";
-import SpendPeriodCountdown from "next-common/components/summary/polkadotTreasurySummary/relayChainTreasury/spendPeriodCountdown";
-import ToBeAwarded from "next-common/components/summary/polkadotTreasurySummary/relayChainTreasury/toBeAwarded";
+import SpendPeriodCountdown from "next-common/components/summary/polkadotTreasurySummary/treasuryStatus/spendPeriodCountdown";
+import ToBeAwarded from "next-common/components/summary/polkadotTreasurySummary/treasuryStatus/toBeAwarded";
 import useSpendPeriodSummary from "next-common/components/summary/treasurySummary/useSpendPeriodSummary";
 import { isNil } from "lodash-es";
 import useToBeAwarded from "next-common/hooks/useToBeAwarded";
-import NextBurn from "next-common/components/summary/polkadotTreasurySummary/relayChainTreasury/nextBurn";
-import SpendPeriod from "next-common/components/summary/polkadotTreasurySummary/relayChainTreasury/spendPeriod";
-import FiatPriceLabel from "../common/fiatPriceLabel";
-import NativeTokenSymbolAsset from "next-common/components/summary/polkadotTreasurySummary/common/nativeTokenSymbolAsset";
+import NextBurn from "next-common/components/summary/polkadotTreasurySummary/treasuryStatus/nextBurn";
+import SpendPeriod from "next-common/components/summary/polkadotTreasurySummary/treasuryStatus/spendPeriod";
 import { useKusamaTreasuryContext } from "next-common/context/treasury/kusamaTreasury";
 import SpendPeriodLabelTip from "../../labelTips/spendPeriod";
 
-export default function RelayChainTreasury() {
+export default function TreasuryStatus() {
   const {
     nativeTreasuryBalanceOnRelayChain,
     isNativeTreasuryBalanceOnRelayChainLoading,
@@ -23,7 +21,7 @@ export default function RelayChainTreasury() {
   const toBeAwarded = useToBeAwarded();
 
   return (
-    <SummaryItem title="Relay Chain">
+    <SummaryItem title="Treasury Status">
       <LoadableContent
         isLoading={
           isNativeTreasuryBalanceOnRelayChainLoading ||
@@ -32,13 +30,7 @@ export default function RelayChainTreasury() {
         }
       >
         <div className="flex flex-col gap-[4px]">
-          <FiatPriceLabel free={nativeTreasuryBalanceOnRelayChain} />
           <div className="!ml-0 flex flex-col gap-y-1">
-            <div>
-              <NativeTokenSymbolAsset
-                free={nativeTreasuryBalanceOnRelayChain}
-              />
-            </div>
             <SummaryLabelItem label="To be awarded">
               <ToBeAwarded toBeAwarded={toBeAwarded} />
             </SummaryLabelItem>
