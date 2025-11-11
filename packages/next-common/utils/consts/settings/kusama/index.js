1fe0c7e29bd9b5446386f801d218146f7e4c756a
leochen <leochen.sat24@gmail.com>
2025-11-11 17:20:22 +0800

    Improve polkadot/kusama multisigs import by calling relay explorer API (#6847)
    * Optimize multisigs import for polkadot&kusama after AHM
    
    * judgement
    
    * judgement fix
    
    * rename

--- packages/next-common/utils/consts/settings/kusama/index.js
+++ packages/next-common/utils/consts/settings/kusama/index.js
@@ -51,6 +51,7 @@ const kusama = {
   useVoteCall: true,
   hasMultisig: true,
   multisigApiPrefix: "statemine-gh-api",
+  relayChainMultisigApiPrefix: "ksm-gh-api",
   description:
     "Kusama is a scalable multi-chain network for radical innovation and early stage Polkadot deployments. Expect Chaos. No promises.",
   modules: mergeChainModules({
