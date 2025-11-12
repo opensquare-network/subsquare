9b11abaa2be61be96a9cd789239a3d6ea448ac1c
chaojun <chaojun@opensquare.network>
2025-11-12 09:48:11 +0800

    Add supply staked data, #6777

--- packages/next-common/components/staking/overview/summary.jsx
+++ packages/next-common/components/staking/overview/summary.jsx
@@ -3,9 +3,20 @@ import SummaryItem from "next-common/components/summary/layout/item";
 import { useEraTimeLeft } from "next-common/hooks/staking/useEraTimeLeft";
 import LoadableContent from "next-common/components/common/loadableContent";
 import Duration from "next-common/components/duration";
+import { useTotalStake } from "next-common/hooks/staking/useTotalStake";
+import { useTotalIssuance } from "next-common/hooks/staking/useTotalIssuance";
 
 export default function StakingOverviewSummary() {
-  const { loading, end } = useEraTimeLeft();
+  const { loading: loadingEraTimeLeft, end } = useEraTimeLeft();
+  const { loading: loadingTotalStake, totalStake } = useTotalStake();
+  const { loading: loadingIssuance, totalIssuance } = useTotalIssuance();
+  let totalStakePercentage = 0;
+  if (!loadingTotalStake && !loadingIssuance) {
+    totalStakePercentage = (
+      (Number(totalStake) / Number(totalIssuance)) *
+      100
+    ).toFixed(2);
+  }
 
   return (
     <SummaryLayout>
@@ -13,10 +24,15 @@ export default function StakingOverviewSummary() {
         <span>{0}</span>
       </SummaryItem>
       <SummaryItem title="Supply Staked">
-        <span>{0}</span>
+        <LoadableContent
+          isLoading={loadingTotalStake || loadingIssuance}
+          size={16}
+        >
+          <span>{totalStakePercentage}%</span>
+        </LoadableContent>
       </SummaryItem>
       <SummaryItem title="Next Reward Distribution">
-        <LoadableContent isLoading={loading} size={16}>
+        <LoadableContent isLoading={loadingEraTimeLeft} size={16}>
           <Duration time={end} />
         </LoadableContent>
       </SummaryItem>
