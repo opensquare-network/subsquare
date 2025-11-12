98c2b103e0d3e48aaee713541f6f203632666703
quinn-gao <quinnn.gao@gmail.com>
2025-11-12 14:56:29 +0800

    fix: optimize the logic of get relay chain endpoints

--- packages/next-common/context/relayChain/index.js
+++ packages/next-common/context/relayChain/index.js
@@ -1,20 +1,27 @@
 import { useRelayChain } from "next-common/hooks/useRelayChain";
 import getChainSettings from "next-common/utils/consts/settings";
 import { getChainApi } from "next-common/utils/getChainApi";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 
 const RelayChainApiContext = createContext(null);
 
 export function RelayChainApiProvider({ children }) {
   const [api, setApi] = useState(null);
   const relayChain = useRelayChain();
-  const relayChainSettings = getChainSettings(relayChain);
+
+  const endpoints = useMemo(() => {
+    const relayChainSettings = getChainSettings(relayChain);
+
+    return (
+      relayChainSettings?.relayChainEndpoints ||
+      relayChainSettings?.endpoints ||
+      []
+    );
+  }, [relayChain]);
 
   useEffect(() => {
-    getChainApi(relayChainSettings?.endpoints?.map?.((item) => item.url)).then(
-      setApi,
-    );
-  }, [relayChainSettings.endpoints]);
+    getChainApi(endpoints.map?.((item) => item.url)).then(setApi);
+  }, [endpoints]);
 
   return (
     <RelayChainApiContext.Provider value={api}>
