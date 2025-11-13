cddf028bef33f0f35ccdb915e9f9623f1980b1e8
leochen <leochen.sat24@gmail.com>
2025-11-13 14:59:51 +0800

    add native asset transfer column, #6854

--- packages/next-common/components/assethubMigrationAssets/nativeAsset/nativeAssetList.jsx
+++ packages/next-common/components/assethubMigrationAssets/nativeAsset/nativeAssetList.jsx
@@ -6,10 +6,11 @@ import {
 } from "next-common/components/assethubMigrationAssets/assetsList";
 import { useMyNativeAsset } from "next-common/hooks/assets/useMyNativeAsset";
 import { colToken } from "./columns/token";
+import { colTransfer } from "./columns/transfer";
 
 export default function NativeAssetList() {
   const { loading, value } = useMyNativeAsset();
-  const columnsDef = [colToken, colTotal, colTransferrable];
+  const columnsDef = [colToken, colTotal, colTransferrable, colTransfer];
 
   return (
     <div className="relative">
