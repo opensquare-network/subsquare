a4dbd1a62e1840ae3b0c6e4fcb97c01c41b34c0e
leochen <leochen.sat24@gmail.com>
2025-11-17 18:27:43 +0800

    add loading for ExistentialDeposit, #6858

--- packages/next-common/components/popup/fields/existentialDepositField.jsx
+++ packages/next-common/components/popup/fields/existentialDepositField.jsx
@@ -2,20 +2,27 @@ import PopupLabel from "../label";
 import CurrencyInput from "next-common/components/currencyInput";
 import { toPrecision } from "next-common/utils";
 import useApiProperties from "next-common/hooks/useApiProperties";
+import FieldLoading from "next-common/components/icons/fieldLoading";
 
 export default function ExistentialDeposit({ destApi, title }) {
-  const { decimals } = useApiProperties(destApi);
+  const { decimals, isLoading } = useApiProperties(destApi);
+  const loading = isLoading || !destApi;
+
   return (
     <div>
       <PopupLabel text={title || "Destination Existential Deposit"} />
-      <CurrencyInput
-        disabled
-        value={toPrecision(
-          destApi?.consts.balances?.existentialDeposit || 0,
-          decimals,
-        )}
-        symbol="DOT"
-      />
+      {loading ? (
+        <FieldLoading />
+      ) : (
+        <CurrencyInput
+          disabled
+          value={toPrecision(
+            destApi?.consts.balances?.existentialDeposit || 0,
+            decimals,
+          )}
+          symbol="DOT"
+        />
+      )}
     </div>
   );
 }
