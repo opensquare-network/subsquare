8451992f3bd9e7c9ec4fa677d793385fce3fd725
wolyshaw <wolyshaw@users.noreply.github.com>
2025-11-18 17:53:40 +0800

    fix fellowship statistics member

--- packages/collectives-next/pages/fellowship/statistics/index.jsx
+++ packages/collectives-next/pages/fellowship/statistics/index.jsx
@@ -3,6 +3,11 @@ import { withCommonProps } from "next-common/lib";
 import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 import FellowshipCollectivesStatistics from "next-common/components/fellowship/statistics/collectives";
 import CollectivesProvider from "next-common/context/collectives/collectives";
+import {
+  ambassadorMembersApiUri,
+  fellowshipMembersApiUri,
+} from "next-common/services/url";
+import { backendApi } from "next-common/services/nextApi";
 
 export default function FellowshipStatisticsPage() {
   const category = "Fellowship Statistics";
@@ -18,11 +23,21 @@ export default function FellowshipStatisticsPage() {
 }
 
 export const getServerSideProps = withCommonProps(async () => {
-  const tracksProps = await fetchOpenGovTracksProps();
+  const [
+    tracksProps,
+    { result: fellowshipMembers },
+    { result: ambassadorMembers },
+  ] = await Promise.all([
+    fetchOpenGovTracksProps(),
+    backendApi.fetch(fellowshipMembersApiUri),
+    backendApi.fetch(ambassadorMembersApiUri),
+  ]);
 
   return {
     props: {
       ...tracksProps,
+      fellowshipMembers: fellowshipMembers ?? null,
+      ambassadorMembers: ambassadorMembers ?? null,
     },
   };
 });
