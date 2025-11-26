f20ed68fae64f9a802c7f0243447f9da21aa2e78
leochen <leochen.sat24@gmail.com>
2025-11-26 18:29:16 +0800

    Apply new multisig api for bounty curator

--- packages/next-common/hooks/useMultisigAddress.js
+++ packages/next-common/hooks/useMultisigAddress.js
@@ -2,6 +2,16 @@ import { backendApi } from "next-common/services/nextApi";
 import { useAsync } from "react-use";
 import { useIsRelativesApiAvailable } from "next-common/hooks/useIsPureProxy";
 
+export async function fetchMultisigAddress(address) {
+  try {
+    const { result } = await backendApi.fetch(`/multisig/addresses/${address}`);
+
+    return result ?? null;
+  } catch (error) {
+    return null;
+  }
+}
+
 export default function useMultisigAddress(address) {
   const isRelativesApiAvailable = useIsRelativesApiAvailable(address);
 
@@ -10,15 +20,8 @@ export default function useMultisigAddress(address) {
       return false;
     }
 
-    try {
-      const { result } = await backendApi.fetch(
-        `/multisig/addresses/${address}`,
-      );
-
-      return result ?? null;
-    } catch (error) {
-      return null;
-    }
+    const result = await fetchMultisigAddress(address);
+    return result ?? null;
   }, [address, isRelativesApiAvailable]);
 
   return { result, loading };
