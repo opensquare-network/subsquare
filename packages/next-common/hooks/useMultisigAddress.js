5be4227b3646440088aef0660285ffd7c58b78a5
leochen <leochen.sat24@gmail.com>
2025-11-27 14:05:31 +0800

    Fix mimir link judgement

--- packages/next-common/hooks/useMultisigAddress.js
+++ packages/next-common/hooks/useMultisigAddress.js
@@ -1,6 +1,41 @@
 import { backendApi } from "next-common/services/nextApi";
 import { useAsync } from "react-use";
 import { useIsRelativesApiAvailable } from "next-common/hooks/useIsPureProxy";
+import { gql } from "@apollo/client";
+import { useMultisigQuery } from "next-common/hooks/apollo";
+import { useEffect, useState } from "react";
+
+const GET_MULTISIG_ADDRESS = gql`
+  query GetMultisigAddress($account: String!) {
+    multisigAddress(account: $account) {
+      signatories
+      threshold
+    }
+  }
+`;
+
+export function useMultisigAddressFromGraphql(address) {
+  const [result, setResult] = useState(null);
+  const { data, loading } = useMultisigQuery(GET_MULTISIG_ADDRESS, {
+    variables: {
+      account: address,
+    },
+  });
+
+  useEffect(() => {
+    if (loading || !address) {
+      return;
+    }
+
+    const { multisigAddress } = data || {};
+    setResult(multisigAddress);
+  }, [address, data, loading]);
+
+  return {
+    result,
+    loading,
+  };
+}
 
 export async function fetchMultisigAddress(address) {
   try {
@@ -17,7 +52,7 @@ export default function useMultisigAddress(address) {
 
   const { value: result, loading } = useAsync(async () => {
     if (!isRelativesApiAvailable) {
-      return false;
+      return null;
     }
 
     const result = await fetchMultisigAddress(address);
