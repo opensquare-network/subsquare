466d890b8896c7ec3cbec9ec9609f1e4188c9fd4
leochen <leochen.sat24@gmail.com>
2025-11-28 18:36:04 +0800

    Move calendar to the advanced menu, #6923

---
+++ packages/next-common/utils/consts/menu/calendar.js
@@ -0,0 +1,8 @@
+import { MenuCalendar } from "@osn/icons/subsquare";
+
+export const calendarMenu = {
+  value: "calendar",
+  name: "Calendar",
+  pathname: "/calendar",
+  icon: <MenuCalendar />,
+};
