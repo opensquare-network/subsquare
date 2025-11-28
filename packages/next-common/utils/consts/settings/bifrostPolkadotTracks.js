1b5a67912dd95b223412b4107e6235836b888a59
leochen <leochen.sat24@gmail.com>
2025-11-28 14:39:00 +0800

    Default root, #6918

--- packages/next-common/utils/consts/settings/bifrostPolkadotTracks.js
+++ packages/next-common/utils/consts/settings/bifrostPolkadotTracks.js
@@ -1,12 +1,12 @@
 const bifrostPolkadotTracks = [
   {
-    id: 0,
-    name: "Root",
-  },
-  {
     id: 13,
     name: "Treasury Spend",
     max: 100000,
+  },
+  {
+    id: 0,
+    name: "Root",
   },
 ];
 
