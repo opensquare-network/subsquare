723dbf5b3630a5017a5a97a98c1655d221c249cd
leochen <leochen.sat24@gmail.com>
2025-11-28 14:32:38 +0800

    AutoSelectTreasuryTrack for bifrost, #6918

---
+++ packages/next-common/utils/consts/settings/bifrostPolkadotTracks.js
@@ -0,0 +1,13 @@
+const bifrostPolkadotTracks = [
+  {
+    id: 0,
+    name: "Root",
+  },
+  {
+    id: 13,
+    name: "Treasury Spend",
+    max: 100000,
+  },
+];
+
+export default bifrostPolkadotTracks;
