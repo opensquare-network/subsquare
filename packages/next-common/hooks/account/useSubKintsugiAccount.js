e313e511388b47b9194e7f200ab61aa1886b4f55
chaojun <chaojun@opensquare.network>
2025-12-01 15:28:07 +0800

    fix: recursive call

--- packages/next-common/hooks/account/useSubKintsugiAccount.js
+++ packages/next-common/hooks/account/useSubKintsugiAccount.js
@@ -1,12 +1,17 @@
 import { useSymbol } from "next-common/context/chain";
 import useSubStorage from "../common/useSubStorage";
+import { useMemo } from "react";
 
 export default function useSubKintsugiAccount(address) {
   const symbol = useSymbol();
 
+  const tokenSymbol = useMemo(() => {
+    return { token: symbol };
+  }, [symbol]);
+
   const { result, loading } = useSubStorage("tokens", "accounts", [
     address,
-    { token: symbol },
+    tokenSymbol,
   ]);
 
   return { data: result, isLoading: loading };
