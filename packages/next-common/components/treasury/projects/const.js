5b16e22a5ac096232726f956a1eee87b8966c524
wolyshaw <wolyshaw@users.noreply.github.com>
2025-12-02 09:54:36 +0800

    Add fund statistics of Event #6931

--- packages/next-common/components/treasury/projects/const.js
+++ packages/next-common/components/treasury/projects/const.js
@@ -15,6 +15,7 @@ export const CATEGORY_VALUES = {
   GOVERNANCE_PLATFORM: "governance_platform",
   POLKADOT_CLIENT: "polkadot_client",
   MARKETING: "marketing",
+  EVENT: "event",
 };
 
 export const DOUGHNUT_CONFIG_BY_CATEGORY = {
@@ -46,6 +47,7 @@ export const GRID_LABELS = {
 
 export const HORIZONTAL_LABELS = {
   [CATEGORY_VALUES.MARKETING]: "Marketing",
+  [CATEGORY_VALUES.EVENT]: "Events",
 };
 
 export const LABELS = {
