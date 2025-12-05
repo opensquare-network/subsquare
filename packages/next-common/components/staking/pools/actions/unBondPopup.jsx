ecb99cead6455821d799d05c8418315a055f4eed
Chaojun Huang <chaojun@opensquare.network>
2025-12-05 17:39:08 +0800

    Improve unbond pool popup, #6777 (#6962)

--- packages/next-common/components/staking/pools/actions/unBondPopup.jsx
+++ packages/next-common/components/staking/pools/actions/unBondPopup.jsx
@@ -5,7 +5,7 @@ import Signer from "next-common/components/popup/fields/signerField";
 import { useContextApi } from "next-common/context/api";
 import { useChainSettings } from "next-common/context/chain";
 import { useMemo, useState } from "react";
-import BalanceField from "next-common/components/popup/fields/balanceField";
+import AmountInputWithHint from "next-common/components/popup/fields/amountInputWithHint";
 import { useTxBuilder } from "next-common/hooks/useTxBuilder";
 import SecondaryButton from "next-common/lib/button/secondary";
 import { usePopupParams } from "next-common/components/popupWithSigner/context";
@@ -16,6 +16,7 @@ import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import BigNumber from "bignumber.js";
 import { toPrecision } from "next-common/utils";
 import { useMyPool } from "next-common/context/staking/myPool";
+import ErrorMessage from "next-common/components/styled/errorMessage";
 
 function UnBondPopupContent() {
   const { onClose } = usePopupParams();
@@ -23,8 +24,13 @@ function UnBondPopupContent() {
   const [amount, setAmount] = useState();
   const { decimals, symbol } = useChainSettings();
   const realAddress = useRealAddress();
-  const { poolMember } = useMyPool();
+  const { poolMember, loading } = useMyPool();
   const bonded = useMemo(() => poolMember?.points || 0, [poolMember?.points]);
+
+  const exceedMax =
+    amount &&
+    bonded &&
+    BigNumber(amount).gt(BigNumber(bonded).div(Math.pow(10, decimals)));
 
   const { getTxFuncForSubmit, getTxFuncForFee } = useTxBuilder(
     (toastError) => {
@@ -54,24 +60,30 @@ function UnBondPopupContent() {
 
   return (
     <div className="space-y-4">
-      <Signer
-        noSwitchSigner
-        balanceName="Bonded"
-        balance={bonded}
-        isBalanceLoading={false}
-      />
-      <BalanceField
+      <Signer noSwitchSigner />
+      <AmountInputWithHint
         title="Amount"
-        inputBalance={amount}
-        setInputBalance={setAmount}
+        hintLabel="Bonded"
+        maxAmount={bonded}
+        inputAmount={amount}
+        setInputAmount={setAmount}
+        isLoading={loading}
+        decimals={decimals}
         symbol={symbol}
+        hintTooltip={`${BigNumber(bonded)
+          .div(Math.pow(10, decimals))
+          .toString()} ${symbol} is bonded.`}
       />
+      {exceedMax && <ErrorMessage>Amount exceeds bonded balance</ErrorMessage>}
       <AdvanceSettings>
         <EstimatedGas getTxFunc={getTxFuncForFee} />
       </AdvanceSettings>
       <div className="flex justify-between">
         <SecondaryButton onClick={onClose}>Cancel</SecondaryButton>
-        <TxSubmissionButton getTxFunc={getTxFuncForSubmit} />
+        <TxSubmissionButton
+          getTxFunc={getTxFuncForSubmit}
+          disabled={exceedMax}
+        />
       </div>
     </div>
   );
