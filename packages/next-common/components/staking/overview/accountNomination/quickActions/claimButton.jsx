afa64fc17485bd7034ceee89579900f5a7587bc7
leochen <leochen.sat24@gmail.com>
2025-12-09 16:11:54 +0800

    New nominator status claim、payee icon, #6777 (#6983)

--- packages/next-common/components/staking/overview/accountNomination/quickActions/claimButton.jsx
+++ packages/next-common/components/staking/overview/accountNomination/quickActions/claimButton.jsx
@@ -1,12 +1,10 @@
 import Tooltip from "next-common/components/tooltip";
 import { IconButton } from "next-common/components/styled/iconButton";
-import { TrackPromotion } from "@osn/icons/subsquare";
 import { useState } from "react";
 import dynamicPopup from "next-common/lib/dynamic/popup";
+import { SystemTop } from "@osn/icons/subsquare";
 
-const ClaimPayoutPopup = dynamicPopup(() =>
-  import("./claimPopup"),
-);
+const ClaimPayoutPopup = dynamicPopup(() => import("./claimPopup"));
 
 export default function ClaimButton() {
   const [showPopup, setShowPopup] = useState(false);
@@ -18,7 +16,7 @@ export default function ClaimButton() {
           className="text-theme500 bg-theme100"
           onClick={() => setShowPopup(true)}
         >
-          <TrackPromotion className="w-5 h-5" />
+          <SystemTop className="w-5 h-5 rotate-180" />
         </IconButton>
       </Tooltip>
 
