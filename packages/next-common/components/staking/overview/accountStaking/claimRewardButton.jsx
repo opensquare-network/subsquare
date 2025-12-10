a4c35d0d9d46f2a6e4d05e3df84edeb67335fe25
Chaojun Huang <chaojun@opensquare.network>
2025-12-10 13:36:31 +0800

    Update reward layout, #6777 (#6989)
    * Update reward layout, #6777
    
    * Add action tooltip, #6777

--- packages/next-common/components/staking/overview/accountStaking/claimRewardButton.jsx
+++ packages/next-common/components/staking/overview/accountStaking/claimRewardButton.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import dynamicPopup from "next-common/lib/dynamic/popup";
+import Tooltip from "next-common/components/tooltip";
 
 const ClaimPoolRewardPopup = dynamicPopup(() =>
   import(
@@ -12,13 +13,15 @@ export default function ClaimPoolRewardButton() {
 
   return (
     <>
-      <div
-        role="button"
-        className="text-theme500 text12Medium cursor-pointer"
-        onClick={() => setShowClaimPopup(true)}
-      >
-        Claim
-      </div>
+      <Tooltip content="Claim your staking rewards from the pool to your account">
+        <div
+          role="button"
+          className="text-theme500 text12Medium cursor-pointer"
+          onClick={() => setShowClaimPopup(true)}
+        >
+          Claim
+        </div>
+      </Tooltip>
       {showClaimPopup && (
         <ClaimPoolRewardPopup onClose={() => setShowClaimPopup(false)} />
       )}
--- packages/next-common/components/staking/overview/accountStaking/compoundRewardButton.jsx
+++ packages/next-common/components/staking/overview/accountStaking/compoundRewardButton.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import dynamicPopup from "next-common/lib/dynamic/popup";
+import Tooltip from "next-common/components/tooltip";
 
 const CompoundPoolRewardPopup = dynamicPopup(() =>
   import(
@@ -12,13 +13,15 @@ export default function CompoundPoolRewardButton() {
 
   return (
     <>
-      <div
-        role="button"
-        className="text-theme500 text12Medium cursor-pointer"
-        onClick={() => setShowCompoundPopup(true)}
-      >
-        Compound
-      </div>
+      <Tooltip content="Compound your staking rewards back into the staking pool">
+        <div
+          role="button"
+          className="text-theme500 text12Medium cursor-pointer"
+          onClick={() => setShowCompoundPopup(true)}
+        >
+          Compound
+        </div>
+      </Tooltip>
       {showCompoundPopup && (
         <CompoundPoolRewardPopup onClose={() => setShowCompoundPopup(false)} />
       )}
