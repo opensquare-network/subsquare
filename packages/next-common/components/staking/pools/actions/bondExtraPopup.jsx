cd92c4c01af720aeb92ccc2cf0e7f9f73b8829ec
Chaojun Huang <chaojun@opensquare.network>
2025-12-10 13:36:40 +0800

    Remove bond reward from bond extra popup, #6777 (#6991)

--- packages/next-common/components/staking/pools/actions/bondExtraPopup.jsx
+++ packages/next-common/components/staking/pools/actions/bondExtraPopup.jsx
@@ -3,113 +3,80 @@ import Popup from "next-common/components/popup/wrapper/Popup";
 import SignerPopupWrapper from "next-common/components/popupWithSigner/signerPopupWrapper";
 import { useContextApi } from "next-common/context/api";
 import { useChainSettings } from "next-common/context/chain";
-import { useMinJoin } from "next-common/components/staking/pools/hooks/useMinJoin";
-import { toPrecision } from "next-common/utils";
-import { useState } from "react";
-import BalanceField from "next-common/components/popup/fields/balanceField";
-import { useTxBuilder } from "next-common/hooks/useTxBuilder";
+import { useCallback, useState } from "react";
+import AmountInputWithHint from "next-common/components/popup/fields/amountInputWithHint";
 import SecondaryButton from "next-common/lib/button/secondary";
 import { usePopupParams } from "next-common/components/popupWithSigner/context";
 import AdvanceSettings from "next-common/components/summary/newProposalQuickStart/common/advanceSettings";
 import EstimatedGas from "next-common/components/estimatedGas";
 import BigNumber from "bignumber.js";
 import { checkTransferAmount } from "next-common/utils/checkTransferAmount";
-import CommonSelectField from "next-common/components/popup/fields/commonSelectField";
 import useAccountTransferrable from "next-common/hooks/useAccountTransferrable";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import Signer from "next-common/components/popup/fields/signerField";
-
-const FREE_BALANCE_TYPE = "FreeBalance";
-const REWARDS_TYPE = "Rewards";
-
-const TYPE_OPTIONS = [
-  {
-    label: "Free Balance",
-    value: FREE_BALANCE_TYPE,
-  },
-  {
-    label: "Rewards",
-    value: REWARDS_TYPE,
-  },
-];
+import ErrorMessage from "next-common/components/styled/errorMessage";
 
 function BondExtraPopupContent() {
   const { onClose } = usePopupParams();
   const api = useContextApi();
   const realAddress = useRealAddress();
-  const { minJoinBond, loading: minJoinBondLoading } = useMinJoin();
   const [amount, setAmount] = useState();
   const { decimals, symbol } = useChainSettings();
-  const [type, setType] = useState(TYPE_OPTIONS[0].value);
 
   const { transferrable, isLoading: isLoadingTransferrable } =
     useAccountTransferrable(api, realAddress);
 
-  const { getTxFuncForSubmit, getTxFuncForFee } = useTxBuilder(
-    (toastError) => {
-      if (!api || !api.tx.nominationPools) {
-        return;
-      }
+  const exceedMax =
+    transferrable &&
+    amount &&
+    BigNumber(amount).gt(BigNumber(transferrable).div(Math.pow(10, decimals)));
 
-      const checkedAmount = checkTransferAmount({
-        transferAmount: amount,
-        decimals,
-        transferrable,
-      });
+  let errorMessage = "";
+  if (exceedMax) {
+    errorMessage = "Input amount exceeds available balance.";
+  }
 
-      if (BigNumber(checkedAmount).lt(minJoinBond)) {
-        toastError(
-          `Amount must be greater than the min bond extra of ${toPrecision(
-            minJoinBond,
-            decimals,
-          )} ${symbol}`,
-        );
-        return;
-      }
+  const getTxFunc = useCallback(() => {
+    if (!api || !api.tx.nominationPools) {
+      return;
+    }
 
-      const extra =
-        type === FREE_BALANCE_TYPE
-          ? {
-              FreeBalance: checkedAmount,
-            }
-          : "Rewards";
+    const checkedAmount = checkTransferAmount({
+      transferAmount: amount,
+      decimals,
+      transferrable,
+    });
 
-      return api.tx.nominationPools.bondExtra(extra);
-    },
-    [api, amount, minJoinBond, type],
-  );
+    return api.tx.nominationPools.bondExtra({
+      FreeBalance: checkedAmount,
+    });
+  }, [api, amount, decimals, transferrable]);
 
   return (
     <div className="space-y-4">
-      <Signer
-        title="Origin"
-        balance={transferrable}
-        isBalanceLoading={isLoadingTransferrable}
-        noSwitchSigner
-        showTransferable
+      <Signer title="Origin" noSwitchSigner />
+      <AmountInputWithHint
+        title="Amount"
+        hintLabel="Available"
+        maxAmount={transferrable}
+        inputAmount={amount}
+        setInputAmount={setAmount}
+        isLoading={isLoadingTransferrable}
+        decimals={decimals}
+        symbol={symbol}
+        hintTooltip={`${BigNumber(transferrable)
+          .div(Math.pow(10, decimals))
+          .toString()} ${symbol} is available.`}
       />
-      <CommonSelectField
-        title="Type"
-        options={TYPE_OPTIONS}
-        value={type}
-        setValue={(value) => setType(value)}
-      />
-      {type === FREE_BALANCE_TYPE && (
-        <BalanceField
-          title="Amount"
-          inputBalance={amount}
-          setInputBalance={setAmount}
-          symbol={symbol}
-        />
-      )}
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
       <AdvanceSettings>
-        <EstimatedGas getTxFunc={getTxFuncForFee} />
+        <EstimatedGas getTxFunc={getTxFunc} />
       </AdvanceSettings>
       <div className="flex justify-between">
         <SecondaryButton onClick={onClose}>Cancel</SecondaryButton>
         <TxSubmissionButton
-          loading={minJoinBondLoading}
-          getTxFunc={getTxFuncForSubmit}
+          disabled={isLoadingTransferrable || exceedMax}
+          getTxFunc={getTxFunc}
         />
       </div>
     </div>
