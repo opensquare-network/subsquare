89e25016e92d7543c54528a34ca9cae91a5b2025
chaojun <chaojun@opensquare.network>
2025-12-12 13:58:03 +0800

    Improve default treasury proposal title

--- packages/next-common/utils/gov2/title.js
+++ packages/next-common/utils/gov2/title.js
@@ -10,12 +10,3 @@ export function getGov2ReferendumTitle(detail) {
   const parsedTrackName = startCase(trackName);
   return `[${parsedTrackName}] Referendum #${detail?.referendumIndex}`;
 }
-
-export function getGov2TreasuryProposalTitle(detail) {
-  const title = detail?.title?.trim();
-  if (title) {
-    return title;
-  }
-
-  return `Treasury proposal #${detail?.proposalIndex}`;
-}
