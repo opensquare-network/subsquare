e022630f502419796014f041a4474c37c9384eda
chaojun <chaojun@opensquare.network>
2025-12-12 10:23:44 +0800

    fix: icon style, #6777

--- packages/next-common/components/staking/overview/accountNomination/checkNomineesButton.jsx
+++ packages/next-common/components/staking/overview/accountNomination/checkNomineesButton.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import dynamicPopup from "next-common/lib/dynamic/popup";
 import { IconButton } from "next-common/components/styled/iconButton";
-import { SystemMenu } from "@osn/icons/subsquare";
+import { MenuTreasuryProposal } from "@osn/icons/subsquare";
 import Tooltip from "next-common/components/tooltip";
 
 const NomineeListPopup = dynamicPopup(() =>
@@ -17,10 +17,10 @@ export default function CheckNomineesButton({ nominator }) {
     <>
       <Tooltip content="Show Nominees">
         <IconButton
-          className="text-theme500 bg-theme100 [&_svg_path]:stroke-theme500"
+          className="text-theme500 bg-theme100 [&_svg_path]:fill-theme500"
           onClick={() => setShowNomineePopup(true)}
         >
-          <SystemMenu className="w-5 h-5" />
+          <MenuTreasuryProposal className="w-5 h-5" />
         </IconButton>
       </Tooltip>
       {showNomineePopup && (
