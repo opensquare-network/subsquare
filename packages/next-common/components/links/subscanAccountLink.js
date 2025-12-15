56e7ea4907e2642a139c7e256dcf0358ee8b929d
leochen <leochen.sat24@gmail.com>
2025-12-15 18:00:24 +0800

    Modify subscan link for AHM chains in profile

--- packages/next-common/components/links/subscanAccountLink.js
+++ packages/next-common/components/links/subscanAccountLink.js
@@ -1,20 +1,42 @@
 import { useChain, useChainSettings } from "next-common/context/chain";
 import IconLink from "./iconLink";
 import { LinkSubscan } from "@osn/icons/subsquare";
+import { isAssetHubMigrated } from "next-common/utils/consts/isAssetHubMigrated";
+import { isRelayChain } from "next-common/utils/chain";
+import { useMemo } from "react";
 
-export default function SubScanAccountLink({ address }) {
-  const chain = useChain();
+function SubScanAccountLinkWrapper({ children }) {
   const { integrations } = useChainSettings();
   if (!integrations?.subscan) {
     return null;
   }
 
-  const domain = integrations.subscan.domain || chain;
+  return children;
+}
+
+function SubScanAccountLinkImpl({ address }) {
+  const chain = useChain();
+  const { integrations } = useChainSettings();
+  const domain = useMemo(() => {
+    if (isRelayChain(chain) && isAssetHubMigrated()) {
+      return `assethub-${chain}`;
+    }
+
+    return integrations?.subscan?.domain || chain;
+  }, [chain, integrations]);
 
   return (
     <IconLink
       href={`https://${domain}.subscan.io/account/${address}`}
       icon={<LinkSubscan className="w-5 h-5" />}
     />
+  );
+}
+
+export default function SubScanAccountLink({ address }) {
+  return (
+    <SubScanAccountLinkWrapper>
+      <SubScanAccountLinkImpl address={address} />
+    </SubScanAccountLinkWrapper>
   );
 }
