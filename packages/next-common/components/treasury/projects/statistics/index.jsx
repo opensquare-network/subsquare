b5c048f8279649b575b815d5ce6d43ed3e7bffdb
wolyshaw <wolyshaw@users.noreply.github.com>
2025-12-17 15:48:27 +0800

    slim, #6800

--- packages/next-common/components/treasury/projects/statistics/index.jsx
+++ packages/next-common/components/treasury/projects/statistics/index.jsx
@@ -1,7 +1,5 @@
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import ProjectStatisticsChart from "./chart";
-// import { TitleContainer } from "next-common/components/styled/containers/titleContainer";
-// import Tooltip from "next-common/components/tooltip";
 import WindowSizeProvider from "next-common/context/windowSize";
 
 export default function Statistics({ data }) {
@@ -14,10 +12,7 @@ export default function Statistics({ data }) {
   return (
     <WindowSizeProvider>
       <SecondaryCard className="flex flex-col gap-y-4">
-        <div className="text-textPrimary text14Bold">
-          {label}
-          {/* <Tooltip content="The prices are calculated at awarded time."></Tooltip> */}
-        </div>
+        <div className="text-textPrimary text14Bold">{label}</div>
         <div className="flex gap-x-6 gap-y-4 justify-start w-full max-sm:flex-col">
           <ProjectStatisticsChart
             projects={projects}
