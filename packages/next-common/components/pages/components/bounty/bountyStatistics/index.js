955da6aadc288a91bb5f85a23d660a4a398b7695
chaojun <chaojun@opensquare.network>
2025-12-29 10:09:19 +0800

    fix mb layout, #7027

--- packages/next-common/components/pages/components/bounty/bountyStatistics/index.js
+++ packages/next-common/components/pages/components/bounty/bountyStatistics/index.js
@@ -4,7 +4,7 @@ import HorizontalTabs from "./horizontalTabs";
 export default function BountyStatistics() {
   return (
     <div className="space-y-4">
-      <div className="flex items-start">
+      <div className="sm:flex">
         <CuratorChart />
       </div>
       <HorizontalTabs />
