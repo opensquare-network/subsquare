3f7b2e43d09d369f65268d2e84fc86e614b10cbf
leochen <leochen.sat24@gmail.com>
2026-01-05 15:10:17 +0800

    Revert "new icon compoent, #7066"
    This reverts commit 0f6214377a3da80276f99dbdfe221825adaf2c9a.

--- packages/next-common/components/data/vesting/table/vestAction.jsx
+++ packages/next-common/components/data/vesting/table/vestAction.jsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import Unlock from "next-common/components/icons/unlock";
+import { SystemSignature } from "@osn/icons/subsquare";
 import { cn } from "next-common/utils";
 import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { useVestPopup } from "next-common/components/data/vesting/context/vestPopupContext";
@@ -29,8 +29,8 @@ function VestActionImpl({ account, unlockable }) {
             : "border-neutral400 cursor-pointer",
         )}
       >
-        <Unlock
-          className={cn("w-4 h-4 text-textSecondary", !hasUnlockable && "text-textDisabled")}
+        <SystemSignature
+          className={cn("w-4 h-4", !hasUnlockable && "text-textDisabled")}
         />
       </div>
     </Tooltip>
