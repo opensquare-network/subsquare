86ff526614aee9e9d278ab47ae95f0cc38f0ddd9
leochen <leochen.sat24@gmail.com>
2026-01-07 15:40:08 +0800

    remove useless code

--- packages/next-common/utils/hooks/useRealAddress.js
+++ packages/next-common/utils/hooks/useRealAddress.js
@@ -1,7 +1,6 @@
 import { useUser } from "next-common/context/user";
 
 export default function useRealAddress() {
-  return "15BWkPmv1fJkDZEFVZyNZa97ugTxcR61WRNSkTeA5M2xN1DP";
   const user = useUser();
   return user?.proxyAddress || user?.address;
 }
