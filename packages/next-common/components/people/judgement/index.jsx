cfd65e47de660c43de7ce4e1cd9d21b4220f4e8f
chaojun <chaojun@opensquare.network>
2026-01-07 13:31:03 +0800

    Implement non-login page, #6881

--- packages/next-common/components/people/judgement/index.jsx
+++ packages/next-common/components/people/judgement/index.jsx
@@ -2,14 +2,9 @@ import ListLayout from "next-common/components/layout/ListLayout";
 import ChainSocialLinks from "next-common/components/chain/socialLinks";
 import PeopleCommonProvider from "next-common/components/people/common/commonProvider";
 import generateLayoutRawTitle from "next-common/utils/generateLayoutRawTitle";
-import Email from "./email";
-import Discord from "./discord";
-import Twitter from "./twitter";
-import Github from "./github";
-import Element from "./element";
-import Loading from "next-common/components/loading";
-import useMyJudgementRequest from "../hooks/useMyJudgementRequest";
-import JudgementSummary from "./summary";
+import NoWalletConnected from "next-common/components/noWalletConnected";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
+import JudgementPageContent from "./content";
 
 export const tabs = [
   {
@@ -21,55 +16,23 @@ export const tabs = [
 ];
 
 export default function JudgementPage() {
-  const { value, loading: isLoadingMyJudgementRequest } =
-    useMyJudgementRequest();
-
-  const request = value?.items[0] || null;
+  const realAddress = useRealAddress();
 
   return (
     <PeopleCommonProvider>
       <ListLayout
-        title="Subsquare Judgement"
-        seoInfo={{ rawTitle: generateLayoutRawTitle("Subsquare Judgement") }}
-        description={"Subsquare judgement"}
+        title="Identity Judgement"
+        seoInfo={{ rawTitle: generateLayoutRawTitle("Identity Judgement") }}
+        description={"My identity judgement request."}
         headContent={<ChainSocialLinks />}
       >
-        <JudgementSummary request={request} />
-        <div className="pt-4 grid grid-cols-1 gap-4">
-          {isLoadingMyJudgementRequest && !request ? (
-            <div className="p-4 flex justify-center">
-              <Loading size="24" />
-            </div>
-          ) : (
-            <>
-              {request?.info?.email && (
-                <div className="bg-neutral100 border-b border-neutral300 p-4 rounded-lg flex">
-                  <Email request={request} />
-                </div>
-              )}
-              {request?.info?.element && (
-                <div className="bg-neutral100 border-b border-neutral300 p-4 rounded-lg flex">
-                  <Element />
-                </div>
-              )}
-              {request?.info?.discord && (
-                <div className="bg-neutral100 border-b border-neutral300 p-4 rounded-lg flex">
-                  <Discord request={request} />
-                </div>
-              )}
-              {request?.info?.twitter && (
-                <div className="bg-neutral100 border-b border-neutral300 p-4 rounded-lg flex">
-                  <Twitter request={request} />
-                </div>
-              )}
-              {request?.info?.github && (
-                <div className="bg-neutral100 border-b border-neutral300 p-4 rounded-lg flex">
-                  <Github request={request} />
-                </div>
-              )}
-            </>
-          )}
-        </div>
+        {!realAddress ? (
+          <div className="h-full flex items-center justify-center">
+            <NoWalletConnected text="Connect wallet to see your judgement request." />
+          </div>
+        ) : (
+          <JudgementPageContent />
+        )}
       </ListLayout>
     </PeopleCommonProvider>
   );
