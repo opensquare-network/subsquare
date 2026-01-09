e9c6bb3f99f7aa385bfa5e8b9f81e2fc0d82b412
leochen <leochen.sat24@gmail.com>
2026-01-09 11:36:28 +0800

    Veted transfer button, #7082

---
+++ packages/next-common/components/data/vesting/newVest/index.jsx
@@ -0,0 +1,11 @@
+import PrimaryButton from "next-common/lib/button/primary";
+
+export default function NewVest() {
+  return (
+    <div className="inline-flex ml-2">
+      <PrimaryButton size="small" onClick={() => {}}>
+        Vested Transfer
+      </PrimaryButton>
+    </div>
+  );
+}
