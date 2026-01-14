fd0c054be352d4d9e6ffb0f899a17c24d147941e
chaojun <chaojun@opensquare.network>
2026-01-14 16:54:15 +0800

    fix:  add people check on Page, #6881

--- packages/next/pages/people/usernames/index.jsx
+++ packages/next/pages/people/usernames/index.jsx
@@ -1,12 +1,20 @@
 import dynamicClientOnly from "next-common/lib/dynamic/clientOnly";
 import { PeopleGlobalProvider } from "..";
+import getChainSettings from "next-common/utils/consts/settings";
+import { CHAIN } from "next-common/utils/constants";
 export { getServerSideProps } from "../index";
 
 const PeopleUsernamesPageImpl = dynamicClientOnly(() =>
   import("next-common/components/people/usernames"),
 );
 
+const isPeopleSupported = !!getChainSettings(CHAIN).modules?.people;
+
 export default function PeopleUsernamesPage() {
+  if (!isPeopleSupported) {
+    return null;
+  }
+
   return (
     <PeopleGlobalProvider>
       <PeopleUsernamesPageImpl />
