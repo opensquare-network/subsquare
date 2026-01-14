c3cf74f5494460df434d6a1875b168cc6dd750cf
Chaojun Huang <chaojun@opensquare.network>
2026-01-14 18:09:43 +0800

    As an user, I want to see spend statistics of a bounty, #7027 (#7049)
    * Add bounty statistics tab, #7027
    
    * Update, #7027
    
    * fix: outlables, #7027
    
    * Add click to show curator proposals details popup, #7027
    
    * Update, #7027
    
    * refactor, #7027
    
    * Update, #7027
    
    * Add bar chart, #7027
    
    * refactor, #7027
    
    * fix: mb layout, #7027
    
    * fix mb layout, #7027
    
    * fix, #7027
    
    * Add beneficiaries tab, #7027
    
    * improve bar label layout, #7027
    
    * fix, #7027
    
    * Sort bars, #7027
    
    * Popup title, #7027
    
    * Add curator vs others chart, #7027
    
    * refactor, #7027
    
    * Update layout, #7027
    
    * Update, #7027
    
    * fix layout, #7027
    
    * Other -> Others, #7027
    
    * Hide categories list, #7027

---
+++ packages/next-common/components/pages/components/bounty/bountyStatistics/proposalsPopup/content.jsx
@@ -0,0 +1,83 @@
+import ValueDisplay from "next-common/components/valueDisplay";
+import { toPrecision } from "next-common/utils";
+import SummaryLayout from "next-common/components/summary/layout/layout";
+import SummaryItem from "next-common/components/summary/layout/item";
+import Link from "next-common/components/link";
+import { cn } from "next-common/utils";
+import normalizeChildBountyListItem from "next-common/utils/viewfuncs/treasury/normalizeChildBountyListItem";
+import { childBountyColumnsDef } from "./columns";
+import { useAsync } from "react-use";
+import { backendApi } from "next-common/services/nextApi";
+import ItemsList from "./itemsList";
+import { usePost } from "next-common/context/post";
+
+export function PostTitle({ url, index, title, noLink, className }) {
+  return (
+    <div className={cn("truncate max-w-[inherit] text-textPrimary", className)}>
+      <span>{`#${index}`}</span>
+      {noLink ? (
+        <span>{title}</span>
+      ) : (
+        <Link href={url} title={title}>
+          <span>{title}</span>
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default function PopupContent({ data }) {
+  const post = usePost();
+  const parentBountyId = post.bountyIndex;
+
+  const { value: childBounties, loading: childBountiesLoading } =
+    useAsync(async () => {
+      if (!data?.childBounties?.length) {
+        return [];
+      }
+      const { result } = await backendApi.fetch(
+        "treasury/child-bounties?simple=true&" +
+          data.childBounties
+            .map((item) => "ids=" + `${parentBountyId}_${item.index}`)
+            .join("&"),
+      );
+      if (!result) {
+        return [];
+      }
+      return result.map((item) => {
+        const payout =
+          data?.childBounties?.find((payout) => payout.index === item.index) ||
+          {};
+        const normalizedChildBounty = normalizeChildBountyListItem(
+          process.env.NEXT_PUBLIC_CHAIN,
+          item,
+        );
+        return {
+          ...normalizedChildBounty,
+          ...payout,
+        };
+      });
+    }, [data, parentBountyId]);
+
+  return (
+    <>
+      <Summary totalFiat={data?.totalPayoutFiatValue || 0} />
+      <ItemsList
+        items={childBounties}
+        loading={childBountiesLoading}
+        columnsDef={childBountyColumnsDef}
+        noDataText="No child bounties"
+      />
+    </>
+  );
+}
+
+function Summary({ totalFiat }) {
+  return (
+    <SummaryLayout>
+      <SummaryItem title="Total" className="[&>div>div:last-child]:flex">
+        <ValueDisplay value={toPrecision(totalFiat)} symbol="" prefix="$" />
+      </SummaryItem>
+    </SummaryLayout>
+  );
+}
