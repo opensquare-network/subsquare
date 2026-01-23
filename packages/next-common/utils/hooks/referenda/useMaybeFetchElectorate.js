cc8f483740aa1a1798c03259849e7601afd359fa
leochen <leochen.sat24@gmail.com>
2026-01-23 16:01:45 +0800

    Fetch finished democracy electorate from server #7148

--- packages/next-common/utils/hooks/referenda/useMaybeFetchElectorate.js
+++ packages/next-common/utils/hooks/referenda/useMaybeFetchElectorate.js
@@ -9,19 +9,17 @@ import {
 } from "../../../store/reducers/referendumSlice";
 import useDemocracyTally from "../../../context/post/democracy/referendum/tally";
 import useChainOrScanHeight from "next-common/hooks/height";
-import { useConditionalContextApi } from "next-common/context/migration/conditionalApi";
+import { useContextApi } from "next-common/context/api";
 
 export default function useMaybeFetchElectorate(referendum) {
-  const api = useConditionalContextApi();
+  const api = useContextApi();
   const dispatch = useDispatch();
   const electorate = useSelector(electorateSelector);
   const referendumStatus = useSelector(referendumStatusSelector);
   const nowHeight = useChainOrScanHeight();
   const tally = useDemocracyTally();
 
-  const { voteFinished, voteFinishedHeight } = extractVoteInfo(
-    referendum?.timeline,
-  );
+  const { voteFinished } = extractVoteInfo(referendum?.timeline);
   const possibleElectorate = referendumStatus?.tally?.electorate;
 
   useEffect(() => {
@@ -30,20 +28,16 @@ export default function useMaybeFetchElectorate(referendum) {
       return;
     }
 
-    if (!electorate && tally?.electorate) {
-      dispatch(setElectorate(tally.electorate));
-    }
-
     if (possibleElectorate) {
       dispatch(setElectorate(possibleElectorate));
       return;
     }
 
-    if (api) {
-      dispatch(fetchElectorate(api, voteFinishedHeight || nowHeight));
+    if (api && !voteFinished) {
+      dispatch(fetchElectorate(api, nowHeight));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [api, dispatch, voteFinishedHeight, nowHeight, possibleElectorate]);
+  }, [api, dispatch, nowHeight, possibleElectorate]);
 
   return electorate;
 }
