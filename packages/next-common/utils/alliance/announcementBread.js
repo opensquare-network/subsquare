194652cf22c4bcd295b729aa7df75bfd8806ea96
chaojun <chaojun@opensquare.network>
2026-02-11 17:20:44 +0800

    Undici has an unbounded decompression chain in HTTP responses on Node.js Fetch API via Content-Encoding leads to resource exhaustion, #147

--- packages/next-common/utils/alliance/announcementBread.js
+++ packages/next-common/utils/alliance/announcementBread.js
@@ -1,15 +1,27 @@
-import * as isIPFS from "is-ipfs";
+import { CID } from "multiformats/cid";
 
-export default function getAnnouncementBreadcrumbName(id = "", announcement = {}) {
+function isCID(value) {
+  try {
+    CID.parse(value);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+export default function getAnnouncementBreadcrumbName(
+  id = "",
+  announcement = {},
+) {
   const cid = announcement?.cid;
   if (cid) {
-    return `${ cid.slice(0, 4) }...${ cid.slice(-4) }`;
+    return `${cid.slice(0, 4)}...${cid.slice(-4)}`;
   }
 
   const maybeCid = id?.split("_").pop();
-  if (isIPFS.cid(maybeCid)) {
-    return `${ cid.slice(0, 4) }...${ cid.slice(-4) }`;
+  if (isCID(maybeCid)) {
+    return `${maybeCid.slice(0, 4)}...${maybeCid.slice(-4)}`;
   }
 
-  return `${ id.slice(0, 4) }...`;
+  return `${id.slice(0, 4)}...`;
 }
