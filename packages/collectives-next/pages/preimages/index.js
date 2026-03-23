755846e7bb33fa2fecacd59222a587807cb78810
chaojun <chaojun@opensquare.network>
2026-03-23 15:22:40 +0800

    fix: collectives preimages

--- packages/collectives-next/pages/preimages/index.js
+++ packages/collectives-next/pages/preimages/index.js
@@ -2,6 +2,7 @@ import ListLayout from "next-common/components/layout/ListLayout";
 import PreImagesList from "next-common/components/preImages/preImagesList";
 import { getServerSidePropsWithTracks } from "next-common/services/serverSide";
 import PreImagesFooter from "next-common/components/preImages/footer";
+import { PapiProvider } from "next-common/context/papi";
 
 export default function PreimagesPage() {
   const title = "Preimages";
@@ -14,7 +15,9 @@ export default function PreimagesPage() {
       description="Preimage can be submitted and stored on-chain against the hash later, upon the proposal's dispatch."
       summaryFooter={<PreImagesFooter />}
     >
-      <PreImagesList />
+      <PapiProvider>
+        <PreImagesList />
+      </PapiProvider>
     </ListLayout>
   );
 }
