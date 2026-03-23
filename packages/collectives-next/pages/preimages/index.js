a46fa6179c46a243642bf05ca6264f8c020142cd
chaojun <chaojun@opensquare.network>
2026-03-23 16:19:08 +0800

    Remove unused provider

--- packages/collectives-next/pages/preimages/index.js
+++ packages/collectives-next/pages/preimages/index.js
@@ -2,7 +2,6 @@ import ListLayout from "next-common/components/layout/ListLayout";
 import PreImagesList from "next-common/components/preImages/preImagesList";
 import { getServerSidePropsWithTracks } from "next-common/services/serverSide";
 import PreImagesFooter from "next-common/components/preImages/footer";
-import { PapiProvider } from "next-common/context/papi";
 
 export default function PreimagesPage() {
   const title = "Preimages";
@@ -15,9 +14,7 @@ export default function PreimagesPage() {
       description="Preimage can be submitted and stored on-chain against the hash later, upon the proposal's dispatch."
       summaryFooter={<PreImagesFooter />}
     >
-      <PapiProvider>
-        <PreImagesList />
-      </PapiProvider>
+      <PreImagesList />
     </ListLayout>
   );
 }
