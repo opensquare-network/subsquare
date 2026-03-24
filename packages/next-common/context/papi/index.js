b6580219072f8a5d1a81453136fe30305b84f4db
wolyshaw <wolyshaw@users.noreply.github.com>
2026-03-24 17:46:30 +0800

    summary

--- packages/next-common/context/papi/index.js
+++ packages/next-common/context/papi/index.js
@@ -1,20 +1,31 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { createClient } from "polkadot-api";
 import { getWsProvider } from "polkadot-api/ws-provider";
 import { currentNodeSelector } from "next-common/store/reducers/nodeSlice";
 import { useSelector } from "react-redux";
-import { getMetadata, setMetadata } from "next-common/utils/papiMetadataCache";
+import {
+  getMetadata as getCachedMetadata,
+  setMetadata,
+} from "next-common/utils/papiMetadataCache";
+import {
+  metadata as metadataCodec,
+  unifyMetadata,
+} from "@polkadot-api/substrate-bindings";
 
 const getPapi = async (currentEndpoint) => {
   const wsProvider = getWsProvider(currentEndpoint);
   const client = await createClient(wsProvider, {
-    getMetadata,
+    getMetadata: getCachedMetadata,
     setMetadata,
   });
   const api = client.getUnsafeApi();
+  const metadataRaw = await client._request("state_getMetadata", []);
+  const metadata = unifyMetadata(metadataCodec.dec(metadataRaw));
+  const pallets = metadata?.pallets ?? [];
   return {
     api,
     client,
+    pallets,
   };
 };
 
@@ -24,14 +35,16 @@ export function PapiProvider({ children }) {
   const currentEndpoint = useSelector(currentNodeSelector);
   const [api, setApi] = useState(null);
   const [client, setClient] = useState(null);
+  const [pallets, setPallets] = useState([]);
 
   useEffect(() => {
     if (!currentEndpoint) {
       return;
     }
-    getPapi(currentEndpoint).then(({ api, client }) => {
+    getPapi(currentEndpoint).then(({ api, client, pallets }) => {
       setApi(api);
       setClient(client);
+      setPallets(pallets);
     });
   }, [currentEndpoint]);
 
@@ -44,7 +57,7 @@ export function PapiProvider({ children }) {
   }, [client]);
 
   return (
-    <PapiContext.Provider value={{ api, client }}>
+    <PapiContext.Provider value={{ api, client, pallets }}>
       {children}
     </PapiContext.Provider>
   );
@@ -56,8 +69,32 @@ export function useContextPapiApi() {
 }
 
 export function useContextPapi() {
-  const { api, client } = useContext(PapiContext);
-  return { api, client };
+  const { api, client, pallets } = useContext(PapiContext);
+
+  const checkPallet = useCallback(
+    (palletName, storageName) => {
+      if (!palletName) {
+        return false;
+      }
+
+      const pallet = pallets?.find((item) => item.name === palletName);
+
+      if (!pallet) {
+        return false;
+      }
+
+      if (!storageName) {
+        return true;
+      }
+
+      return !!pallet?.storage?.items?.some(
+        (item) => item.name === storageName,
+      );
+    },
+    [pallets],
+  );
+
+  return { api, client, checkPallet };
 }
 
 export default PapiContext;
