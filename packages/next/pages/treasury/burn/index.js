3e1c9ee229cff50a632fe178547d95ad20363923
wolyshaw <wolyshaw@users.noreply.github.com>
2026-03-24 10:36:46 +0800

    Add openGov tracks props to burn page

--- packages/next/pages/treasury/burn/index.js
+++ packages/next/pages/treasury/burn/index.js
@@ -1,6 +1,7 @@
 import { withCommonProps } from "next-common/lib";
 import { useChainSettings } from "next-common/context/chain";
 import { backendApi } from "next-common/services/nextApi";
+import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 import { fetchList } from "next-common/services/list";
 import TreasuryBurn from "next-common/components/treasury/burn";
 import { TreasuryProvider } from "next-common/context/treasury";
@@ -19,14 +20,17 @@ export default function TreasuryBurnPage() {
 }
 
 export const getServerSideProps = withCommonProps(async (context) => {
-  const [{ result: burnSummary }, burnList, burnChart] = await Promise.all([
-    backendApi.fetch("/treasury/burnt/summary"),
-    fetchList("/treasury/burnt", context),
-    backendApi.fetch("/treasury/burnt/chart"),
-  ]);
+  const [tracksProps, { result: burnSummary }, burnList, burnChart] =
+    await Promise.all([
+      fetchOpenGovTracksProps(),
+      backendApi.fetch("/treasury/burnt/summary"),
+      fetchList("/treasury/burnt", context),
+      backendApi.fetch("/treasury/burnt/chart"),
+    ]);
 
   return {
     props: {
+      ...tracksProps,
       burnSummary: burnSummary ?? null,
       burnList: burnList ?? null,
       burnChart: burnChart ?? null,
