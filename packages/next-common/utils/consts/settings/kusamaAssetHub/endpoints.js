5854b12cc495a5da4477d702c7c26df1b33b360f
Milos Kriz <82968568+miloskriz@users.noreply.github.com>
2026-04-14 01:09:50 +0100

    Amend `asset-hub-kusama` endpoints by the IBP

--- packages/next-common/utils/consts/settings/kusamaAssetHub/endpoints.js
+++ packages/next-common/utils/consts/settings/kusamaAssetHub/endpoints.js
@@ -1,13 +1,13 @@
 const kusamaAssetHubNodes = [
   { name: "Parity", url: "wss://kusama-asset-hub-rpc.polkadot.io" },
-  { name: "IBP1", url: "wss://sys.ibp.network/asset-hub-kusama" },
+  { name: "IBP1", url: "wss://asset-hub-kusama.ibp.network" },
+  { name: "IBP2", url: "wss://asset-hub-kusama.dotters.network" },
   {
     name: "RadiumBlock",
     url: "wss://statemine.public.curie.radiumblock.co/ws",
   },
   { name: "Dwellir", url: "wss://asset-hub-kusama-rpc.n.dwellir.com" },
   { name: "Dwellir Tunisia", url: "wss://statemine-rpc-tn.dwellir.com" },
-  { name: "IBP2", url: "wss://asset-hub-kusama.dotters.network/" },
   { name: "LuckyFriday", url: "wss://rpc-asset-hub-kusama.luckyfriday.io" },
   { name: "Stakeworld", url: "wss://ksm-rpc.stakeworld.io/assethub" },
   {
