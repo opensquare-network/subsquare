9bc6728c116d15f5dd7d06b9d480365b7b80912e
chaojun <chaojun@opensquare.network>
2026-05-25 11:35:36 +0800

    fix: calendar beneficiary field

--- packages/next-common/components/calendar/dayEvents/eventInfoCard/infoItem/beneficiaryItem.js
+++ packages/next-common/components/calendar/dayEvents/eventInfoCard/infoItem/beneficiaryItem.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { ItemWrapper } from "./styled";
 import AddressUser from "next-common/components/user/addressUser";
+import { useChain } from "next-common/context/chain";
+import { extractAddressFromXcmAccountLocation } from "next-common/utils/xcm/address";
 
 export default function BeneficiaryItem({ beneficiary }) {
+  const chain = useChain();
+  const address = extractAddressFromXcmAccountLocation(beneficiary, chain);
+
+  if (!address) {
+    return null;
+  }
+
   return (
     <ItemWrapper>
       <span>Beneficiary:</span>
       <AddressUser
-        add={beneficiary}
-        className="text12Medium text-[var(--sapphire500)]"
+        add={address}
+        className="text12Medium text-sapphire500"
         showAvatar={false}
       />
     </ItemWrapper>
