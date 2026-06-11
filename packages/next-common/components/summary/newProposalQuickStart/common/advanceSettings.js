9fd9e8337a0d816cea3ec57aa831b637f0ce7120
chaojun <chaojun@opensquare.network>
2026-06-11 16:35:16 +0800

    Rename, #7355

--- packages/next-common/components/summary/newProposalQuickStart/common/advanceSettings.js
+++ packages/next-common/components/summary/newProposalQuickStart/common/advanceSettings.js
@@ -7,7 +7,7 @@ import SecondaryButton from "next-common/lib/button/secondary";
 export default function AdvanceSettings({ children, defaultShow = false }) {
   const [showAdvanced, setShowAdvanced] = useState(defaultShow);
   return (
-    <div className="has-[[data-content]:empty]:hidden">
+    <div className="has-[[data-advance-settings-content]:empty]:hidden">
       <div className="flex items-center justify-center w-full gap-4">
         <Divider className="flex-1" />
 
@@ -30,7 +30,10 @@ export default function AdvanceSettings({ children, defaultShow = false }) {
 
         <Divider className="flex-1" />
       </div>
-      <div data-content className={showAdvanced ? "space-y-4" : "hidden"}>
+      <div
+        data-advance-settings-content
+        className={showAdvanced ? "space-y-4" : "hidden"}
+      >
         {children}
       </div>
     </div>
