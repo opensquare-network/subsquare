fe248def4988fc56d11a54569b99f614a10f4148
chaojun <chaojun@opensquare.network>
2026-06-16 18:43:34 +0800

    Remove link

--- packages/next-common/components/data/recovery/index.jsx
+++ packages/next-common/components/data/recovery/index.jsx
@@ -4,7 +4,7 @@ import RecoveryExplorerTable from "./table";
 export default function RecoveryExplorer() {
   return (
     <>
-      <PageHeader href="https://wiki.polkadot.com/kusama/kusama-social-recovery/" />
+      <PageHeader />
       <RecoveryExplorerTable />
     </>
   );
