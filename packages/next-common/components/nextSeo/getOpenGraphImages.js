26bca2b1b777d3edcd6a2c6dc1afba90e8f0354e
chaojun <chaojun@opensquare.network>
2026-07-20 18:25:12 +0800

    Remove small image since it is not used anymore

--- packages/next-common/components/nextSeo/getOpenGraphImages.js
+++ packages/next-common/components/nextSeo/getOpenGraphImages.js
@@ -4,35 +4,27 @@ import { STATICS_CDN_URL } from "next-common/utils/consts/statics";
 const CHAIN_COVER_FILENAMES_MAP = {
   [Chains.hydradx]: {
     large: "hydration.jpg",
-    small: "hydration-small.jpg",
   },
   [Chains.hydradxTestnet]: {
     large: "hydration.jpg",
-    small: "hydration-small.jpg",
   },
   [Chains.zkverifyTestnet]: {
     large: "zkverify.jpg",
-    small: "zkverify-small.jpg",
   },
   [Chains.zkverify]: {
     large: "zkverify.jpg",
-    small: "zkverify-small.jpg",
   },
   [Chains.kusamaAssetHub]: {
     large: "kusama-asset-hub.jpg",
-    small: "kusama-asset-hub-small.jpg",
   },
   [Chains.westendAssetHub]: {
     large: "westend-asset-hub.jpg",
-    small: "westend-asset-hub-small.jpg",
   },
   [Chains.litentry]: {
     large: "heima.jpg",
-    small: "heima-small.jpg",
   },
   [Chains.polkadot]: {
     large: "polkadot-v2.jpg",
-    small: "polkadot-small.jpg",
   },
 };
 
@@ -44,7 +36,6 @@ function getCoverFilenames(chainValue) {
   return (
     CHAIN_COVER_FILENAMES_MAP[chainValue] || {
       large: `${chainValue}.jpg`,
-      small: `${chainValue}-small.jpg`,
     }
   );
 }
@@ -54,6 +45,5 @@ export function getOpenGraphImages(chainValue) {
 
   return {
     large: resolveCoversRepoFilepathUrl(coverFilenames.large),
-    small: resolveCoversRepoFilepathUrl(coverFilenames.small),
   };
 }
