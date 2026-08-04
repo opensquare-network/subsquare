f100183b19d86defa884140c6b95ec619895cbfc
chaojun <chaojun@opensquare.network>
2026-08-04 18:25:34 +0800

    fix: ssr improvement for secretary claimants list

--- packages/next-common/components/secretary/statistics/claimants/index.jsx
+++ packages/next-common/components/secretary/statistics/claimants/index.jsx
@@ -1,13 +1,9 @@
 import { useMemo } from "react";
 import DataList from "next-common/components/dataList";
-import { backendApi } from "next-common/services/nextApi";
-import { fellowshipStatisticsMembersApi } from "next-common/services/url";
 import { defaultPageSize } from "next-common/utils/constants";
-import { useAsync } from "react-use";
 import { useStatisticsClaimantColumn } from "../../../fellowship/statistics/expenditure/claimants/columns/claimant";
 import { useStatisticsClaimantsCyclesColumn } from "../../../fellowship/statistics/expenditure/claimants/columns/cycles";
 import { useStatisticsClaimantsRankColumn } from "../../../fellowship/statistics/expenditure/claimants/columns/rank";
-import { useState, useEffect } from "react";
 import usePaginationComponent from "next-common/components/pagination/usePaginationComponent";
 import { SecondaryCard } from "next-common/components/styled/containers/secondaryCard";
 import { StatisticsTitle } from "next-common/components/statistics/styled.js";
@@ -141,14 +137,21 @@ function useSecretaryClaimantsPaidColumn(paymentReferenda) {
 
 function SecretaryClaimantsTable({
   members = [],
-  membersApi = fellowshipStatisticsMembersApi,
   paymentReferenda = [],
+  membersData = [],
+  membersLoading = false,
 }) {
-  const [total, setTotal] = useState(0);
-  const [processedData, setProcessedData] = useState([]);
-  const [rowData, setRowData] = useState([]);
-  const [tableLoading, setTableLoading] = useState(false);
   const pageSize = defaultPageSize;
+
+  const processedData = useMemo(
+    () =>
+      membersLoading || !members
+        ? membersData
+        : handleClaimantsData(membersData, members),
+    [membersData, members, membersLoading],
+  );
+
+  const total = processedData.length;
   const { page, component: pageComponent } = usePaginationComponent(
     total,
     pageSize,
@@ -162,51 +165,20 @@ function SecretaryClaimantsTable({
     useSecretaryClaimantsPaidColumn(paymentReferenda),
   ];
 
-  const { value: originalMembers } = useAsync(async () => {
-    setTableLoading(true);
-    if (!membersApi) {
+  const rowData = useMemo(() => {
+    if (processedData.length <= 0) {
       return [];
     }
-    try {
-      const resp = await backendApi.fetch(membersApi, {
-        page,
-        pageSize: defaultPageSize,
-      });
-      return resp?.result || [];
-    } catch {
-      setTableLoading(false);
-      return [];
-    }
-  }, [membersApi]);
-
-  useEffect(() => {
-    if (originalMembers && members) {
-      const processed = handleClaimantsData(originalMembers, members);
-      setProcessedData(processed);
-      setTotal(processed.length);
-      setTableLoading(false);
-    }
-  }, [originalMembers, members]);
-
-  useEffect(() => {
-    if (processedData.length > 0) {
-      const paginatedData = paginateData(processedData, page, defaultPageSize);
-      const rows = paginatedData.map((item, idx) =>
-        columns.map((col) => col.cellRender(item, idx)),
-      );
-      setRowData(rows);
-    }
+    const paginatedData = paginateData(processedData, page, defaultPageSize);
+    return paginatedData.map((item, idx) =>
+      columns.map((col) => col.cellRender(item, idx)),
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [processedData, page]);
 
   return (
     <div>
-      <DataList
-        noDataText="No data"
-        loading={tableLoading}
-        columns={columns}
-        rows={rowData}
-      />
+      <DataList noDataText="No data" columns={columns} rows={rowData} />
       {pageComponent}
     </div>
   );
@@ -214,8 +186,9 @@ function SecretaryClaimantsTable({
 
 export default function SecretaryStatisticsClaimants({
   members = [],
-  membersApi = fellowshipStatisticsMembersApi,
+  membersLoading = false,
   paymentReferenda = [],
+  membersData = [],
 }) {
   return (
     <SecondaryCard>
@@ -223,8 +196,9 @@ export default function SecretaryStatisticsClaimants({
         <StatisticsTitle>Top Beneficiary</StatisticsTitle>
         <SecretaryClaimantsTable
           members={members}
-          membersApi={membersApi}
+          membersLoading={membersLoading}
           paymentReferenda={paymentReferenda}
+          membersData={membersData}
         />
       </div>
     </SecondaryCard>
