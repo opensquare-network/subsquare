5b709f929e53aced18f66d191fb016ae8d51dd7b
chaojun <chaojun@opensquare.network>
2026-08-07 18:32:49 +0800

    Update account column, #7443

--- packages/next-common/components/assethubMigrationAssets/liquidPools/table/columns/account.jsx
+++ packages/next-common/components/assethubMigrationAssets/liquidPools/table/columns/account.jsx
@@ -1,16 +1,12 @@
 import Link from "next-common/components/link";
-import Tooltip from "next-common/components/tooltip";
-import { addressEllipsis } from "next-common/utils";
+import AddressUser from "next-common/components/user/addressUser";
 
 export const colAccount = {
   name: "Account",
   style: { textAlign: "left", width: "140px", minWidth: "140px" },
   render: (item) => (
-    <Link
-      href={`/user/${item.owner}`}
-      className="text14Medium text-theme500 hover:text-theme500"
-    >
-      <Tooltip content={item.owner}>{addressEllipsis(item.owner)}</Tooltip>
+    <Link href={`/user/${item.owner}`} className="text14Medium">
+      <AddressUser add={item.owner} />
     </Link>
   ),
 };
