bcc869ffb6e3851fe1754f74b02fdd95637fa435
wolyshaw <wolyshaw@users.noreply.github.com>
2026-08-07 15:27:54 +0800

    Fix fellowship feeds filters and identity search navigation (#7442)
    * load OpenGov tracks on fellowship feeds page
    
    * correct keyboard navigation for identity search result

--- packages/next-common/services/serverSide/fellowship/feeds.js
+++ packages/next-common/services/serverSide/fellowship/feeds.js
@@ -2,22 +2,26 @@ import { withCommonProps } from "next-common/lib";
 import { backendApi } from "next-common/services/nextApi";
 import { getFeedsEvent } from "next-common/utils/fellowship/getFeedsEvent";
 import { sectionMap } from "next-common/utils/consts/fellowship/feeds";
+import { fetchOpenGovTracksProps } from "next-common/services/serverSide";
 
 const defaultPageSize = 25;
 
 const getFellowshipFeedsServerSideProps = withCommonProps(async (context) => {
   const { page = 1, section = "", event = "", who = "" } = context.query;
 
-  const { result: feeds } = await backendApi.fetch("fellowship/feeds", {
-    page,
-    pageSize: defaultPageSize,
-    section: sectionMap[section] || section,
-    event: getFeedsEvent(section, event),
-    who,
-  });
-
+  const [tracksProps, { result: feeds }] = await Promise.all([
+    fetchOpenGovTracksProps(),
+    backendApi.fetch("fellowship/feeds", {
+      page,
+      pageSize: defaultPageSize,
+      section: sectionMap[section] || section,
+      event: getFeedsEvent(section, event),
+      who,
+    }),
+  ]);
   return {
     props: {
+      ...tracksProps,
       feeds: feeds || [],
     },
   };
