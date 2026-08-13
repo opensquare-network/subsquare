04ddea4918d391b45e6e7b9ae116208b1fc8d308
chaojun <chaojun@opensquare.network>
2026-08-13 10:43:52 +0800

    fix: fee display

--- packages/next-common/components/hydrationCrossChainPopup/crossChainFeeSummary.jsx
+++ packages/next-common/components/hydrationCrossChainPopup/crossChainFeeSummary.jsx
@@ -1,8 +1,25 @@
+import BigNumber from "bignumber.js";
 import LoadableContent from "next-common/components/common/loadableContent";
-import ValueDisplay from "next-common/components/valueDisplay";
 import { GreyPanel } from "next-common/components/styled/containers/greyPanel";
+import Tooltip from "next-common/components/tooltip";
 import { toPrecision } from "next-common/utils";
 import { isNil } from "lodash-es";
+
+function toFeePrecision(value, decimals) {
+  const normalized = new BigNumber(value).dividedBy(10 ** decimals);
+  let sig = 4;
+  if (normalized.gt(1)) {
+    const intPartLen = Math.ceil(Math.log10(Number(normalized) + 1));
+    sig = Math.min(
+      21,
+      (normalized.gt(99999.9999) ? 0 : normalized.gt(999.9999) ? 2 : 4) +
+        intPartLen,
+    );
+  }
+  return new Intl.NumberFormat("en-US", {
+    maximumSignificantDigits: sig,
+  }).format(Number(normalized));
+}
 
 function FeeRow({ title, fee, loading }) {
   return (
@@ -12,10 +29,16 @@ function FeeRow({ title, fee, loading }) {
         {isNil(fee) ? (
           <span>-</span>
         ) : (
-          <ValueDisplay
-            value={toPrecision(fee.amount, fee.decimals, 4)}
-            symbol={fee.symbol}
-          />
+          <Tooltip
+            content={`${toPrecision(fee.amount, fee.decimals)} ${fee.symbol}`}
+          >
+            <span className="inline-flex items-center gap-x-1">
+              {toFeePrecision(fee.amount, fee.decimals)}
+              <span className="value-display-symbol text-textTertiary">
+                {fee.symbol}
+              </span>
+            </span>
+          </Tooltip>
         )}
       </LoadableContent>
     </div>
