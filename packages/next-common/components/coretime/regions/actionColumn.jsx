798e17f61cb370a59c8a61a1cd577a50a273e5f3
wolyshaw <wolyshaw@users.noreply.github.com>
2026-09-14 13:49:26 +0800

    improve actions button, #7532

--- packages/next-common/components/coretime/regions/actionColumn.jsx
+++ packages/next-common/components/coretime/regions/actionColumn.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { isNil } from "lodash-es";
 import * as Popover from "@radix-ui/react-popover";
 import { SystemMore } from "@osn/icons/subsquare";
 import SecondaryButton from "next-common/lib/button/secondary";
@@ -6,13 +7,44 @@ import {
   OptionItem,
   OptionWrapper,
 } from "next-common/components/internalDropdown/styled";
+import useCoretimeStatus from "next-common/context/coretime/status";
+import { isSameAddress } from "next-common/utils";
+import useRealAddress from "next-common/utils/hooks/useRealAddress";
 import { RegionStatus } from "./utils";
 
 export default function RegionActionColumn({ region, onAction }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const realAddress = useRealAddress();
+  const lastCommittedTimeslice = useCoretimeStatus()?.lastCommittedTimeslice;
+  const isOwner = isSameAddress(region.owner, realAddress);
   const isExpired = region.status === RegionStatus.Expired;
+  const canSchedule =
+    isOwner &&
+    !isExpired &&
+    !isNil(lastCommittedTimeslice) &&
+    region.end > lastCommittedTimeslice + 1;
+  const availableActions = [
+    { action: "details", title: "Details", canOpen: true },
+    { action: "assign", title: "Assign", canOpen: canSchedule },
+    { action: "pool", title: "Pool", canOpen: canSchedule },
+    { action: "transfer", title: "Transfer", canOpen: isOwner },
+    {
+      action: "partition",
+      title: "Partition",
+      canOpen: isOwner && region.end - region.begin > 1,
+    },
+    {
+      action: "interlace",
+      title: "Interlace",
+      canOpen: isOwner && region.parts > 1,
+    },
+  ].filter(({ canOpen }) => canOpen);
 
   const handleSelectAction = (nextAction) => {
+    if (!availableActions.some(({ action }) => action === nextAction)) {
+      return;
+    }
+
     setIsMenuOpen(false);
     onAction({ action: nextAction, region });
   };
@@ -33,58 +65,17 @@ export default function RegionActionColumn({ region, onAction }) {
             className="z-50"
           >
             <OptionWrapper className="static !shadow-200">
-              <OptionItem
-                as="button"
-                type="button"
-                className="w-full"
-                onClick={() => handleSelectAction("details")}
-              >
-                Details
-              </OptionItem>
-              <OptionItem
-                as="button"
-                type="button"
-                className="w-full disabled:cursor-not-allowed disabled:text-textDisabled"
-                disabled={isExpired}
-                onClick={() => handleSelectAction("assign")}
-              >
-                Assign
-              </OptionItem>
-              <OptionItem
-                as="button"
-                type="button"
-                className="w-full disabled:cursor-not-allowed disabled:text-textDisabled"
-                disabled={isExpired}
-                onClick={() => handleSelectAction("pool")}
-              >
-                Pool
-              </OptionItem>
-              <OptionItem
-                as="button"
-                type="button"
-                className="w-full"
-                onClick={() => handleSelectAction("transfer")}
-              >
-                Transfer
-              </OptionItem>
-              <OptionItem
-                as="button"
-                type="button"
-                className="w-full disabled:cursor-not-allowed disabled:text-textDisabled"
-                disabled={region.end - region.begin <= 1}
-                onClick={() => handleSelectAction("partition")}
-              >
-                Partition
-              </OptionItem>
-              <OptionItem
-                as="button"
-                type="button"
-                className="w-full disabled:cursor-not-allowed disabled:text-textDisabled"
-                disabled={region.parts <= 1}
-                onClick={() => handleSelectAction("interlace")}
-              >
-                Interlace
-              </OptionItem>
+              {availableActions.map(({ action, title }) => (
+                <OptionItem
+                  key={action}
+                  $as="button"
+                  type="button"
+                  className="w-full"
+                  onClick={() => handleSelectAction(action)}
+                >
+                  {title}
+                </OptionItem>
+              ))}
             </OptionWrapper>
           </Popover.Content>
         </Popover.Portal>
